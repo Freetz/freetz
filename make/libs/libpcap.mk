24a2da435d8672d2b8cbbe2dcacc5b241e061ba8
Oliver Metz <oliver@freetz.org>
2007-02-23 15:00:33 +0000

    nomsg
    git-svn-id: http://svn.freetz.org/branches/0.2.9_26@33 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/libpcap.mk
+++ make/libs/libpcap.mk
@@ -11,7 +11,7 @@ $(DL_DIR)/$(LIBPCAP_SOURCE):
 $(LIBPCAP_DIR)/.unpacked: $(DL_DIR)/$(LIBPCAP_SOURCE)
 	tar -C $(SOURCE_DIR) $(VERBOSE) -xzf $(DL_DIR)/$(LIBPCAP_SOURCE)
 	for i in $(LIBPCAP_MAKE_DIR)/patches/*.libpcap.patch; do \
-		patch -d $(LIBPCAP_DIR) -p0 < $$i; \
+		patch -d $(LIBPCAP_DIR) -p1 < $$i; \
 	done
 	touch $@
 
@@ -21,7 +21,7 @@ $(LIBPCAP_DIR)/.configured: $(LIBPCAP_DIR)/.unpacked
 		PATH="$(TARGET_TOOLCHAIN_PATH)" \
 		CFLAGS="$(TARGET_CFLAGS) -I$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include" \
 		CPPFLAGS="-I$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include" \
-		LDSHARED="$(TARGET_CC) -static-libgcc -shared -Wl,-soname,libpcap.so.0.8" \
+		LDFLAGS="-static-libgcc -L$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib" \
 		ac_cv_linux_vers=2.6.13.1 \
 		./configure \
 		--target=$(GNU_TARGET_NAME) \
@@ -44,20 +44,35 @@ $(LIBPCAP_DIR)/.configured: $(LIBPCAP_DIR)/.unpacked
 		--with-pcap=linux \
 		$(DISABLE_NLS) \
 		$(DISABLE_LARGEFILE) \
+		$(DISABLE_LZO) \
+		--enable-shared \
+		--enable-static \
+		--disable-pthread \
+		--enable-debug \
+		--disable-plugins \
+		--disable-management \
+		--disable-socks \
+		--disable-http \
+		--enable-password-save \
+		--enable-small \
+		--enable-shared \
+		--enable-static \
+		--disable-yydebug \
+		--with-build-cc="$(HOSTCC)" \
 	);
 	touch $@
 
 $(LIBPCAP_DIR)/.compiled: $(LIBPCAP_DIR)/.configured
 	PATH=$(TARGET_TOOLCHAIN_PATH) $(MAKE) \
 		-C $(LIBPCAP_DIR) \
-		$(TARGET_CONFIGURE_OPTS) 
+		$(TARGET_CONFIGURE_OPTS) all
 	touch $@
 
 $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcap.so: $(LIBPCAP_DIR)/.compiled
 	PATH=$(TARGET_TOOLCHAIN_PATH) $(MAKE) \
 		-C $(LIBPCAP_DIR) \
 		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
-		install install-shared
+		install
 	touch -c $@
 
 ifeq ($(strip $(DS_EXTERNAL_COMPILER)),y)
@@ -80,9 +95,11 @@ libpcap-install: libpcap-precompiled
 
 libpcap-uninstall:
 	rm -rf root/usr/lib/libpcap*.so*
-	PATH=$(TARGET_TOOLCHAIN_PATH) $(MAKE) -C $(LIBPCAP_DIR) DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" uninstall
+	rm -f $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcap*.so*
+	rm -f $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcap.a
 
 libpcap-dirclean:
-	PATH=$(TARGET_TOOLCHAIN_PATH) $(MAKE) -C $(LIBPCAP_DIR) DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" uninstall
+	rm -f $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcap*.so*
+	rm -f $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcap.a
 	rm -rf $(LIBPCAP_DIR)
 
