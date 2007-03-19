7afd5b498943e446bbda9cf11d81e72c184c961c
Alexander Kriegisch <kriegaex@freetz.org>
2007-03-19 22:28:04 +0000

    - fix precompiled toolchain download URL in toolchain/make/download-toolchain.mk - add forgotten cpmaccfg in make/Config.in - remove precompiled libs from root/lib
    git-svn-id: http://svn.freetz.org/trunk@84 f5190166-0702-4917-9039-51ec32eddaf5

--- toolchain/make/download-toolchain.mk
+++ toolchain/make/download-toolchain.mk
@@ -2,7 +2,7 @@ KERNEL_TOOLCHAIN_VERSION:=0.1
 TARGET_TOOLCHAIN_VERSION:=0.1
 TARGET_TOOLCHAIN_SOURCE:=target-toolchain-dsmod-$(TARGET_TOOLCHAIN_VERSION).tar.lzma
 KERNEL_TOOLCHAIN_SOURCE:=kernel-toolchain-dsmod-$(KERNEL_TOOLCHAIN_VERSION).tar.lzma
-TOOLCHAIN_SITE:=https://dsmod.wirsind.info/precompiled
+TOOLCHAIN_SITE:=http://dsmod.wirsind.info/precompiled
 
 
 $(DL_DIR)/$(KERNEL_TOOLCHAIN_SOURCE):
@@ -15,12 +15,14 @@ download-toolchain: $(TOOLCHAIN_DIR)/kernel/.installed $(TOOLCHAIN_DIR)/target/.
 
 $(TOOLCHAIN_DIR)/kernel/.installed: $(DL_DIR)/$(KERNEL_TOOLCHAIN_SOURCE) $(TOOLS_DIR)/busybox
 	mkdir -p $(TOOLCHAIN_DIR)/build
+	rm -rf $(TOOLCHAIN_DIR)/kernel
 	$(TOOLS_DIR)/busybox tar -xaf $(DL_DIR)/$(KERNEL_TOOLCHAIN_SOURCE) -C $(TOOLCHAIN_DIR)/build
 	-@ln -s $(BUILD_DIR)/$(CROSSTOOL_COMPILER)/mipsel-unknown-linux-gnu $(TOOLCHAIN_DIR)/kernel
 	@touch $@
 
 $(TOOLCHAIN_DIR)/target/.installed: $(DL_DIR)/$(TARGET_TOOLCHAIN_SOURCE) $(TOOLS_DIR)/busybox
 	mkdir -p $(TOOLCHAIN_DIR)/build
+	rm -rf $(TOOLCHAIN_DIR)/target
 	$(TOOLS_DIR)/busybox tar -xaf $(DL_DIR)/$(TARGET_TOOLCHAIN_SOURCE) -C $(TOOLCHAIN_DIR)/build
 	-@ln -s $(BUILD_DIR)/$(TARGET_TOOLCHAIN_COMPILER)/$(REAL_GNU_TARGET_NAME) $(TOOLCHAIN_DIR)/target
 	@touch $@
