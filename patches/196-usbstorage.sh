3e2702d78fde3962ed98f94373608a94aef2e243
Alexander Kriegisch <kriegaex@freetz.org>
2007-03-19 21:29:04 +0000

    Re-creating trunk, branches and tags root directories.
    git-svn-id: http://svn.freetz.org/trunk@81 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/196-usbstorage.sh
+++
@@ -1,5 +0,0 @@
-if [ "$DS_PATCH_USBSTORAGE" == "y" ]
-then
-	[ "$DS_VERBOSITY_LEVEL" -ge 1 ] && echo "${L1}applying USB storage patch"
-	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage.patch"
-fi
--- patches/210-international.sh
+++
@@ -1,6 +0,0 @@
-if [ "$DS_PATCH_INTERNATIONAL" == "y" ]
-then
-	# from http://www.the-construct.com/
-	[ "$DS_VERBOSITY_LEVEL" -ge 1 ] && echo "${L1}applying international patch"
-	sed -i -e "s/LKZ 0/LKZ 1/g" "${HTML_MOD_DIR}/fon/sip1.js"
-fi
