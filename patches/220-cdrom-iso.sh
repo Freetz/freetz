124f97979fa680583852d1aab6732d9f916c9de7
Oliver Metz <oliver@freetz.org>
2007-04-27 21:36:46 +0000

    Add FritzBox Fon (06.04.30) to ds26
    git-svn-id: http://svn.freetz.org/trunk@236 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/220-cdrom-iso.sh
+++ patches/220-cdrom-iso.sh
@@ -2,11 +2,16 @@ if [ "$DS_REMOVE_CDROM_ISO" == "y" ]
 then
 	[ "$DS_VERBOSITY_LEVEL" -ge 1 ] && echo "${L1}removing cdrom.iso"
 	rm -f "${FILESYSTEM_MOD_DIR}/lib/modules/cdrom.iso"
+	sed -i -e 's/cdrom_fallback=0/cdrom_fallback=1/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
 	if [ "$DS_TYPE_FON_5050" == "y" ]
 	then
 		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/rc.S-no-cdrom-fallback_5050.patch"
 	else
-		sed -i -e 's/cdrom_fallback=0/cdrom_fallback=1/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
+	    if [ "$DS_TYPE_FON" == "y" ]
+	    then
+		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/rc.S-no-cdrom-fallback_fon.patch"
+	    else
 		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/rc.S-no-cdrom-fallback.patch"
+	    fi
 	fi
 fi
