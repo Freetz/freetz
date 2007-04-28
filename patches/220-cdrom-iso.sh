a699d61ad0ac255b582fe17f12172173e85b84c3
Alexander Kriegisch <kriegaex@freetz.org>
2007-04-28 06:47:07 +0000

    Bugfix: CRLF line endings led to a build error. During the bug hunting session, the whole file was reformatted, until finally the trivial formatting error was discovered. I then just left the formatting as it was.
    git-svn-id: http://svn.freetz.org/trunk@238 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/220-cdrom-iso.sh
+++ patches/220-cdrom-iso.sh
@@ -1,17 +1,12 @@
-if [ "$DS_REMOVE_CDROM_ISO" == "y" ]
-then
+if [ "$DS_REMOVE_CDROM_ISO" == "y" ]; then
 	[ "$DS_VERBOSITY_LEVEL" -ge 1 ] && echo "${L1}removing cdrom.iso"
 	rm -f "${FILESYSTEM_MOD_DIR}/lib/modules/cdrom.iso"
 	sed -i -e 's/cdrom_fallback=0/cdrom_fallback=1/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
-	if [ "$DS_TYPE_FON_5050" == "y" ]
-	then
+	if [ "$DS_TYPE_FON_5050" == "y" ]; then
 		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/rc.S-no-cdrom-fallback_5050.patch"
-	else
-	    if [ "$DS_TYPE_FON" == "y" ]
-	    then
+	elif [ "$DS_TYPE_FON" == "y" ]; then
 		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/rc.S-no-cdrom-fallback_fon.patch"
-	    else
+	else
 		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/rc.S-no-cdrom-fallback.patch"
-	    fi
 	fi
 fi
