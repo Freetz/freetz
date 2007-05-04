e5685192608d8678e53489ad0576518b3e54c496
Oliver Metz <oliver@freetz.org>
2007-05-04 19:45:10 +0000

    Bump Labor ECO version to 29.04.32-7153
    git-svn-id: http://svn.freetz.org/trunk@254 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/7170_labor/130-foncalls.sh
+++ patches/7170_labor/130-foncalls.sh
@@ -1,8 +1,9 @@
 [ "$DS_VERBOSITY_LEVEL" -ge 1 ] && echo "${L1}applying foncalls.patch"
 
-if [ "$DS_TYPE_LABOR_USB" == "y" ] || [ "$DS_TYPE_LABOR_VPN" == "y" ]
-then
+if [ "$DS_TYPE_LABOR_USB" == "y" ] || [ "$DS_TYPE_LABOR_VPN" == "y" ]; then
     modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/foncalls_7170_labor_usb.patch
+elif [ "$DS_TYPE_LABOR_ECO" == "y" ]; then
+    modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/foncalls_7170_labor_eco.patch
 else
     modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/foncalls_7170_labor.patch
 fi
--- patches/7170_labor/140-printk.sh
+++ patches/7170_labor/140-printk.sh
@@ -1,7 +1,5 @@
 [ "$DS_VERBOSITY_LEVEL" -ge 1 ] && echo "${L1}applying printk.patch"
-
-if [ "$DS_TYPE_LABOR_PHONE" == "y" ]
-then
+if [ "$DS_TYPE_LABOR_PHONE" == "y" ] || [ "$DS_TYPE_LABOR_ECO" == "y" ]; then
     modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/printk_7170_labor_phone.patch
 else
     modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/printk_7170_labor.patch
