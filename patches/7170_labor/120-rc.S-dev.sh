111f1869041d49e7f3725e37448eda50b1bd7150
Oliver Metz <oliver@freetz.org>
2007-05-10 17:01:53 +0000

    Fix wrong LF in patches/7170_labor
    git-svn-id: http://svn.freetz.org/trunk@297 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/7170_labor/120-rc.S-dev.sh
+++ patches/7170_labor/120-rc.S-dev.sh
@@ -1,6 +1,6 @@
 [ "$DS_VERBOSITY_LEVEL" -ge 1 ] && echo "${L1}applying rc.S-dev.patch"
 if [ "$DS_TYPE_LABOR_PHONE" == "y" ] || [ "$DS_TYPE_LABOR_ECO" == "y" ]; then
     modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/rc.S-dev_7170_labor_phone.patch
-else
+    else
     modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/rc.S-dev_7170_labor.patch
 fi
