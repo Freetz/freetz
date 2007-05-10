61ae0d39b1aae92b8b6762d2e2bc746485134366
Oliver Metz <oliver@freetz.org>
2007-05-10 14:03:14 +0000

    Harmonizing Targets
    git-svn-id: http://svn.freetz.org/trunk@294 f5190166-0702-4917-9039-51ec32eddaf5

--- tools/make/busybox-tools.mk
+++ tools/make/busybox-tools.mk
@@ -1,44 +1,43 @@
 BUSYBOX_TOOLS_MAKE_DIR:=$(TOOLS_DIR)/make
-BUSYBOX_TOOLS_DIR:=$(SOURCE_DIR)/busybox-host
-
+BUSYBOX_TOOLS_DIR:=$(SOURCE_DIR)/busybox-tools
 BUSYBOX_TOOLS_CONFIG_FILE:=$(BUSYBOX_TOOLS_MAKE_DIR)/Config.busybox
-BUSYBOX_VERSION:=1.4.1
-BUSYBOX_SOURCE:=busybox-$(BUSYBOX_VERSION).tar.bz2
-BUSYBOX_SITE:=http://www.busybox.net/downloads
-BUSYBOX_HOST_DIR:=busybox
-BUSYBOX_HOST_BINARY:=busybox
+BUSYBOX_TOOLS_VERSION:=1.4.2
+BUSYBOX_TOOLS_SOURCE:=busybox-$(BUSYBOX_VERSION).tar.bz2
+BUSYBOX_TOOLS_SITE:=http://www.busybox.net/downloads
+BUSYBOX_TOOLS_DIR:=busybox
+BUSYBOX_TOOLS_BINARY:=busybox
 
 # Activate on demand to avoid collision with identical target for regular
 # busybox package
 ifneq ($(strip $(DS_HAVE_DOT_CONFIG)),y)
-$(DL_DIR)/$(BUSYBOX_SOURCE): $(DL_DIR)
-	wget -P $(DL_DIR) $(BUSYBOX_SITE)/$(BUSYBOX_SOURCE)
+$(DL_DIR)/$(BUSYBOX_TOOLS_SOURCE): $(DL_DIR)
+	wget -P $(DL_DIR) $(BUSYBOX_TOOLS_SITE)/$(BUSYBOX_TOOLS_SOURCE)
 endif
 
-$(BUSYBOX_TOOLS_DIR)/.unpacked: $(DL_DIR)/$(BUSYBOX_SOURCE)
+$(BUSYBOX_TOOLS_DIR)/.unpacked: $(DL_DIR)/$(BUSYBOX_TOOLS_SOURCE)
 	mkdir -p $(BUSYBOX_TOOLS_DIR)
-	tar -C $(BUSYBOX_TOOLS_DIR) $(VERBOSE) -xjf $(DL_DIR)/$(BUSYBOX_SOURCE)
+	tar -C $(BUSYBOX_TOOLS_DIR) $(VERBOSE) -xjf $(DL_DIR)/$(BUSYBOX_TOOLS_SOURCE)
 	for i in $(BUSYBOX_TOOLS_MAKE_DIR)/patches/*.busybox.patch; do \
-		patch -d $(BUSYBOX_TOOLS_DIR)/busybox-$(BUSYBOX_VERSION) -p0 < $$i; \
+		patch -d $(BUSYBOX_TOOLS_DIR)/busybox-$(BUSYBOX_TOOLS_VERSION) -p0 < $$i; \
 	done
 	touch $@
 
 $(BUSYBOX_TOOLS_DIR)/.configured: $(BUSYBOX_TOOLS_DIR)/.unpacked $(BUSYBOX_TOOLS_CONFIG_FILE)
-	cp $(BUSYBOX_TOOLS_CONFIG_FILE) $(BUSYBOX_TOOLS_DIR)/busybox-$(BUSYBOX_VERSION)/.config
-	$(MAKE) -C $(BUSYBOX_TOOLS_DIR)/busybox-$(BUSYBOX_VERSION) oldconfig
+	cp $(BUSYBOX_TOOLS_CONFIG_FILE) $(BUSYBOX_TOOLS_DIR)/busybox-$(BUSYBOX_TOOLS_VERSION)/.config
+	$(MAKE) -C $(BUSYBOX_TOOLS_DIR)/busybox-$(BUSYBOX_TOOLS_VERSION) oldconfig
 	touch $@
 
-$(BUSYBOX_TOOLS_DIR)/busybox-$(BUSYBOX_VERSION)/busybox: $(BUSYBOX_TOOLS_DIR)/.configured
-	$(MAKE) -C $(BUSYBOX_TOOLS_DIR)/busybox-$(BUSYBOX_VERSION)
+$(BUSYBOX_TOOLS_DIR)/busybox-$(BUSYBOX_TOOLS_VERSION)/busybox: $(BUSYBOX_TOOLS_DIR)/.configured
+	$(MAKE) -C $(BUSYBOX_TOOLS_DIR)/busybox-$(BUSYBOX_TOOLS_VERSION)
 
-$(TOOLS_DIR)/busybox: $(BUSYBOX_TOOLS_DIR)/busybox-$(BUSYBOX_VERSION)/busybox
-	cp $(BUSYBOX_TOOLS_DIR)/busybox-$(BUSYBOX_VERSION)/$(BUSYBOX_HOST_BINARY) $(TOOLS_DIR)/$(BUSYBOX_HOST_BINARY)
+$(TOOLS_DIR)/busybox: $(BUSYBOX_TOOLS_DIR)/busybox-$(BUSYBOX_TOOLS_VERSION)/busybox
+	cp $(BUSYBOX_TOOLS_DIR)/busybox-$(BUSYBOX_TOOLS_VERSION)/$(BUSYBOX_TOOLS_BINARY) $(TOOLS_DIR)/$(BUSYBOX_TOOLS_BINARY)
 	@ln -s busybox $(TOOLS_DIR)/makedevs
 
 busybox-tools: $(TOOLS_DIR)/busybox
 
 busybox-tools-clean:
-	-$(MAKE) -C $(BUSYBOX_TOOLS_DIR)/busybox-$(BUSYBOX_VERSION) clean
+	-$(MAKE) -C $(BUSYBOX_TOOLS_DIR)/busybox-$(BUSYBOX_TOOLS_VERSION) clean
 	rm -f $(TOOLS_DIR)/busybox
 	rm -f $(TOOLS_DIR)/makedevs
 
