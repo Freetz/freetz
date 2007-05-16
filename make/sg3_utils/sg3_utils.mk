92c1262179f2b57fbf0e7caee1993ee85aedc315
Oliver Metz <oliver@freetz.org>
2007-05-16 16:33:50 +0000

    Integrate spindown-cgi and sg3_utils (by derheimi)
    git-svn-id: http://svn.freetz.org/trunk@315 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/sg3_utils/sg3_utils.mk
@@ -0,0 +1,63 @@
+SG3UTILS_VERSION:=1.24
+SG3UTILS_SOURCE:=sg3_utils-$(SG3UTILS_VERSION).tgz
+SG3UTILS_SITE:=http://sg.torque.net/sg/p/
+SG3UTILS_DIR:=$(SOURCE_DIR)/sg3_utils-$(SG3UTILS_VERSION)
+SG3UTILS_MAKE_DIR:=$(MAKE_DIR)/sg3_utils
+SG3UTILS_PKG_VERSION:=0.1
+SG3UTILS_PKG_SITE:=http://www.heimpold.de/dsmod
+SG3UTILS_PKG_NAME:=sg3_utils-$(SG3UTILS_VERSION)
+SG3UTILS_PKG_SOURCE:=sg3_utils-$(SG3UTILS_VERSION)-dsmod-$(SG3UTILS_PKG_VERSION).tar.bz2
+SG3UTILS_TARGET_DIR:=$(PACKAGES_DIR)/$(SG3UTILS_PKG_NAME)/root/usr/bin
+SG3UTILS_TARGET_BINARY:=sg_start
+
+$(DL_DIR)/$(SG3UTILS_SOURCE):
+	wget -P $(DL_DIR) $(SG3UTILS_SITE)/$(SG3UTILS_SOURCE)
+
+$(DL_DIR)/$(SG3UTILS_PKG_SOURCE):
+	@wget -P $(DL_DIR) $(SG3UTILS_PKG_SITE)/$(SG3UTILS_PKG_SOURCE)
+
+$(SG3UTILS_DIR)/.unpacked: $(DL_DIR)/$(SG3UTILS_SOURCE)
+	tar -C $(SOURCE_DIR) $(VERBOSE) -xzf $(DL_DIR)/$(SG3UTILS_SOURCE)
+#	for i in $(SG3UTILS_MAKE_DIR)/patches/*.patch; do \
+#		patch -d $(SG3UTILS_DIR) -p1 < $$i; \
+#	done
+	touch $@
+
+$(SG3UTILS_DIR)/$(SG3UTILS_TARGET_BINARY): $(SG3UTILS_DIR)/.unpacked
+	PATH="$(TARGET_PATH)" CFLAGS="$(TARGET_CFLAGS)" CC="$(TARGET_CC)" LD="$(TARGET_LD)" \
+		make -f no_lib/Makefile.linux sg_start -C $(SG3UTILS_DIR)
+	touch $@
+
+$(PACKAGES_DIR)/.$(SG3UTILS_PKG_NAME): $(DL_DIR)/$(SG3UTILS_PKG_SOURCE)
+	@tar -C $(PACKAGES_DIR) -xjf $(DL_DIR)/$(SG3UTILS_PKG_SOURCE)
+	@touch $@
+
+sg3_utils: $(PACKAGES_DIR)/.$(SG3UTILS_PKG_NAME)
+
+sg3_utils-package: $(PACKAGES_DIR)/.$(SG3UTILS_PKG_NAME)
+	tar -C $(PACKAGES_DIR) $(VERBOSE) --exclude .svn -cjf $(PACKAGES_BUILD_DIR)/$(SG3UTILS_PKG_SOURCE) $(SG3UTILS_PKG_NAME)
+
+sg3_utils-precompiled: uclibc $(SG3UTILS_DIR)/$(SG3UTILS_TARGET_BINARY) sg3_utils
+	$(TARGET_STRIP) $(SG3UTILS_DIR)/$(SG3UTILS_TARGET_BINARY)
+	cp $(SG3UTILS_DIR)/$(SG3UTILS_TARGET_BINARY) $(SG3UTILS_TARGET_DIR)
+
+sg3_utils-source: $(SG3UTILS_DIR)/.unpacked $(PACKAGES_DIR)/.$(SG3UTILS_PKG_NAME)
+
+sg3_utils-clean:
+	-$(MAKE) -C $(SG3UTILS_DIR) clean
+	rm -f $(SG3UTILS_DIR)/.installed
+	rm -f $(SG3UTILS_DIR)/.built
+	rm -f $(SG3UTILS_DIR)/.configured
+	rm -f $(PACKAGES_BUILD_DIR)/$(SG3UTILS_PKG_SOURCE)
+
+sg3_utils-dirclean:
+	rm -rf $(SG3UTILS_DIR)
+	rm -rf $(PACKAGES_DIR)/$(SG3UTILS_PKG_NAME)
+	rm -f $(PACKAGES_DIR)/.$(SG3UTILS_PKG_NAME)
+
+sg3_utils-list:
+ifeq ($(strip $(DS_PACKAGE_SG3UTILS)),y)
+	@echo "S90sg3_utils-$(SG3UTILS_VERSION)" >> .static
+else
+	@echo "S90sg3_utils-$(SG3UTILS_VERSION)" >> .dynamic
+endif
