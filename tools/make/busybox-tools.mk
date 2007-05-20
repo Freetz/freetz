c134533afe4fbb49cba88e447e9b5fb32e645b25
Oliver Metz <oliver@freetz.org>
2007-05-20 18:58:31 +0000

    Don't use $(DL_DIR) as dependancy
    git-svn-id: http://svn.freetz.org/trunk@322 f5190166-0702-4917-9039-51ec32eddaf5

--- tools/make/busybox-tools.mk
+++ tools/make/busybox-tools.mk
@@ -10,7 +10,8 @@ BUSYBOX_TOOLS_TARGET_BINARY:=busybox
 # Activate on demand to avoid collision with identical target for regular
 # busybox package
 ifneq ($(strip $(DS_HAVE_DOT_CONFIG)),y)
-$(DL_DIR)/$(BUSYBOX_TOOLS_SOURCE): $(DL_DIR)
+$(DL_DIR)/$(BUSYBOX_TOOLS_SOURCE): 
+	mkdir -p $(DL_DIR)
 	wget -P $(DL_DIR) $(BUSYBOX_TOOLS_SITE)/$(BUSYBOX_TOOLS_SOURCE)
 endif
 
