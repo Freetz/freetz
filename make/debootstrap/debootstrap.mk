8fcc8f9665d5991af7cf30489950fd1d081a73a9
Alexander Kriegisch <kriegaex@freetz.org>
2007-05-24 06:46:20 +0000

    Simplified debootstrap build process by removing the "convert Debian package to DS-Mod package" intermediate staging step and providing an extensive description about how to build & update the DS-Mod package in packages/debootstrap-0.3.3.2/README instead. This results in   - an updated download package 0.2 with mentioned README and without binary   - the removal of 01_trap.patch (patched file is part of DS-Mod package)   - a dramatically simplified debootstrap.mk
    git-svn-id: http://svn.freetz.org/trunk@352 f5190166-0702-4917-9039-51ec32eddaf5

--- make/debootstrap/debootstrap.mk
+++ make/debootstrap/debootstrap.mk
@@ -1,80 +1,61 @@
 DEBOOTSTRAP_VERSION:=0.3.3.2
 DEBOOTSTRAP_SOURCE:=debootstrap_$(DEBOOTSTRAP_VERSION).tar.gz
-DEBOOTSTRAP_BINARY:=debootstrap-udeb_$(DEBOOTSTRAP_VERSION)_mipsel.udeb
-DEBOOTSTRAP_SITE:=http://ftp.de.debian.org/debian/pool/main/d/debootstrap/
-DEBOOTSTRAP_DIR:=$(SOURCE_DIR)/debootstrap-$(DEBOOTSTRAP_VERSION)
+DEBOOTSTRAP_SITE:=http://ftp.de.debian.org/debian/pool/main/d/debootstrap
 DEBOOTSTRAP_MAKE_DIR:=$(MAKE_DIR)/debootstrap
-DEBOOTSTRAP_PKG_VERSION:=0.1
+DEBOOTSTRAP_DIR:=$(SOURCE_DIR)/debootstrap-$(DEBOOTSTRAP_VERSION)
+DEBOOTSTRAP_BINARY:=$(DEBOOTSTRAP_DIR)/pkgdetails
+DEBOOTSTRAP_TARGET_DIR:=$(PACKAGES_DIR)/debootstrap-$(DEBOOTSTRAP_VERSION)
+DEBOOTSTRAP_TARGET_BINARY:=$(DEBOOTSTRAP_TARGET_DIR)/root/usr/lib/debootstrap/pkgdetails
+DEBOOTSTRAP_PKG_VERSION:=0.2
 DEBOOTSTRAP_PKG_SITE:=http://www.heimpold.de/dsmod
 DEBOOTSTRAP_PKG_NAME:=debootstrap-$(DEBOOTSTRAP_VERSION)
 DEBOOTSTRAP_PKG_SOURCE:=debootstrap-$(DEBOOTSTRAP_VERSION)-dsmod-$(DEBOOTSTRAP_PKG_VERSION).tar.bz2
-DEBOOTSTRAP_TARGET_ROOTDIR:=$(PACKAGES_DIR)/$(DEBOOTSTRAP_PKG_NAME)/root
 
 $(DL_DIR)/$(DEBOOTSTRAP_SOURCE): | $(DL_DIR)
 	wget -P $(DL_DIR) $(DEBOOTSTRAP_SITE)/$(DEBOOTSTRAP_SOURCE)
 
-$(DL_DIR)/$(DEBOOTSTRAP_BINARY): | $(DL_DIR)
-	wget -P $(DL_DIR) $(DEBOOTSTRAP_SITE)/$(DEBOOTSTRAP_BINARY)
-
 $(DL_DIR)/$(DEBOOTSTRAP_PKG_SOURCE): | $(DL_DIR)
 	@$(DL_TOOL) $(DL_DIR) $(TOPDIR)/.config $(DEBOOTSTRAP_PKG_SOURCE) $(DEBOOTSTRAP_PKG_SITE)
 
-$(DEBOOTSTRAP_DIR)/.unpacked: $(DL_DIR)/$(DEBOOTSTRAP_SOURCE) $(DL_DIR)/$(DEBOOTSTRAP_BINARY)
+$(DEBOOTSTRAP_DIR)/.unpacked: $(DL_DIR)/$(DEBOOTSTRAP_SOURCE)
 	tar -C $(SOURCE_DIR) $(VERBOSE) -xzf $(DL_DIR)/$(DEBOOTSTRAP_SOURCE)
-	( cd $(DEBOOTSTRAP_DIR); \
-		ar -x ../../$(DL_DIR)/$(DEBOOTSTRAP_BINARY) data.tar.gz; \
-		mkdir -p target; \
-		cd target && tar xzf ../data.tar.gz && cd .. \
-	);
-	for i in $(DEBOOTSTRAP_MAKE_DIR)/patches/*.patch; do \
-		patch -d $(DEBOOTSTRAP_DIR) -p1 < $$i; \
-	done
-	touch $@
-
-$(DEBOOTSTRAP_DIR)/.configured: $(DEBOOTSTRAP_DIR)/.unpacked
-	touch $@
-
-$(DEBOOTSTRAP_DIR)/.built: $(DEBOOTSTRAP_DIR)/.configured
-	cd $(DEBOOTSTRAP_DIR) && $(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CC) $(TARGET_CFLAGS) -static-libgcc -o pkgdetails pkgdetails.c
-	touch $@
-
-$(DEBOOTSTRAP_DIR)/.installed: $(DEBOOTSTRAP_DIR)/.built
-	$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_STRIP) $(DEBOOTSTRAP_DIR)/pkgdetails
-	cp -a $(DEBOOTSTRAP_DIR)/pkgdetails $(DEBOOTSTRAP_DIR)/target/usr/lib/debootstrap
-	rm -f $(DEBOOTSTRAP_DIR)/target/usr/lib/debootstrap/scripts/breezy
-	rm -f $(DEBOOTSTRAP_DIR)/target/usr/lib/debootstrap/scripts/hoary
-	rm -f $(DEBOOTSTRAP_DIR)/target/usr/lib/debootstrap/scripts/warty
-	rm -f $(DEBOOTSTRAP_DIR)/target/usr/lib/debootstrap/scripts/woody
+	#for i in $(DEBOOTSTRAP_MAKE_DIR)/patches/*.patch; do \
+	#	patch -d $(DEBOOTSTRAP_DIR) -p1 < $$i; \
+	#done
 	touch $@
 
 $(PACKAGES_DIR)/.$(DEBOOTSTRAP_PKG_NAME): $(DL_DIR)/$(DEBOOTSTRAP_PKG_SOURCE) | $(PACKAGES_DIR)
 	@tar -C $(PACKAGES_DIR) -xjf $(DL_DIR)/$(DEBOOTSTRAP_PKG_SOURCE)
 	@touch $@
 
+$(DEBOOTSTRAP_BINARY): $(DEBOOTSTRAP_DIR)/.unpacked
+	cd $(DEBOOTSTRAP_DIR) && $(TARGET_CONFIGURE_OPTS) \
+		$(TARGET_CC) $(TARGET_CFLAGS) -static-libgcc -o pkgdetails pkgdetails.c
+
+$(DEBOOTSTRAP_TARGET_BINARY): $(DEBOOTSTRAP_BINARY)
+	$(TARGET_STRIP) $(DEBOOTSTRAP_BINARY)
+	cp $(DEBOOTSTRAP_BINARY) $(DEBOOTSTRAP_TARGET_BINARY)
+
 debootstrap: $(PACKAGES_DIR)/.$(DEBOOTSTRAP_PKG_NAME)
 
 debootstrap-package: $(PACKAGES_DIR)/.$(DEBOOTSTRAP_PKG_NAME)
 	tar -C $(PACKAGES_DIR) $(VERBOSE) --exclude .svn -cjf $(PACKAGES_BUILD_DIR)/$(DEBOOTSTRAP_PKG_SOURCE) $(DEBOOTSTRAP_PKG_NAME)
 
-debootstrap-precompiled: uclibc $(DEBOOTSTRAP_DIR)/.installed debootstrap
-	mkdir -p $(DEBOOTSTRAP_TARGET_ROOTDIR)
-	cp -a $(DEBOOTSTRAP_DIR)/target/* $(DEBOOTSTRAP_TARGET_ROOTDIR)
+debootstrap-precompiled: uclibc debootstrap $(DEBOOTSTRAP_TARGET_BINARY)
 
 debootstrap-source: $(DEBOOTSTRAP_DIR)/.unpacked $(PACKAGES_DIR)/.$(DEBOOTSTRAP_PKG_NAME)
 
 debootstrap-clean:
 	-$(MAKE) -C $(DEBOOTSTRAP_DIR) clean
-	rm -f $(DEBOOTSTRAP_DIR)/.installed
-	rm -f $(DEBOOTSTRAP_DIR)/.built
-	rm -f $(DEBOOTSTRAP_DIR)/.configured
 	rm -f $(PACKAGES_BUILD_DIR)/$(DEBOOTSTRAP_PKG_SOURCE)
 
 debootstrap-dirclean:
 	rm -rf $(DEBOOTSTRAP_DIR)
 	rm -rf $(PACKAGES_DIR)/$(DEBOOTSTRAP_PKG_NAME)
 	rm -f $(PACKAGES_DIR)/.$(DEBOOTSTRAP_PKG_NAME)
+
+debootstrap-uninstall:
+	rm -f $(DEBOOTSTRAP_TARGET_BINARY)
 
 debootstrap-list:
 ifeq ($(strip $(DS_PACKAGE_DEBOOTSTRAP)),y)
