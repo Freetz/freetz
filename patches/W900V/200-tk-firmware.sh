8b7958bddb8960d569fc59ce28e315a25eb4697f
Oliver Metz <oliver@freetz.org>
2007-05-30 11:31:22 +0000

    W900V: Make Webinterface available for OEM=avm
    git-svn-id: http://svn.freetz.org/trunk@376 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/W900V/200-tk-firmware.sh
+++ patches/W900V/200-tk-firmware.sh
@@ -10,8 +10,12 @@ rm -rf "${FILESYSTEM_MOD_DIR}/usr/www/tcom"
 [ "$DS_VERBOSITY_LEVEL" -ge 1 ] && echo "${L1}copying AVM Webinterface"
 mkdir "${FILESYSTEM_MOD_DIR}/usr/www/all"
 "$TAR" -cf - -C "${DIR}/.tk/original/filesystem/usr/www/all" . | "$TAR" -xf - -C "${FILESYSTEM_MOD_DIR}/usr/www/all"
-ln -sf /usr/www/all "${FILESYSTEM_MOD_DIR}/usr/www/tcom"
-"$TAR" -cf - -C "${DIR}/.tk/original/filesystem/etc/default.Fritz_Box_7150/avm" . | "$TAR" -xkf - -C "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_DECT_W900V/tcom" >/dev/null 2>&1 
+ln -sf all "${FILESYSTEM_MOD_DIR}/usr/www/tcom"
+ln -sf all "${FILESYSTEM_MOD_DIR}/usr/www/avm"
+
+rm -rf "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_DECT_W900V/tcom"
+"$TAR" -cf - -C "${DIR}/.tk/original/filesystem/etc/default.Fritz_Box_7150" avm | "$TAR" -xf - -C "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_DECT_W900V" 
+ln -sf avm "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_DECT_W900V/tcom"
 
 [ "$DS_VERBOSITY_LEVEL" -ge 1 ] && echo "${L1}copying mailer to /sbin/mailer"
-"$TAR" -cf - -C "${DIR}/.tk/original/filesystem/sbin" mailer | "$TAR" -xkf - -C "${FILESYSTEM_MOD_DIR}/sbin" >/dev/null 2>&1 
+"$TAR" -cf - -C "${DIR}/.tk/original/filesystem/sbin" mailer | "$TAR" -xkf - -C "${FILESYSTEM_MOD_DIR}/sbin" 
