c2128152b94b7158dd6d9b31ee86db4944e76f86
Oliver Metz <oliver@freetz.org>
2007-06-05 19:20:51 +0000

    Bump fakeroot version to 1.7.1, remove it from To-do list
    git-svn-id: http://svn.freetz.org/trunk@421 f5190166-0702-4917-9039-51ec32eddaf5

--- tools/make/fakeroot.mk
+++ tools/make/fakeroot.mk
@@ -1,4 +1,4 @@
-FAKEROOT_VERSION:=1.6.5
+FAKEROOT_VERSION:=1.7.1
 FAKEROOT_SOURCE:=fakeroot_$(FAKEROOT_VERSION).tar.gz
 FAKEROOT_SITE:=http://ftp.debian.org/debian/pool/main/f/fakeroot
 FAKEROOT_DIR:=$(SOURCE_DIR)/fakeroot-$(FAKEROOT_VERSION)
