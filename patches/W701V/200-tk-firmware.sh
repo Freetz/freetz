930b9a98bfe88ac05fcaafe72b82ab63936d2dce
Oliver Metz <oliver@freetz.org>
2007-06-06 00:17:22 +0000

    Updating some patches
    git-svn-id: http://svn.freetz.org/trunk@432 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/W701V/200-tk-firmware.sh
+++ patches/W701V/200-tk-firmware.sh
@@ -11,7 +11,7 @@ rm -rf "${FILESYSTEM_MOD_DIR}/usr/www/tcom"
 mkdir "${FILESYSTEM_MOD_DIR}/usr/www/all"
 "$TAR" -cf - -C "${DIR}/.tk/original/filesystem/usr/www/all" --exclude=html/de/usb . | "$TAR" -xf - -C "${FILESYSTEM_MOD_DIR}/usr/www/all"
 ln -sf /usr/www/all "${FILESYSTEM_MOD_DIR}/usr/www/tcom"
-"$TAR" -cf - -C "${DIR}/.tk/original/filesystem/etc/default.Fritz_Box_7170_26/avm" . | "$TAR" -xkf - -C "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_SpeedportW701V/tcom" >/dev/null 2>&1 
+"$TAR" -cf - -C "${DIR}/.tk/original/filesystem/etc/default.Fritz_Box_7170/avm" . | "$TAR" -xkf - -C "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_SpeedportW701V/tcom" >/dev/null 2>&1 
 
 [ "$DS_VERBOSITY_LEVEL" -ge 1 ] && echo "${L1}copying mailer to /sbin/mailer"
 "$TAR" -cf - -C "${DIR}/.tk/original/filesystem/sbin" mailer | "$TAR" -xkf - -C "${FILESYSTEM_MOD_DIR}/sbin" >/dev/null 2>&1 
