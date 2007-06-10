4372def752a5c2222b71b280fe56830d35477f89
Oliver Metz <oliver@freetz.org>
2007-06-10 01:02:03 +0000

    Fix patches
    git-svn-id: http://svn.freetz.org/trunk@465 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/191-webmenu_wol.sh
+++ patches/191-webmenu_wol.sh
@@ -1,11 +1,17 @@
 if [ "$DS_PACKAGE_WOL_CGI" == "y" ]
 then
 	echo1 "applying wol patch"
-	if [ "$DS_TYPE_FON_WLAN_7170" == "y" ]
+	if [ "$DS_TYPE_FON_WLAN_7170" == "y" ] || [ "$DS_TYPE_FON_WLAN_7150" == "y" ]
 	then
 	    modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/${DS_TYPE_LANG_STRING}/191-webmenu-wol-7170.patch"
-	elif [ "$DS_TYPE_FON_WLAN_7141" == "y" ]
+	elif [ "$DS_TYPE_FON_WLAN_7140" == "y" ] || [ "$DS_TYPE_FON_WLAN_7141" == "y" ]
 	then
 	    modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/${DS_TYPE_LANG_STRING}/191-webmenu-wol-7141.patch"
+	elif [ "$DS_TYPE_FON_WLAN" == "y" ] || [ "$DS_TYPE_FON_WLAN_7050" == "y" ]
+	then    
+	    modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/${DS_TYPE_LANG_STRING}/191-webmenu-wol-7050.patch"
+	elif [ "$DS_TYPE_FON" == "y" ] || [ "$DS_TYPE_FON_5050" == "y" ] 
+	then    
+	    modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/${DS_TYPE_LANG_STRING}/191-webmenu-wol-fon.patch"
 	fi
 fi
