a8cf9df605243e3e23efd8de29378de68bd31c9c
Oliver Metz <oliver@freetz.org>
2007-06-15 21:07:53 +0000

    Don't use storage.patch for W900V, fix W701V webmenu patch
    git-svn-id: http://svn.freetz.org/trunk@507 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/280-storage.sh
+++ patches/280-storage.sh
@@ -1,4 +1,4 @@
-if [ "$DS_HAS_USB_HOST" == "y" ]
+if [ "$DS_HAS_USB_HOST" == "y" ] && [ "$DS_TYPE_SPEEDPORT_W900V" != "y" ]
 then
 	echo1 "patching run_mount"
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/storage.patch"
