48a78ff10a1b58bc9aaee65f521ab3fe57d9f305
Oliver Metz <oliver@freetz.org>
2007-06-21 21:17:53 +0000

    Bumpd ctorrent to version dnh3.1, remove old patch, fix Makefile.in
    git-svn-id: http://svn.freetz.org/trunk@547 f5190166-0702-4917-9039-51ec32eddaf5

--- make/ctorrent/ctorrent.mk
+++ make/ctorrent/ctorrent.mk
@@ -1,8 +1,8 @@
-CTORRENT_VERSION:=1.3.4-dnh3
+CTORRENT_VERSION:=dnh3.1
 CTORRENT_SOURCE:=ctorrent-$(CTORRENT_VERSION).tar.gz
 CTORRENT_SITE:=http://www.rahul.net/dholmes/ctorrent/
 CTORRENT_MAKE_DIR:=$(MAKE_DIR)/ctorrent
-CTORRENT_DIR:=$(SOURCE_DIR)/ctorrent-dnh3
+CTORRENT_DIR:=$(SOURCE_DIR)/ctorrent-$(CTORRENT_VERSION)
 CTORRENT_BINARY:=$(CTORRENT_DIR)/ctorrent
 CTORRENT_TARGET_DIR:=$(PACKAGES_DIR)/ctorrent-$(CTORRENT_VERSION)
 CTORRENT_TARGET_BINARY:=$(CTORRENT_TARGET_DIR)/root/usr/bin/ctorrent
@@ -18,9 +18,9 @@ $(DL_DIR)/$(CTORRENT_PKG_SOURCE): | $(DL_DIR)
 
 $(CTORRENT_DIR)/.unpacked: $(DL_DIR)/$(CTORRENT_SOURCE)
 	tar -C $(SOURCE_DIR) $(VERBOSE) -xzf $(DL_DIR)/$(CTORRENT_SOURCE)
-	for i in $(CTORRENT_MAKE_DIR)/patches/*.patch; do \
-		patch -d $(CTORRENT_DIR) -p0 < $$i; \
-	done
+#	for i in $(CTORRENT_MAKE_DIR)/patches/*.patch; do \
+#		patch -d $(CTORRENT_DIR) -p0 < $$i; \
+#	done
 	touch $@
 
 $(CTORRENT_DIR)/.configured: $(CTORRENT_DIR)/.unpacked 
