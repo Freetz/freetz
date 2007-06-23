cb0c6a26cffac7f872de2f9c66ace1cdf95b3c49
Alexander Kriegisch <kriegaex@freetz.org>
2007-06-23 21:06:20 +0000

    Remove ECO firmware (menuconfig options and patches), because it is obsolete, because its features have been included into the current firmware releases.
    git-svn-id: http://svn.freetz.org/trunk@579 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/7170_labor/120-rc.S-dev.sh
+++ patches/7170_labor/120-rc.S-dev.sh
@@ -1,5 +1,5 @@
 echo1 "applying rc.S-dev.patch"
-if [ "$DS_TYPE_LABOR_PHONE" == "y" ] || [ "$DS_TYPE_LABOR_ECO" == "y" ]; then
+if [ "$DS_TYPE_LABOR_PHONE" == "y" ]; then
     modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/rc.S-dev_7170_labor_phone.patch
     else
     modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/rc.S-dev_7170_labor.patch
