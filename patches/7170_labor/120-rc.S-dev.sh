46988590729fa098d476887eb667ac60edff6b98
Alexander Kriegisch <kriegaex@freetz.org>
2007-06-24 01:37:43 +0000

    Fix and beautify (make fit more nicely) Labor patches
    git-svn-id: http://svn.freetz.org/trunk@584 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/7170_labor/120-rc.S-dev.sh
+++ patches/7170_labor/120-rc.S-dev.sh
@@ -1,6 +1,6 @@
 echo1 "applying rc.S-dev.patch"
 if [ "$DS_TYPE_LABOR_PHONE" == "y" ]; then
-    modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/rc.S-dev_7170_labor_phone.patch
-    else
-    modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/rc.S-dev_7170_labor.patch
+	modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/rc.S-dev_7170_labor_phone.patch
+else
+	modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/rc.S-dev_7170_labor.patch
 fi
