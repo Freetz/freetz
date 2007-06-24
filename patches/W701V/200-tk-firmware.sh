f8ebee5168243784914e026524cccfff97f90f4c
Oliver Metz <oliver@freetz.org>
2007-06-24 22:40:46 +0000

    Accept OEM avm for W701V
    git-svn-id: http://svn.freetz.org/trunk@599 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/W701V/200-tk-firmware.sh
+++ patches/W701V/200-tk-firmware.sh
@@ -10,8 +10,11 @@ rm -rf "${FILESYSTEM_MOD_DIR}/usr/www/tcom"
 echo1 "copying AVM Webinterface"
 mkdir "${FILESYSTEM_MOD_DIR}/usr/www/all"
 "$TAR" -cf - -C "${DIR}/.tk/original/filesystem/usr/www/all" --exclude=html/de/usb . | "$TAR" -xf - -C "${FILESYSTEM_MOD_DIR}/usr/www/all"
-ln -sf /usr/www/all "${FILESYSTEM_MOD_DIR}/usr/www/tcom"
+ln -sf all "${FILESYSTEM_MOD_DIR}/usr/www/tcom"
+ln -sf all "${FILESYSTEM_MOD_DIR}/usr/www/avm"
+
 "$TAR" -cf - -C "${DIR}/.tk/original/filesystem/etc/default.Fritz_Box_7170/avm" . | "$TAR" -xkf - -C "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_SpeedportW701V/tcom" >/dev/null 2>&1 
+ln -sf avm "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_SpeedportW701V/tcom"
 
 echo1 "copying mailer to /sbin/mailer"
 "$TAR" -cf - -C "${DIR}/.tk/original/filesystem/sbin" mailer | "$TAR" -xkf - -C "${FILESYSTEM_MOD_DIR}/sbin" >/dev/null 2>&1 
