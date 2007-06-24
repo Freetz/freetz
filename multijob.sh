703db1dea620dab8f5da054a9ec851c602595188
Oliver Metz <oliver@freetz.org>
2007-06-24 20:48:32 +0000

    Fix typo in multijob.sh
    git-svn-id: http://svn.freetz.org/trunk@593 f5190166-0702-4917-9039-51ec32eddaf5

--- multijob.sh
+++ multijob.sh
@@ -1,4 +1,4 @@
 #!/bin/sh 
 echo "Number of jobs is now adjusted over menuconfig."
 echo "Advanced options->Compiler options->Number of jobs"
-echo "multijob.sh will be removed in fututre releases"
+echo "multijob.sh will be removed in future releases"
