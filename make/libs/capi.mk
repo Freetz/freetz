cb28d3ba8ffe7c27af612cc553eda35b9dfd7768
Oliver Metz <oliver@freetz.org>
2007-06-27 13:06:25 +0000

    Integrate libcapi20, rcapid (by Nicolai Ehemann)
    git-svn-id: http://svn.freetz.org/trunk@634 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/libs/capi.mk
@@ -0,0 +1,55 @@
+CAPI_VERSION:=2.0
+CAPI_LIB_VERSION:=3.0.4
+CAPI_SOURCE:=libcapi-$(CAPI_VERSION).tar.bz2
+CAPI_SITE:=http://dsmod.magenbrot.net
+CAPI_MAKE_DIR:=$(MAKE_DIR)/libs
+CAPI_DIR:=$(SOURCE_DIR)/libcapi-$(CAPI_VERSION)
+CAPI_BINARY:=$(CAPI_DIR)/licapi.so.$(CAPI_LIB_VERSION)
+CAPI_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/libcapi20.so.$(CAPI_LIB_VERSION)
+CAPI_TARGET_DIR:=root/lib
+CAPI_TARGET_BINARY:=$(CAPI_TARGET_DIR)/libcapi20.so.$(CAPI_LIB_VERSION)
+
+$(DL_DIR)/$(CAPI_SOURCE): | $(DL_DIR)
+	wget -P $(DL_DIR) $(CAPI_SITE)/$(CAPI_SOURCE)
+
+$(CAPI_DIR)/.unpacked: $(DL_DIR)/$(CAPI_SOURCE)
+	tar -C $(SOURCE_DIR) $(VERBOSE) -xjf $(DL_DIR)/$(CAPI_SOURCE)
+#	for i in $(CAPI_MAKE_DIR)/patches/*.capi.patch; do \
+#		patch -d $(CAPI_DIR) -p0 < $$i; \
+#	done
+	touch $@
+
+$(CAPI_BINARY): $(CAPI_DIR)/.unpacked
+	PATH=$(TARGET_TOOLCHAIN_PATH) \
+	CROSS_COMPILE="$(TARGET_CROSS)" \
+	CFLAGS="$(TARGET_CFLAGS)" \
+	CAPI20_VERS="$CAPI_VERSION)" \
+	$(MAKE) -C $(CAPI_DIR) all
+
+$(CAPI_STAGING_BINARY): $(CAPI_BINARY)
+	PATH=$(TARGET_TOOLCHAIN_PATH) $(MAKE) -C $(CAPI_DIR) \
+		FILESYSTEM="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+		install
+
+$(CAPI_TARGET_BINARY): $(CAPI_STAGING_BINARY)
+	cp -a $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libcapi*.so* $(CAPI_TARGET_DIR)
+	$(TARGET_STRIP) $@
+
+capi: $(CAPI_STAGING_BINARY)
+
+capi-precompiled: uclibc capi $(CAPI_TARGET_BINARY)
+
+capi-source: $(CAPI_DIR)/.unpacked
+
+capi-clean:
+	-$(MAKE) -C $(CAPI_DIR) clean
+	rm -f $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libcapi20.* \
+			$(STAGING_DIR)/include/capi20.h \
+			$(STAGING_DIR)/include/capiutils.h \
+			$(STAGING_DIR)/include/capicmd.h
+
+capi-uninstall:
+	rm -f $(CAPI_TARGET_DIR)/libcapi*.so*
+
+capi-dirclean:
+	rm -rf $(CAPI_DIR)
---
+++ make/rcapid/rcapid.mk
@@ -0,0 +1,89 @@
+RCAPID_VERSION:=0.1
+RCAPID_SOURCE:=rcapid-$(RCAPID_VERSION).tar.bz2
+RCAPID_SITE:=http://dsmod.magenbrot.net
+RCAPID_DIR:=$(SOURCE_DIR)/rcapid-$(RCAPID_VERSION)
+RCAPID_MAKE_DIR:=$(MAKE_DIR)/rcapid
+RCAPID_BINARY:=$(RCAPID_DIR)/rcapid
+RCAPID_PKG_VERSION:=0.1
+RCAPID_PKG_SITE:=http://fbox.enlightened.de/ds/rcapid/
+RCAPID_PKG_NAME:=rcapid-$(RCAPID_PKG_VERSION)
+RCAPID_PKG_SOURCE:=rcapid-dsmod-$(RCAPID_PKG_VERSION).tar.bz2
+RCAPID_TARGET_DIR:=$(PACKAGES_DIR)/$(RCAPID_PKG_NAME)
+RCAPID_TARGET_BINARY:=$(RCAPID_TARGET_DIR)/root/usr/sbin/rcapid
+
+$(DL_DIR)/$(RCAPID_SOURCE):
+	wget -P $(DL_DIR) $(RCAPID_SITE)/$(RCAPID_SOURCE)
+
+$(DL_DIR)/$(RCAPID_PKG_SOURCE):
+	@$(DL_TOOL) $(DL_DIR) $(TOPDIR)/.config $(RCAPID_PKG_SOURCE) $(RCAPID_PKG_SITE)
+
+$(RCAPID_DIR)/.unpacked: $(DL_DIR)/$(RCAPID_SOURCE)
+	tar -C $(SOURCE_DIR) $(VERBOSE) -xjf $(DL_DIR)/$(RCAPID_SOURCE)
+#	for i in $(RCAPID_MAKE_DIR)/patches/*.patch; do \
+#		patch -d $(RCAPID_DIR) -p0 < $$i; \
+#	done
+	touch $@
+
+$(RCAPID_DIR)/.configured: $(RCAPID_DIR)/.unpacked
+	( cd $(RCAPID_DIR); rm -f config.{cache,status}; \
+		$(TARGET_CONFIGURE_OPTS) \
+		CFLAGS="$(TARGET_CFLAGS)" \
+		CC="$(TARGET_CC)" \
+		./configure \
+		--target=$(GNU_TARGET_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--build=$(GNU_HOST_NAME) \
+		--program-prefix="" \
+		--program-suffix="" \
+		--prefix=/usr \
+		--exec-prefix=/usr \
+		--bindir=/usr/bin \
+		--sbindir=/usr/sbin \
+		--libexecdir=/usr/lib \
+		--datadir=/usr/share \
+		--sysconfdir=/etc \
+		--localstatedir=/var \
+		--libdir=/usr/lib \
+		--includedir=/usr/include \
+		--infodir=/usr/share/info \
+		--mandir=/usr/share/man \
+		$(DISABLE_LARGEFILE) \
+		$(DISABLE_NLS) \
+	);
+	touch $@
+
+$(RCAPID_BINARY): $(RCAPID_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+	$(MAKE) -C $(RCAPID_DIR)
+
+$(RCAPID_TARGET_BINARY): $(RCAPID_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(PACKAGES_DIR)/.$(RCAPID_PKG_NAME): $(DL_DIR)/$(RCAPID_PKG_SOURCE)
+	@tar -C $(PACKAGES_DIR) -xjf $(DL_DIR)/$(RCAPID_PKG_SOURCE)
+	@touch $@
+
+rcapid: $(PACKAGES_DIR)/.$(RCAPID_PKG_NAME)
+
+rcapid-precompiled: uclibc capi-precompiled rcapid $(RCAPID_TARGET_BINARY)
+
+rcapid-package: $(PACKAGES_DIR)/.$(RCAPID_PKG_NAME)
+	tar -C $(PACKAGES_DIR) $(VERBOSE) --exclude .svn -cjf $(PACKAGES_BUILD_DIR)/$(RCAPID_PKG_SOURCE) $(RCAPID_PKG_NAME) 
+
+rcapid-source: $(RCAPID_DIR)/.unpacked $(PACKAGES_DIR)/.$(RCAPID_PKG_NAME)
+
+rcapid-clean:
+	-$(MAKE) -C $(RCAPID_DIR) clean
+	rm -f $(PACKAGES_BUILD_DIR)/$(RCAPID_PKG_SOURCE)
+
+rcapid-dirclean:
+	rm -rf $(RCAPID_DIR)
+	rm -rf $(PACKAGES_DIR)/$(RCAPID_PKG_NAME)
+	rm -f $(PACKAGES_DIR)/.$(RCAPID_PKG_NAME)
+
+rcapid-list:
+ifeq ($(strip $(DS_PACKAGE_RCAPID)),y)
+	@echo "S40rcapid-$(RCAPID_PKG_VERSION)" >> .static
+else
+	@echo "S40rcapid-$(RCAPID_PKG_VERSION)" >> .dynamic
+endif
