936e6fa9025f47b2ea67fe2a1ed773e74670e50a
Oliver Metz <oliver@freetz.org>
2007-06-27 12:56:28 +0000

    Copy igdd from tk firmware to filesystem (w501v, w701v and w900v), change tar to cp
    git-svn-id: http://svn.freetz.org/trunk@632 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/W701V/200-tk-firmware.sh
+++ patches/W701V/200-tk-firmware.sh
@@ -16,5 +16,8 @@ ln -sf all "${FILESYSTEM_MOD_DIR}/usr/www/avm"
 "$TAR" -cf - -C "${DIR}/.tk/original/filesystem/etc/default.Fritz_Box_7170/avm" . | "$TAR" -xkf - -C "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_SpeedportW701V/tcom" >/dev/null 2>&1 
 ln -sf avm "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_SpeedportW701V/tcom"
 
-echo1 "copying mailer to /sbin/mailer"
-"$TAR" -cf - -C "${DIR}/.tk/original/filesystem/sbin" mailer | "$TAR" -xkf - -C "${FILESYSTEM_MOD_DIR}/sbin" >/dev/null 2>&1 
+echo1 "copying mailer"
+cp -a "${DIR}/.tk/original/filesystem/sbin/mailer" "${FILESYSTEM_MOD_DIR}/sbin" >/dev/null 2>&1 
+
+echo1 "copying igdd"
+cp -a "${DIR}/.tk/original/filesystem/sbin/igdd" "${FILESYSTEM_MOD_DIR}/sbin" >/dev/null 2>&1 
--- patches/W900V/200-tk-firmware.sh
+++ patches/W900V/200-tk-firmware.sh
@@ -17,5 +17,8 @@ rm -rf "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_DECT_W900V/tcom"
 "$TAR" -cf - -C "${DIR}/.tk/original/filesystem/etc/default.Fritz_Box_7150" avm | "$TAR" -xf - -C "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_DECT_W900V" 
 ln -sf avm "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_DECT_W900V/tcom"
 
-echo1 "copying mailer to /sbin/mailer"
-"$TAR" -cf - -C "${DIR}/.tk/original/filesystem/sbin" mailer | "$TAR" -xkf - -C "${FILESYSTEM_MOD_DIR}/sbin" 
+echo1 "copying mailer"
+cp -a "${DIR}/.tk/original/filesystem/sbin/mailer" "${FILESYSTEM_MOD_DIR}/sbin" >/dev/null 2>&1 
+
+echo1 "copying igdd"
+cp -a "${DIR}/.tk/original/filesystem/sbin/igdd" "${FILESYSTEM_MOD_DIR}/sbin" >/dev/null 2>&1 
