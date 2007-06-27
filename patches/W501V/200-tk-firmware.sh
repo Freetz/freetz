bf9985df58c1ea06c794f411e963a6b72bf9479f
Alexander Kriegisch <kriegaex@freetz.org>
2007-06-27 22:49:32 +0000

    Just to answer mike's question from #637: bash does not perform pathname expansion within single or double quotes. Check the diff to see how I changed it. Another change concerns the ">/dev/null 2>&1" stuff. I think if there are errors during copying, we want to see them. So I removed the redirection.
    git-svn-id: http://svn.freetz.org/trunk@638 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/W501V/200-tk-firmware.sh
+++ patches/W501V/200-tk-firmware.sh
@@ -17,13 +17,9 @@ ln -sf all "${FILESYSTEM_MOD_DIR}/usr/www/avm"
 ln -sf avm "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_SpeedportW501V/tcom"
 
 echo1 "copying mailer"
-cp -a "${DIR}/.tk/original/filesystem/sbin/mailer" "${FILESYSTEM_MOD_DIR}/sbin" >/dev/null 2>&1 
+cp -a ${DIR}/.tk/original/filesystem/sbin/mailer "${FILESYSTEM_MOD_DIR}/sbin/"
 
 echo1 "copying igdd + required libs"
-cp -a "${DIR}/.tk/original/filesystem/lib/libavmupnp.so.2.0.0" "${FILESYSTEM_MOD_DIR}/lib/" > /dev/null 2>&1
-cp -a "${DIR}/.tk/original/filesystem/lib/libavmupnp.so.2"     "${FILESYSTEM_MOD_DIR}/lib/" > /dev/null 2>&1
-cp -a "${DIR}/.tk/original/filesystem/lib/libavmupnp.so"       "${FILESYSTEM_MOD_DIR}/lib/" > /dev/null 2>&1
-cp -a "${DIR}/.tk/original/filesystem/lib/libmxml.so.1.0.0" "${FILESYSTEM_MOD_DIR}/lib/" > /dev/null 2>&1
-cp -a "${DIR}/.tk/original/filesystem/lib/libmxml.so.1"     "${FILESYSTEM_MOD_DIR}/lib/" > /dev/null 2>&1
-cp -a "${DIR}/.tk/original/filesystem/lib/libmxml.so"       "${FILESYSTEM_MOD_DIR}/lib/" > /dev/null 2>&1
-cp -a "${DIR}/.tk/original/filesystem/sbin/igdd" "${FILESYSTEM_MOD_DIR}/sbin" > /dev/null 2>&1
+cp -a ${DIR}/.tk/original/filesystem/lib/libavmupnp.so* "${FILESYSTEM_MOD_DIR}/lib/"
+cp -a ${DIR}/.tk/original/filesystem/lib/libmxml.so* "${FILESYSTEM_MOD_DIR}/lib/"
+cp -a ${DIR}/.tk/original/filesystem/sbin/igdd "${FILESYSTEM_MOD_DIR}/sbin/"
