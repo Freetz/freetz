f528802de7183a7a140253dedbfe2a720bd96868
Alexander Kriegisch <kriegaex@freetz.org>
2007-06-30 00:22:46 +0000

    - Add remark to ldd item in TODO - Minor cosmetic changes (mixed space/tab indentation, 'echo ""' instead of   just 'echo') in push.sh
    git-svn-id: http://svn.freetz.org/trunk@647 f5190166-0702-4917-9039-51ec32eddaf5

--- tools/push.sh
+++ tools/push.sh
@@ -17,23 +17,23 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 function push() {
-	echo ""
+	echo
 	echo " * You should now reboot your box. Waiting for box to shut down for restart ..."
 	while [ `ping -c1 -w1 192.168.178.1 | grep 'receive' | awk '{ print $4 }'` == "1" ]; do
 		sleep 1
 	done
 
-	echo ""
+	echo
 	echo " * No reply from box. Assuming restart ..."
 	while [ `ping -c1 -w1 192.168.178.1 | grep 'receive' | awk '{ print $4 }'` == "0" ]; do
 		sleep 1
 	done
 
-	echo ""
+	echo
 	echo " * Box is back up again. Initiating transfer of '$1' ..."
-	echo ""
+	echo
 
-	ftp -n -p <<EOT
+	ftp -n -p <<'EOT'
 open 192.168.178.1
 user adam2 adam2
 debug
@@ -49,36 +49,36 @@ EOT
 ###
 
 if [ -z "$1" ]; then
-	echo ""
-        echo "Usage: $0 <firmware> [ -f ]"
+	echo
+	echo "Usage: $0 <firmware> [ -f ]"
 	echo ""
 	echo "firmware    firmware file to flash (mostly kernel.image)"
 	echo "-f          disable safty prompt"
-	echo ""
-        exit 1
+	echo
+	exit 1
 fi
 
 if [ "$2" = "-f" ]; then
 	push $1
 else
-	echo		""
+	echo
 	echo		"!!! WARNING !!! WARNING !!! WARNING !!! WARNING !!! WARNING !!!" 
 	echo		"!!!   THERE IS NO WARRENTY AT ALL !!! USE AT YOU OWN RISK   !!!"
-	echo		""
+	echo
 	echo	-n	"Are you sure, that you want to flash "
 	echo	-en	"\033[4m$1\033[0m "
 	echo	-n	"directly to "
 	echo	-e	"\033[4mmtd1\033[0m?"
-	echo		""
+	echo
 	echo	-n	"proceed (y/n) "
 
 	read -n 1 -s PROCEED
-	echo ""
+	echo
 
 	if [ "$PROCEED" = "y" ]; then
 		push $1	
 	else
-		echo ""
+		echo
 		echo "aborted"
 	fi
 fi
