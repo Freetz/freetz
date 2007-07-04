8a8844c466bedb1db67b1f71268afb269f46af62
Alexander Kriegisch <kriegaex@freetz.org>
2007-07-04 13:31:32 +0000

    Orange!Box patch failed for W701V -> create patch variant for Speedport
    git-svn-id: http://svn.freetz.org/trunk@726 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/250-orangebox.sh
+++ patches/250-orangebox.sh
@@ -1,24 +1,26 @@
-if [ "$DS_PACKAGE_ORANGEBOX" == "y" ]
-then
+if [ "$DS_PACKAGE_ORANGEBOX" == "y" ]; then
 	echo1 "adding orangebox-lines to rc.S"
-	if [ "$DS_TYPE_LABOR" == "y" ] && ! [ "$DS_TYPE_LABOR_WLAN" == "y" ]
-	then
-	    if [ "$DS_TYPE_LABOR_PHONE" == "y" ]
-	    then
-		modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/orangebox_7170_labor_phone.patch
-	    else
-		modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/orangebox_7170_labor.patch
-	    fi
+	if [ "$DS_TYPE_LABOR" == "y" ] && ! [ "$DS_TYPE_LABOR_WLAN" == "y" ]; then
+		if [ "$DS_TYPE_LABOR_PHONE" == "y" ]; then
+			modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/orangebox_7170_labor_phone.patch
+		else
+			modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/orangebox_7170_labor.patch
+		fi
+	elif [ "${DS_TYPE_STRING/[579]0[01]/x0y}" == "Wx0yV" ]; then
+		# kriegaex: At the time of writing this the patch would succeed for
+		# W701V and W900V, but fail for W501V. But Orangebox can only can be
+		# selected for W701V anyway. Why this is so, I don't know.
+		# TODO(?): orangebox-enable the other two and then split the patch
+		# again for W501V or make one for each box.
+		modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/orangebox_speedport.patch
 	else
-	modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/orangebox.patch
+		modpatch "$FILESYSTEM_MOD_DIR" "$PATCHES_DIR"/cond/orangebox.patch
 	fi
-	
-	if [ "$DS_PACKAGE_ORANGEBOX_TSB" == "y" ]
-	then
-	    sed -i -e "s/isTSB 0/isTSB 1/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
+
+	if [ "$DS_PACKAGE_ORANGEBOX_TSB" == "y" ]; then
+		sed -i -e "s/isTSB 0/isTSB 1/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
 	fi
-	if [ "$DS_PACKAGE_WOL_CGI" == "y" ]
-	then
-	    sed -i -e "s/isWOL 0/isWOL 1/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
+	if [ "$DS_PACKAGE_WOL_CGI" == "y" ]; then
+		sed -i -e "s/isWOL 0/isWOL 1/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
 	fi
 fi
