1e90702b5b2c7e960e60bfeeb07b6a55c14b03ba
Oliver Metz <oliver@freetz.org>
2007-07-05 19:58:12 +0000

    Merge in #749 from branch 'stable-15'
    git-svn-id: http://svn.freetz.org/trunk@750 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/W701V/200-tk-firmware.sh
+++ patches/W701V/200-tk-firmware.sh
@@ -13,7 +13,8 @@ mkdir "${FILESYSTEM_MOD_DIR}/usr/www/all"
 ln -sf all "${FILESYSTEM_MOD_DIR}/usr/www/tcom"
 ln -sf all "${FILESYSTEM_MOD_DIR}/usr/www/avm"
 
-"$TAR" -c -C "${DIR}/.tk/original/filesystem/etc/default.Fritz_Box_7170/avm" . | "$TAR" -xk -C "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_SpeedportW701V/tcom"
+rm -rf "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_SpeedportW701V/tcom"
+"$TAR" -c -C "${DIR}/.tk/original/filesystem/etc/default.Fritz_Box_7170" avm | "$TAR" -x -C "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_SpeedportW701V" 
 ln -sf avm "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_SpeedportW701V/tcom"
 
 echo1 "copying mailer"
