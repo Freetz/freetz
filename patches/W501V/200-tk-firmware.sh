165fce698fab2e2a86f7f096dee5734e85c30f64
Michael Hampicke <mike@freetz.org>
2007-07-06 19:49:47 +0000

    - Fixed typo
    git-svn-id: http://svn.freetz.org/branches/stable-15@763 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/W501V/200-tk-firmware.sh
+++ patches/W501V/200-tk-firmware.sh
@@ -13,7 +13,7 @@ mkdir "${FILESYSTEM_MOD_DIR}/usr/www/all"
 ln -sf all "${FILESYSTEM_MOD_DIR}/usr/www/tcom"
 ln -sf all "${FILESYSTEM_MOD_DIR}/usr/www/avm"
 
-"$TAR" -c -C "${DIR}/.tk/original/filesystem/etc/default.Fritz_Box_7140/avm" . | "$TAR" -xk -C "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_SpeedportW501V/tcom"
+"$TAR" -c -C "${DIR}/.tk/original/filesystem/etc/default.Fritz_Box_7140/avm" . | "$TAR" -x -C "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_SpeedportW501V/tcom"
 ln -sf avm "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_SpeedportW501V/tcom"
 
 echo1 "copying mailer"
@@ -26,3 +26,7 @@ cp -a ${DIR}/.tk/original/filesystem/sbin/igdd "${FILESYSTEM_MOD_DIR}/sbin/"
 
 echo1 "replacing multid"
 cp -a ${DIR}/.tk/original/filesystem/sbin/multid "${FILESYSTEM_MOD_DIR}/sbin/"
+
+echo1 "copying ar7login"
+cp -a ${DIR}/.tk/original/filesystem/sbin/ar7login "${FILESYSTEM_MOD_DIR}/sbin/"
+
