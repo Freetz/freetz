1606b54514a493600455be50342e12f75b9410f0
Alexander Kriegisch <kriegaex@freetz.org>
2007-07-07 23:37:22 +0000

    svn merge -r 770:774 http://netfreaks.org/svn/ds-mod/trunk -- MC package improvements:   * Remove 143 KB of unnecessary syntax highlighting definitions in 22 files,     because MC 4.5.0 only supports hard-coded syntax highlighting (big string     in edit/syntax.c). Syntax definitions were externalised CVS version 1.17     of that file, but in 4.5.0 we have version 1.11 which is three years     older. The fascinating thing is that nobody ever complained about this.   * Update MC default settings, hopefully nobosy will complain:       + Editor tab width changed from 8 to 4       + Learn keys set to values which putty sends with TERM=xterm in telnet         and ssh sessions. The assumption here is that most users probably use         Putty on WinXP as their preferred terminal client. Other users can         still create their own ~/.mc/ini in debug.cfg or otherwise, assuming         that if they are Linux users who need this, they probably are more         geek-ish than Windows users.       + A few other minor changes which I had in my long-tested private         ini-file   * Remove MC online help file (115 KB) from package, because it now is an     optional component (see below).   * Bump download package to version 0.5 and update it on mirrors.   * Online help is a separate menuconfig option now. If chosen, the file will     be copied from the source to the package directory, otherwise the file     will be removed. This option does not yield a recompile.   * Syntax highlighting is a separate menuconfig option now. If chosen, the     main binary will contain hard-coded syntax highlighting definitions. This     option yields a complete recompile, if changed, because a conditional     patch must be applied to exclude the highlighting code from the binary.   * Both new menuconfig options have a default of "y", because this is how it     used to be all the time.
    git-svn-id: http://svn.freetz.org/branches/stable-15@775 f5190166-0702-4917-9039-51ec32eddaf5

--- make/mc/mc.mk
+++ make/mc/mc.mk
@@ -4,11 +4,16 @@ MC_SITE:=http://www.ibiblio.org/pub/Linux/utils/file/managers/mc/old
 MC_MAKE_DIR:=$(MAKE_DIR)/mc
 MC_DIR:=$(SOURCE_DIR)/mc-$(MC_VERSION)
 MC_BINARY:=$(MC_DIR)/src/mc
-MC_PKG_VERSION:=0.4
+MC_HELP:=$(MC_DIR)/src/mc.hlp
+MC_PKG_VERSION:=0.5
 MC_PKG_SOURCE:=mc-$(MC_VERSION)-dsmod-$(MC_PKG_VERSION).tar.bz2
 MC_PKG_SITE:=http://dsmod.magenbrot.net
 MC_TARGET_DIR:=$(PACKAGES_DIR)/mc-$(MC_VERSION)
 MC_TARGET_BINARY:=$(MC_TARGET_DIR)/root/usr/bin/mc.bin
+MC_TARGET_HELP:=$(MC_TARGET_DIR)/root/usr/lib/mc/mc.hlp
+
+MC_DS_CONFIG_FILE:=$(MC_MAKE_DIR)/.ds_config
+MC_DS_CONFIG_TEMP:=$(MC_MAKE_DIR)/.ds_config.temp
 
 $(DL_DIR)/$(MC_SOURCE): | $(DL_DIR)
 	wget -P $(DL_DIR) $(MC_SITE)/$(MC_SOURCE)
@@ -16,11 +21,27 @@ $(DL_DIR)/$(MC_SOURCE): | $(DL_DIR)
 $(DL_DIR)/$(MC_PKG_SOURCE): | $(DL_DIR)
 	@$(DL_TOOL) $(DL_DIR) $(TOPDIR)/.config $(MC_PKG_SOURCE) $(MC_PKG_SITE)
 
-$(MC_DIR)/.unpacked: $(DL_DIR)/$(MC_SOURCE)
+# Exclude online help setting (DS_MC_ONLINE_HELP) from config file, because we
+# do not want a full rebuild if only one help file must be copied or removed.
+#@echo "DS_MC_ONLINE_HELP=$(if $(DS_MC_ONLINE_HELP),y,n)" >> $(MC_DS_CONFIG_TEMP)
+$(MC_DS_CONFIG_FILE): $(TOPDIR)/.config
+	@echo "DS_MC_SYNTAX_COLOURING=$(if $(DS_MC_SYNTAX_COLOURING),y,n)" > $(MC_DS_CONFIG_TEMP)
+	@diff -q $(MC_DS_CONFIG_TEMP) $(MC_DS_CONFIG_FILE) || \
+		cp $(MC_DS_CONFIG_TEMP) $(MC_DS_CONFIG_FILE)
+	@rm -f $(MC_DS_CONFIG_TEMP)
+
+# Make sure that a perfectly clean build is performed whenever DS-Mod package
+# options have changed. The safest way to achieve this is by starting over
+# with the source directory.
+$(MC_DIR)/.unpacked: $(DL_DIR)/$(MC_SOURCE) $(MC_DS_CONFIG_FILE)
+	rm -rf $(MC_DIR)
 	tar -C $(SOURCE_DIR) $(VERBOSE) -xzf $(DL_DIR)/$(MC_SOURCE)
 	for i in $(MC_MAKE_DIR)/patches/*.patch; do \
 		patch -d $(MC_DIR) -p0 < $$i; \
 	done
+ifneq ($(strip $(DS_MC_SYNTAX_COLOURING)),y)
+	patch -d $(MC_DIR) -p0 < $(MC_MAKE_DIR)/patches/cond/mc-no-syntax-colouring.patch
+endif
 	touch $@
 
 $(MC_DIR)/.configured: $(MC_DIR)/.unpacked
@@ -52,17 +73,24 @@ $(MC_DIR)/.configured: $(MC_DIR)/.unpacked
 		--without-x \
 		--without-subshell \
 		--with-terminfo \
+		--without-gpm-mouse \
+		--with-included-slang \
 	);
 	touch $@
 
 $(MC_BINARY): $(MC_DIR)/.configured
 	PATH="$(TARGET_PATH)" $(MAKE) -C $(MC_DIR)
-	
-$(MC_TARGET_BINARY): $(MC_BINARY) 
+
+$(MC_HELP): $(MC_DIR)/.unpacked
+
+$(MC_TARGET_BINARY): $(MC_BINARY)
 	mkdir -p $(MC_TARGET_DIR)/root/usr/share/terminfo/x
 	cp $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/share/terminfo/x/xterm \
 		$(MC_TARGET_DIR)/root/usr/share/terminfo/x/
 	$(INSTALL_BINARY_STRIP)
+
+$(MC_TARGET_HELP): $(MC_HELP)
+	cp $(MC_HELP) $(MC_TARGET_HELP)
 
 $(PACKAGES_DIR)/.mc-$(MC_VERSION): $(DL_DIR)/$(MC_PKG_SOURCE) | $(PACKAGES_DIR)
 	@tar -C $(PACKAGES_DIR) -xjf $(DL_DIR)/$(MC_PKG_SOURCE)
@@ -73,9 +101,16 @@ mc: $(PACKAGES_DIR)/.mc-$(MC_VERSION)
 mc-package: $(PACKAGES_DIR)/.mc-$(MC_VERSION)
 	tar -C $(PACKAGES_DIR) $(VERBOSE) --exclude .svn -cjf $(PACKAGES_BUILD_DIR)/$(MC_PKG_SOURCE) mc-$(MC_VERSION)
 
-mc-precompiled: uclibc ncurses mc $(MC_TARGET_BINARY) 
+ifeq ($(strip $(DS_MC_ONLINE_HELP)),y)
+mc-precompiled: uclibc ncurses mc $(MC_TARGET_BINARY) $(MC_TARGET_HELP)
+else
+mc-precompiled: uclibc ncurses mc $(MC_TARGET_BINARY) mc-clean-help
+endif
 
 mc-source: $(MC_DIR)/.unpacked $(PACKAGES_DIR)/.mc-$(MC_VERSION)
+
+mc-clean-help: 
+	@rm -f $(MC_TARGET_HELP)
 
 mc-clean:
 	-$(MAKE) -C $(MC_DIR) clean
@@ -88,6 +123,7 @@ mc-dirclean:
 
 mc-uninstall: 
 	rm -f $(MC_TARGET_BINARY)
+	rm -f $(MC_TARGET_HELP)
 	rm -rf $(MC_TARGET_DIR)/root/usr/share/terminfo/x
 
 mc-list:
