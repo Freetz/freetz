f45f761e25a82ec81f6ced5dd598245e3c0405b2
Alexander Kriegisch <kriegaex@freetz.org>
2007-07-11 14:11:52 +0000

    Add Downloader CGI 0.1 by hermann72pb (ip-phone-forum.de) Support thread: http://www.ip-phone-forum.de/showthread.php?t=134934
    git-svn-id: http://svn.freetz.org/trunk@828 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/downloader/downloader.mk
@@ -0,0 +1,37 @@
+DOWNLOADER_VERSION:=0.1
+DOWNLOADER_PKG_SOURCE:=downloader-$(DOWNLOADER_VERSION)-dsmod.tar.bz2
+DOWNLOADER_PKG_SITE:=http://dsmod.3dfxatwork.de
+
+$(DL_DIR)/$(DOWNLOADER_PKG_SOURCE):
+	@$(DL_TOOL) $(DL_DIR) $(TOPDIR)/.config $(DOWNLOADER_PKG_SOURCE) $(DOWNLOADER_PKG_SITE)
+
+$(PACKAGES_DIR)/.downloader-$(DOWNLOADER_VERSION): $(DL_DIR)/$(DOWNLOADER_PKG_SOURCE)
+	@tar -C $(PACKAGES_DIR) -xjf $(DL_DIR)/$(DOWNLOADER_PKG_SOURCE)
+	@touch $@
+
+downloader: $(PACKAGES_DIR)/.downloader-$(DOWNLOADER_VERSION)
+
+downloader-package: $(PACKAGES_DIR)/.downloader-$(DOWNLOADER_VERSION)
+	tar -C $(PACKAGES_DIR) $(VERBOSE) --exclude .svn -cjf $(PACKAGES_BUILD_DIR)/$(DOWNLOADER_PKG_SOURCE) downloader-$(DOWNLOADER_VERSION)
+
+# If a compile should ever be necessary, don't forget to add 'uclibc' to prerequisites
+downloader-precompiled: downloader
+
+downloader-source: $(DOWNLOADER_DIR)/.downloader-$(DOWNLOADER_VERSION)
+
+downloader-clean:
+	rm -f $(PACKAGES_BUILD_DIR)/$(DOWNLOADER_PKG_SOURCE)
+
+downloader-dirclean:
+	rm -rf $(PACKAGES_DIR)/downloader-$(DOWNLOADER_VERSION)
+	rm -f $(PACKAGES_DIR)/.downloader-$(DOWNLOADER_VERSION)
+
+# Nothing to do here at the moment
+downloader-uninstall:
+
+downloader-list:
+ifeq ($(strip $(DS_PACKAGE_DOWNLOADER)),y)
+	@echo "S30downloader-$(DOWNLOADER_VERSION)" >> .static
+else
+	@echo "S30downloader-$(DOWNLOADER_VERSION)" >> .dynamic
+endif
