f262318ae59d166ebb4b6cd9b5d5d8f88b146162
Oliver Metz <oliver@freetz.org>
2007-07-20 21:31:09 +0000

    - Forgot to fix 195-webmenu_signed.sh (international firmware) - Allow  DS_PATCH_MEDIASRV only for boxes with USB-host
    git-svn-id: http://svn.freetz.org/trunk@884 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/195-webmenu_signed.sh
+++ patches/195-webmenu_signed.sh
@@ -1,5 +1,9 @@
-if [ "$DS_PATCH_SIGNED" == "y" ]
-then
-	echo1 "applying webmenu signed patch"
+if [ "$DS_PATCH_SIGNED" == "y" ]; then
+    echo1 "applying webmenu signed patch"
+    
+    if [ "$DS_TYPE_LANG_STRING" == "de" ]; then
 	sed -i -e "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/all/html/de/home/home.js
+    elif [ "$DS_TYPE_LANG_STRING" == "en" ]; then
+	sed -i -e "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/en/html/en/home/home.js
+    fi
 fi
