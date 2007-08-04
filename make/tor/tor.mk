ddb4071c58b95b6215b4d9acef36a472885bcef8
mickey <mickey@freetz.org>
2007-08-04 15:47:43 +0000

    updated to 0.1.2.16 (critical security bugfix, see http://archives.seul.org/or/announce/Aug-2007/msg00000.html)
    git-svn-id: http://svn.freetz.org/trunk@952 f5190166-0702-4917-9039-51ec32eddaf5

--- make/tor/tor.mk
+++ make/tor/tor.mk
@@ -1,6 +1,6 @@
 # released under the GNU public license v2
 #
-TOR_VERSION:=0.1.2.15
+TOR_VERSION:=0.1.2.16
 TOR_SOURCE:=tor-$(TOR_VERSION).tar.gz
 TOR_SITE:=http://tor.eff.org/dist
 TOR_MAKE_DIR:=$(MAKE_DIR)/tor
