0c085027e3f5d7ce15523bf7b77040fa65b1db85
Oliver Metz <oliver@freetz.org>
2007-08-15 07:50:51 +0000

    - Remove whitespace
    git-svn-id: http://svn.freetz.org/trunk@982 f5190166-0702-4917-9039-51ec32eddaf5

--- toolchain/make/download-toolchain.mk
+++ toolchain/make/download-toolchain.mk
@@ -19,14 +19,14 @@ download-toolchain: $(TOOLCHAIN_DIR)/kernel/.installed $(TOOLCHAIN_DIR)/target/.
 $(TOOLCHAIN_DIR)/kernel/.installed: $(DL_DIR)/$(KERNEL_TOOLCHAIN_SOURCE) $(TOOLS_DIR)/busybox
 	mkdir -p $(TOOLCHAIN_DIR)/build
 	rm -rf $(TOOLCHAIN_DIR)/kernel
-	$(TOOLS_DIR)/busybox tar $(VERBOSE) -xaf  $(DL_DIR)/$(KERNEL_TOOLCHAIN_SOURCE) -C $(TOOLCHAIN_DIR)/build
+	$(TOOLS_DIR)/busybox tar $(VERBOSE) -xaf $(DL_DIR)/$(KERNEL_TOOLCHAIN_SOURCE) -C $(TOOLCHAIN_DIR)/build
 	-@ln -s $(BUILD_DIR)/gcc-$(KERNEL_TOOLCHAIN_GCC_VERSION)-glibc-$(KERNEL_TOOLCHAIN_GLIBC_VERSION)/mipsel-unknown-linux-gnu $(TOOLCHAIN_DIR)/kernel
 	@touch $@
 
 $(TOOLCHAIN_DIR)/target/.installed: $(DL_DIR)/$(TARGET_TOOLCHAIN_SOURCE) $(TOOLS_DIR)/busybox
 	mkdir -p $(TOOLCHAIN_DIR)/build
 	rm -rf $(TOOLCHAIN_DIR)/target
-	$(TOOLS_DIR)/busybox tar $(VERBOSE) -xaf  $(DL_DIR)/$(TARGET_TOOLCHAIN_SOURCE) -C $(TOOLCHAIN_DIR)/build
+	$(TOOLS_DIR)/busybox tar $(VERBOSE) -xaf $(DL_DIR)/$(TARGET_TOOLCHAIN_SOURCE) -C $(TOOLCHAIN_DIR)/build
 	-@ln -s $(BUILD_DIR)/$(TARGET_TOOLCHAIN_COMPILER)/$(REAL_GNU_TARGET_NAME) $(TOOLCHAIN_DIR)/target
 	@touch $@
 
