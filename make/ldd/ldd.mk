d5097d72cdaf5e8ff6af30b5c0a4c2df1901fec5
Oliver Metz <oliver@freetz.org>
2007-08-21 22:18:27 +0000

    - Add ldd as package under debug helpers
    git-svn-id: http://svn.freetz.org/trunk@1005 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/ldd/ldd.mk
@@ -0,0 +1,55 @@
+LDD_VERSION:=0.1
+LDD_SOURCE:=ldd-$(LDD_VERSION).tar.bz2
+LDD_SITE:=http://dsmod.magenbrot.net
+LDD_DIR:=$(SOURCE_DIR)/ldd-$(LDD_VERSION)
+LDD_SOURCE_FILE:=$(LDD_DIR)/ldd.c
+LDD_BINARY:=$(LDD_DIR)/ldd
+LDD_PKG_VERSION:=0.1
+LDD_PKG_SITE:=http://dsmod.magenbrot.net
+LDD_PKG_SOURCE:=ldd-$(LDD_VERSION)-dsmod-$(LDD_PKG_VERSION).tar.bz2
+LDD_TARGET_DIR:=$(PACKAGES_DIR)/ldd-$(LDD_VERSION)
+LDD_TARGET_BINARY:=$(LDD_TARGET_DIR)/root/usr/bin/ldd
+
+
+$(DL_DIR)/$(LDD_SOURCE): | $(DL_DIR)
+	@$(DL_TOOL) $(DL_DIR) $(TOPDIR)/.config $(LDD_SOURCE) $(LDD_SITE)
+
+$(DL_DIR)/$(LDD_PKG_SOURCE): | $(DL_DIR)
+	@$(DL_TOOL) $(DL_DIR) $(TOPDIR)/.config $(LDD_PKG_SOURCE) $(LDD_PKG_SITE)
+
+$(LDD_DIR)/.unpacked: $(DL_DIR)/$(LDD_SOURCE)
+	tar -C $(SOURCE_DIR) $(VERBOSE) -xjf $(DL_DIR)/$(LDD_SOURCE)
+	touch $@
+
+$(LDD_BINARY): $(LDD_DIR)/.unpacked
+	PATH="$(TARGET_PATH)" \
+	    $(TARGET_CC) \
+	    $(TARGET_CFLAGS) \
+	    -DUCLIBC_RUNTIME_PREFIX=\ \
+	    $(LDD_SOURCE_FILE) -o $@ 
+
+$(LDD_TARGET_BINARY): $(LDD_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(PACKAGES_DIR)/.ldd-$(LDD_VERSION): $(DL_DIR)/$(LDD_PKG_SOURCE) | $(PACKAGES_DIR)
+	@tar -C $(PACKAGES_DIR) -xjf $(DL_DIR)/$(LDD_PKG_SOURCE)
+	@touch $@
+
+ldd: $(PACKAGES_DIR)/.ldd-$(LDD_VERSION)
+
+ldd-package: $(PACKAGES_DIR)/.ldd-$(LDD_VERSION)
+	tar -C $(PACKAGES_DIR) $(VERBOSE) --exclude .svn -czf $(PACKAGES_BUILD_DIR)/$(LDD_PKG_SOURCE) ldd-$(LDD_VERSION)
+
+ldd-precompiled: uclibc ldd $(LDD_TARGET_BINARY)
+
+ldd-source: $(LDD_DIR)/.unpacked $(PACKAGES_DIR)/.ldd-$(LDD_VERSION)
+
+ldd-clean:
+	-$(MAKE) -C $(LDD_DIR) clean
+	rm -f $(PACKAGES_BUILD_DIR)/$(LDD_PKG_SOURCE)
+
+ldd-dirclean:
+	rm -rf $(LDD_DIR)
+
+ldd-uninstall:
+	rm -f $(LDD_TARGET_BINARY)
