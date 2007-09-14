e7d21d9d2111684c484a5a74cf6cd3fdb22df4c8
Oliver Metz <oliver@freetz.org>
2007-09-14 21:05:41 +0000

    - Bump vpnc version to 0.5.1, update package - Add new hybrid auth feature (webinterface integration has to be done)
    git-svn-id: http://svn.freetz.org/trunk@1063 f5190166-0702-4917-9039-51ec32eddaf5

--- make/vpnc/vpnc.mk
+++ make/vpnc/vpnc.mk
@@ -1,4 +1,4 @@
-VPNC_VERSION:=0.5.0
+VPNC_VERSION:=0.5.1
 VPNC_SOURCE:=vpnc-$(VPNC_VERSION).tar.gz
 VPNC_SITE:=http://www.unix-ag.uni-kl.de/~massar/vpnc
 VPNC_MAKE_DIR:=$(MAKE_DIR)/vpnc
@@ -10,13 +10,36 @@ VPNC_PKG_SITE:=http://131.246.137.121/~metz/dsmod/packages
 VPNC_TARGET_DIR:=$(PACKAGES_DIR)/vpnc-$(VPNC_VERSION)
 VPNC_TARGET_BINARY:=$(VPNC_TARGET_DIR)/root/sbin/vpnc
 
+VPNC_DS_CONFIG_FILE:=$(VPNC_MAKE_DIR)/.ds_config
+VPNC_DS_CONFIG_TEMP:=$(VPNC_MAKE_DIR)/.ds_config.temp
+
+ifeq ($(strip $(DS_PACKAGE_VPNC_WITH_HYBRID_AUTH)),y)
+VPNC_CPPFLAGS:=-DOPENSSL_GPL_VIOLATION
+VPNC_LDFLAGS:=-lcrypto
+VPNC_OPENSSL:=openssl-precompiled
+else
+VPNC_CPPFLAGS:=
+VPNC_LDFLAGS:=
+VPNC_OPENSSL:=
+endif
+
 $(DL_DIR)/$(VPNC_SOURCE): | $(DL_DIR)
 	wget -P $(DL_DIR) $(VPNC_SITE)/$(VPNC_SOURCE)
 
 $(DL_DIR)/$(VPNC_PKG_SOURCE): | $(DL_DIR)
 	@$(DL_TOOL) $(DL_DIR) $(TOPDIR)/.config $(VPNC_PKG_SOURCE) $(VPNC_PKG_SITE)
 
-$(VPNC_DIR)/.unpacked: $(DL_DIR)/$(VPNC_SOURCE)
+$(VPNC_DS_CONFIG_FILE): $(TOPDIR)/.config
+	@echo "DS_PACKAGE_VPNC_WITH_HYBRID_AUTH=$(if $(DS_PACKAGE_VPNC_WITH_HYBRID_AUTH),y,n)" > $(VPNC_DS_CONFIG_TEMP)
+	@diff -q $(VPNC_DS_CONFIG_TEMP) $(VPNC_DS_CONFIG_FILE) || \
+	cp $(VPNC_DS_CONFIG_TEMP) $(VPNC_DS_CONFIG_FILE)
+	@rm -f $(VPNC_DS_CONFIG_TEMP)
+
+# Make sure that a perfectly clean build is performed whenever DS-Mod package
+# options have changed. The safest way to achieve this is by starting over
+# with the source directory.
+$(VPNC_DIR)/.unpacked: $(DL_DIR)/$(VPNC_SOURCE) $(VPNC_DS_CONFIG_FILE)
+	rm -rf $(VPNC_DIR)
 	tar -C $(SOURCE_DIR) $(VERBOSE) -xzf $(DL_DIR)/$(VPNC_SOURCE)
 	for i in $(VPNC_MAKE_DIR)/patches/*.patch; do \
 		$(PATCH_TOOL) $(VPNC_DIR) $$i; \
@@ -25,10 +48,10 @@ $(VPNC_DIR)/.unpacked: $(DL_DIR)/$(VPNC_SOURCE)
 
 $(VPNC_BINARY): $(VPNC_DIR)/.unpacked 
 	PATH=$(TARGET_PATH) \
-	    CPPFLAGS="-I$(TARGET_MAKE_PATH)/../usr/include" \
-	    $(MAKE) CC="$(TARGET_CC)" -C $(VPNC_DIR) \
+	    CPPFLAGS="-I$(TARGET_MAKE_PATH)/../usr/include $(VPNC_CPPFLAGS)" \
+	    $(MAKE) CC="$(TARGET_CC)" -C $(VPNC_DIR) vpnc \
 	    EXTRA_CFLAGS="$(TARGET_CFLAGS)" \
-	    EXTRA_LDFLAGS="-L$(TARGET_MAKE_PATH)/../usr/lib" 
+	    EXTRA_LDFLAGS="-L$(TARGET_MAKE_PATH)/../usr/lib $(VPNC_LDFLAGS)" \
 
 $(VPNC_TARGET_BINARY): $(VPNC_BINARY)
 	$(INSTALL_BINARY_STRIP)
@@ -42,7 +65,7 @@ vpnc: $(PACKAGES_DIR)/.vpnc-$(VPNC_VERSION)
 vpnc-package: $(PACKAGES_DIR)/.vpnc-$(VPNC_VERSION)
 	tar -C $(PACKAGES_DIR) $(VERBOSE) --exclude .svn -cjf $(PACKAGES_BUILD_DIR)/$(VPNC_PKG_SOURCE) vpnc-$(VPNC_VERSION)
 
-vpnc-precompiled: uclibc libgpg-error-precompiled libgcrypt-precompiled \
+vpnc-precompiled: uclibc $(VPNC_OPENSSL) libgpg-error-precompiled libgcrypt-precompiled \
 		    vpnc $(VPNC_TARGET_BINARY)
 
 vpnc-source: $(VPNC_DIR)/.unpacked $(PACKAGES_DIR)/.vpnc-$(VPNC_VERSION)
@@ -50,12 +73,14 @@ vpnc-source: $(VPNC_DIR)/.unpacked $(PACKAGES_DIR)/.vpnc-$(VPNC_VERSION)
 vpnc-clean:
 	-$(MAKE) -C $(VPNC_DIR) clean
 	rm -f $(PACKAGES_BUILD_DIR)/$(VPNC_PKG_SOURCE)
+	rm -f $(VPNC_DS_CONFIG_FILE)
 
 vpnc-dirclean:
 	rm -rf $(VPNC_DIR)
 	rm -rf $(PACKAGES_DIR)/vpnc-$(VPNC_VERSION)
 	rm -f $(PACKAGES_DIR)/.vpnc-$(VPNC_VERSION)
-	
+	rm -f $(VPNC_DS_CONFIG_FILE)
+
 vpnc-uninstall:
 	rm -f $(VPNC_TARGET_BINARY)
 
