042fbb4ad83de0eb3a2bcc39f3bc702317814b9d
Alexander Kriegisch <kriegaex@freetz.org>
2007-09-29 11:14:31 +0000

    Concatenate many ifeq-endif sequences into one larger ifeq-else-...-else-endif sequence.
    git-svn-id: http://svn.freetz.org/trunk@1093 f5190166-0702-4917-9039-51ec32eddaf5

--- make/avm-gpl/avm-gpl.mk
+++ make/avm-gpl/avm-gpl.mk
@@ -2,26 +2,16 @@ AVM_SITE:=ftp://ftp.avm.de/develper/opensrc
 
 ifeq ($(AVM_VERSION),04.29)
 AVM_SOURCE:=fritzbox-source-files-04.29.tar.bz2
-endif
-
-ifeq ($(AVM_VERSION),04.30)
+else ifeq ($(AVM_VERSION),04.30)
 AVM_SOURCE:=fritzbox7141-source-files-04.30.tar.bz2
-endif
-
-ifeq ($(AVM_VERSION),04.33)
+else ifeq ($(AVM_VERSION),04.33)
 AVM_SOURCE:=fritzbox-source-files-04.33.tar.bz2
-endif
-
-ifeq ($(AVM_VERSION),04.40)
+else ifeq ($(AVM_VERSION),04.40)
 AVM_SOURCE:=fritzbox-source-files.04.40.tar.bz2
-endif
-
-ifeq ($(AVM_VERSION),r4884)
+else ifeq ($(AVM_VERSION),r4884)
 AVM_SOURCE:=GPL-r4884-8mb_26-tar.bz2
 AVM_SITE:=http://www.t-home.de/dlp/eki/downloads/Speedport/Speedport%20W%20701%20V
-endif
-
-ifeq ($(AVM_VERSION),r7203)
+else ifeq ($(AVM_VERSION),r7203)
 AVM_SOURCE:=GPL-r7203-4mb_26-tar.bz2
 AVM_SITE:=http://www.t-home.de/dlp/eki/downloads/Speedport/Speedport_W501V
 endif
