d49fc099a818af0962ecc713fba6ddc8e59b0331
Oliver Metz <oliver@freetz.org>
2007-09-30 17:42:55 +0000

    Forgot to save changes
    git-svn-id: http://svn.freetz.org/trunk@1111 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/100-getcons.sh
+++ patches/100-getcons.sh
@@ -1,4 +1,4 @@
-# We don't have getcons anymore but setconsole does the same
+# Setconsole does the same. So delete getcons.
 echo1 "removing /bin/getcons"
 rm -f "${FILESYSTEM_MOD_DIR}/bin/getcons"
 sed -i -e 's/getcons/setconsole/g' ${FILESYSTEM_MOD_DIR}/etc/profile
