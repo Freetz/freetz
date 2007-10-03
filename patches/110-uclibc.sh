dcb96ddca9f080e81f32419ac95d7a3287efea4a
Oliver Metz <oliver@freetz.org>
2007-10-03 21:40:43 +0000

    - librt-0.9.28 (uClibc) was missing in Config.in - Patch removing uClibc-0.9.28 files when uClibc-0.9.29 is selected
    git-svn-id: http://svn.freetz.org/trunk@1132 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ patches/110-uclibc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+rm_files()
+{
+    for file in $1; do
+	echo2 "$file"
+	rm -f "$file"
+    done
+}
+
+if [ "$DS_TARGET_UCLIBC_VERSION" == "0.9.29" ]; then
+    echo1 "removing uClibc-0.9.28 files"
+    ln -sf ld-uClibc-0.9.29.so ${FILESYSTEM_MOD_DIR}/lib/ld.so.1
+    rm_files "$(find ${FILESYSTEM_MOD_DIR}/lib -maxdepth 1 -type f -name '*0.9.28*')"
+fi
