37a28b6b39fd4b3b70107c5d1e401f954da18123
Oliver Metz <oliver@freetz.org>
2007-10-05 16:26:13 +0000

    - Fix deadlock kernel-configured vs. kernel-toolchain - Remove 2nd pass gcc from gcc.mk - Now it's really fast
    git-svn-id: http://svn.freetz.org/trunk@1134 f5190166-0702-4917-9039-51ec32eddaf5

--- toolchain/make/kernel/gcc/gcc.mk
+++ toolchain/make/kernel/gcc/gcc.mk
@@ -23,8 +23,7 @@ GCC_KERNEL_SOURCE:=gcc-$(GCC_KERNEL_VERSION).tar.bz2
 GCC_KERNEL_SITE:=ftp://gcc.gnu.org/pub/gcc/releases/gcc-$(GCC_KERNEL_VERSION)
 GCC_KERNEL_DIR:=$(KERNEL_TOOLCHAIN_DIR)/gcc-$(GCC_KERNEL_VERSION)
 GCC_KERNEL_MAKE_DIR:=$(TOOLCHAIN_DIR)/make/kernel/gcc
-GCC_KERNEL_BUILD_DIR1:=$(KERNEL_TOOLCHAIN_DIR)/gcc-$(GCC_KERNEL_VERSION)-initial
-GCC_KERNEL_BUILD_DIR2:=$(KERNEL_TOOLCHAIN_DIR)/gcc-$(GCC_KERNEL_VERSION)-final
+GCC_KERNEL_BUILD_DIR:=$(KERNEL_TOOLCHAIN_DIR)/gcc-$(GCC_KERNEL_VERSION)-build
 
 ifeq ($(strip $(DS_STATIC_TOOLCHAIN)),y)
 GCC_KERNEL_EXTRA_MAKE_OPTIONS:="LDFLAGS=-static"
@@ -33,7 +32,6 @@ GCC_KERNEL_EXTRA_MAKE_OPTIONS:=
 endif
 
 GCC_KERNEL_STRIP_HOST_BINARIES:=true
-GCC_KERNEL_USE_SJLJ_EXCEPTIONS:=--enable-sjlj-exceptions
 
 $(DL_DIR)/$(GCC_KERNEL_SOURCE): | $(DL_DIR)
 	wget --passive-ftp -P $(DL_DIR) $(GCC_KERNEL_SITE)/$(GCC_KERNEL_SOURCE)
@@ -46,15 +44,9 @@ $(GCC_KERNEL_DIR)/.unpacked: $(DL_DIR)/$(GCC_KERNEL_SOURCE)
 	done
 	touch $@
 
-##############################################################################
-#
-#   initial gcc
-#
-##############################################################################
-
-$(GCC_KERNEL_BUILD_DIR1)/.configured: $(GCC_KERNEL_DIR)/.unpacked
-	mkdir -p $(GCC_KERNEL_BUILD_DIR1)
-	( cd $(GCC_KERNEL_BUILD_DIR1); PATH=$(KERNEL_TOOLCHAIN_PATH) \
+$(GCC_KERNEL_BUILD_DIR)/.configured: $(GCC_KERNEL_DIR)/.unpacked
+	mkdir -p $(GCC_KERNEL_BUILD_DIR)
+	( cd $(GCC_KERNEL_BUILD_DIR); PATH=$(KERNEL_TOOLCHAIN_PATH) \
 		CC="$(HOSTCC)" \
 		$(GCC_KERNEL_DIR)/configure \
 		--prefix=$(KERNEL_TOOLCHAIN_STAGING_DIR) \
@@ -70,95 +62,33 @@ $(GCC_KERNEL_BUILD_DIR1)/.configured: $(GCC_KERNEL_DIR)/.unpacked
 		--without-headers \
 		--disable-threads \
 		--disable-nls \
-		$(GCC_KERNEL_EXTRA_CONFIG_OPTIONS) \
-	);
-	### not in buildroot, what does this do?
-	mkdir -p $(GCC_KERNEL_BUILD_DIR1)/gcc
-	cp $(GCC_KERNEL_DIR)/gcc/defaults.h $(GCC_KERNEL_BUILD_DIR1)/gcc/defaults.h
-	$(SED) -i -e 's/\.eh_frame/.text/' $(GCC_KERNEL_BUILD_DIR1)/gcc/defaults.h
-	touch $@
-
-$(GCC_KERNEL_BUILD_DIR1)/.compiled: $(GCC_KERNEL_BUILD_DIR1)/.configured
-	PATH=$(KERNEL_TOOLCHAIN_PATH) $(MAKE) $(GCC_KERNEL_EXTRA_MAKE_OPTIONS) -C $(GCC_KERNEL_BUILD_DIR1) all-gcc
-	touch $@
-
-$(KERNEL_TOOLCHAIN_STAGING_DIR)/bin/$(REAL_GNU_KERNEL_NAME)-gcc: $(GCC_KERNEL_BUILD_DIR1)/.compiled
-	PATH=$(KERNEL_TOOLCHAIN_PATH) $(MAKE) -C $(GCC_KERNEL_BUILD_DIR1) install-gcc
-
-gcc_kernel_initial: binutils-kernel $(KERNEL_TOOLCHAIN_STAGING_DIR)/bin/$(REAL_GNU_KERNEL_NAME)-gcc
-
-gcc_kernel_initial-clean:
-	rm -rf $(GCC_KERNEL_BUILD_DIR1)
-
-gcc_kernel_initial-dirclean:
-	rm -rf $(GCC_KERNEL_BUILD_DIR1) $(GCC_KERNEL_DIR)
-
-##############################################################################
-#
-#   final gcc
-#
-##############################################################################
-
-$(GCC_KERNEL_BUILD_DIR2)/.configured: $(GCC_KERNEL_DIR)/.unpacked
-	mkdir -p $(GCC_KERNEL_BUILD_DIR2)
-	# Important!  Required for limits.h to be fixed.
-	ln -sf ../include $(KERNEL_TOOLCHAIN_STAGING_DIR)/$(REAL_GNU_KERNEL_NAME)/sys-include
-	( cd $(GCC_KERNEL_BUILD_DIR2); PATH=$(KERNEL_TOOLCHAIN_PATH) \
-		CC="$(HOSTCC)" \
-		$(GCC_KERNEL_DIR)/configure \
-		--prefix=$(KERNEL_TOOLCHAIN_STAGING_DIR) \
-		--build=$(GNU_HOST_NAME) \
-		--host=$(GNU_HOST_NAME) \
-		--target=$(REAL_GNU_KERNEL_NAME) \
-		--enable-languages=c \
-		--disable-__cxa_atexit \
-		--enable-target-optspace \
-		--with-gnu-ld \
-		--disable-libmudflap \
-		--disable-shared \
-		--without-headers \
-		--disable-threads \
-		--disable-nls \
-		--disable-largefile \
-		$(GCC_KERNEL_USE_SJLJ_EXCEPTIONS) \
-		$(GCC_KERNEL_EXTRA_CONFIG_OPTIONS) \
 	);
 	touch $@
 
-$(GCC_KERNEL_BUILD_DIR2)/.compiled: $(GCC_KERNEL_BUILD_DIR2)/.configured
-	PATH=$(KERNEL_TOOLCHAIN_PATH) $(MAKE) $(GCC_KERNEL_EXTRA_MAKE_OPTIONS) -C $(GCC_KERNEL_BUILD_DIR2) all
-	touch $(GCC_KERNEL_BUILD_DIR2)/.compiled
+$(GCC_KERNEL_BUILD_DIR)/.compiled: $(GCC_KERNEL_BUILD_DIR)/.configured
+	PATH=$(KERNEL_TOOLCHAIN_PATH) $(MAKE) $(GCC_KERNEL_EXTRA_MAKE_OPTIONS) -C $(GCC_KERNEL_BUILD_DIR) all
+	touch $@
 
-$(GCC_KERNEL_BUILD_DIR2)/.installed: $(GCC_KERNEL_BUILD_DIR2)/.compiled
-	PATH=$(KERNEL_TOOLCHAIN_PATH) $(MAKE) -C $(GCC_KERNEL_BUILD_DIR2) install
+$(KERNEL_TOOLCHAIN_STAGING_DIR)/bin/$(REAL_GNU_KERNEL_NAME)-gcc: $(GCC_KERNEL_BUILD_DIR)/.compiled
+	PATH=$(KERNEL_TOOLCHAIN_PATH) $(MAKE) -C $(GCC_KERNEL_BUILD_DIR) install
 	# Strip the host binaries
 ifeq ($(GCC_KERNEL_STRIP_HOST_BINARIES),true)
 	-strip --strip-all -R .note -R .comment $(KERNEL_TOOLCHAIN_STAGING_DIR)/bin/$(REAL_GNU_KERNEL_NAME)-*
 endif
-	# Set up the symlinks to enable lying about target name.
-	set -e; \
-	( cd $(KERNEL_TOOLCHAIN_STAGING_DIR); \
-		ln -sf $(REAL_GNU_KERNEL_NAME) $(GNU_KERNEL_NAME); \
-		cd bin; \
-		for app in $(REAL_GNU_KERNEL_NAME)-* ; do \
-			ln -sf $${app} \
-		   	$(GNU_KERNEL_NAME)$${app##$(REAL_GNU_KERNEL_NAME)}; \
-		done; \
-	);
-	touch $(GCC_KERNEL_BUILD_DIR2)/.installed
+	touch $@
 
-gcc-kernel: binutils-kernel gcc_kernel_initial $(GCC_KERNEL_BUILD_DIR2)/.installed
+gcc-kernel: binutils-kernel $(KERNEL_TOOLCHAIN_STAGING_DIR)/bin/$(REAL_GNU_KERNEL_NAME)-gcc
 
 gcc-kernel-source: $(DL_DIR)/$(GCC_KERNEL_SOURCE)
 
 gcc-kernel-clean:
-	rm -rf $(GCC_KERNEL_BUILD_DIR2)
+	rm -rf $(GCC_KERNEL_BUILD_DIR)
 	for prog in cpp gcc gcc-kernel-[0-9]* protoize unprotoize gcov gccbug cc; do \
 	    rm -f $(KERNEL_TOOLCHAIN_STAGING_DIR)/bin/$(REAL_GNU_KERNEL_NAME)-$$prog \
 	    rm -f $(KERNEL_TOOLCHAIN_STAGING_DIR)/bin/$(GNU_KERNEL_NAME)-$$prog; \
 	done
 
-gcc-kernel-dirclean: gcc_initial-dirclean
-	rm -rf $(GCC_KERNEL_BUILD_DIR2)
+gcc-kernel-dirclean:
+	rm -rf $(GCC_KERNEL_BUILD_DIR)  $(GCC_KERNEL_DIR)
 
-.PHONY: gcc-kernel gcc_kernel_initial
+.PHONY: gcc-kernel
