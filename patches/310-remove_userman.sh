9e2cb839fe1e649f61c55e7bb15f0735bde9a772
Oliver Metz <oliver@freetz.org>
2007-10-09 12:39:16 +0000

    * remove_userman: Add patch for boxes without wlan
    git-svn-id: http://svn.freetz.org/trunk@1161 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/310-remove_userman.sh
+++ patches/310-remove_userman.sh
@@ -18,5 +18,9 @@ if [ "$DS_REMOVE_USERMAN" == "y" ]; then
 	rm_files "$(find ${FILESYSTEM_MOD_DIR}/lib/modules -name userman)"
 	rm_files "$(find ${HTML_LANG_MOD_DIR} -name 'userlist*' -o -name 'useradd*')"
 	sed -i -e "s/KIDS=y/KIDS=n/g" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.init"
-	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/${DS_TYPE_LANG_STRING}/remove_userman.patch"
+	if [ "$DS_HAS_WLAN" == "y" ]; then
+	    modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/${DS_TYPE_LANG_STRING}/remove_userman_wlan.patch"
+	else
+	    modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/${DS_TYPE_LANG_STRING}/remove_userman.patch"
+	fi
 fi
