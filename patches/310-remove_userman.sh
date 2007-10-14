b96e5cb2be8dcda7ff9a2dfee114efae6c4bc973
Oliver Metz <oliver@freetz.org>
2007-10-14 23:41:03 +0000

    * Remove_userman: Because I'm too lazy sed deletes lines instead of a patch,    perhaps I will make patches if I feel fancy
    git-svn-id: http://svn.freetz.org/trunk@1202 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/310-remove_userman.sh
+++ patches/310-remove_userman.sh
@@ -13,19 +13,14 @@ if [ "$DS_REMOVE_USERMAN" == "y" ]; then
     rm -f "${FILESYSTEM_MOD_DIR}/bin/usermand"
     rm_files "$(find ${FILESYSTEM_MOD_DIR}/lib/modules -name userman)"
     rm_files "$(find ${HTML_LANG_MOD_DIR} -name 'userlist*' -o -name 'useradd*')"
+	for j in userlist useradd; do
+		for i in $(find "${HTML_LANG_MOD_DIR}" -type f | xargs grep -l $j); do
+        	sed -i -e "/$j/d" $i
+		done
+	done
     if [ -e "$FILESYSTEM_MOD_DIR/etc/init.d/rc.init" ]; then
-	sed -i -e "s/KIDS=y/KIDS=n/g" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.init"
-	if [ "$DS_HAS_WLAN" == "y" ]; then
-	    modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/${DS_TYPE_LANG_STRING}/remove_userman_wlan_old.patch"
-	else
-	    modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/${DS_TYPE_LANG_STRING}/remove_userman_old.patch"
-	fi
+		sed -i -e "s/KIDS=y/KIDS=n/g" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.init"
     else
-	sed -i -e "s/CONFIG_KIDS=y/CONFIG_KIDS=n/g" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.conf"
-	if [ "$DS_HAS_WLAN" == "y" ]; then
-	    modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/${DS_TYPE_LANG_STRING}/remove_userman_wlan_new.patch"
-        else
-	    modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/${DS_TYPE_LANG_STRING}/remove_userman_new.patch"
-        fi
+		sed -i -e "s/CONFIG_KIDS=y/CONFIG_KIDS=n/g" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.conf"
     fi
 fi
