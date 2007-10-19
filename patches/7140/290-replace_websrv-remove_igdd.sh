a2f0f891cad14ff6b0bd2a94c269235db0f59831
Oliver Metz <oliver@freetz.org>
2007-10-19 01:53:27 +0000

    Integrate new A-CH firmwares for 5010, 7140, 7170
    git-svn-id: http://svn.freetz.org/trunk@1223 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/7140/290-replace_websrv-remove_igdd.sh
+++ patches/7140/290-replace_websrv-remove_igdd.sh
@@ -3,6 +3,8 @@
 echo1 "applying replace websrv / remove igdd patch for 7140"
 if [ "$DS_TYPE_LANG_EN" == "y" ]; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/290-replace_websrv-remove_igdd_7140_en.patch"
+elif [ "$DS_TYPE_LANG_A_CH" == "y" ]; then
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/290-replace_websrv-remove_igdd_7140_a_ch.patch"
 else
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/290-replace_websrv-remove_igdd_7140_de.patch"
 fi
---
+++ patches/7170/120-rc.S-dev.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ $DS_TYPE_LANG_A_CH == "y" ]; then
+    modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/120-rc.S-dev_7170.a_ch.patch"
+else
+    modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/120-rc.S-dev_7170.patch"
+fi
