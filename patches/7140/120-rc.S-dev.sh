7a7f3c164d7c609ce13e7a61d4ae36988e036b07
Oliver Metz <oliver@freetz.org>
2007-10-19 23:08:38 +0000

    Fix patches for 7140 and 7170 (#1223)
    git-svn-id: http://svn.freetz.org/trunk@1235 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/7140/120-rc.S-dev.sh
+++ patches/7140/120-rc.S-dev.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if [ $DS_TYPE_LANG_A_CH == "y" ]; then
+
+if [ "$DS_TYPE_LANG_A_CH" == "y" ]; then
     modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/120-rc.S-dev_7140.a_ch.patch"
 else
     modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/120-rc.S-dev_7140.patch"
--- patches/7170/110-rc.S-mknod.sh
+++ patches/7170/110-rc.S-mknod.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 if [ "$DS_TYPE_LANG_A_CH" == "y" ]; then
     modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/110-rc.S-mknod_7170.a_ch.patch"
 else
