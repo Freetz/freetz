96cd8b1e35b2ae03518fffcbce047cf3a9ed96d6
Oliver Metz <oliver@freetz.org>
2007-10-20 16:49:54 +0000

    * Used wrong variable in modusers * Use same hash for webinterface and shadow
    git-svn-id: http://svn.freetz.org/trunk@1244 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/100-profile_password.sh
+++ patches/100-profile_password.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cat << 'EOF' >> ${FILESYSTEM_MOD_DIR}/etc/profile
-if [ $USER == "root" ] && grep -q '^root:$1$20WQVLFF$0Sc.MbHHNC0JaIJf6qZR1/' /etc/shadow; then
+if [ $USER == "root" ] && grep -q '^root:$1$9trcFt52$m6sHT4qeLllv0v9YJ2yV2/' /etc/shadow; then
     echo
     passwd
     modusers save
