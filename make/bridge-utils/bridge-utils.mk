5cfe99bb0e0a15a47acc93b82792c3fb2bcd2b3a
Ralf Friedl <Ralf.Friedl@online.de>
2007-10-26 20:29:04 +0000

    Define macro PACKAGE_CONFIGURED_CONFIGURE, change bridge-utils to use it.
    git-svn-id: http://svn.freetz.org/trunk@1278 f5190166-0702-4917-9039-51ec32eddaf5

--- make/bridge-utils/bridge-utils.mk
+++ make/bridge-utils/bridge-utils.mk
@@ -6,28 +6,24 @@ BRIDGE_UTILS_SITE:=http://mesh.dl.sourceforge.net/sourceforge/bridge
 BRIDGE_UTILS_MAKE_DIR:=$(MAKE_DIR)/bridge-utils
 BRIDGE_UTILS_DIR:=$(SOURCE_DIR)/bridge-utils-$(BRIDGE_UTILS_VERSION)
 BRIDGE_UTILS_BINARY:=$(BRIDGE_UTILS_DIR)/brctl/brctl
-#BRIDGE_UTILS_PKG_SOURCE:=bridge-utils-$(BRIDGE_UTILS_VERSION)-dsmod.tar.bz2
-#BRIDGE_UTILS_PKG_SITE:=http://131.246.137.121/~metz/dsmod/packages
 BRIDGE_UTILS_TARGET_DIR:=$(PACKAGES_DIR)/bridge-utils-$(BRIDGE_UTILS_VERSION)
 BRIDGE_UTILS_TARGET_BINARY:=$(BRIDGE_UTILS_TARGET_DIR)/root/sbin/brctl
-BRIDGE_UTILS_STARTLEVEL=40 # for PACKAGE_LIST
+BRIDGE_UTILS_STARTLEVEL=90 # for PACKAGE_LIST, no autostart
+
+$(PACKAGE_UC)_CONFIGURE_PRE_CMDS:=
+$(PACKAGE_UC)_CONFIGURE_PRE_CMDS += aclocal --force ;
+$(PACKAGE_UC)_CONFIGURE_PRE_CMDS += libtoolize --force ;
+$(PACKAGE_UC)_CONFIGURE_PRE_CMDS += autoconf --force ;
+$(PACKAGE_UC)_CONFIGURE_PRE_CMDS += autoheader --force ;
+
+$(PACKAGE_UC)_CONFIGURE_OPTIONS:=
+$(PACKAGE_UC)_CONFIGURE_OPTIONS += --with-randomdev=/dev/random
+$(PACKAGE_UC)_CONFIGURE_OPTIONS += --with-linux-headers=$(KERNEL_DIR)/linux/include
+
 
 $(PACKAGE_SOURCE_DOWNLOAD)
 $(PACKAGE_BIN_UNPACKED)
-
-$(BRIDGE_UTILS_DIR)/.configured: $(BRIDGE_UTILS_DIR)/.unpacked
-	( cd $(BRIDGE_UTILS_DIR); rm -f config.{cache,status}; \
-		aclocal --force ; \
-		libtoolize --force ; \
-		autoconf --force ; \
-		autoheader --force ; \
-		$(TARGET_CONFIGURE_ENV) \
-		./configure \
-		$(TARGET_CONFIGURE_OPTIONS) \
-		--with-randomdev=/dev/random \
-		--with-linux-headers=$(KERNEL_DIR)/linux/include \
-	)
-	touch $@
+$(PACKAGE_CONFIGURED_CONFIGURE)
 
 $(BRIDGE_UTILS_BINARY): $(BRIDGE_UTILS_DIR)/.configured
 	PATH="$(TARGET_PATH)" $(MAKE) -C $(BRIDGE_UTILS_DIR)
@@ -36,15 +32,7 @@ $(BRIDGE_UTILS_TARGET_BINARY): $(BRIDGE_UTILS_BINARY)
 	mkdir -p $(dir $(BRIDGE_UTILS_TARGET_BINARY))
 	$(INSTALL_BINARY_STRIP)
 
-#$(PACKAGES_DIR)/.bridge-utils-$(BRIDGE_UTILS_VERSION): $(DL_DIR)/$(BRIDGE_UTILS_PKG_SOURCE) | $(PACKAGES_DIR)
-#	@tar -C $(PACKAGES_DIR) -xjf $(DL_DIR)/$(BRIDGE_UTILS_PKG_SOURCE)
-#	@touch $@
-
-#bridge-utils: $(PACKAGES_DIR)/.bridge-utils-$(BRIDGE_UTILS_VERSION)
 bridge-utils: uclibc $(BRIDGE_UTILS_TARGET_BINARY)
-
-#bridge-utils-package: $(PACKAGES_DIR)/.bridge-utils-$(BRIDGE_UTILS_VERSION)
-#	tar -C $(PACKAGES_DIR) $(VERBOSE) --exclude .svn -cjf $(PACKAGES_BUILD_DIR)/$(BRIDGE_UTILS_PKG_SOURCE) bridge-utils-$(BRIDGE_UTILS_VERSION)
 
 bridge-utils-precompiled: bridge-utils $(BRIDGE_UTILS_TARGET_BINARY)
 
