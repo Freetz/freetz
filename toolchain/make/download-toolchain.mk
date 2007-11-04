cf5bbb4829eb242cacc7989666327314e98fdb51
Oliver Metz <oliver@freetz.org>
2007-11-04 22:28:32 +0000

    download-toolchain: Make busybox order-only prerequisite
    git-svn-id: http://svn.freetz.org/trunk@1361 f5190166-0702-4917-9039-51ec32eddaf5

--- toolchain/make/download-toolchain.mk
+++ toolchain/make/download-toolchain.mk
@@ -17,14 +17,14 @@ $(DL_DIR)/$(TARGET_TOOLCHAIN_SOURCE): | $(DL_DIR)
 download-toolchain: $(TOOLCHAIN_DIR)/kernel/.installed $(TOOLCHAIN_DIR)/target/.installed \
 						$(ROOT_DIR)/lib/libc.so.0 $(ROOT_DIR)/lib/libgcc_s.so.1
 
-$(TOOLCHAIN_DIR)/kernel/.installed: $(DL_DIR)/$(KERNEL_TOOLCHAIN_SOURCE) $(TOOLS_DIR)/busybox
+$(TOOLCHAIN_DIR)/kernel/.installed: $(DL_DIR)/$(KERNEL_TOOLCHAIN_SOURCE) | $(TOOLS_DIR)/busybox
 	mkdir -p $(TOOLCHAIN_DIR)/build
 	rm -rf $(TOOLCHAIN_DIR)/kernel
 	$(TOOLS_DIR)/busybox tar $(VERBOSE) -xaf $(DL_DIR)/$(KERNEL_TOOLCHAIN_SOURCE) -C $(TOOLCHAIN_DIR)/build
 	-@ln -s $(BUILD_DIR)/gcc-$(KERNEL_TOOLCHAIN_GCC_VERSION)/mipsel-unknown-linux-gnu $(TOOLCHAIN_DIR)/kernel
 	@touch $@
 
-$(TOOLCHAIN_DIR)/target/.installed: $(DL_DIR)/$(TARGET_TOOLCHAIN_SOURCE) $(TOOLS_DIR)/busybox
+$(TOOLCHAIN_DIR)/target/.installed: $(DL_DIR)/$(TARGET_TOOLCHAIN_SOURCE) | $(TOOLS_DIR)/busybox
 	mkdir -p $(TOOLCHAIN_DIR)/build
 	rm -rf $(TOOLCHAIN_DIR)/target
 	$(TOOLS_DIR)/busybox tar $(VERBOSE) -xaf $(DL_DIR)/$(TARGET_TOOLCHAIN_SOURCE) -C $(TOOLCHAIN_DIR)/build
