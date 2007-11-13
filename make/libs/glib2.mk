7f62bbe4c002319a170ef63cfc246cf98e5817eb
Oliver Metz <oliver@freetz.org>
2007-11-13 21:47:34 +0000

    * Rename glib to glib2 * Add some lines found in openwrt to Makefile
    git-svn-id: http://svn.freetz.org/trunk@1396 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/glib2.mk
+++ make/libs/glib2.mk
@@ -1,18 +1,26 @@
 $(call PKG_INIT_LIB, 2.12.12)
 $(PKG)_LIB_VERSION:=0.1200.12
-$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE:=glib-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=ftp://ftp.gtk.org/pub/glib/2.12
+$(PKG)_DIR:=$(SOURCE_DIR)/glib-$($(PKG)_VERSION)
 $(PKG)_BINARY:=$($(PKG)_DIR)/glib/.libs/libglib-2.0.so.$($(PKG)_LIB_VERSION)
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libglib-2.0.so.$($(PKG)_LIB_VERSION)
 $(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libglib-2.0.so.$($(PKG)_LIB_VERSION)
 
-$(PKG)_DEPENDS_ON := gettext libiconv
+$(PKG)_DEPENDS_ON += gettext libiconv
 
 $(PKG)_CONFIGURE_ENV += glib_cv_stack_grows=no
 $(PKG)_CONFIGURE_ENV += glib_cv_uscore=no
+$(PKG)_CONFIGURE_ENV += glib_cv_long_long_format=11
+$(PKG)_CONFIGURE_ENV += glib_cv_have_strlcpy=yes
+$(PKG)_CONFIGURE_ENV += ac_cv_func_posix_getpwuid_r=yes
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
-
+$(PKG)_CONFIGURE_OPTIONS += --enable-debug=no
+$(PKG)_CONFIGURE_OPTIONS += --disable-mem-pools
+$(PKG)_CONFIGURE_OPTIONS += --disable-rebuilds
+$(PKG)_CONFIGURE_OPTIONS += --with-libiconv=gnu
+$(PKG)_CONFIGURE_OPTIONS += --with-threads=posix
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -20,19 +28,20 @@ $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	PATH=$(TARGET_TOOLCHAIN_PATH) \
-		$(MAKE) -C $(GLIB_DIR)/glib \
-		all
+		$(MAKE) -C $(GLIB2_DIR)/glib \
+		all 
 
 $($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
 	PATH=$(TARGET_TOOLCHAIN_PATH) \
-		$(MAKE) -C $(GLIB_DIR)/glib \
+		$(MAKE) -C $(GLIB2_DIR)/glib \
 		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
 		install
 	$(PKG_FIX_LIBTOOL_LA) \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/libglib-2.0.la
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/libglib-2.0.la \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/pkgconfig/glib-2.0.pc
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
-	cp -a $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libglib-2.0.so* $(GLIB_TARGET_DIR)/
+	cp -a $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libglib-2.0.so* $(GLIB2_TARGET_DIR)/
 	$(TARGET_STRIP) $@
 
 $(pkg): $($(PKG)_STAGING_BINARY)
@@ -40,10 +49,10 @@ $(pkg): $($(PKG)_STAGING_BINARY)
 $(pkg)-precompiled: uclibc $(pkg) $($(PKG)_TARGET_BINARY)
 
 $(pkg)-clean:
-	-$(MAKE) -C $(GLIB_DIR) clean
+	-$(MAKE) -C $(GLIB2_DIR) clean
 	rm -f $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libglib-2.0*
 
 $(pkg)-uninstall:
-	rm -f $(GLIB_TARGET_DIR)/libglib-2.0.so*
+	rm -f $(GLIB2_TARGET_DIR)/libglib-2.0.so*
 
 $(PKG_FINISH)
