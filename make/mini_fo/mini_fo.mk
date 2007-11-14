3d0dc91f3282dc35ffcbdc72bd01fa506929b799
Oliver Metz <oliver@freetz.org>
2007-11-14 00:29:46 +0000

    * Update mini_fo package, copied from nfsroot * Remove two obsolete patches
    git-svn-id: http://svn.freetz.org/trunk@1402 f5190166-0702-4917-9039-51ec32eddaf5

--- make/mini_fo/mini_fo.mk
+++ make/mini_fo/mini_fo.mk
@@ -1,33 +1,17 @@
-MINI_FO_VERSION:=0.2b
-MINI_FO_PKG_SOURCE:=mini_fo-dsmod-$(MINI_FO_VERSION).tar.bz2
-MINI_FO_PKG_SITE:=http://dsmod.3dfxatwork.de
+$(call PKG_INIT_BIN, 0.3)
 
-$(DL_DIR)/$(MINI_FO_PKG_SOURCE): | $(DL_DIR)
-	@$(DL_TOOL) $(DL_DIR) $(TOPDIR)/.config $(MINI_FO_PKG_SOURCE) $(MINI_FO_PKG_SITE)
-
-$(PACKAGES_DIR)/.mini_fo-$(MINI_FO_VERSION): $(DL_DIR)/$(MINI_FO_PKG_SOURCE) | $(PACKAGES_DIR)
-	@tar -C $(PACKAGES_DIR) -xjf $(DL_DIR)/$(MINI_FO_PKG_SOURCE)
+$(PACKAGES_DIR)/.$(pkg)-$($(PKG)_VERSION):
+	mkdir -p $(MINI_FO_TARGET_DIR)/root
+	if test -d $(MINI_FO_MAKE_DIR)/files; then \
+	tar -c -C $(MINI_FO_MAKE_DIR)/files --exclude=.svn . | tar -x -C $(MINI_FO_TARGET_DIR) ; \
+	fi
 	@touch $@
-
-mini_fo: $(PACKAGES_DIR)/.mini_fo-$(MINI_FO_VERSION)
-
-mini_fo-package: $(PACKAGES_DIR)/.mini_fo-$(MINI_FO_VERSION)
-	tar -C $(PACKAGES_DIR) $(VERBOSE) --exclude .svn -cjf $(PACKAGES_BUILD_DIR)/$(MINI_FO_PKG_SOURCE) mini_fo-$(MINI_FO_VERSION)
-
+    
+mini_fo: $(PACKAGES_DIR)/.$(pkg)-$($(PKG)_VERSION)
+    
 mini_fo-precompiled: mini_fo
-
-mini_fo-source: $(PACKAGES_DIR)/.mini_fo-$(MINI_FO_VERSION)
-
+    
 mini_fo-clean:
 	rm -f $(PACKAGES_BUILD_DIR)/$(MINI_FO_PKG_SOURCE)
 
-mini_fo-dirclean:
-	rm -rf $(PACKAGES_DIR)/mini_fo-$(MINI_FO_VERSION)
-	rm -f $(PACKAGES_DIR)/.mini_fo-$(MINI_FO_VERSION)
-
-mini_fo-list:
-ifeq ($(strip $(DS_PACKAGE_MINI_FO)),y)
-	@echo "S40mini_fo-$(MINI_FO_VERSION)" >> .static
-else
-	@echo "S40mini_fo-$(MINI_FO_VERSION)" >> .dynamic
-endif
+$(PKG_FINISH)
--- patches/160-mini_fo.sh
+++
@@ -1,5 +0,0 @@
-if [ "$DS_PACKAGE_MINI_FO" == "y" ]
-then
-	echo1 "applying mini_fo patch"
-	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/mini_fo-inittab.patch"
-fi
