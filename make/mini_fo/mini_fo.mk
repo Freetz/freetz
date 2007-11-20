5006c44eb0748e2199a5fe8e77ea87e7c5d9c5b4
Alexander Kriegisch <kriegaex@freetz.org>
2007-11-20 05:54:43 +0000

    - Improve rc.mini_fo a bit - Canonise mini_fo.mk - Remove old mini_fo package
    git-svn-id: http://svn.freetz.org/trunk@1448 f5190166-0702-4917-9039-51ec32eddaf5

--- make/mini_fo/mini_fo.mk
+++ make/mini_fo/mini_fo.mk
@@ -1,17 +1,18 @@
 $(call PKG_INIT_BIN, 0.3)
 
-$(PACKAGES_DIR)/.$(pkg)-$($(PKG)_VERSION):
-	mkdir -p $(MINI_FO_TARGET_DIR)/root
-	if test -d $(MINI_FO_MAKE_DIR)/files; then \
-	tar -c -C $(MINI_FO_MAKE_DIR)/files --exclude=.svn . | tar -x -C $(MINI_FO_TARGET_DIR) ; \
-	fi
-	@touch $@
-    
-mini_fo: $(PACKAGES_DIR)/.$(pkg)-$($(PKG)_VERSION)
-    
-mini_fo-precompiled: mini_fo
-    
-mini_fo-clean:
+$(PKG_UNPACKED)
+
+$(pkg): $(PACKAGES_DIR)/.$(pkg)-$($(PKG)_VERSION)
+
+# If a compile should ever be necessary, don't forget to add 'uclibc' to prerequisites
+$(pkg)-precompiled: $(pkg)
+
+$(pkg)-source: $(PACKAGES_DIR)/.$(pkg)-$($(PKG)_VERSION)
+
+$(pkg)-clean:
 	rm -f $(PACKAGES_BUILD_DIR)/$(MINI_FO_PKG_SOURCE)
+
+# Nothing to do here at the moment
+$(pkg)-uninstall:
 
 $(PKG_FINISH)
