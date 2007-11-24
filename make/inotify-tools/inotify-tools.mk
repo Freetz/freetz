b958c8b163f7f6f22271edcd6068c4bb46c12517
Alexander Kriegisch <kriegaex@freetz.org>
2007-11-24 17:32:41 +0000

    Replace "inotify-tools" by "$(pkg)"
    git-svn-id: http://svn.freetz.org/trunk@1475 f5190166-0702-4917-9039-51ec32eddaf5

--- make/inotify-tools/inotify-tools.mk
+++ make/inotify-tools/inotify-tools.mk
@@ -1,6 +1,6 @@
 $(call PKG_INIT_BIN, 3.10)
 $(PKG)_LIB_VERSION:=0.3.3
-$(PKG)_SOURCE:=inotify-tools-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=http://mesh.dl.sourceforge.net/sourceforge/inotify-tools
 $(PKG)_INWAIT_BINARY:=$($(PKG)_DIR)/src/.libs/inotifywait
 $(PKG)_INWATCH_BINARY:=$($(PKG)_DIR)/src/.libs/inotifywatch
@@ -31,18 +31,18 @@ $($(PKG)_LIB_TARGET_BINARY): $($(PKG)_LIB_BINARY)
 	cp -a $(INOTIFY_TOOLS_DIR)/libinotifytools/src/.libs/libinotifytools.so* $(INOTIFY_TOOLS_TARGET_DIR)/root/usr/lib/
 	$(TARGET_STRIP) $@
 
-inotify-tools:
+$(pkg):
 
-inotify-tools-precompiled: uclibc inotify-tools \
+$(pkg)-precompiled: uclibc $(pkg) \
 		$($(PKG)_INWAIT_TARGET_BINARY) \
 		$($(PKG)_INWATCH_TARGET_BINARY) \
 		$($(PKG)_LIB_TARGET_BINARY)
 
-inotify-tools-clean:
+$(pkg)-clean:
 	-$(MAKE) -C $(INOTIFY_TOOLS_DIR) clean
 	rm -f $(INOTIFY_TOOLS_DIR)/.compiled
 
-inotify-tools-uninstall:
+$(pkg)-uninstall:
 	rm -f $(INOTIFY_TOOLS_INWAIT_TARGET_BINARY)
 	rm -f $(INOTIFY_TOOLS_INWATCH_TARGET_BINARY)
 	rm -f $(INOTIFY_TOOLS_TARGET_DIR)/root/usr/lib/libinotifytools.so*
