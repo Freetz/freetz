eeb8bc64ba681fe232dbf25c868b21cc72d52a68
Oliver Metz <oliver@freetz.org>
2007-11-24 11:51:54 +0000

    Fix inetd package, remove "experimental" warning in Config.in
    git-svn-id: http://svn.freetz.org/trunk@1473 f5190166-0702-4917-9039-51ec32eddaf5

--- make/inetd/inetd.mk
+++ make/inetd/inetd.mk
@@ -3,18 +3,12 @@ $(PKG)_PKG_VERSION:=$($(PKG)_VERSION)
 $(PKG)_PKG_NAME:=inetd-$($(PKG)_PKG_VERSION)
 $(PKG)_STARTLEVEL=20
 
-$(PACKAGES_DIR)/.$($(PKG)_PKG_NAME):
-	mkdir -p $(INETD_DEST_DIR)
-	tar -c -C $(INETD_MAKE_DIR)/files --exclude=.svn . | tar -x -C $(INETD_DEST_DIR)
-	@touch $@
-
-inetd: $(PACKAGES_DIR)/.$($(PKG)_PKG_NAME)
-
-inetd-precompiled: inetd
+$(PKG_UNPACKED)
 
 inetd-clean:
 	rm -f $(PACKAGES_BUILD_DIR)/$(INETD_PKG_SOURCE)
 
-inetd-uninstall:
+# Nothing to do here at the moment
+$(pkg)-uninstall:
 
 $(PKG_FINISH)
