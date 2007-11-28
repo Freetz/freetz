a70b9669b32d359bcc93aa39bb5ef80f608df551
Oliver Metz <oliver@freetz.org>
2007-11-28 21:52:40 +0000

    * Fix webmenu_signed patch for a-ch firmware * Fix webmenu-wol patch for 7270 * Don't allow replace websrv, mediasrv, remove upnp patches for 7270
    git-svn-id: http://svn.freetz.org/trunk@1499 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/195-webmenu_signed.sh
+++ patches/195-webmenu_signed.sh
@@ -1,7 +1,7 @@
 [ "$DS_PATCH_SIGNED" == "y" ] || return 0
 echo1 "applying webmenu signed patch"
 
-if [ "$DS_TYPE_LANG_STRING" == "de" ]; then
+if [ "$DS_TYPE_LANG_STRING" == "de" ] || [ "$DS_TYPE_LANG_STRING" == "a-ch" ] ; then
 	sed -i -e "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/all/html/de/home/home.js
 elif [ "$DS_TYPE_LANG_STRING" == "en" ]; then
 	sed -i -e "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/en/html/en/home/home.js
