2d712eefe61b95975db05e2503aa9fb2d6e86291
Oliver Metz <oliver@freetz.org>
2007-12-18 22:24:10 +0000

    * Update 7170 to 29.04.49 * Add 7270 Labor Mini (9640) * Deactivate some features because they do not work correctly (e.g. remove help)
    git-svn-id: http://svn.freetz.org/trunk@1578 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/7170/110-rc.S-mknod.sh
+++
@@ -1,6 +0,0 @@
-if [ "$DS_TYPE_LANG_A_CH" == "y" ]; then
-	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/110-rc.S-mknod_7170.a_ch.patch"
-else
-	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/110-rc.S-mknod_7170.patch"
-fi
-
--- patches/7170/290-replace_websrv-remove_igdd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo1 "applying replace websrv / remove igdd patch for 7170"
-if [ "$DS_TYPE_LANG_A_CH" == "y" ]; then
-	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/290-replace_websrv-remove_igdd_7170_a_ch.patch"
-else
-	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/290-replace_websrv-remove_igdd_7170_de.patch"
-fi
