0042b106612bce29ebe3289b1a13defd97d269db
Alexander Kriegisch <kriegaex@freetz.org>
2007-12-22 00:53:51 +0000

    Add workaround for "remove help" patch: In all firmwares' instances of the file "wds.js", the old sed command for removing the function assumed that there were no nested curly braces within the function body. This led to only partial deletion of the function and an invalid JavaScript file.
    I wonder why nobody seems to have experienced problems with it so far. Maybe
    every tester had switched off WDS.
    
    
    git-svn-id: http://svn.freetz.org/trunk@1584 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/220-help.sh
+++ patches/220-help.sh
@@ -7,8 +7,21 @@ find "${HTML_LANG_MOD_DIR}/menus" -type f |
 sed -i -e '/setvariable var:txtHelp/d' "${HTML_LANG_MOD_DIR}/global.inc"
 find "${HTML_LANG_MOD_DIR}/.." -name "*.html" -type f |
 	xargs sed -s -i -e '/<input type="button" onclick="uiDoHelp/d'
+
+# Known special case with nested curly braces -> match until next function
+# definition, but make sure that the latter is not deleted (exclude from range
+# by using an sed 'b' branch command)
+#
+# TODO: This is a nightmare and a typical problem of "sed patches". To fix this
+# properly we would need a real parser capable of exactly matching and removing
+# a (JavaScript) function definition.
+find "${HTML_LANG_MOD_DIR}/.." -name "wds.js" -type f |
+	xargs sed -s -i -e '/function uiDoHelp/, /^function/ { /^function uiDoHelp/d; /^function/b; d }'
+
+# Delete remaining function definitions without nested curly braces
 find "${HTML_LANG_MOD_DIR}/.." -name "*.js" -type f |
 	xargs sed -s -i -e '/function uiDoHelp/,/^}/d'
+
 find "${HTML_LANG_MOD_DIR}/js" -name "*.js" -type f |
 	xargs sed -s -i -e '/function jslPopHelp(pagename) {/,/}/d'
 
