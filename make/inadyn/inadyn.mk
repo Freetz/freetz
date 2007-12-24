b42a0e7c65374642ee032406b63a91de296d0da2
Oliver Metz <oliver@freetz.org>
2007-12-24 17:46:01 +0000

    Update inadyn makefile and package
    git-svn-id: http://svn.freetz.org/trunk@1599 f5190166-0702-4917-9039-51ec32eddaf5

--- make/inadyn/inadyn.mk
+++ make/inadyn/inadyn.mk
@@ -1,66 +1,34 @@
-INADYN_VERSION:=1.96.2
-INADYN_SOURCE:=inadyn.v$(INADYN_VERSION).zip
-#INADYN_SITE:=http://inadyn.ina-tech.net
-INADYN_SITE:=http://www.mirrorservice.org/sites/ftp.freebsd.org/pub/FreeBSD/distfiles
-INADYN_MAKE_DIR:=$(MAKE_DIR)/inadyn
-INADYN_DIR:=$(SOURCE_DIR)/inadyn-$(INADYN_VERSION)
-INADYN_BINARY:=$(INADYN_DIR)/inadyn/bin/linux/inadyn
-INADYN_PKG_VERSION:=0.5
-INADYN_PKG_SITE:=http://www.stieber-online.de/ds-mod
-INADYN_PKG_NAME:=inadyn-$(INADYN_VERSION)
-INADYN_PKG_SOURCE:=inadyn-$(INADYN_VERSION)-dsmod-$(INADYN_PKG_VERSION).tar.bz2
-INADYN_TARGET_DIR:=$(PACKAGES_DIR)/$(INADYN_PKG_NAME)
-INADYN_TARGET_BINARY:=$(INADYN_TARGET_DIR)/root/usr/sbin/inadyn
+$(call PKG_INIT_BIN,1.96.2)
+$(PKG)_SOURCE:=inadyn.v$($(PKG)_VERSION).zip
+#$(PKG)_SITE:=http://inadyn.ina-tech.net
+$(PKG)_SITE:=http://www.mirrorservice.org/sites/ftp.freebsd.org/pub/FreeBSD/distfiles
+$(PKG)_DIR:=$(SOURCE_DIR)/inadyn
+$(PKG)_BINARY:=$($(PKG)_DIR)/bin/linux/inadyn
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/inadyn
 
-$(DL_DIR)/$(INADYN_SOURCE): | $(DL_DIR)
-	wget -P $(DL_DIR) $(INADYN_SITE)/$(INADYN_SOURCE)
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
 
-$(DL_DIR)/$(INADYN_PKG_SOURCE): | $(DL_DIR)
-	@$(DL_TOOL) $(DL_DIR) $(TOPDIR)/.config $(INADYN_PKG_SOURCE) $(INADYN_PKG_SITE)
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+		$(MAKE) -C $(INADYN_DIR) \
+		CC="mipsel-linux-gcc" \
+		STRIP="mipsel-linux-strip" \
+		CFLAGS="$(TARGET_CFLAGS)" \
+		LDFLAGS="" 
 
-$(INADYN_DIR)/.unpacked: $(DL_DIR)/$(INADYN_SOURCE)
-	unzip -u $(DL_DIR)/$(INADYN_SOURCE) -d $(INADYN_DIR)
-	for i in $(INADYN_MAKE_DIR)/patches/*.patch; do \
-		$(PATCH_TOOL) $(INADYN_DIR) $$i; \
-	done
-	touch $@
-
-$(INADYN_BINARY): $(INADYN_DIR)/.unpacked
-	PATH="$(TARGET_PATH)" $(MAKE) CC="mipsel-linux-gcc" \
-		STRIP="mipsel-linux-strip" CFLAGS="$(TARGET_CFLAGS)" \
-		LDFLAGS="" -C $(INADYN_DIR)/inadyn 
-
-$(INADYN_TARGET_BINARY): $(INADYN_BINARY)
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
 
-$(PACKAGES_DIR)/.$(INADYN_PKG_NAME): $(DL_DIR)/$(INADYN_PKG_SOURCE) | $(PACKAGES_DIR)
-	@tar -C $(PACKAGES_DIR) -xjf $(DL_DIR)/$(INADYN_PKG_SOURCE)
-	@touch $@
+inadyn:
 
-inadyn: $(PACKAGES_DIR)/.$(INADYN_PKG_NAME)
-
-inadyn-precompiled: uclibc inadyn $(INADYN_TARGET_BINARY)
-
-inadyn-package: $(PACKAGES_DIR)/.$(INADYN_PKG_NAME)
-	tar -C $(PACKAGES_DIR) $(VERBOSE) --exclude .svn -cjf $(PACKAGES_BUILD_DIR)/$(INADYN_PKG_SOURCE) $(INADYN_PKG_NAME) 
-
-inadyn-source: $(INADYN_DIR)/.unpacked $(PACKAGES_DIR)/.$(INADYN_PKG_NAME)
+inadyn-precompiled: $($(PKG)_TARGET_BINARY)
 
 inadyn-clean:
-	-$(MAKE) -C $(INADYN_DIR)/inadyn clean
-	rm -f $(PACKAGES_BUILD_DIR)/$(INADYN_PKG_SOURCE)
-
-inadyn-dirclean:
-	rm -rf $(INADYN_DIR)
-	rm -rf $(PACKAGES_DIR)/$(INADYN_PKG_NAME)
-	rm -f $(PACKAGES_DIR)/.$(INADYN_PKG_NAME)
+	-$(MAKE) -C $(INADYN_DIR) clean
 
 inadyn-uninstall:
-	rm -f $(INADYN_TARGET_BINARY)
+	$(RM) $(INADYN_TARGET_BINARY)
 
-inadyn-list:
-ifeq ($(strip $(DS_PACKAGE_INADYN)),y)
-	@echo "S40inadyn-$(INADYN_VERSION)" >> .static
-else
-	@echo "S40inadyn-$(INADYN_VERSION)" >> .dynamic
-endif
+$(PKG_FINISH)
