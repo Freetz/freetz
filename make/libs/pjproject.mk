ab5e1a921ea01a79cab91d0b3bbbaa0ce17ce736
Oliver Metz <oliver@freetz.org>
2007-12-27 00:19:00 +0000

    * Bump pjproject to version 0.8 * Bump dtmfbox to version 0.4 (beta 7)
    git-svn-id: http://svn.freetz.org/trunk@1618 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/pjproject.mk
+++ make/libs/pjproject.mk
@@ -1,5 +1,5 @@
-$(call PKG_INIT_LIB, 0.7.0)
-$(PKG)_LIB_VERSION:=0.7.0
+$(call PKG_INIT_LIB, 0.8.0)
+$(PKG)_LIB_VERSION:=0.8.0
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=http://fritz.v3v.de/dtmfbox/libs
 $(PKG)_BINARY:=$($(PKG)_DIR)/pjsip/lib/libpjsip.a
@@ -10,6 +10,7 @@ $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpjsip.a
 $(PKG)_CONFIGURE_DEFOPTS := n
 
 $(PKG)_CONFIGURE_ENV += CFLAGS="$(TARGET_CFLAGS) -DPJ_DEBUG=0 -DNDEBUG=0"
+#-DPJMEDIA_HAS_SPEEX_AEC=0 -DPJMEDIA_HAS_L16_CODEC=0 -DPJMEDIA_HAS_GSM_CODEC=0 -DPJMEDIA_HAS_SPEEX_CODEC=0 -DPJMEDIA_HAS_ILBC_CODEC=0 -DPJMEDIA_SOUND_IMPLEMENTATION=PJMEDIA_SOUND_NULL_SOUND -DPJMEDIA_RESAMPLE_IMP=PJMEDIA_RESAMPLE_LIBRESAMPLE"
 $(PKG)_CONFIGURE_ENV += LDFLAGS="-lm"
 
 $(PKG)_CONFIGURE_OPTIONS += --target=$(GNU_TARGET_NAME)
@@ -32,6 +33,7 @@ $(PKG)_CONFIGURE_OPTIONS += --sysconfdir=/etc
 $(PKG)_CONFIGURE_OPTIONS += $(DISABLE_NLS)
 $(PKG)_CONFIGURE_OPTIONS += $(DISABLE_LARGEFILE)
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
+$(PKG)_CONFIGURE_OPTIONS += --enable-static
 $(PKG)_CONFIGURE_OPTIONS += --disable-sound
 $(PKG)_CONFIGURE_OPTIONS += --disable-large-filter
 $(PKG)_CONFIGURE_OPTIONS += --disable-small-filter
@@ -42,6 +44,7 @@ $(PKG)_CONFIGURE_OPTIONS += --disable-speex-codec
 $(PKG)_CONFIGURE_OPTIONS += --disable-ilbc-codec
 $(PKG)_CONFIGURE_OPTIONS += --disable-ssl
 $(PKG)_CONFIGURE_OPTIONS += --disable-floating-point
+$(PKG)_CONFIGURE_OPTIONS += --disable-libsamplerate
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -49,21 +52,21 @@ $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_DIR)/.depend: $($(PKG)_DIR)/.configured
 	PATH=$(TARGET_TOOLCHAIN_PATH) \
-	    $(MAKE) -C $(PJPROJECT_DIR) dep \
-	    TARGET_NAME="$(REAL_GNU_TARGET_NAME)"
+		$(MAKE) -C $(PJPROJECT_DIR) dep \
+		TARGET_NAME="$(REAL_GNU_TARGET_NAME)"
 	touch $@
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.depend
 	PATH=$(TARGET_TOOLCHAIN_PATH) \
-	    $(MAKE) -C $(PJPROJECT_DIR) all \
-	    TARGET_NAME="$(REAL_GNU_TARGET_NAME)"
+		$(MAKE) -C $(PJPROJECT_DIR) all \
+		TARGET_NAME="$(REAL_GNU_TARGET_NAME)"
 
 $($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
 	PATH=$(TARGET_TOOLCHAIN_PATH) \
-	    $(MAKE) -C $(PJPROJECT_DIR) \
-	    DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
-	    TARGET_NAME="$(REAL_GNU_TARGET_NAME)" \
-	    install
+		$(MAKE) -C $(PJPROJECT_DIR) \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+		TARGET_NAME="$(REAL_GNU_TARGET_NAME)" \
+		install
 
 #$(PJSIP_TARGET_BINARY): $(PJSIP_STAGING_BINARY)
 #	cp -a $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpjsip*.so* $(PJPROJECT_TARGET_DIR)
@@ -71,17 +74,17 @@ $($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
 
 $(pkg): $($(PKG)_STAGING_BINARY)
 
-$(pkg)-precompiled: uclibc $(pkg) #$($(PKG)_TARGET_BINARY)
+$(pkg)-precompiled:
 
 $(pkg)-clean:
 	-$(MAKE) -C $(PJPROJECT_DIR) \
-	    TARGET_NAME="$(REAL_GNU_TARGET_NAME)" \
-	    clean
+		TARGET_NAME="$(REAL_GNU_TARGET_NAME)" \
+		clean
 	rm -f $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpj*.a \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libresample.a
 	 
 
 #$(pkg)-uninstall:
 #	rm -f $(PJPROJECT_TARGET_DIR)/libpjsip*.so*
-    
+
 $(PKG_FINISH)
