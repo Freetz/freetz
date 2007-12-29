11855f5a159fb713fc4b5fbadab4a537db56da6b
Oliver Metz <oliver@freetz.org>
2007-12-29 21:06:00 +0000

    Add madplay package (by bodega)
    git-svn-id: http://svn.freetz.org/trunk@1632 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/madplay/madplay.mk
@@ -0,0 +1,41 @@
+$(call PKG_INIT_BIN, 0.15.2b)
+$(PKG)_SOURCE:=madplay-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=http://mesh.dl.sourceforge.net/sourceforge/mad
+$(PKG)_BINARY:=$($(PKG)_DIR)/madplay
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/madplay
+
+$(PKG)_DEPENDS_ON += zlib libid3tag libmad
+
+$(PKG)_CONFIGURE_PRE_CMDS += autoconf --force ;
+
+$(PKG)_CONFIGURE_OPTIONS += --disable-shared
+$(PKG)_CONFIGURE_OPTIONS += --enable-static
+$(PKG)_CONFIGURE_OPTIONS += --without-libiconv-prefix
+$(PKG)_CONFIGURE_OPTIONS += --without-libintl-prefix
+$(PKG)_CONFIGURE_OPTIONS += --without-rpath
+$(PKG)_CONFIGURE_OPTIONS += --disable-profiling
+$(PKG)_CONFIGURE_OPTIONS += --disable-debugging
+
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $(MADPLAY_DIR)/.configured
+	PATH="$(TARGET_PATH)"; \
+		$(MAKE) -C $(MADPLAY_DIR)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(MADPLAY_DIR) clean
+
+$(pkg)-uninstall:
+	rm -f $(MADPLAY_TARGET_BINARY)
+
+$(PKG_FINISH)
