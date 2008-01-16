9197f7a6e70ff4bc893e48bdf69fb23b2f4b757c
Oliver Metz <oliver@freetz.org>
2008-01-16 00:36:56 +0000

    Updating and moving around some patches for 7140
    git-svn-id: http://svn.freetz.org/trunk@1671 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/7140/110-rc.S-mknod.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ "$DS_TYPE_LANG_A_CH" == "y" ]; then
-    modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/110-rc.S-mknod_7140.a_ch.patch"
-else
-    modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/110-rc.S-mknod_7140.patch"
-fi
