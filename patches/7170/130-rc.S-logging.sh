f20fd903686d6305d844140e5e0016fdc9fe7099
Oliver Metz <oliver@freetz.org>
2008-01-16 00:38:50 +0000

    Add 58.04.47 and 29.04.47: International version for 7170 (english, annex a and annex b)
    git-svn-id: http://svn.freetz.org/trunk@1672 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ patches/7170/130-rc.S-logging.sh
@@ -0,0 +1,7 @@
+if [ "$DS_TYPE_LANG_EN" == "y" ]; then
+	if [ "$DS_TYPE_ANNEX_A" == "y" ]; then
+		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/en/rc.S-logging-7170-annexa.patch"
+	else
+		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/en/rc.S-logging-7170-annexb.patch"
+	fi
+fi
