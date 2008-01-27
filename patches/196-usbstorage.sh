25d45ba452620a3c10ec437bcce54e775351fc05
Oliver Metz <oliver@freetz.org>
2008-01-27 02:21:30 +0000

    * Activate usbstorage patch by default * Add automount feature introduced by tron (http://www.ip-phone-forum.de/showpost.php?p=946916&postcount=23) TODO: webinterface page for loading modules * Fix webmenu-wol-W900V.patch (fuzzy)
    git-svn-id: http://svn.freetz.org/trunk@1694 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/196-usbstorage.sh
+++ patches/196-usbstorage.sh
@@ -1,4 +1,15 @@
 [ "$DS_PATCH_USBSTORAGE" == "y" ] || return 0
 echo1 "applying USB storage patch"
-modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage.patch"
+if [ "$DS_TYPE_2170" == "y" ] || \
+	[ "$DS_TYPE_FON_WLAN_7140" == "y" -a "$DS_TYPE_LANG_DE" == "y" ] || \
+	[ "$DS_TYPE_FON_7150" == "y" ] || \
+	[ "$DS_TYPE_WLAN_3130" == "y" ] || \
+	[ "$DS_TYPE_WLAN_3131" == "y" ] || \
+	[ "$DS_TYPE_WLAN_3170" == "y" ]; then
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage_wotam.patch"
+elif [ "$DS_TYPE_SPEEDPORT_W900V" == "y" ]; then
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage_w900v.patch"
+else
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage.patch"
+fi
 
