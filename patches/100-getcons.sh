aa9ac05cede720a12ed7edbfce70f6a02e37d91c
Oliver Metz <oliver@freetz.org>
2008-02-01 17:32:40 +0000

    * Add menuconfig option to remove getcons from /etc/profile
    git-svn-id: http://svn.freetz.org/trunk@1710 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/100-getcons.sh
+++ patches/100-getcons.sh
@@ -2,3 +2,8 @@
 echo1 "removing /bin/getcons"
 rm -f "${FILESYSTEM_MOD_DIR}/bin/getcons"
 sed -i -e 's/getcons/setconsole/g' ${FILESYSTEM_MOD_DIR}/etc/profile
+
+if [ "$DS_PATCH_GETCONS" == "y" ]; then
+	sed -i -e '/setconsole/d' ${FILESYSTEM_MOD_DIR}/etc/profile
+	sed -i -e '/Console Ausgaben/d' ${FILESYSTEM_MOD_DIR}/etc/profile
+fi
