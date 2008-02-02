316aea570dd3d0fa64cefd438f2c0bc50a33a780
Oliver Metz <oliver@freetz.org>
2008-02-02 16:09:28 +0000

    * Adresses #6 (but this is not the final solution, perhaps we should make a patch for this)
    git-svn-id: http://svn.freetz.org/trunk@1715 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/100-getcons.sh
+++ patches/100-getcons.sh
@@ -5,5 +5,4 @@ sed -i -e 's/getcons/setconsole/g' ${FILESYSTEM_MOD_DIR}/etc/profile
 
 if [ "$DS_PATCH_GETCONS" == "y" ]; then
 	sed -i -e '/setconsole/d' ${FILESYSTEM_MOD_DIR}/etc/profile
-	sed -i -e '/Console Ausgaben/d' ${FILESYSTEM_MOD_DIR}/etc/profile
 fi
