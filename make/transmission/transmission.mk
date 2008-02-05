d71437a08260273445a6fea3dea6ca0ba837c23c
Oliver Metz <oliver@freetz.org>
2008-02-05 23:41:02 +0000

    * Bump transmission to version 1.04  * Change motd from ds-mod to freetz (by Whoopie)
    git-svn-id: http://svn.freetz.org/trunk@1720 f5190166-0702-4917-9039-51ec32eddaf5

--- make/transmission/transmission.mk
+++ make/transmission/transmission.mk
@@ -1,15 +1,14 @@
-$(call PKG_INIT_BIN, 0.93)
+$(call PKG_INIT_BIN, 1.04)
 $(PKG)_SOURCE:=transmission-$($(PKG)_VERSION).tar.bz2
 $(PKG)_SITE:=http://download.m0k.org/transmission/files
-$(PKG)_BINARY:=$($(PKG)_DIR)/cli/transmission-cli
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/transmission-cli
+$(PKG)_BINARY:=$($(PKG)_DIR)/cli/transmissioncli
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/transmissioncli
 
 $(PKG)_DEPENDS_ON := zlib openssl
 
 $(PKG)_CONFIGURE_ENV += CROSS="$(TARGET_CROSS)"
 $(PKG)_CONFIGURE_ENV += CC="$(TARGET_CC)"
 $(PKG)_CONFIGURE_ENV += PKG_CONFIG_PATH="$(TARGET_MAKE_PATH)/../usr/lib/pkgconfig"
-$(PKG)_CONFIGURE_OPTIONS += --disable-gtk
 
 
 $(PKG_SOURCE_DOWNLOAD)
