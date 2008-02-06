Freetz/freetz

commit 088166d9cd01040027bdfcdfbe7a0f476a499597

* openvpn: Bump version to 2.1_rc7, convert to new style (by Whoopie), resolves #14
git-svn-id: http://svn.freetz.org/trunk@1728 f5190166-0702-4917-9039-51ec32eddaf5

make/openvpn/openvpn.mk
@@ -1,126 +1,52 @@
-# based on OpenWrt SVN
-OPENVPN_VERSION:=2.1_rc4
-OPENVPN_SOURCE:=openvpn-$(OPENVPN_VERSION).tar.gz
-OPENVPN_SITE:=http://openvpn.net/release
-OPENVPN_MAKE_DIR:=$(MAKE_DIR)/openvpn
-OPENVPN_DIR:=$(SOURCE_DIR)/openvpn-$(OPENVPN_VERSION)
-OPENVPN_BINARY:=$(OPENVPN_DIR)/openvpn
-OPENVPN_PKG_VERSION:=0.7
-OPENVPN_PKG_SITE:=http://netfreaks.org/ds-mod
+$(call PKG_INIT_BIN, 2.1_rc7)
+$(PKG)_SOURCE:=openvpn-$(OPENVPN_VERSION).tar.gz
+$(PKG)_SITE:=http://openvpn.net/release
+$(PKG)_BINARY:=$($(PKG)_DIR)/openvpn
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/root/usr/sbin/openvpn
+$(PKG)_STARTLEVEL=50
 
 ifeq ($(strip $(DS_PACKAGE_OPENVPN_WITH_LZO)),y)
-OPENVPN_LZO:=lzo-precompiled 
-else
-OPENVPN_LZO:=
+$(PKG)_DEPENDS_ON := lzo
 endif
 
-OPENVPN_PKG_NAME:=openvpn-$(OPENVPN_VERSION)
-OPENVPN_PKG_SOURCE:=openvpn-$(OPENVPN_VERSION)-dsmod-$(OPENVPN_PKG_VERSION).tar.bz2
-OPENVPN_TARGET_DIR:=$(PACKAGES_DIR)/$(OPENVPN_PKG_NAME)
-OPENVPN_TARGET_BINARY:=$(OPENVPN_TARGET_DIR)/root/usr/sbin/openvpn
+$(PKG)_CONFIG_SUBOPTS += DS_PACKAGE_OPENVPN_WITH_LZO
+$(PKG)_CONFIG_SUBOPTS += DS_PACKAGE_OPENVPN_WITH_MGMNT
+$(PKG)_CONFIG_SUBOPTS += DS_PACKAGE_OPENVPN_ENABLE_SMALL
 
-OPENVPN_DS_CONFIG_FILE:=$(OPENVPN_MAKE_DIR)/.ds_config
-OPENVPN_DS_CONFIG_TEMP:=$(OPENVPN_MAKE_DIR)/.ds_config.temp
+$(PKG)_CONFIGURE_OPTIONS += --sysconfdir=/mod/etc/openvpn
+$(PKG)_CONFIGURE_OPTIONS += $(if $(DS_PACKAGE_OPENVPN_WITH_LZO),,--disable-lzo)
+$(PKG)_CONFIGURE_OPTIONS += --enable-shared
+$(PKG)_CONFIGURE_OPTIONS += --disable-static
+$(PKG)_CONFIGURE_OPTIONS += --disable-pthread
+$(PKG)_CONFIGURE_OPTIONS += --disable-debug
+$(PKG)_CONFIGURE_OPTIONS += --disable-plugins
+$(PKG)_CONFIGURE_OPTIONS += $(if $(DS_PACKAGE_OPENVPN_WITH_MGMNT),--enable-management,--disable-management)
+$(PKG)_CONFIGURE_OPTIONS += --disable-pkcs11
+$(PKG)_CONFIGURE_OPTIONS += --disable-socks
+$(PKG)_CONFIGURE_OPTIONS += --disable-http
+$(PKG)_CONFIGURE_OPTIONS += --enable-password-save
+$(PKG)_CONFIGURE_OPTIONS += $(if $(DS_PACKAGE_OPENVPN_ENABLE_SMALL),--enable-small,--disable-small)
 
-$(DL_DIR)/$(OPENVPN_SOURCE): | $(DL_DIR)
-	wget -P $(DL_DIR) $(OPENVPN_SITE)/$(OPENVPN_SOURCE)
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
 
-$(DL_DIR)/$(OPENVPN_PKG_SOURCE): | $(DL_DIR)
-	@$(DL_TOOL) $(DL_DIR) $(TOPDIR)/.config $(OPENVPN_PKG_SOURCE) $(OPENVPN_PKG_SITE)
-
-$(OPENVPN_DS_CONFIG_FILE): $(TOPDIR)/.config
-	@echo "DS_PACKAGE_OPENVPN_WITH_LZO=$(if $(DS_PACKAGE_OPENVPN_WITH_LZO),y,n)" > $(OPENVPN_DS_CONFIG_TEMP)
-	@echo "DS_PACKAGE_OPENVPN_WITH_MGMNT=$(if $(DS_PACKAGE_OPENVPN_WITH_MGMNT),y,n)" >> $(OPENVPN_DS_CONFIG_TEMP)
-	@echo "DS_PACKAGE_OPENVPN_ENABLE_SMALL=$(if $(DS_PACKAGE_OPENVPN_ENABLE_SMALL),y,n)" >> $(OPENVPN_DS_CONFIG_TEMP)
-	@diff -q $(OPENVPN_DS_CONFIG_TEMP) $(OPENVPN_DS_CONFIG_FILE) || \
-		cp $(OPENVPN_DS_CONFIG_TEMP) $(OPENVPN_DS_CONFIG_FILE)
-	@rm -f $(OPENVPN_DS_CONFIG_TEMP)
-
-# Make sure that a perfectly clean build is performed whenever DS-Mod package
-# options have changed. The safest way to achieve this is by starting over
-# with the source directory.
-$(OPENVPN_DIR)/.unpacked: $(DL_DIR)/$(OPENVPN_SOURCE) $(OPENVPN_DS_CONFIG_FILE)
-	rm -rf $(OPENVPN_DIR)
-	tar -C $(SOURCE_DIR) $(VERBOSE) -xzf $(DL_DIR)/$(OPENVPN_SOURCE)
-	touch $@
-
-$(OPENVPN_DIR)/.configured: $(OPENVPN_DIR)/.unpacked
-	( cd $(OPENVPN_DIR); rm -f config.{cache,status}; \
-		$(TARGET_CONFIGURE_OPTS) \
-		CFLAGS="$(TARGET_CFLAGS)" \
-		CPPFLAGS="-I$(TARGET_MAKE_PATH)/../usr/include" \
-		LDFLAGS="-L$(TARGET_MAKE_PATH)/../usr/lib" \
-		./configure \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--program-prefix="" \
-		--program-suffix="" \
-		--prefix=/usr \
-		--exec-prefix=/usr \
-		--bindir=/usr/bin \
-		--datadir=/usr/share \
-		--includedir=/usr/include \
-		--infodir=/usr/share/info \
-		--libdir=/usr/lib \
-		--libexecdir=/usr/lib \
-		--localstatedir=/var \
-		--mandir=/usr/share/man \
-		--sbindir=/usr/sbin \
-		--sysconfdir=/mod/etc/openvpn \
-		$(DISABLE_LARGEFILE) \
-		$(DISABLE_NLS) \
-		$(if $(DS_PACKAGE_OPENVPN_WITH_LZO),,--disable-lzo) \
-		--enable-shared \
-		--disable-static \
-		--disable-pthread \
-		--disable-debug \
-		--disable-plugins \
-		$(if $(DS_PACKAGE_OPENVPN_WITH_MGMNT),--enable-management,--disable-management) \
-		--disable-socks \
-		--disable-http \
-		--enable-password-save \
-		$(if $(DS_PACKAGE_OPENVPN_ENABLE_SMALL),--enable-small,--disable-small) \
-	);
-	touch $@
-
-$(OPENVPN_BINARY): $(OPENVPN_DIR)/.configured
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	PATH="$(TARGET_PATH)" \
 		$(MAKE) -C $(OPENVPN_DIR)
 
-$(OPENVPN_TARGET_BINARY): $(OPENVPN_BINARY)
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
 
-$(PACKAGES_DIR)/.$(OPENVPN_PKG_NAME): $(DL_DIR)/$(OPENVPN_PKG_SOURCE) | $(PACKAGES_DIR)
-	@tar -C $(PACKAGES_DIR) -xjf $(DL_DIR)/$(OPENVPN_PKG_SOURCE)
-	@touch $@
+$(pkg):
 
-openvpn: $(PACKAGES_DIR)/.$(OPENVPN_PKG_NAME)
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
-openvpn-package: #$(PACKAGES_DIR)/.$(OPENVPN_PKG_NAME)
-	tar -C $(PACKAGES_DIR) $(VERBOSE) --exclude .svn -cjf $(PACKAGES_BUILD_DIR)/$(OPENVPN_PKG_SOURCE) $(OPENVPN_PKG_NAME)
-
-openvpn-precompiled: uclibc $(OPENVPN_LZO) openssl-precompiled openvpn $(OPENVPN_TARGET_BINARY)
-
-openvpn-source: $(OPENVPN_DIR)/.unpacked $(PACKAGES_DIR)/.$(OPENVPN_PKG_NAME)
-
-openvpn-clean:
+$(pkg)-clean:
 	-$(MAKE) -C $(OPENVPN_DIR) clean
-	rm -f $(PACKAGES_BUILD_DIR)/$(OPENVPN_PKG_SOURCE)
-	rm -f $(OPENVPN_DS_CONFIG_FILE)
+	$(RM) $(OPENVPN_DS_CONFIG_FILE)
 
-openvpn-dirclean:
-	rm -rf $(OPENVPN_DIR)
-	rm -rf $(PACKAGES_DIR)/$(OPENVPN_PKG_NAME)
-	rm -f $(PACKAGES_DIR)/.$(OPENVPN_PKG_NAME)
-	rm -f $(OPENVPN_DS_CONFIG_FILE)
+$(pkg)-uninstall:
+	$(RM) $(OPENVPN_TARGET_BINARY)
 
-openvpn-uninstall:
-	rm -f $(OPENVPN_TARGET_BINARY)
-
-openvpn-list:
-ifeq ($(strip $(DS_PACKAGE_OPENVPN)),y)
-	@echo "S50openvpn-$(OPENVPN_VERSION)" >> .static
-else
-	@echo "S50openvpn-$(OPENVPN_VERSION)" >> .dynamic
-endif
+$(PKG_FINISH)

make/openvpn/Config.in
@@ -1,5 +1,5 @@
 config DS_PACKAGE_OPENVPN
-	bool "OpenVPN 2.1_rc4"
+	bool "OpenVPN 2.1_rc7"
 	select DS_LIB_libssl
 	default n
 	help

commit 6a52315f4fcc59d9f62b34e2266f13ca164d0d3d

* openvpn.mk: Fix typo (introduced in r1728)
git-svn-id: http://svn.freetz.org/trunk@1730 f5190166-0702-4917-9039-51ec32eddaf5

make/openvpn/openvpn.mk
@@ -2,7 +2,7 @@ $(call PKG_INIT_BIN, 2.1_rc7)
 $(PKG)_SOURCE:=openvpn-$(OPENVPN_VERSION).tar.gz
 $(PKG)_SITE:=http://openvpn.net/release
 $(PKG)_BINARY:=$($(PKG)_DIR)/openvpn
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/root/usr/sbin/openvpn
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/openvpn
 $(PKG)_STARTLEVEL=50
 
 ifeq ($(strip $(DS_PACKAGE_OPENVPN_WITH_LZO)),y)