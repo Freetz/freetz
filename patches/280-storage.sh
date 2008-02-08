2f69b8da82ff68acf248a5741f008b977028eacb
Oliver Metz <oliver@freetz.org>
2008-02-08 17:01:31 +0000

    * Bump 7270 firmware version to 54.04.52
    git-svn-id: http://svn.freetz.org/trunk@1750 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/280-storage.sh
+++ patches/280-storage.sh
@@ -1,7 +1,7 @@
 [ "$DS_HAS_USB_HOST" == "y" ] || return 0
 echo1 "patching run_mount"
-if [ "$DS_TYPE_FON_WLAN_7270" == "y" -a "$DS_TYPE_LABOR_BETA" == "y" ]; then
-	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/storage_7270_beta.patch"
+if [ "$DS_TYPE_FON_WLAN_7270" == "y" ]; then
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/storage_7270.patch"
 elif [ "$DS_TYPE_SPEEDPORT_W900V" != "y" ]; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/storage.patch"
 fi
