a14097bfa87e622d89ed6ae48ca4b39f81b8f41d
Oliver Metz <oliver@freetz.org>
2008-02-09 01:54:25 +0000

    * Bump ntfs-3g version to 1.2129  * ntfs: Remove package  * automount selects ntfs binary and ext3 module
    git-svn-id: http://svn.freetz.org/trunk@1752 f5190166-0702-4917-9039-51ec32eddaf5

--- make/ntfs/ntfs.mk
+++ make/ntfs/ntfs.mk
@@ -1,4 +1,4 @@
-$(call PKG_INIT_BIN, 1.1120)
+$(call PKG_INIT_BIN, 1.2129)
 $(PKG)_SOURCE:=ntfs-3g-$($(PKG)_VERSION).tgz
 $(PKG)_SITE:=http://www.ntfs-3g.org/
 $(PKG)_DIR:=$(SOURCE_DIR)/ntfs-3g-$($(PKG)_VERSION)
@@ -9,10 +9,9 @@ $(PKG)_STARTLEVEL=30
 $(PKG)_DEPENDS_ON += fuse
 
 $(PKG)_CONFIGURE_PRE_CMDS += autoconf --force ;
-$(PKG)_CONFIGURE_ENV += FUSE_MODULE_CFLAGS="-I$(TARGET_MAKE_PATH)/../usr/include/fuse"
-$(PKG)_CONFIGURE_ENV += FUSE_MODULE_LIBS="-pthread -lfuse -ldl"
 $(PKG)_CONFIGURE_OPTIONS += --disable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
+$(PKG)_CONFIGURE_OPTIONS += --disable-library
 
 
 $(PKG_SOURCE_DOWNLOAD)
@@ -28,7 +27,7 @@ $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
 
-ntfs: uclibc $($(PKG)_TARGET_BINARY)
+ntfs:
 
 ntfs-precompiled: $($(PKG)_TARGET_BINARY)
 
