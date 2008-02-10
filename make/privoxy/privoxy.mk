56765d06a19fc3de7a08fdfa5d659764a69e28ea
Whoopie <whoopie79@gmx.net>
2008-02-10 11:21:28 +0000

    privoxy: new macro style and files directory
    git-svn-id: http://svn.freetz.org/trunk@1765 f5190166-0702-4917-9039-51ec32eddaf5

--- make/privoxy/privoxy.mk
+++ make/privoxy/privoxy.mk
@@ -1,112 +1,51 @@
-# released under the GNU public license v2
-#
-PRIVOXY_VERSION:=3.0.6
-PRIVOXY_SOURCE:=privoxy-$(PRIVOXY_VERSION)-stable-src.tar.gz
-PRIVOXY_SITE:=http://surfnet.dl.sourceforge.net/sourceforge/ijbswa
-PRIVOXY_MAKE_DIR:=$(MAKE_DIR)/privoxy
-PRIVOXY_DIR:=$(SOURCE_DIR)/privoxy-$(PRIVOXY_VERSION)-stable
-PRIVOXY_BINARY:=$(PRIVOXY_DIR)/privoxy
-PRIVOXY_PKG_VERSION:=0.4
-PRIVOXY_PKG_SITE:=http://netfreaks.org/ds-mod
-PRIVOXY_PKG_NAME:=privoxy-$(PRIVOXY_VERSION)
-PRIVOXY_PKG_SOURCE:=privoxy-$(PRIVOXY_VERSION)-dsmod-$(PRIVOXY_PKG_VERSION).tar.bz2
-PRIVOXY_TARGET_DIR:=$(PACKAGES_DIR)/$(PRIVOXY_PKG_NAME)
-PRIVOXY_TARGET_BINARY:=$(PRIVOXY_TARGET_DIR)/root/usr/sbin/privoxy
+$(call PKG_INIT_BIN, 3.0.6)
+$(PKG)_SOURCE:=privoxy-$($(PKG)_VERSION)-stable-src.tar.gz
+$(PKG)_SITE:=http://surfnet.dl.sourceforge.net/sourceforge/ijbswa
+$(PKG)_DIR:=$(SOURCE_DIR)/privoxy-$($(PKG)_VERSION)-stable
+$(PKG)_BINARY:=$($(PKG)_DIR)/privoxy
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/privoxy
+$(PKG)_STARTLEVEL=40
 
-$(DL_DIR)/$(PRIVOXY_SOURCE): | $(DL_DIR)
-	wget -P $(DL_DIR) $(PRIVOXY_SITE)/$(PRIVOXY_SOURCE)
+$(PKG)_CONFIGURE_PRE_CMDS += autoheader;
+$(PKG)_CONFIGURE_PRE_CMDS += autoconf;
 
-$(DL_DIR)/$(PRIVOXY_PKG_SOURCE): | $(DL_DIR)
-	@$(DL_TOOL) $(DL_DIR) $(TOPDIR)/.config $(PRIVOXY_PKG_SOURCE) $(PRIVOXY_PKG_SITE)
+$(PKG)_CONFIGURE_ENV += ac_cv_func_setpgrp_void=yes
 
-$(PRIVOXY_DIR)/.unpacked: $(DL_DIR)/$(PRIVOXY_SOURCE)
-	tar -C $(SOURCE_DIR) $(VERBOSE) -xzf $(DL_DIR)/$(PRIVOXY_SOURCE)
-	for i in $(PRIVOXY_MAKE_DIR)/patches/*.patch; do \
-		$(PATCH_TOOL) $(PRIVOXY_DIR) $$i; \
-	done
-	touch $@
+$(PKG)_CONFIGURE_OPTIONS += --sysconfdir=/mod/etc
+$(PKG)_CONFIGURE_OPTIONS += --with-docbook=no
+$(PKG)_CONFIGURE_OPTIONS += --disable-pthread
+$(PKG)_CONFIGURE_OPTIONS += --disable-stats
+$(PKG)_CONFIGURE_OPTIONS += --disable-dynamic-pcre
+$(PKG)_CONFIGURE_OPTIONS += --disable-dynamic-pcrs
 
-$(PRIVOXY_DIR)/.configured: $(PRIVOXY_DIR)/.unpacked
-	( cd $(PRIVOXY_DIR); rm -f config.status; \
-		autoheader; \
-		autoconf; \
-		$(TARGET_CONFIGURE_OPTS) \
-		CFLAGS="$(TARGET_CFLAGS)" \
-		CPPFLAGS="-I$(TARGET_MAKE_PATH)/../usr/include" \
-		LDFLAGS="" \
-		ac_cv_func_setpgrp_void=yes \
-		./configure \
-		  --target=$(GNU_TARGET_NAME) \
-		  --host=$(GNU_TARGET_NAME) \
-		  --build=$(GNU_HOST_NAME) \
-		  --program-prefix="" \
-		  --program-suffix="" \
-		  --prefix=/usr \
-		  --exec-prefix=/usr \
-		  --bindir=/usr/sbin \
-		  --datadir=/usr/share \
-		  --includedir=/usr/include \
-		  --infodir=/usr/share/info \
-		  --libdir=/usr/lib \
-		  --libexecdir=/usr/lib \
-		  --localstatedir=/var \
-		  --mandir=/usr/share/man \
-		  --sbindir=/usr/sbin \
-		  --sysconfdir=/mod/etc \
-		  $(DISABLE_LARGEFILE) \
-		  $(DISABLE_NLS) \
-		  --with-docbook=no \
-		  --disable-pthread \
-		  --disable-stats \
-		  --disable-dynamic-pcre \
-		  --disable-dynamic-pcrs \
-	);
-	touch $@
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
 
-$(PRIVOXY_BINARY): $(PRIVOXY_DIR)/.configured
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	PATH="$(TARGET_PATH)" \
 	$(MAKE) -C $(PRIVOXY_DIR)
 
-$(PRIVOXY_TARGET_BINARY): $(PRIVOXY_BINARY)
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
 	for s in `find $(PRIVOXY_DIR)/templates/ -type f`; do \
 		d=$$(basename $$s); \
-		egrep -v "^#\ " $$s | egrep -v "^#*$$" >$(PRIVOXY_TARGET_DIR)/root/etc/privoxy/templates/$$d; \
+		egrep -v "^#\ " $$s | egrep -v "^#*$$" >$(PRIVOXY_DEST_DIR)/etc/privoxy/templates/$$d; \
 	done
 	for s in $(PRIVOXY_DIR)/default.filter $(PRIVOXY_DIR)/default.action $(PRIVOXY_DIR)/standard.action \
 		$(PRIVOXY_DIR)/user.action $(PRIVOXY_DIR)/user.filter; do \
 		d=$$(basename $$s); \
-		egrep -v "^#" $$s | egrep -v "^$$" >$(PRIVOXY_TARGET_DIR)/root/etc/privoxy/$$d; \
+		egrep -v "^#" $$s | egrep -v "^$$" >$(PRIVOXY_DEST_DIR)/etc/privoxy/$$d; \
 	done; true
 
-$(PACKAGES_DIR)/.$(PRIVOXY_PKG_NAME): $(DL_DIR)/$(PRIVOXY_PKG_SOURCE) | $(PACKAGES_DIR)
-	@tar -C $(PACKAGES_DIR) -xjf $(DL_DIR)/$(PRIVOXY_PKG_SOURCE)
-	@touch $@
+$(pkg):
 
-privoxy: $(PACKAGES_DIR)/.$(PRIVOXY_PKG_NAME)
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
-privoxy-package: $(PACKAGES_DIR)/.$(PRIVOXY_PKG_NAME)
-	tar -C $(PACKAGES_DIR) $(VERBOSE) --exclude .svn -cjf $(PACKAGES_BUILD_DIR)/$(PRIVOXY_PKG_SOURCE) $(PRIVOXY_PKG_NAME)
-
-privoxy-precompiled: uclibc privoxy $(PRIVOXY_TARGET_BINARY)
-
-privoxy-source: $(PRIVOXY_DIR)/.unpacked $(PACKAGES_DIR)/.$(PRIVOXY_PKG_NAME)
-
-privoxy-clean:
+$(pkg)-clean:
 	-$(MAKE) -C $(PRIVOXY_DIR) clean
-	rm -f $(PACKAGES_BUILD_DIR)/$(PRIVOXY_PKG_SOURCE)
 
-privoxy-dirclean:
-	rm -rf $(PRIVOXY_DIR)
-	rm -rf $(PACKAGES_DIR)/$(PRIVOXY_PKG_NAME)
-	rm -f $(PACKAGES_DIR)/.$(PRIVOXY_PKG_NAME)
+$(pkg)-uninstall:
+	$(RM) $(PRIVOXY_TARGET_BINARY)
 
-privoxy-uninstall:
-	rm -f $(PRIVOXY_TARGET_BINARY)
-
-privoxy-list:
-ifeq ($(strip $(DS_PACKAGE_PRIVOXY)),y)
-	@echo "S40privoxy-$(PRIVOXY_VERSION)" >> .static
-else
-	@echo "S40privoxy-$(PRIVOXY_VERSION)" >> .dynamic
-endif
+$(PKG_FINISH)
