e4ca5b499dbf282dee55de30178465e3ebadf54a
Oliver Metz <oliver@freetz.org>
2008-02-10 22:46:36 +0000

    * Bump 7270 labor mini to 54.04.52-10105 (http://www.ip-phone-forum.de/showpost.php?p=1034498&postcount=90)  * Disable 7270 labor beta (Image isn't available for download anymore)
    git-svn-id: http://svn.freetz.org/trunk@1794 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/280-storage.sh
+++ patches/280-storage.sh
@@ -1,7 +1,11 @@
 [ "$DS_HAS_USB_HOST" == "y" ] || return 0
 echo1 "patching run_mount"
 if [ "$DS_TYPE_FON_WLAN_7270" == "y" ]; then
-	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/storage_7270.patch"
+	if [ "$DS_TYPE_LABOR_MINI" == "y" ]; then
+		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/storage_7270_labor_mini.patch"
+	else
+		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/storage_7270.patch"
+	fi
 elif [ "$DS_TYPE_SPEEDPORT_W900V" != "y" ]; then
-	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/storage.patch"
+		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/storage.patch"
 fi
