6e3f155235577f7052b2e0314f6e8136d15f8d56
Whoopie <whoopie79@gmx.net>
2008-02-10 12:59:42 +0000

    virtualip-cgi: new macro style and files directory
    git-svn-id: http://svn.freetz.org/trunk@1773 f5190166-0702-4917-9039-51ec32eddaf5

--- make/virtualip-cgi/virtualip-cgi.mk
+++ make/virtualip-cgi/virtualip-cgi.mk
@@ -1,37 +1,13 @@
-VIRTUALIP_CGI_VERSION:=0.4.2
-VIRTUALIP_CGI_PKG_SOURCE:=virtualip-cgi-$(VIRTUALIP_CGI_VERSION)-dsmod.tar.bz2
-VIRTUALIP_CGI_PKG_SITE:=http://dsmod.3dfxatwork.de
+$(call PKG_INIT_BIN, 0.4.2)
+$(PKG)_STARTLEVEL=10
 
+$(PKG_UNPACKED)
 
-$(DL_DIR)/$(VIRTUALIP_CGI_PKG_SOURCE): | $(DL_DIR)
-	@$(DL_TOOL) $(DL_DIR) $(TOPDIR)/.config $(VIRTUALIP_CGI_PKG_SOURCE) $(VIRTUALIP_CGI_PKG_SITE)
+$(pkg)-cgi:
 
-$(PACKAGES_DIR)/.virtualip-cgi-$(VIRTUALIP_CGI_VERSION): $(DL_DIR)/$(VIRTUALIP_CGI_PKG_SOURCE) | $(PACKAGES_DIR)
-	@tar -C $(PACKAGES_DIR) -xjf $(DL_DIR)/$(VIRTUALIP_CGI_PKG_SOURCE)
-	@touch $@
+$(pkg)-cgi-precompiled:
 
-virtualip-cgi: $(PACKAGES_DIR)/.virtualip-cgi-$(VIRTUALIP_CGI_VERSION)
+$(pkg)-cgi-clean:
+	$(RM) $(PACKAGES_BUILD_DIR)/$(WOL_CGI_PKG_SOURCE)
 
-virtualip-cgi-package: $(PACKAGES_DIR)/.virtualip-cgi-$(VIRTUALIP_CGI_VERSION)
-	tar -C $(PACKAGES_DIR) $(VERBOSE) --exclude .svn -cjf $(PACKAGES_BUILD_DIR)/$(VIRTUALIP_CGI_PKG_SOURCE) virtualip-cgi-$(VIRTUALIP_CGI_VERSION)
-
-virtualip-cgi-precompiled: virtualip-cgi
-
-virtualip-cgi-source: $(PACKAGES_DIR)/.virtualip-cgi-$(VIRTUALIP_CGI_VERSION)
-
-virtualip-cgi-clean:
-	rm -f $(PACKAGES_BUILD_DIR)/$(VIRTUALIP_CGI_PKG_SOURCE)
-
-virtualip-cgi-dirclean:
-	rm -rf $(PACKAGES_DIR)/virtualip-cgi-$(VIRTUALIP_CGI_VERSION)
-	rm -f $(PACKAGES_DIR)/.virtualip-cgi-$(VIRTUALIP_CGI_VERSION)
-
-# Nothing to do here at the moment
-virtualip-cgi-uninstall:
-
-virtualip-cgi-list:
-ifeq ($(strip $(DS_PACKAGE_VIRTUALIP_CGI)),y)
-	@echo "S10virtualip-cgi-$(VIRTUALIP_CGI_VERSION)" >> .static
-else
-	@echo "S10virtualip-cgi-$(VIRTUALIP_CGI_VERSION)" >> .dynamic
-endif
+$(PKG_FINISH)
