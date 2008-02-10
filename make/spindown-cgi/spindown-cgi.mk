cc0017e8de9792491e751a1584d2dc9bf55ade0f
Whoopie <whoopie79@gmx.net>
2008-02-10 14:03:27 +0000

    spindown-cgi: new macro style and files directory
    git-svn-id: http://svn.freetz.org/trunk@1777 f5190166-0702-4917-9039-51ec32eddaf5

--- make/spindown-cgi/spindown-cgi.mk
+++ make/spindown-cgi/spindown-cgi.mk
@@ -1,37 +1,13 @@
-SPINDOWN_CGI_VERSION:=0.2
-SPINDOWN_CGI_PKG_SOURCE:=spindown-cgi-$(SPINDOWN_CGI_VERSION)-dsmod.tar.bz2
-SPINDOWN_CGI_PKG_SITE:=http://www.heimpold.de/dsmod
+$(call PKG_INIT_BIN,0.2)
+$(PKG)_STARTLEVEL=20
 
+$(PKG_UNPACKED)
 
-$(DL_DIR)/$(SPINDOWN_CGI_PKG_SOURCE): | $(DL_DIR)
-	@$(DL_TOOL) $(DL_DIR) $(TOPDIR)/.config $(SPINDOWN_CGI_PKG_SOURCE) $(SPINDOWN_CGI_PKG_SITE)
+$(pkg)-cgi:
 
-$(PACKAGES_DIR)/.spindown-cgi-$(SPINDOWN_CGI_VERSION): $(DL_DIR)/$(SPINDOWN_CGI_PKG_SOURCE) | $(PACKAGES_DIR)
-	@tar -C $(PACKAGES_DIR) -xjf $(DL_DIR)/$(SPINDOWN_CGI_PKG_SOURCE)
-	@touch $@
+$(pkg)-cgi-precompiled:
 
-spindown-cgi: $(PACKAGES_DIR)/.spindown-cgi-$(SPINDOWN_CGI_VERSION)
+$(pkg)-cgi-clean:
+	$(RM) $(PACKAGES_BUILD_DIR)/$(SPINDOWN_CGI_PKG_SOURCE)
 
-spindown-cgi-package: $(PACKAGES_DIR)/.spindown-cgi-$(SPINDOWN_CGI_VERSION)
-	tar -C $(PACKAGES_DIR) $(VERBOSE) --exclude .svn -cjf $(PACKAGES_BUILD_DIR)/$(SPINDOWN_CGI_PKG_SOURCE) spindown-cgi-$(SPINDOWN_CGI_VERSION)
-
-spindown-cgi-precompiled: spindown-cgi
-
-spindown-cgi-source: $(PACKAGES_DIR)/.spindown-cgi-$(SPINDOWN_CGI_VERSION)
-
-spindown-cgi-clean:
-	rm -f $(PACKAGES_BUILD_DIR)/$(SPINDOWN_CGI_PKG_SOURCE)
-
-spindown-cgi-dirclean:
-	rm -rf $(PACKAGES_DIR)/spindown-cgi-$(SPINDOWN_CGI_VERSION)
-	rm -f $(PACKAGES_DIR)/.spindown-cgi-$(SPINDOWN_CGI_VERSION)
-
-# Nothing to do here at the moment
-spindown-cgi-uninstall:
-
-spindown-cgi-list:
-ifeq ($(strip $(DS_PACKAGE_SPINDOWN_CGI)),y)
-	@echo "S20spindown-cgi-$(SPINDOWN_CGI_VERSION)" >> .static
-else
-	@echo "S20spindown-cgi-$(SPINDOWN_CGI_VERSION)" >> .dynamic
-endif
+$(PKG_FINISH)
