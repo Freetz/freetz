0df60d9edcea0533dabd1118c8d92b3ad3c0014f
Whoopie <whoopie79@gmx.net>
2008-02-11 15:22:43 +0000

    quagga: new macro style and files directory
    git-svn-id: http://svn.freetz.org/trunk@1807 f5190166-0702-4917-9039-51ec32eddaf5

--- make/quagga/quagga.mk
+++ make/quagga/quagga.mk
@@ -1,34 +1,37 @@
-QUAGGA_VERSION:=0.99.6
-QUAGGA_SOURCE:=quagga-$(QUAGGA_VERSION).tar.gz
-QUAGGA_SITE:=http://www.quagga.net/download
-QUAGGA_DIR:=$(SOURCE_DIR)/quagga-$(QUAGGA_VERSION)
-QUAGGA_MAKE_DIR:=$(MAKE_DIR)/quagga
-QUAGGA_BINARY:=$(QUAGGA_DIR)/zebra/.libs/zebra
-QUAGGA_PKG_VERSION:=0.1
-QUAGGA_PKG_SITE:=http://www.heimpold.de/dsmod
-QUAGGA_PKG_NAME:=quagga-$(QUAGGA_VERSION)
-QUAGGA_PKG_SOURCE:=quagga-$(QUAGGA_VERSION)-dsmod-$(QUAGGA_PKG_VERSION).tar.bz2
-QUAGGA_TARGET_DIR:=$(PACKAGES_DIR)/$(QUAGGA_PKG_NAME)/root
-QUAGGA_TARGET_BINARY:=$(QUAGGA_TARGET_DIR)/usr/sbin/zebra
-QUAGGA_TARGET_LIBDIR:=$(QUAGGA_TARGET_DIR)/usr/lib
+$(call PKG_INIT_BIN, 0.99.6)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=http://www.quagga.net/download
+$(PKG)_BINARY:=$($(PKG)_DIR)/zebra/.libs/zebra
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/zebra
+$(PKG)_TARGET_LIBDIR:=$($(PKG)_DEST_DIR)/usr/lib
+$(PKG)_STARTLEVEL=80
 
-QUAGGA_CONFIGURE_OPTIONS=\
-  --enable-user=root \
-  --enable-group=root \
-  --enable-shared \
-  --disable-static \
-  --enable-multipath=8 \
-  --enable-vtysh \
-  --with-gnu-ld \
-  --disable-capabilities \
-  $(if $(DS_PACKAGE_QUAGGA_BGPD),--enable-bgpd,--disable-bgpd) \
-  $(if $(DS_PACKAGE_QUAGGA_RIPD),--enable-ripd,--disable-ripd) \
-  $(if $(DS_PACKAGE_QUAGGA_RIPNGD),--enable-ripngd,--disable-ripngd) \
-  $(if $(DS_PACKAGE_QUAGGA_OSPFD),--enable-ospfd,--disable-ospfd) \
-  $(if $(DS_PACKAGE_QUAGGA_OSPF6D),--enable-ospf6d,--disable-ospf6d) \
-  $(if $(DS_PACKAGE_QUAGGA_ISISD),--enable-isisd,--disable-isisd)
+$(PKG)_DEPENDS_ON := ncurses readline
 
-QUAGGA_DAEMONS:=zebra \
+$(PKG)_CONFIG_SUBOPTS += DS_PACKAGE_QUAGGA_BGPD
+$(PKG)_CONFIG_SUBOPTS += DS_PACKAGE_QUAGGA_RIPD
+$(PKG)_CONFIG_SUBOPTS += DS_PACKAGE_QUAGGA_RIPNGD
+$(PKG)_CONFIG_SUBOPTS += DS_PACKAGE_QUAGGA_OSPFD
+$(PKG)_CONFIG_SUBOPTS += DS_PACKAGE_QUAGGA_OSPF6D
+$(PKG)_CONFIG_SUBOPTS += DS_PACKAGE_QUAGGA_ISISD
+
+$(PKG)_CONFIGURE_OPTIONS += --sysconfdir=/etc/quagga
+$(PKG)_CONFIGURE_OPTIONS += --localstatedir=/var/run/quagga
+$(PKG)_CONFIGURE_OPTIONS += --enable-user=root
+$(PKG)_CONFIGURE_OPTIONS += --enable-group=root
+$(PKG)_CONFIGURE_OPTIONS += --enable-shared
+$(PKG)_CONFIGURE_OPTIONS += --disable-static
+$(PKG)_CONFIGURE_OPTIONS += --enable-multipath=8
+$(PKG)_CONFIGURE_OPTIONS += --enable-vtysh
+$(PKG)_CONFIGURE_OPTIONS += --disable-capabilities
+$(PKG)_CONFIGURE_OPTIONS += $(if $(DS_PACKAGE_QUAGGA_BGPD),--enable-bgpd,--disable-bgpd)
+$(PKG)_CONFIGURE_OPTIONS += $(if $(DS_PACKAGE_QUAGGA_RIPD),--enable-ripd,--disable-ripd)
+$(PKG)_CONFIGURE_OPTIONS += $(if $(DS_PACKAGE_QUAGGA_RIPNGD),--enable-ripngd,--disable-ripngd)
+$(PKG)_CONFIGURE_OPTIONS += $(if $(DS_PACKAGE_QUAGGA_OSPFD),--enable-ospfd,--disable-ospfd)
+$(PKG)_CONFIGURE_OPTIONS += $(if $(DS_PACKAGE_QUAGGA_OSPF6D),--enable-ospf6d,--disable-ospf6d)
+$(PKG)_CONFIGURE_OPTIONS += $(if $(DS_PACKAGE_QUAGGA_ISISD),--enable-isisd,--disable-isisd)
+
+$(PKG)_DAEMONS:=zebra \
 		$(if $(DS_PACKAGE_QUAGGA_BGPD),bgpd,) \
 		$(if $(DS_PACKAGE_QUAGGA_RIPD),ripd,) \
 		$(if $(DS_PACKAGE_QUAGGA_RIPNGD),ripngd,) \
@@ -36,83 +39,28 @@ QUAGGA_DAEMONS:=zebra \
 	        $(if $(DS_PACKAGE_QUAGGA_OSPF6D),ospf6d,) \
 		$(if $(DS_PACKAGE_QUAGGA_ISISD),isisd,)
 
-QUAGGA_DS_CONFIG_FILE:=$(QUAGGA_MAKE_DIR)/.ds_config
-QUAGGA_DS_CONFIG_TEMP:=$(QUAGGA_MAKE_DIR)/.ds_config.temp
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
 
-$(DL_DIR)/$(QUAGGA_SOURCE): | $(DL_DIR)
-	wget -P $(DL_DIR) $(QUAGGA_SITE)/$(QUAGGA_SOURCE)
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+		LD="$(TARGET_LD)" \
+		$(MAKE) -C $(QUAGGA_DIR)
 
-$(DL_DIR)/$(QUAGGA_PKG_SOURCE): | $(DL_DIR)
-	@$(DL_TOOL) $(DL_DIR) $(TOPDIR)/.config $(QUAGGA_PKG_SOURCE) $(QUAGGA_PKG_SITE)
-
-$(QUAGGA_DS_CONFIG_FILE): $(TOPDIR)/.config
-	@echo "DS_PACKAGE_QUAGGA_BGPD=$(if $(DS_PACKAGE_QUAGGA_BGPD),y,n)" > $(QUAGGA_DS_CONFIG_TEMP)
-	@echo "DS_PACKAGE_QUAGGA_RIPD=$(if $(DS_PACKAGE_QUAGGA_RIPD),y,n)" >> $(QUAGGA_DS_CONFIG_TEMP)
-	@echo "DS_PACKAGE_QUAGGA_RIPNGD=$(if $(DS_PACKAGE_QUAGGA_RIPNGD),y,n)" >> $(QUAGGA_DS_CONFIG_TEMP)
-	@echo "DS_PACKAGE_QUAGGA_OSPFD=$(if $(DS_PACKAGE_QUAGGA_OSPFD),y,n)" >> $(QUAGGA_DS_CONFIG_TEMP)
-	@echo "DS_PACKAGE_QUAGGA_OSPF6D=$(if $(DS_PACKAGE_QUAGGA_OSPF6D),y,n)" >> $(QUAGGA_DS_CONFIG_TEMP)
-	@echo "DS_PACKAGE_QUAGGA_ISISD=$(if $(DS_PACKAGE_QUAGGA_ISISD),y,n)" >> $(QUAGGA_DS_CONFIG_TEMP)
-	@diff -q $(QUAGGA_DS_CONFIG_TEMP) $(QUAGGA_DS_CONFIG_FILE) || \
-	    cp $(QUAGGA_DS_CONFIG_TEMP) $(QUAGGA_DS_CONFIG_FILE)
-	@rm -f $(QUAGGA_DS_CONFIG_TEMP)
-
-# Make sure that a perfectly clean build is performed whenever DS-Mod package
-# options have changed. The safest way to achieve this is by starting over
-# with the source directory.
-$(QUAGGA_DIR)/.unpacked: $(DL_DIR)/$(QUAGGA_SOURCE) $(QUAGGA_DS_CONFIG_FILE)
-	rm -rf $(QUAGGA_DIR)
-	tar -C $(SOURCE_DIR) $(VERBOSE) -xzf $(DL_DIR)/$(QUAGGA_SOURCE)
-	for i in $(QUAGGA_MAKE_DIR)/patches/*.patch; do \
-	    $(PATCH_TOOL) $(QUAGGA_DIR) $$i; \
-	done
-	touch $@
-
-$(QUAGGA_DIR)/.configured: $(QUAGGA_DIR)/.unpacked
-	( cd $(QUAGGA_DIR); rm -f config.{cache,status}; \
-		$(TARGET_CONFIGURE_OPTS) \
-		CFLAGS="$(TARGET_CFLAGS)" \
-		CPPFLAGS="-I$(TARGET_MAKE_PATH)/../usr/include" \
-		LDFLAGS="-L$(TARGET_MAKE_PATH)/../usr/lib" \
-		./configure \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--program-prefix="" \
-		--program-suffix="" \
-		--prefix=/usr \
-		--exec-prefix=/usr \
-		--bindir=/usr/bin \
-		--sbindir=/usr/sbin \
-		--libexecdir=/usr/lib \
-		--datadir=/usr/share \
-		--sysconfdir=/etc/quagga \
-		--localstatedir=/var/run/quagga \
-		--libdir=/usr/lib \
-		--includedir=/usr/include \
-		--infodir=/usr/share/info \
-		--mandir=/usr/share/man \
-		$(DISABLE_LARGEFILE) \
-		$(DISABLE_NLS) \
-		$(QUAGGA_CONFIGURE_OPTIONS) \
-	);
-	touch $@
-
-$(QUAGGA_BINARY): $(QUAGGA_DIR)/.configured
-	PATH="$(TARGET_PATH)" LD="$(TARGET_LD)" $(MAKE) -C $(QUAGGA_DIR)
-
-$(QUAGGA_TARGET_BINARY): $(QUAGGA_BINARY)
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	# copy, strip and chmod base libs
 	cp -d $(QUAGGA_DIR)/lib/.libs/lib*.so* $(QUAGGA_TARGET_LIBDIR)
 	$(TARGET_STRIP) $(QUAGGA_TARGET_LIBDIR)/lib*.so*
 	chmod -x $(QUAGGA_TARGET_LIBDIR)/lib*.so*
 	# vtysh
-	cp $(QUAGGA_DIR)/vtysh/.libs/vtysh $(QUAGGA_TARGET_DIR)/usr/bin
-	$(TARGET_STRIP) $(QUAGGA_TARGET_DIR)/usr/bin/vtysh
+	cp $(QUAGGA_DIR)/vtysh/.libs/vtysh $(QUAGGA_DEST_DIR)/usr/bin
+	$(TARGET_STRIP) $(QUAGGA_DEST_DIR)/usr/bin/vtysh
 	# install routing routing daemons and libs
 	for d in $(QUAGGA_DAEMONS); do \
 		if [ -f $(QUAGGA_DIR)/$$d/.libs/$$d ]; then \
-			cp $(QUAGGA_DIR)/$$d/.libs/$$d $(QUAGGA_TARGET_DIR)/usr/sbin; \
-			$(TARGET_STRIP) $(QUAGGA_TARGET_DIR)/usr/sbin/$$d; \
+			cp $(QUAGGA_DIR)/$$d/.libs/$$d $(QUAGGA_DEST_DIR)/usr/sbin; \
+			$(TARGET_STRIP) $(QUAGGA_DEST_DIR)/usr/sbin/$$d; \
 		fi; \
 		(shopt -s nullglob; for f in $(QUAGGA_DIR)/$$d/.libs/lib*.so*; do \
 			cp -d $$f $(QUAGGA_TARGET_LIBDIR); \
@@ -121,39 +69,17 @@ $(QUAGGA_TARGET_BINARY): $(QUAGGA_BINARY)
 	# install supervisor daemon
 	$(INSTALL_BINARY_STRIP)
 
-quagga-uninstall:
-	rm -f $(QUAGGA_TARGET_LIBDIR)/lib*.so*	
-	rm -f $(QUAGGA_TARGET_DIR)/usr/sbin/*
-	rm -f $(QUAGGA_TARGET_DIR)/usr/bin/vtysh
+$(pkg):
 
-$(PACKAGES_DIR)/.$(QUAGGA_PKG_NAME): $(DL_DIR)/$(QUAGGA_PKG_SOURCE) | $(PACKAGES_DIR)
-	@tar -C $(PACKAGES_DIR) -xjf $(DL_DIR)/$(QUAGGA_PKG_SOURCE)
-	@touch $@
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
-quagga: $(PACKAGES_DIR)/.$(QUAGGA_PKG_NAME)
-
-quagga-package: $(PACKAGES_DIR)/.$(QUAGGA_PKG_NAME)
-	tar -C $(PACKAGES_DIR) $(VERBOSE) --exclude .svn -cjf $(PACKAGES_BUILD_DIR)/$(QUAGGA_PKG_SOURCE) $(QUAGGA_PKG_NAME)
-
-quagga-precompiled: uclibc ncurses-precompiled readline-precompiled quagga $(QUAGGA_TARGET_BINARY)
-
-quagga-source: $(QUAGGA_DIR)/.unpacked $(PACKAGES_DIR)/.$(QUAGGA_PKG_NAME)
-
-quagga-clean:
+$(pkg)-clean:
 	-$(MAKE) -C $(QUAGGA_DIR) clean
-	rm -f $(PACKAGES_BUILD_DIR)/$(QUAGGA_PKG_SOURCE)
-	rm -f $(QUAGGA_DIR)/.configured
-	rm -f $(QUAGGA_DS_CONFIG_FILE)
+	$(RM) $(QUAGGA_DS_CONFIG_FILE)
 
-quagga-dirclean:
-	rm -rf $(QUAGGA_DIR)
-	rm -rf $(PACKAGES_DIR)/$(QUAGGA_PKG_NAME)
-	rm -f $(PACKAGES_DIR)/.$(QUAGGA_PKG_NAME)
-	rm -f $(QUAGGA_DS_CONFIG_FILE)
+$(pkg)-uninstall:
+	$(RM) $(QUAGGA_TARGET_LIBDIR)/lib*.so*	
+	$(RM) $(QUAGGA_DEST_DIR)/usr/sbin/*
+	$(RM) $(QUAGGA_DEST_DIR)/usr/bin/vtysh
 
-quagga-list:
-ifeq ($(strip $(DS_PACKAGE_QUAGGA)),y)
-	@echo "S80quagga-$(QUAGGA_VERSION)" >> .static
-else
-	@echo "S80quagga-$(QUAGGA_VERSION)" >> .dynamic
-endif
+$(PKG_FINISH)
