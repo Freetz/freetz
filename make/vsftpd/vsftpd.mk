d3518c533f5eaa463677e41849718d7f812dc0d8
Whoopie <whoopie79@gmx.net>
2008-02-11 11:37:16 +0000

    vsftpd: new macro style and files directory
    git-svn-id: http://svn.freetz.org/trunk@1799 f5190166-0702-4917-9039-51ec32eddaf5

--- make/vsftpd/vsftpd.mk
+++ make/vsftpd/vsftpd.mk
@@ -1,67 +1,31 @@
-VSFTPD_VERSION:=2.0.5
-VSFTPD_SOURCE:=vsftpd-$(VSFTPD_VERSION).tar.gz
-VSFTPD_SITE:=ftp://vsftpd.beasts.org/users/cevans/
-VSFTPD_MAKE_DIR:=$(MAKE_DIR)/vsftpd
-VSFTPD_DIR:=$(SOURCE_DIR)/vsftpd-$(VSFTPD_VERSION)
-VSFTPD_BINARY:=$(VSFTPD_DIR)/vsftpd
-VSFTPD_PKG_VERSION:=0.1d
-VSFTPD_PKG_SITE:=http://131.246.137.121/~metz/dsmod/packages
+$(call PKG_INIT_BIN, 2.0.5)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=ftp://vsftpd.beasts.org/users/cevans/
+$(PKG)_BINARY:=$($(PKG)_DIR)/vsftpd
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/vsftpd
+$(PKG)_STARTLEVEL=40
 
-VSFTPD_PKG_NAME:=vsftpd-$(VSFTPD_VERSION)
-VSFTPD_PKG_SOURCE:=vsftpd-$(VSFTPD_VERSION)-dsmod-$(VSFTPD_PKG_VERSION).tar.bz2
-VSFTPD_TARGET_DIR:=$(PACKAGES_DIR)/$(VSFTPD_PKG_NAME)
-VSFTPD_TARGET_BINARY:=$(VSFTPD_TARGET_DIR)/root/usr/sbin/vsftpd
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
 
-$(DL_DIR)/$(VSFTPD_SOURCE): | $(DL_DIR)
-	wget -P $(DL_DIR) $(VSFTPD_SITE)/$(VSFTPD_SOURCE)
-
-$(DL_DIR)/$(VSFTPD_PKG_SOURCE): | $(DL_DIR)
-	@$(DL_TOOL) $(DL_DIR) $(TOPDIR)/.config $(VSFTPD_PKG_SOURCE) $(VSFTPD_PKG_SITE)
-
-$(VSFTPD_DIR)/.unpacked: $(DL_DIR)/$(VSFTPD_SOURCE)
-	tar -C $(SOURCE_DIR) $(VERBOSE) -xzf $(DL_DIR)/$(VSFTPD_SOURCE)
-	for i in $(VSFTPD_MAKE_DIR)/patches/*.patch; do \
-		$(PATCH_TOOL) $(VSFTPD_DIR) $$i; \
-	done
-	touch $@
-
-$(VSFTPD_BINARY): $(VSFTPD_DIR)/.unpacked
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	PATH="$(TARGET_PATH)" \
 		$(MAKE) -C $(VSFTPD_DIR) \
 		CC="$(TARGET_CC)" \
 		CFLAGS="$(TARGET_CFLAGS)"
 
-$(PACKAGES_DIR)/.$(VSFTPD_PKG_NAME): $(DL_DIR)/$(VSFTPD_PKG_SOURCE) | $(PACKAGES_DIR)
-	@tar -C $(PACKAGES_DIR) -xjf $(DL_DIR)/$(VSFTPD_PKG_SOURCE)
-	@touch $@
-
-$(VSFTPD_TARGET_BINARY): $(VSFTPD_BINARY)
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
 
-vsftpd: $(PACKAGES_DIR)/.$(VSFTPD_PKG_NAME)
+$(pkg):
 
-vsftpd-package: $(PACKAGES_DIR)/.$(VSFTPD_PKG_NAME)
-	tar -C $(PACKAGES_DIR) $(VERBOSE) --exclude .svn -cjf $(PACKAGES_BUILD_DIR)/$(VSFTPD_PKG_SOURCE) $(VSFTPD_PKG_NAME) 
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
-vsftpd-precompiled: uclibc vsftpd $(VSFTPD_TARGET_BINARY)
-
-vsftpd-source: $(VSFTPD_DIR)/.unpacked $(PACKAGES_DIR)/.$(VSFTPD_PKG_NAME)
-
-vsftpd-clean:
+$(pkg)-clean:
 	-$(MAKE) -C $(VSFTPD_DIR) clean
-	rm -f $(PACKAGES_BUILD_DIR)/$(VSFTPD_PKG_SOURCE)
 
-vsftpd-dirclean:
-	rm -rf $(VSFTPD_DIR)
-	rm -rf $(PACKAGES_DIR)/$(VSFTPD_PKG_NAME)
-	rm -f $(PACKAGES_DIR)/.$(VSFTPD_PKG_NAME)
+$(pkg)-uninstall:
+	$(RM) $(VSFTPD_TARGET_BINARY)
 
-vsftpd-uninstall:
-	rm -f $(VSFTPD_TARGET_BINARY)
-
-vsftpd-list:
-ifeq ($(strip $(DS_PACKAGE_VSFTPD)),y)
-	@echo "S40vsftpd-$(VSFTPD_VERSION)" >> .static
-else
-	@echo "S40vsftpd-$(VSFTPD_VERSION)" >> .dynamic
-endif
+$(PKG_FINISH)
