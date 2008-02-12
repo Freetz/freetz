2535ebec45e13236b2c8961be31d265d182e567d
Whoopie <whoopie79@gmx.net>
2008-02-12 00:05:46 +0000

    samba: small cleanup
    git-svn-id: http://svn.freetz.org/trunk@1822 f5190166-0702-4917-9039-51ec32eddaf5

--- make/samba/samba.mk
+++ make/samba/samba.mk
@@ -1,15 +1,15 @@
 $(call PKG_INIT_BIN, 3.0.24)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=ftp://de3.$(pkg).org/pub/$(pkg)/old-versions
-$(PKG)_SMBD_BINARY:=$($(PKG)_DIR)/source/bin/smbd
-$(PKG)_TARGET_SMBD_BINARY:=$($(PKG)_DEST_DIR)/sbin/smbd
+$(PKG)_BINARY:=$($(PKG)_DIR)/source/bin/smbd
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/sbin/smbd
 $(PKG)_STARTLEVEL=40
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_NOP)
 
-$($(PKG)_SMBD_BINARY): $($(PKG)_DIR)/.configured
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	PATH="$(TARGET_PATH)" \
 		$(MAKE) -C $(SAMBA_DIR)/source \
 		CC=$(TARGET_CC) \
@@ -18,19 +18,18 @@ $($(PKG)_SMBD_BINARY): $($(PKG)_DIR)/.configured
 		CODEPAGEDIR="/mod/usr/share/samba" \
 		proto all
 
-$($(PKG)_TARGET_SMBD_BINARY): $($(PKG)_SMBD_BINARY)
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
 
-samba:
+$(pkg):
 
-samba-precompiled: $($(PKG)_TARGET_SMBD_BINARY)
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
-samba-clean:
-	-$(MAKE) -C $($(PKG)_DIR)/source clean
-	rm -rf $($(PKG)_DIR)/source/bin
-	rm -f $(PACKAGES_BUILD_DIR)/$($(PKG)_PKG_SOURCE)
+$(pkg)-clean:
+	-$(MAKE) -C $(SAMBA_DIR)/source clean
+	$(RM) -r $(SAMBA_DIR)/source/bin
 
-samba-uninstall:
-	rm -f $($(PKG)_TARGET_SMBD_BINARY)
+$(pkg)-uninstall:
+	$(RM) $(SAMBA_TARGET_BINARY)
 
 $(PKG_FINISH)
