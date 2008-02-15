0c1246cff26f96768f2883821722ef6fcd9cc550
Whoopie <whoopie79@gmx.net>
2008-02-15 10:24:14 +0000

    pppd, pptpd, pptp: first round for pptpd and pptp integration, work in progress! dropbear: fix comment in limits patch
    git-svn-id: http://svn.freetz.org/trunk@1858 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/pptp/pptp.mk
@@ -0,0 +1,30 @@
+$(call PKG_INIT_BIN,1.7.1)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=http://mesh.dl.sourceforge.net/sourceforge/pptpclient
+$(PKG)_BINARY:=$($(PKG)_DIR)/pptp
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/pptp
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+		$(MAKE) -C $(PPTP_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(PPTP_DIR) clean
+
+$(pkg)-uninstall:
+	$(RM) $(PPTP_TARGET_BINARY)
+
+$(PKG_FINISH)
