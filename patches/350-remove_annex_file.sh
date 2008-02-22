d055ab8a19179af9ee7668f2ad0240a9712d7a80
Oliver Metz <oliver@freetz.org>
2008-02-22 21:20:16 +0000

    * 7270: Add patch to remove Annex firmware files
    git-svn-id: http://svn.freetz.org/trunk@1903 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ patches/350-remove_annex_file.sh
@@ -0,0 +1,18 @@
+rm_files()
+{
+	for file in $1; do
+		echo2 "$file"
+		rm -rf "$file"
+	done
+}
+
+if [ "$DS_REMOVE_ANNEX_A_FIRMWARE" == "y" ]; then
+	echo1 "removing Annex A firmware file"
+	rm_files "${FILESYSTEM_MOD_DIR}/lib/modules/dsp_ur8/ur8-A-dsl.bin"
+fi
+
+if [ "$DS_REMOVE_ANNEX_B_FIRMWARE" == "y" ]; then
+	echo1 "removing Annex B firmware file"
+	rm_files "${FILESYSTEM_MOD_DIR}/lib/modules/dsp_ur8/ur8-B-dsl.bin"
+fi
+
