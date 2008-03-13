9bc1516134313337df43abf2cb8c42efa6f33fed
Lars Falkenhain <lars.falkenhain@gmail.com>
2008-03-13 17:24:16 +0000

    * vim added (ported from OpenWRT)
    git-svn-id: http://svn.freetz.org/trunk@2006 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/vim/vim.mk
@@ -0,0 +1,42 @@
+$(call PKG_INIT_BIN, 7.1)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SITE:=ftp://ftp.vim.org/pub/vim/unix
+$(PKG)_DIR:=$(SOURCE_DIR)/$(pkg)71
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/$(pkg)
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/$(pkg)
+#$(PKG)_STARTLEVEL=99
+$(PKG)_DEPENDS_ON=ncurses
+
+$(PKG)_CONFIGURE_OPTIONS += --disable-gui
+$(PKG)_CONFIGURE_OPTIONS += --disable-gtktest
+$(PKG)_CONFIGURE_OPTIONS += --disable-xim
+$(PKG)_CONFIGURE_OPTIONS += --with-features=tiny
+$(PKG)_CONFIGURE_OPTIONS += --without-x
+$(PKG)_CONFIGURE_OPTIONS += --disable-multibyte
+$(PKG)_CONFIGURE_OPTIONS += --disable-netbeans
+$(PKG)_CONFIGURE_OPTIONS += --disable-gpm
+$(PKG)_CONFIGURE_OPTIONS += --with-tlib=ncurses
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+		$(MAKE) $(VIM_MAKE_OPTIONS) -C $(VIM_DIR)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(VIM_DIR) clean
+	$(RM) $(VIM_FREETZ_CONFIG_FILE)
+
+$(pkg)-uninstall:
+	$(RM) $(VIM_TARGET_BINARY)
+
+$(PKG_FINISH)
