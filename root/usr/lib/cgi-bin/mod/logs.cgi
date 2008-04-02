Freetz/freetz

commit 1fb18120608dd905d613ede26f3e9cfb467b24b9

* Add checkbox to enable statistics for mounted partitions  * Add textfield to enter cgi_width (default: 730)
git-svn-id: http://svn.freetz.org/trunk@2066 f5190166-0702-4917-9039-51ec32eddaf5

root/usr/lib/cgi-bin/mod/logs.cgi
@@ -1,0 +1,35 @@
+#!/bin/sh
+
+PATH=/bin:/usr/bin:/sbin:/usr/sbin
+. /usr/lib/libmodcgi.sh
+
+let _width=$_cgi_width-230
+
+if [ -r /var/log/mod_load.log ]; then
+	echo '<h1>/var/log/mod_load.log</h1>'
+	echo -n '<pre style="width: '$_width'px; overflow: auto;">'
+	httpd -e "$(cat /var/log/mod_load.log)"
+	echo '</pre>'
+fi
+
+if [ -r /var/log/mod_net.log ]; then
+	echo '<h1>/var/log/mod_net.log</h1>'
+	echo -n '<pre style="width: '$_width'px; overflow: auto;">'
+	httpd -e "$(cat /var/log/mod_net.log)"
+	echo '</pre>'
+fi
+
+if [ -r /var/log/mod_voip.log ]; then
+	echo '<h1>/var/log/mod_voip.log</h1>'
+	echo -n '<pre style="width: '$_width'px; overflow: auto;">'
+	httpd -e "$(cat /var/log/mod_voip.log)"
+	echo '</pre>'
+fi
+
+if [ -r /var/log/mod.log ]; then
+	echo '<h1>/var/log/mod.log</h1>'
+	echo -n '<pre style="width: '$_width'px; overflow: auto;">'
+	httpd -e "$(cat /var/log/mod.log)"
+	echo '</pre>'
+fi
+

root/usr/lib/cgi-bin/mod/mounted.cgi
@@ -1,0 +1,32 @@
+#!/bin/sh
+
+PATH=/bin:/usr/bin:/sbin:/usr/bin
+. /usr/lib/libmodcgi.sh
+
+stat_bar() {
+	let multip="($_cgi_width-230-50)/100";
+	percent=$1; let bar="percent*multip"; let grey="(100-percent)*multip"
+	echo '<p><img src="/images/green.png" width="'"$bar"'" height="10" border="0" alt=""><img src="/images/grey.png" width="'"$grey"'" height="10" border="0" alt=""> &nbsp;&nbsp;'$percent' %</p>'
+}
+
+sec_begin '$(lang de:"Eingeh&auml;ngte Partitionen" en:"Mounted partitions")'
+if [ "$(mount|grep "/dev/sd")" -o "$(mount|grep "/dev/mapper")" ]; then
+	for dummy in `mount|grep "/dev/mapper"|cut -d " " -f 3`; do
+		PARTITIONS="$PARTITIONS $dummy"
+	done
+	for dummy in `mount|grep "/dev/sd"|cut -d " " -f 3`;do
+		PARTITIONS="$PARTITIONS $dummy"
+	done
+	for dummy in $PARTITIONS;do
+		dfrow=$(df -h| grep $dummy)
+		total="$(  echo $dfrow | awk '{print $2}' |sed s/k/" K"/g |sed s/M/" M"/g |sed s/G/" G"/g )"
+		used="$(   echo $dfrow | awk '{print $3}' |sed s/k/" K"/g |sed s/M/" M"/g |sed s/G/" G"/g )"
+		percent="$(echo $dfrow | awk '{print $5}' |sed s/[^0-9]//g )"
+		echo "<p><b>$dummy</b> ($(mount|grep $dummy|cut -d " " -f 1)):<br>"$used"B $(lang de:"von" en:"of") "$total"B $(lang de:"belegt" en:"used")</p>"
+		stat_bar $percent
+	done
+else
+	echo "<br>$(lang de:"keine gefunden" en:"none found")<br>"
+fi
+sec_end
+

root/usr/mww/cgi-bin/pkgstatus.cgi
@@ -6,12 +6,15 @@ PATH=/bin:/usr/bin:/sbin:/usr/sbin
 package="$(echo "$QUERY_STRING" | sed -e 's/^.*pkg=//' -e 's/&.*$//' -e 's/\.//g')"
 cgi="$(echo "$QUERY_STRING" | sed -e 's/^.*cgi=//' -e 's/&.*$//' -e 's/\.//g')"
 
+#if [ -r "/mod/etc/default.$package/$package.cfg" ]; then
+#	. /mod/etc/conf/$package.cfg
+#fi
 
 cgi_begin "$package" "status_$(echo $cgi | sed -e "s/\//__/")"
 
 
 if [ -x "/mod/usr/lib/cgi-bin/$cgi.cgi" ]; then
-	/mod/usr/lib/cgi-bin/$cgi.cgi
+	. /mod/usr/lib/cgi-bin/$cgi.cgi
 else
 	echo "<p><b>$(lang de:"Fehler" en:"Error"):</b> $(lang de:"Kein Skript f&uuml;r das die Statusanzeige" en:"no script for status display") '$package/$cgi'.</p>"
 fi

root/usr/mww/cgi-bin/status.cgi
@@ -7,8 +7,10 @@ get_env() {
 	cat /proc/sys/urlader/environment | grep "^$1" | sed -e 's/'"$1"'	//'
 }
 
+
 stat_bar() {
-	percent=$1; let bar="percent*4"; let grey="(100-percent)*4"
+	let multip="($_cgi_width-230-50)/100";
+	percent=$1; let bar="percent*multip"; let grey="(100-percent)*multip"
 	echo '<p><img src="/images/green.png" width="'"$bar"'" height="10" border="0" alt=""><img src="/images/grey.png" width="'"$grey"'" height="10" border="0" alt=""> &nbsp;&nbsp;'$percent' %</p>'
 }
 
@@ -121,6 +123,14 @@ stat_bar $percent
 sec_end
 fi
 
+if [ "$(grep MOD_MOUNTED_MAIN /mod/etc/conf/mod.cfg |cut -d"'" -f2)" = yes ];then
+	/usr/lib/cgi-bin/mod/mounted.cgi
+fi
+
+#ODER BESSER:
+#[ -r "/mod/etc/conf/mod.cfg" ] && . /mod/etc/conf/mod.cfg
+#[ "$MOD_MOUNTED_MAIN" = yes ] && /usr/lib/cgi-bin/mod/mounted.cgi
+# ??
 stat_button 'restart_dsld' '$(lang de:"DSL-Reconnect" en:"Reconnect DSL")'
 stat_button 'cleanup' '$(lang de:"TFFS aufräumen" en:"Clean up TFFS")'
 stat_button 'fw_attrib' '$(lang de:"Attribute bereinigen" en:"Clean up attributes")'

.language
@@ -29,4 +29,6 @@ files
 	usr/mww/cgi-bin/save.cgi
 	usr/mww/cgi-bin/settings.cgi
 	usr/mww/cgi-bin/status.cgi
+	usr/lib/cgi-bin/mod/mounted.cgi
+	etc/init.d/rc.mod
 }

root/etc/default.mod/mod.cfg
@@ -6,3 +6,6 @@ export MOD_LIMIT='61440'
 export MOD_SWAP='no'
 export MOD_SWAP_FILE=''
 export MOD_TELNETD='no'
+export MOD_CGI_WIDTH='730'
+export MOD_MOUNTED_MAIN='no'
+export MOD_MOUNTED_SUB='no'

root/etc/init.d/rc.mod
@@ -46,6 +46,10 @@ case "$1" in
 		[ -r /tmp/flash/modules.def ] && deffile='/tmp/flash/modules.def'
 		modreg file 'modules' 'modules' 0 "$deffile"
 
+		[ -r "/mod/etc/conf/mod.cfg" ] && . /mod/etc/conf/mod.cfg
+		modreg status mod '$(lang de:"Logdateien" en:"Logfiles")' mod/logs
+		[ "$MOD_MOUNTED_SUB" = yes ] && modreg status mod '$(lang de:"Partitionen" en:"Partitions")' mod/mounted
+
 		start
 		;;
 	start)

root/usr/lib/libmodcgi.sh
@@ -10,7 +10,6 @@ if [ "$1" = "status" ]; then
 			echo "<div id=\"status_$(echo $cgi | sed -e "s/\//__/")\" class=\"su\"><a href=\"/cgi-bin/pkgstatus.cgi?pkg=$pkg&cgi=$cgi\">$title</a></div>"
 		done
 	fi
-	echo "<div id=\"status_logs\" class=\"su\"><a href=\"/cgi-bin/logs.cgi\">Logs</a></div>"
 fi
 
 cat << EOF
@@ -78,7 +77,9 @@ EOF
 fi
 
 # set width of table
-if [ ! "$_cgi_width" ]; then let _cgi_width="730"; fi
+if [ ! "$_cgi_width" ]; then 
+	let _cgi_width="$(grep MOD_CGI_WIDTH /mod/etc/conf/mod.cfg |cut -d"'" -f2)"; 
+fi
 let _cgi_total_width="$_cgi_width+40"
 
 cat << EOF

commit 1651b69c67080c105cd56d934a1b37d1a77dfd18

* Fix some errors introduced in r2066   * +x for logs.cgi and mounted.cgi   * Add comment to rrdstats/Config.in (nice)   * Remove logs.cgi   * Remove comments from pkgstatus.cgi and status.cgi
git-svn-id: http://svn.freetz.org/trunk@2071 f5190166-0702-4917-9039-51ec32eddaf5

make/rrdstats/Config.in
@@ -4,4 +4,4 @@ config FREETZ_PACKAGE_RRDSTATS
 	default n
 	help
 		Lets you view graphically the stats of your Freetz Box.
-
+		Optional: Install BusyBox's "nice" to save spare resources.

root/usr/lib/cgi-bin/mod/logs.cgi
@@ -1,7 +1,3 @@
-#!/bin/sh
-
-PATH=/bin:/usr/bin:/sbin:/usr/sbin
-. /usr/lib/libmodcgi.sh
 
 let _width=$_cgi_width-230
 

root/usr/lib/cgi-bin/mod/mounted.cgi
@@ -1,7 +1,3 @@
-#!/bin/sh
-
-PATH=/bin:/usr/bin:/sbin:/usr/bin
-. /usr/lib/libmodcgi.sh
 
 stat_bar() {
 	let multip="($_cgi_width-230-50)/100";
@@ -10,19 +6,15 @@ stat_bar() {
 }
 
 sec_begin '$(lang de:"Eingeh&auml;ngte Partitionen" en:"Mounted partitions")'
-if [ "$(mount|grep "/dev/sd")" -o "$(mount|grep "/dev/mapper")" ]; then
-	for dummy in `mount|grep "/dev/mapper"|cut -d " " -f 3`; do
-		PARTITIONS="$PARTITIONS $dummy"
-	done
-	for dummy in `mount|grep "/dev/sd"|cut -d " " -f 3`;do
-		PARTITIONS="$PARTITIONS $dummy"
-	done
-	for dummy in $PARTITIONS;do
-		dfrow=$(df -h| grep $dummy)
+MPOINTS=$(mount|grep -E "^/dev/sd|^/dev/mapper/|^.* on .* type cifs"|cut -d" " -f3)
+if [ "$MPOINTS" ]; then
+	for dummy in $MPOINTS; do
+		dfrow=$(df -h|grep " $dummy$")
 		total="$(  echo $dfrow | awk '{print $2}' |sed s/k/" K"/g |sed s/M/" M"/g |sed s/G/" G"/g )"
 		used="$(   echo $dfrow | awk '{print $3}' |sed s/k/" K"/g |sed s/M/" M"/g |sed s/G/" G"/g )"
 		percent="$(echo $dfrow | awk '{print $5}' |sed s/[^0-9]//g )"
-		echo "<p><b>$dummy</b> ($(mount|grep $dummy|cut -d " " -f 1)):<br>"$used"B $(lang de:"von" en:"of") "$total"B $(lang de:"belegt" en:"used")</p>"
+		thename="$(echo $dfrow | awk '{print $1}' )"
+		echo "<p><b>$dummy</b> ($thename):<br>"$used"B $(lang de:"von" en:"of") "$total"B $(lang de:"belegt" en:"used")</p>"
 		stat_bar $percent
 	done
 else

root/usr/mww/cgi-bin/logs.cgi
@@ -1,37 +1,0 @@
-#!/bin/sh
-
-PATH=/bin:/usr/bin:/sbin:/usr/sbin
-. /usr/lib/libmodcgi.sh
-
-let _width=$_cgi_width-230
-cgi_begin 'Logs' 'status_logs'
-
-if [ -r /var/log/mod_load.log ]; then
-	echo '<h1>/var/log/mod_load.log</h1>'
-	echo -n '<pre style="width: '$_width'px; overflow: auto;">'
-	httpd -e "$(cat /var/log/mod_load.log)"
-	echo '</pre>'
-fi
-
-if [ -r /var/log/mod_net.log ]; then
-	echo '<h1>/var/log/mod_net.log</h1>'
-	echo -n '<pre style="width: '$_width'px; overflow: auto;">'
-	httpd -e "$(cat /var/log/mod_net.log)"
-	echo '</pre>'
-fi
-
-if [ -r /var/log/mod_voip.log ]; then
-	echo '<h1>/var/log/mod_voip.log</h1>'
-	echo -n '<pre style="width: '$_width'px; overflow: auto;">'
-	httpd -e "$(cat /var/log/mod_voip.log)"
-	echo '</pre>'
-fi
-
-if [ -r /var/log/mod.log ]; then
-	echo '<h1>/var/log/mod.log</h1>'
-	echo -n '<pre style="width: '$_width'px; overflow: auto;">'
-	httpd -e "$(cat /var/log/mod.log)"
-	echo '</pre>'
-fi
-
-cgi_end

root/usr/mww/cgi-bin/pkgstatus.cgi
@@ -6,9 +6,6 @@ PATH=/bin:/usr/bin:/sbin:/usr/sbin
 package="$(echo "$QUERY_STRING" | sed -e 's/^.*pkg=//' -e 's/&.*$//' -e 's/\.//g')"
 cgi="$(echo "$QUERY_STRING" | sed -e 's/^.*cgi=//' -e 's/&.*$//' -e 's/\.//g')"
 
-#if [ -r "/mod/etc/default.$package/$package.cfg" ]; then
-#	. /mod/etc/conf/$package.cfg
-#fi
 
 cgi_begin "$package" "status_$(echo $cgi | sed -e "s/\//__/")"
 

root/usr/mww/cgi-bin/status.cgi
@@ -123,14 +123,10 @@ stat_bar $percent
 sec_end
 fi
 
-if [ "$(grep MOD_MOUNTED_MAIN /mod/etc/conf/mod.cfg |cut -d"'" -f2)" = yes ];then
-	/usr/lib/cgi-bin/mod/mounted.cgi
+if [ "$(grep MOD_MOUNTED_MAIN /mod/etc/conf/mod.cfg |cut -d"'" -f2)" = yes ]; then
+	. /usr/lib/cgi-bin/mod/mounted.cgi
 fi
 
-#ODER BESSER:
-#[ -r "/mod/etc/conf/mod.cfg" ] && . /mod/etc/conf/mod.cfg
-#[ "$MOD_MOUNTED_MAIN" = yes ] && /usr/lib/cgi-bin/mod/mounted.cgi
-# ??
 stat_button 'restart_dsld' '$(lang de:"DSL-Reconnect" en:"Reconnect DSL")'
 stat_button 'cleanup' '$(lang de:"TFFS aufräumen" en:"Clean up TFFS")'
 stat_button 'fw_attrib' '$(lang de:"Attribute bereinigen" en:"Clean up attributes")'

root/usr/share/style.css
@@ -10,7 +10,7 @@ h1, legend		{ margin-top: 5px; color: black; font: bold 14px sans-serif; }
 h2			{ margin-top: 10px; margin-bottom: 7px; padding: 0px; color: black; font: italic 14px sans-serif; }
 pre			{ font-size: 14px; border: 2px dotted #bbbbbb; padding: 10px; }
 table			{ empty-cells: show; }
-fieldset		{ margin: 0px; margin-top: 10px; margin-bottom: 10px; padding: 10px; width: 500px; }
+fieldset		{ margin: 0px; margin-top: 10px; margin-bottom: 10px; padding: 10px; margin-right:185px; }
 form.btn		{ margin: 0px; }
 div.btn			{ float: left; margin-top: 10px; padding-right: 3px; }
 div.menu		{ width: 150px; float: right; border: 2px dotted #bbbbbb; margin: 0px; margin-left: 20px; margin-top: 10px; margin-bottom: 10px; padding: 2px; padding-top: 0px; }
@@ -20,7 +20,7 @@ div.menu div a:visited,
 div.menu div a:hover	{ color: black; }
 div.menu div.su		{ margin-top: 0px; font: normal 12px sans-serif; }
 div.menu a		{ padding-left: 5px; padding-right: 5px; }
-div.body		{ width: 500px; }
+#div.body		{ width: 500px; }
 div.title		{ color: white; font: bold 18px Verdana, sans-serif; margin-left: 5px; }
 div.version		{ color: #bae3ff; font: italic bold 12px Verdana, sans-serif; margin-right: 5px; float: right; }
 div.opt			{ color: gray; font: normal 10px Verdana, sans-serif; margin-right: 5px; float: right; }