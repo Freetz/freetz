76a1189f5a8eff5cca5abc7e2b977af6b137121e
Oliver Metz <oliver@freetz.org>
2008-04-19 23:40:28 +0000

    * Bump 3170 firmware version to 49.04.53  * Config.in: Fix intendation, activate some remove patches for 3170
    git-svn-id: http://svn.freetz.org/trunk@2115 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/220-cdrom-iso.sh
+++ patches/220-cdrom-iso.sh
@@ -4,6 +4,8 @@ rm -f "${FILESYSTEM_MOD_DIR}/lib/modules/cdrom.iso"
 sed -i -e 's/cdrom_fallback=0/cdrom_fallback=1/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
 if [ "$FREETZ_TYPE_FON_5050" == "y" ]; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/rc.S-no-cdrom-fallback_5050.patch"
+elif [ "$FREETZ_TYPE_WLAN_3170" == "y" ]; then
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/rc.S-no-cdrom-fallback_3170.patch"
 elif [ "$FREETZ_TYPE_FON" == "y" ] || [ "$FREETZ_TYPE_300IP_AS_FON" == "y" ] \
 	|| [ "$FREETZ_TYPE_WLAN_3020" == "y" ] || [ "$FREETZ_TYPE_WLAN_3030" == "y" ] \
 	|| [ "$FREETZ_TYPE_FON_WLAN" == "y" ]; then
