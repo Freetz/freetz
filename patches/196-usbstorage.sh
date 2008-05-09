fdb3c506aabd057e07f2956f6bb8b867893e365d
Lars Falkenhain <lars.falkenhain@gmail.com>
2008-05-09 10:45:09 +0000

    * added support for "new" 7140 en (Annex A und B)
    git-svn-id: http://svn.freetz.org/trunk@2193 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/196-usbstorage.sh
+++ patches/196-usbstorage.sh
@@ -5,6 +5,8 @@ if [ "$FREETZ_TYPE_2170" == "y" -a "$FREETZ_TYPE_LABOR" != "y" ] || \
 	[ "$FREETZ_TYPE_FON_7150" == "y" ] || \
 	[ "$FREETZ_TYPE_WLAN_3130" == "y" ]; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage_wotam.patch"
+elif [ "$FREETZ_TYPE_FON_WLAN_7140" == "y" -a "$FREETZ_TYPE_LANG_EN" == "y" ]; then
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/en/usbstorage_7140.patch"
 elif [ "$FREETZ_TYPE_SPEEDPORT_W900V" == "y" ]; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage_w900v.patch"
 elif [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" ]; then
--- patches/280-storage.sh
+++ patches/280-storage.sh
@@ -4,6 +4,8 @@ if [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" ]; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/storage_7170.patch"
 elif [ "$FREETZ_TYPE_FON_WLAN_7270" == "y" ]; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/storage_7270.patch"
+elif [ "$FREETZ_TYPE_FON_WLAN_7140" == "y" -a "$FREETZ_TYPE_LANG_EN" == "y" ]; then
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/en/storage_7140.patch"
 elif [ "$FREETZ_TYPE_SPEEDPORT_W900V" != "y" ]; then
 		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/storage.patch"
 fi
