76059c4f4adb6cb67eaa0ad780eb144759e5b0b5
Oliver Metz <oliver@freetz.org>
2008-05-13 22:16:54 +0000

    * replace kernel with 04.49 fon wlan en works for me  * Fix remove cdrom patch for fon (wlan) en  * fon (wlan) 04.49 don't have websrv
    git-svn-id: http://svn.freetz.org/trunk@2208 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/220-cdrom-iso.sh
+++ patches/220-cdrom-iso.sh
@@ -6,10 +6,15 @@ if [ "$FREETZ_TYPE_FON_5050" == "y" ]; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/rc.S-no-cdrom-fallback_5050.patch"
 elif [ "$FREETZ_TYPE_WLAN_3170" == "y" ]; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/rc.S-no-cdrom-fallback_3170.patch"
-elif [ "$FREETZ_TYPE_FON" == "y" ] || [ "$FREETZ_TYPE_300IP_AS_FON" == "y" ] \
-	|| [ "$FREETZ_TYPE_WLAN_3020" == "y" ] || [ "$FREETZ_TYPE_WLAN_3030" == "y" ] \
-	|| [ "$FREETZ_TYPE_FON_WLAN" == "y" ]; then
+elif [ "$FREETZ_TYPE_FON" == "y" -a "$FREETZ_TYPE_LANG_DE" ] || \
+	[ "$FREETZ_TYPE_300IP_AS_FON" == "y" -a "$FREETZ_TYPE_LANG_DE" ] || \
+	[ "$FREETZ_TYPE_WLAN_3020" == "y" ] || [ "$FREETZ_TYPE_WLAN_3030" == "y" ] || \
+	[ "$FREETZ_TYPE_FON_WLAN" == "y"  -a "$FREETZ_TYPE_LANG_DE" ]; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/rc.S-no-cdrom-fallback_fon.patch"
+elif [ "$FREETZ_TYPE_FON" == "y" -a "$FREETZ_TYPE_LANG_EN" ] || \
+	[ "$FREETZ_TYPE_300IP_AS_FON" == "y" -a "$FREETZ_TYPE_LANG_EN" ]|| \
+	[ "$FREETZ_TYPE_FON_WLAN" == "y"  -a "$FREETZ_TYPE_LANG_EN" ]; then
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/rc.S-no-cdrom-fallback_fon_04.49.patch"
 else
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/rc.S-no-cdrom-fallback.patch"
 fi
