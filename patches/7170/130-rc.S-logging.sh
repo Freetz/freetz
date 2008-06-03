d4a39fc68199b6d801bc17073647fd7c46e1d91c
Oliver Metz <oliver@freetz.org>
2008-06-03 08:47:06 +0000

    * Forgot to remove patch in r2203 (fixes #173)
    git-svn-id: http://svn.freetz.org/trunk@2267 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/7170/130-rc.S-logging.sh
+++
@@ -1,7 +0,0 @@
-if [ "$FREETZ_TYPE_LANG_EN" == "y" ]; then
-	if [ "$FREETZ_TYPE_ANNEX_A" == "y" ]; then
-		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/en/rc.S-logging-7170-annexa.patch"
-	else
-		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/en/rc.S-logging-7170-annexb.patch"
-	fi
-fi
