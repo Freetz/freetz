ed20f38c476841f00b40421d5cc3278ead731c99
Nicolai Ehemann <en@enlightened.de>
2008-06-07 21:26:24 +0000

    merge in r2173:2174 from branches/freetz-1.1:  * add nagios 2.11    * TODO: make full package
    git-svn-id: http://svn.freetz.org/trunk@2293 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/nagios/nagios.mk
@@ -0,0 +1,41 @@
+$(call PKG_INIT_BIN, 2.11)
+$(PKG)_SOURCE:=nagios-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=http://mesh.dl.sourceforge.net/sourceforge/nagios
+$(PKG)_BINARY:=$($(PKG)_DIR)/base/nagios
+$(PKG)_STATS_BINARY:=$($(PKG)_DIR)/base/nagiostats
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/nagios
+$(PKG)_STATS_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/nagiostats
+
+$(PKG)_DEPENDS_ON := microperl
+
+$(PKG)_CONFIGURE_OPTIONS += --with-nagios-user="root"
+$(PKG)_CONFIGURE_OPTIONS += --with-nagios-group="root"
+$(PKG)_CONFIGURE_OPTIONS += --sysconfdir="/mod/etc/nagios"
+$(PKG)_CONFIGURE_OPTIONS += --without-gd-lib
+$(PKG)_CONFIGURE_OPTIONS += --without-gd-inc
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY) $($(PKG)_STATS_BINARY): $($(PKG)_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+		$(MAKE) -C $(NAGIOS_DIR) all
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$($(PKG)_STATS_TARGET_BINARY): $($(PKG)_STATS_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_STATS_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(NAGIOS_DIR) clean
+
+$(pkg)-uninstall:
+	$(RM) $(NAGIOS_TARGET_BINARY)
+
+$(PKG_FINISH)
