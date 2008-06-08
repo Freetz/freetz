599d08c188e3136a61d560c6de6d41de453c9747
Lars Falkenhain <lars.falkenhain@gmail.com>
2008-06-08 10:48:39 +0000

    * add uclibc-patch from oliver * add replace_kernel option for 7170/7270/Speedports (fixes #171). thx oliver
    git-svn-id: http://svn.freetz.org/trunk@2306 f5190166-0702-4917-9039-51ec32eddaf5

--- toolchain/make/download-toolchain.mk
+++ toolchain/make/download-toolchain.mk
@@ -4,8 +4,8 @@ include $(TOOLCHAIN_DIR)/make/target/uclibc/uclibc.mk
 
 KERNEL_TOOLCHAIN_VERSION:=0.3
 TARGET_TOOLCHAIN_VERSION:=0.6
-KERNEL_TOOLCHAIN_SOURCE:=kernel-toolchain-freetz-$(KERNEL_TOOLCHAIN_VERSION).tar.lzma
-TARGET_TOOLCHAIN_SOURCE:=target-toolchain-freetz-$(TARGET_TOOLCHAIN_VERSION).tar.lzma
+KERNEL_TOOLCHAIN_SOURCE:=gcc-$(KERNEL_TOOLCHAIN_GCC_VERSION)-freetz-$(KERNEL_TOOLCHAIN_VERSION).tar.lzma
+TARGET_TOOLCHAIN_SOURCE:=gcc-$(TARGET_TOOLCHAIN_GCC_VERSION)-uclibc-$(TARGET_TOOLCHAIN_UCLIBC_VERSION)-freetz-$(TARGET_TOOLCHAIN_VERSION).tar.lzma
 TOOLCHAIN_SITE:=http://freetz.wirsind.info
 
 $(DL_DIR)/$(KERNEL_TOOLCHAIN_SOURCE): | $(DL_DIR)
@@ -20,14 +20,14 @@ download-toolchain: $(TOOLCHAIN_DIR)/kernel/.installed kernel-configured \
 
 $(TOOLCHAIN_DIR)/kernel/.installed: $(DL_DIR)/$(KERNEL_TOOLCHAIN_SOURCE) | $(TOOLS_DIR)/busybox
 	mkdir -p $(TOOLCHAIN_DIR)/build
-	rm -rf $(TOOLCHAIN_DIR)/kernel
+	$(RM) $(TOOLCHAIN_DIR)/kernel
 	$(TOOLS_DIR)/busybox tar $(VERBOSE) -xaf $(DL_DIR)/$(KERNEL_TOOLCHAIN_SOURCE) -C $(TOOLCHAIN_DIR)/build
 	-@ln -s $(BUILD_DIR)/gcc-$(KERNEL_TOOLCHAIN_GCC_VERSION)/mipsel-unknown-linux-gnu $(TOOLCHAIN_DIR)/kernel
 	@touch $@
 
 $(TOOLCHAIN_DIR)/target/.installed: $(DL_DIR)/$(TARGET_TOOLCHAIN_SOURCE) | $(TOOLS_DIR)/busybox
 	mkdir -p $(TOOLCHAIN_DIR)/build
-	rm -rf $(TOOLCHAIN_DIR)/target
+	$(RM) $(TOOLCHAIN_DIR)/target
 	$(TOOLS_DIR)/busybox tar $(VERBOSE) -xaf $(DL_DIR)/$(TARGET_TOOLCHAIN_SOURCE) -C $(TOOLCHAIN_DIR)/build
 	-@ln -s $(BUILD_DIR)/$(TARGET_TOOLCHAIN_COMPILER)/$(REAL_GNU_TARGET_NAME) $(TOOLCHAIN_DIR)/target
 	@touch $@
