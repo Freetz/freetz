e4a99dec546ce963c1df2b8205604f59bd0148e5
Whoopie <whoopie79@gmx.net>
2008-06-12 08:24:09 +0000

    add new packages: * httptunnel 3.3 (made by ao, some changes by me) * tree 1.5.1.2 (version 1.5.2 doesn't compile with uClibc)
    git-svn-id: http://svn.freetz.org/trunk@2316 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/tree/tree.mk
@@ -0,0 +1,31 @@
+$(call PKG_INIT_BIN,1.5.1.2)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tgz
+$(PKG)_SITE:=ftp://mama.indstate.edu/linux/tree
+$(PKG)_BINARY:=$($(PKG)_DIR)/tree
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/tree
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+		$(MAKE) -C $(TREE_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)" \
+		LDFLAGS=""
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(TREE_DIR) clean
+
+$(pkg)-uninstall:
+	$(RM) $(TREE_TARGET_BINARY)
+
+$(PKG_FINISH)
