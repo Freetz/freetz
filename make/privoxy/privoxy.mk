e94bd9eb974505dcc3c62b3ac74d4fe112a86f32
Whoopie <whoopie79@gmx.net>
2008-06-12 13:22:45 +0000

    privoxy: bump to 3.0.8 (should we enable zlib support? I prepared it, but commented it out in the files for the moment )
    git-svn-id: http://svn.freetz.org/trunk@2319 f5190166-0702-4917-9039-51ec32eddaf5

--- make/privoxy/privoxy.mk
+++ make/privoxy/privoxy.mk
@@ -1,4 +1,4 @@
-$(call PKG_INIT_BIN, 3.0.6)
+$(call PKG_INIT_BIN, 3.0.8)
 $(PKG)_SOURCE:=privoxy-$($(PKG)_VERSION)-stable-src.tar.gz
 $(PKG)_SITE:=http://surfnet.dl.sourceforge.net/sourceforge/ijbswa
 $(PKG)_DIR:=$(SOURCE_DIR)/privoxy-$($(PKG)_VERSION)-stable
@@ -6,17 +6,17 @@ $(PKG)_BINARY:=$($(PKG)_DIR)/privoxy
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/privoxy
 $(PKG)_STARTLEVEL=40
 
-$(PKG)_CONFIGURE_PRE_CMDS += autoheader;
-$(PKG)_CONFIGURE_PRE_CMDS += autoconf;
+#$(PKG)_DEPENDS := zlib
 
 $(PKG)_CONFIGURE_ENV += ac_cv_func_setpgrp_void=yes
+$(PKG)_CONFIGURE_ENV += PKG_CONFIG_PATH="$(TARGET_MAKE_PATH)/../usr/lib/pkgconfig"
 
 $(PKG)_CONFIGURE_OPTIONS += --sysconfdir=/mod/etc
 $(PKG)_CONFIGURE_OPTIONS += --with-docbook=no
 $(PKG)_CONFIGURE_OPTIONS += --disable-pthread
 $(PKG)_CONFIGURE_OPTIONS += --disable-stats
 $(PKG)_CONFIGURE_OPTIONS += --disable-dynamic-pcre
-$(PKG)_CONFIGURE_OPTIONS += --disable-dynamic-pcrs
+#$(PKG)_CONFIGURE_OPTIONS += --enable-zlib
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
