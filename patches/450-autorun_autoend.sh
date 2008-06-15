4d4bf95f553b247e497c05f29735fdca875d5d15
Whoopie <whoopie79@gmx.net>
2008-06-15 11:33:33 +0000

    bump 7170/7270 labor gaming to 11435 move autorun/autoend patch to patches section
    git-svn-id: http://svn.freetz.org/trunk@2324 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/450-autorun_autoend.sh
+++ patches/450-autorun_autoend.sh
@@ -1,5 +1,9 @@
 [ "$FREETZ_AUTORUN_AUTOEND" == "y" ] || return 0
 echo1 "patching run_mount and storage: autorun/autoend"
-modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/450-autorun-run_mount.patch"
+if [ "$FREETZ_TYPE_LABOR_GAMING" ]; then
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/450-autorun-run_mount-labor_gaming.patch"
+else
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/450-autorun-run_mount.patch"
+fi
 modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/450-autoend-storage.patch"
 
