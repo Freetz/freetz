c96e3931a15349456fd3935f350d31053464c70b
Oliver Metz <oliver@freetz.org>
2008-06-20 19:55:03 +0000

    * Bump firmware versions:   * 2170: 39.04.57   * 7140 (A-CH, Annex A): 39.04.57   * 7170 (A-CH, Annex A): 58.04.57  * Delete 2170_vpn firmware, vpn is now in regular firmware
    git-svn-id: http://svn.freetz.org/trunk@2334 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/196-usbstorage.sh
+++ patches/196-usbstorage.sh
@@ -1,12 +1,13 @@
 [ "$FREETZ_PATCH_USBSTORAGE" == "y" ] || return 0
 echo1 "applying USB storage patch"
-if [ "$FREETZ_TYPE_2170" == "y" -a "$FREETZ_TYPE_LABOR" != "y" ] || \
-	[ "$FREETZ_TYPE_FON_WLAN_7140" == "y" -a "$FREETZ_TYPE_LANG_DE" == "y" ] || \
+if 	[ "$FREETZ_TYPE_FON_WLAN_7140" == "y" -a "$FREETZ_TYPE_LANG_DE" == "y" ] || \
 	[ "$FREETZ_TYPE_WLAN_3130" == "y" ]; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage_wotam.patch"
 elif [ "$FREETZ_TYPE_FON_WLAN_7140" == "y" -a "$FREETZ_TYPE_LANG_EN" == "y" ]; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/en/usbstorage_7140.patch"
-elif [ "$FREETZ_TYPE_FON_7150" == "y" ] || \
+elif [ "$FREETZ_TYPE_2170" == "y" ] || \
+	[ "$FREETZ_TYPE_FON_7150" == "y" ] || \
+	[ "$FREETZ_TYPE_FON_WLAN_7140" == "y" -a "$FREETZ_TYPE_LANG_A_CH" == "y" ] || \
 	[ "$FREETZ_TYPE_FON_WLAN_7141" == "y" ] || \
 	[ "$FREETZ_TYPE_FON_WLAN_7170" == "y" ] || \
 	[ "$FREETZ_TYPE_FON_WLAN_7270" == "y" ] || \
@@ -15,8 +16,6 @@ elif [ "$FREETZ_TYPE_FON_7150" == "y" ] || \
 	[ "$FREETZ_TYPE_SPEEDPORT_W900V" == "y" ]; then
 		if [ "$FREETZ_TYPE_LABOR_GAMING" ]; then
 			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage_7270_labor_gaming.patch"
-		elif [ "$FREETZ_TYPE_LANG_A_CH" == "y" ]; then
-			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage.patch"
 		else
 			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage_7270.patch"
 		fi
--- patches/280-storage.sh
+++ patches/280-storage.sh
@@ -4,11 +4,7 @@ if [ "$FREETZ_TYPE_FON_7150" == "y" ] || \
 	[ "$FREETZ_TYPE_FON_WLAN_7141" == "y" ] || \
 	[ "$FREETZ_TYPE_FON_WLAN_7170" == "y" ] || \
 	[ "$FREETZ_TYPE_SPEEDPORT_W900V" == "y" ]; then
-		if [ "$FREETZ_TYPE_LANG_A_CH" == "y" ]; then
-			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/storage.patch"
-		else
-			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/storage_7170.patch"
-		fi
+		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/storage_7170.patch"
 elif [ "$FREETZ_TYPE_FON_WLAN_7270" == "y" ]; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/storage_7270.patch"
 elif [ "$FREETZ_TYPE_FON_WLAN_7140" == "y" -a "$FREETZ_TYPE_LANG_EN" == "y" ]; then
