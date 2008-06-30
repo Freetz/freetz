11c5028436ae0a9da1a8ebd72f02437c15da4092
Nicolai Ehemann <en@enlightened.de>
2008-06-30 14:34:01 +0000

    fixed unintended dependence in wol.mk
    git-svn-id: http://svn.freetz.org/trunk@2362 f5190166-0702-4917-9039-51ec32eddaf5

--- make/wol/wol.mk
+++ make/wol/wol.mk
@@ -5,14 +5,13 @@ $(PKG)_DIR:=$(SOURCE_DIR)/$(pkg)-$($(PKG)_VERSION)
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/$(pkg)
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/$(pkg)
 
-$(PKG)_DEPENDS_ON := 
 $(PKG)_CONFIGURE_ENV := jm_cv_func_working_malloc=yes
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
-$($(PKG)_BINARY): $($(PKG)_DIR)/.configured $(CURL_LIB_STAGING_BINARY)
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured 
 	PATH="$(TARGET_PATH)" \
 		$(MAKE) -C $(WOL_DIR) \
 		CROSS="$(TARGET_MAKE_PATH)/$(TARGET_CROSS)" \
