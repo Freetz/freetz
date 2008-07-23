6e7fa8658d44106d2c70c8bada7304600a987eb9
Marco Zissen <maz@freetz.org>
2008-07-23 17:15:31 +0000

    * new v0.5.0_beta1
    git-svn-id: http://svn.freetz.org/trunk@2389 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/pjproject.mk
+++ make/libs/pjproject.mk
@@ -1,42 +1,29 @@
-$(call PKG_INIT_LIB, 0.8.0)
-$(PKG)_LIB_VERSION:=0.8.0
-$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(call PKG_INIT_LIB, 0.9.0)
+$(PKG)_LIB_VERSION:=0.9.0
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_SITE:=http://fritz.v3v.de/dtmfbox/libs
 $(PKG)_BINARY:=$($(PKG)_DIR)/pjsip/lib/libpjsip.a
-$(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpjsip.a
+$(PKG)_CONFIG_SITE:=$(PJPROJECT_DIR)/pjlib/include/pj/config_site.h
+$(PKG)_STAGING_BINARY:=$(PJPROJECT_DIR)/pjproject.build
 # only static lib
 #$(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libpjsip.so.$($(PKG)_LIB_VERSION)
 
-$(PKG)_CONFIGURE_PRE_CMDS += cp aconfigure.ac configure.ac ;
-$(PKG)_CONFIGURE_PRE_CMDS += autoconf --force ;
-
-$(PKG)_CFLAGS:=-DPJ_DEBUG=0
-$(PKG)_CFLAGS+=-DNDEBUG=0
-$(PKG)_CFLAGS+=-DPJ_OS_HAS_CHECK_STACK=0 
-$(PKG)_CFLAGS+=-DPJ_ENABLE_EXTRA_CHECK=0
-$(PKG)_CFLAGS+=-DPJ_HAS_ERROR_STRING=0
-$(PKG)_CFLAGS+=-DPJSIP_SAFE_MODULE=0
-$(PKG)_CFLAGS+=-DPJ_LOG_MAX_LEVEL=5
-$(PKG)_CFLAGS+=-DPJ_TERM_HAS_COLOR=0
-$(PKG)_CFLAGS+=-DPJ_HAS_FLOATING_POINT=0
-$(PKG)_CFLAGS+=-DPJMEDIA_SDP_NEG_PREFER_REMOTE_CODEC_ORDER=$(if $(FREETZ_PACKAGE_DTMFBOX_PREFER_REMOTE_CODEC_ORDER),1,0)
-$(PKG)_CFLAGS+=-DPJMEDIA_SOUND_BUFFER_COUNT=$(FREETZ_PACKAGE_DTMFBOX_SOUND_BUFFER_COUNT)
-
-$(PKG)_CONFIGURE_OPTIONS += --enable-shared
-$(PKG)_CONFIGURE_OPTIONS += --enable-static
 $(PKG)_CONFIGURE_OPTIONS += --disable-sound
+$(PKG)_CONFIGURE_OPTIONS += --disable-oss
 $(PKG)_CONFIGURE_OPTIONS += --disable-large-filter
 $(PKG)_CONFIGURE_OPTIONS += --disable-small-filter
 $(PKG)_CONFIGURE_OPTIONS += --disable-ssl
 $(PKG)_CONFIGURE_OPTIONS += --disable-floating-point
 $(PKG)_CONFIGURE_OPTIONS += --disable-libsamplerate
+$(PKG)_CONFIGURE_OPTIONS += --disable-srtp
 $(PKG)_CONFIGURE_OPTIONS += --disable-l16-codec
+$(PKG)_CONFIGURE_OPTIONS += --disable-g722-codec
 $(PKG)_CONFIGURE_OPTIONS += --disable-speex-aec
-$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_DTMFBOX_WITH_G711_CODEC),,--disable-g711-codec)
-$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_DTMFBOX_WITH_GSM_CODEC),,--disable-gsm-codec)
-$(PKG)_CONFIGURE_OPTIONS += --disable-speex-codec
 $(PKG)_CONFIGURE_OPTIONS += --disable-ilbc-codec
-#$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_DTMFBOX_WITH_SPEEX_CODEC),,--disable-speex-codec)
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_DTMFBOX_WITH_GSM_CODEC),,--disable-gsm-codec)
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_DTMFBOX_WITH_SPEEX_CODEC),,--disable-speex-codec)
+#$(PKG)_CONFIGURE_OPTIONS += --disable-speex-codec
+#$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_DTMFBOX_WITH_G711_CODEC),,--disable-g711-codec)
 #$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_DTMFBOX_WITH_ILBC_CODEC),,--disable-ilbc-codec)
 
 $(PKG_SOURCE_DOWNLOAD)
@@ -44,23 +31,42 @@ $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_DIR)/.depend: $($(PKG)_DIR)/.configured
+	cd $(PJPROJECT_DIR)
+	rm -f $(PJPROJECT_CONFIG_SIZE)
+	echo "#undef PJ_OS_HAS_CHECK_STACK"  		>> $(PJPROJECT_CONFIG_SITE)
+	echo "#define PJ_OS_HAS_CHECK_STACK 0"  	>> $(PJPROJECT_CONFIG_SITE)
+	echo "#define PJ_TERM_HAS_COLOR 0"		>> $(PJPROJECT_CONFIG_SITE)
+	echo "#define PJ_ENABLE_EXTRA_CHECK 0"  	>> $(PJPROJECT_CONFIG_SITE)
+	echo "#define PJ_DEBUG 0"  			>> $(PJPROJECT_CONFIG_SITE)
+	echo "#define NDEBUG 0"  			>> $(PJPROJECT_CONFIG_SITE)
+	echo "#define PJ_LOG_LEVEL_MAX 4"  		>> $(PJPROJECT_CONFIG_SITE)
+	echo "#define PJ_HAS_ERROR_STRING 0"  		>> $(PJPROJECT_CONFIG_SITE)
+	echo "#define PJSIP_SAFE_MODULE 0"  		>> $(PJPROJECT_CONFIG_SITE)
+	echo "#define PJMEDIA_HAS_SRTP 0" 		>> $(PJPROJECT_CONFIG_SITE)
+	echo "#define PJ_HAS_FLOATING_POINT 0"		>> $(PJPROJECT_CONFIG_SITE)
+	echo "#define SIOCGIFCONF 1"  			>> $(PJPROJECT_CONFIG_SITE)
+	echo "#undef PJ_HAS_NET_IF_H" 			>> $(PJPROJECT_CONFIG_SITE)
+	echo "#undef PJ_HAS_IFADDR_H" 			>> $(PJPROJECT_CONFIG_SITE)
+	echo "#define PJMEDIA_SOUND_IMPLEMENTATION PJMEDIA_SOUND_NULL_SOUND" >> $(PJPROJECT_CONFIG_SITE)
 	PATH=$(TARGET_TOOLCHAIN_PATH) \
-		$(MAKE) -C $(PJPROJECT_DIR) dep \
-		TARGET_NAME="$(REAL_GNU_TARGET_NAME)"
+	    LDFLAGS="-lm" \
+	    $(MAKE) -C $(PJPROJECT_DIR) dep
 	touch $@
 
-$($(PKG)_BINARY): $($(PKG)_DIR)/.depend
+$($(PKG)_STAGING_BINARY): $($(PKG)_DIR)/.depend
+	cd $(PJPROJECT_DIR)
 	PATH=$(TARGET_TOOLCHAIN_PATH) \
-		$(MAKE1) -C $(PJPROJECT_DIR) all \
-		CC_CFLAGS="$(PJPROJECT_CFLAGS)" \
-		TARGET_NAME="$(REAL_GNU_TARGET_NAME)"
+	    LDFLAGS="-lm" \
+	    $(MAKE1) -C $(PJPROJECT_DIR)
+	touch $@
 
-$($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
-	PATH=$(TARGET_TOOLCHAIN_PATH) \
-		$(MAKE) -C $(PJPROJECT_DIR) \
-		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
-		TARGET_NAME="$(REAL_GNU_TARGET_NAME)" \
-		install
+#$($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
+#	cd $(PJPROJECT_DIR)
+#	PATH=$(TARGET_TOOLCHAIN_PATH) \
+#	    $(MAKE1) -C $(PJPROJECT_DIR) LDFLAGS="-lm" \
+#	    DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+#	install
+#	touch $@
 
 #$(PJSIP_TARGET_BINARY): $(PJSIP_STAGING_BINARY)
 #	cp -a $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpjsip*.so* $(PJPROJECT_TARGET_DIR)
@@ -74,9 +80,10 @@ $(pkg)-clean:
 	-$(MAKE) -C $(PJPROJECT_DIR) \
 		TARGET_NAME="$(REAL_GNU_TARGET_NAME)" \
 		clean
-	rm -f $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpj*.a \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libresample.a
-	 
+#		rm -f $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/local/lib/libpj*.a \
+#		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/local/lib/libresample-*.a \
+#		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/local/lib/libsrtp-*.a \
+#		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/local/lib/libmilenage-*.a
 
 #$(pkg)-uninstall:
 #	rm -f $(PJPROJECT_TARGET_DIR)/libpjsip*.so*
