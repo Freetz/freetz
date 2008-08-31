b5384f677bf894cbe99f04c9d633273131599448
Lars Falkenhain <lars.falkenhain@gmail.com>
2008-08-31 18:15:39 +0000

    * iptables version bump to 1.4.1.1 (thx MaxMuster, fixes #215)
    git-svn-id: http://svn.freetz.org/trunk@2476 f5190166-0702-4917-9039-51ec32eddaf5

--- make/iptables/iptables.mk
+++ make/iptables/iptables.mk
@@ -1,29 +1,21 @@
 # take care of patches/150-iptables.sh
-$(call PKG_INIT_BIN, 1.3.7)
-$(PKG)_VERSION:=1.3.7
+$(call PKG_INIT_BIN, 1.4.1.1)
+#$(PKG)_VERSION:=1.4.1.1
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_SITE:=http://netfilter.org/projects/$(pkg)/files
 $(PKG)_BINARY:=$($(PKG)_DIR)/iptables
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/iptables
-$(PKG)_EXTENSIONS_DIR:=$($(PKG)_DEST_DIR)/usr/lib/iptables
+$(PKG)_EXTENSIONS_DIR:=$($(PKG)_DEST_DIR)/usr/lib/xtables
 $(PKG)_TARGET_EXTENSIONS:=$($(PKG)_EXTENSIONS_DIR)/.installed
-$(PKG)_KERNEL_DIR:=$(FREETZ_BASE_DIR)/$(KERNEL_SOURCE_DIR)
+$(PKG)_CONFIGURE_ENV += AR="$(TARGET_MAKE_PATH)/$(TARGET_CROSS)ar" RANLIB="$(TARGET_MAKE_PATH)/$(TARGET_CROSS)ranlib"
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
-$(PKG_CONFIGURED_NOP)
+$(PKG_CONFIGURED_CONFIGURE)
 
-$($(PKG)_BINARY): \
-		$($(PKG)_DIR)/.configured | kernel-source
-	chmod +x $(IPTABLES_DIR)/extensions/.*-test*
-	$(MAKE) KERNEL_DIR="$(IPTABLES_KERNEL_DIR)" \
-		CC="$(TARGET_CC)" \
-		LD="$(TARGET_LD)" \
-		COPT_FLAGS="$(TARGET_CFLAGS)" \
-		LDFLAGS="-rdynamic" \
-		PREFIX=/usr \
-		DO_IPV6=0 \
-		-C $(IPTABLES_DIR)
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured | kernel-source
+	find "$(IPTABLES_DIR)" -name "*ip6*" -o -name "*ipv6*" | xargs rm -rf
+	$(MAKE) -C $(IPTABLES_DIR)
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
@@ -37,9 +29,7 @@ $($(PKG)_TARGET_EXTENSIONS): $($(PKG)_BINARY)
 $(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_TARGET_EXTENSIONS)
 
 $(pkg)-clean:
-	-$(MAKE) KERNEL_DIR="$(IPTABLES_KERNEL_DIR)" \
-		CC="$(TARGET_CC)" \
-		-C $(IPTABLES_DIR) clean
+	-$(MAKE) -C $(IPTABLES_DIR) clean
 
 $(pkg)-uninstall:
 	rm -f $(IPTABLES_TARGET_BINARY)
