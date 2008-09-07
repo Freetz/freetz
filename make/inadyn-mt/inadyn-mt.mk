417cb4fc8a985fb954dbef674b4582ea3b486d15
Oliver Metz <oliver@freetz.org>
2008-09-07 15:11:29 +0000

    * Replace old inadyn package with inadyn-mt
    git-svn-id: http://svn.freetz.org/trunk@2508 f5190166-0702-4917-9039-51ec32eddaf5

--- make/inadyn-mt/inadyn-mt.mk
+++ make/inadyn-mt/inadyn-mt.mk
@@ -1,10 +1,8 @@
-$(call PKG_INIT_BIN,1.96.2)
-$(PKG)_SOURCE:=inadyn.v$($(PKG)_VERSION).zip
-$(PKG)_SITE:=http://www.inatech.eu/inadyn
-#$(PKG)_SITE:=http://www.mirrorservice.org/sites/ftp.freebsd.org/pub/FreeBSD/distfiles
-$(PKG)_DIR:=$(SOURCE_DIR)/inadyn
-$(PKG)_BINARY:=$($(PKG)_DIR)/bin/linux/inadyn
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/inadyn
+$(call PKG_INIT_BIN,02.12.24)
+$(PKG)_SOURCE:=$(pkg).v.$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=http://switch.dl.sourceforge.net/sourceforge/inadyn-mt
+$(PKG)_BINARY:=$($(PKG)_DIR)/bin/linux/inadyn-mt
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/inadyn-mt
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
