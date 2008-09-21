3d17f9d34d94800779aa44cadd4b036dca208efb
Lars Falkenhain <lars.falkenhain@gmail.com>
2008-09-21 20:37:37 +0000

    * fix usb-storage patch for 7170 AiO caused by 7270 version bump
    git-svn-id: http://svn.freetz.org/trunk@2581 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/196-usbstorage.sh
+++ patches/196-usbstorage.sh
@@ -17,8 +17,10 @@ elif [ "$FREETZ_TYPE_2170" == "y" ] || \
 		if [ "$FREETZ_TYPE_LABOR_GAMING" == "y" ] || \
 			[ "$FREETZ_TYPE_LABOR_DSL" == "y" ]; then
 			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage_7270_labor_gaming.patch"
-		elif [ "$FREETZ_TYPE_LABOR_ALL" == "y" ]; then
+		elif [ "$FREETZ_TYPE_LABOR_ALL" == "y" -a "$FREETZ_TYPE_FON_WLAN_7270" == "y" ]; then
 			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage_7270_labor_all.patch"
+		elif [ "$FREETZ_TYPE_LABOR_ALL" == "y" -a "$FREETZ_TYPE_FON_WLAN_7170" == "y"  ]; then
+      modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage_7170_labor_all.patch"
 		else
 			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage_7270.patch"
 		fi
