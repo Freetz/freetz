03ea395246d9f2e2352d6a749564f6f492efffca
Nicolai Ehemann <en@enlightened.de>
2008-09-21 15:50:15 +0000

    * added unstable package tinc (1.0.8) * added sane-backends to CHANGELOG
    git-svn-id: http://svn.freetz.org/trunk@2578 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/tinc/tinc.mk
@@ -0,0 +1,31 @@
+$(call PKG_INIT_BIN, 1.0.8)
+$(PKG)_SOURCE:=tinc-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=http://www.tinc-vpn.org/packages/
+$(PKG)_DIR:=$(SOURCE_DIR)/tinc-$($(PKG)_VERSION)
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/tincd
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/tincd
+
+$(PKG)_DEPENDS_ON := openssl
+$(PKG)_DEPENDS_ON += lzo
+$(PKG)_DEPENDS_ON += zlib
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+	$(MAKE) -C $(TINC_DIR)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(TINC_DIR) clean
+
+$(pkg)-uninstall:
+	rm -f $(TINC_TARGET_BINARY)
+
+$(PKG_FINISH)
