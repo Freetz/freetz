b79558bbbfa50b72fe8b79b2cf971cb13c2e90ab
Oliver Metz <oliver@freetz.org>
2008-09-24 00:43:23 +0000

    * patches: fix usage of rm_files in remove tr069 patch (addresses #256)
    git-svn-id: http://svn.freetz.org/trunk@2587 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/260-remove_tr069.sh
+++ patches/260-remove_tr069.sh
@@ -9,7 +9,7 @@ rm_files()
 }
 
 echo1 "removing tr069 stuff"
-rm_files "${FILESYSTEM_MOD_DIR}/usr/share/ctlmgr/libtr069.so" \
-	"${FILESYSTEM_MOD_DIR}/bin/tr069starter" \
-	"${FILESYSTEM_MOD_DIR}/usr/bin/tr069fwupdate"
+rm_files "${FILESYSTEM_MOD_DIR}/usr/share/ctlmgr/libtr069.so"
+rm_files "${FILESYSTEM_MOD_DIR}/bin/tr069starter"
+rm_files "${FILESYSTEM_MOD_DIR}/usr/bin/tr069fwupdate"
 
