806d105488d652b7e7898589b81c836832934058
Nicolai Ehemann <en@enlightened.de>
2008-10-12 12:11:53 +0000

    implemented new package squid-3.0.STABLE9 with some fixes from OpenWRT build
    git-svn-id: http://svn.freetz.org/trunk@2646 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/squid/squid.mk
@@ -0,0 +1,31 @@
+$(call PKG_INIT_BIN, 3.0.STABLE9)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SITE:=http://www.squid-cache.org/Versions/v$(firstword $(subst ., ,$($(PKG)_VERSION)))/$(firstword $(subst ., ,$($(PKG)_VERSION))).$(word 2,$(subst ., ,$($(PKG)_VERSION)))
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/squid
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/squid
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+		$(MAKE) -C $(SQUID_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(SQUID_DIR) clean
+	$(RM) $(SQUID_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(SQUID_TARGET_BINARY)
+
+$(PKG_FINISH)
