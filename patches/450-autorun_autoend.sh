285c8b6b9979934c6b80ce3841c9d23e47b76457
Oliver Metz <oliver@freetz.org>
2008-10-16 21:51:09 +0000

    * Bump DSL Labor firmwares:  * 7270: 54.04.99-12606  * 7170: 29.04.99-12526
    git-svn-id: http://svn.freetz.org/trunk@2651 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/450-autorun_autoend.sh
+++ patches/450-autorun_autoend.sh
@@ -1,6 +1,6 @@
 [ "$FREETZ_AUTORUN_AUTOEND" == "y" ] || return 0
 echo1 "patching run_mount and storage: autorun/autoend"
-if [ "$FREETZ_TYPE_LABOR_GAMING" ] || [ "$FREETZ_TYPE_WLAN_3270" ]; then
+if  [ "$FREETZ_TYPE_LABOR_DSL" ] || [ "$FREETZ_TYPE_LABOR_GAMING" ] || [ "$FREETZ_TYPE_WLAN_3270" ]; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/450-autorun-run_mount-labor_gaming.patch"
 elif [ "$FREETZ_TYPE_LABOR_ALL" ]; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/450-autorun-run_mount-labor_all.patch"
