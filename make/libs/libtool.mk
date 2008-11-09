4edf9c8c1315e90fe7d26c6f5df10109a79f31da
Oliver Metz <oliver@freetz.org>
2008-11-09 01:09:51 +0000

    * add missing libtool.mk from r2703
    git-svn-id: http://svn.freetz.org/trunk@2706 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/libs/libtool.mk
@@ -0,0 +1,45 @@
+$(call PKG_INIT_LIB, 1.5.26)
+$(PKG)_LIB_VERSION:=3.1.6
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=http://ftp.gnu.org/gnu/libtool
+$(PKG)_BINARY:=$($(PKG)_DIR)/libltdl/.libs/libltdl.so.$($(PKG)_LIB_VERSION)
+$(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libltdl.so.$($(PKG)_LIB_VERSION)
+$(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libltdl.so.$($(PKG)_LIB_VERSION)
+
+TARGET_CONFIGURE_ENV += GLOBAL_LIBDIR=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib
+
+$(PKG)_CONFIGURE_OPTIONS += --enable-shared
+$(PKG)_CONFIGURE_OPTIONS += --enable-static
+
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	PATH=$(TARGET_TOOLCHAIN_PATH) \
+		$(MAKE) -C $(LIBTOOL_DIR)/libltdl
+
+$($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
+	PATH=$(TARGET_TOOLCHAIN_PATH) $(MAKE) \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+		-C $(LIBTOOL_DIR)/libltdl install
+	$(PKG_FIX_LIBTOOL_LA) \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/libltdl.la
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
+	cp -a $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libltdl*.so* $(LIBTOOL_TARGET_DIR)/
+	$(TARGET_STRIP) $@
+
+$(pkg): $($(PKG)_STAGING_BINARY)
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(LIBTOOL_DIR) clean
+	rm -rf $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libltdl.*
+
+$(pkg)-uninstall:
+	rm -f $(LIBTOOL_TARGET_DIR)/libltdl*.so*
+
+$(PKG_FINISH)
