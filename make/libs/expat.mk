4ce85d66c6214ec354e3676045a37095655472c2
Oliver Metz <oliver@freetz.org>
2008-11-09 01:19:10 +0000

    * expat: Fix directories and cleanup
    git-svn-id: http://svn.freetz.org/trunk@2709 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/expat.mk
+++ make/libs/expat.mk
@@ -8,14 +8,6 @@ $(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libexpat.so.$($(PKG)_LIB_VERSION)
 
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
-$(PKG)_CONFIGURE_OPTIONS += --libdir=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib
-$(PKG)_CONFIGURE_OPTIONS += --includedir=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include
-$(PKG)_CONFIGURE_OPTIONS += --bindir=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin
-$(PKG)_CONFIGURE_OPTIONS += --sbindir=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/sbin
-$(PKG)_CONFIGURE_OPTIONS += --datadir=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/share
-$(PKG)_CONFIGURE_OPTIONS += --mandir=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/man
-$(PKG)_CONFIGURE_OPTIONS += --infodir=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/info
-#$(PKG)_CONFIGURE_OPTIONS += --with-ssl="openssl"
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -32,7 +24,6 @@ $($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
 		-C $(EXPAT_DIR) install
 	$(PKG_FIX_LIBTOOL_LA) \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/libexpat.la 
-#		$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/pkgconfig/bluez.pc
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
 	cp -a $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libexpat*.so* $(EXPAT_TARGET_DIR)/
@@ -45,7 +36,6 @@ $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 $(pkg)-clean:
 	-$(MAKE) -C $(EXPAT_DIR) clean
 	rm -rf $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libexpat.*
-#		$(TARGET_TOOLCHAIN_STAGING_DIR)/include/bluetooth \
 
 $(pkg)-uninstall:
 	rm -f $(EXPAT_TARGET_DIR)/libexpat*.so*
