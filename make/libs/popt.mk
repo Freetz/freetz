0bd5cab3094ce412b8b7cb4a8f459b1f97956f8c
Oliver Metz <oliver@freetz.org>
2008-11-09 01:11:02 +0000

    * popt: Don't check for libintl and libiconv
    git-svn-id: http://svn.freetz.org/trunk@2707 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/popt.mk
+++ make/libs/popt.mk
@@ -6,9 +6,13 @@ $(PKG)_BINARY:=$($(PKG)_DIR)/.libs/libpopt.so.$($(PKG)_LIB_VERSION)
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpopt.so.$($(PKG)_LIB_VERSION)
 $(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libpopt.so.$($(PKG)_LIB_VERSION)
 
+$(PKG)_CONFIGURE_PRE_CMDS += autoconf --force ;
+
 $(PKG)_CONFIGURE_ENV += ac_cv_va_copy=yes
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
+$(PKG)_CONFIGURE_OPTIONS += --without-libintl-prefix
+$(PKG)_CONFIGURE_OPTIONS += --without-libiconv-prefix
 
 
 $(PKG_SOURCE_DOWNLOAD)
