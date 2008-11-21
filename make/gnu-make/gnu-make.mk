bf5280d21d67f2cf06ba1fd3e05dd145a5ee61dc
Oliver Metz <oliver@freetz.org>
2008-11-21 21:55:17 +0000

    * Add new package gnu-make to use toolchain on the box
    git-svn-id: http://svn.freetz.org/trunk@2744 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/gnu-make/gnu-make.mk
@@ -0,0 +1,35 @@
+$(call PKG_INIT_BIN, 3.81)
+$(PKG)_SOURCE:=make-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SITE:=http://ftp.gnu.org/pub/gnu/make
+$(PKG)_DIR:=$(SOURCE_DIR)/make-$($(PKG)_VERSION)
+$(PKG)_BINARY:=$($(PKG)_DIR)/make
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/make
+
+
+$(PKG)_CONFIGURE_ENV += make_cv_sys_gnu_glob=no
+$(PKG)_CONFIGURE_ENV += GLOBINC='-I$($(PKG)DIR)/glob'
+$(PKG)_CONFIGURE_ENV += GLOBLIB=glob/libglob.a
+
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+		$(MAKE) -C $(GNU_MAKE_DIR)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(GNU_MAKE_DIR) clean
+
+$(pkg)-uninstall: 
+	rm -f $(GNU_MAKE_TARGET_BINARY)
+
+$(PKG_FINISH)
