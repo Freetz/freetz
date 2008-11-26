62e5708704e44b1485724ad4bc11efd979ef856f
Oliver Metz <oliver@freetz.org>
2008-11-26 11:26:19 +0000

    merge in r2801 from trunk:  * Use same grep for oems in 100-webif.sh patches   * Fixes problem with 7170 Labor AiO
    git-svn-id: http://svn.freetz.org/branches/freetz-stable-1.1@2802 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/7141_labor_mini/100-webif.sh
+++ patches/7141_labor_mini/100-webif.sh
@@ -1,7 +1,7 @@
 [ "$FREETZ_TYPE_LANG_DE" == "y" ] || return 0
 echo1 "Applying symlinks, deleting additional webinterfaces"
 mv ${FILESYSTEM_MOD_DIR}/usr/www/avm ${FILESYSTEM_MOD_DIR}/usr/www/all
-oems="avm 1und1 freenet"
+oems="$(grep 'for i in  avm' "${FIRMWARE_MOD_DIR}/var/install" | head -n 1 | sed -e 's/^.*for i in\(.*\); do.*$/\1/')"
 for i in $oems; do
 	rm -rf ${FILESYSTEM_MOD_DIR}/usr/www/$i
 	ln -s all ${FILESYSTEM_MOD_DIR}/usr/www/$i
