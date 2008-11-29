e8dcc8768880347579254681fdfa83da43896fb2
Oliver Metz <oliver@freetz.org>
2008-11-29 13:18:23 +0000

    * gw6c:  * Repack source and put it on our mirror  * Change target from linux to openwrt  * Build without openssl (fixes #304)  * Link against uClibc++ instead of libstdc++
    git-svn-id: http://svn.freetz.org/trunk@2814 f5190166-0702-4917-9039-51ec32eddaf5

--- make/gw6/gw6.mk
+++ make/gw6/gw6.mk
@@ -1,39 +1,41 @@
 $(call PKG_INIT_BIN, 5.1)
-$(PKG)_SOURCE:=gw6c-5_1-RELEASE-src.tar.gz
-$(PKG)_SITE:=http://go6.net/4105/file.asp?file_id=150
-$(PKG)_DIR:=$(SOURCE_DIR)/tspc-advanced
-$(PKG)_BINARY:=$($(PKG)_DIR)/bin/gw6c
+$(PKG)_SOURCE:=gw6c-$($(PKG)_VERSION).tar.bz2
+#$(PKG)_SITE:=http://go6.net/4105/file.asp?file_id=150
+$(PKG)_BINARY:=$($(PKG)_DIR)/tspc-advanced/bin/gw6c
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/gw6c
 
+$(PKG)_DEPENDS_ON := uclibcxx
 
-### PKG_SOURCE_DOWNLOAD
-int_gwsix_source:=$($(PKG)_SOURCE)
-int_gwsix_site:=$($(PKG)_SITE)
-$(DL_DIR)/$($(PKG)_SOURCE): | $(DL_DIR)
-	@if [ -e $(MIRROR_DIR)/$(int_gwsix_source) ]; then \
-		echo "Found $(int_gwsix_source) in $(MIRROR_DIR), creating hard link"; \
-		ln $(MIRROR_DIR)/$(int_gwsix_source) $(DL_DIR); \
-	else \
-		wget -O $(DL_DIR)/$(int_gwsix_source) $(int_gwsix_site); \
-	fi
+GW6C_OPTS:= \
+	C_COMPILER="$(TARGET_CC) -c -DNO_STDLIBCXX" \
+	COMPILER="$(TARGET_CROSS)g++ -c -DNO_STDLIBCXX" \
+	CPP_FLAGS="$(TARGET_CFLAGS) -fno-builtin -nostdinc++ -I. -Wall -DNDEBUG \
+		-I$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/uClibc++" \
+	RANLIB="$(TARGET_CROSS)ranlib" \
+	ARCHIVER="$(TARGET_CROSS)ar" \
+	C_LINKER="$(TARGET_CC)" \
+	LINKER="$(TARGET_CROSS)g++"
 
-$(pkg)-download: $(DL_DIR)/$($(PKG)_SOURCE)
 
+$(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_NOP)
 
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	PATH="$(TARGET_PATH)" \
-		$(MAKE) -C $(GW6_DIR) all \
-		target=linux \
+		$(MAKE) -C $(GW6_DIR)/gw6c-config \
+		$(GW6C_OPTS)
+	PATH="$(TARGET_PATH)" \
+		$(MAKE) -C $(GW6_DIR)/gw6c-messaging \
+		$(GW6C_OPTS)
+	PATH="$(TARGET_PATH)" \
+		$(MAKE) -C $(GW6_DIR)/tspc-advanced \
 		CC="$(TARGET_CC)" \
-		CXX="$(TARGET_MAKE_PATH)/$(TARGET_CROSS)c++" \
-		AR="$(TARGET_MAKE_PATH)/$(TARGET_CROSS)ar" \
-		RANLIB="$(TARGET_MAKE_PATH)/$(TARGET_CROSS)ranlib"
-		
-		
-		
+		CFLAGS="$(TARGET_CFLAGS) -fno-builtin -Wall -I../../include -I../../platform/openwrt -I../.." \
+		LDFLAGS="-L../../gw6cconfig -L../../gw6cmessaging -nodefaultlibs -luClibc++ -lgcc_s" \
+		target="openwrt"
+
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
 
@@ -42,9 +44,9 @@ $(pkg):
 $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
 $(pkg)-clean:
-	-$(MAKE) -C $(GW6_DIR) target=linux clean
-	-$(MAKE) -C $(SOURCE_DIR)/gw6c-config target=linux clean
-	-$(MAKE) -C $(SOURCE_DIR)/gw6c-messaging target=linux clean
+	-$(MAKE) -C $(GW6_DIR)/tspc-advanced target=openwrt clean
+	-$(MAKE) -C $(GW6_DIR)/gw6c-config target=openwrt clean
+	-$(MAKE) -C $(GW6_DIR)/gw6c-messaging target=openwrt clean
 
 $(pkg)-uninstall:
 	$(RM) $($(PKG)_TARGET_BINARY)
