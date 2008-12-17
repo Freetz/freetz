5e15000b27936f4e0cc1a8e5285684688e90454b
Oliver Metz <oliver@freetz.org>
2008-12-17 23:59:26 +0000

    * Fix 16 MB Patches for 7270 Labor PHONE and UMTS
    git-svn-id: http://svn.freetz.org/trunk@2903 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/115-7270_16MB.sh
+++ patches/115-7270_16MB.sh
@@ -3,6 +3,10 @@
 echo1 "applying 16MB patch"
 if [ "$FREETZ_TYPE_LABOR_DSL" == "y" ];then
 	modpatch "$FIRMWARE_MOD_DIR" "${PATCHES_DIR}/cond/7270_labor_dsl_16MB_flash.patch"
+elif [ "$FREETZ_TYPE_LABOR_PHONE" == "y" ];then
+	modpatch "$FIRMWARE_MOD_DIR" "${PATCHES_DIR}/cond/7270_labor_phone_16MB_flash.patch"
+elif [ "$FREETZ_TYPE_LABOR_UMTS" == "y" ];then
+	modpatch "$FIRMWARE_MOD_DIR" "${PATCHES_DIR}/cond/7270_labor_umts_16MB_flash.patch"
 else
 	modpatch "$FIRMWARE_MOD_DIR" "${PATCHES_DIR}/cond/7270_16MB_flash.patch"
 fi
