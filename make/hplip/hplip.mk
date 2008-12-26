0373c24a492c2f0cf30e97285f6844edef8d140e
Whoopie <whoopie79@gmx.net>
2008-12-26 19:24:02 +0000

    hplip: bump to 2.8.12 hp-utils: change "DS-MOD" to "Freetz" sane-backends: fix init script, only symlink or copy files if they exist.
    git-svn-id: http://svn.freetz.org/trunk@2933 f5190166-0702-4917-9039-51ec32eddaf5

--- make/hplip/hplip.mk
+++ make/hplip/hplip.mk
@@ -1,4 +1,4 @@
-$(call PKG_INIT_BIN, 2.8.10)
+$(call PKG_INIT_BIN, 2.8.12)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=@SF/hplip
 $(PKG)_LIB_IP_VERSION=0.0.1
@@ -68,7 +68,7 @@ $($(PKG)_LIB_HPAIO_TARGET_BINARY): $($(PKG)_LIB_HPAIO_STAGING_BINARY)
 	mkdir -p $(HPLIP_DEST_DIR)/usr/share
 	cp -a $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/sane/libsane-hpaio.so* $(HPLIP_DEST_DIR)/usr/lib/sane
 	cp -R $(TARGET_TOOLCHAIN_STAGING_DIR)/etc/default.hplip $(HPLIP_DEST_DIR)/etc
-	cp -R $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/share/hplip $(HPLIP_DEST_DIR)/usr/share
+	#cp -R $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/share/hplip $(HPLIP_DEST_DIR)/usr/share
 	$(TARGET_STRIP) $@
 
 $(pkg):
