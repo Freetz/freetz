d040c6ee2c6abcd548f1c769da232bd8a74be865
Whoopie <whoopie79@gmx.net>
2009-01-13 00:18:33 +0000

    hp-utils: bump to 0.3.2
    git-svn-id: http://svn.freetz.org/trunk@2983 f5190166-0702-4917-9039-51ec32eddaf5

--- make/hp-utils/hp-utils.mk
+++ make/hp-utils/hp-utils.mk
@@ -1,4 +1,4 @@
-$(call PKG_INIT_BIN,0.3.1)
+$(call PKG_INIT_BIN,0.3.2)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_SITE:=http://www.michaeldenk.de/projects/hp-utils
 $(PKG)_BINARY:=$($(PKG)_DIR)/hp-levels
@@ -20,16 +20,20 @@ $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	cp $(HP_UTILS_DIR)/hp-clean $(HP_UTILS_DEST_DIR)/usr/bin
+	cp $(HP_UTILS_DIR)/hp-faxsetup $(HP_UTILS_DEST_DIR)/usr/bin
 	cp $(HP_UTILS_DIR)/hp-levels $(HP_UTILS_DEST_DIR)/usr/bin
 	cp $(HP_UTILS_DIR)/hp-printserv $(HP_UTILS_DEST_DIR)/usr/bin
 	cp $(HP_UTILS_DIR)/hp-probe $(HP_UTILS_DEST_DIR)/usr/bin
 	cp $(HP_UTILS_DIR)/hp-status $(HP_UTILS_DEST_DIR)/usr/bin
+	cp $(HP_UTILS_DIR)/hp-timedate $(HP_UTILS_DEST_DIR)/usr/bin
 	cp -a $(HP_UTILS_DIR)/libhp-utils.so* $(HP_UTILS_DEST_DIR)/usr/lib
 	$(TARGET_STRIP) $(HP_UTILS_DEST_DIR)/usr/bin/hp-clean \
+		$(HP_UTILS_DEST_DIR)/usr/bin/hp-faxsetup \
 		$(HP_UTILS_DEST_DIR)/usr/bin/hp-levels \
 		$(HP_UTILS_DEST_DIR)/usr/bin/hp-printserv \
 		$(HP_UTILS_DEST_DIR)/usr/bin/hp-probe \
 		$(HP_UTILS_DEST_DIR)/usr/bin/hp-status \
+		$(HP_UTILS_DEST_DIR)/usr/bin/hp-timedate \
 		$(HP_UTILS_DEST_DIR)/usr/lib/libhp-utils.so.$(HP_UTILS_VERSION)
 
 $(pkg):
@@ -41,8 +45,11 @@ $(pkg)-clean:
 
 $(pkg)-uninstall:
 	$(RM) $(HP_UTILS_DEST_DIR)/usr/bin/hp-clean \
+		$(HP_UTILS_DEST_DIR)/usr/bin/hp-faxsetup \
 		$(HP_UTILS_DEST_DIR)/usr/bin/hp-levels \
 		$(HP_UTILS_DEST_DIR)/usr/bin/hp-probe \
-		$(HP_UTILS_DEST_DIR)/usr/bin/hp-status
+		$(HP_UTILS_DEST_DIR)/usr/bin/hp-status \
+		$(HP_UTILS_DEST_DIR)/usr/bin/hp-printserv \
+		$(HP_UTILS_DEST_DIR)/usr/bin/hp-timedate
 
 $(PKG_FINISH)
