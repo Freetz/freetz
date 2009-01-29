6375a44cb114d813bf53bfa598bbcd73c1d71ec7
Oliver Metz <oliver@freetz.org>
2009-01-29 14:43:42 +0000

    * remove_assistant patch: Fix for 7270 Labor AiO and Phone
    git-svn-id: http://svn.freetz.org/trunk@3020 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/220-assistant.sh
+++ patches/220-assistant.sh
@@ -6,12 +6,24 @@ if [ -e "${HTML_LANG_MOD_DIR}/html/de" ];then
 else
 	HTML_DIR="${HTML_LANG_MOD_DIR}/html/en"
 fi
+
+if [ "$FREETZ_TYPE_FON_WLAN_7270" == "y" ] && \
+	[ "$FREETZ_TYPE_LABOR_PHONE" == "y" -o "$FREETZ_TYPE_LABOR_AIO" == "y" ]; then
+	cp ${HTML_DIR}/first/provider.js ${HTML_DIR}/js
+fi
+
 rm_files "${HTML_DIR}/first" \
 	 "${HTML_DIR}/konfig" \
 	 "${HTML_LANG_MOD_DIR}/html/index_assi.html" \
 	 "${HTML_LANG_MOD_DIR}/html/assistent.html"
 find "${HTML_DIR}/menus" -type f |
 	xargs sed -s -i -e '/var:menuAssistent/d'
+
+if [ "$FREETZ_TYPE_FON_WLAN_7270" == "y" ] && \
+	[ "$FREETZ_TYPE_LABOR_PHONE" == "y" -o "$FREETZ_TYPE_LABOR_AIO" == "y" ]; then
+	mkdir -p ${HTML_DIR}/first
+	ln -s ../js/provider.js ${HTML_DIR}/first/provider.js
+fi
 
 if [ -e "$HTML_DIR/home/sitemap.html" ]; then
 	if [ "$FREETZ_HAS_PHONE" == "y" ]; then
