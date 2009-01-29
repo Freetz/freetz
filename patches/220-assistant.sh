0e0992cbf5cbcac640018e95ee40da1bf3ff36c8
Oliver Metz <oliver@freetz.org>
2009-01-29 15:37:46 +0000

    * Add 7240 Labor Phone 73.04.68  * Update remove_assistant patch for 7170 Labor AiO
    git-svn-id: http://svn.freetz.org/trunk@3022 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/220-assistant.sh
+++ patches/220-assistant.sh
@@ -7,7 +7,7 @@ else
 	HTML_DIR="${HTML_LANG_MOD_DIR}/html/en"
 fi
 
-if [ "$FREETZ_TYPE_FON_WLAN_7270" == "y" ] && \
+if [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" -o "$FREETZ_TYPE_FON_WLAN_7240" == "y" -o "$FREETZ_TYPE_FON_WLAN_7270" == "y" ] && \
 	[ "$FREETZ_TYPE_LABOR_PHONE" == "y" -o "$FREETZ_TYPE_LABOR_AIO" == "y" ]; then
 	cp ${HTML_DIR}/first/provider.js ${HTML_DIR}/js
 fi
@@ -19,7 +19,7 @@ rm_files "${HTML_DIR}/first" \
 find "${HTML_DIR}/menus" -type f |
 	xargs sed -s -i -e '/var:menuAssistent/d'
 
-if [ "$FREETZ_TYPE_FON_WLAN_7270" == "y" ] && \
+if [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" -o "$FREETZ_TYPE_FON_WLAN_7240" == "y" -o "$FREETZ_TYPE_FON_WLAN_7270" == "y" ] && \
 	[ "$FREETZ_TYPE_LABOR_PHONE" == "y" -o "$FREETZ_TYPE_LABOR_AIO" == "y" ]; then
 	mkdir -p ${HTML_DIR}/first
 	ln -s ../js/provider.js ${HTML_DIR}/first/provider.js
@@ -27,7 +27,7 @@ fi
 
 if [ -e "$HTML_DIR/home/sitemap.html" ]; then
 	if [ "$FREETZ_HAS_PHONE" == "y" ]; then
-		if [ "$FREETZ_TYPE_FON_WLAN_7270" == "y" -a "$FREETZ_TYPE_LABOR_PHONE" == "y" ]; then
+		if [ "$FREETZ_TYPE_FON_WLAN_7270" == "y" -o "$FREETZ_TYPE_FON_WLAN_7240" == "y" ] && [ "$FREETZ_TYPE_LABOR_PHONE" == "y" ]; then
 			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant_7270_labor_phone.patch"
 		elif [ "$FREETZ_TYPE_FON_WLAN_7270" == "y" -a "$FREETZ_TYPE_LABOR_AIO" == "y" ]; then
                 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant_7270_labor_aio.patch"
