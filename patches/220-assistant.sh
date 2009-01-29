0da002faceeab21e4b5fd822561c11ee9faaa8a4
Oliver Metz <oliver@freetz.org>
2009-01-29 15:17:44 +0000

    * Add 7170 Labor AiO 29.04.70-13475
    git-svn-id: http://svn.freetz.org/trunk@3021 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/220-assistant.sh
+++ patches/220-assistant.sh
@@ -33,8 +33,10 @@ if [ -e "$HTML_DIR/home/sitemap.html" ]; then
                 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant_7270_labor_aio.patch"
 		elif [ "$FREETZ_TYPE_FON_WLAN_7240" == "y" ] || [ "$FREETZ_TYPE_FON_WLAN_7270" == "y" ]; then
 			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant_7270.patch"
-		elif [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" -a "$FREETZ_TYPE_LABOR" == "y" ]; then
+		elif [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" -a "$FREETZ_TYPE_LABOR_DSL" == "y" ]; then
 			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant_7170_labor_dsl.patch"
+		elif [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" -a "$FREETZ_TYPE_LABOR_AIO" == "y" ]; then
+			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant_7170.patch"
 		elif [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" ]; then
 			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant_7170.patch"
 		else
--- patches/450-autorun_autoend.sh
+++ patches/450-autorun_autoend.sh
@@ -1,6 +1,6 @@
 [ "$FREETZ_AUTORUN_AUTOEND" == "y" ] || return 0
 echo1 "patching run_mount and storage: autorun/autoend"
-if [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" -a "$FREETZ_TYPE_LABOR" == "y" ]; then
+if [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" -a "$FREETZ_TYPE_LABOR_DSL" == "y" ]; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/450-autorun-run_mount_7170_labor_dsl.patch"
 elif [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" ] || \
 	[ "$FREETZ_TYPE_FON_WLAN_7240"  == "y" ] || \
@@ -10,7 +10,7 @@ elif [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" ] || \
 else
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/450-autorun-run_mount.patch"
 fi
-if  [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" -a "$FREETZ_TYPE_LABOR" == "y" ]; then
+if  [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" -a "$FREETZ_TYPE_LABOR_DSL" == "y" ]; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/450-autoend-storage_7170_labor_dsl.patch"
 elif [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" ] || \
 	[ "$FREETZ_TYPE_FON_WLAN_7240" == "y" ] || \
