4fa1ab386d7c6c430c26602e14bef174f56addc4
Oliver Metz <oliver@freetz.org>
2009-01-30 19:49:18 +0000

    [freetz-stable-1.0] merge in r2970:r2975, r2982, r2985, r2988, r2996:r2997, r3007:r3008, r3015:r3016, r3020:r3023  * firmware updates:   * 7240 Labor Phone 73.04.70-13483   * 7270 Labor AiO 54.04.70-13486   * 7270 Labor Phone 54.04.97-13483   * 3270 47.04.68  * packages:   * openssl:    * bump to 0.9.8j (security update)    * Fix parallel build issues    * openvpn: updates by MaxMuster   * tor: bump to 0.2.0.33 (security update)  * patches:   * 7170: Fix remove assistant and autorun_autoend patch for Labor DSL   * 7240: Activate online counter too   * remove one reference to FREETZ_TYPE_LABOR_VPN which is not supported anymore, disable "remove mediasrv" for 7240/7270   * remove_assistant patch: Fix for 7270 Labor AiO and Phone  * other:   * fix typo in make/README.Makefiles
    git-svn-id: http://svn.freetz.org/branches/freetz-stable-1.0@3027 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/115-7270_16MB.sh
+++ patches/115-7270_16MB.sh
@@ -5,8 +5,8 @@ if [ "$FREETZ_TYPE_LABOR_DSL" == "y" ];then
 	modpatch "$FIRMWARE_MOD_DIR" "${PATCHES_DIR}/cond/7270_labor_dsl_16MB_flash.patch"
 elif [ "$FREETZ_TYPE_LABOR_PHONE" == "y" ];then
 	modpatch "$FIRMWARE_MOD_DIR" "${PATCHES_DIR}/cond/7270_labor_phone_16MB_flash.patch"
-elif [ "$FREETZ_TYPE_LABOR_UMTS" == "y" ];then
-	modpatch "$FIRMWARE_MOD_DIR" "${PATCHES_DIR}/cond/7270_labor_umts_16MB_flash.patch"
+elif [ "$FREETZ_TYPE_LABOR_AIO" == "y" ];then
+	modpatch "$FIRMWARE_MOD_DIR" "${PATCHES_DIR}/cond/7270_labor_aio_16MB_flash.patch"
 else
 	modpatch "$FIRMWARE_MOD_DIR" "${PATCHES_DIR}/cond/7270_16MB_flash.patch"
 fi
--- patches/220-assistant.sh
+++ patches/220-assistant.sh
@@ -6,6 +6,12 @@ if [ -e "${HTML_LANG_MOD_DIR}/html/de" ];then
 else
 	HTML_DIR="${HTML_LANG_MOD_DIR}/html/en"
 fi
+
+if [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" -o "$FREETZ_TYPE_FON_WLAN_7240" == "y" -o "$FREETZ_TYPE_FON_WLAN_7270" == "y" ] && \
+	[ "$FREETZ_TYPE_LABOR_PHONE" == "y" -o "$FREETZ_TYPE_LABOR_AIO" == "y" ]; then
+	cp ${HTML_DIR}/first/provider.js ${HTML_DIR}/js
+fi
+
 rm_files "${HTML_DIR}/first" \
 	 "${HTML_DIR}/konfig" \
 	 "${HTML_LANG_MOD_DIR}/html/index_assi.html" \
@@ -13,16 +19,34 @@ rm_files "${HTML_DIR}/first" \
 find "${HTML_DIR}/menus" -type f |
 	xargs sed -s -i -e '/var:menuAssistent/d'
 
+if [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" -o "$FREETZ_TYPE_FON_WLAN_7240" == "y" -o "$FREETZ_TYPE_FON_WLAN_7270" == "y" ] && \
+	[ "$FREETZ_TYPE_LABOR_PHONE" == "y" -o "$FREETZ_TYPE_LABOR_AIO" == "y" ]; then
+	mkdir -p ${HTML_DIR}/first
+	ln -s ../js/provider.js ${HTML_DIR}/first/provider.js
+fi
+
 if [ -e "$HTML_DIR/home/sitemap.html" ]; then
 	if [ "$FREETZ_HAS_PHONE" == "y" ]; then
-		if [ "$FREETZ_TYPE_FON_WLAN_7240" == "y" ] || [ "$FREETZ_TYPE_FON_WLAN_7270" == "y" ]; then
+		if [ "$FREETZ_TYPE_FON_WLAN_7270" == "y" -o "$FREETZ_TYPE_FON_WLAN_7240" == "y" ] && [ "$FREETZ_TYPE_LABOR_PHONE" == "y" ]; then
+			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant_7270_labor_phone.patch"
+		elif [ "$FREETZ_TYPE_FON_WLAN_7270" == "y" -a "$FREETZ_TYPE_LABOR_AIO" == "y" ]; then
+                	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant_7270_labor_aio.patch"
+		elif [ "$FREETZ_TYPE_FON_WLAN_7240" == "y" ] || [ "$FREETZ_TYPE_FON_WLAN_7270" == "y" ]; then
 			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant_7270.patch"
+		elif [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" -a "$FREETZ_TYPE_LABOR_DSL" == "y" ]; then
+			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant_7170_labor_dsl.patch"
+		elif [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" -a "$FREETZ_TYPE_LABOR_AIO" == "y" ]; then
+			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant_7170.patch"
 		elif [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" ]; then
 			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant_7170.patch"
 		else
 			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant.patch"
 		fi
 	else	
-		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant_wop.patch"
+		if [ "$FREETZ_TYPE_WLAN_3270" == "y" ]; then
+			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant_3270.patch"
+		else
+			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant_wop.patch"
+		fi
 	fi
 fi
