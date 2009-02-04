c4a37682826015d315048d23042103444848055f
Oliver Metz <oliver@freetz.org>
2009-02-04 07:25:40 +0000

    * Add nmap portscanner (binary only)   * Patches taken from NSLU2   * Newer versions don't finish tests on my box
    git-svn-id: http://svn.freetz.org/trunk@3042 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/nmap/nmap.mk
@@ -0,0 +1,49 @@
+$(call PKG_INIT_BIN, 4.68)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SITE:=http://nmap.org/dist
+$(PKG)_BINARY:=$($(PKG)_DIR)/nmap
+$(PKG)_SERVICES_LIST:=$($(PKG)_DIR)/nmap-services
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/nmap
+$(PKG)_TARGET_SERVICES_LIST:=$($(PKG)_DEST_DIR)/usr/share/nmap/nmap-services
+
+$(PKG)_DEPENDS_ON := uclibcxx libpcap
+
+$(PKG)_CONFIGURE_ENV += PKG_CONFIG_PATH="$(TARGET_MAKE_PATH)/../usr/lib/pkgconfig"
+
+$(PKG)_CONFIGURE_OPTIONS += --with-liblua=included
+$(PKG)_CONFIGURE_OPTIONS += --with-pcap=linux
+$(PKG)_CONFIGURE_OPTIONS += --with-pcre=included
+$(PKG)_CONFIGURE_OPTIONS += --without-openssl
+$(PKG)_CONFIGURE_OPTIONS += --with-pcap=linux
+$(PKG)_CONFIGURE_OPTIONS += --with-nmapfe=no
+$(PKG)_CONFIGURE_OPTIONS += --without-zenmap
+
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY) $($(PKG)_SERVICES_LIST): $($(PKG)_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+		$(MAKE) -C $(NMAP_DIR)
+	touch $(NMAP_SERVICES_LIST)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$($(PKG)_TARGET_SERVICES_LIST): $($(PKG)_SERVICES_LIST)
+	mkdir -p $(dir $@)
+	cp $^ $@
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_TARGET_SERVICES_LIST)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(NMAP_DIR) clean
+	$(RM) $(NMAP_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(NMAP_TARGET_BINARY)
+
+$(PKG_FINISH)
