937710f1e9de2332534ff573eee63b273bd83a3b
Lars Falkenhain <lars.falkenhain@gmail.com>
2009-02-08 17:59:33 +0000

    enables 7170 Labor AiO for Speedport W900V & Speedport W701V
    git-svn-id: http://svn.freetz.org/trunk@3051 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/100-w900v_7170.sh
+++ patches/100-w900v_7170.sh
@@ -23,6 +23,8 @@ if [ ! "$FREETZ_REMOVE_DECT" == "y" ];then
 	#Test if a beta-image or a labor-image is used and use another patchfile for them
 	if [ "$FREETZ_TYPE_LABOR_DSL" == "y" ];then
 		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/sp2fritz-W900V_7170_labor_dsl.patch"
+	elif [ "$FREETZ_TYPE_LABOR_AIO" == "y" ];then
+                modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/sp2fritz-W900V_7170_labor_aio.patch"
 	else
 		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/sp2fritz-W900V_7170.patch"
 	fi
