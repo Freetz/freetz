a22a35e0fa55eea946e6c8c24905fd412d143e44
Oliver Metz <oliver@freetz.org>
2009-03-02 21:50:40 +0000

    * Add ruby-fcgi package (by mehle)
    git-svn-id: http://svn.freetz.org/trunk@3118 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/ruby-fcgi/ruby-fcgi.mk
@@ -0,0 +1,26 @@
+$(call PKG_INIT_BIN, 0.8.7)
+$(PKG)_SOURCE:=ruby-fcgi-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=http://www.moonwolf.com/ruby/archive
+$(PKG)_BINARY:=$(RUBY_FCGI_DIR)/lib/fcgi.rb
+$(PKG)_TARGET_BINARY:=$(RUBY_FCGI_DEST_DIR)/usr/lib/ruby/1.8/fcgi.rb
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	mkdir -p $(dir $@)
+	cp $^ $@
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+
+$(pkg)-uninstall: 
+	$(RM) $(RUBY_FCGI_TARGET_BINARY)
+
+$(PKG_FINISH)
