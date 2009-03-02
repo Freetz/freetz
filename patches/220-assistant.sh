f269f114b3200b6024d7090ca2e93f8940524a6d
Oliver Metz <oliver@freetz.org>
2009-03-02 22:35:08 +0000

    * Fix remove assistant and volumecounter patch for 7240 (broken sinc 7270 54.04.70)   * addresses #390
    git-svn-id: http://svn.freetz.org/trunk@3120 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/220-assistant.sh
+++ patches/220-assistant.sh
@@ -21,7 +21,9 @@ if [ -e "$HTML_DIR/home/sitemap.html" ]; then
 	if [ "$FREETZ_HAS_PHONE" == "y" ]; then
 		if [ "$FREETZ_TYPE_FON_WLAN_7270" == "y" -o "$FREETZ_TYPE_FON_WLAN_7240" == "y" ] && [ "$FREETZ_TYPE_LABOR_PHONE" == "y" ]; then
 			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant_7270_labor_phone.patch"
-		elif [ "$FREETZ_TYPE_FON_WLAN_7240" == "y" ] || [ "$FREETZ_TYPE_FON_WLAN_7270" == "y" ]; then
+		elif [ "$FREETZ_TYPE_FON_WLAN_7240" == "y" ]; then
+			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/$FREETZ_TYPE_LANG_STRING/remove_assistant_7240.patch"
+		elif [ "$FREETZ_TYPE_FON_WLAN_7270" == "y" ]; then
 			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/$FREETZ_TYPE_LANG_STRING/remove_assistant_7270.patch"
 		elif [ "$FREETZ_TYPE_FON_WLAN_7170" == "y" -a "$FREETZ_TYPE_LABOR_DSL" == "y" ]; then
 			modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_assistant_7170_labor_dsl.patch"
