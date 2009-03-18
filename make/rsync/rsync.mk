09af3fd8b3c8187784cf7b63cab1b1fc2b449ffa
Whoopie <whoopie79@gmx.net>
2009-03-18 19:24:41 +0000

    rsync: initial commit, version 1.0.5 (closes #394)
    git-svn-id: http://svn.freetz.org/trunk@3181 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/rsync/rsync.mk
@@ -0,0 +1,37 @@
+$(call PKG_INIT_BIN, 3.0.5)
+$(PKG)_SOURCE:=rsync-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=http://samba.anu.edu.au/ftp/rsync
+$(PKG)_BINARY:=$($(PKG)_DIR)/rsync
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/rsync
+
+$(PKG)_DEPENDS_ON := popt
+
+$(PKG)_CONFIGURE_OPTIONS += --disable-iconv
+$(PKG)_CONFIGURE_OPTIONS += --disable-locale
+$(PKG)_CONFIGURE_OPTIONS += --without-included-popt
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_TARGET_IPV6_SUPPORT),,--disable-ipv6)
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+		$(MAKE) -C $(RSYNC_DIR) \
+		CC="$(TARGET_CC)"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(RSYNC_DIR) clean
+	$(RM) $(RSYNC_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(RSYNC_TARGET_BINARY)
+
+$(PKG_FINISH)
