416819ae9feb0aabe2439d40cf05147938f72a9d
Oliver Metz <oliver@freetz.org>
2009-04-01 09:15:03 +0000

    * Simplify 7270 16 MB flash patch
    git-svn-id: http://svn.freetz.org/trunk@3223 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/115-7270_16MB.sh
+++ patches/115-7270_16MB.sh
@@ -1,12 +1,5 @@
 [ "$FREETZ_TYPE_FON_WLAN_7270_16MB" == "y" -a "$FREETZ_TYPE_LANG_DE" == "y" ] || return 0
 
 echo1 "applying 16MB patch"
-if [ "$FREETZ_TYPE_LABOR_DSL" == "y" ];then
-	modpatch "$FIRMWARE_MOD_DIR" "${PATCHES_DIR}/cond/7270_labor_dsl_16MB_flash.patch"
-elif [ "$FREETZ_TYPE_LABOR_PHONE" == "y" ];then
-	modpatch "$FIRMWARE_MOD_DIR" "${PATCHES_DIR}/cond/7270_labor_phone_16MB_flash.patch"
-elif [ "$FREETZ_TYPE_LABOR_WLAN" == "y" ];then
-	modpatch "$FIRMWARE_MOD_DIR" "${PATCHES_DIR}/cond/7270_labor_wlan_16MB_flash.patch"
-else
-	modpatch "$FIRMWARE_MOD_DIR" "${PATCHES_DIR}/cond/7270_16MB_flash.patch"
-fi
+modpatch "$FIRMWARE_MOD_DIR" "${PATCHES_DIR}/cond/${FREETZ_TYPE_STRING}_16MB_flash.patch"
+
