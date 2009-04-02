9753b0732c58a81637d96cc8f83cf856fbcff730
Oliver Metz <oliver@freetz.org>
2009-04-02 14:31:15 +0000

    * 7270 16MB patch: Replace patch files with sed command (by RalfFriedl)
    git-svn-id: http://svn.freetz.org/trunk@3230 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/115-7270_16MB.sh
+++ patches/115-7270_16MB.sh
@@ -1,5 +1,8 @@
 [ "$FREETZ_TYPE_FON_WLAN_7270_16MB" == "y" -a "$FREETZ_TYPE_LANG_DE" == "y" ] || return 0
 
 echo1 "applying 16MB patch"
-modpatch "$FIRMWARE_MOD_DIR" "${PATCHES_DIR}/cond/${FREETZ_TYPE_STRING}_16MB_flash.patch"
-
+sed -i \
+	-e 's/kernel_start=0x90010000/kernel_start=0x90020000/' \
+	-e 's/kernel_size=7798784/kernel_size=16121856/' \
+	-e 's/urlader_size=65536/urlader_size=131072/' \
+	"${FIRMWARE_MOD_DIR}/var/install"
