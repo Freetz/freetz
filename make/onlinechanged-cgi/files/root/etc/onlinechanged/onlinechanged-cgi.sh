003d945aa2049a1ec4bbebaebe9fc7c367c50b70
Oliver Metz <oliver@freetz.org>
2009-04-03 15:46:21 +0000

    * Add new package onlinechanged-cgi   * This is a webinterface to execute own scripts/programs when online status has changed   * by mehle
    git-svn-id: http://svn.freetz.org/trunk@3240 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/onlinechanged-cgi/files/root/etc/onlinechanged/onlinechanged-cgi.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+PATH=/bin:/usr/bin:/sbin:/usr/sbin
+
+DAEMON=onlinechanged-cgi
+
+if [ -f /tmp/flash/$DAEMON ]; then
+case "$1" in
+	online )
+		(sleep 5; sh /tmp/flash/$DAEMON online)&
+	;;
+	offline )
+		(sleep 5; sh /tmp/flash/$DAEMON offline)&
+	;;
+esac
+fi
