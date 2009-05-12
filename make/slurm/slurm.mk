3cd41dacbae0eef085f9617f28d826ae8d01c458
Oliver Metz <oliver@freetz.org>
2009-05-12 23:39:00 +0000

    * Add slurm binary only (realtime network interface monitor)
    git-svn-id: http://svn.freetz.org/trunk@3330 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/slurm/slurm.mk
@@ -0,0 +1,32 @@
+$(call PKG_INIT_BIN, 0.3.3)
+$(PKG)_SOURCE:=slurm-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=http://www.wormulon.net/slurm
+$(PKG)_BINARY:=$($(PKG)_DIR)/slurm
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/slurm
+
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+		$(MAKE) -C $(SLURM_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(SLURM_DIR) clean
+	$(RM) $(SLURM_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(SLURM_TARGET_BINARY)
+
+$(PKG_FINISH)
