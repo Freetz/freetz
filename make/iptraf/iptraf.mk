6c61711e8ac840dd9b1848ba4cb4b3bcde8258fa
Oliver Metz <oliver@freetz.org>
2009-05-13 20:27:10 +0000

    * Add iptraf binary only package (by reiffert)
    git-svn-id: http://svn.freetz.org/trunk@3332 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/iptraf/iptraf.mk
@@ -0,0 +1,34 @@
+$(call PKG_INIT_BIN, 3.0.0)
+$(PKG)_SOURCE:=iptraf-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=ftp://iptraf.seul.org/pub/iptraf
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/iptraf
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/iptraf
+
+$(PKG)_DEPENDS_ON := ncurses
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+		$(MAKE) -C $(IPTRAF_DIR)/src \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)" \
+		INCLUDEDIR="-I../support"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(IPTRAF_DIR)/src clean
+	$(RM) $(IPTRAF_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(IPTRAF_TARGET_BINARY)
+
+$(PKG_FINISH)
