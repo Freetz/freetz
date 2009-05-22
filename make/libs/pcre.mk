6c76be7a9e606f5fecad53daa653dd2e04a9fde7
Oliver Metz <oliver@freetz.org>
2009-05-22 18:59:21 +0000

    * libpcre: Build libcreposix too addresses #451, by er13
    git-svn-id: http://svn.freetz.org/trunk@3365 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/pcre.mk
+++ make/libs/pcre.mk
@@ -1,10 +1,18 @@
 $(call PKG_INIT_LIB, 7.8)
-$(PKG)_LIB_VERSION:=0.0.1
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_SITE:=ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre
-$(PKG)_BINARY:=$($(PKG)_DIR)/.libs/libpcre.so.$($(PKG)_LIB_VERSION)
-$(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcre.so.$($(PKG)_LIB_VERSION)
-$(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libpcre.so.$($(PKG)_LIB_VERSION)
+
+$(PKG)_LIB_VERSION:=0.0.1
+$(PKG)_LIBNAME=libpcre.so.$($(PKG)_LIB_VERSION)
+$(PKG)_BINARY:=$($(PKG)_DIR)/.libs/$($(PKG)_LIBNAME)
+$(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/$($(PKG)_LIBNAME)
+$(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/$($(PKG)_LIBNAME)
+
+$(PKG)_POSIX_LIB_VERSION:=0.0.0
+$(PKG)_POSIX_LIBNAME=libpcreposix.so.$($(PKG)_POSIX_LIB_VERSION)
+$(PKG)_POSIX_BINARY:=$($(PKG)_DIR)/.libs/$($(PKG)_POSIX_LIBNAME)
+$(PKG)_POSIX_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/$($(PKG)_POSIX_LIBNAME)
+$(PKG)_POSIX_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/$($(PKG)_POSIX_LIBNAME)
 
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
@@ -16,27 +24,32 @@ $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
-$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+$($(PKG)_BINARY) $($(PKG)_POSIX_BINARY): $($(PKG)_DIR)/.configured
 	PATH=$(TARGET_PATH) \
 		$(MAKE) -C $(PCRE_DIR) \
 		CFLAGS="$(TARGET_CFLAGS)"\
 		all
 
-$($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
+$($(PKG)_STAGING_BINARY) $($(PKG)_POSIX_STAGING_BINARY): $($(PKG)_BINARY) $($(PKG)_POSIX_BINARY)
 	PATH=$(TARGET_PATH) $(MAKE) \
 		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
 		-C $(PCRE_DIR) install
 	$(PKG_FIX_LIBTOOL_LA) \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcre.la \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcreposix.la \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/pcre-config
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
-	cp -a $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcre*.so* $(PCRE_TARGET_DIR)/
+	cp -a $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcre.so* $(PCRE_TARGET_DIR)/
 	$(TARGET_STRIP) $@
 
-$(pkg): $($(PKG)_STAGING_BINARY)
+$($(PKG)_POSIX_TARGET_BINARY): $($(PKG)_POSIX_STAGING_BINARY)
+	cp -a $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcreposix.so* $(PCRE_TARGET_DIR)/
+	$(TARGET_STRIP) $@
 
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+$(pkg): $($(PKG)_STAGING_BINARY) $($(PKG)_POSIX_STAGING_BINARY)
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_POSIX_TARGET_BINARY)
 
 $(pkg)-clean:
 	-$(MAKE) -C $(PCRE_DIR) clean
