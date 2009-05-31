c7f261c6abb928962e650fe7d1a22b75bce3b7c5
Lars Falkenhain <lars.falkenhain@gmail.com>
2009-05-31 16:06:54 +0000

    * fix removal of igdd/upnp for non-aio-firmwares by markuschen (refs #445)
    git-svn-id: http://svn.freetz.org/trunk@3378 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/290-replace_websrv-remove_igdd.sh
+++ patches/290-replace_websrv-remove_igdd.sh
@@ -5,8 +5,7 @@ fi
 
 if [ "$FREETZ_REMOVE_UPNP" == "y" ]; then
 	echo1 "removing AVM UPnP daemon (igdd or upnpd)"
-	rm_files $(ls -1 ${FILESYSTEM_MOD_DIR}/sbin/igdd) \
-		 $(ls -l ${FILESYSTEM_MOD_DIR}/sbin/upnpd) \
+	rm_files $(find ${FILESYSTEM_MOD_DIR}/sbin -maxdepth 1 -type f -name upnpd -o -name igdd | xargs) \
 		 $(find ${FILESYSTEM_MOD_DIR}/etc -maxdepth 1 -type d -name 'default.*' | xargs -I{} find {} -name 'any.xml' -o -name 'fbox*.xml') \
 		 $(find ${FILESYSTEM_MOD_DIR}/etc -maxdepth 1 -type d -name 'default.*' | xargs -I{} find {} -name '*igd*')
 	if [ "$FREETZ_REMOVE_UPNP_LIBS" == "y" ]; then
