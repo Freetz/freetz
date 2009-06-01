f366d190b0c84e8153079118e824e5752a17eb7f
Lars Falkenhain <lars.falkenhain@gmail.com>
2009-06-01 12:40:00 +0000

    * OpenVPN : Version bump to 2.1_rc17 by er13 * vsftpd: Version bump to 2.1.2 by er13 (closes #450)
    git-svn-id: http://svn.freetz.org/trunk@3382 f5190166-0702-4917-9039-51ec32eddaf5

--- make/vsftpd/vsftpd.mk
+++ make/vsftpd/vsftpd.mk
@@ -1,4 +1,4 @@
-$(call PKG_INIT_BIN, 2.1.0)
+$(call PKG_INIT_BIN, 2.1.2)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=ftp://vsftpd.beasts.org/users/cevans
 $(PKG)_BINARY:=$($(PKG)_DIR)/vsftpd
