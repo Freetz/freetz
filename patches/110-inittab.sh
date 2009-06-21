ae0de4086998790c7aedec76fecf411a98fa0313
Oliver Metz <oliver@freetz.org>
2009-06-21 17:53:49 +0000

    * patches: Use our own inittab for all boxes
    git-svn-id: http://svn.freetz.org/trunk@3423 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/110-inittab.sh
+++ patches/110-inittab.sh
@@ -1,9 +1,10 @@
 cat << 'EOF' > "${FILESYSTEM_MOD_DIR}/etc/inittab"
 #
+::restart:/sbin/init
 ::sysinit:/etc/init.d/rc.S
 
 # Start an "askfirst" shell on the console (whatever that may be)
-::askfirst:-/bin/sh
+ttyS0::askfirst:-/bin/sh
 
 # Stuff to do before rebooting
 ::shutdown:/bin/sh -c /var/post_install
