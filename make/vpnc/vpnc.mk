9a7757778cf405c9c06fbfe3e8e7edeb11e61322
Oliver Metz <oliver@freetz.org>
2009-06-21 17:42:19 +0000

    * vpnc: Add support for NAT (by markuschen)   * fixes #214
    git-svn-id: http://svn.freetz.org/trunk@3422 f5190166-0702-4917-9039-51ec32eddaf5

--- make/vpnc/vpnc.mk
+++ make/vpnc/vpnc.mk
@@ -8,6 +8,7 @@ $(PKG)_STARTLEVEL=40
 $(PKG)_DEPENDS_ON := libgpg-error libgcrypt
 
 $(PKG)_CONFIG_SUBOPTS += FREETZ_PACKAGE_VPNC_WITH_HYBRID_AUTH
+$(PKG)_CONFIG_SUBOPTS += FREETZ_PACKAGE_VPNC_NAT_SUPPORT
 
 ifeq ($(strip $(FREETZ_PACKAGE_VPNC_WITH_HYBRID_AUTH)),y)
 VPNC_CPPFLAGS := -DOPENSSL_GPL_VIOLATION
@@ -33,9 +34,15 @@ $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
 
+$(PKG)_NAT_SUPPORT: $(PACKAGES_DIR)/.$(pkg)-$($(PKG)_VERSION)
+ifeq ($(strip $(FREETZ_PACKAGE_VPNC_NAT_SUPPORT)),y)  
+	@$(SED) -i -e 's/#start_vpn_nat/start_vpn_nat/g' $(VPNC_DEST_DIR)/etc/default.vpnc/vpnc-script
+	@$(SED) -i -e 's/#stop_vpn_nat/stop_vpn_nat/g' $(VPNC_DEST_DIR)/etc/default.vpnc/vpnc-script
+endif  
+
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $(PKG)_NAT_SUPPORT
 
 $(pkg)-clean:
 	-$(MAKE) -C $(VPNC_DIR) clean
