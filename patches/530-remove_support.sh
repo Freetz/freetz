d000b3f6463f7436865ce43906a06e7ba7fc9690
Oliver Metz <oliver@freetz.org>
2009-06-21 17:20:46 +0000

    * Add patch to remove AVM support tools, saves around 20kb compressed. But there are different savings for different firmwares.   * fixes #459, by markuschen
    git-svn-id: http://svn.freetz.org/trunk@3421 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ patches/530-remove_support.sh
@@ -0,0 +1,6 @@
+[ "$FREETZ_REMOVE_SUPPORT" == "y" ] || return 0
+echo1 "remove support files"
+rm_files $(find ${FILESYSTEM_MOD_DIR}/sbin -maxdepth 1 -name eventsdump \
+		-o -name showdsldstat -o -name showaddrs -o -name showroutes -o -name brctl | xargs) \
+	 "${FILESYSTEM_MOD_DIR}/bin/supportdata*" \
+	 "${HTML_LANG_MOD_DIR}/html/support*.html"
