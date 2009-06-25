31c557d25dadd223371d30a88f220dfda12a29b7
Oliver Metz <oliver@freetz.org>
2009-06-25 07:58:19 +0000

    * fix typo in remove tr069 patch
    git-svn-id: http://svn.freetz.org/trunk@3442 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/260-remove_tr069.sh
+++ patches/260-remove_tr069.sh
@@ -1,7 +1,7 @@
 [ "$FREETZ_REMOVE_TR069" == "y" ] || return 0
 
 echo1 "removing tr069 stuff"
-rm_files "${FILESYSTEM_MOD_DIR}/usr/share/ctlmgr/libtr064.so"
+rm_files "${FILESYSTEM_MOD_DIR}/usr/share/ctlmgr/libtr064.so" \
 	 "${FILESYSTEM_MOD_DIR}/usr/share/ctlmgr/libtr069.so" \
 	 "${FILESYSTEM_MOD_DIR}/bin/tr069starter" \
 	 "${FILESYSTEM_MOD_DIR}/sbin/tr069discover" \
