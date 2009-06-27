40cce1816e975350ba396f81f6f5a21de481bed8
Lars Falkenhain <lars.falkenhain@gmail.com>
2009-06-27 11:20:41 +0000

    * 7240: Version bump to 73.04.76 * patches-subdir: remove of unneeded labor-firmwares for the 7240
    git-svn-id: http://svn.freetz.org/trunk@3445 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/470-volumecounter.sh
+++ patches/470-volumecounter.sh
@@ -1,7 +1,4 @@
-[ "$FREETZ_TYPE_FON_WLAN_7240" == "y" \
-	-o "$FREETZ_TYPE_FON_WLAN_7270" == "y" \
-	-a "$FREETZ_TYPE_LANG_DE" == "y" ] && ! [ "$FREETZ_TYPE_LABOR_AIO" == "y" ] \
-  || return 0
+[ "$FREETZ_TYPE_FON_WLAN_7270" == "y" -a "$FREETZ_TYPE_LANG_DE" == "y" ] && ! [ "$FREETZ_TYPE_LABOR_AIO" == "y" ] || return 0
 
 echo1 "adding volume counter"
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/470-volumecounter_${FREETZ_TYPE_STRING}.patch"
