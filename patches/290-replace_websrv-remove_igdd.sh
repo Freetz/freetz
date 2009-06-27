97583333a73053de03550fcd39771fe1eda3cee5
Lars Falkenhain <lars.falkenhain@gmail.com>
2009-06-27 15:20:30 +0000

    * another fix for 7240 (hopefully the last)
    git-svn-id: http://svn.freetz.org/trunk@3448 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/290-replace_websrv-remove_igdd.sh
+++ patches/290-replace_websrv-remove_igdd.sh
@@ -1,19 +1,40 @@
-if [ "$FREETZ_REMOVE_WEBSRV" == "y" ]; then
-	echo1 "removing AVM websrv"
-	rm_files $(ls -1 "${FILESYSTEM_MOD_DIR}/sbin/websrv")
-fi
-
-if [ "$FREETZ_REMOVE_UPNP" == "y" ]; then
-	echo1 "removing AVM UPnP daemon (igdd or upnpd)"
-	rm_files $(find ${FILESYSTEM_MOD_DIR}/sbin -maxdepth 1 -type f -name upnpd -o -name igdd | xargs) \
-		 $(find ${FILESYSTEM_MOD_DIR}/etc -maxdepth 1 -type d -name 'default.*' | xargs -I{} find {} -name 'any.xml' -o -name 'fbox*.xml') \
-		 $(find ${FILESYSTEM_MOD_DIR}/etc -maxdepth 1 -type d -name 'default.*' | xargs -I{} find {} -name '*igd*')
-	if [ "$FREETZ_REMOVE_UPNP_LIBS" == "y" ]; then
-		rm_files $(ls -1 ${FILESYSTEM_MOD_DIR}/lib/libavmupnp*)
-	fi
-fi
-
-if [ "$FREETZ_REMOVE_WEBSRV" == "y" ] && [ "$FREETZ_REMOVE_UPNP_LIBS" == "y" ]; then
-	echo1 "removing libwebsrv.so"
-	rm_files $(ls -1 ${FILESYSTEM_MOD_DIR}/lib/libwebsrv.so*)
-fi
+--- etc/init.d/rc.net.orig	2008-12-17 22:22:17.000000000 +0100
++++ etc/init.d/rc.net	2008-12-17 22:22:17.000000000 +0100
+@@ -57,9 +57,25 @@
+    WLAN_TEST=0
+ fi
+ 
++# Do we have a UPnP server (upnpd) or was ist stripped from the firmware?
++_upnpd=$(basename $(which upnpd) 2> /dev/null)
++# Does a multid option to start without UPnP device (-U) exist?
++_multid_upnp=$(/sbin/multid -? 2>&1 | grep upnp)
++# Does a dsld option to start without igd (-g) exist?
++_dsld_upnp=$(/sbin/dsld -? 2>&1 | grep igd)
++# Set multid "no UPnP" option, if
++#   a) it has the parameter at all AND
++#   b) upnpd binary does *not* exist
++[ "$_multid_upnp" ] && [ ! "$_upnpd" ] && MULTIDPARAM="$MULTIDPARAM -U"
++# Set dsld "no igd" option, if
++#   a) it has the parameter at all AND
++#   b) upnpd binary does *not* exist
++[ "$_dsld_upnp" ] && [ ! "$_upnpd" ] && DSLDDPARAM="-g"
++
+ 
+ upnpdstart()
+ {
++	 [ "$_upnpd" ] || return;
+    if [ "`pidof upnpd`" = "" ] ; then
+       upnpd $VERBOSEPARAM
+       sleep 1
+@@ -285,9 +301,9 @@
+    if [ "$CONFIG_DSL" ] ; then
+      if [ "`pidof dsld`" = "" ] ; then
+        if [ "$CONFIG_RAMSIZE" = 8 ] ; then
+-         dsld -i -n $NICEPARAM -r 600 $VERBOSEPARAM
++         dsld -i -n $NICEPARAM -r 600 $VERBOSEPARAM $DSLDDPARAM
+        else
+-         dsld -i -n $NICEPARAM $VERBOSEPARAM
++         dsld -i -n $NICEPARAM $VERBOSEPARAM $DSLDDPARAM
+        fi
+      fi
+    fi
