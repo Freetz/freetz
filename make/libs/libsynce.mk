043cf0c152dd8d1a4bb6171b53e0ae26803a0172
Whoopie <whoopie79@gmx.net>
2009-06-30 12:31:26 +0000

    libsynce: add helper library for SynCE, a framework to sync WinCE devices (closes #468)
    git-svn-id: http://svn.freetz.org/trunk@3466 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/libs/libsynce.mk
@@ -0,0 +1,48 @@
+$(call PKG_INIT_LIB, 0.10.0)
+$(PKG)_LIB_VERSION:=0.0.0
+$(PKG)_SOURCE:=synce-$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=@SF/synce
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/.libs/$(pkg).so.$($(PKG)_LIB_VERSION)
+$(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/$(pkg).so.$($(PKG)_LIB_VERSION)
+$(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/$(pkg).so.$($(PKG)_LIB_VERSION)
+
+
+$(PKG)_CONFIGURE_OPTIONS += --enable-shared
+$(PKG)_CONFIGURE_OPTIONS += --enable-static
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	PATH=$(TARGET_PATH) \
+		$(MAKE) -C $(LIBSYNCE_DIR) \
+		all
+
+$($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
+	PATH=$(TARGET_PATH) \
+		$(MAKE) -C $(LIBSYNCE_DIR) \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+		install
+	$(PKG_FIX_LIBTOOL_LA) \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libsynce.la \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libsynce.pc
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
+	cp -a $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libsynce*.so* $(LIBSYNCE_TARGET_DIR)/
+	$(TARGET_STRIP) $@
+
+$(pkg): $($(PKG)_STAGING_BINARY)
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(LIBSYNCE_DIR) clean
+	$(RM) $(TARGET_TOOLCHAIN_STAGING_DIR)/lib/libsynce* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/pkgconfig/libsynce.pc \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/include/synce*.h
+
+$(pkg)-uninstall:
+	$(RM) $(LIBSYNCE_TARGET_DIR)/libsynce*.so*
+
+$(PKG_FINISH)
