1b46d1ab276dd0eabb853764cf31a55afb3cdec2
Lars Falkenhain <lars.falkenhain@gmail.com>
2009-07-30 14:29:27 +0000

    patches: fix typos
    git-svn-id: http://svn.freetz.org/trunk@3516 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/200-vcc.sh
+++ patches/200-vcc.sh
@@ -5,7 +5,7 @@ if [ -e "${HTML_LANG_MOD_DIR}/html/de" ];then
 else
 	HTML_DIR="${HTML_LANG_MOD_DIR}/html/en"
 fi
-if ifFreetzType W501V W701V W901V; then
+if isFreetzType W501V W701V W900V; then
 	sed -i -e "s/avme/tcom/g" "${HTML_DIR}/fon/sipoptionen.js"
 else
 	sed -i -e "s/avme/1und1/g" "${HTML_DIR}/fon/sipoptionen.js"
