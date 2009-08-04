5494868f7448b91e6a7861601ae5d1b331ce88ce
Oliver Metz <oliver@freetz.org>
2009-08-04 19:16:14 +0000

    * Add new package: fortune (by zyrill)
    git-svn-id: http://svn.freetz.org/trunk@3540 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/fortune/fortune.mk
@@ -0,0 +1,32 @@
+$(call PKG_INIT_BIN, 1.2)
+$(PKG)_SOURCE:=fortune.fb-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SITE:=http://homepages.tu-darmstadt.de/~pkrueger
+$(PKG)_BINARY:=$($(PKG)_DIR)/fortune
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/fortune
+
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+		$(MAKE) -C $(FORTUNE_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(FORTUNE_DIR) clean
+	$(RM) $(FORTUNE_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(FORTUNE_TARGET_BINARY)
+
+$(PKG_FINISH)
