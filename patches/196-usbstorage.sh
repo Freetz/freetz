6ca4a78eef303f5bb78f93b2693a6762d83167ae
Oliver Metz <oliver@freetz.org>
2009-08-08 09:38:01 +0000

    * Bump 7150 to 38.04.71
    git-svn-id: http://svn.freetz.org/trunk@3545 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/196-usbstorage.sh
+++ patches/196-usbstorage.sh
@@ -1,12 +1,12 @@
 [ "$FREETZ_PATCH_USBSTORAGE" == "y" ] || return 0
 echo1 "applying USB storage patch"
-if isFreetzType 2170 3131 7150 W900V; then
+if isFreetzType 2170 3131 W900V; then
 		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage_3170.patch"
 elif isFreetzType 3130; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage_wotam.patch"
 elif isFreetzType 7141 7170 && isFreetzType LANG_A_CH; then
 		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage_7170_a_ch.patch"
-elif isFreetzType 3170 3270 7141 7170 7240 7270 7270_V3; then
+elif isFreetzType 3170 3270 7141 7150 7170 7240 7270 7270_V3; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/usbstorage_${FREETZ_TYPE_STRING}.patch"
 elif isFreetzType 7140; then
 	if isFreetzType LANG_A_CH; then
