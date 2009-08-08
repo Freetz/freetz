23e830d5ae6fdb324e2a869a915a5cd3854c2bbb
Markus Hempel <info@markushempel.de>
2009-08-08 18:43:26 +0000

    * New feature: remove only voipd, not the complete telephonie functionalty * dect-remove: faster code and additional remove of lib/libfoncclient.so* when REMOVE_MINID is selected
    git-svn-id: http://svn.freetz.org/trunk@3551 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ patches/331-remove-voipd.sh
@@ -0,0 +1,21 @@
+[ "$FREETZ_REMOVE_VOIPD" == "y" ] || return 0
+
+echo1 "removing VoIP files"
+for files in bin/voipd bin/showvoipdstat lib/libsiplib.so* lib/librtpstream*.so* \
+	lib/libosip*2.so* usr/www/all/html/de/fon/sip* usr/www/all/html/de/first/sip*; do
+	rm_files ${FILESYSTEM_MOD_DIR}/$files
+done
+
+echo1 "patching web UI"
+sed -i -e '/document.write(Sip.\{1,5\}(.*))/d' "${FILESYSTEM_MOD_DIR}/usr/www/all/html/de/home/home.html"
+sed -i -e '/\<Status$1\>/d' "${FILESYSTEM_MOD_DIR}/usr/www/all/html/de/home/home.html"
+sed -i -e '/jslGoTo('fon','siplist')/d;/^<?.* sip.*?>$/d' "${FILESYSTEM_MOD_DIR}/usr/www/all/html/de/menus/menu2_fon.html"
+
+echo1 "patching rc.conf"
+sed -i -e "s/CONFIG_FON_IPHONE=.*$/CONFIG_FON_IPHONE=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+sed -i -e "s/CONFIG_FON_HD=.*$/CONFIG_FON_HD=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+sed -i -e "s/CONFIG_T38=.*$/CONFIG_T38=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+sed -i -e "s/CONFIG_FONQUALITY=.*$/CONFIG_FONQUALITY=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+
+touch "${FILESYSTEM_MOD_DIR}/bin/voipd"
+chmod +x "${FILESYSTEM_MOD_DIR}/bin/voipd"
--- patches/520-remove_dect.sh
+++ patches/520-remove_dect.sh
@@ -1,14 +1,12 @@
 [ "$FREETZ_REMOVE_DECT" == "y" ] || return 0
 echo1 "removing DECT files"
 for files in \
-	lib/modules/2.6.??.?/kernel/drivers/isdn/avm_dect \
-	lib/modules/2.6.??.?/kernel/drivers/char/dect_io usr/bin/dect_manager \
-	usr/share/ctlmgr/libdect.so /bin/supportdata.dect sbin/start_dect_update.sh; do
+	lib/modules/2.6.??.?/kernel/drivers/isdn/avm_dect lib/modules/2.6.??.?/kernel/drivers/char/dect_io usr/bin/dect_manager \
+	usr/share/ctlmgr/libdect.so bin/supportdata.dect sbin/start_dect_update.sh \
+	$(find ${FILESYSTEM_MOD_DIR} -iwholename "*usr/www/*/html/*dect*" -printf "%P\n"); do
 	rm_files "${FILESYSTEM_MOD_DIR}/$files"
 done
-for files in $(find ${FILESYSTEM_MOD_DIR}/usr/www/all/html -name '*dect*' -o -name '*Dect*'); do
-	rm_files "$files"
-done
+[ "$FREETZ_REMOVE_MINID" == "y" ] && rm_files "${FILESYSTEM_MOD_DIR}/lib/libfoncclient.so*"
 
 echo1 "patching web UI"
 sed -i -e "s/document.write(Dect.\{1,10\}(.*))//g" "${FILESYSTEM_MOD_DIR}/usr/www/all/html/de/home/home.html"
@@ -16,9 +14,6 @@ sed -i -e "/jslGoTo('dect'/d;/^<?.*[dD]ect.*?>$/d" "${FILESYSTEM_MOD_DIR}/usr/ww
 
 echo1 "patching rc.S"
 sed -i -e "s/^modprobe dect_io$//g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
-
-#echo1 "patching device.table"
-#sed -i -e "/^\/dev\/dect_io/ d" "${FILESYSTEM_MOD_DIR}/etc/device.table"
 
 echo1 "patching rc.conf"
 sed -i -e "s/\(CONFIG_.*DECT.*=\).*/\1\"n\"/" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
