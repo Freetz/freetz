3f4d6d4ba757128de8e090f5807cfb8d20617f08
Markus Hempel <info@markushempel.de>
2009-08-16 10:13:05 +0000

    remove avm-vpn: removes now more files and the related web UI entrys
    git-svn-id: http://svn.freetz.org/trunk@3580 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/510-remove_avm_vpn.sh
+++ patches/510-remove_avm_vpn.sh
@@ -2,10 +2,23 @@
 echo1 "removing AVM-VPN files"
 for files in \
  	bin/avmike \
+ 	lib/libikeapi*.so* \
+ 	lib/libikecrypto*.so* \
+ 	lib/libikeossl*.so* \
 	usr/share/ctlmgr/libvpnstat.so \
+	$(find ${FILESYSTEM_MOD_DIR} -iwholename "*usr/www/*/html/*vpn*" -printf "%P\n") \
 	; do
 	rm_files "${FILESYSTEM_MOD_DIR}/$files"
 done
+
+echo1 "patching WebUI"
+for files in $(grep -rsl '<? setvariable var:showVpn 1 ?>' ${FILESYSTEM_MOD_DIR}); do
+	echo2 "patching $files"
+	sed -i -e 's/<? setvariable var:showVpn 1 ?>//' "$files"
+done
+
+
+
 
 echo1 "patching rc.conf"
 sed -i -e "s/CONFIG_VPN=.*$/CONFIG_VPN=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
