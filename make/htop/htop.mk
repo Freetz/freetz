52533917a58379e18ae1a1f11f033e217a442cdd
Lars Falkenhain <lars.falkenhain@gmail.com>
2009-08-29 03:04:50 +0000

    * add htop support by astrapi (closes #531)"
    git-svn-id: http://svn.freetz.org/trunk@3608 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/htop/htop.mk
@@ -0,0 +1,34 @@
+$(call PKG_INIT_BIN, 0.8.3)
+$(PKG)_SOURCE:=htop-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=@SF/htop
+$(PKG)_BINARY:=$($(PKG)_DIR)/htop
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/htop
+$(PKG)_SOURCE_MD5:=5c9f093f9eaddf6e77aa6d54c2116d0c
+$(PKG)_CONFIGURE_OPTIONS += ac_cv_file__proc_stat=yes
+$(PKG)_CONFIGURE_OPTIONS += ac_cv_file__proc_meminfo=yes
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+		$(MAKE) -C $(HTOP_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(HTOP_DIR) clean
+	$(RM) $(HTOP_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(HTOP_TARGET_BINARY)
+
+$(PKG_FINISH)
