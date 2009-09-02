93bce64f0968175ff86c579df3d147c23a8215ee
Lars Falkenhain <lars.falkenhain@gmail.com>
2009-09-02 18:18:19 +0000

    * 7140 a_ch: bump version to 39.04.76 (refs #534)
    git-svn-id: http://svn.freetz.org/trunk@3629 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/500-remove-mediasrv.sh
+++ patches/500-remove-mediasrv.sh
@@ -12,11 +12,12 @@ for files in \
 done
 [ "$FREETZ_REMOVE_MINID" == "y" ] && rm_files "${FILESYSTEM_MOD_DIR}/lib/libavmid3*.so*"
 
+if !(isFreetzType 7140 && isFreetzType LANG_A_CH); then
 echo1 "patching Web UI"
 sed -i -e "/^<p.*uiViewUseMusik.*<\/p>$/ {
 	N
 	s/\n//g
 	D }" "${FILESYSTEM_MOD_DIR}/usr/www/all/html/de/nas/einstellungen.html"
-
+fi
 echo1 "patching rc.conf"
 sed -i -e "s/CONFIG_MEDIASRV=.*$/CONFIG_MEDIASRV=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
