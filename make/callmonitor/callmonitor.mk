ad720501f7f2acaec84bf203afde93eac6672cfa
Andreas Bühmann <andreas.buehmann@web.de>
2009-09-12 06:59:22 +0000

    Update to callmonitor-1.16
    git-svn-id: http://svn.freetz.org/trunk@3656 f5190166-0702-4917-9039-51ec32eddaf5

--- make/callmonitor/callmonitor.mk
+++ make/callmonitor/callmonitor.mk
@@ -1,26 +1,45 @@
-$(call PKG_INIT_BIN, 1.15.1)
+$(call PKG_INIT_BIN, 1.16)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION)-freetz.tar.bz2
 $(PKG)_SITE:=http://download.berlios.de/callmonitor
 $(PKG)_DIR:=$(SOURCE_DIR)/$(pkg)-$($(PKG)_VERSION)
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/recode
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/lib/callmonitor/bin/recode
+$(PKG)_TARGET_BINARY:=$($(PKG)_DIR)/root/usr/lib/callmonitor/bin/recode
 $(PKG)_STARTLEVEL=30
+
+CALLMONITOR_FEATURES:=$(foreach feat,webif actions monitor phonebook,\
+    	$(if $(CALLMONITOR_$(feat)),$(feat)))
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
-$(PKG_CONFIGURED_NOP)
+
+.PHONY: FORCE
+
+$(CALLMONITOR_DIR)/.features.new: FORCE
+	echo $(CALLMONITOR_FEATURES) > $@
+
+$(CALLMONITOR_DIR)/.features: $(CALLMONITOR_DIR)/.features.new
+	if ! diff -q $< $@; then cp $< $@; fi
+
+$($(PKG)_DIR)/.configured: $($(PKG)_DIR)/.unpacked $($(PKG)_DIR)/.features
+	$(MAKE) -C $(CALLMONITOR_DIR) configure
+	@touch $@
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(MAKE) $(TARGET_CONFIGURE_ENV) -C $(CALLMONITOR_DIR)/src
+	$(MAKE) $(TARGET_CONFIGURE_ENV) -C $(CALLMONITOR_DIR) build
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
 
-$(pkg) $(pkg)-precompiled: $($(PKG)_TARGET_DIR)/.packaged $($(PKG)_TARGET_BINARY)
+$(pkg)-clean: FORCE
+	if [ -d "$(CALLMONITOR_DIR)" ]; then $(MAKE) -C $(CALLMONITOR_DIR) clean; else true; fi
 
-$($(PKG)_TARGET_DIR)/.packaged: $(CALLMONITOR_DIR)/.configured
+$(pkg) $(pkg)-precompiled: $($(PKG)_TARGET_DIR)/.packaged
+
+$($(PKG)_TARGET_DIR)/.packaged: $(CALLMONITOR_DIR)/.configured $($(PKG)_TARGET_BINARY)
+	rm -rf $(CALLMONITOR_TARGET_DIR)
 	mkdir -p $(CALLMONITOR_TARGET_DIR)/root
-	tar -c -C $(CALLMONITOR_DIR)/root --exclude=.svn . | tar -x -C $(CALLMONITOR_DEST_DIR)
+	tar -c -C $(CALLMONITOR_DIR)/root --files-from=$(CALLMONITOR_DIR)/.files | \
+	    tar -x -C $(CALLMONITOR_DEST_DIR)
 	cp $(CALLMONITOR_DIR)/.language $(CALLMONITOR_TARGET_DIR)/
 	@touch $@
 
