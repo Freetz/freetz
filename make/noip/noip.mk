ede1b589e99297eceed69ee6027e638e2e46a24f
Lars Falkenhain <lars.falkenhain@gmail.com>
2009-09-17 17:43:44 +0000

    * add binary only noip-client (by sf3978)
    git-svn-id: http://svn.freetz.org/trunk@3669 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/noip/noip.mk
@@ -0,0 +1,29 @@
+$(call PKG_INIT_BIN, 2.1.9)
+$(PKG)_SOURCE:=noip-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=http://www.no-ip.com/client/linux
+$(PKG)_BINARY:=$($(PKG)_DIR)/noip2
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/noip2
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+	$(MAKE) -C $(NOIP_DIR)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(NOIP_DIR) clean
+	$(RM) $(NOIP_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(NOIP_TARGET_BINARY)
+
+$(PKG_FINISH)
