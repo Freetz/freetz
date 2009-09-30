5ee0b12573e476fe2b6b485daf4650e2a9ece50d
Whoopie <whoopie79@gmx.net>
2009-09-30 10:01:49 +0000

    update patches for international FW for Eumex 300IP (offsets, fuzz, cosmetics)
    git-svn-id: http://svn.freetz.org/trunk@3720 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/300ip_as_fon/200-tk-firmware.sh
+++ patches/300ip_as_fon/200-tk-firmware.sh
@@ -1,3 +1,2 @@
-echo "FREETZ_TYPE_LANG_STRING=$FREETZ_TYPE_LANG_STRING"
 # executing sh according to language / version 
 . "${PATCHES_DIR}/${FREETZ_TYPE_STRING}/200-tk-firmware-${FREETZ_TYPE_LANG_STRING}"
