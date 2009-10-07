e6562eba861a830c62205b07f46d7a1d829eaa77
Whoopie <whoopie79@gmx.net>
2009-10-07 08:21:13 +0000

    bip: bump version to 0.8.2 wget: bump version to 1.12
    git-svn-id: http://svn.freetz.org/trunk@3736 f5190166-0702-4917-9039-51ec32eddaf5

--- make/bip/bip.mk
+++ make/bip/bip.mk
@@ -1,4 +1,4 @@
-$(call PKG_INIT_BIN,0.8.1)
+$(call PKG_INIT_BIN,0.8.2)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=http://bip.t1r.net/downloads
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/bip
--- make/wget/wget.mk
+++ make/wget/wget.mk
@@ -1,4 +1,4 @@
-$(call PKG_INIT_BIN,1.11.4)
+$(call PKG_INIT_BIN,1.12)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=http://ftp.gnu.org/gnu/wget
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/wget
