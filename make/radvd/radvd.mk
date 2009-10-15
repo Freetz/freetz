cd556838becfc28176469ead3da8f1ffaafe9e91
Whoopie <whoopie79@gmx.net>
2009-10-15 08:43:12 +0000

    radvd: bump to version 1.5
    git-svn-id: http://svn.freetz.org/trunk@3745 f5190166-0702-4917-9039-51ec32eddaf5

--- make/radvd/radvd.mk
+++ make/radvd/radvd.mk
@@ -1,9 +1,9 @@
-$(call PKG_INIT_BIN, 1.4)
+$(call PKG_INIT_BIN, 1.5)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=http://www.litech.org/radvd/dist
 $(PKG)_BINARY:=$($(PKG)_DIR)/$(pkg)
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/$(pkg)
-$(PKG)_SOURCE_MD5:=ad57121d57a716aa5db33668e4fb32c5 
+$(PKG)_SOURCE_MD5:=78598b60ed9d64920739189c3dc0934d
 
 $(PKG)_DEPENDS_ON := flex
 
