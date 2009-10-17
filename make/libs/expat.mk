f3cc099a65f2311e3fdd18879a8aa860d0db85d3
Gene Rudoy <gene@freetz.org>
2009-10-17 22:19:44 +0000

    add uclibc++ dependency to expat to prevent its configure script from messing up the config.cache file (fixes #561)
    git-svn-id: http://svn.freetz.org/trunk@3755 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/expat.mk
+++ make/libs/expat.mk
@@ -5,7 +5,19 @@ $(PKG)_SITE:=http://mesh.dl.sourceforge.net/sourceforge/expat
 $(PKG)_BINARY:=$($(PKG)_DIR)/.libs/libexpat.so.$($(PKG)_LIB_VERSION)
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libexpat.so.$($(PKG)_LIB_VERSION)
 $(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libexpat.so.$($(PKG)_LIB_VERSION)
-$(PKG)_SOURCE_MD5:=ee8b492592568805593f81f8cdf2a04c 
+$(PKG)_SOURCE_MD5:=ee8b492592568805593f81f8cdf2a04c
+
+# NB: Expat actually doesn't depend on uclibc++, but its configure script
+# carries out some c++ related tests. Provided expat is compiled before
+# uclibc++, which is normally the case on a clean build, these tests fail.
+# This, in turn, results in a messed up make/config.cache file,
+# i.e. config.cache with some wrong c++ related entries. These wrong entries
+# do not affect expat itself, but they may (and they actually do) affect
+# other packages/libraries written in c++.
+# This is the reason we add uclibcxx dependency here. This causes
+# uclibc++ to be compiled before expat, and configure tests not
+# to fail anymore.
+$(PKG)_DEPENDS_ON := uclibcxx
 
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
@@ -16,7 +28,7 @@ $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	PATH=$(TARGET_PATH) \
-		$(MAKE) -C $(EXPAT_DIR) \
+		$(MAKE) -C $(EXPAT_DIR)
 
 
 $($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
@@ -24,7 +36,7 @@ $($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
 		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
 		-C $(EXPAT_DIR) install
 	$(PKG_FIX_LIBTOOL_LA) \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libexpat.la 
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libexpat.la
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
 	cp -a $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libexpat*.so* $(EXPAT_TARGET_DIR)/
