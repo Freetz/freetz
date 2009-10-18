1e502576badfe706c3e3746dca0f91c9ba4198ea
Oliver Metz <oliver@freetz.org>
2009-10-18 17:07:39 +0000

    * Move section from davfs/Makefile.in to libs/Makefile.in   * Fixes improper build order of uclibcxx and expat   * Remove work around   * refs #561
    git-svn-id: http://svn.freetz.org/trunk@3765 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/expat.mk
+++ make/libs/expat.mk
@@ -7,17 +7,6 @@ $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libexpat.so.$($(P
 $(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libexpat.so.$($(PKG)_LIB_VERSION)
 $(PKG)_SOURCE_MD5:=ee8b492592568805593f81f8cdf2a04c
 
-# NB: Expat actually doesn't depend on uclibc++, but its configure script
-# carries out some c++ related tests. Provided expat is compiled before
-# uclibc++, which is normally the case on a clean build, these tests fail.
-# This, in turn, results in a messed up make/config.cache file,
-# i.e. config.cache with some wrong c++ related entries. These wrong entries
-# do not affect expat itself, but they may (and they actually do) affect
-# other packages/libraries written in c++.
-# This is the reason we add uclibcxx dependency here. This causes
-# uclibc++ to be compiled before expat, and configure tests not
-# to fail anymore.
-$(PKG)_DEPENDS_ON := uclibcxx
 
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
