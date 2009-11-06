06be3e52ae9b506d0456cfc8440405dc691a0525
Markus Hempel <info@markushempel.de>
2009-11-06 18:36:17 +0000

    * pppd-2.5.1git: some corrections from r3820 and one patch removed
    git-svn-id: http://svn.freetz.org/trunk@3824 f5190166-0702-4917-9039-51ec32eddaf5

--- make/pppd/pppd.mk
+++ make/pppd/pppd.mk
@@ -16,12 +16,19 @@ $(PKG)_DEPENDS_ON := libpcap
 $(PKG_SOURCE_DOWNLOAD)
 
 $($(PKG)_DIR)/.unpacked: $(DL_DIR)/$($(PKG)_SOURCE) | $(SOURCE_DIR)
-	$(RM) -r $($(PKG)_DIR)
+	$(RM) -r $(PPPD_DIR)
 	tar -xOf $(DL_DIR)/$(PPPD_SOURCE) ppp-2.4.5git1/upstream/tarballs/$(PPPD_SOURCE) | \
 		tar -C $(SOURCE_DIR) $(VERBOSE) -xzf - 
 	shopt -s nullglob; for i in $(PPPD_MAKE_DIR)/patches/*.patch; do \
 		$(PATCH_TOOL) $(PPPD_DIR) $$i; \
 	done
+	touch $@
+
+$(PACKAGES_DIR)/.$(pkg)-$($(PKG)_VERSION):
+	mkdir -p $(PPPD_TARGET_DIR)/root
+	if test -d $(PPPD_MAKE_DIR)/files; then \
+		tar -c -C $(PPPD_MAKE_DIR)/files --exclude=.svn . | tar -x -C $(PPPD_TARGET_DIR) ; \
+	fi
 	touch $@
 
 $(PKG_CONFIGURED_CONFIGURE)
@@ -45,8 +52,7 @@ endif
 
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) \
-			$($(PKG)_CHAT_TARGET_BINARY)
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_CHAT_TARGET_BINARY) $(PACKAGES_DIR)/.$(pkg)-$($(PKG)_VERSION)
 
 $(pkg)-clean:
 	-$(MAKE) -C $(PPPD_DIR) clean
