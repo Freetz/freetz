a1ecb1035044c24d35864a52e438c6a060da50a6
Gene Rudoy <gene@freetz.org>
2009-11-08 12:13:57 +0000

    * add new package unrar * provide external option for libuClibc++
    git-svn-id: http://svn.freetz.org/trunk@3846 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/unrar/unrar.mk
@@ -0,0 +1,44 @@
+$(call PKG_INIT_BIN, 3.9.6)
+$(PKG)_DIR:=$(subst -$($(PKG)_VERSION),,$($(PKG)_DIR))
+$(PKG)_SOURCE:=unrarsrc-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=http://www.rarlab.com/rar/
+$(PKG)_BINARY:=$($(PKG)_DIR)/unrar
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/unrar
+$(PKG)_SOURCE_MD5:=59b23dd05ff70791cd6449effa7fc3b6
+
+$(PKG)_DEPENDS_ON := uclibcxx
+
+$(PKG)_CONFIG_SUBOPTS += FREETZ_PACKAGE_UNRAR_STATIC
+ifeq ($(strip $(FREETZ_PACKAGE_UNRAR_STATIC)),y)
+$(PKG)_LDFLAGS:="-static"
+else
+$(PKG)_LDFLAGS:=""
+endif
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+		$(MAKE) -C $(UNRAR_DIR) -f makefile.unix \
+		CXX="$(TARGET_CXX)" \
+		CXXFLAGS="$(TARGET_CFLAGS)" \
+		DEFINES="" \
+		LDFLAGS="$(UNRAR_LDFLAGS)" \
+		STRIP="$(TARGET_STRIP)"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(UNRAR_DIR) -f makefile.unix clean
+
+$(pkg)-uninstall:
+	$(RM) $(UNRAR_TARGET_BINARY)
+
+$(PKG_FINISH)
