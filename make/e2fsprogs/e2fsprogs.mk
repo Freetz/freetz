83c49f64e5009532932043001121db70318ebf4e
Oliver Metz <oliver@freetz.org>
2009-11-08 12:56:41 +0000

    [test-branch]:  * Fix unintented version change
    git-svn-id: http://svn.freetz.org/branches/test-branch@3847 f5190166-0702-4917-9039-51ec32eddaf5

--- make/e2fsprogs/e2fsprogs.mk
+++ make/e2fsprogs/e2fsprogs.mk
@@ -1,4 +1,4 @@
-$(call PKG_INIT_BIN, 1.41.4)
+$(call PKG_INIT_BIN, 1.41.9)
 $(PKG)_SOURCE:=e2fsprogs-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=@SF/e2fsprogs
 $(PKG)_DIR:=$(SOURCE_DIR)/e2fsprogs-$($(PKG)_VERSION)
