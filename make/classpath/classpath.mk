fa7d33b10bdf61930a6c4ecf17c507cd1c7c27a6
Gene Rudoy <gene@freetz.org>
2009-11-12 02:48:14 +0000

    [test-branch, classpath]:  * fix dependencies  * rewrite makefile  * remove unnecessary/empty directories
    git-svn-id: http://svn.freetz.org/branches/test-branch@3871 f5190166-0702-4917-9039-51ec32eddaf5

--- make/classpath/classpath.mk
+++ make/classpath/classpath.mk
@@ -1,65 +1,67 @@
 $(call PKG_INIT_BIN, 0.97.2)
-$(PKG)_UGLY_VERSION:=0.0.0
+$(PKG)_LIB_VERSION:=0.0.0
 $(PKG)_SOURCE:=classpath-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=6a35347901ace03c31cc49751b338f31
 $(PKG)_SITE:=ftp://ftp.gnu.org/gnu/classpath
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/lib/mini.jar
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/share/classpath/mini.jar
-$(PKG)_LIB_BINARY:=$($(PKG)_DIR)/native/jni/java-lang/.libs/libjavalang.so.$($(PKG)_UGLY_VERSION)
-$(PKG)_LIB_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/classpath/libjavalang.so.$($(PKG)_UGLY_VERSION)
-$(PKG)_LIB_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/lib/classpath/libjavalang.so.$($(PKG)_UGLY_VERSION)
-$(PKG)_SOURCE_MD5:=6a35347901ace03c31cc49751b338f31 
 
+#TODO: FREETZ_LIBRARY_PATH !?
+$(PKG)_LIBRARY_DIR:=/usr/lib/classpath
+$(PKG)_LIBNAMES_SHORT:=io lang langmanagement langreflect net nio util
+$(PKG)_LIBNAMES_LONG:=$(CLASSPATH_LIBNAMES_SHORT:%=libjava%.so.$($(PKG)_LIB_VERSION))
+$(PKG)_LIBS_BUILD_DIR_SHORT:=io lang lang lang net nio util
+$(PKG)_LIBS_BUILD_DIR:=$(join $(CLASSPATH_LIBS_BUILD_DIR_SHORT:%=$($(PKG)_DIR)/native/jni/java-%/.libs/),$(CLASSPATH_LIBNAMES_LONG))
+$(PKG)_LIBS_STAGING_DIR:=$(CLASSPATH_LIBNAMES_LONG:%=$(TARGET_TOOLCHAIN_STAGING_DIR)$(CLASSPATH_LIBRARY_DIR)/%)
+$(PKG)_LIBS_TARGET_DIR:=$(CLASSPATH_LIBNAMES_LONG:%=$($(PKG)_DEST_DIR)$(CLASSPATH_LIBRARY_DIR)/%)
+
+$(PKG)_BUILD_PREREQ += fastjar
+$(PKG)_BUILD_PREREQ_HINT := Hint: on Debian-like systems this binary is provided by the package of the same name
+
+ifeq ($(strip $(FREETZ_TARGET_UCLIBC_VERSION_0_9_28)),y)
 $(PKG)_DEPENDS_ON := libiconv
+endif
 
 $(PKG)_CONFIGURE_OPTIONS += --disable-gtk-peer
 $(PKG)_CONFIGURE_OPTIONS += --disable-qt-peer
 $(PKG)_CONFIGURE_OPTIONS += --disable-gconf-peer
 $(PKG)_CONFIGURE_OPTIONS += --without-libiconv-prefix
-$(PKG)_CONFIGURE_OPTIONS += --disable-plugin       
-$(PKG)_CONFIGURE_OPTIONS += --with-ecj               
+$(PKG)_CONFIGURE_OPTIONS += --disable-plugin
+$(PKG)_CONFIGURE_OPTIONS += --with-ecj
 $(PKG)_CONFIGURE_OPTIONS += --disable-Werror
-
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
-$($(PKG)_BINARY) $($(PKG)_LIB_BINARY): $($(PKG)_DIR)/.configured
+$($(PKG)_BINARY) $($(PKG)_LIBS_BUILD_DIR): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(CLASSPATH_DIR)
 	cp $(CLASSPATH_MAKE_DIR)/mini.classlist $(CLASSPATH_DIR)/lib;
 	( cd $(CLASSPATH_DIR)/lib; fastjar -Mcf mini.jar -@ < mini.classlist );
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
-	cp $(CLASSPATH_BINARY) $(CLASSPATH_TARGET_BINARY)
+	$(INSTALL_BINARY)
 
-$($(PKG)_LIB_STAGING_BINARY): $($(PKG)_LIB_BINARY)
+$($(PKG)_LIBS_STAGING_DIR): $($(PKG)_LIBS_BUILD_DIR)
 	$(SUBMAKE) -C $(CLASSPATH_DIR)/native/jni \
 		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" install
 	$(PKG_FIX_LIBTOOL_LA) \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/classpath/libjavaio.la \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/classpath/libjavalang.la \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/classpath/libjavalangmanagement.la \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/classpath/libjavalangreflect.la \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/classpath/libjavanet.la \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/classpath/libjavanio.la \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/classpath/libjavautil.la
-	touch $@
+		$(CLASSPATH_LIBNAMES_SHORT:%=$(TARGET_TOOLCHAIN_STAGING_DIR)$(CLASSPATH_LIBRARY_DIR)/libjava%.la)
 
-$($(PKG)_LIB_TARGET_BINARY): $($(PKG)_LIB_STAGING_BINARY)
-	cp -a $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/classpath/libjava*.so* $(CLASSPATH_DEST_DIR)/usr/lib/classpath/
-	$(TARGET_STRIP) $(CLASSPATH_DEST_DIR)/usr/lib/classpath/libjava*.so*
-	touch $@
+$($(PKG)_LIBS_TARGET_DIR): $($(PKG)_DEST_DIR)$(CLASSPATH_LIBRARY_DIR)/%: $(TARGET_TOOLCHAIN_STAGING_DIR)$(CLASSPATH_LIBRARY_DIR)/%
+	$(INSTALL_LIBRARY_STRIP)
 
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_LIB_TARGET_BINARY)
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_LIBS_TARGET_DIR)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(CLASSPATH_DIR) clean
 
 $(pkg)-uninstall:
-	$(RM) $(CLASSPATH_TARGET_BINARY)
-	$(RM) root/usr/lib/libjava*.so*
-	$(RM) $(CLASSPATH_DIR)/.installed
+	$(RM) \
+		$(CLASSPATH_TARGET_BINARY) \
+		$($(PKG)_DEST_DIR)$(CLASSPATH_LIBRARY_DIR)/libjava*.so*
 
 $(PKG_FINISH)
