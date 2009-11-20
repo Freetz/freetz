a3ed2c9c92925090bb00f1bbbedbcdd9172b9ed9
Gene Rudoy <gene@freetz.org>
2009-11-20 00:05:54 +0000

    add another bunch of missing configure-environment-values
    git-svn-id: http://svn.freetz.org/trunk@3914 f5190166-0702-4917-9039-51ec32eddaf5

--- make/php/php.mk
+++ make/php/php.mk
@@ -27,6 +27,16 @@ $(PKG)_CONFIGURE_ENV += php_cv_sizeof_ssize_t=4
 $(PKG)_CONFIGURE_ENV += php_cv_sizeof_ptrdiff_t=4
 $(PKG)_CONFIGURE_ENV += ac_cv_c_bigendian_php=no
 $(PKG)_CONFIGURE_ENV += php_cv_sizeof_intmax_t=8
+$(PKG)_CONFIGURE_ENV += ac_cv_func_getaddrinfo=yes
+$(PKG)_CONFIGURE_ENV += ac_cv_c_stack_direction=-1
+$(PKG)_CONFIGURE_ENV += ac_cv_ebcdic=no
+$(PKG)_CONFIGURE_ENV += ac_cv_header_stdc=yes
+$(PKG)_CONFIGURE_ENV += ac_cv_func_memcmp_clean=no
+$(PKG)_CONFIGURE_ENV += ac_cv_func_utime_null=no
+$(PKG)_CONFIGURE_ENV += ac_cv_time_r_type=POSIX
+$(PKG)_CONFIGURE_ENV += ac_cv_what_readdir_r=POSIX
+$(PKG)_CONFIGURE_ENV += ac_cv_write_stdout=yes
+$(PKG)_CONFIGURE_ENV += cookie_io_functions_use_off64_t=yes
 $(PKG)_CONFIGURE_ENV += lt_cv_prog_gnu_ldcxx=yes
 $(PKG)_CONFIGURE_ENV += lt_cv_path_NM="$(TARGET_TOOLCHAIN_SYMLINK)/bin/mipsel-linux-nm -B"
 
