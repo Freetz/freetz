82c554038abe566c2535ab2af37ba1001473bb66
Markus Hempel <info@markushempel.de>
2009-11-22 19:25:32 +0000

    * pppd: version bump to official version 2.4.5
    git-svn-id: http://svn.freetz.org/trunk@3934 f5190166-0702-4917-9039-51ec32eddaf5

--- make/pppd/pppd.mk
+++ make/pppd/pppd.mk
@@ -1,40 +1,22 @@
-$(call PKG_INIT_BIN, 2.4.5git)
-$(PKG)_SOURCE:=ppp_2.4.5~git20081126t100229.orig.tar.gz
-$(PKG)_SITE:=https://launchpad.net/ubuntu/+archive/primary/+files
+$(call PKG_INIT_BIN, 2.4.5)
+$(PKG)_SOURCE:=ppp-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=ftp://ftp.samba.org/pub/ppp
 $(PKG)_DIR:=$(SOURCE_DIR)/ppp-$($(PKG)_VERSION)
 $(PKG)_BINARY:=$($(PKG)_DIR)/pppd/pppd
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/pppd
 $(PKG)_CHAT_BINARY:=$(PPPD_DIR)/chat/chat
 $(PKG)_CHAT_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/chat
 $(PKG)_STARTLEVEL=40
-$(PKG)_SOURCE_MD5:=fadde1f8ffd294fd9c06425ea6bd5cfd
+$(PKG)_SOURCE_MD5:=4621bc56167b6953ec4071043fe0ec57
 
 $(PKG)_CONFIG_SUBOPTS += FREETZ_PACKAGE_PPPD_CHAT
-
-$(PKG)_DEPENDS_ON := libpcap
 
 ifeq ($(strip $(FREETZ_TARGET_IPV6_SUPPORT)),y)
 PPPD_CFLAGS := -DFREETZ_TARGET_IPV6_SUPPORT
 endif
 
 $(PKG_SOURCE_DOWNLOAD)
-
-$($(PKG)_DIR)/.unpacked: $(DL_DIR)/$($(PKG)_SOURCE) | $(SOURCE_DIR)
-	$(RM) -r $(PPPD_DIR)
-	tar -xOf $(DL_DIR)/$(PPPD_SOURCE) ppp-2.4.5git1/upstream/tarballs/$(PPPD_SOURCE) | \
-		tar -C $(SOURCE_DIR) $(VERBOSE) -xzf -
-	shopt -s nullglob; for i in $(PPPD_MAKE_DIR)/patches/*.patch; do \
-		$(PATCH_TOOL) $(PPPD_DIR) $$i; \
-	done
-	touch $@
-
-$(PACKAGES_DIR)/.$(pkg)-$($(PKG)_VERSION):
-	mkdir -p $(PPPD_TARGET_DIR)/root
-	if test -d $(PPPD_MAKE_DIR)/files; then \
-		tar -c -C $(PPPD_MAKE_DIR)/files --exclude=.svn . | tar -x -C $(PPPD_TARGET_DIR) ; \
-	fi
-	touch $@
-
+$(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY) \
