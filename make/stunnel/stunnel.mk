059e647d27bb1b3b6363d66b81a2055050e617e8
Whoopie <whoopie79@gmx.net>
2009-11-25 14:52:20 +0000

    stunnel: bump version to 4.28, add option to build statically, enable IPv6 only if FREETZ_TARGET_IPV6_SUPPORT is set
    git-svn-id: http://svn.freetz.org/trunk@3966 f5190166-0702-4917-9039-51ec32eddaf5

--- make/stunnel/stunnel.mk
+++ make/stunnel/stunnel.mk
@@ -1,17 +1,26 @@
-$(call PKG_INIT_BIN,4.27)
+$(call PKG_INIT_BIN,4.28)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=http://www.stunnel.org/download/stunnel/src
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/$(pkg)
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/$(pkg)
 $(PKG)_STARTLEVEL=30
-$(PKG)_SOURCE_MD5:=3c655d815576f50046a1c28744b88681
+$(PKG)_SOURCE_MD5:=5bf753a042047f40a938e82ec7ece569
 
 $(PKG)_DEPENDS_ON := openssl zlib
+
+$(PKG)_LIBS := -lutil -lssl -lcrypto -lz -ldl
+
+ifeq ($(strip $(FREETZ_PACKAGE_STUNNEL_STATIC)),y)
+$(PKG)_LDFLAGS := -all-static
+endif
 
 $(PKG)_CONFIGURE_OPTIONS += --disable-libwrap
 $(PKG)_CONFIGURE_OPTIONS += --with-threads=fork
 #$(PKG)_CONFIGURE_OPTIONS += --with-threads=pthread
 $(PKG)_CONFIGURE_OPTIONS += --with-ssl="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_TARGET_IPV6_SUPPORT),--enable-ipv6,--disable-ipv6)
+
+$(PKG)_CONFIG_SUBOPTS += FREETZ_PACKAGE_STUNNEL_STATIC
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -19,7 +28,9 @@ $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	PATH="$(TARGET_PATH)" \
-		$(MAKE) -C $(STUNNEL_DIR)
+		$(MAKE) -C $(STUNNEL_DIR) \
+		LDFLAGS="$(STUNNEL_LDFLAGS)" \
+		LIBS="$(STUNNEL_LIBS)"
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
