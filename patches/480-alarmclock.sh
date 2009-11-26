0fac9e974db0a509973ff196dd47daad9f4447d4
cuma <cuma@freetz.org>
2009-11-26 18:39:30 +0000

    3rd alarm-clock updated:  * now for: 7150  7112 7141 7170  3070_V3  3270 7240 7270 7270_V3  * currently unsupported 7050 7113 7140
    git-svn-id: http://svn.freetz.org/trunk@3975 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/480-alarmclock.sh
+++ patches/480-alarmclock.sh
@@ -1,11 +1,19 @@
 [ "$FREETZ_PATCH_ALARMCLOCK" == "y" ] || return 0
 echo1 "adding alarm-clock"
 
-if isFreetzType 7170 7141 && isFreetzType LANG_DE; then
+if isFreetzType 7150 && isFreetzType LANG_DE; then
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/${FREETZ_TYPE_LANG_STRING}/480-alarmclock_7150.patch"
+fi
+
+if isFreetzType 7112 7141 7170 && isFreetzType LANG_DE; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/${FREETZ_TYPE_LANG_STRING}/480-alarmclock.patch"
 fi
 
-if isFreetzType 7270 7270_V3 3270; then
+if isFreetzType 3270_V3; then
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/480-alarmclock_3020v3.patch"
+fi
+
+if isFreetzType 3270 7240 7270 7270_V3; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/480-alarmclock.patch"
 fi
 
