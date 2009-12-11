18b3c38d7133c9488bd407cb2d049289e1cf7233
Gene Rudoy <gene@freetz.org>
2009-12-11 12:39:45 +0000

    vnstat: makefile cleanups
    git-svn-id: http://svn.freetz.org/trunk@4028 f5190166-0702-4917-9039-51ec32eddaf5

--- make/vnstat/vnstat.mk
+++ make/vnstat/vnstat.mk
@@ -1,47 +1,36 @@
 $(call PKG_INIT_BIN, 1.9)
 $(PKG)_SOURCE:=vnstat-$($(PKG)_VERSION).tar.gz
-$(PKG)_SITE:=http://humdi.net/vnstat
-$(PKG)_BINARY:=$($(PKG)_DIR)/src/vnstat
-$(PKG)_DAEMON:=$($(PKG)_DIR)/src/vnstatd
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/vnstat
-$(PKG)_TARGET_DAEMON:=$($(PKG)_DEST_DIR)/usr/bin/vnstatd
 $(PKG)_SOURCE_MD5:=ebaf8352fa3674faea2fe2ce1001a38d
+$(PKG)_SITE:=http://humdi.net/vnstat
+$(PKG)_BINARIES_ALL := vnstat vnstatd
+$(PKG)_BINARIES := $(filter-out $(if $(FREETZ_PACKAGE_VNSTAT_DAEMON),,vnstatd),$($(PKG)_BINARIES_ALL))
+$(PKG)_BINARIES_BUILD_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DIR)/src/%)
+$(PKG)_BINARIES_TARGET_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DEST_DIR)/usr/bin/%)
 
-VNSTAT_LIBS := -lm
+$(PKG)_LIBS := -lm
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_NOP)
 
-$($(PKG)_BINARY) $($(PKG)_DAEMON): $($(PKG)_DIR)/.configured
+$($(PKG)_BINARIES_BUILD_DIR): $($(PKG)_DIR)/.configured
 	PATH="$(TARGET_PATH)" \
 	$(MAKE) -C $(VNSTAT_DIR) \
 	CC="$(TARGET_CC)" \
 	CFLAGS="$(TARGET_CFLAGS)" \
-	LIBS="$(VNSTAT_LIBS)" 
+	LIBS="$(VNSTAT_LIBS)"
 
-$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
-	$(INSTALL_BINARY_STRIP)
-
-$($(PKG)_TARGET_DAEMON): $($(PKG)_DAEMON)
+$($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/src/%
 	$(INSTALL_BINARY_STRIP)
 
 $(pkg):
-ifeq ($(strip $(FREETZ_PACKAGE_VNSTAT_DAEMON)),y)
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_TARGET_DAEMON)
-else
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $(pkg)-clean-daemon
-endif
 
-$(pkg)-clean-daemon:
-	$(RM) $(VNSTAT_TARGET_DAEMON)
+$(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR)
 
 $(pkg)-clean:
 	-$(MAKE) -C $(VNSTAT_DIR) clean
-	$(RM) $(VNSTAT_DIR)/.configured
 
 $(pkg)-uninstall:
-	$(RM) $(VNSTAT_TARGET_BINARY) \
-		 $(VNSTAT_TARGET_DAEMON)
+	$(RM) $(VNSTAT_BINARIES_ALL:%=$(VNSTAT_DEST_DIR)/usr/bin/%)
 
 $(PKG_FINISH)
