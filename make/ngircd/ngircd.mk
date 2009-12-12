6b80be61c175bc9efe93df6641bc4d3569c6016f
Oliver Metz <oliver@freetz.org>
2009-12-12 10:27:57 +0000

    * ngircd: Bump to version 15, add tcp_wrappers, openssl and zlib support, add to external (by sf3978)
    git-svn-id: http://svn.freetz.org/trunk@4030 f5190166-0702-4917-9039-51ec32eddaf5

--- make/ngircd/ngircd.mk
+++ make/ngircd/ngircd.mk
@@ -1,20 +1,65 @@
-$(call PKG_INIT_BIN, 14.1)
+$(call PKG_INIT_BIN, 15)
 $(PKG)_SOURCE:=ngircd-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=ftp://ftp.berlios.de/pub/ngircd
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/ngircd/ngircd
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/ngircd
-$(PKG)_SOURCE_MD5:=eef90855414c35bfb6590d17e24ee06f
-
-ifeq ($(strip $(FREETZ_PACKAGE_NGIRCD_WITH_TCP_WRAPPERS)),y)
-$(PKG)_DEPENDS_ON += tcp_wrappers
-endif
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/ngircd
+$(PKG)_SOURCE_MD5:=c183a85eba6fe51255983848f099c8ae
 
 $(PKG)_CONFIG_SUBOPTS += FREETZ_PACKAGE_NGIRCD_WITH_TCP_WRAPPERS
+$(PKG)_CONFIG_SUBOPTS += FREETZ_PACKAGE_NGIRCD_WITH_ZLIB
+$(PKG)_CONFIG_SUBOPTS += FREETZ_PACKAGE_NGIRCD_WITH_SSL
+$(PKG)_CONFIG_SUBOPTS += FREETZ_PACKAGE_NGIRCD_STATIC
 
-$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_NGIRCD_WITH_TCP_WRAPPERS),--with-tcp-wrappers=$(TARGET_TOOLCHAIN_STAGING_DIR),--without-tcp-wrappers)
-$(PKG)_CONFIGURE_OPTIONS += --with-syslog
+$(PKG)_LDFLAGS = ""
+$(PKG)_LIBS = ""
+
+ifeq ($(strip $(FREETZ_PACKAGE_NGIRCD_WITH_TCP_WRAPPERS)),y)
+$(PKG)_DEPENDS_ON := tcp_wrappers
+$(PKG)_CONFIGURE_OPTIONS += --with-tcp-wrappers=$(TARGET_TOOLCHAIN_STAGING_DIR)
+$(PKG)_LDFLAGS += -lwrap
+else
+$(PKG)_CONFIGURE_OPTIONS += --without-tcp-wrappers
+endif
+
+ifeq ($(strip $(FREETZ_PACKAGE_NGIRCD_WITH_ZLIB)),y)
+$(PKG)_DEPENDS_ON := zlib
+NGIRCD_LDFLAGS += -lz
+$(PKG)_CONFIGURE_OPTIONS += --with-zlib
+else
 $(PKG)_CONFIGURE_OPTIONS += --without-zlib
+endif
+
+ifeq ($(strip $(FREETZ_PACKAGE_NGIRCD_WITH_SSL)),y)
+$(PKG)_DEPENDS_ON := openssl
+$(PKG)_CONFIGURE_OPTIONS += --with-openssl
+
+ifeq ($(strip $(FREETZ_PACKAGE_NGIRCD_WITH_TCP_WRAPPERS)),y)
+NGIRCD_LIBS := -lssl -lcrypto -ldl -lwrap
+else
+NGIRCD_LIBS := -lssl -lcrypto -ldl
+endif
+ifeq ($(strip $(FREETZ_PACKAGE_NGIRCD_WITH_ZLIB)),y)
+NGIRCD_LIBS := -lssl -lcrypto -ldl -lz
+else
+NGIRCD_LIBS := -lssl -lcrypto -ldl
+endif
+
+ifeq ($(strip $(FREETZ_PACKAGE_NGIRCD_STATIC)),y)
+NGIRCD_LDFLAGS += -static
+ifeq ($(strip $(FREETZ_PACKAGE_NGIRCD_WITH_ZLIB)),y)
+ifeq ($(strip $(FREETZ_PACKAGE_NGIRCD_WITH_TCP_WRAPPERS)),y)
+NGIRCD_LIBS := -lssl -lcrypto -ldl -lwrap -lz
+endif
+endif
+else
+NGIRCD_LIBS := -lssl -lcrypto -ldl
+endif
+else
 $(PKG)_CONFIGURE_OPTIONS += --without-openssl
+NGIRCD_LDFLAGS += -ldl
+endif
+
+$(PKG)_CONFIGURE_OPTIONS += --with-syslog
 $(PKG)_CONFIGURE_OPTIONS += --without-ident
 $(PKG)_CONFIGURE_OPTIONS += --without-zeroconf
 $(PKG)_CONFIGURE_OPTIONS += --without-kqueue
@@ -25,7 +70,11 @@ $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	PATH="$(TARGET_PATH)" \
-		$(MAKE) -C $(NGIRCD_DIR)
+		$(MAKE) -C $(NGIRCD_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)" \
+		LDFLAGS="$(TARGET_LDFLAGS) $(NGIRCD_LDFLAGS)" \
+		LIBS="$(NGIRCD_LIBS)"
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
