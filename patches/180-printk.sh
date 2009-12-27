f42bcd52da7837ec8a11af9729a572f6ba24b144
Oliver Metz <oliver@freetz.org>
2009-12-27 15:24:00 +0000

    * Try again to enable kernel messages on boxes with kernel 2.6.19.2 (3270, 7240, 7270)   * please complain in the ticket if you have problems like non working DECT   * refs #254
    git-svn-id: http://svn.freetz.org/trunk@4115 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/180-printk.sh
+++ patches/180-printk.sh
@@ -3,7 +3,7 @@ if isFreetzType 5010 5050 7050 7140 7141 7150 7170; then
 	modsed "s/takeover_printk=1/takeover_printk=0/g" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.S"
 elif isFreetzType 3270 7240 7270 7270_V3; then
 	echo1 "applying printk patch"
-#	modsed "s/AVM_PRINTK/STD_PRINTK/g" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.S"
+	modsed "s/AVM_PRINTK/STD_PRINTK/g" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.S"
 fi
 
 modsed "/^cat \/dev\/debug.*$/d" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.S"
