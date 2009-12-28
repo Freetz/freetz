02ff1c8599877130e4d4207489956ab4b80f9f5a
Gene Rudoy <gene@freetz.org>
2009-12-28 18:18:29 +0000

    lftp:  * add libexpat to dependency list as on systems where it's available lftp automatically links it in and its configure script doesn't provide --without-expat or similar option (it's still possible to get rid of this dependency but we would have to patch configure script for it)  * don't hardcode library path  * use tar.bz2 instead of tar.gz
    git-svn-id: http://svn.freetz.org/trunk@4131 f5190166-0702-4917-9039-51ec32eddaf5

--- make/lftp/lftp.mk
+++ make/lftp/lftp.mk
@@ -1,12 +1,11 @@
 $(call PKG_INIT_BIN, 4.0.5)
-$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SOURCE_MD5:=5400cad5f91e131ac2ded7c24aea594c
 $(PKG)_SITE:=http://ftp.yars.free.net/pub/source/lftp
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/lftp
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/lftp
-$(PKG)_SOURCE_MD5:=73ea519c9e9cdfa47b714c5c91093a0d
 
-$(PKG)_DEPENDS_ON := ncurses readline uclibcxx
-
+$(PKG)_DEPENDS_ON := ncurses readline uclibcxx expat
 
 ifeq ($(strip $(FREETZ_PACKAGE_LFTP_WITH_SSL)),y)
 $(PKG)_DEPENDS_ON += openssl
@@ -15,6 +14,8 @@ endif
 ifeq ($(strip $(FREETZ_PACKAGE_LFTP_STATIC)),y)
 $(PKG)_LDFLAGS := -all-static
 endif
+
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
 
 $(PKG)_CONFIGURE_OPTIONS += --without-libiconv-prefix
 $(PKG)_CONFIGURE_OPTIONS += --without-libintl-prefix
