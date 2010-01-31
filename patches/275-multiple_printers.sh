7eb5077e5718bc001a34f0e8c28a49d82ac9eda1
Markus Hempel <info@markushempel.de>
2010-01-31 12:41:55 +0000

    * version bump 7170 labor preview 29.04.80-16205, disable kernel replace for this firmware * remove md5sum for labor firmwares
    git-svn-id: http://svn.freetz.org/trunk@4257 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/275-multiple_printers.sh
+++ patches/275-multiple_printers.sh
@@ -1,3 +1,8 @@
 [ "$FREETZ_PATCH_MULTIPLE_PRINTERS" == "y" ] || return 0
 	echo1 "adding support for multiple printers"
-	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/275-multiple_printers.patch"
+
+	if isFreetzType 7170 && isFreetzType LABOR_PREVIEW; then
+		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/275-multiple_printers_7170_labor_preview.patch"
+	else
+		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/275-multiple_printers.patch"
+	fi
