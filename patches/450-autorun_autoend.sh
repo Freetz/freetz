e4f2c6668b8bba74877768a471d7e68250dac405
Markus Hempel <info@markushempel.de>
2010-02-23 19:07:53 +0000

    * add new labor NAS for 7270 (only 16MB !!) and 7270_v3
    git-svn-id: http://svn.freetz.org/trunk@4351 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/450-autorun_autoend.sh
+++ patches/450-autorun_autoend.sh
@@ -1,8 +1,8 @@
 [ "$FREETZ_AUTORUN_AUTOEND" == "y" ] || return 0
 echo1 "patching run_mount and storage: autorun/autoend"
-if isFreetzType 7270 7270_V3 && isFreetzType LABOR_PHONE; then
-	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/450-autorun-run_mount_7270_labor_phone.patch"
-	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/450-autoend-storage_7270_labor_phone.patch"
+if isFreetzType 7270 7270_V3 && isFreetzType LABOR_PHONE LABOR_NAS; then
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/450-autorun-run_mount_${FREETZ_TYPE_STRING}.patch"
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/450-autoend-storage_${FREETZ_TYPE_STRING}.patch"
 elif isFreetzType 3270 3270_V3 7141 7170 7240 7270 7270_V3 || \
 	( isFreetzType 7140 && isFreetzType LANG_EN LANG_A_CH); then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/450-autorun-run_mount_7270.patch"
