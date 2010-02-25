2f1859b3e68fcece6cf41dbfb4008e844bd795d3
Markus Hempel <info@markushempel.de>
2010-02-25 07:50:47 +0000

    correct some entrys fpr 7170_labor_dsl (refs #695)
    git-svn-id: http://svn.freetz.org/trunk@4354 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/275-multiple_printers.sh
+++ patches/275-multiple_printers.sh
@@ -1,7 +1,7 @@
 [ "$FREETZ_PATCH_MULTIPLE_PRINTERS" == "y" ] || return 0
 	echo1 "adding support for multiple printers"
 
-	if isFreetzType 7170; then
+	if isFreetzType 7170 && ! isFreetzType LABOR_DSL; then
 		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/275-multiple_printers_7170.patch"
 	else
 		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/275-multiple_printers.patch"
