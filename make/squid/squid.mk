56c71122fba114488a23a46b20350d05e248d63f
Oliver Metz <oliver@freetz.org>
2010-02-25 22:48:17 +0000

    * squid: Add Transparent Proxy support for Linux (Netfilter) systems (patch by BMRMorph)   * refs #696
    git-svn-id: http://svn.freetz.org/trunk@4358 f5190166-0702-4917-9039-51ec32eddaf5

--- make/squid/squid.mk
+++ make/squid/squid.mk
@@ -1,11 +1,13 @@
 $(call PKG_INIT_BIN, 3.0.STABLE9)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SOURCE_MD5:=4009abfbf33d86f40db3ec4280716a0e
 $(PKG)_SITE:=http://www.squid-cache.org/Versions/v$(firstword $(subst ., ,$($(PKG)_VERSION)))/$(firstword $(subst ., ,$($(PKG)_VERSION))).$(word 2,$(subst ., ,$($(PKG)_VERSION)))
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/squid
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/squid
-$(PKG)_SOURCE_MD5:=4009abfbf33d86f40db3ec4280716a0e
 
 $(PKG)_DEPENDS_ON := uclibcxx
+
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_SQUID_TRANSPARENT),--enable-linux-netfilter,--disable-linux-netfilter)
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
