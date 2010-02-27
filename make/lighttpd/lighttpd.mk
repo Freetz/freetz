70d1151bebc4adbe6217af6d3aa38745a272d95f
Gene Rudoy <gene@freetz.org>
2010-02-27 19:41:41 +0000

    lighttpd:  * add Lua and libxml2-based XML support, both optional  * disable mod_mysql_vhost as it doesn't make sense to include it without mysql
    git-svn-id: http://svn.freetz.org/trunk@4371 f5190166-0702-4917-9039-51ec32eddaf5

--- make/lighttpd/lighttpd.mk
+++ make/lighttpd/lighttpd.mk
@@ -28,7 +28,11 @@ $(PKG)_MODULES_TARGET_DIR := $($(PKG)_MODULES:%=$($(PKG)_DEST_DIR)$($(PKG)_MODUL
 $(PKG)_NOT_INCLUDED := $(patsubst %,$($(PKG)_DEST_DIR)$($(PKG)_MODULES_DIR)/mod_%.so,$(filter-out $($(PKG)_MODULES),$($(PKG)_MODULES_ALL)))
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_LIGHTTPD_WITH_SSL
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_LIGHTTPD_WITH_LUA
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_LIGHTTPD_MOD_COMPRESS
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_LIGHTTPD_MOD_WEBDAV_WITH_PROPS
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_LIGHTTPD_MOD_WEBDAV_WITH_LOCKS
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_E2FSPROGS_STATIC
 
 $(PKG)_DEPENDS_ON := pcre
 
@@ -44,12 +48,12 @@ else
 $(PKG)_CONFIGURE_OPTIONS += --without-zlib
 endif
 
-#ifeq ($(strip $(FREETZ_PACKAGE_LIGHTTPD_WITH_LUA)),y)
-#$(PKG)_DEPENDS_ON += lua
-#$(PKG)_CONFIGURE_OPTIONS += --with-lua
-#else
+ifeq ($(strip $(FREETZ_PACKAGE_LIGHTTPD_WITH_LUA)),y)
+$(PKG)_DEPENDS_ON += lua
+$(PKG)_CONFIGURE_OPTIONS += --with-lua
+else
 $(PKG)_CONFIGURE_OPTIONS += --without-lua
-#endif
+endif
 
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
 
@@ -59,7 +63,6 @@ $(PKG)_CONFIGURE_OPTIONS += --libdir=$($(PKG)_MODULES_DIR)
 $(PKG)_CONFIGURE_OPTIONS += --sysconfdir=/etc/lighttpd
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
-$(PKG)_CONFIGURE_OPTIONS += --disable-rpath
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_TARGET_IPV6_SUPPORT),,--disable-ipv6)
 $(PKG)_CONFIGURE_OPTIONS += --without-attr
 $(PKG)_CONFIGURE_OPTIONS += --without-bzip2
@@ -69,11 +72,21 @@ $(PKG)_CONFIGURE_OPTIONS += --without-ldap
 $(PKG)_CONFIGURE_OPTIONS += --without-memcache
 $(PKG)_CONFIGURE_OPTIONS += --with-pcre="yes"
 $(PKG)_CONFIGURE_OPTIONS += --without-valgrind
-
-# TODO: does it make sense to include mod_mysql_vhost module having mysql disabled?
 $(PKG)_CONFIGURE_OPTIONS += --without-mysql
-# TODO: this needs libxml2 and sqlite, does it make sense to include mod_webdav without libxml2?
-#$(PKG)_CONFIGURE_OPTIONS += --with-webdav-props
+
+ifeq ($(strip $(FREETZ_PACKAGE_LIGHTTPD_MOD_WEBDAV_WITH_PROPS)),y)
+$(PKG)_DEPENDS_ON += libxml2 sqlite
+$(PKG)_CONFIGURE_OPTIONS += --with-webdav-props
+else
+$(PKG)_CONFIGURE_OPTIONS += --without-webdav-props
+endif
+
+ifeq ($(strip $(FREETZ_PACKAGE_LIGHTTPD_MOD_WEBDAV_WITH_LOCKS)),y)
+$(PKG)_DEPENDS_ON += e2fsprogs # we need libuuid from it
+$(PKG)_CONFIGURE_OPTIONS += --with-webdav-locks
+else
+$(PKG)_CONFIGURE_OPTIONS += --without-webdav-locks
+endif
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
