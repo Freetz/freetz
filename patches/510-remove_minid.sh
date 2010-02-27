e12bdc047e5b97550dbf90864865460f8359c787
Oliver Metz <oliver@freetz.org>
2010-02-27 09:52:41 +0000

    [freetz-stable-1.1] Merge in r4302:4303, r4307, r4318 from trunk: ------------------------------------------------------------------------ r4302 | oliver | 2010-02-15 07:46:37 +0000 (Mo, 15 Feb 2010) | 1 line  * Update README ------------------------------------------------------------------------ r4303 | markuschen | 2010-02-16 07:22:01 +0000 (Di, 16 Feb 2010) | 1 line remove minid: correct one dependency (libavm_audio.so needed by aurad ------------------------------------------------------------------------ r4307 | markuschen | 2010-02-17 08:09:06 +0000 (Mi, 17 Feb 2010) | 1 line * add (gnu) tar to prerequisites (special options needed by kernel build) ------------------------------------------------------------------------ r4318 | cuma | 2010-02-18 01:03:29 +0000 (Do, 18 Feb 2010) | 1 line added favicon by atomphil (Freetz-Webinterface only) ------------------------------------------------------------------------
    git-svn-id: http://svn.freetz.org/branches/freetz-stable-1.1@4365 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/510-remove_minid.sh
+++ patches/510-remove_minid.sh
@@ -10,10 +10,20 @@ for files in \
 	bin/telephon.plugin \
 	bin/update.plugin \
 	etc/minid \
+	lib/libavcodec.so* \
+	lib/libavformat.so* \
+	lib/libavutil.so* \
+	lib/libflashclient.so* \
+	lib/libhttp.so* \
+	lib/libmediacli.so* \
 	usr/share/ctlmgr/libmini.so \
 	; do
 	rm_files "${FILESYSTEM_MOD_DIR}/$files"
 done
+
+[ "$FREETZ_REMOVE_VOIPD" == "y" ] && rm_files "${FILESYSTEM_MOD_DIR}/lib/libfoncclient*"
+[ "$FREETZ_REMOVE_MEDIASRV" == "y" ] && rm_files "${FILESYSTEM_MOD_DIR}/lib/libavmid3*.so*"
+[ "$FREETZ_REMOVE_AURA_USB" == "y" ] && rm_files "${FILESYSTEM_MOD_DIR}/lib/libavm_audio.so*"
 
 echo1 "patching rc.conf"
 modsed "s/CONFIG_MINI=.*$/CONFIG_MINI=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
