a6efc66ed74bfc9a4c6a01aee76134498274a70f
Oliver Metz <oliver@freetz.org>
2010-02-27 16:16:12 +0000

    * squid: Bump to version 3.0.STABLE24 (Patch by BMRMorph)   * fixes #698
    git-svn-id: http://svn.freetz.org/trunk@4367 f5190166-0702-4917-9039-51ec32eddaf5

--- make/squid/squid.mk
+++ make/squid/squid.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 3.0.STABLE9)
+$(call PKG_INIT_BIN, 3.0.STABLE24)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=4009abfbf33d86f40db3ec4280716a0e
+$(PKG)_SOURCE_MD5:=325c8977b64397666bf538d54bb6f128
 $(PKG)_SITE:=http://www.squid-cache.org/Versions/v$(firstword $(subst ., ,$($(PKG)_VERSION)))/$(firstword $(subst ., ,$($(PKG)_VERSION))).$(word 2,$(subst ., ,$($(PKG)_VERSION)))
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/squid
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/squid
