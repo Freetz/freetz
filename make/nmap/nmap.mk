8910fab3dacda9b5032e853564ffa802f497ecdd
Gene Rudoy <gene@freetz.org>
2010-02-28 10:45:55 +0000

    nmap:  * fix patches & makefile (libpcap was always linked in statically)  * provide options allowing liblua and libpcre to be linked in dynamically
    git-svn-id: http://svn.freetz.org/trunk@4375 f5190166-0702-4917-9039-51ec32eddaf5

--- make/nmap/nmap.mk
+++ make/nmap/nmap.mk
@@ -1,19 +1,31 @@
 $(call PKG_INIT_BIN, 4.68)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SITE:=http://nmap.org/dist
-$(PKG)_BINARY:=$($(PKG)_DIR)/nmap
-$(PKG)_SERVICES_LIST:=$($(PKG)_DIR)/nmap-services
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/nmap
-$(PKG)_TARGET_SERVICES_LIST:=$($(PKG)_DEST_DIR)/usr/share/nmap/nmap-services
 $(PKG)_SOURCE_MD5:=c363d32a00c697d15996fced22072b6c
+$(PKG)_SITE:=http://nmap.org/dist
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/nmap
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/nmap
+$(PKG)_SERVICES_LIST:=$($(PKG)_DIR)/nmap-services
+$(PKG)_TARGET_SERVICES_LIST:=$($(PKG)_DEST_DIR)/usr/share/nmap/nmap-services
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_NMAP_WITH_SHARED_LUA
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_NMAP_WITH_SHARED_PCRE
 
 $(PKG)_DEPENDS_ON := uclibcxx libpcap
+ifeq ($(strip $(FREETZ_PACKAGE_NMAP_WITH_SHARED_LUA)),y)
+$(PKG)_DEPENDS_ON += lua
+$(PKG)_CONFIGURE_ENV += LUAINCLUDE=-I"$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/lua"
+endif
+$(PKG)_CONFIGURE_ENV += ac_cv_header_lua_h=$(if $(FREETZ_PACKAGE_NMAP_WITH_SHARED_LUA),yes,no)
+ifeq ($(strip $(FREETZ_PACKAGE_NMAP_WITH_SHARED_PCRE)),y)
+$(PKG)_DEPENDS_ON += pcre
+endif
 
-$(PKG)_CONFIGURE_OPTIONS += --with-liblua=included
-$(PKG)_CONFIGURE_OPTIONS += --with-pcap=linux
-$(PKG)_CONFIGURE_OPTIONS += --with-pcre=included
+$(PKG)_CONFIGURE_OPTIONS += --with-libpcap="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+$(PKG)_CONFIGURE_OPTIONS += --with-liblua=$(if $(FREETZ_PACKAGE_NMAP_WITH_SHARED_LUA),"$(TARGET_TOOLCHAIN_STAGING_DIR)/usr",included)
+$(PKG)_CONFIGURE_OPTIONS += --with-libpcre=$(if $(FREETZ_PACKAGE_NMAP_WITH_SHARED_PCRE),"$(TARGET_TOOLCHAIN_STAGING_DIR)/usr",included)
+$(PKG)_CONFIGURE_OPTIONS += --with-libdnet=included
 $(PKG)_CONFIGURE_OPTIONS += --without-openssl
-$(PKG)_CONFIGURE_OPTIONS += --with-pcap=linux
 $(PKG)_CONFIGURE_OPTIONS += --with-nmapfe=no
 $(PKG)_CONFIGURE_OPTIONS += --without-zenmap
 
