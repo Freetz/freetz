14b1cfb3bfc65148004bba28424be50fd9c9bd4e
cuma <cuma@freetz.org>
2010-03-01 18:00:08 +0000

    minicom 2.4 added
    git-svn-id: http://svn.freetz.org/trunk@4390 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/minicom/minicom.mk
@@ -0,0 +1,31 @@
+$(call PKG_INIT_BIN,2.4)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=http://alioth.debian.org/frs/download.php/3195
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/minicom
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/minicom
+$(PKG)_SOURCE_MD5:=700976a3c2dcc8bbd50ab9bb1c08837b
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	PATH="$(TARGET_PATH)" \
+		$(MAKE) -C $(MINICOM_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(MINICOM_DIR) clean
+
+$(pkg)-uninstall:
+	$(RM) $(MINICOM_TARGET_BINARY)
+
+$(PKG_FINISH)
