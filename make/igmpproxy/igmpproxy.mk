760e2891724e9a041e7beaeb12bfbffbf7e9b655
Oliver Metz <oliver@freetz.org>
2010-03-05 13:46:30 +0000

    * Add new binary only package igmpproxy (by leibezem)
    git-svn-id: http://svn.freetz.org/trunk@4416 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/igmpproxy/igmpproxy.mk
@@ -0,0 +1,29 @@
+$(call PKG_INIT_BIN, 0.1)
+$(PKG)_SOURCE:=igmpproxy-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=C56F41EC195BC1FE016369BF74EFC5A1
+$(PKG)_SITE:=http://surfnet.dl.sourceforge.net/project/igmpproxy/igmpproxy/0.1
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/igmpproxy
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/igmpproxy
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(IGMPPROXY_DIR)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(IGMPPROXY_DIR) clean
+	$(RM) $(IGMPPROXY_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(IGMPPROXY_TARGET_BINARY)
+
+$(PKG_FINISH)
