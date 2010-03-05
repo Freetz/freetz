cb8438ff82c694904ba65a2cd342e4fe799b3117
Whoopie <whoopie79@gmx.net>
2010-03-05 15:19:15 +0000

    igmpproxy: two small changes
    git-svn-id: http://svn.freetz.org/trunk@4419 f5190166-0702-4917-9039-51ec32eddaf5

--- make/igmpproxy/igmpproxy.mk
+++ make/igmpproxy/igmpproxy.mk
@@ -1,7 +1,7 @@
 $(call PKG_INIT_BIN, 0.1)
-$(PKG)_SOURCE:=igmpproxy-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5:=C56F41EC195BC1FE016369BF74EFC5A1
-$(PKG)_SITE:=http://surfnet.dl.sourceforge.net/project/igmpproxy/igmpproxy/0.1
+$(PKG)_SITE:=@SF/igmpproxy
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/igmpproxy
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/igmpproxy
 
