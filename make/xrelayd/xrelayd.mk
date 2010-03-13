268730665032c2ddbae8c91ed32120554d882e23
Gene Rudoy <gene@freetz.org>
2010-03-13 20:55:54 +0000

    xrelayd:  * link against polarssl  * refs #715
    git-svn-id: http://svn.freetz.org/trunk@4474 f5190166-0702-4917-9039-51ec32eddaf5

--- make/xrelayd/xrelayd.mk
+++ make/xrelayd/xrelayd.mk
@@ -1,11 +1,12 @@
 $(call PKG_INIT_BIN, 0.2.1pre2)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=05f242295fa864bb3b0b7f0712b4dfa3
+
 $(PKG)_SITE:=http://znerol.ch/files
 $(PKG)_BINARY:=$($(PKG)_DIR)/xrelayd
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/xrelayd
-$(PKG)_SOURCE_MD5:=05f242295fa864bb3b0b7f0712b4dfa3
 
-$(PKG)_DEPENDS_ON := xyssl
+$(PKG)_DEPENDS_ON := polarssl
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -15,7 +16,7 @@ $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(XRELAYD_DIR) \
 		CC="$(TARGET_CC)" \
 		CFLAGS="$(TARGET_CFLAGS)" \
-		LD="$(TARGET_CC)" \
+		LD="$(TARGET_CC)"
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
