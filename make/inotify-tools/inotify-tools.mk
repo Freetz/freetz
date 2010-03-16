81d21c9397a55c3c7eafecf976d790c1d75dc582
Oliver Metz <oliver@freetz.org>
2010-03-16 19:15:28 +0000

    * inotify-tools: Bump to version 3.14
    git-svn-id: http://svn.freetz.org/trunk@4498 f5190166-0702-4917-9039-51ec32eddaf5

--- make/inotify-tools/inotify-tools.mk
+++ make/inotify-tools/inotify-tools.mk
@@ -1,14 +1,14 @@
-$(call PKG_INIT_BIN, 3.13)
+$(call PKG_INIT_BIN, 3.14)
 $(PKG)_LIB_VERSION:=0.4.1
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SITE:=@SF/inotify-tools
+$(PKG)_SOURCE_MD5:=b43d95a0fa8c45f8bab3aec9672cf30c
+$(PKG)_SITE:=http://cloud.github.com/downloads/rvoicilas/inotify-tools
 $(PKG)_INWAIT_BINARY:=$($(PKG)_DIR)/src/.libs/inotifywait
 $(PKG)_INWATCH_BINARY:=$($(PKG)_DIR)/src/.libs/inotifywatch
 $(PKG)_LIB_BINARY:=$($(PKG)_DIR)/libinotifytools/src/.libs/libinotifytools.so.$($(PKG)_LIB_VERSION)
 $(PKG)_INWAIT_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/inotifywait
 $(PKG)_INWATCH_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/inotifywatch
 $(PKG)_LIB_TARGET_BINARY:=$($(PKG)_DEST_LIBDIR)/libinotifytools.so.$($(PKG)_LIB_VERSION)
-$(PKG)_SOURCE_MD5:=35d7178297390f18bae451e083362acf
 
 $(PKG)_CONFIGURE_ENV += LD="$(TARGET_LD)"
 
