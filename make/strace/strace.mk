c0e6051c5bf00df03db883c2309eb0b6ee2ccb1c
Oliver Metz <oliver@freetz.org>
2010-03-17 22:45:14 +0000

    * strace: Bump to 4.5.19
    git-svn-id: http://svn.freetz.org/trunk@4511 f5190166-0702-4917-9039-51ec32eddaf5

--- make/strace/strace.mk
+++ make/strace/strace.mk
@@ -1,9 +1,9 @@
-$(call PKG_INIT_BIN, 4.5.18)
+$(call PKG_INIT_BIN, 4.5.19)
 $(PKG)_SOURCE:=strace-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SOURCE_MD5:=2415e435d61e40315a298c80aced0cda
 $(PKG)_SITE:=@SF/strace
 $(PKG)_BINARY:=$($(PKG)_DIR)/strace
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/strace
-$(PKG)_SOURCE_MD5:=e9449fcee97e6a8ed73934c883c870e0
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
