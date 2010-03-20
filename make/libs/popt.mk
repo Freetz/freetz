da961e7088ac5686eebdc4860ca75df1bb83a757
Gene Rudoy <gene@freetz.org>
2010-03-20 22:46:14 +0000

    popt:  * don't depend on autotools-1.10, patch all required files instead  * don't replace libtool as there is no need for it  * should fix problems reported in these IPF-threads http://www.ip-phone-forum.de/showthread.php?t=211897 & http://www.ip-phone-forum.de/showthread.php?t=191414
    git-svn-id: http://svn.freetz.org/trunk@4563 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/popt.mk
+++ make/libs/popt.mk
@@ -1,21 +1,21 @@
 $(call PKG_INIT_LIB, 1.15)
 $(PKG)_LIB_VERSION:=0.0.0
-$(PKG)_SOURCE:=popt-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=c61ef795fa450eb692602a661ec8d7f1
 $(PKG)_SITE:=http://rpm5.org/files/popt
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/.libs/libpopt.so.$($(PKG)_LIB_VERSION)
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpopt.so.$($(PKG)_LIB_VERSION)
 $(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libpopt.so.$($(PKG)_LIB_VERSION)
-$(PKG)_SOURCE_MD5:=c61ef795fa450eb692602a661ec8d7f1
 
-$(PKG)_CONFIGURE_PRE_CMDS += autoconf --force ; autoreconf -f -i ;
+# touch configure.ac to prevent configure from being regenerated
+$(PKG)_CONFIGURE_PRE_CMDS += touch -t 200001010000.00 ./configure.ac;
 
 $(PKG)_CONFIGURE_ENV += ac_cv_va_copy=yes
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
 $(PKG)_CONFIGURE_OPTIONS += --without-libintl-prefix
 $(PKG)_CONFIGURE_OPTIONS += --without-libiconv-prefix
-
-$(call REPLACE_LIBTOOL)
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
