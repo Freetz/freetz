e5e2ff1c581a65d1528e2aebf9112a32f353e519
Gene Rudoy <gene@freetz.org>
2010-03-21 15:13:13 +0000

    * add new package umurmur, by sf3978 (binary only, web-interface/startup-script are on the way) * refs #714
    git-svn-id: http://svn.freetz.org/trunk@4574 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/umurmur/umurmur.mk
@@ -0,0 +1,36 @@
+$(call PKG_INIT_BIN, 0.2.2)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=a841cf38de7fbc8bdacafbd481561da7
+$(PKG)_SITE:=http://$(pkg).googlecode.com/files
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/$(pkg)d
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/$(pkg)d
+
+$(PKG)_DEPENDS_ON := polarssl libconfig
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(UMURMUR_DIR)/src \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS) -DUSE_POLARSSL" \
+		EXTRA_LDFLAGS="-lpolarssl" \
+		AR="$(TARGET_CROSS)ar"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(UMURMUR_DIR)/src clean
+	$(RM) $(UMURMUR_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(UMURMUR_TARGET_BINARY)
+
+$(PKG_FINISH)
