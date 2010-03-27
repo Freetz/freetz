98bb2b48168ca0882c4baca538a404ee4eea3623
Gene Rudoy <gene@freetz.org>
2010-03-27 18:38:35 +0000

    cyassl:  * bump version to 1.4.0  * fix dependencies: add missing libz dependency, remove unnecessary openssl one  * install header files
    git-svn-id: http://svn.freetz.org/trunk@4603 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/cyassl.mk
+++ make/libs/cyassl.mk
@@ -1,16 +1,17 @@
-$(call PKG_INIT_LIB, 1.0.3)
+$(call PKG_INIT_LIB, 1.4.0)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).zip
-$(PKG)_SOURCE_MD5:=e9e85a2d78cd535a049e4acce786e42d
+$(PKG)_SOURCE_MD5:=037397c7df84b9a12e614bf46135df1c
 $(PKG)_SITE:=http://yassl.com
 
-$(PKG)_BINARY:=$($(PKG)_DIR)/src/.libs/libcyassl.so
-$(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libcyassl.so
-$(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libcyassl.so
+$(PKG)_LIBNAME:=libcyassl.so.0.0.0
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/.libs/$($(PKG)_LIBNAME)
+$(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/$($(PKG)_LIBNAME)
+$(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/$($(PKG)_LIBNAME)
 
-$(PKG)_DEPENDS_ON := openssl zlib
+$(PKG)_DEPENDS_ON := zlib
 
 $(PKG)_CONFIGURE_OPTIONS += --enable-opensslExtra
-$(PKG)_CONFIGURE_OPTIONS += --with-zlib="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+$(PKG)_CONFIGURE_OPTIONS += --with-libz="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -20,6 +21,8 @@ $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(CYASSL_DIR)
 
 $($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
+	mkdir -p $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/cyassl; \
+	cp -a $(CYASSL_DIR)/include/openssl/*.h $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/cyassl; \
 	$(INSTALL_LIBRARY)
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
