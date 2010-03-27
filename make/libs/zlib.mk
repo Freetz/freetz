8a851c418bd0e9f96d520529a1a4d633fdaad1e4
Gene Rudoy <gene@freetz.org>
2010-03-27 20:00:22 +0000

    zlib:  * bump version to 1.2.4  * install it to /usr/lib/freetz
    git-svn-id: http://svn.freetz.org/trunk@4604 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/zlib.mk
+++ make/libs/zlib.mk
@@ -1,17 +1,22 @@
-$(call PKG_INIT_LIB, 1.2.3)
+$(call PKG_INIT_LIB, 1.2.4)
 $(PKG)_LIB_VERSION:=$($(PKG)_VERSION)
-$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SOURCE_MD5:=763c6a0b4ad1cdf5549e3ab3f140f4cb
 $(PKG)_SITE:=@SF/libpng
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/libz.so.$($(PKG)_LIB_VERSION)
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libz.so.$($(PKG)_LIB_VERSION)
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/lib/libz.so.$($(PKG)_LIB_VERSION)
-$(PKG)_SOURCE_MD5:=debc62758716a169df9f62e6ab2bc634
+$(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libz.so.$($(PKG)_LIB_VERSION)
 
 $(PKG)_CONFIGURE_DEFOPTS := n
 $(PKG)_CONFIGURE_ENV += CC="$(TARGET_CC)"
 $(PKG)_CONFIGURE_ENV += CFLAGS="$(TARGET_CFLAGS)"
+$(PKG)_CONFIGURE_ENV += AR="$(TARGET_CROSS)ar"
+$(PKG)_CONFIGURE_ENV += RANLIB="$(TARGET_CROSS)ranlib"
 $(PKG)_CONFIGURE_ENV += prefix=/usr
-$(PKG)_CONFIGURE_OPTIONS += --shared
+
+# we could make a patch for it, but as all changes are absolutely identical it's simpler to do it per sed
+$(PKG)_CONFIGURE_PRE_CMDS += $(SED) -i -r -e 's,test "`([(][^)]+[)] 2>)&1`" = "",\1>config.log,g' ./configure;
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -19,7 +24,7 @@ $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(ZLIB_DIR) \
-		all libz.a
+		all
 
 $($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
 	$(SUBMAKE) -C $(ZLIB_DIR) \
@@ -35,9 +40,10 @@ $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(ZLIB_DIR) clean
-	$(RM) $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libz.* \
-			$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/zlib.h \
-			$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/zconf.h
+	$(RM) \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libz.* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/zlib.h \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/zconf.h
 
 $(pkg)-uninstall:
 	$(RM) $(ZLIB_TARGET_DIR)/libz*.so*
