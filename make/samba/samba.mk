25718d456e45bb4d5072b8d7468d607f764d3a3b
Gene Rudoy <gene@freetz.org>
2010-03-27 15:20:53 +0000

    samba:  * revise makefile  * add missing AR and TARGETFS variables
    git-svn-id: http://svn.freetz.org/trunk@4598 f5190166-0702-4917-9039-51ec32eddaf5

--- make/samba/samba.mk
+++ make/samba/samba.mk
@@ -1,62 +1,42 @@
 $(call PKG_INIT_BIN, 3.0.24)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SITE:=ftp://de3.$(pkg).org/pub/$(pkg)/old-versions
-$(PKG)_SMBP_BINARY:=$($(PKG)_DIR)/source/bin/smbpasswd
-$(PKG)_SMBP_TARGET_BINARY:=$($(PKG)_DEST_DIR)/sbin/smbpasswd
-$(PKG)_SMBD_BINARY:=$($(PKG)_DIR)/source/bin/smbd
-$(PKG)_SMBD_TARGET_BINARY:=$($(PKG)_DEST_DIR)/sbin/smbd
-$(PKG)_NMBD_BINARY:=$($(PKG)_DIR)/source/bin/nmbd
-$(PKG)_NMBD_TARGET_BINARY:=$($(PKG)_DEST_DIR)/sbin/nmbd
-$(PKG)_STARTLEVEL=80
 $(PKG)_SOURCE_MD5:=89273f67a6d8067cbbecefaa13747153
+$(PKG)_SITE:=ftp://de3.$(pkg).org/pub/$(pkg)/old-versions
 
-#$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_NMBD
+$(PKG)_STARTLEVEL=80
+
+$(PKG)_BINARIES_ALL := smbpasswd smbd nmbd
+$(PKG)_BINARIES := $(filter-out $(if $(FREETZ_PACKAGE_NMBD),,nmbd),$($(PKG)_BINARIES_ALL))
+$(PKG)_BINARIES_BUILD_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DIR)/source/bin/%)
+$(PKG)_BINARIES_TARGET_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DEST_DIR)/sbin/%)
+$(PKG)_NOT_INCLUDED := $(patsubst %,$($(PKG)_DEST_DIR)/sbin/%,$(filter-out $($(PKG)_BINARIES),$($(PKG)_BINARIES_ALL)))
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_NOP)
 
-$($(PKG)_SMBP_BINARY) $($(PKG)_SMBD_BINARY) $($(PKG)_NMBD_BINARY): $($(PKG)_DIR)/.configured
+$($(PKG)_BINARIES_BUILD_DIR): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(SAMBA_DIR)/source \
-		CC=$(TARGET_CC) \
-		LD=$(TARGET_LD) \
+		CC="$(TARGET_CC)" \
+		LD="$(TARGET_LD)" \
+		AR="$(TARGET_CROSS)ar" \
+		TARGETFS="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
 		SAMBA_CFLAGS="$(TARGET_CFLAGS)" \
 		CODEPAGEDIR="/mod/usr/share/samba" \
-		proto
-	$(SUBMAKE) -C $(SAMBA_DIR)/source \
-		CC=$(TARGET_CC) \
-		LD=$(TARGET_LD) \
-		SAMBA_CFLAGS="$(TARGET_CFLAGS)" \
-		CODEPAGEDIR="/mod/usr/share/samba" \
-		all
+		proto all
 
-$($(PKG)_SMBP_TARGET_BINARY): $($(PKG)_SMBP_BINARY)
-	$(INSTALL_BINARY_STRIP)
-
-$($(PKG)_SMBD_TARGET_BINARY): $($(PKG)_SMBD_BINARY)
-	$(INSTALL_BINARY_STRIP)
-
-$($(PKG)_NMBD_TARGET_BINARY): $($(PKG)_NMBD_BINARY)
+$($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/sbin/%: $($(PKG)_DIR)/source/bin/%
 	$(INSTALL_BINARY_STRIP)
 
 $(pkg):
 
-ifeq ($(strip $(FREETZ_PACKAGE_NMBD)),y)
-$(pkg)-precompiled: $($(PKG)_SMBP_TARGET_BINARY) $($(PKG)_SMBD_TARGET_BINARY) $($(PKG)_NMBD_TARGET_BINARY)
-else
-$(pkg)-precompiled: $($(PKG)_SMBP_TARGET_BINARY) $($(PKG)_SMBD_TARGET_BINARY) $(pkg)-clean-nmbd
-endif
-
-$(pkg)-clean-nmbd:
-	$(RM) $(SAMBA_NMBD_TARGET_BINARY)
+$(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(SAMBA_DIR)/source clean
 	$(RM) -r $(SAMBA_DIR)/source/bin
 
 $(pkg)-uninstall:
-	$(RM) $(SAMBA_SMBP_TARGET_BINARY) \
-		$(SAMBA_SMBD_TARGET_BINARY) \
-		$(SAMBA_NMBD_TARGET_BINARY)
+	$(RM) $(SAMBA_BINARIES_ALL:%=$(SAMBA_DEST_DIR)/sbin/%)
 
 $(PKG_FINISH)
