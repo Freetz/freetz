9b1391f5f8a1a0f0720bf6158613364f47afc76e
Gene Rudoy <gene@freetz.org>
2010-03-27 13:17:12 +0000

    xpdf:  * use recommended variable naming scheme (i.e. FREETZ_PACKAGE_XPDF_* instead of FREETZ_XPDF_*), this also fixes the bug introduced in r4102 and caused no binary to be installed  * install library in /usr/lib/freetz  * add two security patches from official homepage
    git-svn-id: http://svn.freetz.org/trunk@4595 f5190166-0702-4917-9039-51ec32eddaf5

--- make/xpdf/xpdf.mk
+++ make/xpdf/xpdf.mk
@@ -1,14 +1,14 @@
 $(call PKG_INIT_BIN, 3.02)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5:=599dc4cc65a07ee868cf92a667a913d2
-$(PKG)_SITE:=ftp://ftp.foolabs.com/pub/xpdf/
+$(PKG)_SITE:=ftp://ftp.foolabs.com/pub/$(pkg)
 
 $(PKG)_BINARIES_ALL := pdftops pdftotext pdfinfo pdffonts pdfimages
 $(PKG)_BINARIES := $(call PKG_SELECTED_SUBOPTIONS,$($(PKG)_BINARIES_ALL))
 $(PKG)_BINARIES_BUILD_DIR := $(addprefix $($(PKG)_DIR)/xpdf/,$($(PKG)_BINARIES))
 $(PKG)_BINARIES_TARGET_DIR := $(addprefix $($(PKG)_DEST_DIR)/usr/bin/,$($(PKG)_BINARIES))
 $(PKG)_LIBS_BUILD_DIR := $($(PKG)_DIR)/xpdf/libxpdf.so.1
-$(PKG)_LIBS_TARGET_DIR := $($(PKG)_DEST_DIR)/usr/lib/libxpdf.so.1
+$(PKG)_LIBS_TARGET_DIR := $($(PKG)_DEST_LIBDIR)/libxpdf.so.1
 $(PKG)_NOT_INCLUDED := $(patsubst %,$($(PKG)_DEST_DIR)/usr/bin/%,$(filter-out $($(PKG)_BINARIES),$($(PKG)_BINARIES_ALL)))
 
 $(PKG)_DEPENDS_ON := uclibcxx
@@ -31,7 +31,7 @@ $($(PKG)_BINARIES_BUILD_DIR) $($(PKG)_LIBS_BUILD_DIR): $($(PKG)_DIR)/.configured
 $($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/xpdf/%
 	$(INSTALL_BINARY_STRIP)
 
-$($(PKG)_LIBS_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/lib/%: $($(PKG)_DIR)/xpdf/%
+$($(PKG)_LIBS_TARGET_DIR): $($(PKG)_DEST_LIBDIR)/%: $($(PKG)_DIR)/xpdf/%
 	$(INSTALL_LIBRARY_STRIP)
 
 $(pkg):
@@ -42,6 +42,6 @@ $(pkg)-clean:
 	-$(SUBMAKE) -C $(XPDF_DIR) clean
 
 $(pkg)-uninstall:
-	$(RM) $(XPDF_BINARIES_ALL:%=$(XPDF_DEST_DIR)/usr/bin/%) $(XPDF_DEST_DIR)/usr/lib/libxpdf*
+	$(RM) $(XPDF_BINARIES_ALL:%=$(XPDF_DEST_DIR)/usr/bin/%) $(XPDF_DEST_LIBDIR)/libxpdf*
 
 $(PKG_FINISH)
