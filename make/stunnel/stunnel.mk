d463667237e2ee417ffffc8e068307b9877962c5
Gene Rudoy <gene@freetz.org>
2010-03-28 00:36:40 +0000

    stunnel:  * provide an option allowing stunnel to be linked against cyassl  * WARNING: experimental, very slow & errors in syslog
    git-svn-id: http://svn.freetz.org/trunk@4607 f5190166-0702-4917-9039-51ec32eddaf5

--- make/stunnel/stunnel.mk
+++ make/stunnel/stunnel.mk
@@ -1,14 +1,21 @@
 $(call PKG_INIT_BIN,4.32)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=72379c615c5a4986c7981d0941ed2e6b
 $(PKG)_SITE:=http://www.stunnel.org/download/stunnel/src
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/$(pkg)
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/$(pkg)
 $(PKG)_STARTLEVEL=30
-$(PKG)_SOURCE_MD5:=72379c615c5a4986c7981d0941ed2e6b
 
-$(PKG)_DEPENDS_ON := openssl zlib
-
+$(PKG)_DEPENDS_ON := zlib
+ifeq ($(strip $(FREETZ_PACKAGE_STUNNEL_USE_CYASSL)),y)
+$(PKG)_DEPENDS_ON += cyassl
+$(PKG)_LIBS := -lutil -lcyassl -lz -lm -lpthread
+$(PKG)_CONFIGURE_ENV += OPENSSL_ALTERNATIVE=cyassl
+else
+$(PKG)_DEPENDS_ON += openssl
 $(PKG)_LIBS := -lutil -lssl -lcrypto -lz -ldl -lpthread
+endif
 
 ifeq ($(strip $(FREETZ_PACKAGE_STUNNEL_STATIC)),y)
 $(PKG)_LDFLAGS := -all-static
@@ -21,13 +28,14 @@ $(PKG)_CONFIGURE_OPTIONS += --with-ssl="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_TARGET_IPV6_SUPPORT),--enable-ipv6,--disable-ipv6)
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_STUNNEL_STATIC
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_STUNNEL_USE_CYASSL
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(STUNNEL_DIR)\
+	$(SUBMAKE) -C $(STUNNEL_DIR) \
 		LDFLAGS="$(STUNNEL_LDFLAGS)" \
 		LIBS="$(STUNNEL_LIBS)"
 
