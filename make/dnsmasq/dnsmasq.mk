1da729582e5275040755cee67c861ccd904d0d3b
Oliver Metz <oliver@freetz.org>
2010-04-02 15:50:29 +0000

    * dnsmasq: Bump to version 2.52
    git-svn-id: http://svn.freetz.org/trunk@4640 f5190166-0702-4917-9039-51ec32eddaf5

--- make/dnsmasq/dnsmasq.mk
+++ make/dnsmasq/dnsmasq.mk
@@ -1,11 +1,10 @@
-$(call PKG_INIT_BIN, 2.51)
+$(call PKG_INIT_BIN, 2.52)
 $(PKG)_SOURCE:=dnsmasq-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=1bb32fffdb4f977ead607802b5d701d0
 $(PKG)_SITE:=http://thekelleys.org.uk/dnsmasq
-$(PKG)_DIR:=$(SOURCE_DIR)/dnsmasq-$($(PKG)_VERSION)
 $(PKG)_BINARY:=$(DNSMASQ_DIR)/src/dnsmasq
 $(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/root/usr/sbin/dnsmasq
 $(PKG)_STARTLEVEL=40
-$(PKG)_SOURCE_MD5:=97465261a6de5258a3c3edfe51ca16a4
 
 ifneq ($(FREETZ_TARGET_IPV6_SUPPORT),y) 
 $(PKG)_COPTS := -DNO_IPV6
