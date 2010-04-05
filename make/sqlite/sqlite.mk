98b2d67c42f8f460b303f6d7a33dda382bbbb90c
Gene Rudoy <gene@freetz.org>
2010-04-05 01:39:41 +0000

    sqlite:  * bump version to 3.6.23.1  * add optional readline support
    git-svn-id: http://svn.freetz.org/trunk@4664 f5190166-0702-4917-9039-51ec32eddaf5

--- make/sqlite/sqlite.mk
+++ make/sqlite/sqlite.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_BIN, 3.6.23)
+$(call PKG_INIT_BIN, 3.6.23.1)
 $(PKG)_LIB_VERSION:=0.8.6
 $(PKG)_SOURCE:=$(pkg)-amalgamation-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=8f1e86b3909a27f8122b0981afd16fcd
+$(PKG)_SOURCE_MD5:=ed585bb3d4e5c643843ebb1e318644ce
 $(PKG)_SITE:=http://www.sqlite.org
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/.libs/sqlite3
@@ -12,7 +12,7 @@ $(PKG)_LIB_TARGET_BINARY:=$($(PKG)_TARGET_LIBDIR)/libsqlite3.so.$($(PKG)_LIB_VER
 
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
-$(PKG)_CONFIGURE_OPTIONS += --disable-readline
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_SQLITE_WITH_READLINE),--enable-readline,--disable-readline)
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
