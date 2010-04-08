6ec2672ec8a26df36a1682aa2b8733b67f4f0bfc
Oliver Metz <oliver@freetz.org>
2010-04-08 21:04:29 +0000

    * Bump firmware versions:   * Fritz!Box Fon WLAN 7270 (A-CH): 54.04.81   * Fritz!Box Fon WLAN 7270 (English): 54.04.81
    git-svn-id: http://svn.freetz.org/trunk@4680 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/480-alarmclock.sh
+++ patches/480-alarmclock.sh
@@ -6,7 +6,9 @@ if isFreetzType 7150 && isFreetzType LANG_DE; then
 elif isFreetzType 7112 7141 7170 && isFreetzType LANG_DE; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/${FREETZ_TYPE_LANG_STRING}/480-alarmclock.patch"
 elif isFreetzType 3270_V3; then
-	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/480-alarmclock_3020v3.patch"
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/480-alarmclock_3270v3.patch"
+elif isFreetzType 7270 && ! isFreetzType LANG_DE; then
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/${FREETZ_TYPE_LANG_STRING}/480-alarmclock_7270.patch"
 elif isFreetzType 7270 7270_V3 && isFreetzType LABOR; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/480-alarmclock_7270_labor.patch"
 elif isFreetzType 3270 7240 7270 7270_V3; then
