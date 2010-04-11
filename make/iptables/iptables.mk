5be207637b890ee1e11847b92103b6d83b93038c
Gene Rudoy <gene@freetz.org>
2010-04-11 00:10:43 +0000

    iptables:  * revise makefile, use $(PKG)_CONDITIONAL_PATCHES variable instead of cloning the .unpacked rule  * minor cleanups
    git-svn-id: http://svn.freetz.org/trunk@4707 f5190166-0702-4917-9039-51ec32eddaf5

--- make/iptables/iptables.mk
+++ make/iptables/iptables.mk
@@ -1,62 +1,54 @@
 $(call PKG_INIT_BIN, 1.4.1.1)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SOURCE_MD5:=723fa88d8a0915e184f99e03e9bf06cb
 $(PKG)_SITE:=http://netfilter.org/projects/$(pkg)/files
-$(PKG)_BINARY:=$($(PKG)_DIR)/iptables
-$(PKG)_IP6_BINARY:=$($(PKG)_DIR)/ip6tables
-$(PKG)_LIB_BINARY:=$($(PKG)_DIR)/libiptc/libiptc.a
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/iptables
-ifeq ($(FREETZ_TARGET_IPV6_SUPPORT),y)
-$(PKG)_IP6_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/ip6tables
+ifneq ($(FREETZ_TARGET_IPV6_SUPPORT),y)
+$(PKG)_CONDITIONAL_PATCHES+=cond
 endif
-$(PKG)_LIB_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libiptc.a
+
+$(PKG)_BINARIES_ALL:=iptables ip6tables
+$(PKG)_BINARIES:=$(filter-out $(if $(FREETZ_TARGET_IPV6_SUPPORT),,ip6tables),$($(PKG)_BINARIES_ALL))
+$(PKG)_BINARIES_BUILD_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DIR)/%)
+$(PKG)_BINARIES_TARGET_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DEST_DIR)/usr/sbin/%)
+$(PKG)_NOT_INCLUDED := $(patsubst %,$($(PKG)_DEST_DIR)/usr/sbin/%,$(filter-out $($(PKG)_BINARIES),$($(PKG)_BINARIES_ALL)))
+
+$(PKG)_LIB_BUILD_DIR:=$($(PKG)_DIR)/libiptc/libiptc.a
+$(PKG)_LIB_STAGING_DIR:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libiptc.a
 $(PKG)_EXTENSIONS_DIR:=$(ROOT_DIR)/usr/lib/xtables
 $(PKG)_TARGET_EXTENSIONS:=$($(PKG)_EXTENSIONS_DIR)/.installed
-$(PKG)_CONFIGURE_ENV += AR="$(TARGET_MAKE_PATH)/$(TARGET_CROSS)ar" RANLIB="$(TARGET_MAKE_PATH)/$(TARGET_CROSS)ranlib"
+
+$(PKG)_CONFIGURE_ENV += AR="$(TARGET_MAKE_PATH)/$(TARGET_CROSS)ar"
+$(PKG)_CONFIGURE_ENV += RANLIB="$(TARGET_MAKE_PATH)/$(TARGET_CROSS)ranlib"
 $(PKG)_CONFIGURE_OPTIONS += --with-ksource="$(FREETZ_BASE_DIR)/$(KERNEL_SOURCE_DIR)"
-$(PKG)_SOURCE_MD5:=723fa88d8a0915e184f99e03e9bf06cb
 
 $(PKG_SOURCE_DOWNLOAD)
-
-$(IPTABLES_DIR)/.unpacked: $(DL_DIR)/$(IPTABLES_SOURCE)
-	tar -C $(IPTABLES_SOURCE_DIR) $(VERBOSE) -xjf $(DL_DIR)/$(IPTABLES_SOURCE)
-	set -e; shopt -s nullglob; for i in $(IPTABLES_MAKE_DIR)/patches/*.patch; do \
-		$(PATCH_TOOL) $(IPTABLES_DIR) $$i; \
-	done
-ifneq ($(FREETZ_TARGET_IPV6_SUPPORT),y)
-	set -e; shopt -s nullglob; $(PATCH_TOOL) $(IPTABLES_DIR) $(IPTABLES_MAKE_DIR)/patches/cond/009-remove_ipv6.patch
-endif
-	touch $@
-
+$(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
-$($(PKG)_BINARY) $($(PKG)_IP6_BINARY) $($(PKG)_LIB_BINARY): $($(PKG)_DIR)/.configured | kernel-source
+$($(PKG)_BINARIES_BUILD_DIR) $($(PKG)_LIB_BUILD_DIR): $($(PKG)_DIR)/.configured | kernel-source
 	$(SUBMAKE) -C $(IPTABLES_DIR)
 
-$($(PKG)_LIB_STAGING_BINARY): $($(PKG)_LIB_BINARY)
-	mkdir -p $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/
-	cp $< $@
+$($(PKG)_LIB_STAGING_DIR): $($(PKG)_LIB_BUILD_DIR)
+	$(INSTALL_BINARY)
 
-$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+$($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/sbin/%: $($(PKG)_DIR)/%
 	$(INSTALL_BINARY_STRIP)
 
-$($(PKG)_IP6_TARGET_BINARY): $($(PKG)_IP6_BINARY)
-	$(INSTALL_BINARY_STRIP)
-
-$($(PKG)_TARGET_EXTENSIONS): $($(PKG)_BINARY)
+$($(PKG)_TARGET_EXTENSIONS): $($(PKG)_BINARIES_BUILD_DIR)
 	mkdir -p $(IPTABLES_EXTENSIONS_DIR)
 	cp $(IPTABLES_DIR)/extensions/*.so $(IPTABLES_EXTENSIONS_DIR)/
 	$(TARGET_STRIP) $(IPTABLES_EXTENSIONS_DIR)/*.so
 	touch $@
 
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_IP6_TARGET_BINARY) $($(PKG)_TARGET_EXTENSIONS) $($(PKG)_LIB_STAGING_BINARY)
+$(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR) $($(PKG)_TARGET_EXTENSIONS) $($(PKG)_LIB_STAGING_DIR)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(IPTABLES_DIR) clean
-	$(RM) $(IPTABLES_LIB_STAGING_BINARY)
+	$(RM) $(IPTABLES_LIB_STAGING_DIR)
 
 $(pkg)-uninstall:
-	$(RM) $(IPTABLES_TARGET_BINARY)
-	$(RM) $($(PKG)_IP6_TARGET_BINARY)
-	$(RM) $(IPTABLES_EXTENSIONS_DIR)/*
+	$(RM) \
+		$(IPTABLES_BINARIES_ALL:%=$(IPTABLES_DEST_DIR)/usr/sbin/%) \
+		$(IPTABLES_EXTENSIONS_DIR)/*
 
 $(PKG_FINISH)
