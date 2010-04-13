7ea509d94b2cb5bf2e41eb675b01e80025bc675b
Oliver Metz <oliver@freetz.org>
2010-04-13 21:08:05 +0000

    [freetz-stable-1.1] Merge in r3694, r3904:r3905, r3945, r4043, r4060, r4091, r4349, r4459, r4492, r4680, r4688, r4709, r4713, r4717  from trunk: ------------------------------------------------------------------------ r3694 | Whoopie | 2009-09-23 12:48:12 +0200 (Mi, 23 Sep 2009) | 1 line  * davfs2: bump version to 1.4.1, add libneon 0.29 support ------------------------------------------------------------------------ r3904 | er13 | 2009-11-19 01:08:34 +0100 (Do, 19 Nov 2009) | 1 line  * davfs2: fix dependencies ------------------------------------------------------------------------ r3905 | Whoopie | 2009-11-19 11:06:32 +0100 (Do, 19 Nov 2009) | 1 line  * davfs2: bump version to 1.4.5 ------------------------------------------------------------------------ r3945 | oliver | 2009-11-23 19:39:28 +0100 (Mo, 23 Nov 2009) | 1 line  * davfs2: Symlink to Freetz' binaries (instead of AVM) ------------------------------------------------------------------------ r4043 | er13 | 2009-12-14 20:32:28 +0100 (Mo, 14 Dez 2009) | 1 line davfs2: fix libiconv dependency ------------------------------------------------------------------------ r4060 | er13 | 2009-12-18 12:18:35 +0100 (Fr, 18 Dez 2009) | 1 line  * davfs2: ensure davfs2 doesn't try to use host libiconv ------------------------------------------------------------------------ r4091 | er13 | 2009-12-21 23:10:18 +0100 (Mo, 21 Dez 2009) | 1 line  * davfs2: fix segmentation fault with relative mount points ------------------------------------------------------------------------ r4349 | oliver | 2010-02-22 10:55:36 +0100 (Mo, 22 Feb 2010) | 1 line  * davfs2: Fix failure if neon was built with SSL support but not selected with davfs2 ------------------------------------------------------------------------ r4459 | oliver | 2010-03-12 23:20:58 +0100 (Fr, 12 Mär 2010) | 1 line  * davfs: Don't ask for credentials when started from rc-script ------------------------------------------------------------------------ r4492 | er13 | 2010-03-15 00:16:19 +0100 (Mo, 15 Mär 2010) | 1 line  * davfs2: correct config option name ------------------------------------------------------------------------ r4680 | oliver | 2010-04-08 23:04:29 +0200 (Do, 08 Apr 2010) | 3 lines  * Bump firmware versions:   * Fritz!Box Fon WLAN 7270 (A-CH): 54.04.81   * Fritz!Box Fon WLAN 7270 (English): 54.04.81 ------------------------------------------------------------------------ r4688 | oliver | 2010-04-09 23:24:54 +0200 (Fr, 09 Apr 2010) | 1 line  * uclibc: Fix download for uClibc-0.9.28 ------------------------------------------------------------------------ r4709 | oliver | 2010-04-11 11:00:31 +0200 (So, 11 Apr 2010) | 2 lines  * uclibc-0.9.29 source was moved too ------------------------------------------------------------------------ r4713 | oliver | 2010-04-11 16:45:45 +0200 (So, 11 Apr 2010) | 1 line  * patch: Add Annex A for 7270 needs new international firmware too ------------------------------------------------------------------------ r4717 | buehmann | 2010-04-13 14:17:22 +0200 (Di, 13 Apr 2010) | 1 line  * Leerzeichen in MOTD; closes #772 ------------------------------------------------------------------------
    git-svn-id: http://svn.freetz.org/branches/freetz-stable-1.1@4718 f5190166-0702-4917-9039-51ec32eddaf5

--- make/davfs2/davfs2.mk
+++ make/davfs2/davfs2.mk
@@ -1,15 +1,25 @@
-$(call PKG_INIT_BIN,1.3.3)
+$(call PKG_INIT_BIN,1.4.5)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SITE:=@SF/dav
+$(PKG)_SITE:=http://mirrors.zerg.biz/nongnu/davfs2
 $(PKG)_MOUNT_BINARY:=$($(PKG)_DIR)/src/mount.davfs
 $(PKG)_MOUNT_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/mount.davfs
 $(PKG)_UMOUNT_BINARY:=$($(PKG)_DIR)/src/umount.davfs
 $(PKG)_UMOUNT_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/umount.davfs
 
-$(PKG)_DEPENDS_ON := neon libiconv
+$(PKG)_DEPENDS_ON := neon
+$(PKG)_LIBS := \$$(NEON_LIBS)
 
-$(PKG)_CONFIG_SUBOPTS += FREETZ_DAVFS2_WITH_SSL
-$(PKG)_CONFIG_SUBOPTS += FREETZ_DAVFS2_WITH_ZLIB
+ifeq ($(strip $(FREETZ_TARGET_UCLIBC_VERSION_0_9_28)),y)
+$(PKG)_DEPENDS_ON += libiconv
+$(PKG)_LIBS += -liconv
+else
+$(PKG)_CONFIGURE_OPTIONS += --without-libiconv-prefix
+endif
+
+$(PKG)_CONFIGURE_OPTIONS += --without-libintl-prefix
+
+$(PKG)_CONFIG_SUBOPTS += FREETZ_PACKAGE_DAVFS2_WITH_SSL
+$(PKG)_CONFIG_SUBOPTS += FREETZ_PACKAGE_DAVFS2_WITH_ZLIB
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -18,7 +28,7 @@ $(PKG_CONFIGURED_CONFIGURE)
 $($(PKG)_MOUNT_BINARY) $($(PKG)_UMOUNT_BINARY): $($(PKG)_DIR)/.configured
 	PATH="$(TARGET_PATH)" \
 		$(MAKE) -C $(DAVFS2_DIR) \
-		LIBS="-liconv -lneon" \
+		LIBS="$(DAVFS2_LIBS)"
 
 $($(PKG)_MOUNT_TARGET_BINARY): $($(PKG)_MOUNT_BINARY)
 	$(INSTALL_BINARY_STRIP)
