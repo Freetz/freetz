2475fc3a235210462745efc796f994201669b73d
Gene Rudoy <gene@freetz.org>
2010-04-17 17:25:15 +0000

    php:  * add (optional) curl, iconv, ssl and zlib support (by molfi + fixes by me)
    git-svn-id: http://svn.freetz.org/trunk@4735 f5190166-0702-4917-9039-51ec32eddaf5

--- make/php/php.mk
+++ make/php/php.mk
@@ -1,5 +1,5 @@
 $(call PKG_INIT_BIN, 5.2.13)
-$(PKG)_SOURCE:=php-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_SOURCE_MD5:=eb4d0766dc4fb9667f05a68b6041e7d1
 $(PKG)_SITE:=http://de.php.net/distributions
 
@@ -10,22 +10,80 @@ ifeq ($(strip $(FREETZ_PACKAGE_PHP_STATIC)),y)
 $(PKG)_STATIC := -all-static
 endif
 
-$(PKG)_DEPENDS_ON := pcre
+$(PKG)_DEPENDS_ON += pcre
+$(PKG)_CONFIGURE_OPTIONS += --with-pcre-regex="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+
+ifeq ($(strip $(FREETZ_PACKAGE_PHP_WITH_CURL)),y)
+$(PKG)_DEPENDS_ON += curl
+$(PKG)_CONFIGURE_OPTIONS += --with-curl="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+endif
+
 ifeq ($(strip $(FREETZ_PACKAGE_PHP_WITH_GD)),y)
 $(PKG)_DEPENDS_ON += gd
+$(PKG)_CONFIGURE_OPTIONS += --with-gd="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+$(PKG)_CONFIGURE_OPTIONS += --enable-gd-native-ttf
 endif
-ifeq ($(strip $(FREETZ_PACKAGE_PHP_WITH_SQLITE3)),y)
-$(PKG)_DEPENDS_ON += sqlite
+
+ifeq ($(strip $(FREETZ_PACKAGE_PHP_WITH_ICONV)),y)
+$(PKG)_CONFIGURE_OPTIONS += --with-iconv
+$(PKG)_CONFIGURE_OPTIONS += --with-iconv-dir="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+
+ifeq ($(strip $(FREETZ_TARGET_UCLIBC_VERSION_0_9_28)),y)
+$(PKG)_DEPENDS_ON += libiconv
+else
+$(PKG)_CONFIGURE_ENV += ICONV_IN_LIBC_DIR="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
 endif
+
+else
+$(PKG)_CONFIGURE_OPTIONS += --without-iconv
+endif
+
 ifeq ($(strip $(FREETZ_PACKAGE_PHP_WITH_LIBXML)),y)
 $(PKG)_DEPENDS_ON += libxml2
+$(PKG)_CONFIGURE_OPTIONS += --with-libxml-dir="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+endif
+$(PKG)_CONFIGURE_OPTIONS += --without-libexpat-dir #we only want libxml-based XML support to be enabled
+$(PKG)_XML_SUPPORT:=$(if $(FREETZ_PACKAGE_PHP_WITH_LIBXML),enable,disable)
+$(PKG)_CONFIGURE_OPTIONS += --$($(PKG)_XML_SUPPORT)-xml
+$(PKG)_CONFIGURE_OPTIONS += --$($(PKG)_XML_SUPPORT)-libxml
+$(PKG)_CONFIGURE_OPTIONS += --$($(PKG)_XML_SUPPORT)-dom
+$(PKG)_CONFIGURE_OPTIONS += --$($(PKG)_XML_SUPPORT)-simplexml
+$(PKG)_CONFIGURE_OPTIONS += --$($(PKG)_XML_SUPPORT)-xmlreader
+$(PKG)_CONFIGURE_OPTIONS += --$($(PKG)_XML_SUPPORT)-xmlwriter
+
+ifeq ($(strip $(FREETZ_PACKAGE_PHP_WITH_SQLITE2)),y)
+$(PKG)_CONFIGURE_OPTIONS += --with-sqlite
+else
+$(PKG)_CONFIGURE_OPTIONS += --without-sqlite
+endif
+
+ifeq ($(strip $(FREETZ_PACKAGE_PHP_WITH_SQLITE3)),y)
+$(PKG)_DEPENDS_ON += sqlite
+$(PKG)_CONFIGURE_OPTIONS += --with-pdo-sqlite="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+else
+$(PKG)_CONFIGURE_OPTIONS += --without-pdo-sqlite
+endif
+
+ifeq ($(strip $(FREETZ_PACKAGE_PHP_WITH_SSL)),y)
+$(PKG)_DEPENDS_ON += openssl
+$(PKG)_CONFIGURE_OPTIONS += --with-openssl="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+endif
+
+ifeq ($(strip $(FREETZ_PACKAGE_PHP_WITH_ZLIB)),y)
+$(PKG)_DEPENDS_ON += zlib
+$(PKG)_CONFIGURE_OPTIONS += --with-zlib
+$(PKG)_CONFIGURE_OPTIONS += --with-zlib-dir="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
 endif
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PHP_STATIC
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PHP_WITH_CURL
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PHP_WITH_GD
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PHP_WITH_ICONV
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PHP_WITH_LIBXML
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PHP_WITH_SQLITE2
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PHP_WITH_SQLITE3
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PHP_WITH_LIBXML
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PHP_WITH_SSL
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PHP_WITH_ZLIB
 
 $(PKG)_CONFIGURE_ENV += php_cv_sizeof_ssize_t=4
 $(PKG)_CONFIGURE_ENV += php_cv_sizeof_ptrdiff_t=4
@@ -47,46 +105,17 @@ $(PKG)_CONFIGURE_ENV += lt_cv_path_NM="$(TARGET_MAKE_PATH)/$(TARGET_CROSS)nm -B"
 
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
 $(PKG)_CONFIGURE_OPTIONS += --disable-rpath
-
-$(PKG)_CONFIGURE_OPTIONS += --without-iconv
 $(PKG)_CONFIGURE_OPTIONS += --without-pear
-
-$(PKG)_CONFIGURE_OPTIONS += --without-libexpat-dir #we only want libxml-based XML support to be enabled
-ifeq ($(strip $(FREETZ_PACKAGE_PHP_WITH_LIBXML)),y)
-$(PKG)_CONFIGURE_OPTIONS += --with-libxml-dir="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
-endif
-$(PKG)_XML_SUPPORT:=$(if $(FREETZ_PACKAGE_PHP_WITH_LIBXML),enable,disable)
-$(PKG)_CONFIGURE_OPTIONS += --$($(PKG)_XML_SUPPORT)-xml
-$(PKG)_CONFIGURE_OPTIONS += --$($(PKG)_XML_SUPPORT)-libxml
-$(PKG)_CONFIGURE_OPTIONS += --$($(PKG)_XML_SUPPORT)-dom
-$(PKG)_CONFIGURE_OPTIONS += --$($(PKG)_XML_SUPPORT)-simplexml
-$(PKG)_CONFIGURE_OPTIONS += --$($(PKG)_XML_SUPPORT)-xmlreader
-$(PKG)_CONFIGURE_OPTIONS += --$($(PKG)_XML_SUPPORT)-xmlwriter
-
-ifeq ($(strip $(FREETZ_PACKAGE_PHP_WITH_SQLITE2)),y)
-$(PKG)_CONFIGURE_OPTIONS += --with-sqlite
-else
-$(PKG)_CONFIGURE_OPTIONS += --without-sqlite
-endif
-ifeq ($(strip $(FREETZ_PACKAGE_PHP_WITH_SQLITE3)),y)
-$(PKG)_CONFIGURE_OPTIONS += --with-pdo-sqlite="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
-else
-$(PKG)_CONFIGURE_OPTIONS += --without-pdo-sqlite
-endif
 ifneq ($(strip $(FREETZ_TARGET_IPV6_SUPPORT)),y)
 $(PKG)_CONFIGURE_OPTIONS += --disable-ipv6
 endif
 $(PKG)_CONFIGURE_OPTIONS += --enable-force-cgi-redirect
 $(PKG)_CONFIGURE_OPTIONS += --enable-discard-path
 $(PKG)_CONFIGURE_OPTIONS += --enable-fastcgi
+$(PKG)_CONFIGURE_OPTIONS += --enable-mbstring
 $(PKG)_CONFIGURE_OPTIONS += --enable-exif
 $(PKG)_CONFIGURE_OPTIONS += --with-config-file-path=/tmp/flash
 $(PKG)_CONFIGURE_OPTIONS += --with-config-file-scan-dir=/tmp/flash/php
-$(PKG)_CONFIGURE_OPTIONS += --with-pcre-regex="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
-ifeq ($(strip $(FREETZ_PACKAGE_PHP_WITH_GD)),y)
-$(PKG)_CONFIGURE_OPTIONS += --with-gd="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
-$(PKG)_CONFIGURE_OPTIONS += --enable-gd-native-ttf
-endif
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
