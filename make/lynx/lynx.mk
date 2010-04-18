98c38e0c48d662e0e117bc36c7558dd3bc4852f4
Whoopie <whoopie79@gmx.net>
2010-04-18 10:45:21 +0000

    lynx: bump version to 2.8.7 (fixes #782)
    git-svn-id: http://svn.freetz.org/trunk@4746 f5190166-0702-4917-9039-51ec32eddaf5

--- make/lynx/lynx.mk
+++ make/lynx/lynx.mk
@@ -1,17 +1,18 @@
-$(call PKG_INIT_BIN, 2.8.5)
+$(call PKG_INIT_BIN, 2.8.7)
 $(PKG)_SOURCE:=$(pkg)$($(PKG)_VERSION).tar.bz2
 $(PKG)_SITE:=http://lynx.isc.org/lynx$($(PKG)_VERSION)
-$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/$(pkg)2-8-5
+$(PKG)_SOURCE_MD5:=493af4c77ef6761e3f0157cd1be033a0
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/$(pkg)2-8-7
 $(PKG)_BINARY:=$($(PKG)_DIR)/$(pkg)
 $(PKG)_CFG:=$($(PKG)_DIR)/lynx.cfg
 $(PKG)_TARGET_CFG:=$($(PKG)_TARGET_DIR)/root/etc/lynx.cfg
 $(PKG)_TARGET_BINARY:=$(LYNX_TARGET_DIR)/root/usr/bin/lynx
-$(PKG)_SOURCE_MD5:=d1e5134e5d175f913c16cb6768bc30eb
 
 $(PKG)_CONFIGURE_OPTIONS := \
 	--enable-warnings \
 	--with-screen=ncurses \
-	--enable-nested-tables --enable-read-eta \
+	--enable-nested-tables \
+	--enable-read-eta \
 	--enable-charset-choice \
 	--disable-alt-bindings \
 	--disable-bibp-urls \
