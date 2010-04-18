56e483d176636d1270616aa2aad2f6783a7cc4ce
Gene Rudoy <gene@freetz.org>
2010-04-18 20:09:42 +0000

    lynx:  * minor cleanups
    git-svn-id: http://svn.freetz.org/trunk@4751 f5190166-0702-4917-9039-51ec32eddaf5

--- make/lynx/lynx.mk
+++ make/lynx/lynx.mk
@@ -1,14 +1,15 @@
 $(call PKG_INIT_BIN, 2.8.7)
 $(PKG)_SOURCE:=$(pkg)$($(PKG)_VERSION).tar.bz2
-$(PKG)_SITE:=http://lynx.isc.org/lynx$($(PKG)_VERSION)
 $(PKG)_SOURCE_MD5:=493af4c77ef6761e3f0157cd1be033a0
+$(PKG)_SITE:=http://lynx.isc.org/lynx$($(PKG)_VERSION)
 $(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/$(pkg)2-8-7
-$(PKG)_BINARY:=$($(PKG)_DIR)/$(pkg)
-$(PKG)_CFG:=$($(PKG)_DIR)/lynx.cfg
-$(PKG)_TARGET_CFG:=$($(PKG)_TARGET_DIR)/root/etc/lynx.cfg
-$(PKG)_TARGET_BINARY:=$(LYNX_TARGET_DIR)/root/usr/bin/lynx
 
-$(PKG)_CONFIGURE_OPTIONS := \
+$(PKG)_BINARY:=$($(PKG)_DIR)/$(pkg)
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/$(pkg)
+$(PKG)_CFG:=$($(PKG)_DIR)/$(pkg).cfg
+$(PKG)_TARGET_CFG:=$($(PKG)_DEST_DIR)/etc/$(pkg).cfg
+
+$(PKG)_CONFIGURE_OPTIONS += \
 	--enable-warnings \
 	--with-screen=ncurses \
 	--enable-nested-tables \
@@ -24,7 +25,7 @@ $(PKG)_CONFIGURE_OPTIONS := \
 	--disable-nls \
 	--disable-prettysrc \
 	--disable-source-cache \
-	--disable-trace \
+	--disable-trace
 
 $(PKG)_DEPENDS_ON := ncurses
 
@@ -41,8 +42,7 @@ $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
 
 $($(PKG)_TARGET_CFG): $($(PKG)_CFG)
-	mkdir -p $(dir $@)
-	cp $(LYNX_CFG) $(LYNX_TARGET_CFG)
+	$(INSTALL_FILE)
 
 $(pkg):
 
@@ -52,8 +52,6 @@ $(pkg)-clean:
 	-$(SUBMAKE) -C $(LYNX_DIR) clean
 
 $(pkg)-uninstall:
-	$(RM) $(LYNX_TARGET_BINARY)
-	$(RM) $(LYNX_TARGET_CFG)
+	$(RM) $(LYNX_TARGET_BINARY) $(LYNX_TARGET_CFG)
 
 $(PKG_FINISH)
-
