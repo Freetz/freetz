b8e576d71f3be1f6036636fec618f16c2ea10a92
Whoopie <whoopie79@gmx.net>
2010-04-19 14:20:49 +0000

    dns2tcp: bump version to 0.5 (by colonia27 + a lot fixes by me, closes #786)
    git-svn-id: http://svn.freetz.org/trunk@4754 f5190166-0702-4917-9039-51ec32eddaf5

--- make/dns2tcp/dns2tcp.mk
+++ make/dns2tcp/dns2tcp.mk
@@ -1,16 +1,27 @@
-$(call PKG_INIT_BIN,0.4.3)
+$(call PKG_INIT_BIN,0.5)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=cf21e40f289c27bdce831c119eae293e
 $(PKG)_SITE:=http://www.hsc.fr/ressources/outils/dns2tcp/download
 $(PKG)_BINARY:=$($(PKG)_DIR)/server/dns2tcpd
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/dns2tcpd
-$(PKG)_SOURCE_MD5:=d2b322ee27f4ff53dfdad61aa2f42dd8
+
+$(PKG)_DEPENDS_ON := openssl
+
+ifeq ($(strip $(FREETZ_PACKAGE_DNS2TCP_STATIC)),y)
+$(PKG)_LDFLAGS := -static
+endif
+
+$(PKG)_CONFIGURE_OPTIONS += --without-client
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_DNS2TCP_STATIC
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(DNS2TCP_DIR)
+	$(SUBMAKE) -C $(DNS2TCP_DIR) \
+		LDFLAGS="$(DNS2TCP_LDFLAGS)"
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
