0dccee3519e9d0198d5bd5f892e374d7e3970fde
Oliver Metz <oliver@freetz.org>
2010-04-30 12:23:39 +0000

    * bridge-utils: Bump to version 1.4
    git-svn-id: http://svn.freetz.org/trunk@4832 f5190166-0702-4917-9039-51ec32eddaf5

--- make/bridge-utils/bridge-utils.mk
+++ make/bridge-utils/bridge-utils.mk
@@ -1,16 +1,13 @@
-$(call PKG_INIT_BIN,1.2)
+$(call PKG_INIT_BIN,1.4)
 $(PKG)_SOURCE:=bridge-utils-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=@SF/bridge
 $(PKG)_BINARY:=$($(PKG)_DIR)/brctl/brctl
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/sbin/brctl
-$(PKG)_SOURCE_MD5:=1e6cff57ac90d7ab984d9512fdd9f2dd
+$(PKG)_SOURCE_MD5:=0182fcac3a2b307113bbec34e5f1c673
 
-$(PKG)_CONFIGURE_PRE_CMDS += aclocal --force ;
-$(PKG)_CONFIGURE_PRE_CMDS += libtoolize --force ;
-$(PKG)_CONFIGURE_PRE_CMDS += autoconf --force ;
-$(PKG)_CONFIGURE_PRE_CMDS += autoheader --force ;
+$(PKG)_CONFIGURE_PRE_CMDS += ln -sf configure.in configure.ac;
+$(PKG)_CONFIGURE_PRE_CMDS += autoconf > /dev/null 2>&1;
 
-$(PKG)_CONFIGURE_OPTIONS += --with-randomdev=/dev/random
 $(PKG)_CONFIGURE_OPTIONS += --with-linux-headers=$(KERNEL_HEADERS_DIR)
 
 $(PKG_SOURCE_DOWNLOAD)
