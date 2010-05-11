ff7bfb9ee08533da56ea7e955aa0a8801f4a1b31
Oliver Metz <oliver@freetz.org>
2010-05-11 16:07:30 +0000

    * samba: Bump to version 3.0.37   * Use original source   * Syslog support   * Displays usage message   * slightly bigger than before
    git-svn-id: http://svn.freetz.org/trunk@4865 f5190166-0702-4917-9039-51ec32eddaf5

--- make/samba/samba.mk
+++ make/samba/samba.mk
@@ -1,7 +1,8 @@
-$(call PKG_INIT_BIN, 3.0.24)
+$(call PKG_INIT_BIN, 3.0.37)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=89273f67a6d8067cbbecefaa13747153
-$(PKG)_SITE:=ftp://de3.$(pkg).org/pub/$(pkg)/old-versions
+$(PKG)_SOURCE_MD5:=11ed2bfef4090bd5736b194b43f67289
+$(PKG)_SITE:=http://samba.org/samba/ftp/stable
+$(PKG)_BUILD_SUBDIR:=source
 
 $(PKG)_STARTLEVEL=80
 
@@ -11,18 +12,49 @@ $(PKG)_BINARIES_BUILD_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DIR)/source/bin/%)
 $(PKG)_BINARIES_TARGET_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DEST_DIR)/sbin/%)
 $(PKG)_NOT_INCLUDED := $(patsubst %,$($(PKG)_DEST_DIR)/sbin/%,$(filter-out $($(PKG)_BINARIES),$($(PKG)_BINARIES_ALL)))
 
+$(PKG)_DEPENDS_ON+= popt
+
+ifneq ($(FREETZ_TARGET_IPV6_SUPPORT),y)
+$(PKG)_CONFIGURE_ENV += libreplace_cv_HAVE_IPV6=no
+endif
+$(PKG)_CONFIGURE_ENV += ac_cv_func_prctl=no
+$(PKG)_CONFIGURE_ENV += samba_cv_USE_SETRESUID=yes
+$(PKG)_CONFIGURE_ENV += SMB_BUILD_CC_NEGATIVE_ENUM_VALUES=yes
+
+$(PKG)_CONFIGURE_OPTIONS += --disable-swat
+$(PKG)_CONFIGURE_OPTIONS += --disable-cups
+$(PKG)_CONFIGURE_OPTIONS += --disable-iprint
+$(PKG)_CONFIGURE_OPTIONS += --disable-pie
+$(PKG)_CONFIGURE_OPTIONS += --disable-relro
+$(PKG)_CONFIGURE_OPTIONS += --disable-fam
+$(PKG)_CONFIGURE_OPTIONS += --disable-avahi
+$(PKG)_CONFIGURE_OPTIONS += --disable-gnutls
+$(PKG)_CONFIGURE_OPTIONS += --without-ldap
+$(PKG)_CONFIGURE_OPTIONS += --without-ads
+$(PKG)_CONFIGURE_OPTIONS += --without-cifsmount
+$(PKG)_CONFIGURE_OPTIONS += --without-cifsupcall
+$(PKG)_CONFIGURE_OPTIONS += --without-sys-quotas
+$(PKG)_CONFIGURE_OPTIONS += --without-utmp
+$(PKG)_CONFIGURE_OPTIONS += --without-cluster-support
+$(PKG)_CONFIGURE_OPTIONS += --without-acl-support
+$(PKG)_CONFIGURE_OPTIONS += --without-sendfile-support
+$(PKG)_CONFIGURE_OPTIONS += --without-wbclient
+$(PKG)_CONFIGURE_OPTIONS += --without-winbind
+$(PKG)_CONFIGURE_OPTIONS += --with-krb5=no
+$(PKG)_CONFIGURE_OPTIONS += --with-included-popt=no
+$(PKG)_CONFIGURE_OPTIONS += --with-privatedir=/mod/etc
+$(PKG)_CONFIGURE_OPTIONS += --with-configdir=/mod/etc
+$(PKG)_CONFIGURE_OPTIONS += --with-piddir=/var/run
+$(PKG)_CONFIGURE_OPTIONS += --with-syslog
+
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
-$(PKG_CONFIGURED_NOP)
+$(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARIES_BUILD_DIR): $($(PKG)_DIR)/.configured
-	for target in proto all; do \
+	for target in headers all; do \
 	$(SUBMAKE) -C $(SAMBA_DIR)/source \
-		CC="$(TARGET_CC)" \
-		LD="$(TARGET_LD)" \
-		AR="$(TARGET_AR)" \
-		TARGETFS="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
-		SAMBA_CFLAGS="$(TARGET_CFLAGS)" \
+		RANLIB="$(TARGET_RANLIB)" \
 		CODEPAGEDIR="/mod/usr/share/samba" \
 		$$target; \
 	done
