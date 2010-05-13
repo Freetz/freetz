d0816c595c58d9571157ccbab05ed5272570f5f9
Oliver Metz <oliver@freetz.org>
2010-05-13 18:18:25 +0000

    Replace 3 occurences with isFreetzType function
    git-svn-id: http://svn.freetz.org/trunk@4891 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/195-webmenu_signed.sh
+++ patches/195-webmenu_signed.sh
@@ -1,15 +1,15 @@
 [ "$FREETZ_PATCH_SIGNED" == "y" ] || return 0
 echo1 "applying webmenu signed patch"
 
-if [ "$FREETZ_TYPE_LANG_STRING" == "de" ]; then
+if isFreetzType LANG_DE; then
 	modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/all/html/de/home/home.js
-elif [ "$FREETZ_TYPE_LANG_STRING" == "a-ch" ]; then
+elif isFreetzType LANG_A_CH; then
 	if isFreetzType 7270; then
 		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/html/de/home/home.js
 	else
 		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/all/html/de/home/home.js
 	fi
-elif [ "$FREETZ_TYPE_LANG_STRING" == "en" ]; then
+elif isFreetzType LANG_EN; then
 	if isFreetzType 7140 7170 7270 7570; then
 		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/html/de/home/home.js
 	else
