e74ef4a1ba10151feb9392ea41c9c9d362c84f87
Gene Rudoy <gene@freetz.org>
2010-05-16 19:00:39 +0000

    callmonitor:  * revise makefile a bit, make it more similar to other freetz packages
    git-svn-id: http://svn.freetz.org/trunk@4896 f5190166-0702-4917-9039-51ec32eddaf5

--- make/callmonitor/callmonitor.mk
+++ make/callmonitor/callmonitor.mk
@@ -1,26 +1,20 @@
 $(call PKG_INIT_BIN, 1.17.3)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION)-freetz.tar.bz2
+$(PKG)_SOURCE_MD5:=08118cad0bab6a609ab19dee60c438eb
 $(PKG)_SITE:=http://download.berlios.de/callmonitor
+$(PKG)_STARTLEVEL=30
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/recode
 $(PKG)_TARGET_BINARY:=$($(PKG)_DIR)/root/usr/lib/callmonitor/bin/recode
-$(PKG)_STARTLEVEL=30
-$(PKG)_SOURCE_MD5:=08118cad0bab6a609ab19dee60c438eb
 
-CALLMONITOR_FEATURES:=$(foreach feat,webif actions monitor phonebook,\
-	$(if $(FREETZ_PACKAGE_CALLMONITOR_$(feat)),$(feat)))
+$(PKG)_SUBOPTS := webif actions monitor phonebook
+$(PKG)_REBUILD_SUBOPTS += $(foreach subopt,$($(PKG)_SUBOPTS),FREETZ_PACKAGE_CALLMONITOR_$(subopt))
+$(PKG)_FEATURES := $(call PKG_SELECTED_SUBOPTIONS,$($(PKG)_SUBOPTS))
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 
-.PHONY: FORCE
-
-$(CALLMONITOR_DIR)/.features.new: FORCE
-	@echo $(CALLMONITOR_FEATURES) > $@
-
-$(CALLMONITOR_DIR)/.features: $(CALLMONITOR_DIR)/.features.new
-	@if [ ! -e $@ ] || ! diff -q $< $@; then cp $< $@; fi
-
-$($(PKG)_DIR)/.configured: $($(PKG)_DIR)/.unpacked $($(PKG)_DIR)/.features
+$($(PKG)_DIR)/.configured: $($(PKG)_DIR)/.unpacked
+	@echo $(CALLMONITOR_FEATURES) > $(CALLMONITOR_DIR)/.features
 	$(SUBMAKE) -C $(CALLMONITOR_DIR) configure
 	@touch $@
 
@@ -31,16 +25,13 @@ $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
 	chmod 755 $@
 
-$(pkg)-clean: FORCE
-	if [ -d "$(CALLMONITOR_DIR)" ]; then $(SUBMAKE) -C $(CALLMONITOR_DIR) clean; else true; fi
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(CALLMONITOR_DIR) clean
 
 $(pkg) $(pkg)-precompiled: $($(PKG)_TARGET_DIR)/.packaged
 
-$($(PKG)_TARGET_DIR)/.packaged: $(CALLMONITOR_DIR)/.configured $($(PKG)_TARGET_BINARY)
-	rm -rf $(CALLMONITOR_TARGET_DIR)
-	mkdir -p $(CALLMONITOR_TARGET_DIR)/root
-	tar -c -C $(CALLMONITOR_DIR)/root --files-from=$(CALLMONITOR_DIR)/.files | \
-		tar -x -C $(CALLMONITOR_DEST_DIR)
+$($(PKG)_TARGET_DIR)/.packaged: $($(PKG)_DIR)/.configured $($(PKG)_TARGET_BINARY)
+	tar -c -C $(CALLMONITOR_DIR)/root --files-from=$(CALLMONITOR_DIR)/.files | tar -x -C $(CALLMONITOR_DEST_DIR)
 	cp $(CALLMONITOR_DIR)/.language $(CALLMONITOR_TARGET_DIR)/
 	@touch $@
 
