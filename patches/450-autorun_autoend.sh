902db2879c7faa4c329c7a3c1e6e5e74a43c9275
Oliver Metz <oliver@freetz.org>
2010-05-18 07:19:46 +0000

    * Bump firmware:   * Fritz!Box Fon WLAN 7570 (Multi-Language, Annex A/B): 75.04.82
    git-svn-id: http://svn.freetz.org/trunk@4907 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/450-autorun_autoend.sh
+++ patches/450-autorun_autoend.sh
@@ -3,7 +3,7 @@ echo1 "patching run_mount and storage: autorun/autoend"
 if isFreetzType 7270 7270_V3 && isFreetzType LABOR_PHONE LABOR_NAS LABOR_CORE LABOR_IPV6; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/450-autorun-run_mount_${FREETZ_TYPE_STRING}.patch"
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/450-autoend-storage_${FREETZ_TYPE_STRING}.patch"
-elif isFreetzType 3270 3270_V3 7141 7170 7240 7270 7270_V3 || \
+elif isFreetzType 3270 3270_V3 7141 7170 7240 7270 7270_V3 7570 || \
 	( isFreetzType 7140 && isFreetzType LANG_EN LANG_A_CH); then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/450-autorun-run_mount_7270.patch"
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/450-autoend-storage_7270.patch"
