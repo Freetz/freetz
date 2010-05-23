e85f2d3376858fbaa7e36240921bdd428c080941
Oliver Metz <oliver@freetz.org>
2010-05-23 23:26:21 +0000

    * comgt: Forgot to insert some occurences of gcom
    git-svn-id: http://svn.freetz.org/trunk@4927 f5190166-0702-4917-9039-51ec32eddaf5

--- make/comgt/comgt.mk
+++ make/comgt/comgt.mk
@@ -3,8 +3,8 @@ $(PKG)_SOURCE:=$(pkg).$($(PKG)_VERSION).tgz
 $(PKG)_SOURCE_MD5:=db2452680c3d953631299e331daf49ef
 $(PKG)_SITE:=@SF/$(pkg)
 $(PKG)_DIR:=$(SOURCE_DIR)/$(pkg).$($(PKG)_VERSION)
-$(PKG)_BINARY:=$(SOURCE_DIR)/$(pkg).$($(PKG)_VERSION)/$(pkg)
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/$(pkg)
+$(PKG)_BINARY:=$(SOURCE_DIR)/$(pkg).$($(PKG)_VERSION)/gcom
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/gcom
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -14,7 +14,8 @@ $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(COMGT_DIR) \
 		CC="$(TARGET_CC)" \
 		CFLAGS="$(TARGET_CFLAGS)" \
-		LDFLAGS="$(TARGET_LDFLAGS)"
+		LDFLAGS="$(TARGET_LDFLAGS)" \
+		gcom
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
