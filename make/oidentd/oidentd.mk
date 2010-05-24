f1d431c95f3716d89f2ae1848797fad74cb91045
cuma <cuma@freetz.org>
2010-05-24 18:27:50 +0000

    oidentd added (closes #761)
    git-svn-id: http://svn.freetz.org/trunk@4932 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/oidentd/oidentd.mk
@@ -0,0 +1,31 @@
+$(call PKG_INIT_BIN, 2.0.8)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=c3d9a56255819ef8904b867284386911
+$(PKG)_SITE:=http://prdownloads.sourceforge.net/ojnk
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/$(pkg)
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/$(pkg)
+
+$(PKG)_CONFIGURE_OPTIONS += --disable-ipv6
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+		$(SUBMAKE) -C $(OIDENTD_DIR)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(OIDENTD_DIR) clean
+	$(RM) $(OIDENTD_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(OIDENTD_TARGET_BINARY)
+
+$(PKG_FINISH)
