41bd609a728ea95b1509d093282501e378800a59
Whoopie <whoopie79@gmx.net>
2010-05-25 06:14:47 +0000

    oidentd: some cleanups
    git-svn-id: http://svn.freetz.org/trunk@4933 f5190166-0702-4917-9039-51ec32eddaf5

--- make/oidentd/oidentd.mk
+++ make/oidentd/oidentd.mk
@@ -1,18 +1,18 @@
 $(call PKG_INIT_BIN, 2.0.8)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5:=c3d9a56255819ef8904b867284386911
-$(PKG)_SITE:=http://prdownloads.sourceforge.net/ojnk
+$(PKG)_SITE:=@SF/ojnk
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/$(pkg)
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/$(pkg)
 
-$(PKG)_CONFIGURE_OPTIONS += --disable-ipv6
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_TARGET_IPV6_SUPPORT),--enable-ipv6,--disable-ipv6)
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-		$(SUBMAKE) -C $(OIDENTD_DIR)
+	$(SUBMAKE) -C $(OIDENTD_DIR)
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
