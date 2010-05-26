6923d2d1b6ff356ca1fb2fec497ca21741b83d85
cuma <cuma@freetz.org>
2010-05-26 16:06:12 +0000

    oidentd: * set startlevel to 90 (default seems to be 99) * starts now before bip
    git-svn-id: http://svn.freetz.org/trunk@4941 f5190166-0702-4917-9039-51ec32eddaf5

--- make/oidentd/oidentd.mk
+++ make/oidentd/oidentd.mk
@@ -5,6 +5,8 @@ $(PKG)_SITE:=@SF/ojnk
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/$(pkg)
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/$(pkg)
 
+$(PKG)_STARTLEVEL=90
+
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_TARGET_IPV6_SUPPORT),--enable-ipv6,--disable-ipv6)
 
 $(PKG_SOURCE_DOWNLOAD)
