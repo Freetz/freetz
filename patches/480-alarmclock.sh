e9e9ec8984b977fcecd0b2120bb626a70c6c7b29
Oliver Metz <oliver@freetz.org>
2010-06-02 14:29:48 +0000

    * 7570: Use correct alarm clock patch (refs #843)
    git-svn-id: http://svn.freetz.org/trunk@4969 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/480-alarmclock.sh
+++ patches/480-alarmclock.sh
@@ -7,7 +7,7 @@ elif isFreetzType 7112 7141 7170 && isFreetzType LANG_DE; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/${FREETZ_TYPE_LANG_STRING}/480-alarmclock.patch"
 elif isFreetzType 3270_V3; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/480-alarmclock_3270v3.patch"
-elif isFreetzType 7270 && ! isFreetzType LANG_DE; then
+elif isFreetzType 7270 7570 && ! isFreetzType LANG_DE; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/${FREETZ_TYPE_LANG_STRING}/480-alarmclock_7270.patch"
 elif isFreetzType 7270 7270_V3 && isFreetzType LABOR; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/480-alarmclock_7270_labor.patch"
