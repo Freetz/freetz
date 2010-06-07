4af6569481aa2173d6140fc801277d4a9a894e61
Oliver Metz <oliver@freetz.org>
2010-06-07 07:01:58 +0000

    [freetz-stable-1.1]: Fix typo in 200-vcc.sh patch
    git-svn-id: http://svn.freetz.org/branches/freetz-stable-1.1@5000 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/200-vcc.sh
+++ patches/200-vcc.sh
@@ -5,7 +5,7 @@ if [ -e "${HTML_LANG_MOD_DIR}/html/de" ];then
 else
 	HTML_DIR="${HTML_LANG_MOD_DIR}/html/en"
 fi
-if ifFreetzType W501V W701V W901V; then
+if isFreetzType W501V W701V W901V; then
 	modsed "s/avme/tcom/g" "${HTML_DIR}/fon/sipoptionen.js"
 else
 	modsed "s/avme/1und1/g" "${HTML_DIR}/fon/sipoptionen.js"
