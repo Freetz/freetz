df1f2706c38b10e10b252de341609ec5acbd186d
Whoopie <whoopie79@gmx.net>
2010-06-16 21:05:23 +0000

    dns2tcp: bump to version 0.5.2 (refs #866)
    git-svn-id: http://svn.freetz.org/trunk@5081 f5190166-0702-4917-9039-51ec32eddaf5

--- make/dns2tcp/dns2tcp.mk
+++ make/dns2tcp/dns2tcp.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN,0.5.1)
+$(call PKG_INIT_BIN,0.5.2)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=5159b465981476091e68e4154210f729
+$(PKG)_SOURCE_MD5:=51c5dc69f5814c2936ce6832217d292d
 $(PKG)_SITE:=http://www.hsc.fr/ressources/outils/dns2tcp/download
 $(PKG)_BINARY:=$($(PKG)_DIR)/server/dns2tcpd
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/dns2tcpd
