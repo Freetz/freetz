b2e31be83b532ed519bad48cf9289275266ba09c
Gene Rudoy <gene@freetz.org>
2010-06-20 11:18:57 +0000

    matrixssl:  * fix build problem with gcc >= 4.3.x  * minor makefile cleanups
    git-svn-id: http://svn.freetz.org/trunk@5092 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/matrixssl.mk
+++ make/libs/matrixssl.mk
@@ -1,13 +1,12 @@
 $(call PKG_INIT_LIB, 1.7.3)
-#$(PKG)_VERSION:=1-8-3
-#$(PKG)_SOURCE:=matrixssl-$($(PKG)_VERSION)-open.tar.gz
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=b80faff3e389000b9a161dda4812112d
 $(PKG)_SITE:=http://downloads.openwrt.org/sources
-$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/matrixssl
+$(PKG)_DIR:=$(subst -$($(PKG)_VERSION),,$($(PKG)_DIR))
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/libmatrixssl.so
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libmatrixssl.so
 $(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libmatrixssl.so
-$(PKG)_SOURCE_MD5:=b80faff3e389000b9a161dda4812112d
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -16,7 +15,7 @@ $(PKG_CONFIGURED_NOP)
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(MATRIXSSL_DIR)/src \
 		CC="$(TARGET_CC)" \
-		CFLAGS="$(TARGET_CFLAGS) -DLINUX" \
+		CFLAGS="$(TARGET_CFLAGS) $(FPIC) -DLINUX" \
 		CPPFLAGS="-I$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include" \
 		LDFLAGS="-L$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib" \
 		AR="$(TARGET_AR)" \
@@ -24,12 +23,12 @@ $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 		STRIP="$(TARGET_STRIP)"
 
 $($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
-	mkdir -p $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib
-	cp -a $(MATRIXSSL_DIR)/src/libmatrixssl.so* $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/
-	cp $(MATRIXSSL_DIR)/src/libmatrixsslstatic.a $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/
 	mkdir -p $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/matrixSsl
 	cp $(MATRIXSSL_DIR)/matrixSsl.h $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/matrixSsl
 	ln -sf matrixSsl/matrixSsl.h $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/matrixSsl.h
+	mkdir -p $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib
+	cp $(MATRIXSSL_DIR)/src/libmatrixsslstatic.a $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/
+	cp -a $(MATRIXSSL_DIR)/src/libmatrixssl.so* $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
 	$(INSTALL_LIBRARY_STRIP)
@@ -40,8 +39,9 @@ $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(MATRIXSSL_DIR)/src clean
-	$(RM) $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libmatrixssl*
-	$(RM) -r $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/matrixSsl*
+	$(RM) -r \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libmatrixssl* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/matrixSsl*
 
 $(pkg)-uninstall:
 	$(RM) $(MATRIXSSL_TARGET_DIR)/libmatrixssl*.so*
