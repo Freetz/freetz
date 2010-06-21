0cb3f1af3bad7d7ef41dbc9db57e351ca7497bc7
Gene Rudoy <gene@freetz.org>
2010-06-21 07:07:18 +0000

    mcabber:  * it seems mcabber doesn't compile since r4750, fix it
    git-svn-id: http://svn.freetz.org/trunk@5111 f5190166-0702-4917-9039-51ec32eddaf5

--- make/mcabber/mcabber.mk
+++ make/mcabber/mcabber.mk
@@ -1,11 +1,12 @@
 $(call PKG_INIT_BIN, 0.9.10)
 $(PKG)_SOURCE:=mcabber-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SOURCE_MD5:=887415d16c32af58eab2ec2d9bb17fa6
 $(PKG)_SITE:=http://mirror.mcabber.com/files
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/mcabber
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/mcabber
-$(PKG)_SOURCE_MD5:=887415d16c32af58eab2ec2d9bb17fa6
 
-$(PKG)_DEPENDS_ON := glib2 ncurses ncurses-panel
+$(PKG)_DEPENDS_ON := glib2 ncurses
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_MCABBER_STATIC
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_MCABBER_WITH_SSL
@@ -18,7 +19,7 @@ ifeq ($(strip $(FREETZ_PACKAGE_MCABBER_STATIC)),y)
 $(PKG)_LDFLAGS := -static
 endif
 
-$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_MCABBER_WITH_SSL),--with-openssl, --without-ssl)
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_MCABBER_WITH_SSL),--with-openssl,--without-ssl)
 $(PKG)_CONFIGURE_OPTIONS += --disable-gpgme
 
 $(PKG_SOURCE_DOWNLOAD)
