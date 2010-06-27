a47589bdcfa315857e5ac2e803aa506e637fac83
Oliver Metz <oliver@freetz.org>
2010-06-27 12:58:44 +0000

    * Bump 7240, 7270_v2 and 7270_v3 preview firmware version to XX.04.85-17671
    git-svn-id: http://svn.freetz.org/trunk@5161 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/480-alarmclock.sh
+++ patches/480-alarmclock.sh
@@ -9,6 +9,8 @@ elif isFreetzType 3270_V3; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/480-alarmclock_3270v3.patch"
 elif isFreetzType 7270 7570 && ! isFreetzType LANG_DE; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/${FREETZ_TYPE_LANG_STRING}/480-alarmclock_7270.patch"
+elif isFreetzType 7240 7270 7270_V3 && isFreetzType PREVIEW; then
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/480-alarmclock_7270_preview.patch"
 elif isFreetzType 7240 7270 7270_V3 && isFreetzType LABOR; then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/480-alarmclock_7270_labor.patch"
 elif isFreetzType 3270 7240 7270 7270_V3; then
