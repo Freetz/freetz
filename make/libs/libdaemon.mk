5d8f250cadff2ba1739abe4add15d0ff1e2a4199
Whoopie <whoopie79@gmx.net>
2010-07-12 21:19:28 +0000

    libdaemon: add it to the CHANGELOG, small cleanups
    git-svn-id: http://svn.freetz.org/trunk@5255 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/libdaemon.mk
+++ make/libs/libdaemon.mk
@@ -1,11 +1,11 @@
 $(call PKG_INIT_LIB, 0.12)
 $(PKG)_LIB_VERSION:=0.3.1
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=76596823cc1a6d1cdf7779b782ff0ee6
 $(PKG)_SITE:=http://0pointer.de/lennart/projects/libdaemon/
 $(PKG)_BINARY:=$($(PKG)_DIR)/libdaemon/.libs/libdaemon.so.$($(PKG)_LIB_VERSION)
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/libdaemon.so.$($(PKG)_LIB_VERSION)
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_LIB)/libdaemon.so.$($(PKG)_LIB_VERSION)
-$(PKG)_SOURCE_MD5:=76596823cc1a6d1cdf7779b782ff0ee6
 
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
@@ -36,7 +36,7 @@ $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(LIBDAEMON_DIR) clean
 	$(RM) $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libdaemon.* \
-			$(TARGET_TOOLCHAIN_STAGING_DIR)/include/daemon.h
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/include/daemon.h
 
 $(pkg)-uninstall:
 	$(RM) $(LIBDAEMON_DEST_LIB)/libdaemon*.so*
