644a442b7888b1827a4b138445f77d68c0237209
Michael Heimpold <mhei@heimpold.de>
2010-07-12 19:26:30 +0000

    * Include support for libdaemon, a lightweight C library that eases the writing of UNIX daemons.
    git-svn-id: http://svn.freetz.org/trunk@5254 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/libs/libdaemon.mk
@@ -0,0 +1,44 @@
+$(call PKG_INIT_LIB, 0.12)
+$(PKG)_LIB_VERSION:=0.3.1
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=http://0pointer.de/lennart/projects/libdaemon/
+$(PKG)_BINARY:=$($(PKG)_DIR)/libdaemon/.libs/libdaemon.so.$($(PKG)_LIB_VERSION)
+$(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/libdaemon.so.$($(PKG)_LIB_VERSION)
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_LIB)/libdaemon.so.$($(PKG)_LIB_VERSION)
+$(PKG)_SOURCE_MD5:=76596823cc1a6d1cdf7779b782ff0ee6
+
+$(PKG)_CONFIGURE_OPTIONS += --enable-static
+$(PKG)_CONFIGURE_OPTIONS += --enable-shared
+$(PKG)_CONFIGURE_OPTIONS += --disable-lynx
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(LIBDAEMON_DIR) all
+
+$($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
+	$(SUBMAKE) -C $(LIBDAEMON_DIR) \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+		install
+	$(PKG_FIX_LIBTOOL_LA) \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libdaemon.la \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libdaemon.pc
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
+	$(INSTALL_LIBRARY_STRIP)
+
+$(pkg): $($(PKG)_STAGING_BINARY)
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(LIBDAEMON_DIR) clean
+	$(RM) $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libdaemon.* \
+			$(TARGET_TOOLCHAIN_STAGING_DIR)/include/daemon.h
+
+$(pkg)-uninstall:
+	$(RM) $(LIBDAEMON_DEST_LIB)/libdaemon*.so*
+
+$(PKG_FINISH)
