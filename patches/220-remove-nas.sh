2252bc6dc7c49111c4ece8adeb5cf80a79a52031
Oliver Metz <oliver@freetz.org>
2010-07-28 17:07:16 +0000

    * Add patch to remove nas webinterface (only labor preview)   * Idea by Spock1985, refs #915
    git-svn-id: http://svn.freetz.org/trunk@5295 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ patches/220-remove-nas.sh
@@ -0,0 +1,8 @@
+[ "$FREETZ_REMOVE_NAS" == "y" ] || return 0
+
+echo1 "removing nas"
+rm -rf "${FILESYSTEM_MOD_DIR}/usr/www.nas"
+ln -sf www "${FILESYSTEM_MOD_DIR}/usr/www.nas"
+
+modsed "s/CONFIG_NAS.*/CONFIG_NAS=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_nas_preview.patch"
