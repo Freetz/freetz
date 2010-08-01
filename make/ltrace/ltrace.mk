4247de1b0be69c37d032630550e8da24a51c4de8
Oliver Metz <oliver@freetz.org>
2010-08-01 14:31:46 +0000

    * ltrace: Bump to version 0.5.3   * Patches taken from openembedded
    git-svn-id: http://svn.freetz.org/trunk@5350 f5190166-0702-4917-9039-51ec32eddaf5

--- make/ltrace/ltrace.mk
+++ make/ltrace/ltrace.mk
@@ -1,27 +1,17 @@
-LTRACE_SVN_REVISION:=81
-$(call PKG_INIT_BIN, 0.5_$(LTRACE_SVN_REVISION))
-$(PKG)_SOURCE:=ltrace-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SITE:=http://freetz.magenbrot.net
+#LTRACE_SVN_REVISION:=81
+#$(call PKG_INIT_BIN, 0.5_$(LTRACE_SVN_REVISION))
+$(call PKG_INIT_BIN, 0.5.3)
+$(PKG)_SOURCE:=ltrace_$($(PKG)_VERSION).orig.tar.gz
+$(PKG)_SOURCE_MD5:=3fa7fe715ab879db08bd06d1d59fd90f
+$(PKG)_SITE:=http://ftp.debian.org/pool/main/l/ltrace
 $(PKG)_BINARY:=$($(PKG)_DIR)/ltrace
 $(PKG)_CONF:=$($(PKG)_DIR)/etc/ltrace.conf
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/ltrace
 $(PKG)_TARGET_CONF:=$($(PKG)_DEST_DIR)/etc/ltrace.conf
-$(PKG)_SOURCE_MD5:=aedc537a2d937c4fc6f6d95c3b028be0
-
-# Remarks:
-#   - LTRACE_SOURCE is created like this:
-#     svn export -r 81 svn://svn.debian.org/ltrace/ltrace/trunk ltrace-0.5_81
-#     tar cvjf ltrace-0.5_81.tar.bz2 ltrace-0.5_81/
-#   - Because we do not want the build process to depend on the availability
-#     of a Subversion client (svn checkout), we provide the ltrace source
-#     package as a download on Freetz mirrors and use DL_TOOL to download it.
 
 $(PKG)_DEPENDS_ON := libelf	
 
-$(PKG)_CONFIGURE_PRE_CMDS += ./autogen.sh ;
-$(PKG)_CONFIGURE_PRE_CMDS += ( cd sysdeps/linux-gnu/mipsel; \
-					../mksyscallent $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/asm/unistd.h > syscallent.h; \
-					../mksignalent $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/asm/signal.h > signalent.h; );
+$(PKG)_CONFIGURE_PRE_CMDS += ln -sf ./mipsel sysdeps/linux-gnu/mips ;
 
 $(PKG)_CONFIGURE_ENV += LD="$(TARGET_LD)"
 
@@ -36,7 +26,7 @@ $($(PKG)_TARGET_CONF): $($(PKG)_CONF)
 	cp $< $@
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(LTRACE_DIR) ARCH=mipsel
+	$(SUBMAKE) -C $(LTRACE_DIR) ARCH=mips
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
@@ -46,7 +36,7 @@ $(pkg):
 $(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_TARGET_CONF)
 
 $(pkg)-clean:
-	-$(SUBMAKE) -C $(LTRACE_DIR) clean ARCH=mipsel
+	-$(SUBMAKE) -C $(LTRACE_DIR) clean ARCH=mips
 
 $(pkg)-uninstall:
 	$(RM) $(LTRACE_TARGET_BINARY)
