de1bc66502ffd76f533b448663c8e0f879c1d1a2
Oliver Metz <oliver@freetz.org>
2010-08-02 07:27:36 +0000

    * ltrace: Fix compile (refs #932)
    git-svn-id: http://svn.freetz.org/trunk@5361 f5190166-0702-4917-9039-51ec32eddaf5

--- make/ltrace/ltrace.mk
+++ make/ltrace/ltrace.mk
@@ -10,7 +10,7 @@ $(PKG)_TARGET_CONF:=$($(PKG)_DEST_DIR)/etc/ltrace.conf
 $(PKG)_DEPENDS_ON := libelf	
 
 $(PKG)_CONFIGURE_PRE_CMDS += ln -sf ./mipsel sysdeps/linux-gnu/mips ;
-$(PKG)_CONFIGURE_PRE_CMDS += ( cd sysdeps/linux-gnu/mips; \
+$(PKG)_CONFIGURE_PRE_CMDS += ( cd sysdeps/linux-gnu/mips; chmod +x ../mksyscallent_mips; \
 	../mksyscallent_mips $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/asm/unistd.h > syscallent.h; \
 	../mksignalent $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/asm/signal.h > signalent.h; );
 
