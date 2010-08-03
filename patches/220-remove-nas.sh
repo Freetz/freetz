5f06889d1319b1bba342778bde7ce50e11cc6e33
Oliver Metz <oliver@freetz.org>
2010-08-03 09:51:23 +0000

    * Remove internal memory file when removing nas webinterface   * saves nearly 400 KB   * refs #827
    git-svn-id: http://svn.freetz.org/trunk@5391 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/220-remove-nas.sh
+++ patches/220-remove-nas.sh
@@ -6,3 +6,7 @@ ln -sf www "${FILESYSTEM_MOD_DIR}/usr/www.nas"
 
 modsed "s/CONFIG_NAS.*/CONFIG_NAS=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
 modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_nas_preview.patch"
+
+echo2 "removing internal memory"
+rm -rf ${FILESYSTEM_MOD_DIR}/etc/internal_memory_default*.tar
+
