e29eeee720a08dcb4cb469458d5b770e9cf4b82b
Oliver Metz <oliver@freetz.org>
2010-08-04 21:43:36 +0000

    [freetz-stable-1.1] Merge in r3974, r4640, r5062 from trunk: ------------------------------------------------------------------------ r3974 | Whoopie | 2009-11-26 16:05:03 +0100 (Do, 26. Nov 2009) | 2 Zeilen dnsmasq: bump version to 2.51 ------------------------------------------------------------------------ r4640 | oliver | 2010-04-02 17:50:29 +0200 (Fr, 02. Apr 2010) | 1 Zeile  * dnsmasq: Bump to version 2.52 ------------------------------------------------------------------------ r5062 | er13 | 2010-06-13 22:17:27 +0200 (So, 13. Jun 2010) | 3 Zeilen dnsmasq:  * bump version to 2.55
    Fix merge of changeset r4943:
    * Bump firmwares:
     * Fritz!Box Fon WLAN 7170 (English, Annex A): 58.04.82
     * Fritz!Box Fon WLAN 7170 (English, Annex B): 29.04.82
    
    
    git-svn-id: http://svn.freetz.org/branches/freetz-stable-1.1@5407 f5190166-0702-4917-9039-51ec32eddaf5

--- make/dnsmasq/dnsmasq.mk
+++ make/dnsmasq/dnsmasq.mk
@@ -1,20 +1,24 @@
-$(call PKG_INIT_BIN, 2.50)
-$(PKG)_SOURCE:=dnsmasq-$($(PKG)_VERSION).tar.gz
+$(call PKG_INIT_BIN, 2.55)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=http://thekelleys.org.uk/dnsmasq
-$(PKG)_DIR:=$(SOURCE_DIR)/dnsmasq-$($(PKG)_VERSION)
-$(PKG)_BINARY:=$(DNSMASQ_DIR)/src/dnsmasq
-$(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/root/usr/sbin/dnsmasq
 $(PKG)_STARTLEVEL=40
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/dnsmasq
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/dnsmasq
+
+$(PKG)_COPTS := -DNO_IPV6
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_NOP)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(MAKE) CC="$(TARGET_CC)" \
+	PATH="$(TARGET_PATH)" \
+		$(MAKE) -C $(DNSMASQ_DIR) \
+		CC="$(TARGET_CC)" \
+		COPTS="$(DNSMASQ_COPTS)" \
 		CFLAGS="$(TARGET_CFLAGS)" \
-		LDFLAGS="" \
-		-C $(DNSMASQ_DIR)
+		LDFLAGS=""
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
