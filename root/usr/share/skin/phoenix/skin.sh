239576cd28065ee19072a20d8ce75a8be2ac7173
Andreas Bühmann <andreas.buehmann@web.de>
2010-08-05 16:37:52 +0000

    [phoenix] Refs #941:  * Try centered layout  * Red footer, similar to header  * Distinguish section headings from `h1` used within sections  * Try shaded log output
    git-svn-id: http://svn.freetz.org/trunk@5412 f5190166-0702-4917-9039-51ec32eddaf5

--- root/usr/share/skin/phoenix/skin.sh
+++ root/usr/share/skin/phoenix/skin.sh
@@ -5,35 +5,12 @@ cat << EOF
 <link rel="stylesheet" type="text/css" href="/style/phoenix/base.css">
 <link rel="stylesheet" type="text/css" href="/style/colorscheme.css">
 EOF
-
-# custom style for fieldset and div.body
-if [ ! "$_cgi_width" ]; then
-	let _cgi_width=$MOD_CGI_WIDTH
-fi
-export _cgi_width
-let _cgi_total_width="$_cgi_width+40"
-let _usr_style="$_cgi_width-230"
-cat << EOF
-<style type="text/css">
-<!--
-fieldset {
-	margin: 0px;
-	margin-top: 10px;
-	margin-bottom: 10px;
-	padding: 10px;
-	width: ${_usr_style}px;
-}
-div.body {
-	width: ${_usr_style}px;
-}
--->
-</style>
-EOF
 }
 
 _cgi_body_begin() {
 local title=$1 id=$2
 cat << EOF
+<div id="world">
 <div id="header">
 <span class="version">$(html < /etc/.freetz-version)</span>
 <h1><a href="/cgi-bin/index.cgi" class="logo">Freetz</a>&nbsp;<a href="/cgi-bin/about.cgi" target="_blank">&ndash;</a> <span class="title">$title</span></h1>
@@ -48,9 +25,11 @@ echo "<div id='content'>"
 _cgi_body_end() {
 cat << EOF
 </div>
+</div>
 <div id="footer">
 <span class="datetime" title="$(lang de:"Systemzeit des Routers" en:"Router's system time")">$(date +'$(lang de:"%d.%m.%Y" en:"%m/%d/%Y") %H:%M')</span>&nbsp;&ndash;
 <span class="uptime" title="Uptime">$(uptime | sed -r 's/.*(up.*), load.*/\1/')</span>
+</div>
 </div>
 EOF
 }
