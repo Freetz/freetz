aae4c07c6ea2b59c8684ae1ca8192991fad71d3b
Oliver Metz <oliver@freetz.org>
2010-08-10 22:33:05 +0000

    * mc: Bump to 4.6.2
    git-svn-id: http://svn.freetz.org/trunk@5469 f5190166-0702-4917-9039-51ec32eddaf5

--- make/mc/mc.mk
+++ make/mc/mc.mk
@@ -1,17 +1,22 @@
-$(call PKG_INIT_BIN, 4.6.1)
+$(call PKG_INIT_BIN, 4.6.2)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SITE:=http://www.ibiblio.org/pub/Linux/utils/file/managers/mc/
+$(PKG)_SOURCE_MD5:=ec92966f4d0c8b50c344fe901859ae2a
+$(PKG)_SITE:=http://www.midnight-commander.org/downloads
 $(PKG)_HELP:=$($(PKG)_MAKE_DIR)/files/root/usr/share/mc/mc.hlp
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/$(pkg)
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/mc.bin
 $(PKG)_TARGET_HELP:=$($(PKG)_DEST_DIR)/usr/share/mc/mc.hlp
-$(PKG)_SOURCE_MD5:=18b20db6e40480a53bac2870c56fc3c4
+ifneq ($(strip $(FREETZ_PACKAGE_MC_ONLINE_HELP)),y)
+$(PKG)_NOT_INCLUDED:=$($(PKG)_TARGET_HELP)
+endif
 
 $(PKG)_DEPENDS_ON := ncurses-terminfo
 ifeq ($(strip $(FREETZ_TARGET_UCLIBC_VERSION_0_9_28)),y)
 $(PKG)_DEPENDS_ON += libiconv
+$(PKG)_LIBS:=-liconv
 else
 $(PKG)_CONFIGURE_OPTIONS += --without-libiconv-prefix
+$(PKG)_LIBS:=
 endif
 ifeq ($(strip $(FREETZ_PACKAGE_MC_FORCE_GLIB12)),y)
 $(PKG)_DEPENDS_ON += glib
@@ -21,6 +26,8 @@ endif
 ifeq ($(strip $(FREETZ_PACKAGE_MC_WITH_NCURSES)),y)
 $(PKG)_DEPENDS_ON += ncurses
 endif
+
+$(PKG)_CONFIGURE_PRE_CMDS += ./autogen.sh ;
 
 $(PKG)_CONFIGURE_ENV += mc_cv_have_zipinfo=yes
 
@@ -51,6 +58,7 @@ $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(MC_DIR) \
+		LIBICONV="$(MC_LIBS)" \
 		$(if $(FREETZ_PACKAGE_MC_FORCE_GLIB12),GLIB_CFLAGS="-I$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/glib-1.2" GLIB_LIBS="-lglib",)
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
@@ -65,11 +73,8 @@ $(pkg):
 ifeq ($(strip $(FREETZ_PACKAGE_MC_ONLINE_HELP)),y)
 $(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_TARGET_HELP)
 else
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $(pkg)-clean-help
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 endif
-
-$(pkg)-clean-help:
-	@$(RM) $(MC_TARGET_HELP)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(MC_DIR) clean
@@ -78,3 +83,4 @@ $(pkg)-uninstall:
 	$(RM) $(MC_TARGET_BINARY)
 
 $(PKG_FINISH)
+
