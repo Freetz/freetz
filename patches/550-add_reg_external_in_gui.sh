f1af69ee82c561b3af8e6dc1d57a4a6eb9ce422d
Oliver Metz <oliver@freetz.org>
2010-08-22 10:41:34 +0000

    * patches: Fix reg from outside patch (by cgabriel & MaxMuster, closes #609)
    git-svn-id: http://svn.freetz.org/trunk@5608 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/550-add_reg_external_in_gui.sh
+++ patches/550-add_reg_external_in_gui.sh
@@ -1,15 +1,30 @@
 [ "$FREETZ_ADD_REGEXT_GUI" == "y" ] || return 0
+# own modsed, use --follow-symlink 
+# and don't warn if file not found
+# because fon1IPPhone.* files are not allways available; seems easier than something like 
+# [ -x ${dir}/fon1IPPhone.<xy> ] && modsed ...
+
+modsed()
+{
+	if [ -f "$2" ]; then
+		echo2 "patching $2"
+		sed -i --follow-symlink -e "$1" "$2"
+	fi
+}
 echo1 "adding GUI switch to set reg_from_outside"
 
-DIRSS2PATCH=$(find ${FILESYSTEM_MOD_DIR}/usr/www/ -name fon_config)
-if  [ $DIRSS2PATCH ]; then
+DIRSS2PATCH=$(find ${FILESYSTEM_MOD_DIR}/usr/www/ -type d -name fon_config)
+if [ -n "$DIRSS2PATCH" ]; then
 	for dir in $DIRSS2PATCH; do
-		modsed  '/id="uiPostIPPhoneExtNumber"/ a<Input type="hidden" name="voipextension:settings/extension<? echo $var:DeviceIsdnPort ?>/reg_from_outside" value="" id="uiPostIPPhoneExtRegFromOutside" disabled>' ${dir}/fon_config_End.frm
-		modsed  '/jslEnable("uiPostIPPhoneExtNumber")/ ajslCopyValue( "uiPostIPPhoneExtRegFromOutside", "uiIPPhoneExtRegFromOutside"); jslEnable("uiPostIPPhoneExtRegFromOutside");' ${dir}/fon_config_End.js
-		modsed  '/id="uiShowOutgoingMsn"/ i<? if eq "$var:TechType" "IPPHONE" ` \n<tr><td class="c1"><span >reg_from_outside</span></td>\n<td class="c2">\n<? if eq "$var:IPPhoneExtRegFromOutside" "1"\n`\nyes\n` `\nno\n` ?>\n</td></tr>\n` ?>' ${dir}/fon_config_End.html
-		modsed  '/id="uiIPPhoneExtNumber"/ a<input type="hidden" name="var:IPPhoneExtRegFromOutside" value="<? echo $var:IPPhoneExtRegFromOutside ?>" id="uiIPPhoneExtRegFromOutside">' ${dir}/fon_config.frm
-		modsed  '/jslCopyValue( "uiIPPhoneExtNumber", "uiDevicePort")/ ajslCopyValue( "uiIPPhoneExtRegFromOutside", "uiRegFromOutside");' ${dir}/fon_config_IPPhone_1.js
-		modsed  "/<\/table\>/ i<tr style=\"padding-top:15px;padding-bottom:15px\;\"\>\n<td style=\"width: 190px\; height:30px\;\"\>Registrierung v. Extern<\/td\>\n<td\><input type=\"checkbox\" name=\"reg_from_outside\" id=\"uiRegFromOutside\" value=\"0\" onclick='\(this.value=\(this.checked\)? \"1\" : \"0\"\)'></td\>\n</tr\>" ${dir}/fon_config_IPPhone_1.html
+		modsed '/id="uiPostIPPhoneExtNumber"/ a<Input type="hidden" name="voipextension:settings/extension<? echo $var:DeviceIpPhonePort ?>/reg_from_outside" value="" id="uiPostIPPhoneExtRegFromOutside" disabled>' ${dir}/fon_config_End.frm
+		modsed '/jslEnable("uiPostIPPhoneExtNumber")/ ajslCopyValue( "uiPostIPPhoneExtRegFromOutside", "uiIPPhoneExtRegFromOutside"); jslEnable("uiPostIPPhoneExtRegFromOutside");' ${dir}/fon_config_End.js
+		modsed '/id="uiShowOutgoingMsn"/ i<? if eq "$var:TechType" "IPPHONE" ` \n<tr><td class="c1"><span >reg_from_outside</span></td>\n<td class="c2">\n<? if eq "$var:IPPhoneExtRegFromOutside" "1"\n`\nyes\n` `\nno\n` ?>\n</td></tr>\n` ?>' ${dir}/fon_config_End.html
+		modsed '/id="uiIPPhoneExtNumber"/ a<input type="hidden" name="var:IPPhoneExtRegFromOutside" value="<? echo $var:IPPhoneExtRegFromOutside ?>" id="uiIPPhoneExtRegFromOutside">' ${dir}/fon_config.frm
+		modsed '/jslCopyValue( "uiIPPhoneExtNumber", "uiDevicePort")/ ajslSetValue( "uiIPPhoneExtRegFromOutside", (jslGetChecked("uiRegFromOutside"))? "1" : "0");' ${dir}/fon_config_IPPhone_1.js
+		modsed '/<\/table>/ i<tr style="padding-top:15px;padding-bottom:15px;">\n<td style="width: 190px; height:30px;">Registrierung v. Extern</td>\n<td><input type="checkbox" name="reg_from_outside" id="uiRegFromOutside"></td>\n</tr>' ${dir}/fon_config_IPPhone_1.html
+		modsed '/id="uiPostIPPhonePasswd"/ a<Input type="hidden" name="voipextension:settings/extension<? echo $var:DeviceIpPhonePort ?>/reg_from_outside" value="<? query voipextension:settings/extension<? echo $var:DeviceIpPhonePort ?>/reg_from_outside ?>" id="uiPostIPPhoneRegFromOutside">' ${dir}/fon1IPPhone.frm
+		modsed '/function uiDoOnLoad()/,/function uiDoCancel()/ {s%jslDisplay("uiIP_General",true);%&\njslGetCheckValue( "uiRegFromOutside", "uiPostIPPhoneRegFromOutside");% 1 ; s%if (jslGetValue("uiViewPassword") \!="\*\*\*\*")%jslSetCheckValue( "uiPostIPPhoneRegFromOutside", "uiRegFromOutside");\n&%1}' ${dir}/fon1IPPhone.js
+		modsed '/id="uiViewPassword"/ a</tr>\n<tr style="padding-top:15px;padding-bottom:15px;">\n<td style="width: 190px; height:30px;">Registrierung v. Extern</td>\n<td><input type="checkbox" name="reg_from_outside" id="uiRegFromOutside" value="0")"></td>' ${dir}/fon1IPPhone.html
 	done
 fi
 
