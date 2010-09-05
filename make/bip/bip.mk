3d2cf3e60c119d13ddacc496649b4be0ba80c383
Oliver Metz <oliver@freetz.org>
2010-09-05 13:47:54 +0000

    * bip: Bump to version 0.8.5 (fixes #1021)
    git-svn-id: http://svn.freetz.org/trunk@5714 f5190166-0702-4917-9039-51ec32eddaf5

--- make/bip/bip.mk
+++ make/bip/bip.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN,0.8.4)
+$(call PKG_INIT_BIN,0.8.5)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=76fe2800efdfbe54bf72540e40a91150
+$(PKG)_SOURCE_MD5:=9109cdfb9125f40cd40e86f2b3b0e5d0
 $(PKG)_SITE:=http://bip.t1r.net/downloads
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/bip
