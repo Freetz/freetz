fef62c09890c5faadae34bba70aae2d1727afa8f
Andreas Bühmann <andreas.buehmann@web.de>
2010-09-07 15:42:44 +0000

    extra/handler: Fix typos; provide default security level. Refs #1024
    git-svn-id: http://svn.freetz.org/trunk@5733 f5190166-0702-4917-9039-51ec32eddaf5

--- make/mod/files/root/usr/lib/mww/page.d/extra/handler.sh
+++ make/mod/files/root/usr/lib/mww/page.d/extra/handler.sh
@@ -4,15 +4,16 @@ EXTRA_REG=/mod/etc/reg/extra.reg
 IFS='|'
 set -- $(grep "^$PACKAGE|.*|$ID\$" "$EXTRA_REG")
 IFS=$OIFS
+sec=${3:-2}
 
 cgi --id="extra:$PACKAGE/$ID" --help="/packages/$PACKAGE/$ID"
 
-if [ "$sec_level" -gt "$3" ]; then
+if [ "$sec_level" -gt "$sec" ]; then
 	cgi_begin 'Extras'
 	echo '<h1>$(lang de:"Zusatz-Skript" en:"Additional script")</h1>'
 	print_warning '$(lang
-		de:"Dieses Zusatz-Skript in der aktuellen Sicherheitsstufe
-			nicht verf&uuml;gbar!" 
+		de:"Dieses Zusatz-Skript ist in der aktuellen Sicherheitsstufe
+			nicht verf&uuml;gbar!"
 		en:"This script is not available at the current security
 			level!"
 		)'
@@ -26,7 +27,7 @@ script="/mod/usr/lib/cgi-bin/$path.cgi"
 if [ -x "$script" ]; then
 	"$script"
 else
-	cgi_error "$(lang 
+	cgi_error "$(lang
 		de:"Zusatz-Skript '$path.cgi' nicht gefunden."
 		en:"Additional script '$path.cgi' not found."
 	)"
