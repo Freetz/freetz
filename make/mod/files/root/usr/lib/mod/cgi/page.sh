8e5232b8855c2cb198b779d06aaf365d62723867
Andreas Bühmann <andreas.buehmann@web.de>
2010-09-08 16:29:24 +0000

    Make IE not use ancient rendering modes; see http://msdn.microsoft.com/library/cc817574.aspx
    git-svn-id: http://svn.freetz.org/trunk@5736 f5190166-0702-4917-9039-51ec32eddaf5

--- make/mod/files/root/usr/lib/mod/cgi/page.sh
+++ make/mod/files/root/usr/lib/mod/cgi/page.sh
@@ -75,6 +75,7 @@ Content-Type: text/html; charset=ISO-8859-1${CRet}
 Content-Language: $(lang de:"de" en:"en")${CRet}
 Expires: 0${CRet}
 Pragma: no-cache${CRet}
+X-UA-Compatible: IE=edge${CRet}
 ${CRet}
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
    "http://www.w3.org/TR/html4/loose.dtd">
