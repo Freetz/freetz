f2ce24f0f8633d03b39022b77563d2e7d17128d6
Gene Rudoy <gene@freetz.org>
2010-09-15 00:17:57 +0000

    iptables:  * do not install three different binaries, use multibinary with symlinks to it instead (idea by abraXxl)  * fixes 1031
    git-svn-id: http://svn.freetz.org/trunk@5788 f5190166-0702-4917-9039-51ec32eddaf5

--- make/iptables/iptables.mk
+++ make/iptables/iptables.mk
@@ -6,16 +6,16 @@ ifneq ($(FREETZ_TARGET_IPV6_SUPPORT),y)
 $(PKG)_CONDITIONAL_PATCHES+=cond
 endif
 
-$(PKG)_BINARIES_ALL:=iptables ip6tables iptables-save iptables-restore
-$(PKG)_BINARIES:=\
-	$(filter-out \
-		$(if $(FREETZ_TARGET_IPV6_SUPPORT),,ip6tables) \
-		$(if $(FREETZ_PACKAGE_IPTABLES_SAVE_RESTORE),,iptables-save iptables-restore), \
-		$($(PKG)_BINARIES_ALL) \
-	)
+$(PKG)_BINARIES_ALL := iptables-multi ip6tables-multi
+$(PKG)_BINARIES := $(filter-out $(if $(FREETZ_TARGET_IPV6_SUPPORT),,ip6%), $($(PKG)_BINARIES_ALL))
 $(PKG)_BINARIES_BUILD_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DIR)/%)
 $(PKG)_BINARIES_TARGET_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DEST_DIR)/usr/sbin/%)
-$(PKG)_NOT_INCLUDED := $(patsubst %,$($(PKG)_DEST_DIR)/usr/sbin/%,$(filter-out $($(PKG)_BINARIES),$($(PKG)_BINARIES_ALL)))
+
+$(PKG)_SYMLINKS_ALL := iptables iptables-save iptables-restore ip6tables ip6tables-save ip6tables-restore
+$(PKG)_SYMLINKS := $(filter-out $(if $(FREETZ_TARGET_IPV6_SUPPORT),,ip6%) $(if $(FREETZ_PACKAGE_IPTABLES_SAVE_RESTORE),,%-save %-restore), $($(PKG)_SYMLINKS_ALL))
+$(PKG)_SYMLINKS_TARGET_DIR := $($(PKG)_SYMLINKS:%=$($(PKG)_DEST_DIR)/usr/sbin/%)
+
+$(PKG)_NOT_INCLUDED := $(patsubst %,$($(PKG)_DEST_DIR)/usr/sbin/%, $(filter-out $($(PKG)_BINARIES),$($(PKG)_BINARIES_ALL)) $(filter-out $($(PKG)_SYMLINKS),$($(PKG)_SYMLINKS_ALL)))
 
 $(PKG)_LIB_BUILD_DIR:=$($(PKG)_DIR)/libiptc/libiptc.a
 $(PKG)_LIB_STAGING_DIR:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libiptc.a
@@ -39,13 +39,22 @@ $($(PKG)_LIB_STAGING_DIR): $($(PKG)_LIB_BUILD_DIR)
 $($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/sbin/%: $($(PKG)_DIR)/%
 	$(INSTALL_BINARY_STRIP)
 
+define $(PKG)_SYMLINK_RULE
+$$(filter $($(PKG)_DEST_DIR)/usr/sbin/$(1)%,$($(PKG)_SYMLINKS_TARGET_DIR)): $($(PKG)_DEST_DIR)/usr/sbin/$(1)-multi
+	ln -sf $$(notdir $$<) $$@
+endef
+$(eval $(call $(PKG)_SYMLINK_RULE,iptables))
+ifeq ($(FREETZ_TARGET_IPV6_SUPPORT),y)
+$(eval $(call $(PKG)_SYMLINK_RULE,ip6tables))
+endif
+
 $($(PKG)_TARGET_EXTENSIONS): $($(PKG)_BINARIES_BUILD_DIR)
 	mkdir -p $(IPTABLES_EXTENSIONS_DIR)
 	cp $(IPTABLES_DIR)/extensions/*.so $(IPTABLES_EXTENSIONS_DIR)/
 	$(TARGET_STRIP) $(IPTABLES_EXTENSIONS_DIR)/*.so
 	touch $@
 
-$(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR) $($(PKG)_TARGET_EXTENSIONS) $($(PKG)_LIB_STAGING_DIR)
+$(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR) $($(PKG)_SYMLINKS_TARGET_DIR) $($(PKG)_TARGET_EXTENSIONS) $($(PKG)_LIB_STAGING_DIR)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(IPTABLES_DIR) clean
@@ -54,6 +63,7 @@ $(pkg)-clean:
 $(pkg)-uninstall:
 	$(RM) \
 		$(IPTABLES_BINARIES_ALL:%=$(IPTABLES_DEST_DIR)/usr/sbin/%) \
+		$(IPTABLES_SYMLINKS_ALL:%=$(IPTABLES_DEST_DIR)/usr/sbin/%) \
 		$(IPTABLES_EXTENSIONS_DIR)/*
 
 $(PKG_FINISH)
