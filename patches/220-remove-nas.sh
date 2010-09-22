f5102a3be32a205ba065eb53563206323a4b288d
Oliver Metz <oliver@freetz.org>
2010-09-22 01:46:59 +0000

    * Fix errors:   * 7270 v1 doesn't have NAS and IPv6   * fix name of nas patch   * Copy alarmclock patches   * Readd a-ch and en patches for 7270 v2  * refs #1033
    git-svn-id: http://svn.freetz.org/trunk@5836 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/220-remove-nas.sh
+++ patches/220-remove-nas.sh
@@ -5,7 +5,7 @@ rm -rf "${FILESYSTEM_MOD_DIR}/usr/www.nas"
 ln -sf www "${FILESYSTEM_MOD_DIR}/usr/www.nas"
 
 modsed "s/CONFIG_NAS.*/CONFIG_NAS=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
-modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_nas.patch"
+modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_nas_7270.patch"
 
 echo2 "removing internal memory"
 rm -rf ${FILESYSTEM_MOD_DIR}/etc/internal_memory_default*.tar
