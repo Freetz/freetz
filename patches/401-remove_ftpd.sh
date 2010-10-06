b7e49361528f8e82424bbeb908838b9760826e57
Oliver Metz <oliver@freetz.org>
2010-10-06 20:30:59 +0000

    * ftpd: Patch banner string to suppress it   * NAS should work again with this patch   * refs #829
    git-svn-id: http://svn.freetz.org/trunk@5927 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/401-remove_ftpd.sh
+++ patches/401-remove_ftpd.sh
@@ -1,8 +1,17 @@
-[ "$FREETZ_REMOVE_FTPD" == "y" ] || return 0
-echo1 "remove AVM-ftpd files"
-rm_files "${FILESYSTEM_MOD_DIR}/sbin/ftpd" \
-	 "${FILESYSTEM_MOD_DIR}/bin/inetdftp" \
-	 "${FILESYSTEM_MOD_DIR}/etc/ftpd_control"
+[ "$FREETZ_REMOVE_FTPD" == "y" -o "$FREETZ_HAS_NAS" == "y" ] || return 0
 
-echo1 "patching rc.conf"
-modsed "s/CONFIG_FTP=.*$/CONFIG_FTP=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+if [ "$FREETZ_HAS_NAS" == "y" -a "$FREETZ_REMOVE_FTPD" != "y" ]; then
+	echo1 "remove banner from AVM ftpd"
+	perl -pi -e 's#/etc/motd#/etc/fooo#g' "${FILESYSTEM_MOD_DIR}/sbin/ftpd"
+fi
+
+if [ "$FREETZ_REMOVE_FTPD" == "y" ]; then
+	echo1 "remove AVM-ftpd files"
+	rm_files "${FILESYSTEM_MOD_DIR}/sbin/ftpd" \
+		"${FILESYSTEM_MOD_DIR}/bin/inetdftp" \
+		"${FILESYSTEM_MOD_DIR}/etc/ftpd_control"
+
+	echo1 "patching rc.conf"
+	modsed "s/CONFIG_FTP=.*$/CONFIG_FTP=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+	return 0
+fi
