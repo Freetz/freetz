51448d1d06ce741f9151cc395685d2907d1be958
Markus Hempel <info@markushempel.de>
2010-10-07 18:18:04 +0000

    phpxmail: modified source path to reflect correct packed version, exlude special files from target
    git-svn-id: http://svn.freetz.org/trunk@5934 f5190166-0702-4917-9039-51ec32eddaf5

--- make/phpxmail/phpxmail.mk
+++ make/phpxmail/phpxmail.mk
@@ -1,6 +1,7 @@
 $(call PKG_INIT_BIN, 1.5)
 $(PKG)_SOURCE:=$(pkg)$($(PKG)_VERSION).zip
 $(PKG)_SITE:=@SF/$(pkg)
+$(PKG)_SOURCE_DIR:=$($(PKG)_SOURCE_DIR)/$(pkg)-$($(PKG)_VERSION)
 $(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/$(pkg)
 $(PKG)_BINARY:=$($(PKG)_DIR)/index.php
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/etc/default.$(pkg)/config.php.default
@@ -14,7 +15,8 @@ $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	mkdir -p $(PHPXMAIL_DEST_DIR)/usr/mww/phpxmail
-	tar -c -C $(PHPXMAIL_DIR) --exclude=config.php . | tar -x -C $(PHPXMAIL_DEST_DIR)/usr/mww/phpxmail
+	tar -c -C $(PHPXMAIL_DIR) --exclude=config.php --exclude=.unpacked --exclude=.configured \
+		--exclude=.build-* . | tar -x -C $(PHPXMAIL_DEST_DIR)/usr/mww/phpxmail
 	cp $(PHPXMAIL_DIR)/config.php $(PHPXMAIL_TARGET_BINARY)
 
 $(pkg):
