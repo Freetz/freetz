ef449354de05b768f6775925c71e19cb34047ef8
cuma <cuma@freetz.org>
2010-10-09 22:07:12 +0000

    options.cfg: generate options (refs #964)
    git-svn-id: http://svn.freetz.org/trunk@5950 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/900-add_options_cfg.sh
+++ patches/900-add_options_cfg.sh
@@ -1,48 +1,19 @@
 OPTIONS_CFG="${FILESYSTEM_MOD_DIR}/etc/options.cfg"
 
-check_options() {
-	[ "$1" != "y" ] && return
-	shift
-	for current_option in $*; do
-		current_value="$(eval echo \$$current_option)"
-		if [ "$current_value" != "n" -a "$current_value" != "" ]; then
-			echo2 "adding $current_option"
-			echo "$current_option='$current_value'" >> $OPTIONS_CFG
-		fi
-	done
-}
-
 echo1 "creating options.cfg"
 
 if [ "$FREETZ_REMOVE_DOT_CONFIG" != "y" ]; then
-
-echo2 "by symlinking it to .config"
-ln -snf .config $OPTIONS_CFG
-
+	echo2 "by symlinking it to .config"
+	ln -snf .config $OPTIONS_CFG
 else
-
-check_options "$FREETZ_PACKAGE_DAVFS2" \
-	FREETZ_PACKAGE_DAVFS2_WITH_SSL
-check_options "$FREETZ_PACKAGE_LIGHTTPD" \
-	FREETZ_PACKAGE_LIGHTTPD_WITH_SSL \
-	FREETZ_PACKAGE_LIGHTTPD_MOD_ACCESSLOG \
-	FREETZ_PACKAGE_LIGHTTPD_MOD_AUTH \
-	FREETZ_PACKAGE_LIGHTTPD_MOD_EVHOST \
-	FREETZ_PACKAGE_LIGHTTPD_MOD_DIRLISTING \
-	FREETZ_PACKAGE_LIGHTTPD_MOD_REDIRECT \
-	FREETZ_PACKAGE_LIGHTTPD_MOD_CGI \
-	FREETZ_PACKAGE_LIGHTTPD_MOD_COMPRESS \
-	FREETZ_PACKAGE_LIGHTTPD_MOD_STATUS \
-	FREETZ_PACKAGE_LIGHTTPD_MOD_FASTCGI
-check_options "$FREETZ_PACKAGE_NANO" \
-	FREETZ_PACKAGE_NANO_NANORC \
-	FREETZ_PACKAGE_NANO_TINY \
-	FREETZ_PACKAGE_NANO_COLOR_SYNTAX
-check_options "$FREETZ_PACKAGE_OPENDD" \
-	FREETZ_PACKAGE_OPENDD_WITH_SSL
-check_options "$FREETZ_PACKAGE_VSFTPD" \
-	FREETZ_PACKAGE_VSFTPD_WITH_SSL
-check_options "$FREETZ_PACKAGE_XMAIL" \
-	FREETZ_PACKAGE_XMAIL_WITH_SSL
-
+	OPTIONS_FILES="$(find make/*/files/root/etc/init.d/rc.* make/*/files/root/etc/default.*/*_conf make/*/files/root/usr/lib/cgi-bin/*.cgi)"
+	OPTIONS_NAMES="$(grep -ho FREETZ_PACKAGE_[a-zA-Z0-9_]* $OPTIONS_FILES | sort -u)"
+	for OPTIONS_CURRENT in $OPTIONS_NAMES; do
+		OPTIONS_VALUE="$(eval echo \$$OPTIONS_CURRENT)"
+		if [ "$OPTIONS_VALUE" != "n" -a "$OPTIONS_VALUE" != "" ]; then
+			echo2 "adding $OPTIONS_CURRENT"
+			echo "$OPTIONS_CURRENT='$OPTIONS_VALUE'" >> $OPTIONS_CFG
+		fi
+	done
 fi
+
