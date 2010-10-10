e783baad5995599653f614f3f2cee6b9b6f1d72f
Oliver Metz <oliver@freetz.org>
2010-10-10 12:44:16 +0000

    * Bump pciutils to version 3.1.7
    git-svn-id: http://svn.freetz.org/trunk@5959 f5190166-0702-4917-9039-51ec32eddaf5

--- make/pciutils/pciutils.mk
+++ make/pciutils/pciutils.mk
@@ -1,13 +1,13 @@
-$(call PKG_INIT_BIN,3.0.0)
+$(call PKG_INIT_BIN,3.1.7)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_SITE:=http://www.kernel.org/pub/software/utils/pciutils
+$(PKG)_SOURCE_MD5:=35222edb86b50a6641352c66fe829b2e
 $(PKG)_BINARY:=$($(PKG)_DIR)/lspci
 $(PKG)_SETPCI_BINARY:=$($(PKG)_DIR)/setpci
 $(PKG)_IDS:=$($(PKG)_DIR)/pci.ids
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/lspci
 $(PKG)_SETPCI_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/setpci
 $(PKG)_TARGET_IDS:=$($(PKG)_DEST_DIR)/usr/share/pci.ids
-$(PKG)_SOURCE_MD5:=85b5dae042217cf11bca10d52210a78d
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -29,8 +29,7 @@ $($(PKG)_SETPCI_TARGET_BINARY): $($(PKG)_SETPCI_BINARY)
 
 $($(PKG)_TARGET_IDS): $($(PKG)_IDS)
 	$(PCIUTILS_DIR)/update-pciids.sh
-	mkdir -p $(dir $@)
-	cp $^ $@
+	$(INSTALL_FILE)
 
 $(pkg):
 
