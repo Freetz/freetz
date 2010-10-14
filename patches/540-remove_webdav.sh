475f3e8be645792a86c37d729709102835ec9f16
cuma <cuma@freetz.org>
2010-10-14 21:52:01 +0000

    remove-patch for avm's webdav:  * is now selected in menuconfig by freetz-davfs2 (like it was previously in the removescript)  * libctlwebdav.so added  (refs #863)
    git-svn-id: http://svn.freetz.org/trunk@5982 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/540-remove_webdav.sh
+++ patches/540-remove_webdav.sh
@@ -1,17 +1,16 @@
-[ "$FREETZ_PACKAGE_DAVFS2" == "y" -o "$FREETZ_REMOVE_WEBDAV" == "y" ] || return 0
+[ "$FREETZ_REMOVE_WEBDAV" == "y" ] || return 0
 
 echo1 "removing AVM webdav files"
 for files in \
 	lib/libexpat.so* \
 	lib/libneon.so* \
 	sbin/*mount.davfs \
+	usr/share/ctlmgr/libctlwebdav.so \
 	; do
 	rm_files "${FILESYSTEM_MOD_DIR}/$files"
 done
 
 # TODO: Remove "Online-Speicher" from webinterface
 
-if [ "$FREETZ_REMOVE_WEBDAV" == "y" ]; then
-	echo1 "patching rc.conf"
-	modsed "s/CONFIG_WEBDAV=.*$/CONFIG_WEBDAV=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
-fi
+echo1 "patching rc.conf"
+modsed "s/CONFIG_WEBDAV=.*$/CONFIG_WEBDAV=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
