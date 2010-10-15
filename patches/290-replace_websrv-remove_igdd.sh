fd44f04e8dc75022240f5ae1f10930d0d9ed714a
cuma <cuma@freetz.org>
2010-10-15 23:10:37 +0000

    upnp remove patch: create an empty upnpd to satisfy avm's binaries  * prevents "update_usb_info() not found. possible reason: libupnpdevusb.so missing."  * show free space of internal disk on webinterface  * show usb-devices on webinterface (closes #1066)
    git-svn-id: http://svn.freetz.org/trunk@5994 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/290-replace_websrv-remove_igdd.sh
+++ patches/290-replace_websrv-remove_igdd.sh
@@ -5,9 +5,11 @@ fi
 
 if [ "$FREETZ_REMOVE_UPNP" == "y" ]; then
 	echo1 "removing AVM UPnP daemon (igdd or upnpd)"
+	[ -x ${FILESYSTEM_MOD_DIR}/sbin/upnpd ] && FREETZ_REMOVE_UPNP_TOUCH_UPNPD=y
 	rm_files $(find ${FILESYSTEM_MOD_DIR}/sbin -maxdepth 1 -type f -name upnpd -o -name igdd | xargs) \
 		 $(find ${FILESYSTEM_MOD_DIR}/etc -maxdepth 1 -type d -name 'default.*' | xargs -I{} find {} -name 'any.xml' -o -name 'fbox*.xml') \
 		 $(find ${FILESYSTEM_MOD_DIR}/etc -maxdepth 1 -type d -name 'default.*' | xargs -I{} find {} -name '*igd*')
+	[ "$FREETZ_REMOVE_UPNP_TOUCH_UPNPD" == "y" ] && touch ${FILESYSTEM_MOD_DIR}/sbin/upnpd
 	if [ "$FREETZ_REMOVE_UPNP_LIBS" == "y" ]; then
 		rm_files $(ls -1 ${FILESYSTEM_MOD_DIR}/lib/libavmupnp*)
 	fi
