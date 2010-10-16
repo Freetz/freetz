67f46faf243c49b925d9d889d4baac8411d2779a
Gene Rudoy <gene@freetz.org>
2010-10-16 23:10:31 +0000

    add new library libexif
    git-svn-id: http://svn.freetz.org/trunk@6005 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/libs/libexif.mk
@@ -0,0 +1,48 @@
+$(call PKG_INIT_LIB, 0.6.19)
+$(PKG)_LIB_VERSION:=12.3.1
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SOURCE_MD5:=56144a030a4c875c600b1ccf713f69f7
+$(PKG)_SITE:=@SF/libexif
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/.libs/libexif.so.$($(PKG)_LIB_VERSION)
+$(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libexif.so.$($(PKG)_LIB_VERSION)
+$(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libexif.so.$($(PKG)_LIB_VERSION)
+
+$(PKG)_CONFIGURE_OPTIONS += --enable-shared
+$(PKG)_CONFIGURE_OPTIONS += --enable-static
+$(PKG)_CONFIGURE_OPTIONS += --disable-rpath
+$(PKG)_CONFIGURE_OPTIONS += --without-libiconv-prefix
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(LIBEXIF_DIR)
+
+$($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
+	$(SUBMAKE) \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+		-C $(LIBEXIF_DIR) install
+	$(PKG_FIX_LIBTOOL_LA) \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libexif.la \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libexif.pc
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
+	$(INSTALL_LIBRARY_STRIP)
+
+$(pkg): $($(PKG)_STAGING_BINARY)
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(LIBEXIF_DIR) clean
+	$(RM) -r \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/libexif \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libexif* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libexif.pc
+
+$(pkg)-uninstall:
+	$(RM) $(LIBEXIF_TARGET_DIR)/libexif*.so*
+
+$(PKG_FINISH)
