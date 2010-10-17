d13829255af729c7acf7e3e75531634bcc41be35
Gene Rudoy <gene@freetz.org>
2010-10-17 14:53:13 +0000

    * add new package minidlna, looking for volunteers for writing rc.script & web-if
    git-svn-id: http://svn.freetz.org/trunk@6011 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/minidlna/minidlna.mk
@@ -0,0 +1,48 @@
+$(call PKG_INIT_BIN, 1.0.18)
+$(PKG)_DIR:=$(subst -$($(PKG)_VERSION),,$($(PKG)_DIR))
+$(PKG)_SOURCE:=$(pkg)_$($(PKG)_VERSION)_src.tar.gz
+$(PKG)_SOURCE_MD5:=8681b100242f0a2fef93cab7cdef061e
+$(PKG)_SITE:=@SF/minidlna
+
+$(PKG)_BINARY := $($(PKG)_DIR)/minidlna
+$(PKG)_TARGET_BINARY := $($(PKG)_DEST_DIR)/usr/sbin/minidlna
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_MINIDLNA_STATIC
+
+$(PKG)_DEPENDS_ON += ffmpeg libexif flac libid3tag jpeg libogg libvorbis sqlite
+ifeq ($(strip $(FREETZ_TARGET_UCLIBC_VERSION_0_9_28)),y)
+$(PKG)_DEPENDS_ON += libiconv
+$(PKG)_ICONV_LIB += -liconv
+endif
+
+$(PKG)_CONFIGURE_PRE_CMDS += ln -s genconfig.sh configure;
+$(PKG)_CONFIGURE_DEFOPTS := n
+$(PKG)_CONFIGURE_ENV := CROSS_TOOLCHAIN_SYSROOT="$(TARGET_TOOLCHAIN_STAGING_DIR)"
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(MINIDLNA_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS) -Wall -D_GNU_SOURCE -pthread" \
+		LINK_STATICALLY="$(if $(FREETZ_PACKAGE_MINIDLNA_STATIC),yes,no)" \
+		ICONV_LIB="$(MINIDLNA_ICONV_LIB)" \
+		CROSS_TOOLCHAIN_SYSROOT="$(TARGET_TOOLCHAIN_STAGING_DIR)"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(MINIDLNA_DIR) clean
+	$(RM) $(MINIDLNA_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(MINIDLNA_TARGET_BINARY)
+
+$(PKG_FINISH)
