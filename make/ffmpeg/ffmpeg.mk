4e1abe1010837f224c508771abe99039f1aa199c
Gene Rudoy <gene@freetz.org>
2010-10-19 22:24:18 +0000

    ffmpeg:  * bump version to 0.5.3
    git-svn-id: http://svn.freetz.org/trunk@6026 f5190166-0702-4917-9039-51ec32eddaf5

--- make/ffmpeg/ffmpeg.mk
+++ make/ffmpeg/ffmpeg.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 0.5.2)
+$(call PKG_INIT_BIN, 0.5.3)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=451eb428ca97a72c00555d50944cdb24
+$(PKG)_SOURCE_MD5:=36c1045f013f6e8bac33a6200f1f554f
 $(PKG)_SITE:=http://www.ffmpeg.org/releases
 
 $(PKG)_DEPENDS_ON := zlib
