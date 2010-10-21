7556220a68de5351e423e10a7d7978140ac1ca75
Gene Rudoy <gene@freetz.org>
2010-10-21 20:40:35 +0000

    add new package udpxy (by leibezem)
    git-svn-id: http://svn.freetz.org/trunk@6034 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/udpxy/udpxy.mk
@@ -0,0 +1,31 @@
+$(call PKG_INIT_BIN, 1.0-Chipmunk-16)
+$(PKG)_SOURCE:=$(pkg).$($(PKG)_VERSION).tgz
+$(PKG)_SOURCE_MD5:=0bba6fd3e0b0065424b198abdf0acb7d
+$(PKG)_SITE:=@SF/udpxy
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/udpxy
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/udpxy
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(UDPXY_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(UDPXY_DIR) clean
+
+$(pkg)-uninstall:
+	$(RM) $(UDPXY_TARGET_BINARY)
+
+$(PKG_FINISH)
