95c9ff4233b37f5406f7d26125bf1244c109032e
Oliver Metz <oliver@freetz.org>
2010-10-24 18:13:42 +0000

    * libcapi: Update to newest source from AVM (7320 04.86)
    git-svn-id: http://svn.freetz.org/trunk@6060 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/libcapi.mk
+++ make/libs/libcapi.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_LIB, 2.2)
+$(call PKG_INIT_LIB, 2.3)
 $(PKG)_LIB_VERSION:=3.0.4
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=715bd031409960178e19de1fcf2e35e9
+$(PKG)_SOURCE_MD5:=87d3cd1f17061d4ddee01246a1eea926
 $(PKG)_SITE:=http://freetz.magenbrot.net
 $(PKG)_BINARY:=$($(PKG)_DIR)/libcapi20.so.$($(PKG)_LIB_VERSION)
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/libcapi20.so.$($(PKG)_LIB_VERSION)
