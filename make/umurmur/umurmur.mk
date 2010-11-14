09e7fca3d7d27e2c8cad8189da112326584f31bb
Gene Rudoy <gene@freetz.org>
2010-11-14 21:28:27 +0000

    umurmur:  * bump version to 0.2.5
    git-svn-id: http://svn.freetz.org/trunk@6176 f5190166-0702-4917-9039-51ec32eddaf5

--- make/umurmur/umurmur.mk
+++ make/umurmur/umurmur.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 0.2.4)
+$(call PKG_INIT_BIN, 0.2.5)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=ab5cc15b4b1d827443f7950cd935bc6a
+$(PKG)_SOURCE_MD5:=c31fd72c0ac563cf75961ae4e49909d7
 $(PKG)_SITE:=http://$(pkg).googlecode.com/files
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/$(pkg)d
