9a8c37611fe38369632d470b1151890e46296a6c
Oliver Metz <oliver@freetz.org>
2010-11-14 11:01:17 +0000

    * Patches: Comment out remove getcons patch until we have a final patches hook (applied after copying base files)  refs #1026
    git-svn-id: http://svn.freetz.org/trunk@6164 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/100-getcons.sh
+++ patches/100-getcons.sh
@@ -1,9 +1,11 @@
 # Setconsole does the same. So delete getcons.
 echo1 "removing /bin/getcons"
 rm_files "${FILESYSTEM_MOD_DIR}/bin/getcons"
-modsed 's/getcons/setconsole/g' ${FILESYSTEM_MOD_DIR}/etc/profile
 
 if [ "$FREETZ_PATCH_GETCONS" == "y" ]; then
-	modsed '/setconsole/d' ${FILESYSTEM_MOD_DIR}/etc/profile
-	modsed 's/auf dieses Terminal/nicht/' ${FILESYSTEM_MOD_DIR}/etc/profile
+cat >> ${FILESYSTEM_MOD_DIR}/etc/profile << EOF
+
+setconsole
+echo "Console Ausgaben auf dieses Terminal umgelenkt."
+EOF
 fi
