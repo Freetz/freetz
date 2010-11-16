2aa3a345dde7cdebde6f3692a87f8ac0df038017
cuma <cuma@freetz.org>
2010-11-16 21:39:04 +0000

    cifsmount: fixed path of binary
    git-svn-id: http://svn.freetz.org/trunk@6190 f5190166-0702-4917-9039-51ec32eddaf5

--- make/cifsmount/cifsmount.mk
+++ make/cifsmount/cifsmount.mk
@@ -2,7 +2,7 @@ $(call PKG_INIT_BIN, 1.10)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_SITE:=http://freetz.magenbrot.net
 $(PKG)_BINARY:=$($(PKG)_DIR)/mount.cifs
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/mount.cifs
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/sbin/mount.cifs
 $(PKG)_STARTLEVEL=50
 $(PKG)_SOURCE_MD5:=0a05fc528aae1c52046c846d990d26ff
 
