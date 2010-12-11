e6de803a2e0a42f72d66abba84f7bf9bb73be646
Gene Rudoy <gene@freetz.org>
2010-12-11 15:56:54 +0000

    jamvm:  * reenable it for big-endian boxes (it builds just fine with 010-allow_mips.patch, I however didn't test whether it runs as I don't have a big-endian box)  * bump version to 1.5.4  * remove classes.zip from freetz repository and use that from jamvm-tarball instead (they are absolutely identical)
    git-svn-id: http://svn.freetz.org/trunk@6316 f5190166-0702-4917-9039-51ec32eddaf5

--- make/jamvm/jamvm.mk
+++ make/jamvm/jamvm.mk
@@ -1,13 +1,16 @@
-$(call PKG_INIT_BIN, 1.5.1)
-$(PKG)_UGLY_VERSION:=0.0.0
+$(call PKG_INIT_BIN, 1.5.4)
+$(PKG)_LIB_VERSION:=0.0.0
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=5a82751b50391eb092c906ce64f3b6bf
+$(PKG)_SOURCE_MD5:=7654e9657691f5f09c4f481ed4686176
 $(PKG)_SITE:=@SF/$(pkg)
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/jamvm
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/jamvm
-$(PKG)_LIB_BINARY:=$($(PKG)_DIR)/src/.libs/libjvm.so.$($(PKG)_UGLY_VERSION)
-$(PKG)_TARGET_LIB_BINARY:=$($(PKG)_DEST_LIBDIR)/libjvm.so.$($(PKG)_UGLY_VERSION)
+$(PKG)_LIB_BINARY:=$($(PKG)_DIR)/src/.libs/libjvm.so.$($(PKG)_LIB_VERSION)
+$(PKG)_TARGET_LIB_BINARY:=$($(PKG)_DEST_LIBDIR)/libjvm.so.$($(PKG)_LIB_VERSION)
+
+$(PKG)_CLASSES_ZIP:=$($(PKG)_DIR)/lib/classes.zip
+$(PKG)_TARGET_CLASSES_ZIP:=$($(PKG)_DEST_DIR)/usr/share/jamvm/classes.zip
 
 $(PKG)_DEPENDS_ON := zlib libffi-sable classpath
 
@@ -30,15 +33,17 @@ $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 $($(PKG)_TARGET_LIB_BINARY): $($(PKG)_LIB_BINARY)
 	$(INSTALL_LIBRARY_STRIP)
 
+$($(PKG)_TARGET_CLASSES_ZIP): $($(PKG)_CLASSES_ZIP)
+	$(INSTALL_FILE)
+
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_TARGET_LIB_BINARY)
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_TARGET_LIB_BINARY) $($(PKG)_TARGET_CLASSES_ZIP)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(JAMVM_DIR) clean
 
 $(pkg)-uninstall:
-	$(RM) $(JAMVM_TARGET_BINARY)
-	$(RM) $(JAMVM_DEST_LIBDIR)/libjvm*.so*
+	$(RM) $(JAMVM_TARGET_BINARY) $(JAMVM_DEST_LIBDIR)/libjvm*.so* $(JAMVM_TARGET_CLASSES_ZIP)
 
 $(PKG_FINISH)
