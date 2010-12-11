b0af9ea461797c2a86fc3235773aaf6a283e7ba8
Gene Rudoy <gene@freetz.org>
2010-12-11 12:53:02 +0000

    tcp_wrappers:  * fix/cleanup build options, in particular set AUX_OBJ to weak_symbols.o and not to setenv.o as our uClibc do provide setenv  * fixes ngircd static build problems
    git-svn-id: http://svn.freetz.org/trunk@6313 f5190166-0702-4917-9039-51ec32eddaf5

--- make/tcp_wrappers/tcp_wrappers.mk
+++ make/tcp_wrappers/tcp_wrappers.mk
@@ -1,37 +1,42 @@
 $(call PKG_INIT_BIN,7.6)
 $(PKG)_LIB_VERSION:=0.7.6
 $(PKG)_SOURCE:=$(pkg)_$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=e6fa25f71226d090f34de3f6b122fb5a
 $(PKG)_SITE:=ftp://ftp.porcupine.org/pub/security
+
 $(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/$(pkg)_$($(PKG)_VERSION)
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/tcpd
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/sbin/tcpd
 $(PKG)_LIB_BINARY:=$($(PKG)_DIR)/shared/libwrap.so.$($(PKG)_LIB_VERSION)
 $(PKG)_LIB_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libwrap.so.$($(PKG)_LIB_VERSION)
 $(PKG)_LIB_TARGET_BINARY:=$($(PKG)_DEST_LIBDIR)/libwrap.so.$($(PKG)_LIB_VERSION)
-$(PKG)_SOURCE_MD5:=e6fa25f71226d090f34de3f6b122fb5a
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_NOP)
 
 $($(PKG)_BINARY) $($(PKG)_LIB_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(TCP_WRAPPERS_DIR) \
-		config-check
+	for target in config-check all; do \
 	$(SUBMAKE) -C $(TCP_WRAPPERS_DIR) \
 		CC="$(TARGET_CC)" \
 		COPTS="$(TARGET_CFLAGS)" \
+		EXTRA_CFLAGS="-DSYS_ERRLIST_DEFINED -DHAVE_STRERROR -DHAVE_WEAKSYMS -D_REENTRANT -DINET6=$(if $(FREETZ_TARGET_IPV6_SUPPORT),1,0) -Dss_family=__ss_family -Dss_len=__ss_len" \
+		RANLIB="$(TARGET_RANLIB)" \
+		AR="$(TARGET_AR)" \
+		ARFLAGS=rv \
+		AUX_OBJ=weak_symbols.o \
 		LIBS=-lnsl \
 		NETGROUP= \
+		TLI= \
 		VSYSLOG= \
 		BUGS= \
-		EXTRA_CFLAGS="-DSYS_ERRLIST_DEFINED -DHAVE_STRERROR -DHAVE_WEAKSYMS -D_REENTRANT \
-			$(if $(FREETZ_TARGET_IPV6_SUPPORT),-DINET6=1,-DINET6=0) \
-			-Dss_family=__ss_family -Dss_len=__ss_len" \
 		FACILITY=LOG_DAEMON \
 		SEVERITY=LOG_INFO \
 		REAL_DAEMON_DIR=/usr/sbin \
 		STYLE="-DPROCESS_OPTIONS" \
-		all
+		$$target; \
+	done
 
 $($(PKG)_LIB_STAGING_BINARY): $($(PKG)_LIB_BINARY)
 	chmod 644 $(TCP_WRAPPERS_DIR)/tcpd.h
