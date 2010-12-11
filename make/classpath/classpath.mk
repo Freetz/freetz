fb4b1f8b7b1c21f82ac8cf123c05b6d54f75e859
Gene Rudoy <gene@freetz.org>
2010-12-11 23:24:29 +0000

    classpath:  * fix build problem introduced in r6321
    git-svn-id: http://svn.freetz.org/trunk@6329 f5190166-0702-4917-9039-51ec32eddaf5

--- make/classpath/classpath.mk
+++ make/classpath/classpath.mk
@@ -35,6 +35,7 @@ $(PKG)_CONFIGURE_OPTIONS += --disable-plugin
 $(PKG)_CONFIGURE_OPTIONS += --disable-qt-peer
 $(PKG)_CONFIGURE_OPTIONS += --disable-rpath
 $(PKG)_CONFIGURE_OPTIONS += --without-libiconv-prefix
+$(PKG)_CONFIGURE_OPTIONS += --disable-Werror
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
