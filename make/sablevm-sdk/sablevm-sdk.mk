adf9ee914e3a3d99a9e2109954a0e50600ca6031
Gene Rudoy <gene@freetz.org>
2010-12-11 10:53:16 +0000

    sablevm-sdk:  * drop build dependencies on fastjar & jikes (the latter is not available in recent distributions), use bundled versions instead  * prevent RPATH hardcoding, install library into /usr/lib/freetz (dlopened libraries are still installed into /usr/lib/sablevm-classpath)  * link sablevm binary (depends only indirectly on pthread) explicitly against pthread to workaround uClibc' pthread issue (each binary depending on pthread should explicitly link it in to ensure right load order/symbol resolution)  * various cleanups  * fixes build problems reported here http://www.ip-phone-forum.de/showpost.php?p=1646740&postcount=10
    git-svn-id: http://svn.freetz.org/trunk@6311 f5190166-0702-4917-9039-51ec32eddaf5

--- make/sablevm-sdk/sablevm-sdk.mk
+++ make/sablevm-sdk/sablevm-sdk.mk
@@ -4,84 +4,90 @@ $(PKG)_SOURCE_MD5:=18a7c1a92b0748a206f9767a76a6b3bb
 $(PKG)_SITE:=@SF/sablevm
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/sablevm/src/sablevm/.libs/sablevm
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/sablevm
+
 $(PKG)_LIB_CLASSPATH_MINI:=$($(PKG)_DIR)/sablevm-classpath/lib/mini.jar
-$(PKG)_LIB_CLASSPATH_ORIG:=$($(PKG)_DIR)/sablevm-classpath/lib/mini.jar
+$(PKG)_LIB_CLASSPATH_ORIG:=$($(PKG)_DIR)/sablevm-classpath/lib/libclasspath.jar
+$(PKG)_LIB_TARGET_CLASSPATH:=$($(PKG)_DEST_DIR)/usr/share/sablevm-classpath/libclasspath.jar
+
 $(PKG)_LIB_BINARY:=$($(PKG)_DIR)/sablevm/src/libsablevm/.libs/libsablevm-$($(PKG)_VERSION).so
+$(PKG)_LIB_TARGET_BINARY:=$($(PKG)_DEST_LIBDIR)/libsablevm-$($(PKG)_VERSION).so
+
 $(PKG)_LIB_CLASSPATH_BINARY:=$($(PKG)_DIR)/sablevm-classpath/native/jni/java-io/.libs/libjavaio-$($(PKG)_VERSION).so
 $(PKG)_LIB_STAGING_CLASSPATH_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/sablevm-classpath/libjavaio-$($(PKG)_VERSION).so
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/sablevm
-$(PKG)_LIB_TARGET_CLASSPATH:=$($(PKG)_DEST_DIR)/usr/share/sablevm-classpath/libclasspath.jar
-$(PKG)_LIB_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/lib/libsablevm-$($(PKG)_VERSION).so
 $(PKG)_LIB_TARGET_CLASSPATH_BINARY:=$($(PKG)_DEST_DIR)/usr/lib/sablevm-classpath/libjavaio-$($(PKG)_VERSION).so
-
-$(PKG)_BUILD_PREREQ += fastjar jikes
 
 $(PKG)_DEPENDS_ON := libtool popt zlib
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_SABLEVM_SDK_MINI
 
-$(PKG)_CONFIGURE_OPTIONS += --enable-static
-$(PKG)_CONFIGURE_OPTIONS += --enable-shared
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,configure sablevm/configure sablevm/src/libffi/configure sablevm-classpath/configure)
+
+$(PKG)_COMMON_CONFIGURE_OPTIONS += --enable-static
+$(PKG)_COMMON_CONFIGURE_OPTIONS += --enable-shared
+$(PKG)_COMMON_CONFIGURE_OPTIONS += --disable-rpath
+
+$(PKG)_CONFIGURE_OPTIONS += $($(PKG)_COMMON_CONFIGURE_OPTIONS)
 $(PKG)_CONFIGURE_OPTIONS += --disable-cp-tools
 $(PKG)_CONFIGURE_OPTIONS += --disable-gjdoc
 
+$(PKG)_CONFIGURE_OPTIONS += --with-jikes=internal
+$(PKG)_CONFIGURE_OPTIONS += --with-fastjar=internal
+$(PKG)_INTERNAL_JIKES    := "$(FREETZ_BASE_DIR)/$($(PKG)_DIR)/bin/jikes"
+$(PKG)_INTERNAL_FASTJAR  := "$(FREETZ_BASE_DIR)/$($(PKG)_DIR)/bin/fastjar"
+
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
+
+define SABLEVM_SDK_HOST_DEPENDENCY
+$($(PKG)_DIR)/$(1)/.configured: $($(PKG)_DIR)/.unpacked
+	(cd $(SABLEVM_SDK_DIR)/$(1); rm -rf config.cache; ./configure --prefix="$(FREETZ_BASE_DIR)/$(SABLEVM_SDK_DIR)/_dummyprefix");
+	touch $$@
+
+$($(PKG)_DIR)/$(1)/.compiled: $($(PKG)_DIR)/$(1)/.configured
+	$(MAKE) -C $(SABLEVM_SDK_DIR)/$(1)
+	touch $$@
+endef
+
+$(eval $(call SABLEVM_SDK_HOST_DEPENDENCY,jikes))
+$(eval $(call SABLEVM_SDK_HOST_DEPENDENCY,fastjar))
+
 $(PKG_CONFIGURED_CONFIGURE)
+$($(PKG)_DIR)/.configured $($(PKG)_DIR)/sablevm/.configured $($(PKG)_DIR)/sablevm-classpath/.configured : $($(PKG)_DIR)/jikes/.compiled $($(PKG)_DIR)/fastjar/.compiled
 
 $($(PKG)_DIR)/sablevm/.configured: $($(PKG)_DIR)/.unpacked
-	(cd $(SABLEVM_SDK_DIR)/sablevm; rm -rf config.cache; \
+	(cd $(SABLEVM_SDK_DIR)/sablevm; $(RM) -f config.cache; \
 		$(TARGET_CONFIGURE_ENV) \
 		./configure \
-		--cache-file=$(FREETZ_BASE_DIR)/$(MAKE_DIR)/config.cache \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--prefix=/usr \
-		$(DISABLE_NLS) \
-		$(DISABLE_LARGEFILE) \
-		--enable-static \
-		--enable-shared \
-		--srcdir="./../sablevm" \
+		$(TARGET_CONFIGURE_OPTIONS) \
+		$(SABLEVM_SDK_COMMON_CONFIGURE_OPTIONS) \
+		--with-jikes="$(SABLEVM_SDK_INTERNAL_JIKES)" \
+		--with-fastjar="$(SABLEVM_SDK_INTERNAL_FASTJAR)" \
 		--with-internal-libffi=yes \
 		--with-internal-libpopt=no \
-		--with-jikes="$$(which jikes)" \
-		--with-fastjar="$$(which fastjar)" \
-		
 	);
 	touch $@
 
 $($(PKG)_DIR)/sablevm-classpath/.configured: $($(PKG)_DIR)/.unpacked
-	(cd $(SABLEVM_SDK_DIR)/sablevm-classpath; rm -rf config.cache; \
+	(cd $(SABLEVM_SDK_DIR)/sablevm-classpath; $(RM) -f config.cache; \
 		$(TARGET_CONFIGURE_ENV) \
 		./configure \
-		--cache-file=$(FREETZ_BASE_DIR)/$(MAKE_DIR)/config.cache \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--prefix=/usr \
-		$(DISABLE_NLS) \
-		$(DISABLE_LARGEFILE) \
-		--enable-static \
-		--enable-shared \
-		--srcdir="./../sablevm-classpath" \
+		$(TARGET_CONFIGURE_OPTIONS) \
+		$(SABLEVM_SDK_COMMON_CONFIGURE_OPTIONS) \
+		--with-jikes="$(SABLEVM_SDK_INTERNAL_JIKES)" \
+		--with-fastjar="$(SABLEVM_SDK_INTERNAL_FASTJAR)" \
 		--disable-gtk-peer \
 		--disable-gtk-cairo \
 		--without-x \
-		--with-jikes="$$(which jikes)" \
-		--with-fastjar="$$(which fastjar)" \
 	);
 	touch $@
 
-$($(PKG)_BINARY) $($(PKG)_LIB_BINARY): $($(PKG)_DIR)/.configured \
-	$($(PKG)_DIR)/sablevm/.configured $($(PKG)_DIR)/sablevm-classpath/.configured
+$($(PKG)_BINARY) $($(PKG)_LIB_BINARY): $($(PKG)_DIR)/.configured $($(PKG)_DIR)/sablevm/.configured $($(PKG)_DIR)/sablevm-classpath/.configured
 	$(SUBMAKE) -C $(SABLEVM_SDK_DIR) all \
 		EARLY_CONFIGURE= \
 		EXTRA_CONFIGURE=
 	cp $(SABLEVM_SDK_MAKE_DIR)/mini.classlist $(SABLEVM_SDK_DIR)/sablevm-classpath/lib/
-	( cd $(SABLEVM_SDK_DIR)/sablevm-classpath/lib;\
-		fastjar -Mcf mini.jar -@ < mini.classlist ; \
-	)
+	(cd $(SABLEVM_SDK_DIR)/sablevm-classpath/lib; "$(SABLEVM_SDK_INTERNAL_FASTJAR)" -Mcf mini.jar -@ < mini.classlist;)
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
@@ -98,8 +104,7 @@ $($(PKG)_LIB_STAGING_CLASSPATH_BINARY): $($(PKG)_LIB_BINARY)
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/sablevm-classpath/libjavautil.la
 
 $($(PKG)_LIB_TARGET_BINARY): $($(PKG)_LIB_BINARY)
-	cp -a $(SABLEVM_SDK_DIR)/sablevm/src/libsablevm/.libs/libsablevm*.so $(SABLEVM_SDK_DEST_DIR)/usr/lib
-	$(TARGET_STRIP) $@
+	$(INSTALL_LIBRARY_STRIP_WILDCARD_BEFORE_SO)
 
 $($(PKG)_LIB_TARGET_CLASSPATH_BINARY): $($(PKG)_LIB_STAGING_CLASSPATH_BINARY)
 	cp -a $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/sablevm-classpath/libjava*.so* $(SABLEVM_SDK_DEST_DIR)/usr/lib/sablevm-classpath/
@@ -121,7 +126,7 @@ $(pkg)-clean:
 
 $(pkg)-uninstall:
 	$(RM) $(SABLEVM_SDK_TARGET_BINARY)
-	$(RM) $(SABLEVM_SDK_DEST_DIR)/usr/lib/libsablevm*.so*
+	$(RM) $(SABLEVM_SDK_DEST_LIBDIR)/libsablevm*.so*
 	$(RM) $(SABLEVM_SDK_DEST_DIR)/usr/lib/sablevm-classpath/libjava*.so*
 	$(RM) $(SABLEVM_SDK_DEST_DIR)/usr/share/sablevm-classpath/*
 
