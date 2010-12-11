00bf42702b4660b758d09e27bab8ef5b7e3be310
Gene Rudoy <gene@freetz.org>
2010-12-11 13:52:31 +0000

    inotify-tools:  * workaround some packaging errors caused auto*-tools to be unnecessarily called  * should fix build problem reported here http://www.ip-phone-forum.de/showpost.php?p=1646740&postcount=10
    git-svn-id: http://svn.freetz.org/trunk@6314 f5190166-0702-4917-9039-51ec32eddaf5

--- make/inotify-tools/inotify-tools.mk
+++ make/inotify-tools/inotify-tools.mk
@@ -3,13 +3,20 @@ $(PKG)_LIB_VERSION:=0.4.1
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5:=b43d95a0fa8c45f8bab3aec9672cf30c
 $(PKG)_SITE:=http://cloud.github.com/downloads/rvoicilas/inotify-tools
+
 $(PKG)_STARTLEVEL=10
+
 $(PKG)_INWAIT_BINARY:=$($(PKG)_DIR)/src/.libs/inotifywait
-$(PKG)_INWATCH_BINARY:=$($(PKG)_DIR)/src/.libs/inotifywatch
-$(PKG)_LIB_BINARY:=$($(PKG)_DIR)/libinotifytools/src/.libs/libinotifytools.so.$($(PKG)_LIB_VERSION)
 $(PKG)_INWAIT_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/inotifywait
+
+$(PKG)_INWATCH_BINARY:=$($(PKG)_DIR)/src/.libs/inotifywatch
 $(PKG)_INWATCH_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/inotifywatch
+
+$(PKG)_LIB_BINARY:=$($(PKG)_DIR)/libinotifytools/src/.libs/libinotifytools.so.$($(PKG)_LIB_VERSION)
 $(PKG)_LIB_TARGET_BINARY:=$($(PKG)_DEST_LIBDIR)/libinotifytools.so.$($(PKG)_LIB_VERSION)
+
+# fix inotify-tools packaging errors (configure.ac & aclocal.m4 have a later date than libinotifytools/src/inotifytools/inotify.h.in)
+$(PKG)_CONFIGURE_PRE_CMDS += touch -t 200001010000.00 configure.ac aclocal.m4;
 
 $(PKG)_CONFIGURE_ENV += LD="$(TARGET_LD)"
 
@@ -32,13 +39,11 @@ $($(PKG)_LIB_TARGET_BINARY): $($(PKG)_LIB_BINARY)
 
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_INWAIT_TARGET_BINARY) \
-		$($(PKG)_INWATCH_TARGET_BINARY) \
-		$($(PKG)_LIB_TARGET_BINARY)
+$(pkg)-precompiled: $($(PKG)_INWAIT_TARGET_BINARY) $($(PKG)_INWATCH_TARGET_BINARY) $($(PKG)_LIB_TARGET_BINARY)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(INOTIFY_TOOLS_DIR) clean
-	$(RM) $(INOTIFY_TOOLS_DIR)/.compiled
+	$(RM) $(INOTIFY_TOOLS_DIR)/.configured
 
 $(pkg)-uninstall:
 	$(RM) $(INOTIFY_TOOLS_INWAIT_TARGET_BINARY)
