2d4851acae45eac087100aea21f0a29f58d7d625
Oliver Metz <oliver@freetz.org>
2010-12-20 08:47:04 +0000

    * Update patches from last commit (7390 intenational)  * refs #1061
    git-svn-id: http://svn.freetz.org/trunk@6354 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/195-webmenu_signed.sh
+++ patches/195-webmenu_signed.sh
@@ -14,7 +14,7 @@ elif isFreetzType LANG_A_CH; then
 		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/all/html/de/home/home.js
 	fi
 elif isFreetzType LANG_EN; then
-	if isFreetzType 7340 && isFreetzType PREVIEW; then
+	if isFreetzType 7390; then
 		modsed "s/^g_coninf_data.FirmwareSigned = .*/g_coninf_data.FirmwareSigned = \"1\"/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/home/home.lua
 	elif isFreetzType 5124 7140 7170 7270 7340 7570; then
 		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/html/de/home/home.js
