aaab64a9f55bfbdcc69ab21dc7d1ea54a768e949
Oliver Metz <oliver@freetz.org>
2010-12-24 08:34:41 +0000

    * Add new host tool sstrip (by Ralf and me)   * Idea taken from OpenWRT   * refs #1089
    git-svn-id: http://svn.freetz.org/trunk@6373 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ tools/make/sstrip.mk
@@ -0,0 +1,29 @@
+SSTRIP_VERSION:=1.0
+SSTRIP_SOURCE:=sstrip-$(SSTRIP_VERSION).tar.bz2
+SSTRIP_DIR:=$(TOOLS_SOURCE_DIR)/sstrip-$(SSTRIP_VERSION)
+
+$(SSTRIP_DIR)/.unpacked: $(TOOLS_DIR)/source/$(SSTRIP_SOURCE) | $(TOOLS_SOURCE_DIR)
+	tar -C $(TOOLS_SOURCE_DIR) $(VERBOSE) -xjf $(TOOLS_DIR)/source/$(SSTRIP_SOURCE)
+	touch $@
+
+$(SSTRIP_DIR)/sstrip: $(SSTRIP_DIR)/.unpacked
+	$(MAKE) -C $(SSTRIP_DIR) \
+		CC="$(TOOLS_CC)" \
+		CFLAGS="-Wall -O2" \
+		all
+
+$(TOOLS_DIR)/sstrip: $(SSTRIP_DIR)/sstrip
+	cp $(SSTRIP_DIR)/sstrip $(TOOLS_DIR)/sstrip
+
+host-sstrip: $(TOOLS_DIR)/sstrip
+
+host-sstrip-source: $(SSTRIP_DIR)/.unpacked
+
+host-sstrip-clean:
+	-$(MAKE) -C $(SSTRIP_DIR) clean
+
+host-sstrip-dirclean:
+	$(RM) -r $(SSTRIP_DIR)
+
+host-sstrip-distclean: host-sstrip-dirclean
+	$(RM) $(TOOLS_DIR)/sstrip
