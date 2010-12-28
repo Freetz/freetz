0bf15f1e2fc57c534d3decb11bb7ae6c6951d494
Oliver Metz <oliver@freetz.org>
2010-12-28 18:55:09 +0000

    * Copy some more kernel headers for 2.6.13.1 (refs #842)
    git-svn-id: http://svn.freetz.org/trunk@6388 f5190166-0702-4917-9039-51ec32eddaf5

--- make/linux/kernel.mk
+++ make/linux/kernel.mk
@@ -143,7 +143,7 @@ $(KERNEL_DIR)/.prepared: $(KERNEL_DIR)/.configured
 
 $(KERNEL_HEADERS_DEVEL_DIR)/include/linux/version.h: $(KERNEL_DIR)/.prepared
 ifeq ($(strip $(FREETZ_KERNEL_VERSION_2_6_13_1)),y)
-	$(call COPY_KERNEL_HEADERS,$(KERNEL_BUILD_ROOT_DIR),$(KERNEL_HEADERS_DEVEL_DIR),{asm$(comma)asm-generic$(comma)linux})
+	$(call COPY_KERNEL_HEADERS,$(KERNEL_BUILD_ROOT_DIR),$(KERNEL_HEADERS_DEVEL_DIR),{asm$(comma)asm-generic$(comma)linux$(comma)mtd$(comma)scsi$(comma)video}
 else
 	$(SUBMAKE) -C $(KERNEL_BUILD_ROOT_DIR) \
 		CROSS_COMPILE="$(KERNEL_CROSS)" \
