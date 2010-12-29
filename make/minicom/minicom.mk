8c5c5124e20268fc1f2ce199a2351972af8df609
Gene Rudoy <gene@freetz.org>
2010-12-29 15:03:05 +0000

    minicom:  * fix it doesn't compiled with uClibc-0.9.28  * add missing ncurses dependency
    git-svn-id: http://svn.freetz.org/trunk@6398 f5190166-0702-4917-9039-51ec32eddaf5

--- make/minicom/minicom.mk
+++ make/minicom/minicom.mk
@@ -1,9 +1,15 @@
 $(call PKG_INIT_BIN,2.4)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=http://alioth.debian.org/frs/download.php/3195
+$(PKG)_SOURCE_MD5:=700976a3c2dcc8bbd50ab9bb1c08837b
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/minicom
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/minicom
-$(PKG)_SOURCE_MD5:=700976a3c2dcc8bbd50ab9bb1c08837b
+
+$(PKG)_DEPENDS_ON += ncurses
+ifeq ($(strip $(FREETZ_TARGET_UCLIBC_VERSION_0_9_28)),y)
+$(PKG)_DEPENDS_ON += libiconv
+endif
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
