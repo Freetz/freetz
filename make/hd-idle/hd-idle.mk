90ba7e02c72c98594ee8e9e0e44922dedc9f980c
Gene Rudoy <gene@freetz.org>
2010-12-30 13:56:01 +0000

    hd-idle:  * remove unnecessary include of scsi.h  * minor makefile cleanups
    git-svn-id: http://svn.freetz.org/trunk@6405 f5190166-0702-4917-9039-51ec32eddaf5

--- make/hd-idle/hd-idle.mk
+++ make/hd-idle/hd-idle.mk
@@ -1,13 +1,14 @@
 $(call PKG_INIT_BIN, 0.99)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SOURCE_MD5:=7087d9b6b12836e0117bde6a0cc10824
 $(PKG)_SITE:=http://freetz.magenbrot.net
-$(PKG)_SOURCE_FILE:=$($(PKG)_DIR)/hd-idle.c
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/hd-idle
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/hd-idle
+
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_NOP)
-$(PKG)_SOURCE_MD5:=7087d9b6b12836e0117bde6a0cc10824
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(HD_IDLE_DIR) \
