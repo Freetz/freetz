b0dc7ed4df10f9d68ad65afa48839dc6e95ce41a
Gene Rudoy <gene@freetz.org>
2010-12-31 12:23:58 +0000

    sg3_utils:  * remove unnecessary include of scsi.h, fixes #1156  * minor makefile cleanups
    git-svn-id: http://svn.freetz.org/trunk@6408 f5190166-0702-4917-9039-51ec32eddaf5

--- make/sg3_utils/sg3_utils.mk
+++ make/sg3_utils/sg3_utils.mk
@@ -1,9 +1,10 @@
 $(call PKG_INIT_BIN, 1.26, sg3_utils, SG3UTILS)
 $(PKG)_SOURCE:=sg3_utils-$($(PKG)_VERSION).tgz
+$(PKG)_SOURCE_MD5:=9a7aa8d954d75dc6c91e066e215867f2
 $(PKG)_SITE:=http://sg.danny.cz/sg/p
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/sg_start
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/sg_start
-$(PKG)_SOURCE_MD5:=9a7aa8d954d75dc6c91e066e215867f2
 
 $(PKG)_CONFIGURE_OPTIONS += --disable-shared
 
@@ -14,7 +15,6 @@ $(PKG_CONFIGURED_CONFIGURE)
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(SG3UTILS_DIR)/lib
 	$(SUBMAKE) sg_start -C $(SG3UTILS_DIR)/src
-	touch $@
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
@@ -25,12 +25,9 @@ $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(SG3UTILS_DIR) clean
-	$(RM) $(SG3UTILS_DIR)/.installed
-	$(RM) $(SG3UTILS_DIR)/.built
 	$(RM) $(SG3UTILS_DIR)/.configured
 
 $(pkg)-uninstall:
 	$(RM) $(SG3UTILS_TARGET_BINARY)
 
 $(PKG_FINISH)
-
