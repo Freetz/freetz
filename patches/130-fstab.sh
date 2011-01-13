321a2d450f29b0cf9490b743134c8e1fb079520c
Oliver Metz <oliver@freetz.org>
2011-01-13 14:19:52 +0000

    * Use devpts instead of static nodes in /dev/pts (fixes #1159)
    git-svn-id: http://svn.freetz.org/trunk@6428 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/130-fstab.sh
+++ patches/130-fstab.sh
@@ -3,6 +3,7 @@ cat << 'EOF' > "${FILESYSTEM_MOD_DIR}/etc/fstab"
 # /etc/fstab: static file system information.
 #
 # <file system> <mount point>   <type>  <options>               <dump>  <pass>
+devpts          /dev/pts        devpts  mode=0620,gid=5         0       0
 proc            /proc           proc    nosuid,nodev,noexec     0       0
 tmpfs           /var            tmpfs   defaults                0       0
 sysfs           /sys            sysfs   nosuid,nodev,noexec     0       0
