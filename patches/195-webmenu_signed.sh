9098a95f9f089219a040588cfaa5ab5e8fbe91a9
Oliver Metz <oliver@freetz.org>
2011-01-13 15:11:08 +0000

    * Bump 7340 firmware version to 04.86 (refs #1061)   * Remove labor preview
    git-svn-id: http://svn.freetz.org/trunk@6429 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/195-webmenu_signed.sh
+++ patches/195-webmenu_signed.sh
@@ -14,9 +14,9 @@ elif isFreetzType LANG_A_CH; then
 		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/all/html/de/home/home.js
 	fi
 elif isFreetzType LANG_EN; then
-	if isFreetzType 7390; then
+	if isFreetzType 7340 7390; then
 		modsed "s/^g_coninf_data.FirmwareSigned = .*/g_coninf_data.FirmwareSigned = \"1\"/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/home/home.lua
-	elif isFreetzType 5124 7140 7170 7270 7340 7570; then
+	elif isFreetzType 5124 7140 7170 7270 7570; then
 		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/html/de/home/home.js
 	else
 		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/en/html/en/home/home.js
