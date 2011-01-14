1876b9c27da9d4a63993716ad776d6df77d0e98c
Oliver Metz <oliver@freetz.org>
2011-01-14 23:19:28 +0000

    * Add 7113 Multi-Language, Annex A: 90.04.82
    git-svn-id: http://svn.freetz.org/trunk@6436 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/195-webmenu_signed.sh
+++ patches/195-webmenu_signed.sh
@@ -16,7 +16,7 @@ elif isFreetzType LANG_A_CH; then
 elif isFreetzType LANG_EN; then
 	if isFreetzType 7340 7390; then
 		modsed "s/^g_coninf_data.FirmwareSigned = .*/g_coninf_data.FirmwareSigned = \"1\"/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/home/home.lua
-	elif isFreetzType 5124 7140 7170 7270 7570; then
+	elif isFreetzType 5124 7113 7140 7170 7270 7570; then
 		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/html/de/home/home.js
 	else
 		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/en/html/en/home/home.js
