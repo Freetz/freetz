4b094a8f2ab33a32a2422dbbe0f649f1f1d73b55
Whoopie <whoopie79@gmx.net>
2011-01-16 19:48:18 +0000

    busybox: bump version to 1.18.2
    git-svn-id: http://svn.freetz.org/trunk@6441 f5190166-0702-4917-9039-51ec32eddaf5

--- make/busybox/busybox.mk
+++ make/busybox/busybox.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 1.18.1)
+$(call PKG_INIT_BIN, 1.18.2)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=f15fe752d8b7012aa5e59f83b88ccb1c
+$(PKG)_SOURCE_MD5:=69a82091e5710b72db5ce0e14e7c0cd7
 $(PKG)_SITE:=http://www.busybox.net/downloads
 
 $(PKG)_SOURCE_DIR:=$($(PKG)_SOURCE_DIR)/ref-$($(PKG)_REF)
