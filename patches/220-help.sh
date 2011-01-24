3cd9fc55c996632b9abdefad62b05f9beea86246
Oliver Metz <oliver@freetz.org>
2011-01-24 15:08:11 +0000

    * remove help patch: Reenable for 7240, 7270, 7320, 7390   * removes only offline help (no buttons are removed)   * refs #1096
    git-svn-id: http://svn.freetz.org/trunk@6463 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/220-help.sh
+++ patches/220-help.sh
@@ -1,25 +1,34 @@
 [ "$FREETZ_REMOVE_HELP" == "y" ] || return 0
 # from m*.* mod
 echo1 "removing help"
-if [ -e "${HTML_LANG_MOD_DIR}/html/de" ];then
-	HTML_DIR="${HTML_LANG_MOD_DIR}/html/de"
+
+HELP_DIR="${HTML_LANG_MOD_DIR}/help"
+
+if [ -e "${HELP_DIR}" ]; then
+	# remove new lua help
+	rm_files "${HELP_DIR}/hilfe*" "${HELP_DIR}/recht*"
+	modsed 's/local g_txt/local g_txt_removed/' "${HELP_DIR}/home.html"
 else
-	HTML_DIR="${HTML_LANG_MOD_DIR}/html/en"
-fi
-echo1 "${HTML_DIR}"
-rm_files "${HTML_DIR}/help"
-find "${HTML_DIR}/menus" -type f |
-	xargs sed -s -i -e '/var:menuHilfe/d'
-if [ -e "${HTML_DIR}/global.inc" ]; then
-    modsed '/setvariable var:txtHelp/d' "${HTML_DIR}/global.inc"
-fi
-find "${HTML_DIR}/.." -name "*.html" -type f |
-	xargs sed -s -i -e '/<input type="button" onclick="uiDoHelp/d'
+	if [ -e "${HTML_LANG_MOD_DIR}/html/de" ];then
+		HTML_DIR="${HTML_LANG_MOD_DIR}/html/de"
+	else
+		HTML_DIR="${HTML_LANG_MOD_DIR}/html/en"
+	fi
+	echo1 "${HTML_DIR}"
+	rm_files "${HTML_DIR}/help"
+	find "${HTML_DIR}/menus" -type f |
+		xargs sed -s -i -e '/var:menuHilfe/d'
+	if [ -e "${HTML_DIR}/global.inc" ]; then
+	    modsed '/setvariable var:txtHelp/d' "${HTML_DIR}/global.inc"
+	fi
+	find "${HTML_DIR}/.." -name "*.html" -type f |
+		xargs sed -s -i -e '/<input type="button" onclick="uiDoHelp/d'
 
-# Remove functions "uiDoHelp*"
-find "${HTML_DIR}/.." -name '*.js' -type f |
-	xargs -I '{}' "$TOOLS_DIR/developer/remove_js_function.sh" "{}" "uiDoHelp[[:alpha:]]*"
+	# Remove functions "uiDoHelp*"
+	find "${HTML_DIR}/.." -name '*.js' -type f |
+		xargs -I '{}' "$TOOLS_DIR/developer/remove_js_function.sh" "{}" "uiDoHelp[[:alpha:]]*"
 
-# Remove functions "jslPopHelp*"
-find "${HTML_DIR}/js" -name '*.js' -type f |
-	xargs -I '{}' "$TOOLS_DIR/developer/remove_js_function.sh" "{}" "jslPopHelp[[:alpha:]]*"
+	# Remove functions "jslPopHelp*"
+	find "${HTML_DIR}/js" -name '*.js' -type f |
+		xargs -I '{}' "$TOOLS_DIR/developer/remove_js_function.sh" "{}" "jslPopHelp[[:alpha:]]*"
+fi
