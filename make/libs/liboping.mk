d2224ad7bae0dc29d1821658284711ddaad79e92
Gene Rudoy <gene@freetz.org>
2011-02-12 15:14:57 +0000

    * add new library liboping (by TheAdmiral + fixes by me)  * refs #1196
    git-svn-id: http://svn.freetz.org/trunk@6533 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/libs/liboping.mk
@@ -0,0 +1,47 @@
+$(call PKG_INIT_LIB, 1.4.0)
+$(PKG)_LIB_VERSION:=0.2.3
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SOURCE_MD5:=b3d3ef82c8b963266e0ce8db1509cac4
+$(PKG)_SITE:=http://verplant.org/liboping/files
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/.libs/$(pkg).so.$($(PKG)_LIB_VERSION)
+$(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/$(pkg).so.$($(PKG)_LIB_VERSION)
+$(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/$(pkg).so.$($(PKG)_LIB_VERSION)
+
+$(PKG)_CONFIGURE_OPTIONS += --enable-shared
+$(PKG)_CONFIGURE_OPTIONS += --enable-static
+$(PKG)_CONFIGURE_OPTIONS += --with-perl-bindings=no
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(LIBOPING_DIR) all
+
+$($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
+	$(SUBMAKE) -C $(LIBOPING_DIR) \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+		install
+	$(PKG_FIX_LIBTOOL_LA) \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/liboping.la
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
+	$(INSTALL_LIBRARY_STRIP)
+
+$(pkg): $($(PKG)_STAGING_BINARY)
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(LIBOPING_DIR) clean
+	$(RM) \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/liboping* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/oping.h \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/{oping,noping} \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/share/man/man?/*oping*
+
+$(pkg)-uninstall:
+	$(RM) $(LIBOPING_TARGET_DIR)/liboping*.so*
+
+$(PKG_FINISH)
