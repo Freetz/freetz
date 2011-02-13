3565473565d5c4efa0c5fec23b30e377b0ece4c4
Gene Rudoy <gene@freetz.org>
2011-02-13 18:05:57 +0000

    collectd:  * bump version to 4.10.2  * revise makefile (partially based on a patch provided by TheAdmiral in #1196)  * refs #1196
    git-svn-id: http://svn.freetz.org/trunk@6536 f5190166-0702-4917-9039-51ec32eddaf5

--- make/collectd/collectd.mk
+++ make/collectd/collectd.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 4.10.0)
-$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=c473cf8e9f22f5a9f7ef4c5be1b0c436
+$(call PKG_INIT_BIN, 4.10.2)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SOURCE_MD5:=85d9d8d0a1327782661e3c89800aa70e
 $(PKG)_SITE:=http://collectd.org/files
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/$(pkg)
@@ -9,7 +9,6 @@ $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/$(pkg)
 $(PKG)_TYPES_DB:=$($(PKG)_DIR)/src/types.db
 $(PKG)_TARGET_TYPES_DB:=$($(PKG)_DEST_DIR)/usr/share/$(pkg)/types.db
 
-# we need libltdl
 $(PKG)_DEPENDS_ON := libtool
 # ensure system libltdl is used and not the included one
 $(PKG)_CONFIGURE_PRE_CMDS += $(RM) -r libltdl; sed -i -r -e '/SUBDIRS/ s/libltdl//g' ./Makefile.in;
@@ -22,64 +21,35 @@ $(PKG)_CONFIGURE_PRE_CMDS += sed -i -r -e 's,-Werror,,g' ./configure ./src/Makef
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
 
 $(PKG)_CONFIGURE_OPTIONS += --with-nan-emulation
-$(PKG)_CONFIGURE_OPTIONS += --with-fp-layout=nothing #to be reevaluated for 7390
+$(PKG)_CONFIGURE_OPTIONS += --with-fp-layout=$(if $(FREETZ_TARGET_ARCH_BE),endianflip,nothing)
 
-$(PKG)_PLUGINS_ENABLED :=
-$(PKG)_PLUGINS_DISABLED :=
+$(PKG)_FEATURES_DISABLED := \
+    java \
+    libcurl \
+    libdbi \
+    libesmtp libevent \
+    libganglia libgcrypt \
+    libiokit libiptc \
+    libjvm \
+    libkstat libkvm \
+    libmemcached libmodbus libmysql \
+    libnetapp libnetlink libnetsnmp libnotify \
+    libopenipmipthread liboping libowcapi \
+    libpcap libperfstat libperl libpq \
+    librouteros librrd \
+    libsensors libstatgrab \
+    libtokyotyrant \
+    libupsclient \
+    libvirt \
+    libxml2 libxmms \
+    libyajl \
+    oracle \
+    perl-bindings python
+$(PKG)_CONFIGURE_OPTIONS += $(foreach feature,$($(PKG)_FEATURES_DISABLED),--with-$(feature)=no)
 
-# TODO: revise disabled plugins & provide menuconfig options if it makes sense
-
-# 'collect data'-plugins
-$(PKG)_PLUGINS_DISABLED := \
-	apache apcups apple_sensors ascent \
-	battery bind \
-	cpu cpufreq conntrack contextswitch curl curl_json curl_xml \
-	dbi disk df dns \
-	email entropy \
-	filecount fscache \
-	gmond \
-	hddtemp \
-	interface ipmi iptables ipvs irq \
-	libvirt load \
-	madwifi mbmon memcachec memcached memory modbus multimeter mysql \
-	netapp netlink network nfs nginx ntpd nut \
-	olsrd onewire openvpn oracle \
-	pinba ping postgresql powerdns processes protocols \
-	routeros \
-	sensors serial snmp swap \
-	table tail tape tcpconns teamspeak2 ted thermal tokyotyrant \
-	uptime users \
-	vmem vserver \
-	wireless \
-	xmms \
-	zfs_arc
-
-# output-plugins
-$(PKG)_PLUGINS_DISABLED += csv network rrdcached rrdtool unixsock write_http
-
-# binding-plugins
-$(PKG)_PLUGINS_DISABLED += exec java perl python
-
-# logging-plugins
-$(PKG)_PLUGINS_DISABLED += logfile
-$(PKG)_PLUGINS_ENABLED += syslog
-
-# notification-plugins
-$(PKG)_PLUGINS_DISABLED += notify_desktop notify_email
-
-# 'filter chain'-plugins
-$(PKG)_PLUGINS_DISABLED += \
-	match_empty_counter match_hashed match_regex match_timediff match_value \
-	target_notification target_replace target_scale target_set
-
-# miscellaneous-plugins
-$(PKG)_PLUGINS_DISABLED += uuid
-
-# some plugins provide multiple features, e.g. they do both collect data & output it
-# ensure disabled-plugins-list doesn't contain any plugin from the enabled-list
-$(PKG)_PLUGINS_DISABLED := $(filter-out $($(PKG)_PLUGINS_ENABLED),$($(PKG)_PLUGINS_DISABLED))
-
-$(PKG)_CONFIGURE_OPTIONS += $(foreach plugin,$($(PKG)_PLUGINS_DISABLED),--disable-$(plugin))
+# plugins
+$(PKG)_CONFIGURE_OPTIONS += --disable-all-plugins
+$(PKG)_PLUGINS_ENABLED := syslog
 $(PKG)_CONFIGURE_OPTIONS += $(foreach plugin,$($(PKG)_PLUGINS_ENABLED),--enable-$(plugin))
 
 $(PKG)_PLUGINS_DIR := $(FREETZ_LIBRARY_PATH)/$(pkg)
