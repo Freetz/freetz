f127753b8f19ebac6d95c66a9d97e28c38684eab
Oliver Metz <oliver@freetz.org>
2011-02-13 19:52:41 +0000

    * Add 7112 7170 Alien variant (by Dunedan)   * refs #1211
    git-svn-id: http://svn.freetz.org/trunk@6539 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ patches/100-7112_7170.sh
@@ -0,0 +1,50 @@
+isFreetzType 7112_7170 || return 0
+
+if [ -z "$FIRMWARE2" ]; then
+	echo "ERROR: no tk firmware" 1>&2
+	exit 1
+fi
+
+echo1 "adapt firmware for 7112"
+
+echo2 "copying 7112 files"
+cp "${DIR}/.tk/original/filesystem/lib/modules/bitfile.bit" "${FILESYSTEM_MOD_DIR}/lib/modules/"
+cp "${DIR}/.tk/original/filesystem/lib/modules/microvoip-dsl.bin" "${FILESYSTEM_MOD_DIR}/lib/modules/"
+cp -R "${DIR}/.tk/original/filesystem/lib/modules/2.6.13.1-ohio/kernel/drivers/isdn/isdn_fon3" "${FILESYSTEM_MOD_DIR}/lib/modules/2.6.13.1-ohio/kernel/drivers/isdn/"
+cp -R "${DIR}/.tk/original/filesystem/lib/modules/2.6.13.1-ohio/kernel/drivers/char/Piglet_noemif" "${FILESYSTEM_MOD_DIR}/lib/modules/2.6.13.1-ohio/kernel/drivers/char/"
+
+echo2 "deleting obsolete files"
+rm_files "${FILESYSTEM_MOD_DIR}/lib/modules/2.6.13.1-ohio/kernel/drivers/isdn/isdn_fon4"
+rm_files "${FILESYSTEM_MOD_DIR}/lib/modules/2.6.13.1-ohio/kernel/drivers/char/Piglet/"
+
+echo2 "moving default config dir"
+mv "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_7170" "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_7112"
+
+echo2 "patching rc.S and rc.conf"
+modsed "s/piglet_bitfile=\/lib\/modules\/microvoip_isdn_top\.bit.*$/piglet_bitfile=\/lib\/modules\/bitfile\.bit/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
+modsed "s/modprobe Piglet piglet_bitfile=.*$/modprobe Piglet_noemif piglet_bitfile=\$piglet_bitfile piglet_bitfilemode=2/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
+
+modsed "s/CONFIG_VERSION_MAJOR=.*$/CONFIG_VERSION_MAJOR=\"87\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/CONFIG_PRODUKT_NAME=.*$/CONFIG_PRODUKT_NAME=\"FRITZ!Box Fon WLAN 7112\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/CONFIG_PRODUKT=.*$/CONFIG_PRODUKT=\"Fritz_Box_7112\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/CONFIG_INSTALL_TYPE=.*$/CONFIG_INSTALL_TYPE=\"ar7_8MB_xilinx_1eth_2ab_nopstn_wlan_32713\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/HWRevision_ATA=0$/HWRevision_ATA=1/" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/CONFIG_ETH_COUNT=.*$/CONFIG_ETH_COUNT=\"1\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+
+modsed "s/CONFIG_AB_COUNT=.*$/CONFIG_AB_COUNT=\"2\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/CONFIG_CAPI_NT=.*$/CONFIG_CAPI_NT=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/CONFIG_CAPI_POTS=.*$/CONFIG_CAPI_POTS=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/CONFIG_CAPI_TE=.*$/CONFIG_CAPI_TE=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/CONFIG_FON_HD=.*$/CONFIG_FON_HD=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/CONFIG_FONQUALITY=.*$/CONFIG_FONQUALITY=\"y\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/CONFIG_T38=.*$/CONFIG_T38=\"y\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+
+modsed "s/CONFIG_USB_HOST_AVM=.*$/CONFIG_USB_HOST_AVM=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/CONFIG_USB_WLAN_AUTH=.*$/CONFIG_USB_WLAN_AUTH=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/CONFIG_USB_STORAGE=.*$/CONFIG_USB_STORAGE=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/CONFIG_USB_STORAGE_SPINDOWN=.*$/CONFIG_USB_STORAGE_SPINDOWN=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+
+# patch install script to accept firmware from 7170
+echo1 "applying install patch"
+modpatch "$FIRMWARE_MOD_DIR" "${PATCHES_DIR}/cond/install-7112_7170.patch" || exit 2
+
