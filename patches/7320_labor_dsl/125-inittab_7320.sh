292e2f347635f80783ec675ad4363be412edaeaf
Oliver Metz <oliver@freetz.org>
2011-02-16 14:34:15 +0000

    * 7320: Add Labor DSL 100.04.89-19131 (refs #1061)
    git-svn-id: http://svn.freetz.org/trunk@6551 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ patches/7320_labor_dsl/125-inittab_7320.sh
@@ -0,0 +1,12 @@
+cat << 'EOF' > "${FILESYSTEM_MOD_DIR}/etc/inittab"
+#
+::restart:/sbin/init
+::sysinit:/etc/init.d/rc.S
+
+# Start an "askfirst" shell on the console (whatever that may be)
+/dev/ttyS1::askfirst:-/bin/sh
+
+# Stuff to do before rebooting
+::shutdown:/bin/sh -c /var/post_install
+
+EOF
---
+++ patches/7320_labor_dsl/150-S80-rcmod.sh
@@ -0,0 +1,8 @@
+# Emergency stop switch for execution of debug.cfg
+sed -i -r 's#(\. /var/flash/debug\.cfg)#[ "$dbg_off" == "y" ] || \1#g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.tail.sh"
+
+cat << 'EOF' > "${FILESYSTEM_MOD_DIR}/etc/init.d/S80-rcmod"
+# Emergency stop switch for execution of Freetz as a whole
+[ "$ds_off" == "y" ] || . /etc/init.d/rc.mod 2>&1 | tee /var/log/mod.log
+
+EOF
