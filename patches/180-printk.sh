02f5c7a29be7ece9db973e94b167cf49e4253222
Oliver Metz <oliver@freetz.org>
2011-02-20 22:52:09 +0000

    7320: Fix some issues:  * cat /dev/debug is in S17-isdn  * modprobe jffs2 returns false (busybox 1.13.4 returns true!?)  * Takeover AVM device files (Perhaps some Freetz specific devices are missing now)
    git-svn-id: http://svn.freetz.org/trunk@6569 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/180-printk.sh
+++ patches/180-printk.sh
@@ -1,9 +1,13 @@
 if isFreetzType 5010 5050 7050 7140 7141 7150 7170; then
 	echo1 "applying printk patch"
 	modsed "s/takeover_printk=1/takeover_printk=0/g" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.S"
-elif isFreetzType 3270 7240 7270; then
-	echo1 "applying printk patch"
+#elif isFreetzType 3270 7240 7270; then
+#	echo1 "applying printk patch"
 #	modsed "s/AVM_PRINTK/STD_PRINTK/g" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.S"
 fi
 
-modsed "/^cat \/dev\/debug.*$/ s/^/: #/g" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.S"
+if isFreetzType 7320; then
+	modsed "/^cat \/dev\/debug.*$/ s/^/: #/g" "$FILESYSTEM_MOD_DIR/etc/init.d/S17-isdn"
+else
+	modsed "/^cat \/dev\/debug.*$/ s/^/: #/g" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.S"
+fi
