6ad2bd51014fab5de9067166aa42bb7f98d8c71d
Oliver Metz <oliver@freetz.org>
2011-02-20 23:21:12 +0000

    * maxdevcount patch: Fix for 7320
    git-svn-id: http://svn.freetz.org/trunk@6571 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/460-MAXDEVCOUNT.sh
+++ patches/460-MAXDEVCOUNT.sh
@@ -1,3 +1,8 @@
 [ "$FREETZ_PATCH_MAXDEVCOUNT" == "y" ] || return 0
-echo1 "patching usb.pandu: MAXDEVCOUNT"
-modsed "s/^MAXDEVCOUNT=/MAXDEVCOUNT=9	# oldvalue: /g" "${FILESYSTEM_MOD_DIR}/etc/hotplug/usb.pandu"
+if isFreetzType 7320; then
+	file="create_handle.sh"
+else
+	file="usb.pandu"
+fi
+echo1 "patching ${file}: MAXDEVCOUNT"
+modsed "s/^MAXDEVCOUNT=/MAXDEVCOUNT=9	# oldvalue: /g" "${FILESYSTEM_MOD_DIR}/etc/hotplug/${file}"
