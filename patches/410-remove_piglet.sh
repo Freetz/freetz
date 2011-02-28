2c9aa264c1821f0d3de0285fe35be91b636b9dda
Oliver Metz <oliver@freetz.org>
2011-02-28 14:44:44 +0000

    * improved remove piglet patch (by hippie2000)  * refs #1228
    git-svn-id: http://svn.freetz.org/trunk@6583 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/410-remove_piglet.sh
+++ patches/410-remove_piglet.sh
@@ -1,11 +1,35 @@
-isFreetzType 7170 && [ "$FREETZ_REMOVE_PIGLET_V1" == "y" -o "$FREETZ_REMOVE_PIGLET_V2" == "y" ] || return 0
-echo1 "removing 7170 piglet file(s)"
+[ "$FREETZ_REMOVE_PIGLET_POTS" == "y" -o \
+	"$FREETZ_REMOVE_PIGLET_ISDN" == "y" -o \
+	"$FREETZ_REMOVE_PIGLET_V1" == "y" -o \
+	"$FREETZ_REMOVE_PIGLET_V2" == "y" ] || return 0
+echo1 "removing unneeded piglet file(s)"
 
 bitfile_path="${FILESYSTEM_MOD_DIR}/lib/modules"
-bitfile_name="microvoip_isdn_top.bit"
-
 files=""
-[ "$FREETZ_REMOVE_PIGLET_V1" == "y" ] && files+="${bitfile_path}/${bitfile_name}"
-[ "$FREETZ_REMOVE_PIGLET_V2" == "y" ] && files+=" ${bitfile_path}/${bitfile_name}1"
+if [ "$FREETZ_REMOVE_PIGLET_POTS" == "y" ]; then
+	files+="${bitfile_path}/microvoip_top.bit "
+	files+="${bitfile_path}/bitfile_pots.bit "
+	files+="${bitfile_path}/bitfile_pots.bit1 "
+	modsed "s/piglet_bitfilemode=\`\/bin.*$/piglet_bitfilemode=1/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
+fi
+
+if [ "$FREETZ_REMOVE_PIGLET_ISDN" == "y" ]; then
+	files+="${bitfile_path}/microvoip_isdn_top.bit "
+	files+="${bitfile_path}/bitfile_isdn.bit "
+	files+="${bitfile_path}/bitfile_isdn.bit1 "
+	modsed "s/piglet_bitfilemode=\`\/bin.*$/piglet_bitfilemode=2/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
+fi
+
+if [ "$FREETZ_REMOVE_PIGLET_V1" == "y" ]; then
+	files+="${bitfile_path}/microvoip_isdn_top.bit "
+	files+="${bitfile_path}/bitfile_isdn.bit "
+	files+="${bitfile_path}/bitfile_pots.bit "
+fi
+
+if [ "$FREETZ_REMOVE_PIGLET_V2" == "y" ]; then
+	files+="${bitfile_path}/microvoip_isdn_top.bit1 "
+	files+="${bitfile_path}/bitfile_isdn.bit1 "
+	files+="${bitfile_path}/bitfile_pots.bit1 "
+fi
 
 rm_files "$files"
