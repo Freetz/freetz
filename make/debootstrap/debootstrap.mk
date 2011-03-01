575fcfb4807ea38f7873df7df0872dd9a5f4f247
Oliver Metz <oliver@freetz.org>
2011-03-01 22:13:57 +0000

    * debootstrap: Bump version to 1.0.28
    git-svn-id: http://svn.freetz.org/trunk@6586 f5190166-0702-4917-9039-51ec32eddaf5

--- make/debootstrap/debootstrap.mk
+++ make/debootstrap/debootstrap.mk
@@ -1,23 +1,27 @@
-$(call PKG_INIT_BIN, 0.3.3.2etch1)
+$(call PKG_INIT_BIN, 1.0.28)
 #
 # The only binary needed for debootstrap is 'pkgdetails'. In newer debian revisions
-# is has moved from package 'debootstrap' to 'base_installer', however, the source
-# code didn't changed. So we use an ancient version of debootstrap sources to build
-# this binary.
+# is has moved from package 'debootstrap' to 'base_installer'. So we use an own
+# version of debootstrap sources to build this binary.
 # All other files are extracted from the current debootstrap source, at the time of
-# writing this comment this is version 1.0.10.
+# writing this comment this is version 1.0.28
 #
-$(PKG)_SOURCE:=debootstrap_$(DEBOOTSTRAP_VERSION).tar.gz
-$(PKG)_SITE:=http://ftp.de.debian.org/debian/pool/main/d/debootstrap
+#$(PKG)_SOURCE:=debootstrap_$(DEBOOTSTRAP_VERSION).tar.gz
+#$(PKG)_SITE:=http://ftp.de.debian.org/debian/pool/main/d/debootstrap
+#$(PKG)_SOURCE_MD5:=f8172809afe7cfdcdc6745229f024d9d
+
+$(PKG)_BINARY_SOURCE:=$($(PKG)_MAKE_DIR)/src/pkgdetails.c
 $(PKG)_BINARY:=$($(PKG)_DIR)/pkgdetails
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/lib/debootstrap/pkgdetails
-$(PKG)_SOURCE_MD5:=f8172809afe7cfdcdc6745229f024d9d
 
-$(PKG_SOURCE_DOWNLOAD)
+
+#$(PKG_LOCALSOURCE_PACKAGE)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_NOP)
 
-$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+$($(PKG)_BINARY): $($(PKG)_BINARY_SOURCE) $($(PKG)_DIR)/.configured
+	mkdir -p $(DEBOOTSTRAP_DIR)
+	cp -a $< $(DEBOOTSTRAP_DIR)
 	cd $(DEBOOTSTRAP_DIR) && $(TARGET_CONFIGURE_ENV) \
 		$(TARGET_CC) $(TARGET_CFLAGS) -o pkgdetails pkgdetails.c
 
