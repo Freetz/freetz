90bc37cabb18f8c9621e0170632046b9189dad89
cuma <cuma@freetz.org>
2011-03-03 23:47:20 +0000

    update multiple_printers patch for .8x firmware (refs #1185)
    git-svn-id: http://svn.freetz.org/trunk@6600 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/275-multiple_printers.sh
+++ patches/275-multiple_printers.sh
@@ -1,8 +1,10 @@
 [ "$FREETZ_PATCH_MULTIPLE_PRINTERS" == "y" ] || return 0
 	echo1 "adding support for multiple printers"
 
-	if isFreetzType 7170 && ! isFreetzType LABOR_DSL; then
+	if isFreetzType 7170 && ! isFreetzType LABOR_PREVIEW; then
 		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/275-multiple_printers_7170.patch"
+	elif isFreetzType 7170 || isFreetzType 7270; then
+		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/275-multiple_printers_fw86.patch"
 	else
 		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/275-multiple_printers.patch"
 	fi
