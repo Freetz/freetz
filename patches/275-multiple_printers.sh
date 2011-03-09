da67d2eb64c578411208d446e0498306e0d96248
Oliver Metz <oliver@freetz.org>
2011-03-09 15:55:31 +0000

    * multpile printers: Extend patch to 3270, 7240, 7340 and 7390 (refs #1185)
    git-svn-id: http://svn.freetz.org/trunk@6623 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/275-multiple_printers.sh
+++ patches/275-multiple_printers.sh
@@ -3,7 +3,7 @@
 
 	if isFreetzType 7170 && ! isFreetzType LABOR_PREVIEW; then
 		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/275-multiple_printers_7170.patch"
-	elif isFreetzType 7170 || isFreetzType 7270; then
+	elif isFreetzType 3270 3270_V3 7170 7240 7270 7340 7390; then
 		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/275-multiple_printers_fw86.patch"
 	else
 		modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/275-multiple_printers.patch"
