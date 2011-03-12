38b4b5a87becb8cd5053f32baa7c7899527560ad
cuma <cuma@freetz.org>
2011-03-12 16:25:36 +0000

    multid-wrapper: make sure the modsed was sucessfully
    git-svn-id: http://svn.freetz.org/trunk@6635 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/109-rc.net-multid-wrapper.sh
+++ patches/109-rc.net-multid-wrapper.sh
@@ -4,4 +4,5 @@
 
 echo1 "replacing multid by a wrapper for $MULTID_WRAPPER"
 modsed "s#\(^PATH=\)\(.*\)#\1/mod/pkg/$MULTID_WRAPPER/usr/lib/$MULTID_WRAPPER/bin:\2#g" \
- "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.net"
+ "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.net" \
+ "^PATH=/mod/pkg/$MULTID_WRAPPER/usr/lib/$MULTID_WRAPPER/bin:"
