3c035bc633881761c1505c230d1155c7452ef5d8
Oliver Metz <oliver@freetz.org>
2011-03-20 14:18:44 +0000

    ![labor-preview]: Merge r6679 from trunk
    git-svn-id: http://svn.freetz.org/branches/labor_preview@6680 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/115-rc.S.sh
+++ patches/115-rc.S.sh
@@ -1,6 +1,20 @@
-isFreetzType 7320 && return 0
+if isFreetzType 7320; then
+
+# Emergency stop switch for execution of debug.cfg
+sed -i -r 's#(\. /var/flash/debug\.cfg)#[ "$dbg_off" == "y" ] || \1#g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.tail.sh"
+
+cat << 'EOF' > "${FILESYSTEM_MOD_DIR}/etc/init.d/S80-rcmod"
+# Emergency stop switch for execution of Freetz as a whole
+[ "$ds_off" == "y" ] || . /etc/init.d/rc.mod 2>&1 | tee /var/log/mod.log
+
+EOF
+
+else
 
 # Emergency stop switch for execution of debug.cfg
 sed -i -r 's#(\. /var/flash/debug\.cfg)#[ "$dbg_off" == "y" ] || \1#g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
+
 # Emergency stop switch for execution of Freetz as a whole
 echo '[ "$ds_off" == "y" ] || . /etc/init.d/rc.mod 2>&1 | tee /var/log/mod.log' >> "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
+
+fi
