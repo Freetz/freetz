e2748ae5f99d0dc6afa805e2185be6656bf75692
Oliver Metz <oliver@freetz.org>
2011-04-01 08:04:12 +0000

    * inadyn-mt: Bump to version 2.24.36 (by MaxMuster, small changes by me)   * hopefulley fixes known problems   * use threads   * multiservices update by Bryan Hoover (maintainer)
    git-svn-id: http://svn.freetz.org/trunk@6744 f5190166-0702-4917-9039-51ec32eddaf5

--- make/inadyn-mt/inadyn-mt.mk
+++ make/inadyn-mt/inadyn-mt.mk
@@ -1,21 +1,25 @@
-$(call PKG_INIT_BIN,02.12.24)
+$(call PKG_INIT_BIN,02.24.36)
 $(PKG)_SOURCE:=$(pkg).v.$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=14c044a2754417b344be364eeccc6779
 $(PKG)_SITE:=@SF/inadyn-mt
-$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/$(pkg)
-$(PKG)_BINARY:=$($(PKG)_DIR)/bin/linux/inadyn-mt
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/$(pkg).v.$($(PKG)_VERSION)
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/inadyn-mt
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/inadyn-mt
-$(PKG)_SOURCE_MD5:=9e7a8a8841a6b3fe0c6f9412e581324b
+
+
+$(PKG)_CONFIGURE_OPTIONS += --disable-sound
+$(PKG)_CONFIGURE_OPTIONS += --enable-threads
+#$(PKG)_CONFIGURE_OPTIONS += --enable-debug
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
-$(PKG_CONFIGURED_NOP)
+$(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(INADYN_MT_DIR) \
-		CC="$(TARGET_CC)" \
-		STRIP="$(TARGET_STRIP)" \
-		CFLAGS="$(TARGET_CFLAGS)" \
-		LDFLAGS=""
+		inadyn_mt_CFLAGS="" \
+		inadyn_mt_LDFLAGS=""
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
