1d37842cd2c522a3372f3dad0f5bc1f8efeef4df
Oliver Metz <oliver@freetz.org>
2011-04-04 12:59:10 +0000

    [labor_preview]: Applying printk patch (labor preview)  * please test if DECT and MT-D/F's are working with that (7270, 7320, 7390)  * refs #1233
    git-svn-id: http://svn.freetz.org/branches/labor_preview@6766 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/180-printk.sh
+++ patches/180-printk.sh
@@ -1,15 +1,14 @@
-if isFreetzType 5010 5050 7050 7140 7141 7150 7170; then
-	echo1 "applying printk patch"
-	modsed "s/takeover_printk=1/takeover_printk=0/g" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.S"
-#elif isFreetzType 3270 7240 7270; then
-#	echo1 "applying printk patch"
-#	modsed "s/AVM_PRINTK/STD_PRINTK/g" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.S"
-fi
+echo1 "applying printk patch"
 
-if isFreetzType 7320 || (isFreetzType 7390 && isFreetzType LABOR_PREVIEW); then
+if isFreetzType 5010 5050 7050 7140 7141 7150 7170; then
+	modsed "s/takeover_printk=1/takeover_printk=0/g" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.S"
+	file="$FILESYSTEM_MOD_DIR/etc/init.d/rc.S"
+elif isFreetzType 7320 || (isFreetzType 7390 && isFreetzType LABOR_PREVIEW); then
 	file="$FILESYSTEM_MOD_DIR/etc/init.d/S17-isdn"
-elif isFreetzType LABOR_PREVIEW && isFreetzType 7270; then
+	modsed "s/AVM_PRINTK/STD_PRINTK/g" $file
+elif isFreetzType 7270 && isFreetzType LABOR_PREVIEW; then
 	file="$FILESYSTEM_MOD_DIR/etc/init.d/S11-piglet"
+	modsed "s/AVM_PRINTK/STD_PRINTK/g" $file
 else
 	file="$FILESYSTEM_MOD_DIR/etc/init.d/rc.S"
 fi
