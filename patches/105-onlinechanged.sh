7d532b5e0e9f2321108908563fff02c46d7c1d77
cuma <cuma@freetz.org>
2011-04-14 17:37:21 +0000

    onlinechanged:  * moved logging from script to /bin/onlinechanged  * log output of scripts to syslog  * execute only non-empty scripts  * higher priority for opendd (refs #1265)
    git-svn-id: http://svn.freetz.org/trunk@6823 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/105-onlinechanged.sh
+++ patches/105-onlinechanged.sh
@@ -8,8 +8,12 @@ cat << 'EOF' > "${FILESYSTEM_MOD_DIR}/bin/onlinechanged"
 
 PATH=/bin:/usr/bin:/sbin:/usr/sbin
 
+eventadd 1 "Running onlinechanged: $@"
+logger -t ONLINECHANGED "going $@"
 for i in /etc/onlinechanged/* /tmp/onlinechanged /tmp/flash/onlinechanged/*; do
-	[ -f "$i" ] && sh "$i" "$@"
+	[ ! -s "$i" ] && continue
+	logger -t ONLINECHANGED "is executing $i"
+	sh "$i" "$@" 2>&1 | while read line; do logger -t ONLINECHANGED "$line"; done
 done
 EOF
 
