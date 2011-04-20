87220bdf49e69e809ae25e06b4b1af01277fefbb
Oliver Metz <oliver@freetz.org>
2011-04-20 07:18:24 +0000

    * socat: Bump to version 1.7.1.3 (by MaxMuster)   * Add termios switch   * closes #1296
    git-svn-id: http://svn.freetz.org/trunk@6855 f5190166-0702-4917-9039-51ec32eddaf5

--- make/socat/socat.mk
+++ make/socat/socat.mk
@@ -1,15 +1,21 @@
-$(call PKG_INIT_BIN, 1.6.0.1)
-$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(call PKG_INIT_BIN, 1.7.1.3)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_SITE:=http://www.dest-unreach.org/socat/download
 $(PKG)_BINARY:=$($(PKG)_DIR)/socat
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/socat
-$(PKG)_SOURCE_MD5:=5a6a1d1e398d5c4d32fa6515baf477af
+$(PKG)_SOURCE_MD5:=2081987fb0cb0290b8105574058cb329
 
 $(PKG)_DEPENDS_ON := openssl
 
-$(PKG)_CONFIGURE_OPTIONS += --disable-termios
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_SOCAT_WITHTERMIOS),--enable-termios,--disable-termios)
 $(PKG)_CONFIGURE_OPTIONS += --with-ssl="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
 $(PKG)_CONFIGURE_OPTIONS += --enable-ssl
+
+$(PKG)_CONFIGURE_OPTIONS += sc_cv_sys_crdly_shift=9
+$(PKG)_CONFIGURE_OPTIONS += sc_cv_sys_tabdly_shift=11
+$(PKG)_CONFIGURE_OPTIONS += sc_cv_sys_csize_shift=4
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_SOCAT_WITHTERMIOS
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
