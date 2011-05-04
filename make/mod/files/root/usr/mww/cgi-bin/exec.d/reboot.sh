d2cb2b1d6043d9579c8ba397ebebbd577d4b211b
Oliver Metz <oliver@freetz.org>
2011-05-04 08:01:20 +0000

    Following r6919: Forward to main page if it's not a POST request (refs #1324)
    git-svn-id: http://svn.freetz.org/trunk@6929 f5190166-0702-4917-9039-51ec32eddaf5

--- make/mod/files/root/usr/mww/cgi-bin/exec.d/reboot.sh
+++ make/mod/files/root/usr/mww/cgi-bin/exec.d/reboot.sh
@@ -1,5 +1,11 @@
+if [ "$REQUEST_METHOD" != POST ]; then
+	echo "Status: 302 Found"
+	echo "Location: $(href mod status)"
+	exit
+fi
+
 cgi_begin '$(lang de:"Neustart" en:"Reboot") ...'
 echo '<p>$(lang de:"Starte neu" en:"Rebooting") ...</p>'
 echo '<p>$(lang de:"Nach dem Neustart <a href=\"/\" target=\"_top\">hier</a> wieder einloggen." en:"Login <a href=\"/\" target=\"_top\">here</a> after reboot.")</p>'
 cgi_end
-[ "$REQUEST_METHOD" = POST ] && reboot
+reboot
