81e9f2ddaf909e6b1851799b0869503a13d5c8a7
Oliver Metz <oliver@freetz.org>
2011-05-06 13:22:04 +0000

    * chronyd: Remove /dev/rtc from device.table   * Patch AVM's rc.rtc.sh to create rtc-Devices in /dev instead of /var/dev   * thanks to abraxxl for assistance (http://www.ip-phone-forum.de/showthread.php?t=232820)   * fixes #1327
    git-svn-id: http://svn.freetz.org/trunk@6948 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ patches/220-chronyd.sh
@@ -0,0 +1,12 @@
+[ "$FREETZ_HAS_CHRONYD" == "y" ] || return 0
+
+if [ "$FREETZ_REMOVE_CHRONYD" == "y" ]; then 
+	echo1 "remove chronyd files"
+	rm_files "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.chrony" \
+		"${FILESYSTEM_MOD_DIR}/etc/onlinechanged/chrony" \
+		"${FILESYSTEM_MOD_DIR}/sbin/chronyc" \
+		"${FILESYSTEM_MOD_DIR}/sbin/chronyd"
+else
+	[ -e "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.rtc.sh" ] && \
+		modsed "s#/var/dev/rtc#/dev/rtc#g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.rtc.sh"
+fi
