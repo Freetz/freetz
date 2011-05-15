ca2b295c3fc9c7f7d7f02b9390e8ac8b10c5622a
Oliver Metz <oliver@freetz.org>
2011-05-15 12:51:00 +0000

    * openssl patch: Fix replace ssl libs   * but I'm skeptical if it still works
    git-svn-id: http://svn.freetz.org/trunk@7000 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/230-openssl.sh
+++ patches/230-openssl.sh
@@ -2,12 +2,12 @@ if [ "$FREETZ_REPLACE_SSL_LIBS" == "y" ]; then
 	if [ "$FREETZ_LIB_libcrypto" == "y" ]; then
 		echo1 "removing avm's libcrypto and create symlinks"
 		rm_files "${FILESYSTEM_MOD_DIR}/lib/libcrypto.so.0.9.8"
-		ln -sf "/usr/lib/freetz/libcrypto.so.0.9.8" "${FILESYSTEM_MOD_DIR}/lib/libcrypto.so.0.9.8"
+		ln -sf "/usr/lib/freetz/libcrypto.so.1.0.0" "${FILESYSTEM_MOD_DIR}/lib/libcrypto.so.0.9.8"
 	fi
 	if [ "$FREETZ_LIB_libssl" == "y" ]; then
 		echo1 "removing avm's libssl and create symlinks"
 		rm_files "${FILESYSTEM_MOD_DIR}/lib/libssl.so.0.9.8"
-	        ln -sf "/usr/lib/freetz/libssl.so.0.9.8" "${FILESYSTEM_MOD_DIR}/lib/libssl.so.0.9.8"
+		ln -sf "/usr/lib/freetz/libssl.so.1.0.0" "${FILESYSTEM_MOD_DIR}/lib/libssl.so.0.9.8"
 	fi
 fi
 
