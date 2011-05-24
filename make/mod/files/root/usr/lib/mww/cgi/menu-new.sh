8351d76545328ce1b3d05e3c5f6d3b31a729db74
Andreas Bühmann <andreas.buehmann@web.de>
2011-05-24 14:25:04 +0000

    Redirect to AVM webif; refs #1355
    git-svn-id: http://svn.freetz.org/trunk@7044 f5190166-0702-4917-9039-51ec32eddaf5

--- make/mod/files/root/usr/lib/mww/cgi/menu-new.sh
+++ make/mod/files/root/usr/lib/mww/cgi/menu-new.sh
@@ -192,7 +192,7 @@ new_menu_prepare() {
 <li><a id="firmware_update" href="$(href mod update)">$(lang de:"Firmware-Update" en:"Firmware update")</a></li>
 <li><a id="support_file" href="/cgi-bin/support/index.cgi">$(lang de:"Supportdatei erstellen" en:"Create support file")</a></li>
 <li><a id="rudi_shell" href="/cgi-bin/shell/index.cgi" target="_blank">$(lang de:"Rudi-Shell" en:"Rudi shell")</a></li>
-<li><a id="avmwif_link" href="http://$(self_host)/" target="_blank">$(lang de:"AVM-Webinterface" en:"AVM web interface")</a></li>
+<li><a id="avmwif_link" href="/cgi-bin/avm" target="_blank">$(lang de:"AVM-Webinterface" en:"AVM web interface")</a></li>
 EOF
 	} > "$dir/system.sub"
 
