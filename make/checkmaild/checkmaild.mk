0da69885abba995d1f9049dbd4848396c1333174
Oliver Metz <oliver@freetz.org>
2011-05-27 13:51:06 +0000

    * checkmaild: Remove LED and phone notification from daemon   * Desired actions should be written to the mailactions (maillog.cfg) script by the user    * Differences between different box types are hardly maintainable
    git-svn-id: http://svn.freetz.org/trunk@7049 f5190166-0702-4917-9039-51ec32eddaf5

--- make/checkmaild/checkmaild.mk
+++ make/checkmaild/checkmaild.mk
@@ -1,18 +1,20 @@
-$(call PKG_INIT_BIN, 0.4.5)
+$(call PKG_INIT_BIN, 0.4.6)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SOURCE_MD5:=5dce4e4fd27f5ab363a7b071593f1380
 $(PKG)_SITE:=http://freetz.magenbrot.net
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/checkmaild
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/checkmaild
-$(PKG)_SOURCE_MD5:=19bb8c0288fd84246b294eab7e03b09c
+
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_NOP)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) CROSS="$(TARGET_CROSS)" \
-		CFLAGS="$(TARGET_CFLAGS)" \
-		-C $(CHECKMAILD_DIR)
+	$(SUBMAKE) -C $(CHECKMAILD_DIR) \
+		CROSS="$(TARGET_CROSS)" \
+		CFLAGS="$(TARGET_CFLAGS)"
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
