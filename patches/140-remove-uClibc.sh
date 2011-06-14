3aaa4b44bec2e836f37f0f2a97d32862629f09af
Gene Rudoy <gene@freetz.org>
2011-06-14 22:30:18 +0000

    binutils:  * bump version to 2.21.52.0.2  * remove ld.bfd hardlink (cosmetic change)
    uClibc:
     * bump uClibc-0.9.31.x to 0.9.31.1, remove patches made it upstream
    
    gcc-4.5.3:
     * don't install python scripts (cosmetic change)
    
    refs #842
    
    p.s. new download toolchains are on the way
    
    
    
    git-svn-id: http://svn.freetz.org/trunk@7125 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/140-remove-uClibc.sh
+++ patches/140-remove-uClibc.sh
@@ -1,9 +1,13 @@
-( [ "$FREETZ_UCLIBC_0_9_29_BASED_BOX" == "y" ] && [ ! "$FREETZ_TARGET_UCLIBC_VERSION_0_9_29" == "y" ] || \
-[ "$FREETZ_UCLIBC_0_9_30_BASED_BOX" == "y" ] ) || return 0
+( \
+	[ "$FREETZ_UCLIBC_0_9_29_BASED_BOX" == "y" ] && [ ! "$FREETZ_TARGET_UCLIBC_VERSION_0_9_29" == "y" ] \
+	|| [ "$FREETZ_UCLIBC_0_9_30_BASED_BOX" == "y" ] \
+	|| [ "$FREETZ_UCLIBC_0_9_31_BASED_BOX" == "y" ] \
+) || return 0
 
 version=
 [ "$FREETZ_UCLIBC_0_9_29_BASED_BOX" == "y" ] && version="0.9.29"
 [ "$FREETZ_UCLIBC_0_9_30_BASED_BOX" == "y" ] && version="0.9.30"
+[ "$FREETZ_UCLIBC_0_9_31_BASED_BOX" == "y" ] && version="0.9.31"
 
 echo1 "removing uClibc-${version} files"
 rm_files "${FILESYSTEM_MOD_DIR}/lib/*${version}*"
