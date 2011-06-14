c464df3e2b4fdd59c6887f9b4eae507a2c6fa25a
Oliver Metz <oliver@freetz.org>
2011-06-14 20:22:18 +0000

    * 7570: Add option and patches to build 16 MB firmware for IAD   * First step is to update the box with a < 8 MB image with recover   * refs #1339
    git-svn-id: http://svn.freetz.org/trunk@7123 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/100-7570_IAD.sh
+++ patches/100-7570_IAD.sh
@@ -1,6 +1,23 @@
 isFreetzType 7570_IAD || return 0
 
 echo1 "adapt firmware for 7570 IAD"
+
+mv "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_7570" "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_7570_HN"
+
+modsed "s/CONFIG_PRODUKT_NAME=.*$/CONFIG_PRODUKT_NAME=\"Alice IAD 7570 vDSL\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/CONFIG_PRODUKT=.*$/CONFIG_PRODUKT=\"Fritz_Box_7570_HN\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/CONFIG_INSTALL_TYPE=.*$/CONFIG_INSTALL_TYPE=\"ur8_16MB_xilinx_4eth_2ab_dect_isdn_pots_wlan_33906\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/CONFIG_VERSION_MAJOR=.*$/CONFIG_VERSION_MAJOR=\"75\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/ur8_16MB_xilinx_4eth_2ab_isdn_nt_te_pots_wlan_mimo_usb_host_dect_multiannex_13589/ur8_16MB_xilinx_4eth_2ab_isdn_nt_te_pots_wlan_mimo_usb_host_dect_multiannex_hansenet_60170/g" "${FIRMWARE_MOD_DIR}/var/install"
+
 modsed 's/kernel_start=0x90020000/kernel_start=0x90040000/' "${FIRMWARE_MOD_DIR}/var/install"
-modsed 's/kernel_size=16121856/kernel_size=7995392/' "${FIRMWARE_MOD_DIR}/var/install"
 modsed 's/urlader_size=131072/urlader_size=262144/' "${FIRMWARE_MOD_DIR}/var/install"
+
+if [ "$FREETZ_REPLACE_KERNEL" == "y" ]; then
+	# set mtd1 to 16 MB (244 * 64KB)
+	modsed 's/kernel_size=16121856/kernel_size=15990784/' "${FIRMWARE_MOD_DIR}/var/install"
+	modpatch "$FIRMWARE_MOD_DIR" "${PATCHES_DIR}/cond/install-7570_HN.patch"
+else
+	# use only 8 MB (122 * 64 KB)
+	modsed 's/kernel_size=16121856/kernel_size=7995392/' "${FIRMWARE_MOD_DIR}/var/install"
+fi
