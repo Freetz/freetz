40d903396513ed1b5a587bfaf672ff92a79c3091
Oliver Metz <oliver@freetz.org>
2011-06-18 09:50:22 +0000

    * fwmod: Move webdir of avme images to /usr/www/all too (and put a symlink to avme)   * Adapt patches to new webdir
    git-svn-id: http://svn.freetz.org/trunk@7143 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/195-webmenu_signed.sh
+++ patches/195-webmenu_signed.sh
@@ -1,15 +1,9 @@
 [ "$FREETZ_PATCH_SIGNED" == "y" ] || return 0
 echo1 "applying webmenu signed patch"
 
-if isFreetzType LANG_DE; then
+if isFreetzType LANG_DE LANG_A_CH; then
 	if isFreetzType 3270 3270_V3 7240 7270 7320 7390 || ( isFreetzType 7170 && isFreetzType LABOR_PREVIEW); then
 		modsed "s/^g_coninf_data.FirmwareSigned = .*/g_coninf_data.FirmwareSigned = \"1\"/g" ${FILESYSTEM_MOD_DIR}/usr/www/all/home/home.lua
-	else
-		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/all/html/de/home/home.js
-	fi
-elif isFreetzType LANG_A_CH; then
-	if isFreetzType 7270; then
-		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/html/de/home/home.js
 	else
 		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/all/html/de/home/home.js
 	fi
