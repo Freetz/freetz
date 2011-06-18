2aeab0560f547a88814e823895151c05c8de1e70
Gene Rudoy <gene@freetz.org>
2011-06-18 10:06:47 +0000

    gw6:  * rework all patches and the makefile, remove all the mess with explicit linking against uClibc++/libstdc++  * add missing dependency on pthread to menuconfig
    git-svn-id: http://svn.freetz.org/trunk@7144 f5190166-0702-4917-9039-51ec32eddaf5

--- make/gw6/gw6.mk
+++ make/gw6/gw6.mk
@@ -2,21 +2,18 @@ $(call PKG_INIT_BIN, 5.1)
 $(PKG)_SOURCE:=gw6c-$($(PKG)_VERSION).tar.bz2
 #$(PKG)_SITE:=http://go6.net/4105/file.asp?file_id=150
 $(PKG)_SITE:=http://freetz.magenbrot.net
+$(PKG)_SOURCE_MD5:=eeac7292a622681651ec3bd9b2e5b061
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/tspc-advanced/bin/gw6c
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/gw6c
-$(PKG)_SOURCE_MD5:=eeac7292a622681651ec3bd9b2e5b061
 
 $(PKG)_DEPENDS_ON := uclibcxx
 
-GW6C_OPTS:= \
-	C_COMPILER="$(TARGET_CC) -c -DNO_STDLIBCXX" \
-	COMPILER="$(TARGET_CROSS)g++ -c -DNO_STDLIBCXX" \
-	CPP_FLAGS="$(TARGET_CFLAGS) -fno-builtin -nostdinc++ -I. -Wall -DNDEBUG \
-		-I$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/uClibc++" \
-	RANLIB="$(TARGET_RANLIB)" \
-	ARCHIVER="$(TARGET_AR)" \
-	C_LINKER="$(TARGET_CC)" \
-	LINKER="$(TARGET_CROSS)g++"
+$(PKG)_MAKE_DEFINES += CC="$(TARGET_CC)"
+$(PKG)_MAKE_DEFINES += CXX="$(TARGET_CXX)"
+$(PKG)_MAKE_DEFINES += RANLIB="$(TARGET_RANLIB)"
+$(PKG)_MAKE_DEFINES += ARCHIVER="$(TARGET_AR)"
+$(PKG)_MAKE_DEFINES += ADDITIONAL_CPPFLAGS="$(TARGET_CFLAGS) -DNO_STDLIBCXX"
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -24,13 +21,11 @@ $(PKG_CONFIGURED_NOP)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(GW6_DIR)/gw6c-config \
-		$(GW6C_OPTS)
+		$(GW6_MAKE_DEFINES)
 	$(SUBMAKE) -C $(GW6_DIR)/gw6c-messaging \
-		$(GW6C_OPTS)
+		$(GW6_MAKE_DEFINES)
 	$(SUBMAKE) -C $(GW6_DIR)/tspc-advanced \
-		CC="$(TARGET_CC)" \
-		CFLAGS="$(TARGET_CFLAGS) -fno-builtin -Wall -I../../include -I../../platform/openwrt -I../.." \
-		LDFLAGS="-L../../gw6cconfig -L../../gw6cmessaging -nodefaultlibs -luClibc++ -lgcc_s -lpthread" \
+		$(GW6_MAKE_DEFINES) \
 		target="openwrt"
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
@@ -41,11 +36,11 @@ $(pkg):
 $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
 $(pkg)-clean:
-	-$(SUBMAKE) -C $(GW6_DIR)/tspc-advanced target=openwrt clean
-	-$(SUBMAKE) -C $(GW6_DIR)/gw6c-config target=openwrt clean
-	-$(SUBMAKE) -C $(GW6_DIR)/gw6c-messaging target=openwrt clean
+	-$(SUBMAKE) -C $(GW6_DIR)/tspc-advanced $(GW6_MAKE_DEFINES) target=openwrt clean
+	-$(SUBMAKE) -C $(GW6_DIR)/gw6c-messaging $(GW6_MAKE_DEFINES) clean
+	-$(SUBMAKE) -C $(GW6_DIR)/gw6c-config $(GW6_MAKE_DEFINES) clean
 
 $(pkg)-uninstall:
-	$(RM) $($(PKG)_TARGET_BINARY)
+	$(RM) $(GW6_TARGET_BINARY)
 
 $(PKG_FINISH)
