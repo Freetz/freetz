d4e9ec5e886aa0ce50dcef041a1a088ce8483a76
Gene Rudoy <gene@freetz.org>
2011-06-19 14:17:11 +0000

    libdnet:  * bump version to svn revision 665, required by nmap 5.51  * backport some nmap fixes  * refs #728
    git-svn-id: http://svn.freetz.org/trunk@7155 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/libdnet.mk
+++ make/libs/libdnet.mk
@@ -1,8 +1,10 @@
-$(call PKG_INIT_LIB, 1.12)
+$(call PKG_INIT_LIB, 665)
 $(PKG)_LIB_VERSION:=1.0.1
-$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tgz
-$(PKG)_SOURCE_MD5:=9253ef6de1b5e28e9c9a62b882e44cc9
-$(PKG)_SITE:=http://libdnet.googlecode.com/files
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SOURCE_MD5:=66b935e6a4a3fdb6e77b3717e94ebed3
+# release URL
+# $(PKG)_SITE:=http://libdnet.googlecode.com/files
+$(PKG)_SITE:=svn@http://libdnet.googlecode.com/svn/trunk
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/.libs/$(pkg).so.$($(PKG)_LIB_VERSION)
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/$(pkg).so.$($(PKG)_LIB_VERSION)
@@ -12,12 +14,7 @@ $(PKG)_CONFIGURE_OPTIONS += --with-python=no
 $(PKG)_CONFIGURE_OPTIONS += --with-wpdpack=no
 $(PKG)_CONFIGURE_OPTIONS += --with-check=no
 
-# NB: libtool shipped with libdnet is broken
-# it defines shared_ext (later used in soname_spec and lib) to be the value of shrext_cmds, which is however never defined
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
-$(PKG)_CONFIGURE_PRE_CMDS += $(SED) -i -r -e "s,(shrext=('[$$$$]shrext')),\1\nshrext_cmds=\2,g;" ./configure;
-# better/preferred solution, to be used as soon as automake-1.10 related libtool-host problems are solved
-#$(call REPLACE_LIBTOOL)
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -47,8 +44,7 @@ $(pkg)-clean:
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libdnet* \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/dnet \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/dnet.h \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/dnet-config \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/sbin/dnet
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/dnet-config
 
 $(pkg)-uninstall:
 	$(RM) $(LIBDNET_TARGET_DIR)/libdnet*
