97e864adb2da5f4a3da12ca85eaee21a22ea8181
Gene Rudoy <gene@freetz.org>
2011-06-20 20:21:32 +0000

    * added new package netatalk, by svoop & dg1kjd  * fixes #671
    git-svn-id: http://svn.freetz.org/trunk@7166 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/netatalk/files/root/usr/lib/cgi-bin/netatalk.cgi
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+PATH=/bin:/usr/bin:/sbin:/usr/sbin
+. /usr/lib/libmodcgi.sh
+
+check "$NETATALK_ENABLED" yes:auto "*":man
+select "$NETATALK_LOG_LEVEL" \
+  LOG_SEVERE:logsevere \
+  LOG_ERROR:logerror \
+  LOG_WARN:logwarn \
+  LOG_INFO:loginfo \
+  LOG_DEBUG:logdebug \
+  LOG_MAXDEBUG:logmaxdebug \
+	"*":lognote
+
+sec_begin '$(lang de:"Starttyp" en:"Start type")'
+
+cat << EOF
+<p>
+<input id='e1' type='radio' name='enabled' value='yes'$auto_chk><label for='e1'>$(lang de:"Automatisch" en:"Automatic")</label>
+<input id='e2' type='radio' name='enabled' value='no'$man_chk><label for='e2'>$(lang de:"Manuell" en:"Manual")</label>
+</p>
+EOF
+
+sec_end
+sec_begin '$(lang de:"Einstellungen" en:"Settings")'
+
+cat << EOF
+<ul>
+<li><a href='$(href file netatalk applevolumes_default)'>$(lang de:"Freigaben" en:"Shares") (AppleVolumes.default)</a></li>
+<li><a href='$(href file netatalk afpd_conf)'>$(lang de:"Virtuelle Datei-Server" en:"Virtual fileservers") (afpd.conf)</a></li>
+</ul>
+EOF
+
+sec_end
+sec_begin '$(lang de:"Benutzer" en:"Users")'
+
+cat << EOF
+<p>
+$(lang de:"Benutzer werden wie folgt angelegt und persistent gespeichert" en:"Create and persistently save users as follows"):
+</p>
+<ol>
+<li>adduser -g 'Homer Simpson' hsimpson</li>
+<li>modusers save</li>
+<li>modsave flash</li>
+</ol>
+EOF
+
+sec_end
+sec_begin '$(lang de:"Netatalk" en:"Netatalk")'
+
+cat << EOF
+<p>
+<label for='max_clients'>$(lang de:"Maximale Anzahl Verbindungen" en:"Maximum number of clients"): </label>
+<input name='max_clients' type='text' size='4' maxlength='3' value='$(html "$NETATALK_MAX_CLIENTS")'>
+</p>
+
+<p>
+<label for='log_level'>$(lang de:"Log-Level" en:"Log level"): </label>
+<select name='log_level' id='log_level'>
+<option $logsevere_sel>LOG_SEVERE</option>
+<option $logerror_sel>LOG_ERROR</option>
+<option $logwarn_sel>LOG_WARN</option>
+<option $lognote_sel>LOG_NOTE</option>
+<option $loginfo_sel>LOG_INFO</option>
+<option $logdebug_sel>LOG_DEBUG</option>
+<option $logmaxdebug_sel>LOG_MAXDEBUG</option>
+</select>
+</p>
+EOF
+
+sec_end
---
+++ make/netatalk/netatalk.mk
@@ -0,0 +1,73 @@
+$(call PKG_INIT_BIN, 2.1.5)
+$(PKG)_SOURCE := $(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5 := 6034d3f2206e4cc85426e7088b439c97
+$(PKG)_SITE := @SF/netatalk
+
+$(PKG)_LIBS := uams_guest uams_dhx2_passwd
+$(PKG)_LIBS_BUILD_DIR := $($(PKG)_LIBS:%=$($(PKG)_DIR)/etc/uams/.libs/%.so)
+$(PKG)_LIBS_TARGET_DIR := $($(PKG)_LIBS:%=$($(PKG)_DEST_LIBDIR)/%.so)
+
+$(PKG)_BINS_AFPD := afpd hash
+$(PKG)_BINS_AFPD_BUILD_DIR := $($(PKG)_BINS_AFPD:%=$($(PKG)_DIR)/etc/afpd/%)
+$(PKG)_BINS_AFPD_TARGET_DIR := $($(PKG)_BINS_AFPD:%=$($(PKG)_DEST_DIR)/sbin/%)
+
+$(PKG)_BINS_DBD := cnid_dbd cnid_metad dbd
+$(PKG)_BINS_DBD_BUILD_DIR := $($(PKG)_BINS_DBD:%=$($(PKG)_DIR)/etc/cnid_dbd/%)
+$(PKG)_BINS_DBD_TARGET_DIR := $($(PKG)_BINS_DBD:%=$($(PKG)_DEST_DIR)/sbin/%)
+
+$(PKG)_DEPENDS_ON := db libgcrypt
+
+$(PKG)_PTHREAD_LDFLAGS := -lpthread
+
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
+
+$(PKG)_CONFIGURE_OPTIONS += --disable-a2boot
+$(PKG)_CONFIGURE_OPTIONS += --disable-afs
+$(PKG)_CONFIGURE_OPTIONS += --disable-cups
+$(PKG)_CONFIGURE_OPTIONS += --disable-ddp
+$(PKG)_CONFIGURE_OPTIONS += --disable-srvloc
+$(PKG)_CONFIGURE_OPTIONS += --disable-timelord
+$(PKG)_CONFIGURE_OPTIONS += --disable-admin-group
+$(PKG)_CONFIGURE_OPTIONS += --disable-shell-check
+$(PKG)_CONFIGURE_OPTIONS += --disable-tcp-wrappers
+$(PKG)_CONFIGURE_OPTIONS += --with-cnid-default-backend=dbd
+$(PKG)_CONFIGURE_OPTIONS += --with-cnid-dbd-backend
+$(PKG)_CONFIGURE_OPTIONS += --with-cnid-tdb-backend
+$(PKG)_CONFIGURE_OPTIONS += --without-cnid-cdb-backend
+$(PKG)_CONFIGURE_OPTIONS += --without-cnid-last-backend
+$(PKG)_CONFIGURE_OPTIONS += --with-uams-path="$(FREETZ_LIBRARY_PATH)"
+$(PKG)_CONFIGURE_OPTIONS += --with-bdb="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+$(PKG)_CONFIGURE_OPTIONS += --with-libgcrypt-dir="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+$(PKG)_CONFIGURE_OPTIONS += --sysconfdir="/mod/etc"
+$(PKG)_CONFIGURE_OPTIONS += --disable-debugging
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_LIBS_BUILD_DIR) $($(PKG)_BINS_AFPD_BUILD_DIR) $($(PKG)_BINS_DBD_BUILD_DIR): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(NETATALK_DIR) \
+	LDFLAGS="$(TARGET_LDFLAGS) $(NETATALK_PTHREAD_LDFLAGS)"
+
+$($(PKG)_LIBS_TARGET_DIR): $($(PKG)_DEST_LIBDIR)/%: $($(PKG)_DIR)/etc/uams/.libs/%
+	$(INSTALL_LIBRARY_STRIP)
+	ln -sf uams_dhx2_passwd.so $(NETATALK_DEST_LIBDIR)/uams_dhx2.so
+
+$($(PKG)_BINS_AFPD_TARGET_DIR): $($(PKG)_DEST_DIR)/sbin/%: $($(PKG)_DIR)/etc/afpd/%
+	$(INSTALL_BINARY_STRIP)
+
+$($(PKG)_BINS_DBD_TARGET_DIR): $($(PKG)_DEST_DIR)/sbin/%: $($(PKG)_DIR)/etc/cnid_dbd/%
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_LIBS_TARGET_DIR) $($(PKG)_BINS_AFPD_TARGET_DIR) $($(PKG)_BINS_DBD_TARGET_DIR)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(NETATALK_DIR) clean
+	$(RM) $(NETATALK_FREETZ_CONFIG_FILE)
+
+$(pkg)-uninstall:
+	$(RM) $(NETATALK_LIBS_TARGET_DIR) $(NETATALK_BINS_AFPD_TARGET_DIR) $(NETATALK_BINS_DBD_TARGET_DIR)
+
+$(PKG_FINISH)
