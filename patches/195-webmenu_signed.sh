1fc8d5f97404c5ee4cbd574a52196e0964c7b6f3
Oliver Metz <oliver@freetz.org>
2011-06-21 12:45:42 +0000

    * 7170: Bump firmware version to 29.04.87 (not finished yet)   * Update patches   * Disable replace kernel (updated kernel source missing)   * Alien variants untested
    git-svn-id: http://svn.freetz.org/trunk@7169 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/195-webmenu_signed.sh
+++ patches/195-webmenu_signed.sh
@@ -2,15 +2,15 @@
 echo1 "applying webmenu signed patch"
 
 if isFreetzType LANG_DE LANG_A_CH; then
-	if isFreetzType 3270 3270_V3 7240 7270 7320 7390 || ( isFreetzType 7170 && isFreetzType LABOR_PREVIEW); then
+	if isFreetzType 3270 3270_V3 7112 7170 7240 7270 7320 7390; then
 		modsed "s/^g_coninf_data.FirmwareSigned = .*/g_coninf_data.FirmwareSigned = \"1\"/g" ${FILESYSTEM_MOD_DIR}/usr/www/all/home/home.lua
 	else
 		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/all/html/de/home/home.js
 	fi
 elif isFreetzType LANG_EN; then
-	if isFreetzType 7340 7390; then
+	if isFreetzType 7270 7340 7390 7570; then
 		modsed "s/^g_coninf_data.FirmwareSigned = .*/g_coninf_data.FirmwareSigned = \"1\"/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/home/home.lua
-	elif isFreetzType 5124 7113 7140 7170 7270 7570; then
+	elif isFreetzType 5124 7113 7140 7170; then
 		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/html/de/home/home.js
 	else
 		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/en/html/en/home/home.js
