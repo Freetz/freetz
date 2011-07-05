2d843c166078f90dca64a4f9f4682ef48c2a44bc
Oliver Metz <oliver@freetz.org>
2011-07-05 13:18:58 +0000

    * iptables: Bump to v1.4.11.1   * refs #1393   * needs getaddrinfo fix for udp with port numbers   * Credits go to: MaxMuster, markuschen, Whoopie, abraxxl and dileks
    git-svn-id: http://svn.freetz.org/trunk@7255 f5190166-0702-4917-9039-51ec32eddaf5

--- make/iptables/iptables.mk
+++ make/iptables/iptables.mk
@@ -1,34 +1,49 @@
-$(call PKG_INIT_BIN, 1.4.1.1)
+$(call PKG_INIT_BIN, 1.4.11.1)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=723fa88d8a0915e184f99e03e9bf06cb
+$(PKG)_SOURCE_MD5:=7de6e1ae7ed8a2025f184763a6a24b9a
 $(PKG)_SITE:=http://netfilter.org/projects/$(pkg)/files
 $(PKG)_CONDITIONAL_PATCHES+=$(KERNEL_VERSION)
-ifneq ($(FREETZ_TARGET_IPV6_SUPPORT),y)
-$(PKG)_CONDITIONAL_PATCHES+=cond
-endif
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_KERNEL_VERSION
 $(PKG)_REBUILD_SUBOPTS += FREETZ_TARGET_IPV6_SUPPORT
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_IPTABLES_SAVE_RESTORE
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_IPTABLES_XML
 
-$(PKG)_BINARIES_ALL := iptables-multi ip6tables-multi
-$(PKG)_BINARIES := $(filter-out $(if $(FREETZ_TARGET_IPV6_SUPPORT),,ip6%), $($(PKG)_BINARIES_ALL))
-$(PKG)_BINARIES_BUILD_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DIR)/%)
-$(PKG)_BINARIES_TARGET_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DEST_DIR)/usr/sbin/%)
+$(PKG)_BINARY := $($(PKG)_DIR)/iptables/.libs/xtables-multi
+$(PKG)_TARGET_BINARY := $($(PKG)_DEST_DIR)/usr/sbin/xtables-multi
 
-$(PKG)_SYMLINKS_ALL := iptables iptables-save iptables-restore ip6tables ip6tables-save ip6tables-restore
-$(PKG)_SYMLINKS := $(filter-out $(if $(FREETZ_TARGET_IPV6_SUPPORT),,ip6%) $(if $(FREETZ_PACKAGE_IPTABLES_SAVE_RESTORE),,%-save %-restore), $($(PKG)_SYMLINKS_ALL))
+$(PKG)_LIBS_SUBDIRS := libiptc/.libs/
+$(PKG)_LIBS_SUBDIRS += iptables/.libs/
+ifeq ($(strip $(FREETZ_TARGET_IPV6_SUPPORT)),y)
+$(PKG)_LIBS_SUBDIRS += libiptc/.libs/
+endif
+
+$(PKG)_LIBNAMES_ALL := libip4tc.so.0.0.0 libxtables.so.6.0.0 libip6tc.so.0.0.0
+$(PKG)_LIBNAMES := $(filter-out $(if $(FREETZ_TARGET_IPV6_SUPPORT),,libipip6tc%), $($(PKG)_LIBNAMES_ALL))
+$(PKG)_LIBS_BUILD_DIR := $(addprefix $($(PKG)_DIR)/,$(join $($(PKG)_LIBS_SUBDIRS),$($(PKG)_LIBNAMES)))
+$(PKG)_LIBS_TARGET_DIR := $(addprefix $($(PKG)_DEST_LIBDIR)/,$($(PKG)_LIBNAMES))
+
+$(PKG)_SYMLINKS_ALL := iptables iptables-save iptables-restore iptables-xml ip6tables ip6tables-save ip6tables-restore
+$(PKG)_SYMLINKS := $(filter-out $(if $(FREETZ_TARGET_IPV6_SUPPORT),,ip6%) $(if $(FREETZ_PACKAGE_IPTABLES_SAVE_RESTORE),,%-save %-restore) $(if $(FREETZ_PACKAGE_IPTABLES_XML),,%-xml), $($(PKG)_SYMLINKS_ALL))
 $(PKG)_SYMLINKS_TARGET_DIR := $($(PKG)_SYMLINKS:%=$($(PKG)_DEST_DIR)/usr/sbin/%)
 
-$(PKG)_NOT_INCLUDED := $(patsubst %,$($(PKG)_DEST_DIR)/usr/sbin/%, $(filter-out $($(PKG)_BINARIES),$($(PKG)_BINARIES_ALL)) $(filter-out $($(PKG)_SYMLINKS),$($(PKG)_SYMLINKS_ALL)))
+$(PKG)_EXTENSIONS_DIR := $($(PKG)_DIR)/extensions
+$(PKG)_EXTENSIONS_TARGET_DIR := $($(PKG)_DEST_DIR)/usr/lib/xtables
 
-$(PKG)_LIB_BUILD_DIR:=$($(PKG)_DIR)/libiptc/libiptc.a
-$(PKG)_LIB_STAGING_DIR:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libiptc.a
-$(PKG)_EXTENSIONS_DIR:=$(TARGET_SPECIFIC_ROOT_DIR)/usr/lib/xtables
-$(PKG)_TARGET_EXTENSIONS:=$($(PKG)_EXTENSIONS_DIR)/.installed
 
 $(PKG)_CONFIGURE_ENV += AR="$(TARGET_AR)"
 $(PKG)_CONFIGURE_ENV += RANLIB="$(TARGET_RANLIB)"
 $(PKG)_CONFIGURE_OPTIONS += --with-ksource="$(FREETZ_BASE_DIR)/$(KERNEL_SOURCE_DIR)"
+$(PKG)_CONFIGURE_OPTIONS += --enable-ipv4
+ifneq ($(strip $(FREETZ_TARGET_IPV6_SUPPORT)),y)
+$(PKG)_CONFIGURE_OPTIONS += --disable-ipv6
+endif
+ifeq ($(strip $(FREETZ_PACKAGE_IPTABLES_SAVE_RESTORE)),y)
+$(PKG)_CONFIGURE_OPTIONS += --enable-save-restore
+endif
+ifeq ($(strip $(FREETZ_PACKAGE_IPTABLES_XML)),y)
+$(PKG)_CONFIGURE_OPTIONS += --enable-xml
+endif
 
 $(PKG)_CONFIGURE_PRE_CMDS += $(SED) -i -r -e 's,ac_cv(_header_linux),do_not_cache_me\1,g' configure;
 
@@ -36,17 +51,14 @@ $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
-$($(PKG)_BINARIES_BUILD_DIR) $($(PKG)_LIB_BUILD_DIR): $($(PKG)_DIR)/.configured | kernel-source
+$($(PKG)_LIBS_BUILD_DIR) $($(PKG)_BINARY): $($(PKG)_DIR)/.configured | kernel-source
 	$(SUBMAKE) -C $(IPTABLES_DIR)
 
-$($(PKG)_LIB_STAGING_DIR): $($(PKG)_LIB_BUILD_DIR)
-	$(INSTALL_FILE)
-
-$($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/sbin/%: $($(PKG)_DIR)/%
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
 
 define $(PKG)_SYMLINK_RULE
-$$(filter $($(PKG)_DEST_DIR)/usr/sbin/$(1)%,$($(PKG)_SYMLINKS_TARGET_DIR)): $($(PKG)_DEST_DIR)/usr/sbin/$(1)-multi
+$$(filter $($(PKG)_DEST_DIR)/usr/sbin/$(1)%,$($(PKG)_SYMLINKS_TARGET_DIR)): $($(PKG)_TARGET_BINARY)
 	ln -sf $$(notdir $$<) $$@
 endef
 $(eval $(call $(PKG)_SYMLINK_RULE,iptables))
@@ -54,22 +66,26 @@ ifeq ($(FREETZ_TARGET_IPV6_SUPPORT),y)
 $(eval $(call $(PKG)_SYMLINK_RULE,ip6tables))
 endif
 
-$($(PKG)_TARGET_EXTENSIONS): $($(PKG)_BINARIES_BUILD_DIR)
+$($(PKG)_TARGET_EXTENSIONS): $($(PKG)_BINARY)
 	mkdir -p $(IPTABLES_EXTENSIONS_DIR)
-	cp $(IPTABLES_DIR)/extensions/*.so $(IPTABLES_EXTENSIONS_DIR)/
-	$(TARGET_STRIP) $(IPTABLES_EXTENSIONS_DIR)/*.so
+	cp $(IPTABLES_EXTENSIONS_DIR)/*.so* $(IPTABLES_EXTENSIONS_TARGET_DIR)
+	$(TARGET_STRIP) $(IPTABLES_EXTENSIONS_DIR)/*.so*
 	touch $@
 
-$(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR) $($(PKG)_SYMLINKS_TARGET_DIR) $($(PKG)_TARGET_EXTENSIONS) $($(PKG)_LIB_STAGING_DIR)
+$(foreach library,$($(PKG)_LIBS_BUILD_DIR),$(eval $(call INSTALL_LIBRARY_STRIP_RULE,$(library),$(FREETZ_LIBRARY_PATH))))
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_LIBS_TARGET_DIR) $($(PKG)_BINARY) $($(PKG)_SYMLINKS_TARGET_DIR) $($(PKG)_TARGET_EXTENSIONS)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(IPTABLES_DIR) clean
 	$(RM) $(IPTABLES_LIB_STAGING_DIR)
 
 $(pkg)-uninstall:
-	$(RM) \
-		$(IPTABLES_BINARIES_ALL:%=$(IPTABLES_DEST_DIR)/usr/sbin/%) \
-		$(IPTABLES_SYMLINKS_ALL:%=$(IPTABLES_DEST_DIR)/usr/sbin/%) \
-		$(IPTABLES_EXTENSIONS_DIR)/*
+	$(RM) -r \
+		$(IPTABLES_TARGET_BINARY) \
+		$(IPTABLES_DEST_LIBDIR)/lib* \
+		$(IPTABLES_EXTENSIONS_TARGET_DIR)
 
 $(PKG_FINISH)
