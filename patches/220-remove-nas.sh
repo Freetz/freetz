71cd5f1b0992fed8249092b2db72816314ae2853
Oliver Metz <oliver@freetz.org>
2011-07-11 21:48:59 +0000

    * remove nas patch:   * Revert r7304   * Apply patch only if necessary   * Rename patch to reflect that it is used for more than one box   * fixes #1404
    git-svn-id: http://svn.freetz.org/trunk@7319 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/220-remove-nas.sh
+++ patches/220-remove-nas.sh
@@ -5,12 +5,11 @@ rm -rf "${FILESYSTEM_MOD_DIR}/usr/www.nas"
 ln -sf www "${FILESYSTEM_MOD_DIR}/usr/www.nas"
 
 modsed "s/CONFIG_NAS.*/CONFIG_NAS=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
-# Labor Preview has "if (config.NAS)" in menu_page_head.html
+# 7240, 7270 and 7390 Labor Preview have "if (config.NAS) in menu_page_head.html
 # so there is no need to patch
-if ! isFreetzType LABOR_PREVIEW; then
+if isFreetzType 3270 3270_V3 7320 7340 || (isFreetzType 7390 && ! isFreetzType LABOR_PREVIEW); then
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/remove_nas_7270.patch"
 fi
 
 echo2 "removing internal memory"
 rm -rf ${FILESYSTEM_MOD_DIR}/etc/internal_memory_default*.tar
-
