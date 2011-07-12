0d067536b49c189e444d83bd164260904507082f
Gene Rudoy <gene@freetz.org>
2011-07-12 22:44:47 +0000

    tmux:  * bump version to 1.5
    git-svn-id: http://svn.freetz.org/trunk@7332 f5190166-0702-4917-9039-51ec32eddaf5

--- make/tmux/tmux.mk
+++ make/tmux/tmux.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 1.4)
+$(call PKG_INIT_BIN, 1.5)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=0bfc7dd9a5bab192406167589c716a21
+$(PKG)_SOURCE_MD5:=3d4b683572af34e83bc8b183a8285263
 $(PKG)_SITE:=@SF/$(pkg)
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/tmux
@@ -10,21 +10,15 @@ $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_TMUX_STATIC
 
 $(PKG)_DEPENDS_ON := ncurses libevent
 
-$(PKG)_CFLAGS := -std=c99 -D_GNU_SOURCE -D_POSIX_SOURCE -DBUILD="\\\"$($(PKG)_VERSION)\\\""
-
-ifeq ($(strip $(FREETZ_PACKAGE_TMUX_STATIC)),y)
-$(PKG)_LDFLAGS += -static
-endif
+$(PKG)_CONFIGURE_ENV += ac_cv_search_event_init="-lpthread -levent"
+$(PKG)_CONFIGURE_OPTIONS += --enable-static=$(if $(FREETZ_PACKAGE_TMUX_STATIC),yes,no)
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(TMUX_DIR) \
-		CC="$(TARGET_CC)" \
-		CFLAGS="$(TARGET_CFLAGS) $(TMUX_CFLAGS)" \
-		LDFLAGS="$(TMUX_LDFLAGS)"
+	$(SUBMAKE) -C $(TMUX_DIR)
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
