ee533e7e0d5ac41287bf47d30b7bca301f3356b2
Gene Rudoy <gene@freetz.org>
2011-07-16 20:28:20 +0000

    debootstrap:  * convert it to LOCALSOURCE-package  * fixes sporadic unnecessary rebuilds  * refs #1227
    git-svn-id: http://svn.freetz.org/trunk@7340 f5190166-0702-4917-9039-51ec32eddaf5

--- make/debootstrap/debootstrap.mk
+++ make/debootstrap/debootstrap.mk
@@ -10,20 +10,16 @@ $(call PKG_INIT_BIN, 1.0.28)
 #$(PKG)_SITE:=http://ftp.de.debian.org/debian/pool/main/d/debootstrap
 #$(PKG)_SOURCE_MD5:=f8172809afe7cfdcdc6745229f024d9d
 
-$(PKG)_BINARY_SOURCE:=$($(PKG)_MAKE_DIR)/src/pkgdetails.c
 $(PKG)_BINARY:=$($(PKG)_DIR)/pkgdetails
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/lib/debootstrap/pkgdetails
 
-
-#$(PKG_LOCALSOURCE_PACKAGE)
-$(PKG_UNPACKED)
+$(PKG_LOCALSOURCE_PACKAGE)
 $(PKG_CONFIGURED_NOP)
 
-$($(PKG)_BINARY): $($(PKG)_BINARY_SOURCE) $($(PKG)_DIR)/.configured
-	mkdir -p $(DEBOOTSTRAP_DIR)
-	cp -a $< $(DEBOOTSTRAP_DIR)
-	cd $(DEBOOTSTRAP_DIR) && $(TARGET_CONFIGURE_ENV) \
-		$(TARGET_CC) $(TARGET_CFLAGS) -o pkgdetails pkgdetails.c
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(DEBOOTSTRAP_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)"
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
