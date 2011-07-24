6920a4f1ad0079841e5b5d04fc8bdab6dff15590
Oliver Metz <oliver@freetz.org>
2011-07-24 08:23:31 +0000

    * remove mediasrv patch: Fix segfault on 7240, 7270 and 7390 caused by missing libsqlite (refs #1391)
    git-svn-id: http://svn.freetz.org/trunk@7394 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/500-remove-mediasrv.sh
+++ patches/500-remove-mediasrv.sh
@@ -21,5 +21,10 @@ modsed "/^<p.*uiViewUseMusik.*<\/p>$/ {
 	D }" "${FILESYSTEM_MOD_DIR}/usr/www/all/html/de/nas/einstellungen.html"
 fi
 
+if isFreetzType 7240 7270_V2 7270_V3 || (isFreetzType 7390 && isFreetzType LABOR_PREVIEW); then
+	# see http://freetz.org/ticket/1391 for details
+	modsed "s/call_webusb.call_webusb_func(\"scan_info\".*)/\"\" -- &/g" "${FILESYSTEM_MOD_DIR}/usr/www/all/storage/settings.lua"
+fi
+
 echo1 "patching rc.conf"
 modsed "s/CONFIG_MEDIASRV=.*$/CONFIG_MEDIASRV=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
