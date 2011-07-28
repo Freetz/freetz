085e16de5966a1d2ac67b6c11cdc024c591f49ff
Oliver Metz <oliver@freetz.org>
2011-07-28 17:50:56 +0000

    * 7112 Alien patch: Remove more usb files (by abraxxl)
    git-svn-id: http://svn.freetz.org/trunk@7420 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/100-7112_7170.sh
+++ patches/100-7112_7170.sh
@@ -13,10 +13,20 @@ files+=" drivers/isdn/isdn_fon4 drivers/char/Piglet"
 for i in $files; do
 	rm_files "${FILESYSTEM_MOD_DIR}/lib/modules/2.6.13.1-ohio/kernel/$i"
 done
+
+# no internal S0
 rm_files "${FILESYSTEM_MOD_DIR}/lib/modules/microvoip_isdn_top.bit*"
-rm_files "${FILESYSTEM_MOD_DIR}/usr/share/ctlmgr/libctlusb.so"
-rm_files "${FILESYSTEM_MOD_DIR}/lib/libusbcfg*"
-rm_files "${FILESYSTEM_MOD_DIR}/etc/hotplug"
+# USB
+for i in bin/pause bin/reinit_jffs2 \
+	bin/*usb* sbin/*usb* usr/bin/*usb* usr/sbin/*usb* \
+	etc/hotplug etc/samba_control \
+	etc/default.*/*/*usb* etc/init.d/rc.*usb* etc/*usb*.tab \
+	lib/lib*usb*.so* usr/share/*/lib*usb*.so* \
+	usr/www/all/*usb* usr/www/all/html/*usb* usr/www/all/html/*/*usb* usr/www/all/html/*/*/*usb* ; do
+		rm_files "${FILESYSTEM_MOD_DIR}/$i"
+done
+
+
 
 echo2 "copying 7112 files"
 cp -a "${DIR}/.tk/original/filesystem/lib/modules/bitfile.bit" "${FILESYSTEM_MOD_DIR}/lib/modules/"
