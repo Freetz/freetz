da9c311eeae2481196dba9c9750f123e26c31c9a
Oliver Metz <oliver@freetz.org>
2011-08-10 06:16:13 +0000

    * 7390: Bump firmware to version 84.05.05   * refs #1233
    git-svn-id: http://svn.freetz.org/trunk@7461 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/500-remove-mediasrv.sh
+++ patches/500-remove-mediasrv.sh
@@ -24,7 +24,7 @@ modsed "/^<p.*uiViewUseMusik.*<\/p>$/ {
 	D }" "${FILESYSTEM_MOD_DIR}/usr/www/all/html/de/nas/einstellungen.html"
 fi
 
-if isFreetzType 7240 7270_V2 7270_V3 || (isFreetzType 7390 && isFreetzType LABOR_PREVIEW); then
+if isFreetzType 7240 7270_V2 7270_V3 || (isFreetzType 7390 && isFreetzType LANG_DE); then
 	# see http://freetz.org/ticket/1391 for details
 	modsed "s/call_webusb.call_webusb_func(\"scan_info\".*)/\"\" -- &/g" "${FILESYSTEM_MOD_DIR}/usr/www/all/storage/settings.lua"
 fi
