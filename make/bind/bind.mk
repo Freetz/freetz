0ee3cf2c50efa34d6c4821e7cb43593cb13c4529
Gene Rudoy <gene@freetz.org>
2011-08-17 07:09:44 +0000

    bind:  * bump version to 9.8.0-P4
    git-svn-id: http://svn.freetz.org/trunk@7483 f5190166-0702-4917-9039-51ec32eddaf5

--- make/bind/bind.mk
+++ make/bind/bind.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_BIN, 9.8.0-P2)
+$(call PKG_INIT_BIN, 9.8.0-P4)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=http://ftp.isc.org/isc/bind9/$($(PKG)_VERSION)
-$(PKG)_SOURCE_MD5:=5f90dcf1730631feacdad3257c465c54
+$(PKG)_SOURCE_MD5:=13e36e3f28dc1ce7675b45a4192e8389
 
 $(PKG)_STARTLEVEL=40 # multid-wrapper may start it earlier!
 
