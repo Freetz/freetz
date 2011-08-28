cb1eb78ac39bda9430ca5af80a4f4631fa6755d6
Oliver Metz <oliver@freetz.org>
2011-08-28 21:15:58 +0000

    * usbroot: rediscover devices reworked for udev (by schlimmchen)   * refs #1456
    git-svn-id: http://svn.freetz.org/trunk@7552 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ patches/116-usbroot_S46-usb.sh
@@ -0,0 +1,29 @@
+[ "$FREETZ_PACKAGE_USBROOT" == "y" ] || return 0
+
+usb_init_file="${FILESYSTEM_MOD_DIR}/etc/init.d/S46-usb"
+
+[ -x "$usb_init_file" ] || return 0
+
+cat << EOF >> "$usb_init_file"
+
+###########################
+####  Freetz  USBROOT  ####
+###########################
+if [ "\$(/etc/init.d/rc.usbroot status)" = "running" ]; then
+	if [ -n "\$(which udevd)" ]; then
+		# This box employs the udevd, tell it about the
+		# missed "USB disk device plugged in" events.
+		echo "*** USBROOT: Recreating USB disk devices. ***"
+		device="\$(/etc/init.d/rc.usbroot store)"
+		device="\${device%%:*}"
+		device="\${device##*/}"
+		for path in /sys/block/sd[a-z] /sys/block/sd[a-z]/sd[a-z][0-9]
+		do
+			# add device only if it is not the usb root device
+			# so it is not mounted one more time
+			[ "\${path##*/}" = "\$device" ] || echo add > \$path/uevent
+		done
+	fi
+fi
+
+EOF
