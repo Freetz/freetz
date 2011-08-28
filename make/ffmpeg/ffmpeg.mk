5b3896d3a1cc2038a977cbb4969815bcba1bbff3
Gene Rudoy <gene@freetz.org>
2011-08-28 14:43:49 +0000

    ffmpeg:  * bump version to 0.8.2  * revise makefile, most changes are inspired by this openwrt commit https://dev.openwrt.org/changeset/28063
    mediatomb:
     * make it compile with the new ffmpeg version
    
    Note: minidlna requires no change in order to be compiled with the new ffmpeg version, thus it won't be recompiled automatically after this commit, users should do it by themselves (i.e. make minidlna-dirclean)
    
    
    git-svn-id: http://svn.freetz.org/trunk@7551 f5190166-0702-4917-9039-51ec32eddaf5

--- make/ffmpeg/ffmpeg.mk
+++ make/ffmpeg/ffmpeg.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 0.5.4)
+$(call PKG_INIT_BIN, 0.8.2)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=87e771cd0f5d465fbf1a0a4824b7cc24
+$(PKG)_SOURCE_MD5:=967d481c98161582d149aced6e3b2f31
 $(PKG)_SITE:=http://www.ffmpeg.org/releases
 
 $(PKG)_DEPENDS_ON := zlib
@@ -13,9 +13,9 @@ $(PKG)_BINARIES            := $(call PKG_SELECTED_SUBOPTIONS,$($(PKG)_BINARIES_A
 $(PKG)_BINARIES_BUILD_DIR  := $($(PKG)_BINARIES:%=$($(PKG)_DIR)/%)
 $(PKG)_BINARIES_TARGET_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DEST_DIR)/usr/bin/%)
 
-$(PKG)_LIBNAMES_SHORT      := avcodec avdevice avformat avutil postproc
-$(PKG)_LIBVERSIONS_MAJOR   := 52      52       52       49     51
-$(PKG)_LIBVERSIONS_MINOR   := 20.1    1.0      31.0     15.0   2.0
+$(PKG)_LIBNAMES_SHORT      := avcodec avdevice avfilter avformat avutil postproc swscale
+$(PKG)_LIBVERSIONS_MAJOR   := 53      53       2        53       51     51       2
+$(PKG)_LIBVERSIONS_MINOR   := 7.0     1.1      23.0     4.0      9.1    2.0      0.0
 
 $(PKG)_LIBNAMES_LONG_MAJOR := $(join $($(PKG)_LIBNAMES_SHORT:%=lib%.so.),$($(PKG)_LIBVERSIONS_MAJOR))
 $(PKG)_LIBNAMES_LONG       := $(join $($(PKG)_LIBNAMES_LONG_MAJOR:%=%.),$($(PKG)_LIBVERSIONS_MINOR))
@@ -24,66 +24,76 @@ $(PKG)_LIBS_STAGING_DIR    := $($(PKG)_LIBNAMES_LONG:%=$(TARGET_TOOLCHAIN_STAGIN
 $(PKG)_LIBS_TARGET_DIR     := $($(PKG)_LIBNAMES_LONG:%=$($(PKG)_TARGET_LIBDIR)/%)
 
 $(PKG)_ENCODERS  := ac3 jpegls mjpeg mpeg1video mpeg2video mpeg4 pcm_s16be pcm_s16le png vorbis zlib
-$(PKG)_DECODERS  := aac ac3 atrac3 gif h264 jpegls libopenjpeg mjpeg mjpegb mp2 mp3 mpeg1video mpeg2video mpeg4 mpeg4aac mpegvideo pcm_s16be pcm_s16le png vorbis wmav1 wmav2 zlib
-$(PKG)_MUXERS    := ac3 avi ffm flv h264 matroska mjpeg mp3 mp4 mpeg1video mpeg2video mpegts ogg oss rtp
+$(PKG)_DECODERS  := aac ac3 atrac3 gif h264 jpegls libopenjpeg mjpeg mjpegb mp2 mp3 mpeg1video mpeg2video mpeg4 mpegvideo pcm_s16be pcm_s16le png vorbis wmav1 wmav2 zlib
+$(PKG)_MUXERS    := ac3 avi ffm flv h264 matroska mjpeg mp3 mp4 mpeg1video mpeg2video mpegts ogg rtp
 $(PKG)_DEMUXERS  := ac3 avi ffm flv h264 image2 matroska mjpeg mp3 mpegps mpegts mpegvideo ogg rm rtsp sdp v4l2
 $(PKG)_PARSERS   := aac ac3 h264 mjpeg mpegaudio mpegvideo mpeg4video
 $(PKG)_PROTOCOLS := file http pipe rtp tcp udp
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_FFMPEG_ffmpeg
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_FFMPEG_ffserver
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_FFMPEG_IPV6
 $(foreach i,encoder decoder muxer demuxer parser protocol, \
 	$(eval $(PKG)_REBUILD_SUBOPTS += $(patsubst %,FREETZ_PACKAGE_FFMPEG_$(call TOUPPER_NAME,$(i))_%,$($(PKG)_$(call TOUPPER_NAME,$(i))S))) \
 	$(eval $(PKG)_CONFIGURE_$(call TOUPPER_NAME,$(i))S := $(foreach c,$($(PKG)_$(call TOUPPER_NAME,$(i))S),$(if $(FREETZ_PACKAGE_FFMPEG_$(call TOUPPER_NAME,$(i))_$(c)),--enable-$(i)="$(c)"))) \
 )
 
 $(PKG)_CONFIGURE_DEFOPTS := n
+
 $(PKG)_CONFIGURE_OPTIONS += --enable-cross-compile
 $(PKG)_CONFIGURE_OPTIONS += --cross-prefix="$(TARGET_CROSS)"
 $(PKG)_CONFIGURE_OPTIONS += --arch="$(TARGET_ARCH)"
+$(PKG)_CONFIGURE_OPTIONS += --target-os=linux
 $(PKG)_CONFIGURE_OPTIONS += --prefix="/usr"
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
 $(PKG)_CONFIGURE_OPTIONS += --disable-debug
+
 $(PKG)_CONFIGURE_OPTIONS += --enable-gpl
-$(PKG)_CONFIGURE_OPTIONS += --disable-libfaad
+$(PKG)_CONFIGURE_OPTIONS += --enable-version3
+
+$(PKG)_CONFIGURE_OPTIONS += --disable-doc
+$(PKG)_CONFIGURE_OPTIONS += --disable-dxva2
 $(PKG)_CONFIGURE_OPTIONS += --disable-mmx
 $(PKG)_CONFIGURE_OPTIONS += --disable-mmx2
 $(PKG)_CONFIGURE_OPTIONS += --enable-pthreads
 $(PKG)_CONFIGURE_OPTIONS += --disable-optimizations
 $(PKG)_CONFIGURE_OPTIONS += --enable-small
 $(PKG)_CONFIGURE_OPTIONS += --disable-stripping
-$(PKG)_CONFIGURE_OPTIONS += --disable-vhook
 $(PKG)_CONFIGURE_OPTIONS += --enable-zlib
 $(PKG)_CONFIGURE_OPTIONS += --enable-postproc
-
-$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_FFMPEG_ffmpeg),--enable-ffmpeg,--disable-ffmpeg)
-$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_FFMPEG_ffserver),--enable-ffserver,--disable-ffserver)
-$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_FFMPEG_IPV6),--enable-ipv6,--disable-ipv6)
+$(PKG)_CONFIGURE_OPTIONS += --enable-swscale
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_FFMPEG_DECODER_libopenjpeg),--enable-libopenjpeg,--disable-libopenjpeg)
 
 $(PKG)_CONFIGURE_OPTIONS += --disable-bsfs
 $(PKG)_CONFIGURE_OPTIONS += --disable-devices
+$(PKG)_CONFIGURE_OPTIONS += --disable-filters
+$(PKG)_CONFIGURE_OPTIONS += --disable-hwaccels
+
 $(PKG)_CONFIGURE_OPTIONS += --disable-encoders
-$(PKG)_CONFIGURE_OPTIONS += $($(PKG)_CONFIGURE_ENCODERS)
 $(PKG)_CONFIGURE_OPTIONS += --disable-decoders
-$(PKG)_CONFIGURE_OPTIONS += $($(PKG)_CONFIGURE_DECODERS)
 $(PKG)_CONFIGURE_OPTIONS += --disable-muxers
-$(PKG)_CONFIGURE_OPTIONS += $($(PKG)_CONFIGURE_MUXERS)
 $(PKG)_CONFIGURE_OPTIONS += --disable-demuxers
-$(PKG)_CONFIGURE_OPTIONS += $($(PKG)_CONFIGURE_DEMUXERS)
 $(PKG)_CONFIGURE_OPTIONS += --disable-parsers
-$(PKG)_CONFIGURE_OPTIONS += $($(PKG)_CONFIGURE_PARSERS)
 $(PKG)_CONFIGURE_OPTIONS += --disable-protocols
+$(PKG)_CONFIGURE_OPTIONS += $($(PKG)_CONFIGURE_ENCODERS)
+$(PKG)_CONFIGURE_OPTIONS += $($(PKG)_CONFIGURE_DECODERS)
+$(PKG)_CONFIGURE_OPTIONS += $($(PKG)_CONFIGURE_MUXERS)
+$(PKG)_CONFIGURE_OPTIONS += $($(PKG)_CONFIGURE_DEMUXERS)
+$(PKG)_CONFIGURE_OPTIONS += $($(PKG)_CONFIGURE_PARSERS)
 $(PKG)_CONFIGURE_OPTIONS += $($(PKG)_CONFIGURE_PROTOCOLS)
+
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_FFMPEG_ffmpeg),--enable-ffmpeg,--disable-ffmpeg)
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_FFMPEG_ffserver),--enable-ffserver,--disable-ffserver)
+$(PKG)_CONFIGURE_OPTIONS += --disable-ffplay
+$(PKG)_CONFIGURE_OPTIONS += --disable-ffprobe
+
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARIES_BUILD_DIR) $($(PKG)_LIBS_BUILD_DIR): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(FFMPEG_DIR)
+	$(SUBMAKE) -C $(FFMPEG_DIR) V=1
 
 $($(PKG)_LIBS_STAGING_DIR): $($(PKG)_LIBS_BUILD_DIR)
 	$(SUBMAKE) -C $(FFMPEG_DIR) \
