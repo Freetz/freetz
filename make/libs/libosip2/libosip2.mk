a00247cf0cd7006a4aad12cd7e5a49867ebf75b6
Gene Rudoy <gene@freetz.org>
2011-08-31 20:09:00 +0000

    libosip2:  * bump version to 3.5.0
    git-svn-id: http://svn.freetz.org/trunk@7564 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/libosip2/libosip2.mk
+++ make/libs/libosip2/libosip2.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_LIB, 3.3.0)
-$(PKG)_LIB_VERSION:=4.2.0
+$(call PKG_INIT_LIB, 3.5.0)
+$(PKG)_LIB_VERSION:=6.2.0
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=81493bb4d4ae6d55b71a0d4369339125
+$(PKG)_SOURCE_MD5:=7691546f6b3349d10007fc1aaff0f4e0
 $(PKG)_SITE:=@GNU/osip
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/osip2/.libs/libosip2.so.$($(PKG)_LIB_VERSION)
@@ -18,7 +18,6 @@ $(PKG)_CONFIGURE_OPTIONS += --disable-debug
 $(PKG)_CONFIGURE_OPTIONS += --disable-trace
 $(PKG)_CONFIGURE_OPTIONS += --enable-pthread
 $(PKG)_CONFIGURE_OPTIONS += --enable-semaphore
-$(PKG)_CONFIGURE_OPTIONS += --enable-ntimer
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
