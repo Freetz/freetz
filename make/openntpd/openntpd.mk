1d143f39a085c6683fd0ce4ff5c73fe94793f20f
Oliver Metz <oliver@freetz.org>
2011-10-17 06:12:27 +0000

    * OpenNTPD: Step back to working version 3.9p1 (refs #1374)
    git-svn-id: http://svn.freetz.org/trunk@7807 f5190166-0702-4917-9039-51ec32eddaf5

--- make/openntpd/openntpd.mk
+++ make/openntpd/openntpd.mk
@@ -1,27 +1,25 @@
-$(call PKG_INIT_BIN, 4.9.1)
-$(PKG)_SOURCE:=portable_$(pkg)_v$($(PKG)_VERSION).tgz
-$(PKG)_SOURCE_MD5:=aeed0fd9084cd99a7d5f8bf6ba13ce7d
-
-$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/portable_$(pkg)_v$($(PKG)_VERSION)
-#$(PKG)_SITE:=ftp://ftp.openbsd.org/pub/OpenBSD/OpenNTPD/
-# We are using our own openntpd port by TIK
-$(PKG)_SITE:=http://freetz.magenbrot.net
-$(PKG)_BINARY:=$($(PKG)_DIR)/pntpd
+$(call PKG_INIT_BIN, 3.9p1)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=ftp://ftp.openbsd.org/pub/OpenBSD/OpenNTPD/
+$(PKG)_BINARY:=$($(PKG)_DIR)/ntpd
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/ntpd
+$(PKG)_SOURCE_MD5:=afc34175f38d08867c1403d9008600b3
 
 $(PKG)_STARTLEVEL=60 # before aiccu
 
 $(PKG)_CONFIGURE_OPTIONS += --with-builtin-arc4random
 $(PKG)_CONFIGURE_OPTIONS += --with-privsep-user=ntp
-$(PKG)_CONFIGURE_OPTIONS += --with-builtin-md5
-$(PKG)_CONFIGURE_OPTIONS += --with-report-signal=SIGUSR1
+$(PKG)_CONFIGURE_OPTIONS += --with-adjtimex
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(OPENNTPD_DIR)
+	$(SUBMAKE) -C $(OPENNTPD_DIR) \
+		$(TARGET_CONFIGURE_ENV) \
+		CFLAGS="$(TARGET_CFLAGS) -DUSE_ADJTIMEX" \
+		LDFLAGS=""
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
