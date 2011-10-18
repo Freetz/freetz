82d95cd57b19ffe4127bbb4a9cfb7a55f1e2e246
Ralf Friedl <Ralf.Friedl@online.de>
2011-10-18 15:09:26 +0000

    * Add function cgi_print_radiogroup to print a HTML radiogroup (refs #1568)
    git-svn-id: http://svn.freetz.org/trunk@7844 f5190166-0702-4917-9039-51ec32eddaf5

--- make/mod/files/root/usr/lib/mod/cgi/form.sh
+++ make/mod/files/root/usr/lib/mod/cgi/form.sh
@@ -29,3 +29,56 @@ _check() {
 }
 check()  suffix=_chk checked=" checked" _check "$@"
 select() suffix=_sel checked=" selected" _check "$@"
+
+# Print a radiogroup
+#   $1: Name of the variable
+#   $2: current value, selects checked entry
+#   $3: Heading, any HTML text
+#   $4..$n: Liste of radio entries as val:id:text
+#     val: value for this entry, leading '+' for default entry
+#     id: HTML id for this entry, can be empty
+#     text: HTML text for this entry
+cgi_print_radiogroup()
+{
+    local name=$1
+    local value=$2
+    local head=$3
+    local nr nr_chk val chk id text
+    shift 3
+
+    echo "<p>"
+    echo "$head"
+    nr_chk=0
+    nr=1
+    for i; do
+	val="${i%%:*}"
+	if [ "$val" != ${val#+} ]; then
+	    nr_chk=$nr
+	fi
+	if [ "$value" = "$val" ]; then
+	    nr_chk=$nr
+	    break;
+	fi
+	nr=$((nr+1))
+    done
+    nr=1
+    for i; do
+	val="${i%%:*}"
+	i="${i#*:}"
+	id="${i%%:*}"
+	i="${i#*:}"
+	text=$i
+	val=${val#+}
+	if [ -z "$id" ]; then
+	    id="${name}_${val}"
+	fi
+	if [ $nr = $nr_chk ]; then
+	    chk=" checked"
+	else
+	    chk=
+	fi
+	echo "<input id=\"$id\" type=\"radio\" name=\"$name\" value=\"$val\"$chk><label for=\"$id\">$text</label>"
+	nr=$((nr+1))
+    done
+    echo "</p>"
+}
