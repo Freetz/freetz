c8e46a5407ac2a269e356c3fbb392f98789fb779
Oliver Metz <oliver@freetz.org>
2011-10-29 13:16:42 +0000

    Bump busybox version to 1.19.3
    git-svn-id: http://svn.freetz.org/trunk@7921 f5190166-0702-4917-9039-51ec32eddaf5

--- make/busybox/busybox.mk
+++ make/busybox/busybox.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 1.19.2)
+$(call PKG_INIT_BIN, 1.19.3)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=50267054345f1a0b77fe65f6e0e5ba29
+$(PKG)_SOURCE_MD5:=c3938e1ac59602387009bbf1dd1af7f6
 $(PKG)_SITE:=http://www.busybox.net/downloads
 
 $(PKG)_SOURCE_DIR:=$($(PKG)_SOURCE_DIR)/ref-$($(PKG)_REF)
