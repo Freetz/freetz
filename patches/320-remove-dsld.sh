53b1423c0b0195042e7228cc781034603d46b990
cuma <cuma@freetz.org>
2012-01-05 05:23:05 +0000

    remove dsld:  * remove dsld wrapper  * do not remove showdsldstat
    git-svn-id: http://svn.freetz.org/trunk@8360 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/320-remove-dsld.sh
+++ patches/320-remove-dsld.sh
@@ -1,8 +1,10 @@
 [ "$FREETZ_REMOVE_DSLD" == "y" ] || return 0
 
 echo1 "removing dsld files"
-rm_files $(find ${FILESYSTEM_MOD_DIR}/sbin -name '*dsld*') \
-	 $(find ${FILESYSTEM_MOD_DIR}/lib/modules -name dsld)
+rm_files $(find ${FILESYSTEM_MOD_DIR}/sbin \
+				${FILESYSTEM_MOD_DIR}/lib/modules \
+				-name dsld)
+
 if [ -e "$FILESYSTEM_MOD_DIR/etc/init.d/rc.init" ]; then
 	modsed "s/DSL=y/DSL=n/g" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.init"
 else
