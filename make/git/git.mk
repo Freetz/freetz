3768c4443454ff2ba3aa295e72b4e52271ee7841
Gene Rudoy <gene@freetz.org>
2012-01-07 20:00:06 +0000

    git:  * bump version to 1.7.8.3
    git-svn-id: http://svn.freetz.org/trunk@8368 f5190166-0702-4917-9039-51ec32eddaf5

--- make/git/git.mk
+++ make/git/git.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 1.7.8.1)
+$(call PKG_INIT_BIN, 1.7.8.3)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=617fe0301390e98cfc5d665b2702254a
+$(PKG)_SOURCE_MD5:=7a4bc5160166537d4da5eb48a7670dff
 $(PKG)_SITE:=http://git-core.googlecode.com/files
 
 # files to be moved from /usr/lib/git-core to /usr/bin
