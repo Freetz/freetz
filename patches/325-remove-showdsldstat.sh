a0b44a44e9cf7d6e9a11dae5d5e7ad93efc49ea9
cuma <cuma@freetz.org>
2012-01-10 16:10:12 +0000

    remove showdsldstat: remove avm-webdav dependency, thx kriegaex (refs r8382)
    git-svn-id: http://svn.freetz.org/trunk@8384 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/325-remove-showdsldstat.sh
+++ patches/325-remove-showdsldstat.sh
@@ -2,3 +2,7 @@
 
 echo1 "removing showdsldstat file"
 rm_files $(find ${FILESYSTEM_MOD_DIR}/sbin -name showdsldstat)
+
+if [ "$FREETZ_HAS_AVM_WEBDAV" == "y" -a "$FREETZ_REMOVE_WEBDAV" != "y" ]; then 
+	modsed 's/ -o ! -x $EXE_SHOWDSLDSTAT//' "$FILESYSTEM_MOD_DIR/etc/webdav_control"
+fi
