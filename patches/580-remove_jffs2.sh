aed992fd714270219e21157912234989d4a952a7
Alexander Kriegisch <kriegaex@freetz.org>
2012-01-11 02:31:20 +0000

    Improvements:   - It is not necessary to patch ''modules.dep'' because the file is generated     after module removal.   - ''rc.conf'' is edited to reflect the missing jffs2 module.
    git-svn-id: http://svn.freetz.org/trunk@8392 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/580-remove_jffs2.sh
+++ patches/580-remove_jffs2.sh
@@ -1,6 +1,7 @@
 [ "$FREETZ_REMOVE_JFFS2" = "y" ] || return 0;
 
 echo1 "removing jffs2"
-
 rm_files "${MODULES_DIR}/kernel/fs/jffs2"
-modsed ${MODULES_DIR}/modules.dep 's/.\*\+jffs2.ko:.\*//'
+
+echo1 "patching rc.conf"
+modsed "s/\(CONFIG_JFFS2=\).*$/\1\"n\"/" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
