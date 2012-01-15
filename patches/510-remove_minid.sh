Freetz/freetz

commit 3f77ed405020e9e0e47acbad5900be3577c896ea

remove_minid:  * fix dect  * http://www.wehavemorefun.de/fritzbox/index.php/Dect  (refs #1479)
git-svn-id: http://svn.freetz.org/trunk@8406 f5190166-0702-4917-9039-51ec32eddaf5

patches/510-remove_minid.sh
@@ -17,11 +17,7 @@ for files in \
 	bin/telephon.plugin \
 	bin/update.plugin \
 	etc/minid \
-	lib/libavcodec.so* \
-	lib/libavformat.so* \
-	lib/libavutil.so* \
 	lib/libflashclient.so* \
-	lib/libhttp.so* \
 	lib/libmediacli.so* \
 	lib/libnetpbm.so* \
 	usr/share/ctlmgr/libmini.so \
@@ -33,6 +29,16 @@ done
 [ "$FREETZ_REMOVE_VOIPD" == "y" ] && rm_files "${FILESYSTEM_MOD_DIR}/lib/libfoncclient*"
 [ "$FREETZ_REMOVE_MEDIASRV" == "y" ] && rm_files "${FILESYSTEM_MOD_DIR}/lib/libavmid3*.so*"
 [ "$FREETZ_REMOVE_AURA_USB" == "y" ] && rm_files "${FILESYSTEM_MOD_DIR}/lib/libavm_audio.so*"
+[ "$FREETZ_REMOVE_DECT" == "y" ] && \
+for files in \
+	lib/libavcodec.so* \
+	lib/libavformat.so* \
+	lib/libavutil.so* \
+	lib/libhttp.so* \
+	; do
+	rm_files "${FILESYSTEM_MOD_DIR}/$files"
+done
+
 
 # purge contents of rc.media
 [ -e "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.media" ] && echo > "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.media"

commit 07460a7e98290e181dcfe33ae150abbd154ca8eb

remove_minid:  * fix remove_minid-patch for boxes having minid but no DECT, e.g. 7170  * bug introduced in r8406 refs #1479
git-svn-id: http://svn.freetz.org/trunk@8413 f5190166-0702-4917-9039-51ec32eddaf5

patches/510-remove_minid.sh
@@ -29,7 +29,7 @@ done
 [ "$FREETZ_REMOVE_VOIPD" == "y" ] && rm_files "${FILESYSTEM_MOD_DIR}/lib/libfoncclient*"
 [ "$FREETZ_REMOVE_MEDIASRV" == "y" ] && rm_files "${FILESYSTEM_MOD_DIR}/lib/libavmid3*.so*"
 [ "$FREETZ_REMOVE_AURA_USB" == "y" ] && rm_files "${FILESYSTEM_MOD_DIR}/lib/libavm_audio.so*"
-[ "$FREETZ_REMOVE_DECT" == "y" ] && \
+[ "$FREETZ_HAS_AVM_DECT" != "y" -o "$FREETZ_REMOVE_DECT" == "y" ] && \
 for files in \
 	lib/libavcodec.so* \
 	lib/libavformat.so* \