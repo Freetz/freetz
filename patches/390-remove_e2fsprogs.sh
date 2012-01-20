449c04adfde8e4c81d6cb0e3bbaefe2cdedd45aa
cuma <cuma@freetz.org>
2012-01-20 11:29:10 +0000

    remove-e2fstools patch:  * added mkswap  * avm moved usr/sbin/mkfs to sbin/mkfs ->  * use 'find' for filepaths  * btw, 7390-preview has no 'mke2fs' (closes #1674)
    git-svn-id: http://svn.freetz.org/trunk@8476 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/390-remove_e2fsprogs.sh
+++ patches/390-remove_e2fsprogs.sh
@@ -1,17 +1,26 @@
 [ "$FREETZ_REMOVE_AVM_E2FSPROGS" == "y" ] || return 0
 
-echo1 "remove AVM's e2fsprogs files"
+echo1 "remove AVM's e2fsprogs binaries"
 for files in \
-	usr/sbin/blkid \
-	usr/sbin/e2fsck \
-	usr/sbin/fsck* \
-	usr/sbin/mke2fs \
-	usr/sbin/mkfs* \
-	usr/lib/libblkid* \
-	usr/lib/libcom_err* \
-	usr/lib/libe2p* \
-	usr/lib/libext2fs* \
-	usr/lib/libuuid* \
-	; do
+  blkid \
+  e2fsck \
+  fsck* \
+  mke2fs \
+  mkfs* \
+  mkswap \
+  ; do
+	for filenpath in $(find ${FILESYSTEM_MOD_DIR}/sbin/ ${FILESYSTEM_MOD_DIR}/usr/sbin/ -name $files); do
+		rm_files "$filenpath"
+	done
+done
+
+echo1 "remove AVM's e2fsprogs libraries"
+for files in \
+  usr/lib/libblkid* \
+  usr/lib/libcom_err* \
+  usr/lib/libe2p* \
+  usr/lib/libext2fs* \
+  usr/lib/libuuid* \
+  ; do
 	rm_files "${FILESYSTEM_MOD_DIR}/$files"
 done
