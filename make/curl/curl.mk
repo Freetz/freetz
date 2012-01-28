3828d03365d085d693f9db6439cc076edf63834f
Gene Rudoy <gene@freetz.org>
2012-01-28 14:11:10 +0000

    curl:  * bump version to 7.24.0
    git-svn-id: http://svn.freetz.org/trunk@8536 f5190166-0702-4917-9039-51ec32eddaf5

--- make/curl/curl.mk
+++ make/curl/curl.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_BIN, 7.23.1)
+$(call PKG_INIT_BIN, 7.24.0)
 $(PKG)_LIB_VERSION:=4.2.0
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=0296d3196b4bf82c896a869b38dbc5f2
+$(PKG)_SOURCE_MD5:=f912221d75eb8d8fe08900eaf011b023
 $(PKG)_SITE:=http://curl.haxx.se/download
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src$(if $(FREETZ_PACKAGE_CURL_STATIC),,/.libs)/curl
