88fd88aac072302cb8f1a0afdae71a2ed7597bad
cuma <cuma@freetz.org>
2012-02-08 16:26:04 +0000

    300-remove-smbd.sh: cleanup & replacing all 301-remove_smbd.patch by a sed
    git-svn-id: http://svn.freetz.org/trunk@8581 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/300-remove-smbd.sh
+++ patches/300-remove-smbd.sh
@@ -1,24 +1,24 @@
-[ "$FREETZ_HAS_AVM_USB_HOST" == "y" ] && \
-[ "$FREETZ_PACKAGE_SAMBA" == "y" ] && \
-sed -i -e "/killall smbd*$/d" \
-	-e "s/pidof smbd/pidof/g" "${FILESYSTEM_MOD_DIR}/etc/hotplug/storage"
+if [ "$FREETZ_HAS_AVM_USB_HOST" == "y" -a "$FREETZ_PACKAGE_SAMBA" == "y" ]; then
+	sed -i -e "/killall smbd*$/d" -e "s/pidof smbd/pidof/g" "${FILESYSTEM_MOD_DIR}/etc/hotplug/storage"
+fi
 
-[ "$FREETZ_PACKAGE_SAMBA" == "y" ] && \
-( \
-	echo1 "remove AVM samba config"; \
-	rm_files "${FILESYSTEM_MOD_DIR}/etc/samba_config.tar" \
+if [ "$FREETZ_PACKAGE_SAMBA" == "y" || "$FREETZ_REMOVE_SMBD" == "y" ]; then
+	echo1 "remove AVM samba config"
+	rm_files \
 		"${FILESYSTEM_MOD_DIR}/bin/inetdsamba" \
-		"${FILESYSTEM_MOD_DIR}/sbin/samba_config_gen"
-)
+		"${FILESYSTEM_MOD_DIR}/sbin/samba_config_gen" \
+		"${FILESYSTEM_MOD_DIR}/etc/samba_config.tar"
+	echo1 "patching rc.net: renaming sambastart()"
+	modsed 's/^\(sambastart *()\)/\1{ return; }\n_\1/' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.net"
+fi
 
-[ "$FREETZ_REMOVE_SMBD" == "y" ] || return 0
-echo1 "remove AVM-smbd files"
-rm_files "${FILESYSTEM_MOD_DIR}/sbin/smbd" \
-	 "${FILESYSTEM_MOD_DIR}/bin/inetdsamba" \
-	 "${FILESYSTEM_MOD_DIR}/etc/samba_control" \
-	 "${FILESYSTEM_MOD_DIR}/etc/samba_config.tar" \
-	 "${FILESYSTEM_MOD_DIR}/sbin/samba_config_gen" \
-	 "${FILESYSTEM_MOD_DIR}/sbin/smbpasswd"
+if [ "$FREETZ_REMOVE_SMBD" == "y" ]; then
+	echo1 "remove AVM-smbd files"
+	rm_files \
+		"${FILESYSTEM_MOD_DIR}/etc/samba_control" \
+		"${FILESYSTEM_MOD_DIR}/sbin/smbd" \
+		"${FILESYSTEM_MOD_DIR}/sbin/smbpasswd"
+	echo1 "patching rc.conf: modifying CONFIG_SAMBA"
+	modsed "s/CONFIG_SAMBA=.*$/CONFIG_SAMBA=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+fi
 
-echo1 "patching rc.conf"
-modsed "s/CONFIG_SAMBA=.*$/CONFIG_SAMBA=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
