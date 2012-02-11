a6baf7a76d43d976645f2e295cb1a5dfdbb0ccbe
Gene Rudoy <gene@freetz.org>
2012-02-11 15:07:35 +0000

    midnight commander:  * bump version to 4.8.1  * fixes #1254
    git-svn-id: http://svn.freetz.org/trunk@8615 f5190166-0702-4917-9039-51ec32eddaf5

--- make/mc/mc.mk
+++ make/mc/mc.mk
@@ -1,96 +1,94 @@
-$(call PKG_INIT_BIN, 4.6.2)
-$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=ec92966f4d0c8b50c344fe901859ae2a
+$(call PKG_INIT_BIN, 4.8.1)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
+$(PKG)_SOURCE_MD5:=0c9632b799edfb8da4665fbf20546c21
 $(PKG)_SITE:=http://www.midnight-commander.org/downloads
 
-$(PKG)_BINARY:=$($(PKG)_DIR)/src/$(pkg)
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/mc.bin
-$(PKG)_HELP:=$($(PKG)_MAKE_DIR)/files/root/usr/share/mc/mc.hlp
-$(PKG)_TARGET_HELP:=$($(PKG)_DEST_DIR)/usr/share/mc/mc.hlp
-ifneq ($(strip $(FREETZ_PACKAGE_MC_ONLINE_HELP)),y)
-$(PKG)_NOT_INCLUDED:=$($(PKG)_TARGET_HELP)
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/mc
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/mc
+$(PKG)_TARGET_CONS_SAVER_BINARY:=$($(PKG)_DEST_DIR)/usr/lib/mc/cons.saver
+
+$(PKG)_DEPENDS_ON := ncurses-terminfo glib2
+
+$(PKG)_CONFIGURE_ENV += fu_cv_sys_stat_statfs2_bsize=yes
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_MC_WITH_SLANG
+ifeq ($(strip $(FREETZ_PACKAGE_MC_WITH_SLANG)),y)
+$(PKG)_DEPENDS_ON += slang
+$(PKG)_CONFIGURE_OPTIONS += --with-screen=slang
 endif
 
-$(PKG)_DEPENDS_ON := ncurses-terminfo
-ifeq ($(strip $(FREETZ_TARGET_UCLIBC_VERSION_0_9_28)),y)
-$(PKG)_DEPENDS_ON += libiconv
-$(PKG)_LIBS:=-liconv
-else
-$(PKG)_CONFIGURE_OPTIONS += --without-libiconv-prefix
-$(PKG)_LIBS:=
-endif
-ifeq ($(strip $(FREETZ_PACKAGE_MC_FORCE_GLIB12)),y)
-$(PKG)_DEPENDS_ON += glib
-else
-$(PKG)_DEPENDS_ON += glib2
-endif
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_MC_WITH_NCURSES
 ifeq ($(strip $(FREETZ_PACKAGE_MC_WITH_NCURSES)),y)
 $(PKG)_DEPENDS_ON += ncurses
+$(PKG)_CONFIGURE_OPTIONS += --with-screen=ncurses
 endif
 
-$(PKG)_CONFIGURE_ENV += mc_cv_have_zipinfo=yes
+$(PKG)_CONFIGURE_OPTIONS += --without-libintl-prefix
+ifeq ($(strip $(FREETZ_TARGET_UCLIBC_VERSION_0_9_28)),y)
+$(PKG)_DEPENDS_ON += libiconv
+$(PKG)_LIBICONV:=-liconv
+else
+$(PKG)_CONFIGURE_OPTIONS += --without-libiconv-prefix
+endif
 
-$(PKG)_CONFIGURE_OPTIONS += \
-		--disable-charset \
-		--disable-background \
-		--disable-gcc-warnings \
-		--disable-glibtest \
-		$(if $(FREETZ_PACKAGE_MC_FORCE_GLIB12),--with-glib12,--without-glib12) \
-		--without-x \
-		$(if $(FREETZ_PACKAGE_MC_VFS),--with-vfs,--without-vfs) \
-		--without-mcfs \
-		--without-samba \
-		--with-configdir=/etc \
-		--without-ext2undel \
-		--without-terminfo \
-		--without-termcap \
-		--without-slang \
-		$(if $(FREETZ_PACKAGE_MC_SUBSHELL),--with-subshell,--without-subshell) \
-		$(if $(FREETZ_PACKAGE_MC_WITH_NCURSES),--with-screen=ncurses,--with-screen=mcslang) \
-		$(if $(FREETZ_PACKAGE_MC_INTERNAL_EDITOR),--with-edit,--without-edit)
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
+$(PKG)_CONFIGURE_OPTIONS += --disable-rpath
 
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_MC_INTERNAL_EDITOR
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_MC_SUBSHELL
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_MC_WITH_NCURSES
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_MC_FORCE_GLIB12
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_MC_VFS
+$(PKG)_CONFIGURE_OPTIONS += --disable-silent-rules
+$(PKG)_CONFIGURE_OPTIONS += --without-x
+$(PKG)_CONFIGURE_OPTIONS += --without-gpm-mouse
+$(PKG)_CONFIGURE_OPTIONS += --disable-doxygen-doc
+$(PKG)_CONFIGURE_OPTIONS += --disable-doxygen-dot
+$(PKG)_CONFIGURE_OPTIONS += --disable-doxygen-html
+$(PKG)_CONFIGURE_OPTIONS += --disable-mclib
+$(PKG)_CONFIGURE_OPTIONS += --disable-tests
 
-# Workaround mc's packaging problems. Note that this code is mc-4.6.2 specific.
-# Revise it (remove if necessary) if you're about to update mc to a newer version.
-$(PKG)_CONFIGURE_PRE_CMDS += \
-	if which automake-1.10 >/dev/null 2>&1; then \
-		: do nothing; \
-	elif which automake-1.11 >/dev/null 2>&1; then \
-		for f in config.guess config.sub depcomp install-sh missing; do \
-			ln -sf /usr/share/automake-1.11/$$$$f $(abspath $($(PKG)_DIR)/config)/; \
-		done; \
-	else \
-		$(call ERROR,1,automake 1.10.x or 1.11.x is required in order to build Midnight Commander. Please install one of these versions.) \
-	fi;
+$(PKG)_CONFIGURE_OPTIONS += --with-search-engine=glib
+
+$(PKG)_CONFIGURE_OPTIONS += --sysconfdir=/usr/share
+
+$(PKG)_ENDIS_OPTIONS          := background charset vfs vfs-cpio vfs-extfs vfs-ftp vfs-sfs vfs-tar vfs-fish vfs-smb vfs-undelfs
+$(PKG)_ENDIS_OPTIONS_ENABLED  := $(call PKG_SELECTED_SUBOPTIONS,$($(PKG)_ENDIS_OPTIONS),WITH)
+$(PKG)_ENDIS_OPTIONS_DISABLED := $(filter-out $($(PKG)_ENDIS_OPTIONS_ENABLED),$($(PKG)_ENDIS_OPTIONS))
+$(PKG)_CONFIGURE_OPTIONS      += $(foreach option,$($(PKG)_ENDIS_OPTIONS_ENABLED),--enable-$(option))
+$(PKG)_CONFIGURE_OPTIONS      += $(foreach option,$($(PKG)_ENDIS_OPTIONS_DISABLED),--disable-$(option))
+
+$(PKG)_WITH_OPTIONS           := edit diff-viewer subshell
+$(PKG)_WITH_OPTIONS_YES       := $(call PKG_SELECTED_SUBOPTIONS,$($(PKG)_WITH_OPTIONS),WITH)
+$(PKG)_WITH_OPTIONS_NO        := $(filter-out $($(PKG)_WITH_OPTIONS_YES),$($(PKG)_WITH_OPTIONS))
+$(PKG)_CONFIGURE_OPTIONS      += $(foreach option,$($(PKG)_WITH_OPTIONS_YES),--with-$(option))
+$(PKG)_CONFIGURE_OPTIONS      += $(foreach option,$($(PKG)_WITH_OPTIONS_NO),--without-$(option))
+
+$(PKG)_REBUILD_SUBOPTS += $(foreach i,$($(PKG)_ENDIS_OPTIONS) $($(PKG)_WITH_OPTIONS),FREETZ_PACKAGE_MC_WITH_$(call TOUPPER_NAME,$(i)))
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(MC_DIR) \
-		LIBICONV="$(MC_LIBS)" \
-		$(if $(FREETZ_PACKAGE_MC_FORCE_GLIB12),GLIB_CFLAGS="-I$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/glib-1.2" GLIB_LIBS="-lglib",)
+	$(SUBMAKE) -C $(MC_DIR)
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
-	$(INSTALL_BINARY_STRIP)
+	$(SUBMAKE) -C $(MC_DIR) \
+		DESTDIR="$(abspath $(MC_DEST_DIR))" \
+		install
+	$(TARGET_STRIP) $@ $(MC_TARGET_CONS_SAVER_BINARY)
 
-$($(PKG)_TARGET_HELP): $($(PKG)_HELP)
-	$(INSTALL_FILE)
+$($(PKG)_TARGET_DIR)/.exclude: $(TOPDIR)/.config
+	@echo -n "" > $@; \
+	[ "$(FREETZ_PACKAGE_MC_WITH_HELP)" != "y" ] && echo "usr/share/mc/help/mc.hlp" >> $@; \
+	[ "$(FREETZ_PACKAGE_MC_WITH_HELP)" != "y" ] && echo "usr/share/mc/hints" >> $@; \
+	[ "$(FREETZ_PACKAGE_MC_WITH_SYNTAX)" != "y" ] && echo "usr/share/mc/syntax" >> $@; \
+	touch $@
 
-$(pkg):
+$(pkg): $($(PKG)_TARGET_DIR)/.exclude
 
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $(if $(FREETZ_PACKAGE_MC_ONLINE_HELP),$($(PKG)_TARGET_HELP))
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(MC_DIR) clean
 
 $(pkg)-uninstall:
-	$(RM) $(MC_TARGET_BINARY) $(MC_TARGET_HELP)
+	$(RM) -r $(MC_DEST_DIR)/usr/bin/mc* $(MC_DEST_DIR)/usr/lib/mc $(MC_DEST_DIR)/usr/share/mc
 
 $(PKG_FINISH)
