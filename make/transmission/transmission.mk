d02d839954ea5dc3b38e444cf5e55cdb169a51f7
Gene Rudoy <gene@freetz.org>
2012-02-16 21:15:18 +0000

    transmission:  * bump version to 2.50
    git-svn-id: http://svn.freetz.org/trunk@8651 f5190166-0702-4917-9039-51ec32eddaf5

--- make/transmission/transmission.mk
+++ make/transmission/transmission.mk
@@ -1,7 +1,21 @@
-$(call PKG_INIT_BIN, 2.33)
+# Set to y if you want to build transmission from svn.
+# You might need to update the patches yourself.
+# Unsupported, use at your own risk!
+TRANSMISSION_FROM_SVN:=n
+
+ifeq ($(TRANSMISSION_FROM_SVN),y)
+$(call PKG_INIT_BIN, 13228)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=082217a65713ac879410c622cbe6eb26
+$(PKG)_SITE:=svn://svn.transmissionbt.com/Transmission/trunk
+
+$(PKG)_CONFIGURE_PRE_CMDS += $(SED) -i -r -e '/^m4_define.+user_agent_prefix/s,[+],,g' -e '/^m4_define.+peer_id_prefix/s,[XZ]-,0-,g' ./configure.ac;
+$(PKG)_CONFIGURE_PRE_CMDS += AUTOGEN_SUBDIR_MODE=y ./autogen.sh;
+else
+$(call PKG_INIT_BIN, 2.50)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SOURCE_MD5:=c3611108e34fe6ebdcf93da5beb89045
 $(PKG)_SITE:=http://download.m0k.org/transmission/files
+endif
 
 $(PKG)_BINARIES_ALL_SHORT     := cli  daemon  remote  create  edit   show
 $(PKG)_BINARIES_BUILD_SUBDIRS := cli/ daemon/ daemon/ utils/  utils/ utils/
@@ -24,19 +38,18 @@ $(PKG)_DEPENDS_ON := zlib openssl curl libevent
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_TRANSMISSION_STATIC
 
+$(PKG)_CONFIGURE_ENV += HAVE_CXX=yes
+
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
-# remove optimization & debug flags
-$(PKG)_CONFIGURE_PRE_CMDS += $(foreach flag,-O[0-9] -g -ggdb3,$(SED) -i -r -e 's,(C(XX)?FLAGS="[^"]*)$(flag)(( [^"]*)?"),\1\3,g' ./configure;)
-# silence '*LARGEFILE* redefined' warnings
-$(PKG)_CONFIGURE_PRE_CMDS += find $(abspath $($(PKG)_DIR)) -type f -name "*.c" \
-        -exec $(SED) -i -r -e 's|(\#define (_LARGEFILE(64)?_SOURCE))|\#ifndef \2\n\1\n\#endif|g' \{\} \+ ;
+# remove some optimization/debug/warning flags
+$(PKG)_CONFIGURE_PRE_CMDS += $(foreach flag,-O[0-9] -g -ggdb3 -Winline,$(SED) -i -r -e 's,(C(XX)?FLAGS="[^"]*)$(flag)(( [^"]*)?"),\1\3,g' ./configure;)
 
 ifeq ($(strip $(FREETZ_TARGET_UCLIBC_VERSION_0_9_28)),y)
 $(PKG)_CONFIGURE_PRE_CMDS += $(SED) -i -r -e 's,iconv_open,no_iconv_open_in_0928,' ./configure;
 endif
 
 $(PKG)_CONFIGURE_OPTIONS += --disable-mac
-$(PKG)_CONFIGURE_OPTIONS += --disable-gtk
+$(PKG)_CONFIGURE_OPTIONS += --without-gtk
 $(PKG)_CONFIGURE_OPTIONS += --disable-silent-rules
 $(PKG)_CONFIGURE_OPTIONS += --enable-lightweight
 $(PKG)_CONFIGURE_OPTIONS += --enable-utp
