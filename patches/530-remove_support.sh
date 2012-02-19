4351a601823e7dbea9f3f12c783d24da7a8ef0ad
Jörg Jungermann <jj@borkum.net>
2012-02-19 20:10:47 +0000

    * treats /etc/onlinechanged/send_crashreport as support file    and removes it if support file remove patch is selected    fixes #1709
    git-svn-id: http://svn.freetz.org/trunk@8672 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/530-remove_support.sh
+++ patches/530-remove_support.sh
@@ -4,4 +4,5 @@ rm_files $(find ${FILESYSTEM_MOD_DIR}/sbin -maxdepth 1 -name eventsdump \
 		-o -name showaddrs -o -name showroutes | xargs) \
 	 "${FILESYSTEM_MOD_DIR}/bin/showvoipdstat" \
 	 "${FILESYSTEM_MOD_DIR}/bin/supportdata*" \
-	 "${HTML_LANG_MOD_DIR}/html/support*.html"
+	 "${HTML_LANG_MOD_DIR}/html/support*.html" \
+	 "${FILESYSTEM_MOD_DIR}/etc/onlinechanged/send_crashreport"
