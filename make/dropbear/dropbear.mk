e2b8bdef943338e3d797e2471fb3a05bd839a783
Jörg Jungermann <jj@borkum.net>
2012-02-24 23:04:01 +0000

    * update dropbear to cope with CVE-2012-0920 remote vulnerability    http://osdir.com/ml/general/2012-02/msg41170.html
    git-svn-id: http://svn.freetz.org/trunk@8692 f5190166-0702-4917-9039-51ec32eddaf5

--- make/dropbear/dropbear.mk
+++ make/dropbear/dropbear.mk
@@ -1,8 +1,9 @@
-$(call PKG_INIT_BIN, 2011.54)
-$(PKG)_SOURCE:=$(pkg)_$($(PKG)_VERSION).orig.tar.gz 
-$(PKG)_SOURCE_MD5:=ce1e236c4a8df0cb95253defee6716b5
-$(PKG)_SITE:=http://ftp.debian.org/debian/pool/main/d/dropbear 
-#$(PKG)_SITE:=http://matt.ucc.asn.au/dropbear/releases
+$(call PKG_INIT_BIN, 2012.55)
+#$(PKG)_SOURCE:=$(pkg)_$($(PKG)_VERSION).orig.tar.gz 
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=44836e5a0419ba12557f9ea46880077e
+#$(PKG)_SITE:=http://ftp.debian.org/debian/pool/main/d/dropbear 
+$(PKG)_SITE:=http://matt.ucc.asn.au/dropbear/releases
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/dropbearmulti
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/dropbearmulti
