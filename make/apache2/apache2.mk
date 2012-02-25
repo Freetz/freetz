7926ca61568b97d5b7e494b78e1bb630a76db60a
Gene Rudoy <gene@freetz.org>
2012-02-25 18:36:20 +0000

    apache-2.x:  * bump version to 2.2.22  * revise package a bit  * refs #1110
    git-svn-id: http://svn.freetz.org/trunk@8707 f5190166-0702-4917-9039-51ec32eddaf5

--- make/apache2/apache2.mk
+++ make/apache2/apache2.mk
@@ -1,14 +1,11 @@
-$(call PKG_INIT_BIN, 2.2.21)
+$(call PKG_INIT_BIN, 2.2.22)
 $(PKG)_SOURCE:=httpd-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=1696ae62cd879ab1d4dd9ff021a470f2
+$(PKG)_SOURCE_MD5:=9fe3093194c8a57f085ff7c3fc43715f
 $(PKG)_SITE:=@APACHE/httpd
 $(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/httpd-$($(PKG)_VERSION)
 
-$(PKG)_BINARY:=$($(PKG)_DIR)/httpd2
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/httpd2
-
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_APACHE2_STATIC
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_APACHE2_COMPILEINMODS
+$(PKG)_BINARY:=$($(PKG)_DIR)/$(pkg)
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/$(pkg)
 
 $(PKG)_DEPENDS_ON := apr apr-util pcre
 ifeq ($(strip $(FREETZ_PACKAGE_APACHE2_DEFLATE)),y)
@@ -18,16 +15,25 @@ ifeq ($(strip $(FREETZ_PACKAGE_APACHE2_SSL)),y)
 $(PKG)_DEPENDS_ON += openssl
 endif
 
-$(PKG)_CONFIGURE_ENV += ap_cv_void_ptr_lt_long=no
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_APACHE2_DEFLATE
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_APACHE2_SSL
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_APACHE2_COMPILEINMODS
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_APACHE2_STATIC
 
+$(PKG)_CONFIGURE_ENV += ap_cv_void_ptr_lt_long=no
 
 $(PKG)_CONFIGURE_OPTIONS += --with-apr="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/apr-1-config"
 $(PKG)_CONFIGURE_OPTIONS += --with-apr-util="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/apu-1-config"
 $(PKG)_CONFIGURE_OPTIONS += --with-pcre="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/pcre-config"
-$(PKG)_CONFIGURE_OPTIONS += --with-z="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
-$(PKG)_CONFIGURE_OPTIONS += --sysconfdir=/etc/apache2
-$(PKG)_CONFIGURE_OPTIONS += --libexecdir=/usr/lib/apache2
-$(PKG)_CONFIGURE_OPTIONS += --with-program-name=httpd2
+$(PKG)_CONFIGURE_OPTIONS += --with-z=$(if $(FREETZ_PACKAGE_APACHE2_DEFLATE),"$(TARGET_TOOLCHAIN_STAGING_DIR)/usr",no)
+
+$(PKG)_CONFIGURE_OPTIONS += --with-program-name=$(pkg)
+$(PKG)_CONFIGURE_OPTIONS += --with-suexec-bin=/usr/sbin/suexec2
+$(PKG)_CONFIGURE_OPTIONS += --sysconfdir=/etc/$(pkg)
+$(PKG)_CONFIGURE_OPTIONS += --libexecdir=/usr/lib/$(pkg)
+$(PKG)_CONFIGURE_OPTIONS += --datadir=/usr/share/$(pkg)
+$(PKG)_CONFIGURE_OPTIONS += --localstatedir=/var/$(pkg)
+
 $(PKG)_CONFIGURE_OPTIONS += --enable-substitute
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_APACHE2_DEFLATE),--enable-deflate,--disable-deflate)
 $(PKG)_CONFIGURE_OPTIONS += --enable-expires
@@ -39,7 +45,6 @@ $(PKG)_CONFIGURE_OPTIONS += --enable-dav
 $(PKG)_CONFIGURE_OPTIONS += --enable-dav-fs
 $(PKG)_CONFIGURE_OPTIONS += --enable-suexec
 $(PKG)_CONFIGURE_OPTIONS += --enable-rewrite
-$(PKG)_CONFIGURE_OPTIONS += --with-suexec-bin=/usr/sbin/suexec2
 $(PKG)_CONFIGURE_OPTIONS += --enable-cgi
 $(PKG)_CONFIGURE_OPTIONS += --enable-cgid
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_APACHE2_COMPILEINMODS),--enable-modules=all --disable-so,--enable-mods-shared=all --enable-so)
@@ -53,10 +58,15 @@ $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 		$(if $(FREETZ_PACKAGE_APACHE2_STATIC),LDFLAGS="-all-static")
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
-	$(RM) -r $(APACHE2_DEST_DIR)
 	$(SUBMAKE1) -C $(APACHE2_DIR) install \
 		DESTDIR="$(FREETZ_BASE_DIR)/$(APACHE2_DEST_DIR)"
-	$(RM) -r $(APACHE2_DEST_DIR)/{etc,usr/include,usr/share}
+	$(RM) -r \
+		$(APACHE2_DEST_DIR)/etc/apache2/original \
+		$(APACHE2_DEST_DIR)/usr/{bin,include,sbin/envvars-std,share/man} \
+		$(APACHE2_DEST_DIR)/usr/share/apache2/{build,cgi-bin/*,error/README*,icons/README*,icons/*.gif,icons/*/*.gif,manual} \
+		$(APACHE2_DEST_DIR)/var
+	-$(TARGET_STRIP) $(APACHE2_DEST_DIR)/usr/sbin/* $(APACHE2_DEST_DIR)/usr/lib/apache2/*.so
+	mv $(APACHE2_DEST_DIR)/usr/sbin/suexec $(APACHE2_DEST_DIR)/usr/sbin/suexec2
 
 $(pkg):
 
