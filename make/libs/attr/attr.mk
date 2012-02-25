06b1205c6d29742e57ada4be89ea91ae23f943fb
Jörg Jungermann <jj@borkum.net>
2012-02-25 22:00:06 +0000

    * adds libattr to cope with extended posix attributes   * refs #1704
    git-svn-id: http://svn.freetz.org/trunk@8711 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/libs/attr/attr.mk
@@ -0,0 +1,42 @@
+$(call PKG_INIT_LIB, 2.4.44)
+$(PKG)_DL_VERSION:=2.4.44
+$(PKG)_LIB_VERSION:=1.1.0
+$(PKG)_SOURCE:=$(pkg)_$($(PKG)_DL_VERSION).orig.tar.gz
+$(PKG)_SOURCE_MD5:=d132c119831c27350e10b9f885711adc
+$(PKG)_SITE:=http://ftp.de.debian.org/debian/pool/main/a/attr
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/libattr/.libs/lib$(pkg).so.$($(PKG)_LIB_VERSION)
+$(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/lib$(pkg).so.$($(PKG)_LIB_VERSION)
+$(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/lib$(pkg).so.$($(PKG)_LIB_VERSION)
+
+$(PKG)_CONFIGURE_OPTIONS += --enable-shared
+$(PKG)_CONFIGURE_OPTIONS += --enable-gettext=no
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(ATTR_DIR)
+
+$($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
+	$(SUBMAKE) -C $(ATTR_DIR) \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+		libattr-install-dev libattr-install-lib
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
+	$(INSTALL_LIBRARY_STRIP)
+
+$(pkg): $($(PKG)_STAGING_BINARY)
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(ATTR_DIR) clean
+	$(RM) -r \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libattr.so* \
+
+$(pkg)-uninstall:
+	$(RM) $(ATTR_TARGET_DIR)/libattr.so*
+
+$(PKG_FINISH)
