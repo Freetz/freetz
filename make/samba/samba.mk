3cb9d2c8f0072d719840b7035473adadff0d1d54
Gene Rudoy <gene@freetz.org>
2012-02-25 12:05:12 +0000

    samba:  * eliminate code clones in makefiles  * refs #1537
    git-svn-id: http://svn.freetz.org/trunk@8700 f5190166-0702-4917-9039-51ec32eddaf5

--- make/samba/samba.mk
+++ make/samba/samba.mk
@@ -7,9 +7,80 @@ $(PKG)_SITE:=http://samba.org/samba/ftp/stable
 
 $(PKG)_CONDITIONAL_PATCHES+=$($(PKG)_VERSION)
 
+$(PKG)_BUILD_SUBDIR:=source$(if $(FREETZ_SAMBA_VERSION_3_0),,3)
+
 $(PKG)_REBUILD_SUBOPTS += FREETZ_SAMBA_VERSION_3_0
 $(PKG)_REBUILD_SUBOPTS += FREETZ_SAMBA_VERSION_3_6
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_SAMBA_MAX_DEBUG_LEVEL
+$(PKG)_REBUILD_SUBOPTS += FREETZ_TARGET_IPV6_SUPPORT
+$(PKG)_REBUILD_SUBOPTS += FREETZ_TARGET_LFS
+
+$(PKG)_BINARY:=samba_multicall
+$(PKG)_BINARY_BUILD_DIR:=$($(PKG)_DIR)/$($(PKG)_BUILD_SUBDIR)/bin/$($(PKG)_BINARY)
+$(PKG)_BINARY_TARGET_DIR:=$($(PKG)_DEST_DIR)/sbin/$($(PKG)_BINARY)
+
+$(PKG)_SYMLINKS:=smbd nmbd smbpasswd
+$(PKG)_SYMLINKS_TARGET_DIR:=$($(PKG)_SYMLINKS:%=$($(PKG)_DEST_DIR)/sbin/%)
+
+$(PKG)_CODEPAGES:=lowcase.dat upcase.dat valid.dat
+$(PKG)_CODEPAGES_DIR:=/etc/samba
+$(PKG)_CODEPAGES_BUILD_DIR:=$($(PKG)_CODEPAGES:%=$($(PKG)_DIR)/codepages/%)
+$(PKG)_CODEPAGES_TARGET_DIR:=$($(PKG)_CODEPAGES:%=$($(PKG)_DEST_DIR)$($(PKG)_CODEPAGES_DIR)/%)
+
+$(PKG)_DEPENDS_ON += popt
+ifeq ($(strip $(FREETZ_TARGET_UCLIBC_VERSION_0_9_28)),y)
+$(PKG)_DEPENDS_ON += libiconv
+$(PKG)_TARGET_LDFLAGS += -liconv
+endif
 
 include $(MAKE_DIR)/samba/samba$(if $(FREETZ_SAMBA_VERSION_3_0),30,36).mk.in
+
+$(PKG)_TARGET_CFLAGS   += -ffunction-sections -fdata-sections
+$(PKG)_TARGET_CPPFLAGS += -DMAX_DEBUG_LEVEL=$(FREETZ_PACKAGE_SAMBA_MAX_DEBUG_LEVEL) -D__location__=\\\"\\\"
+$(PKG)_TARGET_LDFLAGS  += -Wl,--gc-sections
+
+$(PKG)_MAKE_FLAGS += EXTRA_CFLAGS="$(SAMBA_TARGET_CFLAGS)"
+$(PKG)_MAKE_FLAGS += EXTRA_CPPFLAGS="$(SAMBA_TARGET_CPPFLAGS)"
+$(PKG)_MAKE_FLAGS += EXTRA_LDFLAGS="$(SAMBA_TARGET_LDFLAGS)"
+$(PKG)_MAKE_FLAGS += DYNEXP= PICFLAG=
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY_BUILD_DIR): $($(PKG)_DIR)/.configured
+	for target in $(if $(FREETZ_SAMBA_VERSION_3_0),headers) all; do \
+	$(SUBMAKE) -C $(SAMBA_DIR)/$(SAMBA_BUILD_SUBDIR) \
+		$(SAMBA_MAKE_FLAGS) \
+		$$target; \
+	done
+
+$($(PKG)_CODEPAGES_BUILD_DIR): $($(PKG)_DIR)/.unpacked
+	@touch $@
+
+$($(PKG)_BINARY_TARGET_DIR): $($(PKG)_BINARY_BUILD_DIR)
+	$(INSTALL_BINARY_STRIP)
+
+$($(PKG)_SYMLINKS_TARGET_DIR): $($(PKG)_BINARY_TARGET_DIR)
+	ln -sf $(SAMBA_BINARY) $@
+
+$($(PKG)_CODEPAGES_TARGET_DIR): $($(PKG)_DEST_DIR)$($(PKG)_CODEPAGES_DIR)/%: $($(PKG)_DIR)/codepages/%
+	$(INSTALL_FILE)
+
+$(pkg): $($(PKG)_TARGET_DIR)/.exclude
+
+$($(PKG)_TARGET_DIR)/.exclude: $(TOPDIR)/.config
+	@echo -n "" > $@; \
+	[ "$(FREETZ_PACKAGE_NMBD)" != "y" ] && echo "sbin/nmbd" >> $@; \
+	touch $@
+
+$(pkg)-precompiled: $($(PKG)_BINARY_TARGET_DIR) $($(PKG)_SYMLINKS_TARGET_DIR) $(if $(FREETZ_SAMBA_VERSION_3_0),,$($(PKG)_CODEPAGES_TARGET_DIR))
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(SAMBA_DIR)/$(SAMBA_BUILD_SUBDIR) clean
+	$(RM) -r $(SAMBA_DIR)/$(SAMBA_BUILD_SUBDIR)/bin
+
+$(pkg)-uninstall:
+	$(RM) $(SAMBA_BINARY_TARGET_DIR) $(SAMBA_SYMLINKS_TARGET_DIR) $(SAMBA_CODEPAGES_TARGET_DIR)
 
 $(PKG_FINISH)
