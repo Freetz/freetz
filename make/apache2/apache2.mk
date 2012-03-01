858f42f2187b2990c7feacb8793b5bd0b3709019
Gene Rudoy <gene@freetz.org>
2012-03-01 21:24:00 +0000

    apache2:  * bump version to 2.4.1 (by MaxMuster)  * provide apxs script to allow building of 3rd-party modules  * fix external  * refs #1110
    git-svn-id: http://svn.freetz.org/trunk@8739 f5190166-0702-4917-9039-51ec32eddaf5

--- make/apache2/apache2.mk
+++ make/apache2/apache2.mk
@@ -1,11 +1,14 @@
-$(call PKG_INIT_BIN, 2.2.22)
+$(call PKG_INIT_BIN, 2.4.1)
 $(PKG)_SOURCE:=httpd-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=9fe3093194c8a57f085ff7c3fc43715f
+$(PKG)_SOURCE_MD5:=7d3001c7a26b985d17caa367a868f11c
 $(PKG)_SITE:=@APACHE/httpd
 $(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/httpd-$($(PKG)_VERSION)
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/$(pkg)
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/$(pkg)
+
+$(PKG)_APXS_SCRIPT:=$($(PKG)_DIR)/support/apxs
+$(PKG)_APXS_SCRIPT_STAGING_DIR:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/apxs
 
 $(PKG)_DEPENDS_ON := apr apr-util pcre
 ifeq ($(strip $(FREETZ_PACKAGE_APACHE2_DEFLATE)),y)
@@ -27,10 +30,12 @@ $(PKG)_CONFIGURE_OPTIONS += --with-apr-util="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr
 $(PKG)_CONFIGURE_OPTIONS += --with-pcre="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/pcre-config"
 $(PKG)_CONFIGURE_OPTIONS += --with-z=$(if $(FREETZ_PACKAGE_APACHE2_DEFLATE),"$(TARGET_TOOLCHAIN_STAGING_DIR)/usr",no)
 
+$(PKG)_LIBEXECDIR := /usr/lib/$(pkg)
 $(PKG)_CONFIGURE_OPTIONS += --with-program-name=$(pkg)
 $(PKG)_CONFIGURE_OPTIONS += --with-suexec-bin=/usr/sbin/suexec2
 $(PKG)_CONFIGURE_OPTIONS += --sysconfdir=/etc/$(pkg)
-$(PKG)_CONFIGURE_OPTIONS += --libexecdir=/usr/lib/$(pkg)
+$(PKG)_CONFIGURE_OPTIONS += --includedir=/usr/include/$(pkg)
+$(PKG)_CONFIGURE_OPTIONS += --libexecdir=$($(PKG)_LIBEXECDIR)
 $(PKG)_CONFIGURE_OPTIONS += --datadir=/usr/share/$(pkg)
 $(PKG)_CONFIGURE_OPTIONS += --localstatedir=/var/$(pkg)
 
@@ -47,7 +52,7 @@ $(PKG)_CONFIGURE_OPTIONS += --enable-suexec
 $(PKG)_CONFIGURE_OPTIONS += --enable-rewrite
 $(PKG)_CONFIGURE_OPTIONS += --enable-cgi
 $(PKG)_CONFIGURE_OPTIONS += --enable-cgid
-$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_APACHE2_COMPILEINMODS),--enable-modules=all --disable-so,--enable-mods-shared=all --enable-so)
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_APACHE2_COMPILEINMODS),--enable-mods-static=all --disable-so,--enable-mods-shared=all --enable-so)
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -57,23 +62,43 @@ $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(APACHE2_DIR) \
 		$(if $(FREETZ_PACKAGE_APACHE2_STATIC),LDFLAGS="-all-static")
 
+$($(PKG)_APXS_SCRIPT): $($(PKG)_DIR)/.configured
+	touch $@
+
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(SUBMAKE1) -C $(APACHE2_DIR) install \
 		DESTDIR="$(FREETZ_BASE_DIR)/$(APACHE2_DEST_DIR)"
+# remove unneeded files
 	$(RM) -r \
 		$(APACHE2_DEST_DIR)/etc/apache2/original \
 		$(APACHE2_DEST_DIR)/usr/{bin,include,sbin/envvars-std,share/man} \
 		$(APACHE2_DEST_DIR)/usr/share/apache2/{build,cgi-bin/*,error/README*,icons/README*,icons/*.gif,icons/*/*.gif,manual} \
 		$(APACHE2_DEST_DIR)/var
+# strip binaries & modules
 	-$(TARGET_STRIP) $(APACHE2_DEST_DIR)/usr/sbin/* $(APACHE2_DEST_DIR)/usr/lib/apache2/*.so
+# rename suexec to suexec2 manually, apache ignores --with-suexec-bin option
 	mv $(APACHE2_DEST_DIR)/usr/sbin/suexec $(APACHE2_DEST_DIR)/usr/sbin/suexec2
+
+$($(PKG)_APXS_SCRIPT_STAGING_DIR): $($(PKG)_APXS_SCRIPT)
+	$(SUBMAKE1) -C $(APACHE2_DIR) \
+		install-include install-build \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)"
+	$(SED) -i -r -e 's,^(includedir[ \t]*=[ \t]*)(.*),\1$(TARGET_TOOLCHAIN_STAGING_DIR)\2,' \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/share/apache2/build/config_vars.mk
+	$(INSTALL_FILE)
+	chmod 755 $@
+	$(SED) -i -r -e 's,my \$$STAGING_DIR = "";,my \$$STAGING_DIR = "$(TARGET_TOOLCHAIN_STAGING_DIR)";,' $@
 
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_APXS_SCRIPT_STAGING_DIR)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(APACHE2_DIR) clean
+	$(RM) -r \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/apxs \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/apache2 \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/share/apache2
 
 $(pkg)-uninstall:
 	$(RM) -r $(APACHE2_DEST_DIR)
