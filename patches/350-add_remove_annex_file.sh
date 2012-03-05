2092051aee48d3cef264ab67dac5e6f99e557b79
cuma <cuma@freetz.org>
2012-03-05 21:00:03 +0000

    7390: remove_annex & typo (refs #1733)
    git-svn-id: http://svn.freetz.org/trunk@8765 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/350-add_remove_annex_file.sh
+++ patches/350-add_remove_annex_file.sh
@@ -17,4 +17,5 @@ fi
 if [ "$FREETZ_REMOVE_MULTI_ANNEX_FIRMWARE" == "y" ]; then
 	echo1 "removing Multi-Annex firmware files"
 	rm_files "${FILESYSTEM_MOD_DIR}/lib/modules/dsp_*/*-?-dsl.bin*"
+	rm_files "${FILESYSTEM_MOD_DIR}/lib/modules/*Vx180Code*"
 fi
