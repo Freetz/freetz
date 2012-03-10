cc2fcec5b449cf47c8143c7de912446243a34ebc
cuma <cuma@freetz.org>
2012-03-10 11:01:11 +0000

    Remove the support-files:  * own suboption for removal of "eventsdump"
    git-svn-id: http://svn.freetz.org/trunk@8781 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/530-remove_support.sh
+++ patches/530-remove_support.sh
@@ -1,7 +1,8 @@
 [ "$FREETZ_REMOVE_SUPPORT" == "y" ] || return 0
+[ "$FREETZ_REMOVE_SUPPORT_EVENTSDUMP" == "y" ] && _eventsdump="-name eventsdump -o"
 echo1 "remove support files"
-rm_files $(find ${FILESYSTEM_MOD_DIR}/sbin -maxdepth 1 -name eventsdump \
-		-o -name showaddrs -o -name showroutes | xargs) \
+rm_files $(find ${FILESYSTEM_MOD_DIR}/sbin -maxdepth 1 $_eventsdump \
+		-name showaddrs -o -name showroutes | xargs) \
 	 "${FILESYSTEM_MOD_DIR}/bin/showvoipdstat" \
 	 "${FILESYSTEM_MOD_DIR}/bin/supportdata*" \
 	 "${HTML_LANG_MOD_DIR}/html/support*.html" \
