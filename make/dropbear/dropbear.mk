cd17f81fb6c9d0096b051922a60c7cb39cc0b8b8
Jörg Jungermann <jj@borkum.net>
2012-03-25 15:18:07 +0000

    * dropbear: restore pre r8489 behavior    download source tar ball from debian repositories instead because    of better avaiability.
    (refs #1615)
    
    
    git-svn-id: http://svn.freetz.org/trunk@8839 f5190166-0702-4917-9039-51ec32eddaf5

--- make/dropbear/dropbear.mk
+++ make/dropbear/dropbear.mk
@@ -1,7 +1,8 @@
 $(call PKG_INIT_BIN, 2012.55)
-$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+
+$(PKG)_SITE:=http://ftp.debian.org/debian/pool/main/d/dropbear
+$(PKG)_SOURCE:=$(pkg)_$($(PKG)_VERSION).orig.tar.gz
 $(PKG)_SOURCE_MD5:=44836e5a0419ba12557f9ea46880077e
-$(PKG)_SITE:=http://matt.ucc.asn.au/dropbear/releases
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/dropbearmulti
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/dropbearmulti
