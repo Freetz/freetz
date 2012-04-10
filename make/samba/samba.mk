87a4d43d663d23a905bf9668b839cf807107ae83
Gene Rudoy <gene@freetz.org>
2012-04-10 22:42:53 +0000

    samba-3.0.x:  * add security patch for CVE-2012-1182 (patch taken from http://ftp.samba.org/pub/samba/patches/security/samba-3.0.37-CVE-2012-1182.patch)
    samba-3.6.x:
     * bump version to 3.6.4 (contains fix for CVE-2012-1182)
    
    refs #1537
    
    
    git-svn-id: http://svn.freetz.org/trunk@8890 f5190166-0702-4917-9039-51ec32eddaf5

--- make/samba/samba.mk
+++ make/samba/samba.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_BIN, $(if $(FREETZ_SAMBA_VERSION_3_0),3.0.37,3.6.3))
+$(call PKG_INIT_BIN, $(if $(FREETZ_SAMBA_VERSION_3_0),3.0.37,3.6.4))
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5_3.0.37:=11ed2bfef4090bd5736b194b43f67289
-$(PKG)_SOURCE_MD5_3.6.3:=98ac9db9f4b6ebfc3f013aa193ffb0d1
+$(PKG)_SOURCE_MD5_3.6.4:=d8e070e2a3b12f202f347e4427469bc2
 $(PKG)_SOURCE_MD5:=$($(PKG)_SOURCE_MD5_$($(PKG)_VERSION))
 $(PKG)_SITE:=http://samba.org/samba/ftp/stable
 
