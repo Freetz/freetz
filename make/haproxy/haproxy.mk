29b8df0e0c5effe4e1eff2b7f00cdd0f8a0c5fcc
Ralf Friedl <Ralf.Friedl@online.de>
2012-04-17 06:41:49 +0000

    * add haproxy, by fredl  * closes #1771
    git-svn-id: http://svn.freetz.org/trunk@8899 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/haproxy/haproxy.mk
@@ -0,0 +1,30 @@
+$(call PKG_INIT_BIN, 1.4.20)
+$(PKG)_SOURCE:=haproxy-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=http://haproxy.1wt.eu/download/1.4/src/
+$(PKG)_BINARY:=$($(PKG)_DIR)/haproxy
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/haproxy
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) TARGET=linux26 -C $(HAPROXY_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(EMPTY_DIR) clean
+	$(RM) $(EMPTY_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(EMPTY_TARGET_BINARY)
+
+$(PKG_FINISH)
