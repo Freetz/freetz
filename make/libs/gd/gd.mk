3a4cd0c843b5480f3b8f99c3962bbcbb31783fbe
Gene Rudoy <gene@freetz.org>
2012-04-21 10:17:14 +0000

    libgd:  * bump version to 2.0.36RC1 (contains fixes of libgd bundled with recent php versions)  * refs #1753
    git-svn-id: http://svn.freetz.org/trunk@8915 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/gd/gd.mk
+++ make/libs/gd/gd.mk
@@ -1,8 +1,10 @@
-$(call PKG_INIT_LIB, 2.0.35)
+$(call PKG_INIT_LIB, 2.0.36~rc1)
 $(PKG)_LIB_VERSION:=2.0.0
-$(PKG)_SOURCE:=gd-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=6c6c3dbb7bf079e0bb5fbbfd3bb8a71c
-$(PKG)_SITE:=http://www.libgd.org/releases
+$(PKG)_SOURCE:=libgd2_$($(PKG)_VERSION)~dfsg.orig.tar.gz
+$(PKG)_SOURCE_MD5:=0f4d2fa45627af0e87fcb74f653b66dd
+$(PKG)_SITE:=http://ftp.debian.org/debian/pool/main/libg/libgd2
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/libgd2_$($(PKG)_VERSION)~dfsg.orig
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/.libs/libgd.so.$($(PKG)_LIB_VERSION)
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libgd.so.$($(PKG)_LIB_VERSION)
@@ -10,9 +12,6 @@ $(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libgd.so.$($(PKG)_LIB_VERSION)
 
 $(PKG)_DEPENDS_ON := jpeg libpng freetype
 
-# fix libgd packaging errors (configure.ac has a later date than configure & aclocal.m4 has a later date than config.hin)
-# prevents configure & config.hin from being regenerated, which in turn allows PKG_PREVENT_RPATH_HARDCODING macro to do its job
-$(PKG)_CONFIGURE_PRE_CMDS += touch -t 200001010000.00 ./configure.ac ./aclocal.m4; touch ./config.hin ./configure;
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
 
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
@@ -22,9 +21,6 @@ $(PKG)_CONFIGURE_OPTIONS += --without-libiconv-prefix
 $(PKG)_CONFIGURE_OPTIONS += --with-x=no
 $(PKG)_CONFIGURE_OPTIONS += --with-xpm=no
 $(PKG)_CONFIGURE_OPTIONS += --with-fontconfig=no
-#$(PKG)_CONFIGURE_OPTIONS += --disable-pthreads?
-
-$(call REPLACE_LIBTOOL)
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -34,9 +30,10 @@ $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(GD_DIR)
 
 $($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
-	$(SUBMAKE) \
+	$(SUBMAKE) -C $(GD_DIR) \
 		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
-		-C $(GD_DIR) install
+		includedir=/usr/include/gd \
+		install-libLTLIBRARIES install-includeHEADERS install-binSCRIPTS
 	$(PKG_FIX_LIBTOOL_LA) \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libgd.la \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/gdlib-config
@@ -50,7 +47,8 @@ $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(GD_DIR) clean
-	$(RM) -r $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libgd.* \
+	$(RM) -r \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libgd.* \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/include/gd \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/bin/gdlib-config
 
