a9dc7021b54e1c7444b4fc033455e9c2c18f059c
cuma <cuma@freetz.org>
2012-05-07 15:19:08 +0000

    fix dsl-expert-patch for 7270v1 by using patch before r8340 (closes #1760)
    git-svn-id: http://svn.freetz.org/trunk@8984 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/240-add-dsl-expert-pages.sh
+++ patches/240-add-dsl-expert-pages.sh
@@ -20,8 +20,10 @@ for file in \
 		echo2 "patching $file"
 	fi
 done
-if isFreetzType 7240 7270; then
-	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/dsl-expert-pages_7270.patch"
+if isFreetzType 7270v1; then
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/dsl-expert-pages_7270_04.patch"
+elif isFreetzType 7240 7270_16; then
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/dsl-expert-pages_7270_05.patch"
 else
 	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_DIR}/cond/de/dsl-expert-pages_7170.patch"
 fi
