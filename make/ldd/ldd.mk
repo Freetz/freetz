25c03dd26518b81f99fcbcc744cccadb578109cf
Gene Rudoy <gene@freetz.org>
2012-05-16 23:14:42 +0000

    ldd:  * backport some upstream fixes  * refs #1767
    git-svn-id: http://svn.freetz.org/trunk@9054 f5190166-0702-4917-9039-51ec32eddaf5

--- make/ldd/ldd.mk
+++ make/ldd/ldd.mk
@@ -2,6 +2,7 @@ $(call PKG_INIT_BIN, 0.1)
 $(PKG)_SOURCE:=ldd-$($(PKG)_VERSION).tar.bz2
 $(PKG)_SOURCE_MD5:=d9910f5828f325cca92e4072c69d7caf
 $(PKG)_SITE:=http://freetz.magenbrot.net
+
 $(PKG)_SOURCE_FILE:=$($(PKG)_DIR)/ldd.c
 $(PKG)_BINARY:=$($(PKG)_DIR)/ldd
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/ldd
@@ -14,7 +15,7 @@ $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(FREETZ_LD_RUN_PATH) \
 		$(TARGET_CC) \
 		$(TARGET_CFLAGS) \
-		-DUCLIBC_RUNTIME_PREFIX=\ \
+		-DUCLIBC_RUNTIME_PREFIX=\"/\" \
 		$(LDD_SOURCE_FILE) -o $@
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
