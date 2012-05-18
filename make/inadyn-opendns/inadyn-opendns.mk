cec754b5531e8c98ac48fe57dab3330dc0536a98
cuma <cuma@freetz.org>
2012-05-18 16:47:09 +0000

    add inadyn-opendns, by Whoopie (closes #798)
    git-svn-id: http://svn.freetz.org/trunk@9074 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/inadyn-opendns/inadyn-opendns.mk
@@ -0,0 +1,34 @@
+$(call PKG_INIT_BIN,1.99)
+$(PKG)_SOURCE:=inadyn.source.v$($(PKG)_VERSION).zip
+$(PKG)_SITE:=http://www.opendns.com/support/ddns_files
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/inadyn.source.v$($(PKG)_VERSION)
+$(PKG)_BINARY:=$($(PKG)_DIR)/bin/linux/inadyn
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/inadyn
+
+$(PKG)_DEPENDS_ON := curl
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+		$(SUBMAKE) -C $(INADYN_OPENDNS_DIR) \
+		CC="$(TARGET_CC)" \
+		STRIP="$(TARGET_STRIP)" \
+		CFLAGS="$(TARGET_CFLAGS)" \
+		LDFLAGS="" 
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(INADYN_OPENDNS_DIR) clean
+
+$(pkg)-uninstall:
+	$(RM) $(INADYN_OPENDNS_TARGET_BINARY)
+
+$(PKG_FINISH)
