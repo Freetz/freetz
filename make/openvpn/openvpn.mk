9a8a851a45da4caf3a2400817d0cb85845c3597e
cuma <cuma@freetz.org>
2012-05-20 16:22:40 +0000

    openvpn 2.3-alpha1 version bump, by MaxMuster (closes #1738)
    git-svn-id: http://svn.freetz.org/trunk@9088 f5190166-0702-4917-9039-51ec32eddaf5

--- make/openvpn/openvpn.mk
+++ make/openvpn/openvpn.mk
@@ -1,14 +1,14 @@
-$(call PKG_INIT_BIN, 2.2.2)
+$(call PKG_INIT_BIN, 2.3-alpha1)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=c5181e27b7945fa6276d21873329c5c7
+$(PKG)_SOURCE_MD5:=ef3daa122ff09c12f8b9899859cce405
 $(PKG)_SITE:=http://swupdate.openvpn.net/community/releases
 $(PKG)_BINARY:=$($(PKG)_DIR)/openvpn
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/openvpn
 $(PKG)_STARTLEVEL=81
 
-$(PKG)_DEPENDS_ON := openssl
+$(PKG)_DEPENDS_ON := $(if $(FREETZ_PACKAGE_OPENVPN_POLARSSL),polarssl,openssl)
 
-$(PKG)_LIBS := -lssl -lcrypto -ldl
+$(PKG)_LIBS := $(if $(FREETZ_PACKAGE_OPENVPN_POLARSSL),-lpolarssl,-lssl -lcrypto) -ldl
 
 ifeq ($(strip $(FREETZ_PACKAGE_OPENVPN_WITH_LZO)),y)
 $(PKG)_DEPENDS_ON += lzo
@@ -23,7 +23,8 @@ $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_WITH_LZO
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_WITH_MGMNT
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_ENABLE_SMALL
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_STATIC
-$(PKG)_REBUILD_SUBOPTS += FREETZ_TARGET_IPV6_SUPPORT
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_POLARSSL
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_USE_IPROUTE
 
 # ipv6 patch modifies both files, touch them to prevent configure from being regenerated
 $(PKG)_CONFIGURE_PRE_CMDS += touch -t 200001010000.00 ./configure.ac; touch ./Makefile.in ./configure;
@@ -37,8 +38,14 @@ $(PKG)_CONFIGURE_OPTIONS += --disable-pkcs11
 $(PKG)_CONFIGURE_OPTIONS += --disable-socks
 $(PKG)_CONFIGURE_OPTIONS += --disable-http
 $(PKG)_CONFIGURE_OPTIONS += --enable-password-save
+ifeq ($(strip $(FREETZ_PACKAGE_OPENVPN_USE_IPROUTE)),y)
+$(PKG)_CONFIGURE_OPTIONS += --enable-iproute2
+$(PKG)_CONFIGURE_OPTIONS += --with-iproute-path=/bin/ip
+endif
+ifeq ($(strip $(FREETZ_PACKAGE_OPENVPN_POLARSSL)),y)
+$(PKG)_CONFIGURE_OPTIONS += --with-ssl-type=polarssl
+endif
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_OPENVPN_ENABLE_SMALL),--enable-small,--disable-small)
-$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_TARGET_IPV6_SUPPORT),--enable-ipv6,--disable-ipv6)
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
