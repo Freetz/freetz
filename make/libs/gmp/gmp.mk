88eb8fb1be332137033546c84c7e20f5d67f2353
Oliver Metz <oliver@freetz.org>
2012-06-17 10:37:37 +0000

    gmp: Don't cache lt_cv_path_LDCXX (refs #1856)
    git-svn-id: http://svn.freetz.org/trunk@9172 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/gmp/gmp.mk
+++ make/libs/gmp/gmp.mk
@@ -8,6 +8,8 @@ $(PKG)_BINARY:=$($(PKG)_DIR)/.libs/libgmp.so.$($(PKG)_LIB_VERSION)
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libgmp.so.$($(PKG)_LIB_VERSION)
 $(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libgmp.so.$($(PKG)_LIB_VERSION)
 
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_MAKE_AC_VARIABLES_PACKAGE_SPECIFIC,path_LDCXX,,lt)
+
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --with-readline=no
