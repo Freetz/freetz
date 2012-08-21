bdee5574e50c687e0259443f566de74e817007a5
cuma <cuma@freetz.org>
2012-08-21 22:04:11 +0000

    webmenu signed patch: the generic way (closes #1903)
    git-svn-id: http://svn.freetz.org/trunk@9290 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/195-webmenu_signed.sh
+++ patches/195-webmenu_signed.sh
@@ -1,19 +1,12 @@
 [ "$FREETZ_PATCH_SIGNED" == "y" ] || return 0
 echo1 "applying webmenu signed patch"
 
-if isFreetzType LANG_DE LANG_A_CH; then
-	if isFreetzType 3270 3270_V3 3370 7112 7170 7240 7270 7320 7330 7360 7390; then
-		modsed "s/^g_coninf_data.FirmwareSigned = .*/g_coninf_data.FirmwareSigned = \"1\"/g" ${FILESYSTEM_MOD_DIR}/usr/www/all/home/home.lua
-	else
-		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/all/html/de/home/home.js
-	fi
-elif isFreetzType LANG_EN; then
-	if isFreetzType 7270 7340 7390 7570; then
-		modsed "s/^g_coninf_data.FirmwareSigned = .*/g_coninf_data.FirmwareSigned = \"1\"/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/home/home.lua
-	elif isFreetzType 5124 7113 7140 7170; then
-		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/html/de/home/home.js
-	else
-		modsed "s/^.*var signed.*/\tvar signed = 1;/g" ${FILESYSTEM_MOD_DIR}/usr/www/avme/en/html/en/home/home.js
-	fi
-fi
+for file in /usr/www/all/home/home.lua /usr/www/avme/home/home.lua; do
+	[ ! -e ${FILESYSTEM_MOD_DIR}${file} ] && continue
+	modsed 's/^g_coninf_data.FirmwareSigned = .*/g_coninf_data.FirmwareSigned = "1"/g' ${FILESYSTEM_MOD_DIR}${file}
+done
 
+for file in /usr/www/all/html/de/home/home.js /usr/www/avme/html/de/home/home.js /usr/www/avme/en/html/en/home/home.js; do
+	[ ! -e ${FILESYSTEM_MOD_DIR}${file} ] && continue
+	modsed 's/^.*var signed.*/\tvar signed = 1;/g' ${FILESYSTEM_MOD_DIR}${file}
+done
