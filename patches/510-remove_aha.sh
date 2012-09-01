9e057cbbbe7896f66e798c6c86383dc8659f22b6
cuma <cuma@freetz.org>
2012-09-01 18:16:02 +0000

    add "remove aha" (refs #1910)
    git-svn-id: http://svn.freetz.org/trunk@9326 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ patches/510-remove_aha.sh
@@ -0,0 +1,15 @@
+[ "$FREETZ_REMOVE_AHA" == "y" ] || return 0
+echo1 "removing aha files"
+for files in \
+ 	usr/bin/aha \
+ 	lib/libaha.so* \
+	usr/share/aha/ \
+	etc/init.d/S78-aha \
+	; do
+	rm_files "${FILESYSTEM_MOD_DIR}/$files"
+done
+
+echo1 "patching rc.conf"
+modsed "s/CONFIG_HOME_AUTO=.*$/CONFIG_HOME_AUTO=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/CONFIG_HOME_AUTO_NET=.*$/CONFIG_HOME_AUTO_NET=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/CONFIG_DECT_HOME=.*$/CONFIG_DECT_HOME=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
