9704b1249829b7075dbcb7bf20fc57db16ab8b2d
Gene Rudoy <gene@freetz.org>
2012-09-03 20:24:46 +0000

    libpng:  * bump version to 1.2.50
    git-svn-id: http://svn.freetz.org/trunk@9344 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/libpng/libpng.mk
+++ make/libs/libpng/libpng.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_LIB, 1.2.49)
-$(PKG)_LIB_VERSION:=0.49.0
+$(call PKG_INIT_LIB, 1.2.50)
+$(PKG)_LIB_VERSION:=0.50.0
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
-$(PKG)_SOURCE_MD5:=2e7966a54826a4735f3f52bd8c763c79
+$(PKG)_SOURCE_MD5:=a3e00fccbfe356174ab515b5c00641c7
 $(PKG)_SITE:=@SF/libpng
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/.libs/libpng12.so.$($(PKG)_LIB_VERSION)
