6d51e304d1c76143e96317f2c3ae44cb247c2c2e
cuma <cuma@freetz.org>
2012-09-09 23:49:24 +0000

    try to fix printk patch
    git-svn-id: http://svn.freetz.org/trunk@9370 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/180-printk.sh
+++ patches/180-printk.sh
@@ -1,13 +1,15 @@
 echo1 "applying printk patch"
 
 file="$FILESYSTEM_MOD_DIR/etc/init.d/rc.S"
-if isFreetzType 5010 5050 5124 5140 7050 7112 7140 7141 7150; then
-	modsed "s/takeover_printk=1/takeover_printk=0/g" $file "takeover_printk=0"
-else
-	for conf_script in rc.S S17-isdn S11-piglet; do
-		[ ! -e "$FILESYSTEM_MOD_DIR/etc/init.d/$conf_script" ] && continue
-		file="$FILESYSTEM_MOD_DIR/etc/init.d/$conf_script"
-		modsed "s/AVM_PRINTK/STD_PRINTK/g" $file "STD_PRINTK"
-	done
+if grep -q 'takeover_printk=' "$file" 2>/dev/null; then
+	modsed "s/takeover_printk=1/takeover_printk=0/g" "$file" "takeover_printk=0"
 fi
-modsed "/^cat \/dev\/debug.*$/ s/^/: #/g" $file
+
+for file in rc.S S17-isdn S11-piglet; do
+	if grep -q 'AVM_PRINTK' "$FILESYSTEM_MOD_DIR/etc/init.d/$file" 2>/dev/null; then
+		modsed "s/AVM_PRINTK/STD_PRINTK/g" "$FILESYSTEM_MOD_DIR/etc/init.d/$file" "STD_PRINTK"
+	fi
+	if grep -q '^cat \/dev\/debug' "$FILESYSTEM_MOD_DIR/etc/init.d/$file" 2>/dev/null; then
+		modsed "/^cat \/dev\/debug.*$/ s/^/: #/g" "$FILESYSTEM_MOD_DIR/etc/init.d/$file"
+	fi
+done
