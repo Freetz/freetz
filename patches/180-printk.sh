a44ebf5b4daa8c201540db4219e9e45c800a7a10
Oliver Metz <oliver@freetz.org>
2012-09-09 14:04:48 +0000

    7170: Don't write kernel messages to /dev/debug
    git-svn-id: http://svn.freetz.org/trunk@9360 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/180-printk.sh
+++ patches/180-printk.sh
@@ -1,10 +1,10 @@
 echo1 "applying printk patch"
 
 file="$FILESYSTEM_MOD_DIR/etc/init.d/rc.S"
-if isFreetzType 5010 5050 7050 7140 7141 7150 7170; then
+if isFreetzType 5010 5050 7050 7140 7141 7150; then
 	modsed "s/takeover_printk=1/takeover_printk=0/g" $file "takeover_printk=0"
 else
-	for conf_script in S17-isdn S11-piglet; do
+	for conf_script in rc.S S17-isdn S11-piglet; do
 		[ ! -e "$FILESYSTEM_MOD_DIR/etc/init.d/$conf_script" ] && continue
 		file="$FILESYSTEM_MOD_DIR/etc/init.d/$conf_script"
 		modsed "s/AVM_PRINTK/STD_PRINTK/g" $file "STD_PRINTK"
