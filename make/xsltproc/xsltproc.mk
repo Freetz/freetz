83f2b1fe6b028a5bbd0fbb73f16e2e1e2f39ebe8
Gene Rudoy <gene@freetz.org>
2012-09-13 23:15:46 +0000

    libxslt:  * bump version to 1.1.27
    git-svn-id: http://svn.freetz.org/trunk@9376 f5190166-0702-4917-9039-51ec32eddaf5

--- make/xsltproc/xsltproc.mk
+++ make/xsltproc/xsltproc.mk
@@ -1,14 +1,14 @@
-$(call PKG_INIT_BIN, 1.1.26)
+$(call PKG_INIT_BIN, 1.1.27)
 $(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/libxslt-$($(PKG)_VERSION)
 $(PKG)_SOURCE:=libxslt-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=e61d0364a30146aaa3001296f853b2b9
+$(PKG)_SOURCE_MD5:=4d01ff464b9ea7236925a1adf147c7f8
 $(PKG)_SITE:=ftp://xmlsoft.org/libxslt
 
 $(PKG)_BINARY_BUILD_DIR := $($(PKG)_DIR)/$(pkg)$(if $(FREETZ_PACKAGE_XSLTPROC_STATIC),,/.libs)/$(pkg)
 $(PKG)_BINARY_TARGET_DIR := $($(PKG)_DEST_DIR)/usr/bin/$(pkg)
 
 $(PKG)_LIBNAMES_SHORT := libxslt libexslt
-$(PKG)_LIBVERSIONS := 1.1.26 0.8.15
+$(PKG)_LIBVERSIONS := 1.1.27 0.8.16
 $(PKG)_LIBNAMES_LONG :=  $(join $($(PKG)_LIBNAMES_SHORT:%=%.so.),$($(PKG)_LIBVERSIONS))
 $(PKG)_LIBS_BUILD_DIR := $(join $($(PKG)_LIBNAMES_SHORT:%=$($(PKG)_DIR)/%/.libs/),$($(PKG)_LIBNAMES_LONG))
 $(PKG)_LIBS_STAGING_DIR := $($(PKG)_LIBNAMES_LONG:%=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/%)
