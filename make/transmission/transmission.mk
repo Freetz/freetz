5a1149d32181df7d17419b2a10d9af42efb8b528
Gene Rudoy <gene@freetz.org>
2012-09-28 22:07:53 +0000

    transmission:  * bump version to 2.71
    git-svn-id: http://svn.freetz.org/trunk@9406 f5190166-0702-4917-9039-51ec32eddaf5

--- make/transmission/transmission.mk
+++ make/transmission/transmission.mk
@@ -4,16 +4,16 @@
 TRANSMISSION_FROM_SVN:=n
 
 ifeq ($(TRANSMISSION_FROM_SVN),y)
-$(call PKG_INIT_BIN, 13407)
+$(call PKG_INIT_BIN, 13538)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_SITE:=svn://svn.transmissionbt.com/Transmission/trunk
 
 $(PKG)_CONFIGURE_PRE_CMDS += $(SED) -i -r -e '/^m4_define.+user_agent_prefix/s,[+],,g' -e '/^m4_define.+peer_id_prefix/s,[XZ]-,0-,g' ./configure.ac;
 $(PKG)_CONFIGURE_PRE_CMDS += AUTOGEN_SUBDIR_MODE=y ./autogen.sh;
 else
-$(call PKG_INIT_BIN, 2.61)
+$(call PKG_INIT_BIN, 2.71)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=a7849550d82d8a51ddaacd35edc8afe0
+$(PKG)_SOURCE_MD5:=902393c5a054b4ed4953f7a16d13801a
 $(PKG)_SITE:=http://download.m0k.org/transmission/files
 endif
 
