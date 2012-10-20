5131d82b84afb6d081ee430c6ddbd5e5298c9042
Gene Rudoy <gene@freetz.org>
2012-10-20 08:08:48 +0000

    tmux:  * bump version to 1.7
    git-svn-id: http://svn.freetz.org/trunk@9497 f5190166-0702-4917-9039-51ec32eddaf5

--- make/tmux/tmux.mk
+++ make/tmux/tmux.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 1.6)
+$(call PKG_INIT_BIN, 1.7)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=3e37db24aa596bf108a0442a81c845b3
+$(PKG)_SOURCE_MD5:=2c48fb9beb22eedba7a5de3b78dd0c03
 $(PKG)_SITE:=@SF/$(pkg)
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/tmux
@@ -11,7 +11,7 @@ $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_TMUX_STATIC
 $(PKG)_DEPENDS_ON := ncurses libevent
 
 $(PKG)_CONFIGURE_ENV += ac_cv_search_event_init="-lpthread -levent"
-$(PKG)_CONFIGURE_OPTIONS += --enable-static=$(if $(FREETZ_PACKAGE_TMUX_STATIC),yes,no)
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_TMUX_STATIC),--enable-static)
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
