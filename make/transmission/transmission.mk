8f66e6fb601fcb194bef85d2ee8fcbb28d410512
Gene Rudoy <gene@freetz.org>
2012-10-25 22:00:13 +0000

    transmission:  * bump version to 2.73 (fixes one Mac bug, no changes in linux version)
    git-svn-id: http://svn.freetz.org/trunk@9505 f5190166-0702-4917-9039-51ec32eddaf5

--- make/transmission/transmission.mk
+++ make/transmission/transmission.mk
@@ -4,16 +4,16 @@
 TRANSMISSION_FROM_SVN:=n
 
 ifeq ($(TRANSMISSION_FROM_SVN),y)
-$(call PKG_INIT_BIN, 13582)
+$(call PKG_INIT_BIN, 13589)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_SITE:=svn://svn.transmissionbt.com/Transmission/trunk
 
 $(PKG)_CONFIGURE_PRE_CMDS += $(SED) -i -r -e '/^m4_define.+user_agent_prefix/s,[+],,g' -e '/^m4_define.+peer_id_prefix/s,[XZ]-,0-,g' ./configure.ac;
 $(PKG)_CONFIGURE_PRE_CMDS += AUTOGEN_SUBDIR_MODE=y ./autogen.sh;
 else
-$(call PKG_INIT_BIN, 2.72)
+$(call PKG_INIT_BIN, 2.73)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=aae3c04676d06b2761f64270248f1d63
+$(PKG)_SOURCE_MD5:=03927018656c4b54a92156ab59a8de74
 $(PKG)_SITE:=http://download.m0k.org/transmission/files
 endif
 
