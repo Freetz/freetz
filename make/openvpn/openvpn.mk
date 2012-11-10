f21bc475f8a9f9066075293fa04a3b8e28b69a7d
Gene Rudoy <gene@freetz.org>
2012-11-10 23:44:31 +0000

    openvpn:  * revise menuconfig implementation (make it similar to other multiversion/multissl-packages)  * revise .mk file    - don't create list of required libraries manually, use EXTRA_LIBS to add -ldl instead    - fix setting ifconfig/iproute/route-path doesn't work with 2.3 because it doesn't support --with-*-path options anymore    - fix 2.3 doesn't find libraries because of invalid RPATH hardcoded at build-time    - various other fixes/cleanups  * refs #1738
    TODO: switching openssl<-->polarssl still doesn't work without removing config.cache
    
    
    git-svn-id: http://svn.freetz.org/trunk@9534 f5190166-0702-4917-9039-51ec32eddaf5

--- make/openvpn/openvpn.mk
+++ make/openvpn/openvpn.mk
@@ -1,75 +1,69 @@
-ifeq ($(strip $(FREETZ_PACKAGE_OPENVPN_23)),y)
-$(call PKG_INIT_BIN, 2.3_rc1)
-$(PKG)_SOURCE_MD5:=075ee7c57734a1fe0b02c35dbad51876
-$(PKG)_CONDITIONAL_PATCHES += v2.3
-$(PKG)_BINARY:=$($(PKG)_DIR)/src/openvpn/openvpn
-ifeq ($(strip $(FREETZ_PACKAGE_OPENVPN_STATIC)),y)
-$(PKG)_EXTRA_LDFLAGS += -all-static
-endif
-else
-$(call PKG_INIT_BIN, 2.2.2)
-$(PKG)_SOURCE_MD5:=c5181e27b7945fa6276d21873329c5c7
-$(PKG)_CONDITIONAL_PATCHES += v2.2
-$(PKG)_BINARY:=$($(PKG)_DIR)/openvpn
-endif
+$(call PKG_INIT_BIN,$(if $(FREETZ_PACKAGE_OPENVPN_VERSION_2_2),2.2.2,2.3_rc1))
+$(PKG)_SOURCE_MD5_2.2.2:=c5181e27b7945fa6276d21873329c5c7
+$(PKG)_SOURCE_MD5_2.3_rc1:=075ee7c57734a1fe0b02c35dbad51876
+$(PKG)_SOURCE_MD5:=$($(PKG)_SOURCE_MD5_$($(PKG)_VERSION))
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=http://swupdate.openvpn.net/community/releases
+
+$(PKG)_CONDITIONAL_PATCHES+=$($(PKG)_VERSION)
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/$(if $(FREETZ_PACKAGE_OPENVPN_VERSION_2_2),openvpn,src/openvpn/openvpn)
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/openvpn
+
 $(PKG)_STARTLEVEL=81
 
-$(PKG)_DEPENDS_ON := $(if $(FREETZ_PACKAGE_OPENVPN_POLARSSL),polarssl,openssl)
+$(PKG)_DEPENDS_ON += $(if $(FREETZ_PACKAGE_OPENVPN_OPENSSL),openssl)
+$(PKG)_DEPENDS_ON += $(if $(FREETZ_PACKAGE_OPENVPN_POLARSSL),polarssl)
+$(PKG)_DEPENDS_ON += $(if $(FREETZ_PACKAGE_OPENVPN_WITH_LZO),lzo)
 
-$(PKG)_LIBS := $(if $(FREETZ_PACKAGE_OPENVPN_POLARSSL),-lpolarssl,-lssl -lcrypto) -ldl
-
-ifeq ($(strip $(FREETZ_PACKAGE_OPENVPN_WITH_LZO)),y)
-$(PKG)_DEPENDS_ON += lzo
-$(PKG)_LIBS += -llzo2
-endif
-
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_VERSION_2_2
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_VERSION_2_3
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_OPENSSL
+$(PKG)_REBUILD_SUBOPTS += FREETZ_OPENSSL_SHLIB_VERSION
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_POLARSSL
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_WITH_LZO
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_WITH_MGMNT
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_ENABLE_SMALL
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_STATIC
-$(PKG)_REBUILD_SUBOPTS += FREETZ_OPENSSL_SHLIB_VERSION
-$(PKG)_REBUILD_SUBOPTS += FREETZ_TARGET_IPV6_SUPPORT
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_POLARSSL
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_USE_IPROUTE
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_STATIC
+$(PKG)_REBUILD_SUBOPTS += FREETZ_TARGET_IPV6_SUPPORT
 
+ifeq ($(strip $(FREETZ_PACKAGE_OPENVPN_VERSION_2_2)),y)
+$(PKG)_EXTRA_LIBS += $(if $(FREETZ_PACKAGE_OPENVPN_OPENSSL),-ldl)
+$(PKG)_CONFIGURE_OPTIONS += --disable-http
 # ipv6 patch modifies both files, touch them to prevent configure from being regenerated
 $(PKG)_CONFIGURE_PRE_CMDS += touch -t 200001010000.00 ./configure.ac; touch ./Makefile.in ./configure;
+endif
 
-# add EXTRA_(C|LD)FLAGS
-$(PKG)_CONFIGURE_PRE_CMDS += find $(abspath $($(PKG)_DIR)) -name Makefile.in -type f -exec $(SED) -i -r -e 's,^(C|LD)FLAGS[ \t]*=[ \t]*@\1FLAGS@,& $$$$(EXTRA_\1FLAGS),' \{\} \+;
+ifeq ($(strip $(FREETZ_PACKAGE_OPENVPN_VERSION_2_3)),y)
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
+endif
+
+# 2.3 doesn't support --with-*-path options
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_MAKE_AC_VARIABLES_PACKAGE_SPECIFIC,path_IFCONFIG path_IPROUTE path_ROUTE)
+$(PKG)_CONFIGURE_ENV += $(pkg)_path_IFCONFIG=/sbin/ifconfig
+$(PKG)_CONFIGURE_ENV += $(pkg)_path_IPROUTE=/bin/ip
+$(PKG)_CONFIGURE_ENV += $(pkg)_path_ROUTE=/sbin/route
+
+# add EXTRA_CFLAGS, EXTRA_LDFLAGS, EXTRA_LIBS make variables
+$(PKG)_CONFIGURE_PRE_CMDS += find $(abspath $($(PKG)_DIR)) -name Makefile.in -type f -exec $(SED) -i -r -e 's,^((C|LD)FLAGS|LIBS)[ \t]*=[ \t]*@\1@,& $$$$(EXTRA_\1),' \{\} \+;
 
 $(PKG)_EXTRA_CFLAGS  += -ffunction-sections -fdata-sections
 $(PKG)_EXTRA_LDFLAGS += -Wl,--gc-sections
-
-ifeq ($(strip $(FREETZ_PACKAGE_OPENVPN_STATIC)),y)
-$(PKG)_EXTRA_LDFLAGS += -static
-endif
+$(PKG)_EXTRA_LDFLAGS += $(if $(FREETZ_PACKAGE_OPENVPN_STATIC),$(if $(FREETZ_PACKAGE_OPENVPN_VERSION_2_2),-static,-all-static))
 
 $(PKG)_CONFIGURE_OPTIONS += --sysconfdir=/mod/etc/openvpn
-$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_OPENVPN_WITH_LZO),,--disable-lzo)
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_OPENVPN_WITH_LZO),--enable-lzo,--disable-lzo)
 $(PKG)_CONFIGURE_OPTIONS += --disable-debug
 $(PKG)_CONFIGURE_OPTIONS += --disable-plugins
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_OPENVPN_WITH_MGMNT),--enable-management,--disable-management)
 $(PKG)_CONFIGURE_OPTIONS += --disable-pkcs11
 $(PKG)_CONFIGURE_OPTIONS += --disable-socks
-$(PKG)_CONFIGURE_OPTIONS += --disable-http
 $(PKG)_CONFIGURE_OPTIONS += --enable-password-save
-ifeq ($(strip $(FREETZ_PACKAGE_OPENVPN_USE_IPROUTE)),y)
-$(PKG)_CONFIGURE_OPTIONS += --enable-iproute2
-$(PKG)_CONFIGURE_OPTIONS += --with-iproute-path=/bin/ip
-endif
-ifeq ($(strip $(FREETZ_PACKAGE_OPENVPN_POLARSSL)),y)
-$(PKG)_CONFIGURE_OPTIONS += --with-crypto-library=polarssl
-endif
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_OPENVPN_POLARSSL),--with-crypto-library=polarssl)
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_OPENVPN_USE_IPROUTE),--enable-iproute2)
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_OPENVPN_ENABLE_SMALL),--enable-small,--disable-small)
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_TARGET_IPV6_SUPPORT),--enable-ipv6,--disable-ipv6)
-$(PKG)_CONFIGURE_OPTIONS += --with-ifconfig-path=/sbin/ifconfig
-$(PKG)_CONFIGURE_OPTIONS += --with-iproute-path=/bin/ip
-$(PKG)_CONFIGURE_OPTIONS += --with-route-path=/sbin/route
-$(PKG)_CONFIGURE_OPTIONS += --with-netstat-path=/bin/netstat
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -79,7 +73,8 @@ $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(OPENVPN_DIR) \
 		EXTRA_CFLAGS="$(OPENVPN_EXTRA_CFLAGS)" \
 		EXTRA_LDFLAGS="$(OPENVPN_EXTRA_LDFLAGS)" \
-		LIBS="$(OPENVPN_LIBS)"
+		EXTRA_LIBS="$(OPENVPN_EXTRA_LIBS)" \
+		SOCKETS_LIBS=""
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
