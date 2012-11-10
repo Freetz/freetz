fbe4dd8c1dd732aea1d22ba26ddc60e96439d131
Gene Rudoy <gene@freetz.org>
2012-11-10 19:04:16 +0000

    openntpd:  * readd fixes added in r9465 and removed again in r9530  * revise adjtimex-patch, make it self-contained  * revise driftfile-patch, it depends on adjtimex-patch, add missing ifdefs, move prototypes to proper location  * reorder/rediff patches  * don't override any *FLAGS variable in .mk file, it's not necessary (anymore)  * refs #1374
    git-svn-id: http://svn.freetz.org/trunk@9532 f5190166-0702-4917-9039-51ec32eddaf5

--- make/openntpd/openntpd.mk
+++ make/openntpd/openntpd.mk
@@ -1,31 +1,28 @@
 $(call PKG_INIT_BIN, 3.9p1)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5:=afc34175f38d08867c1403d9008600b3
-$(PKG)_SITE:=ftp://ftp.openbsd.org/pub/OpenBSD/OpenNTPD/
+$(PKG)_SITE:=ftp://ftp.openbsd.org/pub/OpenBSD/OpenNTPD
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/ntpd
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/ntpd
 
 $(PKG)_STARTLEVEL=60 # before aiccu
 
-$(PKG)_CONFIGURE_ENV += ac_cv_have_decl_asprintf=yes 
-$(PKG)_CONFIGURE_ENV += ac_cv_func_setresuid=yes 
-$(PKG)_CONFIGURE_ENV += ac_cv_func_setresgid=yes 
+$(PKG)_CONFIGURE_ENV += ac_cv_have_decl_asprintf=yes
+$(PKG)_CONFIGURE_ENV += ac_cv_func_setresuid=yes
+$(PKG)_CONFIGURE_ENV += ac_cv_func_setresgid=yes
 
-$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_MAKE_AC_VARIABLES_PACKAGE_SPECIFIC,have_decl_LLONG_MAX) 
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_MAKE_AC_VARIABLES_PACKAGE_SPECIFIC,have_decl_LLONG_MAX)
 
 $(PKG)_CONFIGURE_OPTIONS += --with-builtin-arc4random
 $(PKG)_CONFIGURE_OPTIONS += --with-privsep-user=ntp
-$(PKG)_CONFIGURE_OPTIONS += --with-adjtimex
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(OPENNTPD_DIR) \
-		$(TARGET_CONFIGURE_ENV) \
-		CFLAGS="$(TARGET_CFLAGS) -DUSE_ADJTIMEX" \
-		LDFLAGS=""
+	$(SUBMAKE) -C $(OPENNTPD_DIR)
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
