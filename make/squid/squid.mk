3736a61335afdb7673a54adafa0124b27d12358c
Gene Rudoy <gene@freetz.org>
2012-11-17 12:23:40 +0000

    squid:  * drop unnecessary dependencies on libnsl & libresolv, refs #1940  * minor .mk-file cleanups
    git-svn-id: http://svn.freetz.org/trunk@9575 f5190166-0702-4917-9039-51ec32eddaf5

--- make/squid/squid.mk
+++ make/squid/squid.mk
@@ -1,7 +1,8 @@
 $(call PKG_INIT_BIN, 3.0.STABLE26)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_SOURCE_MD5:=f48c2b58b05715f40be0d29ab72fed34
-$(PKG)_SITE:=http://www.squid-cache.org/Versions/v$(firstword $(subst ., ,$($(PKG)_VERSION)))/$(firstword $(subst ., ,$($(PKG)_VERSION))).$(word 2,$(subst ., ,$($(PKG)_VERSION)))
+$(PKG)_SITE:=http://www.squid-cache.org/Versions/v3/$(call GET_MAJOR_VERSION,$($(PKG)_VERSION))
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/squid
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/squid
 
@@ -10,11 +11,12 @@ $(PKG)_REBUILD_SUBOPTS += FREETZ_STDCXXLIB
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_SQUID_TRANSPARENT
 
-$(PKG)_CONFIGURE_ENV += ac_cv_func_setresuid="yes"
-$(PKG)_CONFIGURE_ENV += ac_cv_func_setresuid="yes"
-$(PKG)_CONFIGURE_ENV += ac_cv_func_strnstr="no"
-$(PKG)_CONFIGURE_ENV += ac_cv_func_va_copy="yes"
-$(PKG)_CONFIGURE_ENV += ac_cv_func___va_copy="yes"
+$(PKG)_CONFIGURE_ENV += ac_cv_func_setresuid=yes
+$(PKG)_CONFIGURE_ENV += ac_cv_func_setresuid=yes
+$(PKG)_CONFIGURE_ENV += ac_cv_func_strnstr=no
+$(PKG)_CONFIGURE_ENV += ac_cv_func_va_copy=yes
+$(PKG)_CONFIGURE_ENV += ac_cv_func___va_copy=yes
+$(PKG)_CONFIGURE_ENV += ac_cv_test_checkforhugeobjects=no
 
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_SQUID_TRANSPARENT),--enable-linux-netfilter,--disable-linux-netfilter)
 
