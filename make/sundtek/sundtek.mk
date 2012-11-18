96e8fe3b71df410460d2f1c81aefc9655017d938
Gene Rudoy <gene@freetz.org>
2012-11-18 18:59:59 +0000

    sundtek:  * use custom unpack rule (fixes warnings from r9590)  * add missing selects  * strip binaries/libraries  * add missing BUILD_PREREQ
    git-svn-id: http://svn.freetz.org/trunk@9592 f5190166-0702-4917-9039-51ec32eddaf5

--- make/sundtek/sundtek.mk
+++ make/sundtek/sundtek.mk
@@ -1,8 +1,7 @@
 $(call PKG_INIT_BIN,121113.174818)
-$(PKG)_SOURCE:=sundtek_installer_$($(PKG)_VERSION).sh
+$(PKG)_SOURCE:=$(pkg)_installer_$($(PKG)_VERSION).sh
 $(PKG)_SOURCE_MD5:=fdc3d8d1abb3213a759ce73fa2e83136
-$(PKG)_SOURCE_DIR:=$(SOURCE_DIR)/$(pkg)-$($(PKG)_VERSION)
-$(PKG)_SITE:=http://www.sundtek.de/media/
+$(PKG)_SITE:=http://www.sundtek.de/media
 
 $(PKG)_STARTLEVEL=90 # before rrdstats
 
@@ -12,22 +11,25 @@ $(PKG)_BINARIES := $(join $($(PKG)_BINARIES_PATH),$($(PKG)_BINARIES_ALL))
 $(PKG)_BINARIES_BUILD_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DIR)/opt/%)
 $(PKG)_BINARIES_TARGET_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DEST_DIR)/usr/%)
 
+$(PKG)_BUILD_PREREQ += dd
+
+define $(PKG)_CUSTOM_UNPACK
+	mkdir -p $($(PKG)_DIR); \
+	payload="$$$$(cat $(1) | sed -rn 's!^_SIZE=(.*)!\1!p')"; \
+	dd if=$(1) skip=1 bs=$$$$payload | \
+	tar Oxz $(if $(FREETZ_TARGET_ARCH_LE),mipselbcm,openwrtmipsr2)/installer.tar.gz | \
+	tar xz -C $($(PKG)_DIR)
+endef
+
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_NOP)
 
-$($(PKG)_DIR)/.unpacked:
-	payload="$$(cat $(DL_DIR)/$(SUNDTEK_SOURCE) | sed -rn 's!^_SIZE=(.*)!\1!p')" ;\
-	dd if=$(DL_DIR)/$(SUNDTEK_SOURCE) skip=1 bs=$$payload |\
-	 tar Oxz $(if $(FREETZ_TARGET_ARCH_LE),mipselbcm,openwrtmipsr2)/installer.tar.gz |\
-	 tar xz -C $(SUNDTEK_DIR)
-	@touch $@
-
-$($(PKG)_BINARIES_BUILD_DIR): $($(PKG)_DIR)/.unpacked
-	@chmod 755 $(SUNDTEK_BINARIES_BUILD_DIR)
+$($(PKG)_BINARIES_BUILD_DIR): $($(PKG)_DIR)/.configured
+	@chmod 755 $@
 
 $($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/%: $($(PKG)_DIR)/opt/%
-	$(INSTALL_FILE)
+	$(INSTALL_BINARY_STRIP)
 
 $(pkg):
 
@@ -36,4 +38,3 @@ $(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR)
 $(pkg)-clean:
 
 $(PKG_FINISH)
-
