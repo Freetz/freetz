3d9c0d81dd710370bbbf266b610915159683c1d7
cuma <cuma@freetz.org>
2012-12-07 15:47:37 +0000

    typo (refs #1866)
    git-svn-id: http://svn.freetz.org/trunk@9682 f5190166-0702-4917-9039-51ec32eddaf5

--- make/mod/files/root/usr/lib/cgi-bin/mod/conf/80-ipv6.sh
+++ make/mod/files/root/usr/lib/cgi-bin/mod/conf/80-ipv6.sh
@@ -1,16 +1,16 @@
 
 sec_begin 'IPv6'
-cat << EOF
 
+cat << EOF
 <p>
 $(lang de:"Interfaces IPv6-Adressen zuweisen" en:"Assign IPv6 adresses to interfaces").<br>
 $(lang de:"Syntax" en:"Syntax"): <interface> <IPv6><br>
 $(lang de:"Beispiel" en:"Example"): lan fd21:23c2:dd2f::1/64<br>
 </p>
 <p><textarea name="ipv6_assign" rows="5" cols="50" maxlength="255">$(html "$MOD_IPV6_ASSIGN")</textarea></p>
+EOF
 
 cgi_print_checkbox_p "ipv6_forward" "$MOD_IPV6_FORWARD" \
 	"$(lang de:"Forwarding aktivieren." en:"Enable forwarding.")"
 
-EOF
 sec_end
