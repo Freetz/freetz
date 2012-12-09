2a308a4ef0faf6e7316ac61a2da46f892cccef35
cuma <cuma@freetz.org>
2012-12-09 14:40:37 +0000

    REMOVE ANNEX/DSLD: webif fixes (refs #1838)
    git-svn-id: http://svn.freetz.org/trunk@9688 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ patches/345-hide_dsl.sh
@@ -0,0 +1,12 @@
+[ "$FREETZ_REMOVE_MULTI_ANNEX_FIRMWARE_PRIME" == "y" -o \
+  "$FREETZ_REMOVE_ANNEX_A_FIRMWARE" == "y" -o \
+  "$FREETZ_REMOVE_ANNEX_B_FIRMWARE" == "y" -o \
+  "$FREETZ_REMOVE_DSLD" == "y" ] || return 0
+[ "$FREETZ_ADD_ANNEX_A_FIRMWARE" == "y" ] && return 0
+
+if [ -e "${FILESYSTEM_MOD_DIR}/usr/www/all/home/home.lua" ]; then
+	# patcht Hauptseite > Kasten Anschlüsse > DSL
+	echo1 "hiding dsl"
+	modsed "s/^\(function tr_connect_info_dsl\)()$/\1()\nreturn\nend\n\1_()/" \
+	  "${FILESYSTEM_MOD_DIR}/usr/www/all/home/home.lua" "tr_connect_info_dsl_("
+fi
