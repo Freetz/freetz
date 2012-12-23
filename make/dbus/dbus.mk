04641dc86384af1e20ee78437d214f108f1dc628
Gene Rudoy <gene@freetz.org>
2012-12-23 17:50:17 +0000

    dbus:  * bump version to 1.6.8  * remove local copy of *.conf files, install files from tarball instead
    avahi/dbus:
     * fix pid-file locations
     * move configuration-templates out of /etc/default.$package to a separate directory, reduces number of files copied to /tmp/flash/$package
     * move avahi-dbus config from dbus package to avahi package
     * remove unnecessary copyright headers from configuration files to reduce the size of /var/flash/freetz
     * minor rc.file adjustments
    
    
    git-svn-id: http://svn.freetz.org/trunk@9744 f5190166-0702-4917-9039-51ec32eddaf5

--- make/dbus/dbus.mk
+++ make/dbus/dbus.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_BIN, 1.5.6)
-$(PKG)_LIB_VERSION:=3.6.3
+$(call PKG_INIT_BIN, 1.6.8)
+$(PKG)_LIB_VERSION:=3.7.2
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=0cf9cb877a5c1508b89844050ff9f45f
+$(PKG)_SOURCE_MD5:=3bf059c7dd5eda5f539a1b7cfe7a14a2
 $(PKG)_SITE:=http://$(pkg).freedesktop.org/releases/$(pkg)
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/bus/dbus-daemon
@@ -11,6 +11,10 @@ $(PKG)_LIB_BINARY:=$($(PKG)_DIR)/dbus/.libs/lib$(pkg)-1.so.$($(PKG)_LIB_VERSION)
 $(PKG)_LIB_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/lib$(pkg)-1.so.$($(PKG)_LIB_VERSION)
 $(PKG)_LIB_TARGET_BINARY:=$($(PKG)_TARGET_LIBDIR)/lib$(pkg)-1.so.$($(PKG)_LIB_VERSION)
 
+$(PKG)_CONFIG_TEMPLATE_FILES := session.conf system.conf
+$(PKG)_CONFIG_TEMPLATE_FILES_BUILD_DIR := $(addprefix $($(PKG)_DIR)/bus/,$($(PKG)_CONFIG_TEMPLATE_FILES))
+$(PKG)_CONFIG_TEMPLATE_FILES_TARGET_DIR := $(addprefix $($(PKG)_DEST_DIR)/etc/config-template.dbus/,$($(PKG)_CONFIG_TEMPLATE_FILES))
+
 $(PKG)_DEPENDS_ON := expat
 
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
@@ -18,18 +22,23 @@ $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
 $(PKG)_CONFIGURE_OPTIONS += --disable-abstract-sockets
+$(PKG)_CONFIGURE_OPTIONS += --enable-asserts
 $(PKG)_CONFIGURE_OPTIONS += --disable-ansi
-$(PKG)_CONFIGURE_OPTIONS += --disable-asserts
+$(PKG)_CONFIGURE_OPTIONS += --enable-checks
+$(PKG)_CONFIGURE_OPTIONS += --disable-compiler_coverage
 $(PKG)_CONFIGURE_OPTIONS += --disable-console-owner-file
 $(PKG)_CONFIGURE_OPTIONS += --disable-doxygen-docs
-$(PKG)_CONFIGURE_OPTIONS += --disable-compiler_coverage
+$(PKG)_CONFIGURE_OPTIONS += --disable-epoll
+$(PKG)_CONFIGURE_OPTIONS += --disable-kqueue
+$(PKG)_CONFIGURE_OPTIONS += --disable-option-checking
 $(PKG)_CONFIGURE_OPTIONS += --disable-selinux
+$(PKG)_CONFIGURE_OPTIONS += --disable-silent-rules
 $(PKG)_CONFIGURE_OPTIONS += --disable-tests
 $(PKG)_CONFIGURE_OPTIONS += --disable-verbose-mode
 $(PKG)_CONFIGURE_OPTIONS += --disable-xml-docs
+$(PKG)_CONFIGURE_OPTIONS += --with-init-scripts=none
 $(PKG)_CONFIGURE_OPTIONS += --with-x=no
-$(PKG)_CONFIGURE_OPTIONS += --enable-silent-rules
-$(PKG)_CONFIGURE_OPTIONS += --disable-option-checking
+$(PKG)_CONFIGURE_OPTIONS += --with-xml=expat
 $(PKG)_CONFIGURE_OPTIONS += --with-dbus-user=nobody
 
 $(PKG_SOURCE_DOWNLOAD)
@@ -56,19 +65,27 @@ $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 $($(PKG)_LIB_TARGET_BINARY): $($(PKG)_LIB_STAGING_BINARY)
 	$(INSTALL_LIBRARY_STRIP)
 
+$($(PKG)_CONFIG_TEMPLATE_FILES_BUILD_DIR): $($(PKG)_DIR)/.configured
+	@touch -c $@
+
+$($(PKG)_CONFIG_TEMPLATE_FILES_TARGET_DIR): $($(PKG)_DEST_DIR)/etc/config-template.dbus/%: $($(PKG)_DIR)/bus/%
+	$(INSTALL_FILE)
+
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_LIB_TARGET_BINARY)
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_LIB_TARGET_BINARY) $($(PKG)_CONFIG_TEMPLATE_FILES_TARGET_DIR)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(DBUS_DIR) clean
 	 $(RM) -r \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libdbus-1* \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/dbus-1.pc \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/etc/dbus-1* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/dbus-1.0 \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/dbus-1.0 \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/dbus-1.0
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/share/dbus-1*
 
 $(pkg)-uninstall:
-	$(RM) $(DBUS_TARGET_BINARY) $(DBUS_TARGET_LIBDIR)/libdbus-1*.so*
+	$(RM) $(DBUS_TARGET_BINARY) $(DBUS_TARGET_LIBDIR)/libdbus-1*.so* $(DBUS_CONFIG_TEMPLATE_FILES_TARGET_DIR)
 
 $(PKG_FINISH)
