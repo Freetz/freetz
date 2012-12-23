154e8296fbdece707b4595336370f96ee2c6b538
cuma <cuma@freetz.org>
2012-12-23 16:30:28 +0000

    remove "quicklinks" by  * remove_myfritz  * remove_nas
    (refs #1838)
    
    
    
    git-svn-id: http://svn.freetz.org/trunk@9736 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/220-remove-nas.sh
+++ patches/scripts/220-remove-nas.sh
@@ -5,6 +5,7 @@ rm -rf "${FILESYSTEM_MOD_DIR}/bin/showfritznasdbstat"
 rm -rf "${FILESYSTEM_MOD_DIR}/usr/www.nas"
 ln -sf www "${FILESYSTEM_MOD_DIR}/usr/www.nas"
 
+quickstart_remove nas
 modsed "s/CONFIG_NAS.*/CONFIG_NAS=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
 
 echo2 "removing internal memory"
--- patches/scripts/221-remove_myfritz.sh
+++ patches/scripts/221-remove_myfritz.sh
@@ -4,4 +4,7 @@ echo1 "removing myfritz"
 rm -rf "${FILESYSTEM_MOD_DIR}/usr/www.myfritz"
 ln -s www "${FILESYSTEM_MOD_DIR}/usr/www.myfritz"
 
+homelua_disable tr_myfritz
+quickstart_remove myfritz
+
 modsed "s/CONFIG_MYFRITZ=.*/CONFIG_MYFRITZ=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
