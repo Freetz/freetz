66dd817dab9c8df5e7274937df948108de29075f
cuma <cuma@freetz.org>
2012-12-24 13:05:54 +0000

    hide_dsl:  * add *dsl*.lua (~62kb)  * some other webif removals (refs #1838)
    git-svn-id: http://svn.freetz.org/trunk@9753 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/345-hide_dsl.sh
+++ patches/scripts/345-hide_dsl.sh
@@ -5,7 +5,20 @@
 [ "$FREETZ_ADD_ANNEX_A_FIRMWARE" == "y" ] && return 0
 
 if [ -e "${FILESYSTEM_MOD_DIR}/usr/www/all/home/home.lua" ]; then
-	# patcht Hauptseite > Kasten Anschlüsse > DSL
 	echo1 "hiding dsl"
+	# patcht Hauptseite > Kasten Anschlüsse > DSL
 	homelua_disable tr_connect_info_dsl
+	# patcht Internet > Online-Monitor > Online-Monitor
+	modsed \
+	  '/^box.out(connection.create_connection_row("inetmon"))$/d' \
+	  "${FILESYSTEM_MOD_DIR}/usr/www/all/internet/inetstat_monitor.lua"
+	# patcht Internet > DSL-Informationen
+	modsed \
+	  's!not config.ATA or box.query("box:settings/ata_mode") ~= "1"!false!'\
+	  "${FILESYSTEM_MOD_DIR}/usr/www/all/menus/menu_show.lua"
 fi
+
+rm_files \
+  "${FILESYSTEM_MOD_DIR}/usr/www/all/internet/dsl_*.lua" \
+  "${FILESYSTEM_MOD_DIR}/usr/www/all/internet/vdsl_profile.lua"
+
