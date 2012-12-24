9e8665ef2b90aa78f0ad48c9c102cf8b471d03aa
cuma <cuma@freetz.org>
2012-12-24 12:57:09 +0000

    remove_myfritz: add myfritz*.lua (~49kb) (refs #1838)
    git-svn-id: http://svn.freetz.org/trunk@9750 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/221-remove_myfritz.sh
+++ patches/scripts/221-remove_myfritz.sh
@@ -4,6 +4,8 @@ echo1 "removing myfritz"
 rm -rf "${FILESYSTEM_MOD_DIR}/usr/www.myfritz"
 ln -s www "${FILESYSTEM_MOD_DIR}/usr/www.myfritz"
 
+rm_files "${FILESYSTEM_MOD_DIR}/usr/www/all/internet/myfritz*.lua"
+
 homelua_disable tr_myfritz
 quickstart_remove myfritz
 linkbox_remove myfritz
