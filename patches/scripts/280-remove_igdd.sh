2de1d3d91d4c880238dc055d1a37df6b90b26de8
cuma <cuma@freetz.org>
2012-12-24 13:25:38 +0000

    remove_igdd  * update for 5.50  * it's now called "upnpdevd" (refs #1838)
    git-svn-id: http://svn.freetz.org/trunk@9754 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/280-remove_igdd.sh
+++ patches/scripts/280-remove_igdd.sh
@@ -1,15 +1,18 @@
 [ "$FREETZ_REMOVE_UPNP" == "y" ] || return 0
 
-echo1 "removing AVM UPnP daemon (igdd or upnpd)"
-rm_files $(find ${FILESYSTEM_MOD_DIR}/sbin -maxdepth 1 -type f -name upnpd -o -name igdd | xargs) \
+echo1 "removing AVM UPnP daemon (igdd or upnpd or upnpdevd)"
+rm_files $(find ${FILESYSTEM_MOD_DIR}/sbin -maxdepth 1 -type f -name upnpdevd -o -name upnpd -o -name igdd | xargs) \
 	 $(find ${FILESYSTEM_MOD_DIR}/etc -maxdepth 1 -type d -name 'default.*' | xargs -I{} find {} -name 'any.xml' -o -name 'fbox*.xml') \
 	 $(find ${FILESYSTEM_MOD_DIR}/etc -maxdepth 1 -type d -name 'default.*' | xargs -I{} find {} -name '*igd*')
 [ "$FREETZ_REMOVE_UPNP_LIBS" == "y" ] && rm_files $(ls -1 ${FILESYSTEM_MOD_DIR}/lib/libavmupnp*)
 
 _upnp_file="${FILESYSTEM_MOD_DIR}/etc/init.d/rc.net"
-grep -q "^upnpdstart *()" $_upnp_file && _upnp_name=upnpdstart || _upnp_name=igddstart
-echo1 "patching rc.net: renaming $_upnp_name()"
-modsed "s/^\($_upnp_name *()\)/\1\n{ return; }\n_\1/" "$_upnp_file" "_$_upnp_name"
+for _upnp_name in upnpdevdstart upnpdstart _upnp_name; do
+	grep -q "^$_upnp_name *()" $_upnp_file || continue
+	echo1 "patching rc.net: renaming $_upnp_name()"
+	modsed "s/^\($_upnp_name *()\)/\1\n{ return; }\n_\1/" "$_upnp_file" "_$_upnp_name"
+done
+
 #
 #echo1 "patching rc.net: removing igdd"
 #modsed "s/igdd websrv/multid/g" "$_upnp_file"
