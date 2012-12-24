15aa166074594fe732326c0545cb0b3ca08e309d
Gene Rudoy <gene@freetz.org>
2012-12-24 22:12:46 +0000

    D-Bus:  * add missing CONFIGURE_ENV value (fixes dbus-daemon starts without an error but hangs on each communication attempt)  * disable poll prefer select instead  * add upstream patch fixing some file-descriptor-leaks  * add (optional) D-Bus tools (dbus-monitor, dbus-send, dbus-uuidgen), makes debugging of D-Bus problems easier  * call dbus-uuidgen from rc-script if available  * don't rely on configure defaults, explicitly provide some configure options
    git-svn-id: http://svn.freetz.org/trunk@9758 f5190166-0702-4917-9039-51ec32eddaf5

--- make/dbus/dbus.mk
+++ make/dbus/dbus.mk
@@ -7,6 +7,12 @@ $(PKG)_SITE:=http://$(pkg).freedesktop.org/releases/$(pkg)
 $(PKG)_BINARY:=$($(PKG)_DIR)/bus/dbus-daemon
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/dbus-daemon
 
+$(PKG)_TOOLS_ALL           := monitor send uuidgen
+$(PKG)_TOOLS               := $(call PKG_SELECTED_SUBOPTIONS,$($(PKG)_TOOLS_ALL))
+$(PKG)_TOOLS_BUILD_DIR     := $(addprefix $($(PKG)_DIR)/tools/.libs/dbus-, $($(PKG)_TOOLS_ALL))
+$(PKG)_TOOLS_TARGET_DIR    := $($(PKG)_TOOLS:%=$($(PKG)_DEST_DIR)/usr/bin/dbus-%)
+$(PKG)_NOT_INCLUDED        := $(patsubst %,$($(PKG)_DEST_DIR)/usr/bin/dbus-%,$(filter-out $($(PKG)_TOOLS),$($(PKG)_TOOLS_ALL)))
+
 $(PKG)_LIB_BINARY:=$($(PKG)_DIR)/dbus/.libs/lib$(pkg)-1.so.$($(PKG)_LIB_VERSION)
 $(PKG)_LIB_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/lib$(pkg)-1.so.$($(PKG)_LIB_VERSION)
 $(PKG)_LIB_TARGET_BINARY:=$($(PKG)_TARGET_LIBDIR)/lib$(pkg)-1.so.$($(PKG)_LIB_VERSION)
@@ -19,6 +25,8 @@ $(PKG)_DEPENDS_ON := expat
 
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
 
+$(PKG)_CONFIGURE_ENV += ac_cv_func_accept4=no
+
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
 $(PKG)_CONFIGURE_OPTIONS += --disable-abstract-sockets
@@ -27,8 +35,10 @@ $(PKG)_CONFIGURE_OPTIONS += --disable-ansi
 $(PKG)_CONFIGURE_OPTIONS += --enable-checks
 $(PKG)_CONFIGURE_OPTIONS += --disable-compiler_coverage
 $(PKG)_CONFIGURE_OPTIONS += --disable-console-owner-file
+$(PKG)_CONFIGURE_OPTIONS += --disable-dnotify
 $(PKG)_CONFIGURE_OPTIONS += --disable-doxygen-docs
 $(PKG)_CONFIGURE_OPTIONS += --disable-epoll
+$(PKG)_CONFIGURE_OPTIONS += --enable-inotify
 $(PKG)_CONFIGURE_OPTIONS += --disable-kqueue
 $(PKG)_CONFIGURE_OPTIONS += --disable-option-checking
 $(PKG)_CONFIGURE_OPTIONS += --disable-selinux
@@ -45,7 +55,7 @@ $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
-$($(PKG)_BINARY) $($(PKG)_LIB_BINARY): $($(PKG)_DIR)/.configured
+$($(PKG)_BINARY) $($(PKG)_LIB_BINARY) $($(PKG)_TOOLS_BUILD_DIR): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(DBUS_DIR)
 
 $($(PKG)_LIB_STAGING_BINARY): $($(PKG)_LIB_BINARY)
@@ -65,6 +75,9 @@ $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 $($(PKG)_LIB_TARGET_BINARY): $($(PKG)_LIB_STAGING_BINARY)
 	$(INSTALL_LIBRARY_STRIP)
 
+$($(PKG)_TOOLS_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/tools/.libs/%
+	$(INSTALL_BINARY_STRIP)
+
 $($(PKG)_CONFIG_TEMPLATE_FILES_BUILD_DIR): $($(PKG)_DIR)/.configured
 	@touch -c $@
 
@@ -73,7 +86,7 @@ $($(PKG)_CONFIG_TEMPLATE_FILES_TARGET_DIR): $($(PKG)_DEST_DIR)/etc/config-templa
 
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_LIB_TARGET_BINARY) $($(PKG)_CONFIG_TEMPLATE_FILES_TARGET_DIR)
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_LIB_TARGET_BINARY) $($(PKG)_TOOLS_TARGET_DIR) $($(PKG)_CONFIG_TEMPLATE_FILES_TARGET_DIR)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(DBUS_DIR) clean
@@ -86,6 +99,10 @@ $(pkg)-clean:
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/share/dbus-1*
 
 $(pkg)-uninstall:
-	$(RM) $(DBUS_TARGET_BINARY) $(DBUS_TARGET_LIBDIR)/libdbus-1*.so* $(DBUS_CONFIG_TEMPLATE_FILES_TARGET_DIR)
+	$(RM) \
+		$(DBUS_TARGET_BINARY) \
+		$(DBUS_TARGET_LIBDIR)/libdbus-1*.so* \
+		$(DBUS_TOOLS_ALL:%=$(DBUS_DEST_DIR)/usr/bin/dbus-%) \
+		$(DBUS_CONFIG_TEMPLATE_FILES_TARGET_DIR)
 
 $(PKG_FINISH)
