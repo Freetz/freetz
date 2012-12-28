72da0d32e79aeb26b96531889f4147b60fa760c5
Oliver Metz <oliver@freetz.org>
2012-12-28 10:02:39 +0000

    Bump imagemagick to version 6.8.1-3
    git-svn-id: http://svn.freetz.org/trunk@9782 f5190166-0702-4917-9039-51ec32eddaf5

--- make/imagemagick/imagemagick.mk
+++ make/imagemagick/imagemagick.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 6.8.1-0)
+$(call PKG_INIT_BIN, 6.8.1-3)
 $(PKG)_SOURCE:=ImageMagick-$($(PKG)_VERSION).tar.xz
-$(PKG)_SOURCE_MD5:=151da01e3695a7c4ce3b309b76ecc349
+$(PKG)_SOURCE_MD5:=bae7f2261098f6088fccffac053c6963
 $(PKG)_SITE:=http://www.$(pkg).org/download
 
 $(PKG)_DIR:=$(SOURCE_DIR)/ImageMagick-$($(PKG)_VERSION)
