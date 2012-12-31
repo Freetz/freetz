772d80af2c1f781bd05d2e70588bbea787419383
cuma <cuma@freetz.org>
2012-12-31 14:58:16 +0000

    remove_piglet:  * bitfile_????.bit1 files are gone  * add missing microvoip_isdn_top.bit1
    git-svn-id: http://svn.freetz.org/trunk@9796 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/410-remove_piglet.sh
+++ patches/scripts/410-remove_piglet.sh
@@ -9,14 +9,15 @@ files=""
 if [ "$FREETZ_REMOVE_PIGLET_POTS" == "y" ]; then
 	files+="${bitfile_path}/microvoip_top.bit "
 	files+="${bitfile_path}/bitfile_pots.bit "
-	files+="${bitfile_path}/bitfile_pots.bit1 "
+#	files+="${bitfile_path}/bitfile_pots.bit1 "
 	modsed "s/piglet_bitfilemode=\`\/bin.*$/piglet_bitfilemode=1/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
 fi
 
 if [ "$FREETZ_REMOVE_PIGLET_ISDN" == "y" ]; then
 	files+="${bitfile_path}/microvoip_isdn_top.bit "
+	files+="${bitfile_path}/microvoip_isdn_top.bit1 "
 	files+="${bitfile_path}/bitfile_isdn.bit "
-	files+="${bitfile_path}/bitfile_isdn.bit1 "
+#	files+="${bitfile_path}/bitfile_isdn.bit1 "
 	modsed "s/piglet_bitfilemode=\`\/bin.*$/piglet_bitfilemode=2/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
 fi
 
@@ -28,8 +29,8 @@ fi
 
 if [ "$FREETZ_REMOVE_PIGLET_V2" == "y" ]; then
 	files+="${bitfile_path}/microvoip_isdn_top.bit1 "
-	files+="${bitfile_path}/bitfile_isdn.bit1 "
-	files+="${bitfile_path}/bitfile_pots.bit1 "
+#	files+="${bitfile_path}/bitfile_isdn.bit1 "
+#	files+="${bitfile_path}/bitfile_pots.bit1 "
 fi
 
 rm_files "$files"
