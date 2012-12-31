b9bc6c80733e4d464a0d5505e2f496b7318c03a6
cuma <cuma@freetz.org>
2012-12-31 14:05:41 +0000

    remove tr069 & umtsd: assi handling & more files (refs #1838)
    git-svn-id: http://svn.freetz.org/trunk@9793 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/380-remove-umtsd.sh
+++ patches/scripts/380-remove-umtsd.sh
@@ -1,9 +1,19 @@
+if [ "$FREETZ_REMOVE_UMTSD" == "y" -o "$FREETZ_REMOVE_ASSISTANT" == "y" ]; then
+	modsed \
+	  sed 's/\(^var umts = \).*/\10;/g' \
+	  "${HTML_LANG_MOD_DIR}/html/logincheck.html"
+	modsed \
+	  's/\(^local start_umts.*\) =.*/\1 = false/g' \
+	  "${HTML_LANG_MOD_DIR}/lua/first.lua"
+fi
+
 [ "$FREETZ_REMOVE_UMTSD" == "y" ] || return 0
 echo1 "remove umtsd files"
 rm_files \
-	"${FILESYSTEM_MOD_DIR}/usr/share/ctlmgr/libgsm.so" \
-	"${FILESYSTEM_MOD_DIR}/usr/bin/umtsd" \
-	"${FILESYSTEM_MOD_DIR}/usr/bin/csvd"
+  "${FILESYSTEM_MOD_DIR}/usr/share/ctlmgr/libgsm.so" \
+  "${FILESYSTEM_MOD_DIR}/usr/bin/umtsd" \
+  "${FILESYSTEM_MOD_DIR}/usr/bin/csvd" \
+  "${FILESYSTEM_MOD_DIR}/usr/www/all/assis/internet_umts.lua"
 
 echo1 "patching rc.conf"
 modsed "s/CONFIG_USB_GSM=.*$/CONFIG_USB_GSM=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
