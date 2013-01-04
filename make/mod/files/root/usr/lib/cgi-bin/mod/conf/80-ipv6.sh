0e3a48d7988cdc20732a110e24adfdb36314479d
cuma <cuma@freetz.org>
2013-01-04 19:21:45 +0000

    mod webif: fix "html tags"
    git-svn-id: http://svn.freetz.org/trunk@9807 f5190166-0702-4917-9039-51ec32eddaf5

--- make/mod/files/root/usr/lib/cgi-bin/mod/conf/80-ipv6.sh
+++ make/mod/files/root/usr/lib/cgi-bin/mod/conf/80-ipv6.sh
@@ -4,7 +4,7 @@ sec_begin 'IPv6'
 cat << EOF
 <p>
 $(lang de:"Interfaces IPv6-Adressen zuweisen" en:"Assign IPv6 adresses to interfaces").<br>
-$(lang de:"Syntax" en:"Syntax"): <interface> <IPv6><br>
+$(lang de:"Syntax" en:"Syntax"): &lt;interface&gt; &lt;IPv6&gt;<br>
 $(lang de:"Beispiel" en:"Example"): lan fd21:23c2:dd2f::1/64<br>
 </p>
 <p><textarea name="ipv6_assign" rows="5" cols="50" maxlength="255">$(html "$MOD_IPV6_ASSIGN")</textarea></p>
