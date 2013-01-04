0cc9cdcf174a354f7a84cb39d3867f942a4ae758
cuma <cuma@freetz.org>
2013-01-04 19:34:18 +0000

    iptables: add "static" option, by MaxMuster (closes #1962)
    git-svn-id: http://svn.freetz.org/trunk@9809 f5190166-0702-4917-9039-51ec32eddaf5

--- make/iptables/iptables.mk
+++ make/iptables/iptables.mk
@@ -8,8 +8,9 @@ $(PKG)_REBUILD_SUBOPTS += FREETZ_KERNEL_VERSION
 $(PKG)_REBUILD_SUBOPTS += FREETZ_TARGET_IPV6_SUPPORT
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_IPTABLES_SAVE_RESTORE
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_IPTABLES_XML
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_IPTABLES_STATIC
 
-$(PKG)_BINARY := $($(PKG)_DIR)/iptables/.libs/xtables-multi
+$(PKG)_BINARY := $($(PKG)_DIR)/iptables/$(if $(FREETZ_PACKAGE_IPTABLES_STATIC),,.libs/)xtables-multi
 $(PKG)_TARGET_BINARY := $($(PKG)_DEST_DIR)/usr/sbin/xtables-multi
 
 $(PKG)_LIBS_SUBDIRS := libiptc/.libs/
@@ -18,18 +19,22 @@ ifeq ($(strip $(FREETZ_TARGET_IPV6_SUPPORT)),y)
 $(PKG)_LIBS_SUBDIRS += libiptc/.libs/
 endif
 
+ifneq ($(strip $(FREETZ_PACKAGE_IPTABLES_STATIC)),y)
 $(PKG)_LIBNAMES_ALL := libip4tc.so.0.0.0 libxtables.so.6.0.0 libip6tc.so.0.0.0
 $(PKG)_LIBNAMES := $(filter-out $(if $(FREETZ_TARGET_IPV6_SUPPORT),,libip6tc%), $($(PKG)_LIBNAMES_ALL))
 $(PKG)_LIBS_BUILD_DIR := $(addprefix $($(PKG)_DIR)/,$(join $($(PKG)_LIBS_SUBDIRS),$($(PKG)_LIBNAMES)))
 $(PKG)_LIBS_TARGET_DIR := $(addprefix $($(PKG)_DEST_LIBDIR)/,$($(PKG)_LIBNAMES))
+endif
 
 $(PKG)_SYMLINKS_ALL := iptables iptables-save iptables-restore iptables-xml ip6tables ip6tables-save ip6tables-restore
 $(PKG)_SYMLINKS := $(filter-out $(if $(FREETZ_TARGET_IPV6_SUPPORT),,ip6%) $(if $(FREETZ_PACKAGE_IPTABLES_SAVE_RESTORE),,%-save %-restore) $(if $(FREETZ_PACKAGE_IPTABLES_XML),,%-xml), $($(PKG)_SYMLINKS_ALL))
 $(PKG)_SYMLINKS_TARGET_DIR := $($(PKG)_SYMLINKS:%=$($(PKG)_DEST_DIR)/usr/sbin/%)
 
+ifneq ($(strip $(FREETZ_PACKAGE_IPTABLES_STATIC)),y)
 $(PKG)_EXTENSIONS_BUILD_DIR := $($(PKG)_DIR)/extensions
 $(PKG)_EXTENSIONS_TARGET_DIR := $(TARGET_SPECIFIC_ROOT_DIR)/usr/lib/xtables
 $(PKG)_TARGET_EXTENSIONS := $($(PKG)_EXTENSIONS_TARGET_DIR)/.installed
+endif
 
 $(PKG)_CONFIGURE_ENV += AR="$(TARGET_AR)"
 $(PKG)_CONFIGURE_ENV += RANLIB="$(TARGET_RANLIB)"
@@ -44,6 +49,10 @@ endif
 ifeq ($(strip $(FREETZ_PACKAGE_IPTABLES_XML)),y)
 $(PKG)_CONFIGURE_OPTIONS += --enable-xml
 endif
+ifeq ($(strip $(FREETZ_PACKAGE_IPTABLES_STATIC)),y)
+$(PKG)_CONFIGURE_OPTIONS += --enable-static
+$(PKG)_LDFLAGS += -all-static
+endif
 
 $(PKG)_CONFIGURE_PRE_CMDS += $(SED) -i -r -e 's,ac_cv(_header_linux),do_not_cache_me\1,g' configure;
 
@@ -55,7 +64,7 @@ $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_LIBS_BUILD_DIR) $($(PKG)_BINARY): $($(PKG)_DIR)/.configured | kernel-source
-	$(SUBMAKE) -C $(IPTABLES_DIR)
+	$(SUBMAKE) -C $(IPTABLES_DIR) LDFLAGS="$(TARGET_LDFLAGS) $(IPTABLES_LDFLAGS)"
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
@@ -69,6 +78,7 @@ ifeq ($(FREETZ_TARGET_IPV6_SUPPORT),y)
 $(eval $(call $(PKG)_SYMLINK_RULE,ip6tables))
 endif
 
+ifneq ($(strip $(FREETZ_PACKAGE_IPTABLES_STATIC)),y)
 $($(PKG)_TARGET_EXTENSIONS): $($(PKG)_BINARY)
 	mkdir -p $(IPTABLES_EXTENSIONS_TARGET_DIR)
 	cp $(IPTABLES_EXTENSIONS_BUILD_DIR)/*.so* $(IPTABLES_EXTENSIONS_TARGET_DIR)
@@ -76,6 +86,7 @@ $($(PKG)_TARGET_EXTENSIONS): $($(PKG)_BINARY)
 	touch $@
 
 $(foreach library,$($(PKG)_LIBS_BUILD_DIR),$(eval $(call INSTALL_LIBRARY_STRIP_RULE,$(library),$(FREETZ_LIBRARY_PATH))))
+endif
 
 $(pkg):
 
