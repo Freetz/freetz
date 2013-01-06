ec463b45f60b0ec764a6ec5c3a541b3e9d8b4575
cuma <cuma@freetz.org>
2013-01-06 00:33:24 +0000

    add remove_qos
    git-svn-id: http://svn.freetz.org/trunk@9824 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ patches/scripts/315-remove-qos.sh
@@ -0,0 +1,22 @@
+[ "$FREETZ_REMOVE_QOS" == "y" ] || return 0
+
+echo1 "removing qos files"
+rm_files \
+  "${FILESYSTEM_MOD_DIR}/lib/modules/*/kernel/net/sched/" \
+  "${FILESYSTEM_MOD_DIR}/sbin/qos" \
+  "${FILESYSTEM_MOD_DIR}/sbin/tc" \
+  "${HTML_LANG_MOD_DIR}/internet/traffic*.lua"
+
+# entfernet Internet > Filter (von 'Kindersicherung' mitgenutzt)
+if [ "$FREETZ_REMOVE_KIDS" == "y" ]; then
+	modsed 's/\(setvariable var:showKids \)./\10/g' "${HTML_SPEC_MOD_DIR}/menus/menu2_internet.html"
+fi
+
+# patcht Internet > Filter > Priorisierung
+menulua_remove trafficprio
+# patcht Internet > Filter > Listen
+menulua_remove trafficappl
+
+echo1 "patching rc.conf"
+modsed "s/CONFIG_NQOS=y/CONFIG_NQOS=n/g" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.conf"
+modsed "s/CONFIG_QOS_METER=y/CONFIG_QOS_METER=n/g" "$FILESYSTEM_MOD_DIR/etc/init.d/rc.conf"
