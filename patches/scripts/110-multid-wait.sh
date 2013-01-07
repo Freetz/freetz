30f36b3ecdc7cc0e25b2b330e8ba5965dbc61380
cuma <cuma@freetz.org>
2013-01-07 14:31:10 +0000

    fix multid-wait patch, convert to script:  * since r9702 it was not applied anymore (thx er13 for hint)
    but .. the patch was broken anyway:
     * the loop should be added to multidstart(): http://freetz.org/browser/trunk/patches/110-multid-wait.patch?rev=1415
     * and was moved (by AUTOFIX) to lltddstart(): http://freetz.org/changeset/8903/trunk/patches/110-multid-wait.patch
    
    
    
    git-svn-id: http://svn.freetz.org/trunk@9833 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ patches/scripts/110-multid-wait.sh
@@ -0,0 +1,5 @@
+echo1 "applying multid-wait patch"
+modsed \
+  's@\(eval multid $.*PARAM\)@\1\nlocal countdown=10; while [ $((countdown--)) -gt 0 ] \&\& ! ifconfig eth0 >/dev/null 2>\&1; do sleep 1; done@g' \
+  "$FILESYSTEM_MOD_DIR/etc/init.d/rc.net" \
+  'local countdown=10'
