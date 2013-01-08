a81061127e5434ae5c341c834e5de2c7f0e4072b
Gene Rudoy <gene@freetz.org>
2013-01-08 22:10:53 +0000

    openvpn:  * bump version to 2.3.0 (by MyRaCoLi)  * fixes #1973
    git-svn-id: http://svn.freetz.org/trunk@9848 f5190166-0702-4917-9039-51ec32eddaf5

--- make/openvpn/openvpn.mk
+++ make/openvpn/openvpn.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN,$(if $(FREETZ_PACKAGE_OPENVPN_VERSION_2_2),2.2.2,2.3_rc2))
+$(call PKG_INIT_BIN,$(if $(FREETZ_PACKAGE_OPENVPN_VERSION_2_2),2.2.2,2.3.0))
 $(PKG)_SOURCE_MD5_2.2.2:=c5181e27b7945fa6276d21873329c5c7
-$(PKG)_SOURCE_MD5_2.3_rc2:=0436cd56d01ccc3af98bb50fcabf303a
+$(PKG)_SOURCE_MD5_2.3.0:=56cffde5d5320e0b1ec364d3e486aca9
 $(PKG)_SOURCE_MD5:=$($(PKG)_SOURCE_MD5_$($(PKG)_VERSION))
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=http://swupdate.openvpn.net/community/releases
