eebcf49cb5f3a2997f25b7773da328028a41efff
Gene Rudoy <gene@freetz.org>
2013-01-13 11:21:12 +0000

    busybox:  * more makefile cleanups  * refs #1786
    git-svn-id: http://svn.freetz.org/trunk@9906 f5190166-0702-4917-9039-51ec32eddaf5

--- make/busybox/busybox.mk
+++ make/busybox/busybox.mk
@@ -9,8 +9,6 @@ $(PKG)_BINARY:=$($(PKG)_DIR)/$(pkg)
 $(PKG)_TARGET_DIR:=$(subst -$($(PKG)_VERSION),,$($(PKG)_TARGET_DIR))
 $(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/$(pkg)
 
-$(PKG)_CONFIG_FILE:=$(BUSYBOX_DIR)/.config
-
 $(PKG)_MAKE_FLAGS += CC="$(TARGET_CC)"
 $(PKG)_MAKE_FLAGS += CROSS_COMPILE="$(TARGET_MAKE_PATH)/$(TARGET_CROSS)"
 $(PKG)_MAKE_FLAGS += EXTRA_CFLAGS="$(TARGET_CFLAGS)"
@@ -26,7 +24,7 @@ $(PKG_UNPACKED)
 $($(PKG)_DIR)/.configured: $($(PKG)_DIR)/.unpacked
 	@cat $(TOPDIR)/.config \
 		| sed -nr 's!^(# )*(FREETZ_BUSYBOX_)([^_].*)!\1CONFIG_\3!p' \
-		> $(BUSYBOX_CONFIG_FILE)
+		> $(BUSYBOX_DIR)/.config
 	touch $@
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
@@ -37,15 +35,13 @@ $($(PKG)_BINARY).links: $($(PKG)_BINARY)
 	$(SUBMAKE) -C $(BUSYBOX_DIR) \
 		$(BUSYBOX_MAKE_FLAGS) \
 		busybox.links
-	touch $@
+	touch -c $@
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
 
 $($(PKG)_TARGET_BINARY).links: $($(PKG)_BINARY).links
 	cp $(BUSYBOX_BINARY).links $(BUSYBOX_TARGET_BINARY).links
-
-$(pkg)-source: $($(PKG)_DIR)/.unpacked
 
 $(pkg)-precompiled: uclibc $($(PKG)_TARGET_BINARY) $($(PKG)_TARGET_BINARY).links
 
@@ -54,9 +50,5 @@ $(pkg)-clean: $(pkg)-uninstall
 
 $(pkg)-uninstall:
 	$(RM) $(BUSYBOX_TARGET_BINARY) $(BUSYBOX_TARGET_BINARY).links
-
-$(pkg)-dirclean:
-
-$(pkg)-distclean: busybox-dirclean
 
 $(PKG_FINISH)
