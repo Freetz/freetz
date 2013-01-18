4c1d83f7285884ac16e42fb3eac2339381c2a643
Gene Rudoy <gene@freetz.org>
2013-01-18 22:49:32 +0000

    imagemagick:  * bump version to 6.8.1-10
    git-svn-id: http://svn.freetz.org/trunk@9947 f5190166-0702-4917-9039-51ec32eddaf5

--- make/imagemagick/imagemagick.mk
+++ make/imagemagick/imagemagick.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 6.8.1-9)
+$(call PKG_INIT_BIN, 6.8.1-10)
 $(PKG)_SOURCE:=ImageMagick-$($(PKG)_VERSION).tar.xz
-$(PKG)_SOURCE_MD5:=3a85fce351ff38780a60626395756ba9
+$(PKG)_SOURCE_MD5:=a6ca7bf812136263ecafc02bacccd6a7
 $(PKG)_SITE:=http://www.$(pkg).org/download
 
 $(PKG)_DIR:=$(SOURCE_DIR)/ImageMagick-$($(PKG)_VERSION)
