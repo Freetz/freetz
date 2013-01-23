1d561393c5287b309bf5f66c36ade8c6e4e1398a
cuma <cuma@freetz.org>
2013-01-23 16:08:59 +0000

    EXPERIMENTAL  * patching ctlmgt to save password to another file than /etc/passwd which will be merged by {{{modusers load}}}
    TODO:
     * remove libctlmgr ?
     * run {{{modusers update}}} automatically
    
    !! atm you have to run it manually after  after password changes by avm webif !!
    
    (refs #1958)
    
    
    git-svn-id: http://svn.freetz.org/trunk@9958 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/101-ctlmgr_wrapper.sh
+++ patches/scripts/101-ctlmgr_wrapper.sh
@@ -1,5 +1,12 @@
-[ "$FREETZ_LIB_libctlmgr" == "y" ] || return 0
 
+ln -s "../var/tmp/passxx" "$FILESYSTEM_MOD_DIR/etc/passxx"
+$TOOLS_DIR/sfk replace "$FILESYSTEM_MOD_DIR/usr/bin/ctlmgr" "_/etc/passwd_/etc/passxx_" -yes
+$TOOLS_DIR/sfk replace "$FILESYSTEM_MOD_DIR/usr/bin/ctlmgr" "_/var/tmp/passwd_/var/tmp/passxx_" -yes
+
+
+
+#TODO: remove
+[ "$FREETZ_LIB_libctlmgr" == "y" ] || return 0
 mkdir -p "$FILESYSTEM_MOD_DIR/usr/bin/avm/"
 mv "$FILESYSTEM_MOD_DIR/usr/bin/ctlmgr" "$FILESYSTEM_MOD_DIR/usr/bin/avm/ctlmgr"
 
@@ -10,3 +17,4 @@ exec /usr/bin/avm/ctlmgr "$@"
 EOF
 
 chmod 755 "$FILESYSTEM_MOD_DIR/usr/bin/ctlmgr"
+
