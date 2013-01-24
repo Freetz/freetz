04fe1cb7948d1618653935a3df9be8ef8f08aa65
cuma <cuma@freetz.org>
2013-01-24 17:28:10 +0000

    add FRITZ.Box_6840_LTE_BETA.105.05.29-24421 (refs #1910)
    git-svn-id: http://svn.freetz.org/trunk@9961 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ patches/scripts/108-tffs_nodes.sh
@@ -0,0 +1,14 @@
+[ "$FREETZ_AVM_VERSION_05_5X" == "y" ] || return 0
+echo1 "applying tffs_nodes patch"
+# This creates the char device for saving Freetz "flash"
+
+for f in S08-tffs S01-head; do
+	grep -q '^tffs_nodes_list=""$' "${FILESYSTEM_MOD_DIR}/etc/init.d/$f" && break
+	f=""
+done
+[ -z "$f" ] && error 1 "can't find suitable file for adding node"
+
+modsed \
+  's/\(tffs_nodes_list=\)""/\1"$((0x3C)),freetz"/' \
+  "${FILESYSTEM_MOD_DIR}/etc/init.d/$f" \
+  '$((0x3C)),freetz'
