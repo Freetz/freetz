44193922f4b66d37a6a306c75eaec4fc9406d23d
cuma <cuma@freetz.org>
2013-01-28 21:22:13 +0000

    bump busybox to 1.21.0
    git-svn-id: http://svn.freetz.org/trunk@10002 f5190166-0702-4917-9039-51ec32eddaf5

--- make/busybox/busybox.mk
+++ make/busybox/busybox.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 1.20.2)
+$(call PKG_INIT_BIN, 1.21.0)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=e025414bc6cd79579cc7a32a45d3ae1c
+$(PKG)_SOURCE_MD5:=d613f2e4b580305c1de8691f7b84285e
 $(PKG)_SITE:=http://www.busybox.net/downloads
 
 $(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/$(pkg)-$($(PKG)_VERSION)
