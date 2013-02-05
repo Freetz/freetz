3ac0f7e30255807648a11dd48105616316c62280
cuma <cuma@freetz.org>
2013-02-05 15:02:28 +0000

    remove_webdav: add some files (refs #1838)
    git-svn-id: http://svn.freetz.org/trunk@10040 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/540-remove_webdav.sh
+++ patches/scripts/540-remove_webdav.sh
@@ -7,12 +7,14 @@ for files in \
 	sbin/*mount.davfs \
 	etc/onlinechanged/webdav_net \
 	etc/webdav_control \
+	usr/www/all/html/de/home/home_webdav.txt \
+	bin/webdavcfginfo \
 	; do
 	rm_files "${FILESYSTEM_MOD_DIR}/$files"
 done
 
 if [ ! "$FREETZ_PACKAGE_DAVFS2" == "y" ]; then
-	# TODO: Remove "Online-Speicher" from webinterface
+	# TODO: Remove "Online-Speicher" from webinterface and remove usr/www/all/lua/webdav.lua
 	rm_files "${FILESYSTEM_MOD_DIR}/usr/share/ctlmgr/libctlwebdav.so"
 	echo1 "patching rc.conf"
 	modsed "s/CONFIG_WEBDAV=.*$/CONFIG_WEBDAV=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
