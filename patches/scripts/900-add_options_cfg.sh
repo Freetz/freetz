2a3cebff83e149795d91ceccd287c8aade1489e4
cuma <cuma@freetz.org>
2013-02-05 01:09:34 +0000

    add FREETZ_TAGET_(IPV6_SUPPORT) to options.cfg, used by openvpn
    git-svn-id: http://svn.freetz.org/trunk@10033 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/900-add_options_cfg.sh
+++ patches/scripts/900-add_options_cfg.sh
@@ -7,7 +7,7 @@ if [ "$FREETZ_CREATE_SEPARATE_OPTIONS_CFG" != "y" ]; then
 	ln -snf .config $OPTIONS_CFG
 else
 	OPTIONS_FILES="$(find make/*/files/root/etc/init.d/rc.* make/*/files/root/etc/default.*/*_conf make/*/files/root/usr/lib/cgi-bin/*.cgi)"
-	OPTIONS_NAMES="$(grep -hoE "FREETZ_REPLACE_KERNEL|FREETZ_AVMDAEMON_DISABLE_[A-Z0-9]*|EXTERNAL_FREETZ_PACKAGE[a-zA-Z0-9_]*|FREETZ_PACKAGE[a-zA-Z0-9_]*" $OPTIONS_FILES | sort -u)"
+	OPTIONS_NAMES="$(grep -hoE "FREETZ_TARGET_[A-Z0-9_]*|FREETZ_REPLACE_KERNEL|FREETZ_AVMDAEMON_DISABLE_[A-Z0-9]*|EXTERNAL_FREETZ_PACKAGE[a-zA-Z0-9_]*|FREETZ_PACKAGE[a-zA-Z0-9_]*" $OPTIONS_FILES | sort -u)"
 	for OPTIONS_CURRENT in $OPTIONS_NAMES; do
 		OPTIONS_VALUE="$(eval echo \$$OPTIONS_CURRENT)"
 		if [ "$OPTIONS_VALUE" != "n" -a "$OPTIONS_VALUE" != "" ]; then
