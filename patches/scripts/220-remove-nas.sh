9148c7f85a1070b6274500d8343482ec20ddb142
cuma <cuma@freetz.org>
2013-02-05 12:50:48 +0000

    remove_nas: {{{require("call_webusb")}}} is required even without "nas" (closes #2011)
    git-svn-id: http://svn.freetz.org/trunk@10038 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/220-remove-nas.sh
+++ patches/scripts/220-remove-nas.sh
@@ -5,6 +5,10 @@ rm -rf "${FILESYSTEM_MOD_DIR}/bin/showfritznasdbstat"
 rm -rf "${FILESYSTEM_MOD_DIR}/usr/www.nas"
 ln -sf www "${FILESYSTEM_MOD_DIR}/usr/www.nas"
 
+# patcht Heimnetz > Speicher (NAS). Fehler von AVM? http://freetz.org/ticket/2011
+modsed '/^if config.NAS then$/{N;s/^.*\(\nrequire("call_webusb")\)/if true then\1/g}' "${HTML_LANG_MOD_DIR}/storage/settings.lua"
+
+# entfernt Links zu Fritz.NAS
 quickstart_remove nas
 modsed '/.*class="sm_link_bold" href=".nas.*/d' "${HTML_SPEC_MOD_DIR}/menus/menu2.html"
 
