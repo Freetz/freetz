356d056519bd79bb9acd2ebe408be1bd89142d93
Gene Rudoy <gene@freetz.org>
2013-02-07 00:27:09 +0000

    * add new package html2text (by b_s101 + fixes by me)  * refs #2012
    git-svn-id: http://svn.freetz.org/trunk@10051 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/html2text/html2text.mk
@@ -0,0 +1,34 @@
+$(call PKG_INIT_BIN, 1.3.2a)
+$(PKG)_SOURCE:=html2text-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=6097fe07b948e142315749e6620c9cfc
+$(PKG)_SITE:=http://www.mbayer.de/html2text/downloads
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/html2text
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/html2text
+
+$(PKG)_DEPENDS_ON := $(STDCXXLIB)
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_STDCXXLIB
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(HTML2TEXT_DIR)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(HTML2TEXT_DIR) clean
+	$(RM) $(HTML2TEXT_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(HTML2TEXT_TARGET_BINARY)
+
+$(PKG_FINISH)
