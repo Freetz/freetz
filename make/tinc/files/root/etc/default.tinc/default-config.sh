32c8de10ce540200ba7fc5034af624f7fb8e058b
cuma <cuma@freetz.org>
2013-02-08 14:56:27 +0000

    add tinc webif (refs #2018)
    git-svn-id: http://svn.freetz.org/trunk@10059 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/tinc/files/root/etc/default.tinc/default-config.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+. /mod/etc/conf/tinc.cfg
+
+
+if [ -n "$TINC_SERVER1" -a ! -e "/tmp/flash/tinc/hosts/$TINC_SERVER1" ]; then
+cat << EOF > "/tmp/flash/tinc/hosts/$TINC_SERVER1"
+Address          = server1.dyndns.org
+Port             = 10655
+
+IndirectData     = no
+PMTUDiscovery    = yes
+Compression      = 0
+
+Subnet           = 192.168.201.0/24
+Subnet           = 192.168.200.201/32
+
+-----BEGIN RSA PUBLIC KEY-----
+#
+-----END RSA PUBLIC KEY-----
+EOF
+fi
+
+
+if [ -n "$TINC_SERVER2" -a ! -e "/tmp/flash/tinc/hosts/$TINC_SERVER2" ]; then
+cat << EOF > "/tmp/flash/tinc/hosts/$TINC_SERVER2"
+Address          = server2.dyndns.org
+Port             = 10655
+
+IndirectData     = no
+PMTUDiscovery    = yes
+Compression      = 0
+
+Subnet           = 192.168.202.0/24
+Subnet           = 192.168.200.202/32
+
+-----BEGIN RSA PUBLIC KEY-----
+#
+-----END RSA PUBLIC KEY-----
+EOF
+fi
+
+
+if [ -n "$TINC_SERVER3" -a ! -e "/tmp/flash/tinc/hosts/$TINC_SERVER3" ]; then
+cat << EOF > "/tmp/flash/tinc/hosts/$TINC_SERVER3"
+Address          = server3.dyndns.org
+Port             = 10655
+
+IndirectData     = no
+PMTUDiscovery    = yes
+Compression      = 0
+
+Subnet           = 192.168.203.0/24
+Subnet           = 192.168.200.203/32
+
+-----BEGIN RSA PUBLIC KEY-----
+#
+-----END RSA PUBLIC KEY-----
+EOF
+fi
+
+
+if [ ! -e /tmp/flash/tinc/tinc-down ]; then
+cat << EOF > /tmp/flash/tinc/tinc-down
+ifconfig \$INTERFACE down
+EOF
+chmod + /tmp/flash/tinc/tinc-down
+fi
+
+
+if [ ! -e /tmp/flash/tinc/tinc-up ]; then
+cat << EOF > /tmp/flash/tinc/tinc-up
+ifconfig \$INTERFACE down
+ifconfig \$INTERFACE 192.168.200.200 netmask 255.255.255.0 up
+ifconfig \$INTERFACE up
+#route add -net 192.168.201.0/24 gateway 192.168.200.201
+#route add -net 192.168.202.0/24 gateway 192.168.200.202
+#route add -net 192.168.203.0/24 gateway 192.168.200.203
+EOF
+chmod +x /tmp/flash/tinc/tinc-up
+fi
+
---
+++ make/tinc/files/root/usr/lib/cgi-bin/tinc.cgi
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+. /usr/lib/libmodcgi.sh
+
+
+#
+
+sec_begin '$(lang de:"Starttyp" en:"Start type")'
+
+cgi_print_radiogroup_service_starttype "enabled" "$TINC_ENABLED" "" "" 0
+
+sec_end
+
+#
+
+sec_begin '$(lang de:"Konfiguration" en:"Configuration")'
+
+cgi_print_textline_p "server0" "$TINC_SERVER0" 25/255 "$(lang de:"Alias dieses Servers" en:"Alias of this server"): "
+
+cgi_print_textline_p "server1" "$TINC_SERVER1" 25/255 "$(lang de:"Alias von Server 1" en:"Alias of server 1"): "
+cgi_print_textline_p "server2" "$TINC_SERVER2" 25/255 "$(lang de:"Alias von Server 2" en:"Alias of server 2"): "
+cgi_print_textline_p "server3" "$TINC_SERVER3" 25/255 "$(lang de:"Alias von Server 3" en:"Alias of server 3"): "
+
+sec_end
+
+#
+
+sec_begin '$(lang de:"Erweiterte" en:"Advanced")'
+
+cgi_print_textline_p "debuglevel" "$TINC_DEBUGLEVEL" 1/1 "$(lang de:"Debuglevel" en:"Debug level")(0-5)"
+
+cgi_print_checkbox_p "logfile_enabled" "$TINC_LOGFILE_ENABLED" "$(lang de:"In Datei loggen" en:"Log to file"): "
+cgi_print_textline_p "logfile_name" "$TINC_LOGFILE_NAME" 55/255 "$(lang de:"Logdatei" en:"Log file"): "
+
+
+sec_end
