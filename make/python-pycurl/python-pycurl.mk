413e06e6c95f30e90c0f66d6750b387950913fb3
Gene Rudoy <gene@freetz.org>
2013-02-09 11:58:37 +0000

    * add new package python-pycurl (a python module)  * compile-tested only  * refs #1839
    git-svn-id: http://svn.freetz.org/trunk@10071 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/python-pycurl/python-pycurl.mk
@@ -0,0 +1,40 @@
+$(call PKG_INIT_BIN, 7.19.0)
+$(PKG)_SOURCE:=pycurl-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=919d58fe37e69fe87ce4534d8b6a1c7b
+$(PKG)_SITE:=http://pycurl.sourceforge.net/download
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/pycurl-$($(PKG)_VERSION)
+
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/pycurl.so
+
+$(PKG)_DEPENDS_ON := python curl
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PYTHON_STATIC
+$(PKG)_REBUILD_SUBOPTS += $(filter FREETZ_LIB_libcurl%,$(CURL_REBUILD_SUBOPTS))
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
+	$(call Build/PyMod, \
+		$(PYTHON_PYCURL_DIR), \
+		install --prefix=/usr --root=$(abspath $(PYTHON_PYCURL_DEST_DIR)) \
+		--curl-config=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/curl-config \
+	)
+	$(TARGET_STRIP) $@
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(PYTHON_PYCURL_DIR) clean
+
+$(pkg)-uninstall:
+	$(RM) -r \
+		$(PYTHON_PYCURL_TARGET_BINARY) \
+		$(PYTHON_PYCURL_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/pycurl-*.egg-info \
+		$(PYTHON_PYCURL_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/curl
+
+$(PKG_FINISH)
