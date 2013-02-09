96b4a8b5798f6699a1730a123b4d2a71adf97ba4
Gene Rudoy <gene@freetz.org>
2013-02-09 18:58:50 +0000

    * add new python-module PIL  * compile-tested only  * refs #1839
    git-svn-id: http://svn.freetz.org/trunk@10076 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/python-imaging-library/python-imaging-library.mk
@@ -0,0 +1,41 @@
+$(call PKG_INIT_BIN, 1.1.7)
+$(PKG)_SOURCE:=Imaging-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=fc14a54e1ce02a0225be8854bfba478e
+$(PKG)_SITE:=http://effbot.org/downloads
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/Imaging-$($(PKG)_VERSION)
+
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/PIL/_imaging.so
+
+$(PKG)_DEPENDS_ON := python freetype jpeg zlib
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PYTHON_STATIC
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
+	$(call Build/PyMod, \
+		$(PYTHON_IMAGING_LIBRARY_DIR), \
+		install --prefix=/usr --root=$(abspath $(PYTHON_IMAGING_LIBRARY_DEST_DIR)), \
+		TARGET_ARCH="$(FREETZ_TARGET_ARCH)", \
+		$(PYTHON_IMAGING_LIBRARY_DEST_DIR)$(PYTHON_SITE_PKG_DIR) \
+	)
+	$(RM) -r $(PYTHON_IMAGING_LIBRARY_DEST_DIR)/usr/bin
+	(cd $(PYTHON_IMAGING_LIBRARY_DEST_DIR)$(PYTHON_SITE_PKG_DIR); mv PIL/PIL-*.egg-info .;)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	$(RM) -r $(PYTHON_IMAGING_LIBRARY_DIR)/build
+
+$(pkg)-uninstall:
+	$(RM) -r \
+		$(PYTHON_IMAGING_LIBRARY_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/PIL \
+		$(PYTHON_IMAGING_LIBRARY_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/PIL.pth \
+		$(PYTHON_IMAGING_LIBRARY_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/PIL-*.egg-info
+
+$(PKG_FINISH)
