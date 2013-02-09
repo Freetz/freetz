5cfe164be96edfea0d0c98303c4b2d7847e8245c
Gene Rudoy <gene@freetz.org>
2013-02-09 16:16:20 +0000

    * add new python-module PyCrypto  * compile-tested only  * refs #1839
    git-svn-id: http://svn.freetz.org/trunk@10073 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/python-pycrypto/python-pycrypto.mk
@@ -0,0 +1,38 @@
+$(call PKG_INIT_BIN, 2.6)
+$(PKG)_SOURCE:=pycrypto-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=88dad0a270d1fe83a39e0467a66a22bb
+$(PKG)_SITE:=http://ftp.dlitz.net/pub/dlitz/crypto/pycrypto
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/pycrypto-$($(PKG)_VERSION)
+
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/Crypto/PublicKey/_fastmath.so
+
+$(PKG)_DEPENDS_ON := python gmp
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PYTHON_STATIC
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
+	$(call Build/PyMod, \
+		$(PYTHON_PYCRYPTO_DIR), \
+		install --prefix=/usr --root=$(abspath $(PYTHON_PYCRYPTO_DEST_DIR)), \
+		TARGET_ARCH="$(FREETZ_TARGET_ARCH)" \
+	)
+	find $(dir $@) -type f -name "*.so" -exec $(TARGET_STRIP) \{\} \+
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	$(RM) -r $(PYTHON_PYCRYPTO_DIR)/build
+
+$(pkg)-uninstall:
+	$(RM) -r \
+		$(PYTHON_PYCRYPTO_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/Crypto \
+		$(PYTHON_PYCRYPTO_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/pycrypto-*.egg-info
+
+$(PKG_FINISH)
