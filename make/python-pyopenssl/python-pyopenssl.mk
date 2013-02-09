6ae980b5f419038d4028c8936669e783a8467cc4
Gene Rudoy <gene@freetz.org>
2013-02-09 12:44:13 +0000

    * add new python-module pyOpenSSL  * compile-tested only  * refs #1839
    git-svn-id: http://svn.freetz.org/trunk@10072 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/python-pyopenssl/python-pyopenssl.mk
@@ -0,0 +1,38 @@
+$(call PKG_INIT_BIN, 0.13)
+$(PKG)_SOURCE:=pyopenssl_$($(PKG)_VERSION).orig.tar.gz
+$(PKG)_SOURCE_MD5:=767bca18a71178ca353dff9e10941929
+$(PKG)_SITE:=http://ftp.de.debian.org/debian/pool/main/p/pyopenssl
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/pyOpenSSL-$($(PKG)_VERSION)
+
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/OpenSSL/SSL.so
+
+$(PKG)_DEPENDS_ON := python openssl
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PYTHON_STATIC
+$(PKG)_REBUILD_SUBOPTS += $(OPENSSL_REBUILD_SUBOPTS)
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
+	$(call Build/PyMod, \
+		$(PYTHON_PYOPENSSL_DIR), \
+		install --prefix=/usr --root=$(abspath $(PYTHON_PYOPENSSL_DEST_DIR)) \
+	)
+	$(TARGET_STRIP) $(dir $@)*.so
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	$(RM) -r $(PYTHON_PYOPENSSL_DIR)/build
+
+$(pkg)-uninstall:
+	$(RM) -r \
+		$(PYTHON_PYOPENSSL_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/OpenSSL \
+		$(PYTHON_PYOPENSSL_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/pyOpenSSL-*.egg-info
+
+$(PKG_FINISH)
