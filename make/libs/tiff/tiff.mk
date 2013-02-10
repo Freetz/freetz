26df6de6632ab68f87de2ae1365fbb2e427f428c
Gene Rudoy <gene@freetz.org>
2013-02-10 12:31:05 +0000

    libtiff:  * bump version to 3.9.7  * makefile fixes, cleanups, typo fixes  * fix external not available in menuconfig  * provide C++ version of the library  * refs #1726
    git-svn-id: http://svn.freetz.org/trunk@10082 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/tiff/tiff.mk
+++ make/libs/tiff/tiff.mk
@@ -1,20 +1,19 @@
-$(call PKG_INIT_LIB, 3.9.5)
-$(PKG)_LIB_VERSION:=3.9.5
-$(PKG)_SOURCE:=tiff-$($(PKG)_LIB_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=8fc7ce3b4e1d0cc8a319336967815084
+$(call PKG_INIT_LIB, 3.9.7)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=626102f448ba441d42e3212538ad67d2
 $(PKG)_SITE:=ftp://ftp.remotesensing.org/pub/libtiff
 
-$(PKG)_LIBNAMES_SHORT := libtiff
-# to include also libtiffxx:
-#$(PKG)_LIBNAMES_SHORT := libtiff libtiffxx
-$(PKG)_LIBNAMES_LONG := $($(PKG)_LIBNAMES_SHORT:%=%.so)
+$(PKG)_LIBNAMES_SHORT := libtiff libtiffxx
+$(PKG)_LIBNAMES_LONG := $($(PKG)_LIBNAMES_SHORT:%=%.so.$($(PKG)_VERSION))
 
-$(PKG)_BINARIES :=$($(PKG)_LIBNAMES_LONG:%=$(PKG)_DIR)/.libs/%)
+$(PKG)_BINARIES :=$($(PKG)_LIBNAMES_LONG:%=$($(PKG)_DIR)/libtiff/.libs/%)
 $(PKG)_STAGING_BINARIES := $($(PKG)_LIBNAMES_LONG:%=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/%)
 $(PKG)_TARGET_BINARIES := $($(PKG)_LIBNAMES_LONG:%=$($(PKG)_TARGET_DIR)/%)
 
-$(PKG)_DEPENDS_ON := zlib
-$(PKG)_DEPENDS_ON += jpeg
+$(PKG)_DEPENDS_ON := $(STDCXXLIB) zlib jpeg
+$(PKG)_REBUILD_SUBOPTS += FREETZ_STDCXXLIB
+
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
 
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
@@ -35,7 +34,7 @@ $($(PKG)_STAGING_BINARIES): $($(PKG)_BINARIES)
 	$(PKG_FIX_LIBTOOL_LA) \
 		$(TIFF_LIBNAMES_SHORT:%=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/%.la)
 
-$($(PKG)_TARGET_BINARIES): $($(PKG)_DEST_DIR)/usr/lib/freetz/%.so: $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/%.so
+$($(PKG)_TARGET_BINARIES): $($(PKG)_TARGET_DIR)/%: $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/%
 	$(INSTALL_LIBRARY_STRIP)
 
 $(pkg): $($(PKG)_STAGING_BINARIES)
@@ -44,10 +43,11 @@ $(pkg)-precompiled: $($(PKG)_TARGET_BINARIES)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(TIFF_DIR) clean
-	$(RM) $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libtiff* \
+	$(RM) \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libtiff* \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/tiff*
 
 $(pkg)-uninstall:
-	$(RM) $(LIBTIFF_TARGET_DIR)/libtiff*.so*
+	$(RM) $(TIFF_TARGET_DIR)/libtiff*.so*
 
 $(PKG_FINISH)
