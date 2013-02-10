1df8980ffe32792e1921bb4f9418daffc2695b2b
Gene Rudoy <gene@freetz.org>
2013-02-10 13:32:01 +0000

    tiff:  * convert to binary package  * provide some utils  * refs #1726
    git-svn-id: http://svn.freetz.org/trunk@10083 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/tiff/tiff.mk
@@ -0,0 +1,80 @@
+$(call PKG_INIT_BIN, 3.9.7)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=626102f448ba441d42e3212538ad67d2
+$(PKG)_SITE:=ftp://ftp.remotesensing.org/pub/libtiff
+
+$(PKG)_BINARIES_ALL := bmp2tiff fax2ps fax2tiff gif2tiff ppm2tiff raw2tiff tiff2bw tiff2pdf tiff2ps tiffinfo tiffsplit
+$(PKG)_BINARIES := $(call PKG_SELECTED_SUBOPTIONS,$($(PKG)_BINARIES_ALL))
+$(PKG)_BINARIES_BUILD_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DIR)/tools/.libs/%)
+$(PKG)_BINARIES_TARGET_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DEST_DIR)/usr/bin/%)
+
+$(PKG)_NOT_INCLUDED := $(patsubst %,$($(PKG)_DEST_DIR)/usr/bin/%,$(filter-out $($(PKG)_BINARIES),$($(PKG)_BINARIES_ALL)))
+
+$(PKG)_LIBNAMES_SHORT := libtiff libtiffxx
+$(PKG)_LIBNAMES_LONG := $($(PKG)_LIBNAMES_SHORT:%=%.so.$($(PKG)_VERSION))
+
+$(PKG)_LIBS_BUILD_DIR :=$($(PKG)_LIBNAMES_LONG:%=$($(PKG)_DIR)/libtiff/.libs/%)
+$(PKG)_LIBS_STAGING_DIR := $($(PKG)_LIBNAMES_LONG:%=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/%)
+$(PKG)_LIBS_TARGET_DIR := $($(PKG)_LIBNAMES_LONG:%=$($(PKG)_TARGET_LIBDIR)/%)
+
+$(PKG)_DEPENDS_ON := $(STDCXXLIB) zlib jpeg
+$(PKG)_REBUILD_SUBOPTS += FREETZ_STDCXXLIB
+
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
+
+$(PKG)_CONFIGURE_OPTIONS += --enable-shared
+$(PKG)_CONFIGURE_OPTIONS += --enable-static
+$(PKG)_CONFIGURE_OPTIONS += --enable-cxx=yes
+$(PKG)_CONFIGURE_OPTIONS += --enable-ccitt
+$(PKG)_CONFIGURE_OPTIONS += --enable-packbits
+$(PKG)_CONFIGURE_OPTIONS += --enable-lzw
+$(PKG)_CONFIGURE_OPTIONS += --enable-thunder
+$(PKG)_CONFIGURE_OPTIONS += --enable-next
+$(PKG)_CONFIGURE_OPTIONS += --enable-logluv
+$(PKG)_CONFIGURE_OPTIONS += --enable-mdi
+$(PKG)_CONFIGURE_OPTIONS += --enable-zlib
+$(PKG)_CONFIGURE_OPTIONS += --enable-jpeg
+$(PKG)_CONFIGURE_OPTIONS += --disable-old-jpeg
+$(PKG)_CONFIGURE_OPTIONS += --disable-jbig
+$(PKG)_CONFIGURE_OPTIONS += --without-apple-opengl-framework
+
+$(PKG)_CONFIGURE_OPTIONS += --with-jpeg-include-dir="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include"
+$(PKG)_CONFIGURE_OPTIONS += --with-jpeg-lib-dir="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib"
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARIES_BUILD_DIR) $($(PKG)_LIBS_BUILD_DIR): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(TIFF_DIR)
+
+$($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/tools/.libs/%
+	$(INSTALL_BINARY_STRIP)
+
+$($(PKG)_LIBS_STAGING_DIR): $($(PKG)_LIBS_BUILD_DIR)
+	$(SUBMAKE) -C $(TIFF_DIR)/libtiff \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+		install
+	$(PKG_FIX_LIBTOOL_LA) \
+		$(TIFF_LIBNAMES_SHORT:%=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/%.la)
+
+$($(PKG)_LIBS_TARGET_DIR): $($(PKG)_TARGET_LIBDIR)/%: $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/%
+	$(INSTALL_LIBRARY_STRIP)
+
+$(pkg): $($(PKG)_LIBS_STAGING_DIR)
+
+$(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR) $($(PKG)_LIBS_TARGET_DIR)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(TIFF_DIR) clean
+	$(RM) \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libtiff* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/tiff*
+
+$(pkg)-uninstall:
+	$(RM) \
+		$(TIFF_BINARIES_ALL:%=$(TIFF_DEST_DIR)/usr/bin/%) \
+		$(TIFF_TARGET_DIR)/libtiff*.so*
+
+$(call PKG_ADD_LIB,libtiff)
+$(PKG_FINISH)
