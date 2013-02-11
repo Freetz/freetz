0e41670a3789a16fdd069c070c4e6e3ec432125e
Whoopie <whoopie79@gmx.net>
2013-02-11 11:28:02 +0000

    sslh: add new package, version 1.14
    git-svn-id: http://svn.freetz.org/trunk@10094 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/sslh/sslh.mk
@@ -0,0 +1,34 @@
+$(call PKG_INIT_BIN,1.14)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=36e64bd6c7d0a299d5acd1109988099b
+$(PKG)_SITE:=http://rutschle.net/tech
+$(PKG)_BINARY:=$($(PKG)_DIR)/sslh-fork
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/sslh
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(SSLH_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)" \
+		USELIBCONFIG= \
+		USELIBWRAP= \
+		sslh
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(SSLH_DIR) clean
+	$(RM) $(SSLH_FREETZ_CONFIG_FILE)
+
+$(pkg)-uninstall:
+	$(RM) $(SSLH_TARGET_BINARY)
+
+$(PKG_FINISH)
