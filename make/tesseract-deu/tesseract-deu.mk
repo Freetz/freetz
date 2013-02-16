d8128a087f9d295cd053594b0d1afd975e5decd4
Gene Rudoy <gene@freetz.org>
2013-02-16 12:17:47 +0000

    tesseract:  * add language files
    git-svn-id: http://svn.freetz.org/trunk@10113 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/tesseract-deu/tesseract-deu.mk
@@ -0,0 +1,30 @@
+$(call PKG_INIT_BIN, 3.02)
+$(PKG)_SOURCE:=tesseract-ocr-$($(PKG)_VERSION).deu.tar.gz
+$(PKG)_SOURCE_MD5:=57bdb26ec7c767e126ff97776d8bfb10
+$(PKG)_SITE:=http://tesseract-ocr.googlecode.com/files
+
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/share/tessdata/deu.traineddata
+
+define $(PKG)_CUSTOM_UNPACK
+mkdir -p $($(PKG)_DIR); $(call UNPACK_CMD,$(1),$($(PKG)_DIR))
+endef
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
+	@mkdir -p $(dir $@); \
+	cp $(TESSERACT_DEU_DIR)/tesseract-ocr/tessdata/deu.* $(dir $@); \
+	touch -c $@
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+
+$(pkg)-uninstall:
+	$(RM) -r $(dir $(TESSERACT_DEU_TARGET_BINARY))
+
+$(PKG_FINISH)
---
+++ make/tesseract-eng/tesseract-eng.mk
@@ -0,0 +1,30 @@
+$(call PKG_INIT_BIN, 3.02)
+$(PKG)_SOURCE:=tesseract-ocr-$($(PKG)_VERSION).eng.tar.gz
+$(PKG)_SOURCE_MD5:=3562250fe6f4e76229a329166b8ae853
+$(PKG)_SITE:=http://tesseract-ocr.googlecode.com/files
+
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/share/tessdata/eng.traineddata
+
+define $(PKG)_CUSTOM_UNPACK
+mkdir -p $($(PKG)_DIR); $(call UNPACK_CMD,$(1),$($(PKG)_DIR))
+endef
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
+	@mkdir -p $(dir $@); \
+	cp $(TESSERACT_ENG_DIR)/tesseract-ocr/tessdata/eng.* $(dir $@); \
+	touch -c $@
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+
+$(pkg)-uninstall:
+	$(RM) -r $(dir $(TESSERACT_ENG_TARGET_BINARY))
+
+$(PKG_FINISH)
