fba78c8133db7a9e011b998d0227dfd946ae9460
Gene Rudoy <gene@freetz.org>
2013-03-02 16:15:37 +0000

    kconfig:  * use git_archive method to download kconfig sources, the old method didn't work anymore (s. [http://www.ip-phone-forum.de/showthread.php?t=257466 this thread] for details)  * correct kconfig version (the proper one is some pre-3.8-git)  * add git to the list of build prerequisites
    git-svn-id: http://svn.freetz.org/trunk@10216 f5190166-0702-4917-9039-51ec32eddaf5

--- tools/make/kconfig.mk
+++ tools/make/kconfig.mk
@@ -1,24 +1,16 @@
-KCONFIG_VERSION:=3.7
-KCONFIG_SOURCE:=kconfig-$(KCONFIG_VERSION).tar.gz
-# Cannot check MD5 because URL produces slightly different archives even
-# though unpacked contents are identical.
-#KCONFIG_SOURCE_MD5:=xxx
-KCONFIG_SITE:=http://git.kernel.org/?p=linux/kernel/git/torvalds/linux.git;a=snapshot;h=80c5770e79175a33026cf083a894fd4ebb0b9f1e;sf=tgz
+KCONFIG_VERSION:=v3.8
+KCONFIG_SOURCE:=kconfig-$(KCONFIG_VERSION).tar.xz
+KCONFIG_SITE:=git_archive@git://repo.or.cz/linux-2.6.git,scripts/basic,scripts/kconfig,scripts/Kbuild.include,scripts/Makefile.build,scripts/Makefile.host,scripts/Makefile.lib
 KCONFIG_DIR:=$(TOOLS_SOURCE_DIR)/kconfig-$(KCONFIG_VERSION)
 KCONFIG_MAKE_DIR:=$(TOOLS_DIR)/make
 KCONFIG_TARGET_DIR:=$(TOOLS_DIR)/config
 
-# TODO: Replace by normal use of $(DL_TOOL) as soon as it supports saving
-# files from URLs under another name with '-O'
+kconfig-source: $(DL_DIR)/$(KCONFIG_SOURCE)
 $(DL_DIR)/$(KCONFIG_SOURCE): | $(DL_DIR)
-	wget -O $(DL_DIR)/$(KCONFIG_SOURCE) "$(KCONFIG_SITE)"
-
-kconfig-download: $(DL_DIR)/$(KCONFIG_SOURCE)
+	$(DL_TOOL) $(DL_DIR) $(KCONFIG_SOURCE) $(KCONFIG_SITE)
 
 $(KCONFIG_DIR)/.unpacked: $(DL_DIR)/$(KCONFIG_SOURCE) | $(TOOLS_SOURCE_DIR)
-	mkdir -p $(KCONFIG_DIR)/scripts
-	tar -C $(KCONFIG_DIR)/scripts $(VERBOSE) --wildcards --strip-components=1 \
-		-xf $(DL_DIR)/$(KCONFIG_SOURCE) */basic */kconfig */Makefile.{build,host,lib} */Kbuild.include
+	tar -C $(TOOLS_SOURCE_DIR) $(VERBOSE) -xf $(DL_DIR)/$(KCONFIG_SOURCE)
 	for i in $(KCONFIG_MAKE_DIR)/patches/*.kconfig.patch; do \
 		$(PATCH_TOOL) $(KCONFIG_DIR) $$i; \
 	done;
@@ -60,4 +52,4 @@ kconfig-distclean:
 		$(KCONFIG_TARGET_DIR)/conf \
 		$(KCONFIG_TARGET_DIR)/mconf
 
-.PHONY: kconfig-download kconfig-unpacked kconfig kconfig-clean kconfig-dirclean kconfig-distclean
+.PHONY: kconfig-source kconfig-unpacked kconfig kconfig-clean kconfig-dirclean kconfig-distclean
