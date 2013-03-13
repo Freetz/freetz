7b0b7d013311a076eed91ae078f0fe46edb51910
cuma <cuma@freetz.org>
2013-03-13 00:59:49 +0000

    remove_chronyd: patch webif, thx schlimmchen (closes #2041)
    git-svn-id: http://svn.freetz.org/trunk@10255 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/220-chronyd.sh
+++ patches/scripts/220-chronyd.sh
@@ -1,11 +1,27 @@
 [ "$FREETZ_HAS_AVM_CHRONYD" == "y" ] || return 0
 
-if [ "$FREETZ_REMOVE_CHRONYD" == "y" ]; then 
+if [ "$FREETZ_REMOVE_CHRONYD" == "y" ]; then
 	echo1 "remove chronyd files"
 	rm_files "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.chrony" \
 		"${FILESYSTEM_MOD_DIR}/etc/onlinechanged/chrony" \
 		"${FILESYSTEM_MOD_DIR}/sbin/chronyc" \
 		"${FILESYSTEM_MOD_DIR}/sbin/chronyd"
+		#
+	sedfile="${HTML_LANG_MOD_DIR}/net/network_settings.lua"
+	if [ -e "$sedfile" ]; then
+		# patcht Heimnetz > Netzwerk > Netzwerkeinstellungen > Zeitsynchronisation
+		echo1 "patching ${sedfile##*/}"
+		modsed '/"time:settings\/chrony_enabled"/d' $sedfile
+		#g_var.time_enabled = box.query("time:settings/chrony_enabled") == "1"
+		#cmtable.save_checkbox(ctlmgr_save, "time:settings/chrony_enabled" , "time_server_activ")
+		modsed '/"time:settings\/ntp_server"/d' $sedfile
+		#g_var.time_server = box.query("time:settings/ntp_server")
+		#cmtable.add_var(ctlmgr_save, "time:settings/ntp_server" , box.post.time_server)
+		modsed '/^not_empty(uiViewTimeServerList\/time_server, netset)$/d' $sedfile
+		sedrows=$(cat $sedfile |nl| sed -n 's/^ *\([0-9]*\).*{?859:148?}$/\1/p')
+		sedrowe=$(cat $sedfile |nl| sed -n 's/^ *\([0-9]*\).*<div id="uiShowOtherIpv6Router">$/\1/p')
+		modsed "$((sedrows-2)),$((sedrowe-1))d" $sedfile
+	fi
 else
 	[ -e "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.rtc.sh" ] && \
 		modsed "s#/var/dev/rtc#/dev/rtc#g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.rtc.sh"
