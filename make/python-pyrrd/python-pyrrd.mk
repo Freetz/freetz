0c8c3f2015db0bfa145d48894d7f5a141d5131f0
Gene Rudoy <gene@freetz.org>
2013-03-20 23:42:45 +0000

    * add two new python modules: pyRRD & pyserial (compile tested only)  * requested by molfi in [http://www.ip-phone-forum.de/showthread.php?t=257977 this thread]
    git-svn-id: http://svn.freetz.org/trunk@10279 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/python-pyrrd/python-pyrrd.mk
@@ -0,0 +1,33 @@
+$(call PKG_INIT_BIN, 0.1.0)
+$(PKG)_SOURCE:=PyRRD-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=c33a0760b42a23e45e423b8b9f2cd0b0
+$(PKG)_SITE:=https://pyrrd.googlecode.com/files
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/PyRRD-$($(PKG)_VERSION)
+
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/pyrrd/__init__.py
+
+$(PKG)_DEPENDS_ON := python
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
+	$(call Build/PyMod/PKG, PYTHON_PYRRD)
+	(cd $(dir $@); $(RM) -r backend/tests testing tests)
+	touch -c $@
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	$(RM) -r $(PYTHON_PYRRD_DIR)/build
+
+$(pkg)-uninstall:
+	$(RM) -r \
+		$(PYTHON_PYRRD_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/pyrrd \
+		$(PYTHON_PYRRD_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/PyRRD-*.egg-info
+
+$(PKG_FINISH)
---
+++ make/python-pyserial/python-pyserial.mk
@@ -0,0 +1,32 @@
+$(call PKG_INIT_BIN, 2.6)
+$(PKG)_SOURCE:=pyserial-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=cde799970b7c1ce1f7d6e9ceebe64c98
+$(PKG)_SITE:=https://pypi.python.org/packages/source/p/pyserial
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/pyserial-$($(PKG)_VERSION)
+
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/serial/__init__.py
+
+$(PKG)_DEPENDS_ON := python
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
+	$(call Build/PyMod/PKG, PYTHON_PYSERIAL)
+	touch -c $@
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	$(RM) -r $(PYTHON_PYSERIAL_DIR)/build
+
+$(pkg)-uninstall:
+	$(RM) -r \
+		$(PYTHON_PYSERIAL_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/serial \
+		$(PYTHON_PYSERIAL_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/pyserial-*.egg-info
+
+$(PKG_FINISH)
