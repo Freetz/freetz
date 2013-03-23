92d3d013cc411c8c61bfaffe22cac0637154c795
Gene Rudoy <gene@freetz.org>
2013-03-23 15:57:00 +0000

    * add new package gntp-send - a command line tool for sending notifications to [http://growl.info/ Growl]  * by zickedi
    git-svn-id: http://svn.freetz.org/trunk@10289 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/gntpsend/gntpsend.mk
@@ -0,0 +1,35 @@
+$(call PKG_INIT_BIN, 8241d46903)
+$(PKG)_SOURCE := $(pkg)-$($(PKG)_VERSION).tar.xz
+$(PKG)_SITE := git@https://github.com/mattn/gntp-send.git
+
+$(PKG)_BINARY := $($(PKG)_DIR)/gntp-send
+$(PKG)_TARGET_BINARY := $($(PKG)_DEST_DIR)/usr/bin/gntp-send
+
+$(PKG)_BUILD_PREREQ += libtoolize aclocal automake autoconf
+
+$(PKG)_CONFIGURE_PRE_CMDS += ./autogen.sh;
+
+$(PKG)_CONFIGURE_OPTIONS += --disable-shared
+$(PKG)_CONFIGURE_OPTIONS += --enable-static
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(GNTPSEND_DIR)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(MAKE) -C $(GNTPSEND_DIR) clean
+
+$(pkg)-uninstall:
+	$(RM) $(GNTPSEND_TARGET_BINARY)
+
+$(PKG_FINISH)
