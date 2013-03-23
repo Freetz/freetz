ad0685fb5b0a5ce6f7b49eaf381181822241bbce
Gene Rudoy <gene@freetz.org>
2013-03-23 15:22:04 +0000

    * add new package: python-bjoern - yet another Python WSGI server implementation  * by roadman17  * refs #1643
    git-svn-id: http://svn.freetz.org/trunk@10287 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/python-bjoern/python-bjoern.mk
@@ -0,0 +1,32 @@
+$(call PKG_INIT_BIN, ccd23ee232)
+$(PKG)_SOURCE:=bjoern-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=git@https://github.com/jonashaag/bjoern.git
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/bjoern-$($(PKG)_VERSION)
+
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/bjoern.so
+
+$(PKG)_DEPENDS_ON := python libev
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PYTHON_STATIC
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
+	$(call Build/PyMod/PKG, PYTHON_BJOERN)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(PYTHON_BJOERN_DIR) clean
+
+$(pkg)-uninstall:
+	$(RM) -r \
+		$(PYTHON_BJOERN_TARGET_BINARY) \
+		$(PYTHON_BJOERN_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/bjoern-*.egg-info
+
+$(PKG_FINISH)
