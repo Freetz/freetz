b8cfec11bde35e8308e1fe3805499063a7071d98
Gene Rudoy <gene@freetz.org>
2013-04-20 11:14:28 +0000

    remove_avm_vpn-patch:  * whitespace cleanups
    git-svn-id: http://svn.freetz.org/trunk@10415 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/510-remove_avm_vpn.sh
+++ patches/scripts/510-remove_avm_vpn.sh
@@ -2,14 +2,14 @@
 
 echo1 "removing AVM-VPN files"
 for files in \
- 	bin/avmike \
+	bin/avmike \
 	$(isNeededEntry "libikeapi" "${FILESYSTEM_MOD_DIR}/usr/bin/ctlmgr" "${FILESYSTEM_MOD_DIR}/usr/bin/avm/ctlmgr" || echo 'lib/libikeapi*.so*') \
- 	lib/libikecrypto*.so* \
- 	lib/libikeossl*.so* \
+	lib/libikecrypto*.so* \
+	lib/libikeossl*.so* \
 	usr/share/ctlmgr/libvpnstat.so \
 	usr/www/all/internet/vpn.lua \
 	$(find ${FILESYSTEM_MOD_DIR} -iwholename "*usr/www/*/html/*vpn*" -printf "%P\n") \
-	; do
+; do
 	rm_files "${FILESYSTEM_MOD_DIR}/$files"
 done
 
@@ -18,9 +18,6 @@ for files in $(grep -rsl '<? setvariable var:showVpn 1 ?>' ${FILESYSTEM_MOD_DIR}
 	echo2 "patching $files"
 	modsed 's/<? setvariable var:showVpn 1 ?>//' "$files"
 done
-
-
-
 
 echo1 "patching rc.conf"
 modsed "s/CONFIG_VPN=.*$/CONFIG_VPN=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
