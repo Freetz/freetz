8ed9153584f75019766feed53de6409b599e24da
cuma <cuma@freetz.org>
2013-05-02 22:39:05 +0000

    [freetz-1.2] merge
    r10456 | cuma | 2013-05-02 23:34:06 GMT+01:00
    
    remove-wlan:
     * fix AVM-VPN: Set WLAN to "disabled", by MaxMuster (closes #2089)
    ----------------------------------------------------------------------------
    
    git-svn-id: http://svn.freetz.org/branches/freetz-stable-1.2@10457 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/360-remove-wlan.sh
+++ patches/360-remove-wlan.sh
@@ -9,5 +9,11 @@ rm_files ${FILESYSTEM_MOD_DIR}/lib/modules/fw_dcrhp_1150_ap.bin \
 	${FILESYSTEM_MOD_DIR}/usr/bin/wpa_authenticator \
 	${FILESYSTEM_MOD_DIR}/sbin/avmstickandsurf
 
+for sedfile in $(grep -R -l  "wlan:settings/ap_enabled" ${HTML_LANG_MOD_DIR}/* 2>/dev/null); do
+	# fix AVM-VPN: Set WLAN to "disabled" value "0". Otherwise ctlmgr_ctl reports "no emu" or "" (nothing).
+	echo1 "patching ${sedfile##*/}"
+	modsed 's#box.query("wlan:settings/ap_enabled[^"]*")#"0"#g ; s#<? query wlan:settings/ap_enabled[^ ]* ?>#0#g ; s#{ sz_query = "wlan:settings/ap_enabled"}#{ sz_value = "0" }#g' $sedfile
+done
+
 echo1 "patching rc.conf"
 modsed "s/CONFIG_WLAN=.*$/CONFIG_WLAN=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
