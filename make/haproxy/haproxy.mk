dc6dc6aa7e27aeedadae93a4ff8d7e3e9667f86d
cuma <cuma@freetz.org>
2013-05-02 19:16:05 +0000

    [freetz-2.0] merge (refs #2072)
    r10446 | er13 | 2013-05-01 13:41:55 GMT+01:00
    
    haproxy:
     * use magic (C|LD)FLAGS to reduce binary size by 12kb
     * refs #1771, refs #2072
    
    ----------------------------------------------------------------------------
    r10445 | er13 | 2013-05-01 13:34:42 GMT+01:00
    
    haproxy:
     * add missing dependency
     * refs #1771, refs #2072, refs #2088
    
    ----------------------------------------------------------------------------
    r10444 | er13 | 2013-05-01 13:31:38 GMT+01:00
    
    haproxy:
     * bump version to 1.4.23, security fix, refs #2072
     * add patch disabling epoll for kernels <= 2.6.19, epoll doesn't work on fritz!boxes with these kernel versions
     * use linux2628 as TARGET for all boxes with kernel >= 2.6.28 (enables epoll, tproxy and splice)
     * add missing CHANGELOG entry
     * add missing MD5 checksum
     * minor makefile cleanups
     * compile-tested only
     * refs #1771
     * refs #2088
    
    ----------------------------------------------------------------------------
    
    git-svn-id: http://svn.freetz.org/branches/freetz-stable-2.0@10455 f5190166-0702-4917-9039-51ec32eddaf5

--- make/haproxy/haproxy.mk
+++ make/haproxy/haproxy.mk
@@ -1,17 +1,23 @@
-$(call PKG_INIT_BIN, 1.4.20)
-$(PKG)_SOURCE:=haproxy-$($(PKG)_VERSION).tar.gz
-$(PKG)_SITE:=http://haproxy.1wt.eu/download/1.4/src/
+$(call PKG_INIT_BIN, 1.4.23)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=6535d5e58037ada4b58b439cebe03c79
+$(PKG)_SITE:=http://haproxy.1wt.eu/download/1.4/src
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/haproxy
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/haproxy
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_KERNEL_VERSION_2_6_28_MIN
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_NOP)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) TARGET=linux26 -C $(HAPROXY_DIR) \
+	$(SUBMAKE) -C $(HAPROXY_DIR) \
+		TARGET=$(if $(FREETZ_KERNEL_VERSION_2_6_28_MIN),linux2628,linux26) \
 		CC="$(TARGET_CC)" \
-		CFLAGS="$(TARGET_CFLAGS)"
+		CFLAGS="$(TARGET_CFLAGS) -ffunction-sections -fdata-sections" \
+		LDFLAGS="-Wl,--gc-sections"
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
@@ -22,7 +28,6 @@ $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(HAPROXY_DIR) clean
-	$(RM) $(HAPROXY_DIR)/.configured
 
 $(pkg)-uninstall:
 	$(RM) $(HAPROXY_TARGET_BINARY)
