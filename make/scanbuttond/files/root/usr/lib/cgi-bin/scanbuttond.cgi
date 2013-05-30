926c456de2137267bf37ff68a9144c137c1be5e2
Gene Rudoy <gene@freetz.org>
2013-05-30 21:14:21 +0000

    * add new package [http://scanbuttond.sourceforge.net/ scanbuttond]  * by [https://github.com/lemmy lemmy] + a lot of fixes/changes by me  * compile-tested only  * refs #2111
    git-svn-id: http://svn.freetz.org/trunk@10583 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/scanbuttond/files/root/usr/lib/cgi-bin/scanbuttond.cgi
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+PATH=/bin:/usr/bin:/sbin:/usr/sbin
+. /usr/lib/libmodcgi.sh
+
+check "$SCANBUTTOND_ENABLED" yes:auto "*":man
+
+sec_begin '$(lang de:"Starttyp" en:"Start type")'
+cat << EOF
+<p>
+<input id="e1" type="radio" name="enabled" value="yes"$auto_chk><label for="e1"> $(lang de:"Automatisch" en:"Automatic")</label>
+<input id="e2" type="radio" name="enabled" value="no"$man_chk><label for="e2"> $(lang de:"Manuell" en:"Manual")</label>
+EOF
+cat << EOF
+</p>
+EOF
+
+sec_end
+sec_begin 'Scanbuttond Daemon'
+
+cat << EOF
+<p>
+	<h2>Scanner initialization script</h2>
+	<textarea name="INITSCANNER_SCRIPT" cols="78" rows="10" maxlength="255">$(html "$SCANBUTTOND_INITSCANNER_SCRIPT")</textarea>
+</p>
+<p>
+	<h2>Button-pressed script</h2>
+	<textarea name="BUTTONPRESSED_SCRIPT" cols="78" rows="10" maxlength="255">$(html "$SCANBUTTOND_BUTTONPRESSED_SCRIPT")</textarea>
+</p>
+EOF
+
+sec_end
---
+++ make/scanbuttond/scanbuttond.mk
@@ -0,0 +1,86 @@
+$(call PKG_INIT_BIN, 0.2.3.cvs20090713.orig)
+$(PKG)_LIB_VERSION:=1.0.0
+$(PKG)_SOURCE:=$(pkg)_$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=http://ftp.de.debian.org/debian/pool/main/s/scanbuttond
+$(PKG)_SOURCE_MD5:=a117534dab22c2a7a9a1e5a64b2b0c25
+
+$(PKG)_BUILD_PREREQ += autoreconf automake
+
+$(PKG)_INSTALL_SUBDIR:=_install
+
+$(PKG)_BACKENDS_ALL       := artec_eplus48u epson genesys gt68xx hp3500 hp3900 hp5590 meta mustek niash plustek plustek_umax snapscan
+$(PKG)_BACKENDS           := $(call PKG_SELECTED_SUBOPTIONS,$($(PKG)_BACKENDS_ALL),BACKEND)
+
+$(PKG)_BINARY_BUILD_DIR   := $($(PKG)_DIR)/$($(PKG)_INSTALL_SUBDIR)/usr/bin/scanbuttond
+$(PKG)_BINARY_TARGET_DIR  := $($(PKG)_DEST_DIR)/usr/bin/scanbuttond
+
+$(PKG)_LIBS               := $($(PKG)_BACKENDS:%=libscanbtnd-backend_%.so.$($(PKG)_LIB_VERSION)) libscanbtnd-interface_usb.so.$($(PKG)_LIB_VERSION)
+$(PKG)_LIBS_BUILD_DIR     := $($(PKG)_LIBS:%=$($(PKG)_DIR)/$($(PKG)_INSTALL_SUBDIR)$(FREETZ_LIBRARY_PATH)/%)
+$(PKG)_LIBS_TARGET_DIR    := $($(PKG)_LIBS:%=$($(PKG)_DEST_DIR)$(FREETZ_LIBRARY_PATH)/%)
+
+$(PKG)_SCRIPTS            := buttonpressed.sh.example initscanner.sh.example
+$(PKG)_SCRIPTS_BUILD_DIR  := $($(PKG)_SCRIPTS:%=$($(PKG)_DIR)/$($(PKG)_INSTALL_SUBDIR)/mod/etc/scanbuttond/%)
+$(PKG)_SCRIPTS_TARGET_DIR := $($(PKG)_SCRIPTS:%=$($(PKG)_DEST_DIR)/etc/default.scanbuttond/%)
+
+$(PKG)_META_TARGET_DIR    := $($(PKG)_DEST_DIR)/etc/default.scanbuttond/meta.conf
+
+$(PKG)_REBUILD_SUBOPTS += $(foreach backend,$($(PKG)_BACKENDS_ALL),FREETZ_PACKAGE_SCANBUTTOND_BACKEND_$(backend))
+
+$(PKG)_DEPENDS_ON += libusb
+
+$(PKG)_CONFIGURE_PRE_CMDS += autoreconf --force; automake -a; autoreconf --force;
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
+
+$(PKG)_CONFIGURE_OPTIONS += --libdir=$(FREETZ_LIBRARY_PATH)
+$(PKG)_CONFIGURE_OPTIONS += --libexecdir=$(FREETZ_LIBRARY_PATH)
+$(PKG)_CONFIGURE_OPTIONS += --sysconfdir=/mod/etc
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_DIR)/.compiled: $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(SCANBUTTOND_DIR)
+	@touch $@
+
+$($(PKG)_DIR)/.installed: $($(PKG)_DIR)/.compiled
+	mkdir -p $(SCANBUTTOND_DIR)/$(SCANBUTTOND_INSTALL_SUBDIR) && \
+	$(SUBMAKE) -C $(SCANBUTTOND_DIR) \
+		DESTDIR="$(abspath $(SCANBUTTOND_DIR)/$(SCANBUTTOND_INSTALL_SUBDIR))" \
+		install && \
+	touch $@
+
+$($(PKG)_BINARY_BUILD_DIR) $($(PKG)_LIBS_BUILD_DIR) $($(PKG)_SCRIPTS_BUILD_DIR): $($(PKG)_DIR)/.installed
+
+$($(PKG)_BINARY_TARGET_DIR): $($(PKG)_BINARY_BUILD_DIR)
+	$(INSTALL_BINARY_STRIP)
+
+$($(PKG)_LIBS_TARGET_DIR): $($(PKG)_DEST_DIR)$(FREETZ_LIBRARY_PATH)/%: $($(PKG)_DIR)/$($(PKG)_INSTALL_SUBDIR)$(FREETZ_LIBRARY_PATH)/%
+	$(INSTALL_LIBRARY_STRIP)
+
+$($(PKG)_SCRIPTS_TARGET_DIR): $($(PKG)_DEST_DIR)/etc/default.scanbuttond/%: $($(PKG)_DIR)/$($(PKG)_INSTALL_SUBDIR)/mod/etc/scanbuttond/%
+	$(INSTALL_FILE)
+
+$($(PKG)_META_TARGET_DIR): $($(PKG)_DIR)/.unpacked
+	mkdir -p "$(dir $@)" && \
+	echo -n > "$@" && \
+	for backend in $(SCANBUTTOND_BACKENDS); do \
+		echo libscanbtnd-backend_$$backend >> "$@"; \
+	done
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_BINARY_TARGET_DIR) $($(PKG)_LIBS_TARGET_DIR) $($(PKG)_SCRIPTS_TARGET_DIR) $($(PKG)_META_TARGET_DIR)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(SCANBUTTOND_DIR) clean
+	$(RM) $($(PKG)_DIR)/{.configured,.compiled,.installed}
+
+$(pkg)-uninstall:
+	$(RM) -r \
+		$(SCANBUTTOND_BINARY_TARGET_DIR) \
+		$(SCANBUTTOND_DEST_DIR)$(FREETZ_LIBRARY_PATH)/libscanbtnd* \
+		$(SCANBUTTOND_SCRIPTS_TARGET_DIR) \
+		$(SCANBUTTOND_META_TARGET_DIR)
+
+$(PKG_FINISH)
