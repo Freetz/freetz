92c60c0a1c1407e68e2aa84932608cfeaec26c84
Gene Rudoy <gene@freetz.org>
2013-05-31 10:20:45 +0000

    * add new library - [http://srtp.sourceforge.net/srtp.html libsrtp]  * based on patch provided by avalentin in #706  * refs #706
    git-svn-id: http://svn.freetz.org/trunk@10587 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/libs/srtp/srtp.mk
@@ -0,0 +1,45 @@
+$(call PKG_INIT_LIB, 1.4.4)
+$(PKG)_SOURCE:=$(pkg)_$($(PKG)_VERSION)+20100615~dfsg.orig.tar.gz
+$(PKG)_SOURCE_MD5:=debe7fe729d1122f0d22f682a9f27be8
+$(PKG)_SITE:=http://ftp.de.debian.org/debian/pool/main/s/srtp
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/srtp~dfsg
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/lib$(pkg).so.$($(PKG)_VERSION)
+$(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/lib$(pkg).so.$($(PKG)_VERSION)
+$(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/lib$(pkg).so.$($(PKG)_VERSION)
+
+$(PKG)_CONFIGURE_OPTIONS += --disable-stdout
+$(PKG)_CONFIGURE_OPTIONS += --disable-console
+$(PKG)_CONFIGURE_OPTIONS += --enable-syslog
+$(PKG)_CONFIGURE_OPTIONS += --disable-gdoi
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(SRTP_DIR)
+
+$($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
+	$(SUBMAKE) -C $(SRTP_DIR) \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+		install
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
+	$(INSTALL_LIBRARY_STRIP)
+
+$(pkg): $($(PKG)_STAGING_BINARY)
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(SRTP_DIR) clean
+	$(RM) -r \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libsrtp.* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/srtp
+
+$(pkg)-uninstall:
+	$(RM) $(SRTP_TARGET_DIR)/libsrtp.so*
+
+$(PKG_FINISH)
