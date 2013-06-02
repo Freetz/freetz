07747d5e48c2464ae3e3327c42e92b172aafce68
Gene Rudoy <gene@freetz.org>
2013-06-02 15:19:01 +0000

    * add new library - [https://code.google.com/p/serf/ serf]  * needed for [http://subversion.apache.org/docs/release-notes/1.8.html#neon-deleted upcoming (1.8.x)] subversion version
    git-svn-id: http://svn.freetz.org/trunk@10618 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/libs/serf/serf.mk
@@ -0,0 +1,49 @@
+$(call PKG_INIT_LIB, 1.2.0)
+$(PKG)_LIB_VERSION:=0.0.0
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SOURCE_SHA1:=30b29bd9214d50887abcc20cf82096aaaf5d1d61
+$(PKG)_SITE:=https://serf.googlecode.com/files
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/.libs/libserf-1.so.$($(PKG)_LIB_VERSION)
+$(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/libserf-1.so.$($(PKG)_LIB_VERSION)
+$(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libserf-1.so.$($(PKG)_LIB_VERSION)
+
+$(PKG)_DEPENDS_ON += apr apr-util openssl zlib
+
+$(PKG)_CONFIGURE_OPTIONS += --with-apr="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+$(PKG)_CONFIGURE_OPTIONS += --with-apr-util="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+$(PKG)_CONFIGURE_OPTIONS += --with-openssl="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(SERF_DIR)
+
+$($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
+	$(SUBMAKE) -C $(SERF_DIR) \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+		install
+	$(PKG_FIX_LIBTOOL_LA) \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libserf-1.la \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/serf-1.pc
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
+	$(INSTALL_LIBRARY_STRIP)
+
+$(pkg): $($(PKG)_STAGING_BINARY)
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(SERF_DIR) clean
+	$(RM) -r \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libserf-1.* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/serf-1.pc \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/{serf.h,serf_*.h}
+
+$(pkg)-uninstall:
+	$(RM) $(SERF_TARGET_DIR)/libserf-1.so*
+
+$(PKG_FINISH)
