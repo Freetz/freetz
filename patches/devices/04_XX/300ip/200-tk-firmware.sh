9a627b30e841bfd3a6f801e847802dba36cbe80d
cuma <cuma@freetz.org>
2013-06-17 10:55:11 +0000

    [freetz-2.0] merge (refs #2072)
    ----------------------------------------------------------------------------
    r10664 | MaxMuster | 2013-06-15 20:56:12 GMT+01:00
    
    Eumex300ip: fix patch path
    ----------------------------------------------------------------------------
    r10660 | cuma | 2013-06-15 17:10:06 GMT+01:00
    
    fix r10658
    ----------------------------------------------------------------------------
    r10658 | cuma | 2013-06-15 16:58:59 GMT+01:00
    
    fwmod: add some alien variables
    ----------------------------------------------------------------------------
    r10657 | cuma | 2013-06-15 16:41:11 GMT+01:00
    
    7340 alien: Update FIRMWARES
    ----------------------------------------------------------------------------
    
    git-svn-id: http://svn.freetz.org/branches/freetz-stable-2.0@10682 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/devices/04_XX/300ip/200-tk-firmware.sh
+++ patches/devices/04_XX/300ip/200-tk-firmware.sh
@@ -1,2 +1,2 @@
 # executing sh according to language / version
-. "${PATCHES_DIR}/${FREETZ_TYPE_PREFIX}/200-tk-firmware-${FREETZ_TYPE_LANGUAGE}"
+. "${PATCHES_DIR}/devices/${FREETZ_TYPE_PREFIX_SERIES_SUBDIR}/${FREETZ_TYPE_PREFIX}/200-tk-firmware-${FREETZ_TYPE_LANGUAGE}"
