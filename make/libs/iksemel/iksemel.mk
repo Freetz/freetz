df9dae00f94d2603a53aa26abea298a52db17c79
Gene Rudoy <gene@freetz.org>
2013-06-18 22:52:09 +0000

    * add new library - [https://code.google.com/p/iksemel iksemel]  * based on [http://freetz.org/attachment/ticket/706/iksemel.patch patch] by avalentin, thanks  * refs #706
    git-svn-id: http://svn.freetz.org/trunk@10692 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/libs/iksemel/iksemel.mk
@@ -0,0 +1,54 @@
+$(call PKG_INIT_LIB, 978b733462)
+$(PKG)_LIB_VERSION:=3.1.1
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
+$(PKG)_SITE:=git@https://code.google.com/p/iksemel
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/.libs/lib$(pkg).so.$($(PKG)_LIB_VERSION)
+$(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/lib$(pkg).so.$($(PKG)_LIB_VERSION)
+$(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/lib$(pkg).so.$($(PKG)_LIB_VERSION)
+
+$(PKG)_DEPENDS_ON += openssl
+
+$(PKG)_BUILD_PREREQ += autoreconf
+
+$(PKG)_CONFIGURE_OPTIONS += --enable-shared
+$(PKG)_CONFIGURE_OPTIONS += --enable-static
+$(PKG)_CONFIGURE_OPTIONS += --without-gnutls
+$(PKG)_CONFIGURE_OPTIONS += --disable-python
+
+$(PKG)_CONFIGURE_PRE_CMDS += autoreconf -f -i;
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(IKSEMEL_DIR)
+
+$($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
+	$(RM) -r $(TARGET_TOOLCHAIN_STAGING_DIR)/include/iksemel $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libiksemel*
+	$(SUBMAKE) -C $(IKSEMEL_DIR) \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+		install
+	$(PKG_FIX_LIBTOOL_LA) \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libiksemel.la \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/iksemel.pc
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
+	$(INSTALL_LIBRARY_STRIP)
+
+$(pkg): $($(PKG)_STAGING_BINARY)
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(IKSEMEL_DIR) clean
+	$(RM) -r \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/libiksemel.* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/pkgconfig/iksemel.pc \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/include/iksemel.h
+
+$(pkg)-uninstall:
+	$(RM) $(IKSEMEL_TARGET_DIR)/libiksemel.so*
+
+$(PKG_FINISH)
