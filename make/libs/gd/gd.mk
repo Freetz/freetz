f4d28cd347025b66c41b5b5519b242942989e5f5
Gene Rudoy <gene@freetz.org>
2013-06-22 21:20:05 +0000

    libgd:  * bump version to 2.1.0-rc2, contains fixes and improvements of libgd maintained by developers of php  * required by php-5.5.x
    php-5.3.x/5.4.x:
     * fix build problems with new libgd version
    
    
    git-svn-id: http://svn.freetz.org/trunk@10715 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/gd/gd.mk
+++ make/libs/gd/gd.mk
@@ -1,16 +1,16 @@
-$(call PKG_INIT_LIB, 2.0.36~rc1)
-$(PKG)_LIB_VERSION:=2.0.0
-$(PKG)_SOURCE:=libgd2_$($(PKG)_VERSION)~dfsg.orig.tar.gz
-$(PKG)_SOURCE_MD5:=0f4d2fa45627af0e87fcb74f653b66dd
-$(PKG)_SITE:=http://ftp.debian.org/debian/pool/main/libg/libgd2
+$(call PKG_INIT_LIB, 2.1.0-rc2)
+$(PKG)_LIB_VERSION:=3.0.0
+$(PKG)_SOURCE:=libgd-$($(PKG)_VERSION).tar.xz
+$(PKG)_SOURCE_MD5:=cd6a174145f7117dd7bd466c272a9fbc
+$(PKG)_SITE:=https://bitbucket.org/libgd/gd-libgd/downloads
 
-$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/libgd2_$($(PKG)_VERSION)~dfsg.orig
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/libgd-$($(PKG)_VERSION)
 
-$(PKG)_BINARY:=$($(PKG)_DIR)/.libs/libgd.so.$($(PKG)_LIB_VERSION)
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/.libs/libgd.so.$($(PKG)_LIB_VERSION)
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libgd.so.$($(PKG)_LIB_VERSION)
 $(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libgd.so.$($(PKG)_LIB_VERSION)
 
-$(PKG)_DEPENDS_ON := jpeg libpng freetype
+$(PKG)_DEPENDS_ON := jpeg libpng freetype zlib
 
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
 
@@ -18,9 +18,15 @@ $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
 $(PKG)_CONFIGURE_OPTIONS += --disable-rpath
 $(PKG)_CONFIGURE_OPTIONS += --without-libiconv-prefix
+$(PKG)_CONFIGURE_OPTIONS += --with-fontconfig=no
+$(PKG)_CONFIGURE_OPTIONS += --with-freetype="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+$(PKG)_CONFIGURE_OPTIONS += --with-jpeg="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+$(PKG)_CONFIGURE_OPTIONS += --with-png="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+$(PKG)_CONFIGURE_OPTIONS += --with-tiff=no
+$(PKG)_CONFIGURE_OPTIONS += --with-vpx=no
 $(PKG)_CONFIGURE_OPTIONS += --with-x=no
 $(PKG)_CONFIGURE_OPTIONS += --with-xpm=no
-$(PKG)_CONFIGURE_OPTIONS += --with-fontconfig=no
+$(PKG)_CONFIGURE_OPTIONS += --with-zlib="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -30,11 +36,15 @@ $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(GD_DIR)
 
 $($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
-	$(SUBMAKE) -C $(GD_DIR) \
+	$(SUBMAKE) -C $(GD_DIR)/src \
 		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
-		install-libLTLIBRARIES install-includeHEADERS install-binSCRIPTS
+		install-libLTLIBRARIES install-includeHEADERS
+	$(SUBMAKE) -C $(GD_DIR)/config \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+		install
 	$(PKG_FIX_LIBTOOL_LA) \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libgd.la \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/gdlib.pc \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/gdlib-config
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
@@ -48,7 +58,8 @@ $(pkg)-clean:
 	-$(SUBMAKE) -C $(GD_DIR) clean
 	$(RM) \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libgd.* \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/{entities,gdcache,gdfontg,gdfontl,gdfontmb,gdfonts,gdfontt,gdfx,gd,gd_io}.h \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/gdlib.pc \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/{entities,gdcache,gd_color_map,gd_errors,gdfontg,gdfontl,gdfontmb,gdfonts,gdfontt,gdfx,gd,gd_io,gdpp}.h \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/gdlib-config
 
 $(pkg)-uninstall:
