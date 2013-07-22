df738183e1505aa5a2ddd065fb59cff3d2769ed6
Gene Rudoy <gene@freetz.org>
2013-07-22 19:55:05 +0000

    pcsc-lite:  * fix various makefile bugs  * add missing dependencies  * provide menuconfig symbol for libpcsclite
    git-svn-id: http://svn.freetz.org/trunk@10875 f5190166-0702-4917-9039-51ec32eddaf5

--- make/pcsc-lite/pcsc-lite.mk
+++ make/pcsc-lite/pcsc-lite.mk
@@ -3,46 +3,47 @@ $(PKG)_LIB_VERSION:=1.0.0
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_SOURCE_MD5:=069dc875a2ae2d85a2ebceac73252c0a
 $(PKG)_SITE:=https://alioth.debian.org/frs/download.php/3862
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/pcscd
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/pcscd
+
 $(PKG)_LIB:=$($(PKG)_DIR)/src/.libs/libpcsclite.so.$($(PKG)_LIB_VERSION)
-$(PKG)_TARGET_LIB:=$($(PKG)_DEST_LIBDIR)/libpcsclite.so.$($(PKG)_LIB_VERSION)
-$(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcsclite.so.$($(PKG)_LIB_VERSION)
+$(PKG)_STAGING_LIB:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcsclite.so.$($(PKG)_LIB_VERSION)
+$(PKG)_TARGET_LIB:=$($(PKG)_TARGET_LIBDIR)/libpcsclite.so.$($(PKG)_LIB_VERSION)
 
-$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)  
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
 
-$(PKG)_DEPENDS_ON :=libusb1
+$(PKG)_DEPENDS_ON := libusb1
+
+$(PKG)_CONFIGURE_OPTIONS += --enable-shared
+$(PKG)_CONFIGURE_OPTIONS += --enable-static
 
 $(PKG)_CONFIGURE_OPTIONS += --enable-libusb
 $(PKG)_CONFIGURE_OPTIONS += --enable-scf
 $(PKG)_CONFIGURE_OPTIONS += --disable-libhal
 $(PKG)_CONFIGURE_OPTIONS += --disable-libudev
-$(PKG)_CONFIGURE_OPTIONS += --enable-shared
-$(PKG)_CONFIGURE_OPTIONS += --enable-static
 #$(PKG)_CONFIGURE_OPTIONS += --enable-embedded
 #$(PKG)_CONFIGURE_OPTIONS += --enable-usbdropdir=/mod/usr/lib/pcsc/drivers
-
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY) $($(PKG)_LIB): $($(PKG)_DIR)/.configured
-		$(SUBMAKE) -C $(PCSC_LITE_DIR)
+	$(SUBMAKE) -C $(PCSC_LITE_DIR) V=1
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
-	$(INSTALL_BINARY_STRIP)ls 
+	$(INSTALL_BINARY_STRIP)
 
-$($(PKG)_STAGING_BINARY): $($(PKG)_LIB)
-	$(SUBMAKE) -C $(PCSC_LITE_DIR) \
+$($(PKG)_STAGING_LIB): $($(PKG)_LIB)
+	$(SUBMAKE) -C $(PCSC_LITE_DIR)/src \
 		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
-		install
+		install-libLTLIBRARIES install-nodistheaderDATA install-nobase_includeHEADERS install-pcDATA
 	$(PKG_FIX_LIBTOOL_LA) \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcsclite.a \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcsclite.la
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcsclite.la \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libpcsclite.pc
 
-
-$($(PKG)_TARGET_LIB): $($(PKG)_STAGING_BINARY)
+$($(PKG)_TARGET_LIB): $($(PKG)_STAGING_LIB)
 	$(INSTALL_LIBRARY_STRIP)
 
 $(pkg):
@@ -52,8 +53,15 @@ $(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_TARGET_LIB)
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(PCSC_LITE_DIR) clean
 	$(RM) $(PCSC_LITE_DIR)/.configured
+	$(RM) -r \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/PCSC \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcsclite.* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libpcsclite.pc
 
 $(pkg)-uninstall:
-	$(RM) $(PCSC_LITE_TARGET_BINARY)
+	$(RM) \
+		$(PCSC_LITE_TARGET_BINARY) \
+		$(PCSC_LITE_TARGET_LIBDIR)/libpcsclite.so*
 
+$(call PKG_ADD_LIB,libpcsclite)
 $(PKG_FINISH)
