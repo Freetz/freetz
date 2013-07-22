f3e0fc6ca5e3e034c77d955c3ed880653beb9ba6
Gene Rudoy <gene@freetz.org>
2013-07-22 22:14:18 +0000

    libusb:  * add optional support for [http://www.libusb.org libusb-compat]  * fixes #1226
    git-svn-id: http://svn.freetz.org/trunk@10878 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/libusb/libusb.mk
+++ make/libs/libusb/libusb.mk
@@ -1,23 +1,36 @@
-$(call PKG_INIT_LIB, 0.1.12)
+$(call PKG_INIT_LIB,$(if $(FREETZ_LIB_libusb_0_WITH_COMPAT),0.1.5,0.1.12))
+$(PKG)_DIR_COMPONENT:=$(pkg)$(if $(FREETZ_LIB_libusb_0_WITH_COMPAT),-compat)-$($(PKG)_VERSION)
 $(PKG)_SHORT_VERSION:=$(call GET_MAJOR_VERSION,$($(PKG)_VERSION))
 $(PKG)_LIB_VERSION:=4.4.4
-$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=caf182cbc7565dac0fd72155919672e6
+$(PKG)_SOURCE:=$($(PKG)_DIR_COMPONENT).tar.$(if $(FREETZ_LIB_libusb_0_WITH_COMPAT),bz2,gz)
+$(PKG)_SOURCE_MD5:=$(if $(FREETZ_LIB_libusb_0_WITH_COMPAT),2780b6a758a1e2c2943bdbf7faf740e4,caf182cbc7565dac0fd72155919672e6)
 $(PKG)_SITE:=@SF/$(pkg)
 
-$(PKG)_BINARY:=$($(PKG)_DIR)/.libs/$(pkg)-$($(PKG)_SHORT_VERSION).so.$($(PKG)_LIB_VERSION)
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/$($(PKG)_DIR_COMPONENT)
+
+$(PKG)_CONDITIONAL_PATCHES+=$(pkg)$(if $(FREETZ_LIB_libusb_0_WITH_COMPAT),-compat)
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/$(if $(FREETZ_LIB_libusb_0_WITH_COMPAT),$(pkg)/).libs/$(pkg)-$($(PKG)_SHORT_VERSION).so.$($(PKG)_LIB_VERSION)
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/$(pkg)-$($(PKG)_SHORT_VERSION).so.$($(PKG)_LIB_VERSION)
 $(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/$(pkg)-$($(PKG)_SHORT_VERSION).so.$($(PKG)_LIB_VERSION)
 
+$(PKG)_REBUILD_SUBOPTS += FREETZ_LIB_libusb_0_WITH_LEGACY
+$(PKG)_REBUILD_SUBOPTS += FREETZ_LIB_libusb_0_WITH_COMPAT
+
+$(PKG)_DEPENDS_ON := $(if $(FREETZ_LIB_libusb_0_WITH_COMPAT),libusb1)
+
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
+
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_LIB_libusb_0_WITH_COMPAT),--disable-examples-build)
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(LIBUSB_DIR) all
+	$(SUBMAKE) -C $(LIBUSB_DIR)
 
 $($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
 	$(SUBMAKE) -C $(LIBUSB_DIR) \
@@ -41,7 +54,7 @@ $(pkg)-clean:
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/bin/libusb-config \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/include/usb.h \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/libusb-$(LIBUSB_SHORT_VERSION)* \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/libusb.{a,la} \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/libusb.{a,la,so} \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/pkgconfig/libusb.pc
 
 $(pkg)-uninstall:
