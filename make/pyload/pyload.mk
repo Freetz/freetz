97c9502aa8770f3c043d972fa318477bb7856eea
Gene Rudoy <gene@freetz.org>
2013-07-24 16:03:35 +0000

    pyload:  * bump revision to 669b1e0ec0  * update list of unnecessary files
    git-svn-id: http://svn.freetz.org/trunk@10897 f5190166-0702-4917-9039-51ec32eddaf5

--- make/pyload/pyload.mk
+++ make/pyload/pyload.mk
@@ -1,5 +1,5 @@
 PYLOAD_GIT_REPOSITORY:=https://github.com/pyload/pyload.git
-$(call PKG_INIT_BIN, $(if $(FREETZ_PACKAGE_PYLOAD_VERSION_LATEST_GIT),$(call git-get-latest-revision,$(PYLOAD_GIT_REPOSITORY),stable),5f673918f7))
+$(call PKG_INIT_BIN, $(if $(FREETZ_PACKAGE_PYLOAD_VERSION_LATEST_GIT),$(call git-get-latest-revision,$(PYLOAD_GIT_REPOSITORY),stable),669b1e0ec0))
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
 $(PKG)_SITE:=git@$($(PKG)_GIT_REPOSITORY)
 
@@ -20,6 +20,7 @@ endef
 
 define pyLoad/unnecessary/files
 .hgignore
+.gitattributes
 docs
 icons
 LICENSE
