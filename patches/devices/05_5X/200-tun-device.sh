9b36570dba58c2df2bec00edb4b11337e70a4f6b
Oliver Metz <oliver@freetz.org>
2013-08-06 17:35:15 +0000

    Revert r9767: fix "tun" device for 05.5x
    This did not fix the problem (refs #1959).
    
    git-svn-id: http://svn.freetz.org/trunk@10933 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/devices/05_5X/200-tun-device.sh
+++ patches/devices/05_5X/200-tun-device.sh
@@ -1,3 +1,3 @@
 # Create symlink for tun device under /dev/net/tun
-modsed 's#^\(KERNEL=="tun".*\)#\1, SYMLINK+="tun"#' \
+modsed 's#^\(KERNEL=="tun".*\)#\1, SYMLINK+="net/tun"#' \
 	"${FILESYSTEM_MOD_DIR}/etc/udev/rules.d/50-udev-default.rules"
