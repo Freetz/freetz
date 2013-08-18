1e781dd1641651564918c400bfeae9cd82788a58
Gene Rudoy <gene@freetz.org>
2013-08-18 08:46:55 +0000

    * add new package - [http://www.harding.motd.ca/autossh/ autossh] (moved from sf3978_packages-branch)
    git-svn-id: http://svn.freetz.org/trunk@10941 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/autossh/autossh.mk
@@ -0,0 +1,34 @@
+$(call PKG_INIT_BIN, 1.4c)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tgz
+$(PKG)_SOURCE_SHA1:=d9a1d5a2987e7e5a444b00e63d6590936da1e1f2
+$(PKG)_SITE:=http://www.harding.motd.ca/$(pkg)
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/$(pkg)
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/$(pkg)
+
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
+$(PKG)_CONFIGURE_ENV += ac_cv_lib_nsl_gethostbyname=no
+$(PKG)_CONFIGURE_OPTIONS += --with-ssh=/usr/bin/ssh
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(AUTOSSH_DIR)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(AUTOSSH_DIR) clean
+	$(RM) $(AUTOSSH_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(AUTOSSH_TARGET_BINARY)
+
+$(PKG_FINISH)
