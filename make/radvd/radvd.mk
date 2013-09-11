faaa6e8d6dd17c9272fac3c46f339cef082d6e8c
cuma <cuma@freetz.org>
2013-09-11 20:06:04 +0000

    bump radvd 1.9.3, by dileks (closes #2155)
    git-svn-id: http://svn.freetz.org/trunk@10990 f5190166-0702-4917-9039-51ec32eddaf5

--- make/radvd/radvd.mk
+++ make/radvd/radvd.mk
@@ -1,12 +1,14 @@
-$(call PKG_INIT_BIN, 1.8.3)
+$(call PKG_INIT_BIN, 1.9.3)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=73d533b7b20f5554c8d3e33643945442
+$(PKG)_SOURCE_SHA1:=14b025bdcec77ea7278cd8ee2a32af3f81d26f4d
 $(PKG)_SITE:=http://www.litech.org/radvd/dist
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/$(pkg)
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/$(pkg)
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_KERNEL_VERSION
+
+$(PKG)_DEPENDS_ON := libdaemon
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
