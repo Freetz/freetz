d3faf7e23bce28dc0f96ff5b65fc9f0c26c2bc62
Whoopie <whoopie79@gmx.net>
2013-09-22 06:15:42 +0000

    haserl: bump version to 0.9.32 (refs #2181)
    git-svn-id: http://svn.freetz.org/trunk@11027 f5190166-0702-4917-9039-51ec32eddaf5

--- make/haserl/haserl.mk
+++ make/haserl/haserl.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 0.9.30)
+$(call PKG_INIT_BIN, 0.9.32)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=e25f1a13f7cfe5993e961803446cd718
+$(PKG)_SOURCE_MD5:=18e2208aea6d772d1670c1a648bb6b77
 $(PKG)_SITE:=@SF/haserl
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/haserl
