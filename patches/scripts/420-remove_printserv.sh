65a185d230cfbd0d741a651059f5648b2eaac261
cuma <cuma@freetz.org>
2013-10-06 10:29:16 +0000

    remove_printserv: Fix for fw 5.59, by make (closes #2199)
    git-svn-id: http://svn.freetz.org/trunk@11062 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/420-remove_printserv.sh
+++ patches/scripts/420-remove_printserv.sh
@@ -13,5 +13,11 @@ rm_files "${FILESYSTEM_MOD_DIR}/etc/hotplug/udev-printer-lp"
 [ "$FREETZ_REMOVE_PRINTSERV_MODULE" == "y" ] && \
 	rm_files ${FILESYSTEM_MOD_DIR}/lib/modules/*/kernel/drivers/usb/class/usblp.ko
 
+# patcht System > Diagnose (ab 5.59)
+sedfile="${HTML_LANG_MOD_DIR}/lua/usb_devices.lua"
+echo1 "patching ${sedfile##*/}"
+modsed "s/config\.USB_PRINT_SERV/true/g" $sedfile
+
+
 echo1 "patching rc.conf"
 modsed "s/CONFIG_USB_PRINT_SERV=.*$/CONFIG_USB_PRINT_SERV=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
