0834fa2d7b0b3789ddebde0754ef5ae654025332
cuma <cuma@freetz.org>
2013-10-07 12:51:29 +0000

    nas & samba remove: add some webif patches (closes #2201)
    git-svn-id: http://svn.freetz.org/trunk@11066 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/220-remove-nas.sh
+++ patches/scripts/220-remove-nas.sh
@@ -1,5 +1,9 @@
 [ "$FREETZ_REMOVE_NAS" == "y" ] || return 0
 
+# if nas, mediaserv und samba are removed -> remove_nas deletes menu item Heimnetz > Speicher (NAS)  
+[ "$FREETZ_PACKAGE_SAMBA_SMBD" == "y" -o "$FREETZ_REMOVE_SAMBA" == "y" ] &&
+  [ "$FREETZ_REMOVE_MEDIASRV" == "y" ] && menulua_remove "storage\/settings"
+
 echo1 "removing nas"
 rm -rf "${FILESYSTEM_MOD_DIR}/usr/www.nas"
 ln -sf www "${FILESYSTEM_MOD_DIR}/usr/www.nas"
@@ -7,15 +11,29 @@ rm_files \
   "${FILESYSTEM_MOD_DIR}/sbin/fritznasdb" \
   "${FILESYSTEM_MOD_DIR}/bin/showfritznasdbstat"
 # REMOVE_MEDIASRV uses/removes libavmdb*.so
+echo2 "removing internal memory"
+rm -rf ${FILESYSTEM_MOD_DIR}/etc/internal_memory_default*.tar
 
-# patcht Heimnetz > Speicher (NAS). Fehler von AVM? http://freetz.org/ticket/2011
-modsed '/^if config.NAS then$/{N;s/^.*\(\nrequire("call_webusb")\)/if true then\1/g}' "${HTML_LANG_MOD_DIR}/storage/settings.lua"
+# patcht Uebersicht
+modsed '/.*uiViewTabSpeicherNas.*/{N;N;N;/537:880/d}' "${HTML_LANG_MOD_DIR}/home/home.lua"
+
+# patcht Heimnetz > Speicher (NAS)
+sedfile="${HTML_LANG_MOD_DIR}/storage/settings.lua"
+if [ -e "$sedfile" ]; then
+	echo1 "patching ${sedfile##*/}"
+	sedrows=$(cat $sedfile |nl| sed -n 's/^ *\([0-9]*\).*<h4>{?80:518?}<.h4>.*$/\1/p')
+	sedrowe=$(cat $sedfile |nl| sed -n 's/^ *\([0-9]*\).*<div id="page_bottom">.*$/\1/p')
+	modsed "$((sedrows-1)),$((sedrowe-1))d" $sedfile
+	modsed 's!<div id="btn_form_foot">!<div>\n&!' "$sedfile"
+	modsed 's/\(jxl.disableNode("page_bottom",\).*/\1 false);/g' "$sedfile"
+
+	# Fehler von AVM? http://freetz.org/ticket/2011
+	modsed '/^if config.NAS then$/{N;s/^.*\(\nrequire("call_webusb")\)/if true then\1/g}' "$sedfile"
+fi
 
 # entfernt Links zu Fritz.NAS
 quickstart_remove nas
 modsed '/.*class="sm_link_bold" href=".nas.*/d' "${HTML_SPEC_MOD_DIR}/menus/menu2.html"
 
-echo2 "removing internal memory"
-rm -rf ${FILESYSTEM_MOD_DIR}/etc/internal_memory_default*.tar
-
 modsed "s/CONFIG_NAS.*/CONFIG_NAS=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+
--- patches/scripts/300-remove-samba.sh
+++ patches/scripts/300-remove-samba.sh
@@ -1,3 +1,6 @@
+
+# if nas, mediaserv und samba are removed -> remove_nas deletes menu item Heimnetz > Speicher (NAS)  
+
 if [ "$FREETZ_AVM_HAS_USB_HOST" == "y" -a "$FREETZ_PACKAGE_SAMBA_SMBD" == "y" ]; then
 	sed -i -e "/killall smbd*$/d" -e "s/pidof smbd/pidof/g" "${FILESYSTEM_MOD_DIR}/etc/hotplug/storage"
 fi
@@ -8,8 +11,19 @@ if [ "$FREETZ_PACKAGE_SAMBA_SMBD" == "y" -o "$FREETZ_REMOVE_SAMBA" == "y" ]; the
 	  "${FILESYSTEM_MOD_DIR}/bin/inetdsamba" \
 	  "${FILESYSTEM_MOD_DIR}/sbin/samba_config_gen" \
 	  "${FILESYSTEM_MOD_DIR}/etc/samba_config.tar"
+
 	echo1 "patching rc.net: renaming sambastart()"
 	modsed 's/^\(sambastart *()\)/\1{ return; }\n_\1/' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.net"
+		
+	# patcht Heimnetz > Speicher (NAS)
+	sedfile="${HTML_LANG_MOD_DIR}/storage/settings.lua"
+	if [ -e "$sedfile" ]; then
+		echo1 "patching ${sedfile##*/}"
+		sedrows=$(cat $sedfile |nl| sed -n 's/^ *\([0-9]*\).*<div id="page_bottom">.*$/\1/p')
+		sedrowe=$(cat $sedfile |nl| sed -n 's/^ *\([0-9]*\).*<div id="btn_form_foot">.*$/\1/p')
+		modsed "$((sedrows)),$((sedrowe-1))d" $sedfile
+	fi
+	
 	echo1 "patching rc.conf"
 	modsed "s/CONFIG_SAMBA=.*$/CONFIG_SAMBA=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
 fi
