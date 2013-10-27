e80a3d18484b3e737d2e43bb3797c64804bb2519
cuma <cuma@freetz.org>
2013-10-27 04:35:54 +0000

    small typo
    git-svn-id: http://svn.freetz.org/trunk@11153 f5190166-0702-4917-9039-51ec32eddaf5

--- make/mod/files/root/usr/lib/cgi-bin/mod/conf/60-utmp_wtmp.sh
+++ make/mod/files/root/usr/lib/cgi-bin/mod/conf/60-utmp_wtmp.sh
@@ -5,7 +5,7 @@ if [ "$FREETZ_BUSYBOX_FEATURE_WTMP" == "y" ]; then
 	sec_begin 'wtmp'
 	
 	cgi_print_textline_p "path_wtmp" "$MOD_PATH_WTMP" 50/255 \
-	  "$(lang de:"Pfad f&uuml;r wtmp" en:"Path for wtmp") (default /var/log): "
+	  "$(lang de:"Pfad f&uuml;r wtmp" en:"Path for wtmp") (default /var/log/): "
 	
 	sec_end
 fi
