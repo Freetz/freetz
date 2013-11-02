7a30a8420ceafaffbf95937421df07058b5be2a7
Gene Rudoy <gene@freetz.org>
2013-11-02 18:04:11 +0000

    lzma2eva:  * add eva2lzma (taken from [http://spblinux.de/fbox/tmp/x86/ here])  * may be used to decompress original AVM kernel:     $(TOOLS)/eva2lzma kernel.raw kernel.lzma     $(TOOLS)/lzma d kernel.lzma kernel.decompressed
    git-svn-id: http://svn.freetz.org/trunk@11192 f5190166-0702-4917-9039-51ec32eddaf5

--- tools/make/lzma2eva.mk
+++ tools/make/lzma2eva.mk
@@ -2,21 +2,19 @@ LZMA2EVA_SOURCE:=lzma2eva.tar.bz2
 LZMA2EVA_DIR:=$(TOOLS_SOURCE_DIR)/lzma2eva
 LZMA2EVA_MAKE_DIR:=$(TOOLS_DIR)/make
 
+LZMA2EVA_TOOLS:=lzma2eva eva2lzma
 
 $(LZMA2EVA_DIR)/.unpacked: $(TOOLS_DIR)/source/$(LZMA2EVA_SOURCE) | $(TOOLS_SOURCE_DIR)
 	tar -C $(TOOLS_SOURCE_DIR) $(VERBOSE) -xf $(TOOLS_DIR)/source/$(LZMA2EVA_SOURCE)
-	for i in $(LZMA2EVA_MAKE_DIR)/patches/*.lzma2eva.patch; do \
-		$(PATCH_TOOL) $(LZMA2EVA_DIR) $$i; \
-	done
 	touch $@
 
-$(LZMA2EVA_DIR)/lzma2eva: $(LZMA2EVA_DIR)/.unpacked
+$(LZMA2EVA_TOOLS:%=$(LZMA2EVA_DIR)/%): $(LZMA2EVA_DIR)/.unpacked
 	$(MAKE) CC="$(TOOLS_CC)" LD="$(TOOLS_LD)" -C $(LZMA2EVA_DIR)
 
-$(TOOLS_DIR)/lzma2eva: $(LZMA2EVA_DIR)/lzma2eva
-	cp $(LZMA2EVA_DIR)/lzma2eva $(TOOLS_DIR)/lzma2eva
+$(LZMA2EVA_TOOLS:%=$(TOOLS_DIR)/%): $(TOOLS_DIR)/%: $(LZMA2EVA_DIR)/%
+	cp $< $@
 
-lzma2eva: $(TOOLS_DIR)/lzma2eva
+lzma2eva: $(LZMA2EVA_TOOLS:%=$(TOOLS_DIR)/%)
 
 lzma2eva-source: $(LZMA2EVA_DIR)/.unpacked
 
@@ -27,4 +25,4 @@ lzma2eva-dirclean:
 	$(RM) -r $(LZMA2EVA_DIR)
 
 lzma2eva-distclean: lzma2eva-dirclean
-	$(RM) $(TOOLS_DIR)/lzma2eva
+	$(RM) $(LZMA2EVA_TOOLS:%=$(TOOLS_DIR)/%)
