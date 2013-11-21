4e32406fa05a1d5b76750c102c0b13bb45e7342c
cuma <cuma@freetz.org>
2013-11-21 03:03:50 +0000

    fix gsm_voice for lua\nfix disabling of dslexpert
    git-svn-id: http://svn.freetz.org/trunk@11264 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/382-enable-gsmvoice.sh
+++ patches/scripts/382-enable-gsmvoice.sh
@@ -1,8 +1,14 @@
 [ "$FREETZ_PATCH_GSMVOICE" == "y" ] || return 0
 echo1 "enabling voice-over-gsm"
 
-modsed \
-  "s/^\(return !isNaN(voiceStatus)\).*/\1;/" \
-  "${HTML_SPEC_MOD_DIR}/gsm/gsm.js" \
-  'return !isNaN(voiceStatus)'
-
+if [ "$FREETZ_AVM_HAS_ONLY_LUA" != "y" ]; then
+	modsed \
+	  "s/^\(return !isNaN(voiceStatus)\).*/\1;/" \
+	  "${HTML_SPEC_MOD_DIR}/gsm/gsm.js" \
+	  'return !isNaN(voiceStatus)'
+else
+	modsed \
+	  's/^\(local voice_status=\).*/\11/' \
+	  "{LUA_MOD_DIR}/umts.lua" \
+	  'voice_status=1'
+fi
