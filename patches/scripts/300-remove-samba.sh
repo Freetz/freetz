da96ea3c8a5024e03f1229353ca61dd2bc30afa4
cuma <cuma@freetz.org>
2013-11-22 01:56:06 +0000

    samba patch: add libsamba.so (800kb)
    git-svn-id: http://svn.freetz.org/trunk@11280 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/300-remove-samba.sh
+++ patches/scripts/300-remove-samba.sh
@@ -1,16 +1,19 @@
 
 # if nas, mediaserv und samba are removed -> remove_nas deletes menu item Heimnetz > Speicher (NAS)
 
+# AVM scripts should not kill Freetz Samba
 if [ "$FREETZ_AVM_HAS_USB_HOST" == "y" -a "$FREETZ_PACKAGE_SAMBA_SMBD" == "y" ]; then
 	sed -i -e "/killall smbd*$/d" -e "s/pidof smbd/pidof/g" "${FILESYSTEM_MOD_DIR}/etc/hotplug/storage"
 fi
 
+# remove AVM's specific samba files
 if [ "$FREETZ_PACKAGE_SAMBA_SMBD" == "y" -o "$FREETZ_REMOVE_SAMBA" == "y" ]; then
 	echo1 "remove AVM samba config"
 	rm_files \
 	  "${FILESYSTEM_MOD_DIR}/bin/inetdsamba" \
 	  "${FILESYSTEM_MOD_DIR}/sbin/samba_config_gen" \
-	  "${FILESYSTEM_MOD_DIR}/etc/samba_config.tar"
+	  "${FILESYSTEM_MOD_DIR}/etc/samba_config.tar" \
+	  "${FILESYSTEM_MOD_DIR}/lib/libsamba.so"
 
 	echo1 "patching rc.net: renaming sambastart()"
 	modsed 's/^\(sambastart *()\)/\1{ return; }\n_\1/' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.net"
@@ -28,13 +31,14 @@ if [ "$FREETZ_PACKAGE_SAMBA_SMBD" == "y" -o "$FREETZ_REMOVE_SAMBA" == "y" ]; the
 	modsed "s/CONFIG_SAMBA=.*$/CONFIG_SAMBA=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
 fi
 
-
+# no need  for AVM's or Freetz's nmbd
 if [ "$FREETZ_REMOVE_SAMBA" == "y" ] || \
   [ "$FREETZ_PACKAGE_SAMBA_SMBD" == "y" -a "$FREETZ_PACKAGE_SAMBA_NMBD" != "y" ]; then
 	echo1 "remove AVM's nmbd"
 	rm_files "${FILESYSTEM_MOD_DIR}/sbin/nmbd"
 fi
 
+# no need  for AVM's or Freetz's smbd
 if [ "$FREETZ_REMOVE_SAMBA" == "y" ]; then
 	echo1 "remove AVM samba files"
 	rm_files \
