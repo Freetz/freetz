0d151493b1c84fc416d9fac257fc34eeb485a668
cuma <cuma@freetz.org>
2013-11-23 06:53:14 +0000

    remove_myfritz: hide error message
    git-svn-id: http://svn.freetz.org/trunk@11307 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/221-remove_myfritz.sh
+++ patches/scripts/221-remove_myfritz.sh
@@ -11,10 +11,12 @@ homelua_disable tr_myfritz
 quickstart_remove myfritz
 linkbox_remove myfritz
 #html
-linkbox_file="${HTML_SPEC_MOD_DIR}/menus/menu2.html"
-linkbox_row=$(cat $linkbox_file |nl| sed -n "s/^ *\([0-9]*\).*<a href=..myfritz.. target=._blank.>.*<.a>$/\1/p")
-modsed "$((linkbox_row-8)),$((linkbox_row+18))d" $linkbox_file
-modsed '/.*class="sm_link_bold" href=".myfritz.*/d' "${HTML_SPEC_MOD_DIR}/menus/menu2.html"
-modsed 's/\(setvariable var:showmyfritz \)./\10/g' ${HTML_SPEC_MOD_DIR}/menus/menu2_internet.html
+if [ "$FREETZ_AVM_HAS_ONLY_LUA" != "y" ]; then
+	linkbox_file="${HTML_SPEC_MOD_DIR}/menus/menu2.html"
+	linkbox_row=$(cat $linkbox_file |nl| sed -n "s/^ *\([0-9]*\).*<a href=..myfritz.. target=._blank.>.*<.a>$/\1/p")
+	modsed "$((linkbox_row-8)),$((linkbox_row+18))d" $linkbox_file
+	modsed '/.*class="sm_link_bold" href=".myfritz.*/d' "${HTML_SPEC_MOD_DIR}/menus/menu2.html"
+	modsed 's/\(setvariable var:showmyfritz \)./\10/g' ${HTML_SPEC_MOD_DIR}/menus/menu2_internet.html
+fi
 
 modsed "s/CONFIG_MYFRITZ=.*/CONFIG_MYFRITZ=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
