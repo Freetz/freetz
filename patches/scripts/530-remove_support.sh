425ba11d318341aabca97729cca9cad3c8f005a0
cuma <cuma@freetz.org>
2013-11-29 13:19:02 +0000

    REMOVE_SUPPORT: add lua
    git-svn-id: http://svn.freetz.org/trunk@11339 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/530-remove_support.sh
+++ patches/scripts/530-remove_support.sh
@@ -5,4 +5,5 @@ rm_files \
   $(find ${FILESYSTEM_MOD_DIR}/sbin -maxdepth 1 $_eventsdump -name showaddrs -o -name showroutes | xargs) \
   "${FILESYSTEM_MOD_DIR}/bin/showvoipdstat" \
   "${FILESYSTEM_MOD_DIR}/bin/supportdata*" \
+  "${HTML_MOD_DIR}/all/support.lua" \
   "${HTML_LANG_MOD_DIR}/html/support*.html"
