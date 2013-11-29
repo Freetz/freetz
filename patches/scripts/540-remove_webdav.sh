a5dd6d0dd5ed5ff1dfdc7052c07968120c991a6a
cuma <cuma@freetz.org>
2013-11-29 11:13:10 +0000

    remove_webdav: some webif removals
    git-svn-id: http://svn.freetz.org/trunk@11335 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/540-remove_webdav.sh
+++ patches/scripts/540-remove_webdav.sh
@@ -14,8 +14,18 @@ for files in \
 done
 
 if [ ! "$FREETZ_PACKAGE_DAVFS2" == "y" ]; then
-	# TODO: Remove "Online-Speicher" from webinterface and remove usr/www/all/lua/webdav.lua
 	rm_files "${FILESYSTEM_MOD_DIR}/usr/share/ctlmgr/libctlwebdav.so"
+
+	# patcht Heimnetz > Speicher (NAS) > Speicher an der FRITZ!Box
+	sedfile="${HTML_LANG_MOD_DIR}/storage/settings.lua"
+	if [ -e "$sedfile" ]; then
+		echo1 "patching ${sedfile##*/}"
+		sedrows=$(cat $sedfile |nl| sed -n 's/^ *\([0-9]*\).*id="devices_table_online".*$/\1/p')
+		sedrowe=$(cat $sedfile |nl| sed -n 's/^ *\([0-9]*\).*id="webdavIndexState".*$/\1/p')
+		modsed "$((sedrows)),$((sedrowe+2))d" $sedfile
+		modsed 's/if not(g_webdav_enabled)/& or true/' $sedfile
+	fi
+		
 	echo1 "patching rc.conf"
 	modsed "s/CONFIG_WEBDAV=.*$/CONFIG_WEBDAV=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
 fi
