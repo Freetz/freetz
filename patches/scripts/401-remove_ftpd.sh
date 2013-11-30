701f5688ede031f2dbcd485999a9a6138a74d2d4
Gene Rudoy <gene@freetz.org>
2013-11-30 01:08:01 +0000

    remove_ftpd:  * remove "Speicher für Benutzer aus dem Internet freigeben" from web-if for 04.xx firmwares (tested on 7170 only)  * was removed as part of remove_samba before r11326
    git-svn-id: http://svn.freetz.org/trunk@11344 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/401-remove_ftpd.sh
+++ patches/scripts/401-remove_ftpd.sh
@@ -14,5 +14,16 @@ if [ "$FREETZ_REMOVE_FTPD" == "y" ]; then
 
 	echo1 "patching rc.conf"
 	modsed "s/CONFIG_FTP=.*$/CONFIG_FTP=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+
+	# patcht Heimnetz > Speicher (NAS)
+	sedfile="${HTML_LANG_MOD_DIR}/storage/settings.lua"
+	if [ -e "$sedfile" ]; then
+		echo1 "patching ${sedfile##*/}"
+		# entfernt "Speicher fuer Benutzer aus dem Internet freigeben" (04.xx)
+		sedrows=$(cat $sedfile |nl| sed -n 's/^ *\([0-9]*\).*<div id="uiViewInternetSharingBox".*$/\1/p')
+		sedrowe=$(cat $sedfile |nl| sed -n 's/^ *\([0-9]*\).*<div id="uiViewUseMusikBox".*$/\1/p')
+		[ -n "$sedrows" ] && modsed "$((sedrows)),$((sedrowe-2))d" $sedfile
+	fi
+
 	return 0
 fi
