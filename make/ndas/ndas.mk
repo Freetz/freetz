d36e12914762b840c769daf75f5e2489c250cf4b
Gene Rudoy <gene@freetz.org>
2013-12-15 15:52:53 +0000

    ndas:  * revise freetz makefile  * refs #465
    git-svn-id: http://svn.freetz.org/trunk@11387 f5190166-0702-4917-9039-51ec32eddaf5

--- make/ndas/ndas.mk
+++ make/ndas/ndas.mk
@@ -2,17 +2,17 @@ $(call PKG_INIT_BIN, 1.1-22)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).mipsel.tar.bz2
 $(PKG)_SOURCE_MD5:=f6b5d28b638ac074f54d404e4c136d5e
 $(PKG)_SITE:=http://freetz.magenbrot.net
-$(PKG)_BINARY:=$($(PKG)_DIR)/ndasadmin
-$(PKG)_MOD_BINARY:=$($(PKG)_DIR)/ndas_block.ko
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/ndasadmin
-$(PKG)_MOD_TARGET_DIR:=$(KERNEL_MODULES_DIR)/fs/ndas
-$(PKG)_MOD_TARGET_BINARY:=$($(PKG)_MOD_TARGET_DIR)/ndas_block.ko
+
+$(PKG)_BINARY_BUILD_DIR:=$($(PKG)_DIR)/ndasadmin
+$(PKG)_BINARY_TARGET_DIR:=$($(PKG)_DEST_DIR)/usr/bin/ndasadmin
+
+$(PKG)_MODULES:=ndas_block.ko ndas_core.ko ndas_sal.ko
+$(PKG)_MODULES_BUILD_DIR:=$($(PKG)_MODULES:%=$($(PKG)_DIR)/%)
+$(PKG)_MODULES_TARGET_DIR:=$($(PKG)_MODULES:%=$(KERNEL_MODULES_DIR)/fs/ndas/%)
 
 $(PKG)_DEPENDS_ON := kernel
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_KERNEL_VERSION
-
-$(PKG)_MODULES:=ndas_block.ko ndas_core.ko ndas_sal.ko
 
 NDAS_OPTIONS:= \
 	NDAS_KERNEL_PATH="$(FREETZ_BASE_DIR)/$(KERNEL_SOURCE_DIR)" \
@@ -27,7 +27,11 @@ $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_NOP)
 
-$($(PKG)_MOD_BINARY): $($(PKG)_DIR)/.configured
+$($(PKG)_BINARY_BUILD_DIR): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(NDAS_DIR) \
+		$(NDAS_OPTIONS) ndas-admin
+
+$($(PKG)_MODULES_BUILD_DIR): $($(PKG)_DIR)/.configured
 	$(SUBMAKE1) -C $(NDAS_DIR) \
 		$(NDAS_OPTIONS) \
 		ARCH="$(KERNEL_ARCH)" \
@@ -36,30 +40,20 @@ $($(PKG)_MOD_BINARY): $($(PKG)_DIR)/.configured
 		AR="$(KERNEL_CROSS)ar" \
 		ndas-kernel
 
-$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(NDAS_DIR) \
-		$(NDAS_OPTIONS) ndas-admin
-
-$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+$($(PKG)_BINARY_TARGET_DIR): $($(PKG)_BINARY_BUILD_DIR)
 	$(INSTALL_BINARY_STRIP)
 
-$($(PKG)_MOD_TARGET_BINARY): $($(PKG)_MOD_BINARY)
-	mkdir -p $(dir $@)
-	for i in $(NDAS_MODULES); do \
-		cp $(NDAS_DIR)/$$i $(NDAS_MOD_TARGET_DIR); \
-	done
+$($(PKG)_MODULES_TARGET_DIR): $(KERNEL_MODULES_DIR)/fs/ndas/%: $($(PKG)_DIR)/%
+	$(INSTALL_FILE)
 
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_MOD_TARGET_BINARY)
+$(pkg)-precompiled: $($(PKG)_BINARY_TARGET_DIR) $($(PKG)_MODULES_TARGET_DIR)
 
 $(pkg)-clean:
-	-$(SUBMAKE) -C $(NDAS_DIR) \
-		$(NDAS_OPTIONS) \
-		clean
-	$(RM) $(NDAS_DIR)/.configured
+	-$(SUBMAKE) -C $(NDAS_DIR) $(NDAS_OPTIONS) clean
 
 $(pkg)-uninstall:
-	$(RM) $(NDAS_TARGET_BINARY)
+	$(RM) $(NDAS_BINARY_TARGET_DIR) $(NDAS_MODULES_TARGET_DIR)
 
 $(PKG_FINISH)
