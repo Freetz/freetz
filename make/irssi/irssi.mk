7c3bb6b4e128922dd4d1f2aa28b10ac460212d3e
Gene Rudoy <gene@freetz.org>
2013-12-29 20:55:15 +0000

    irssi:  * remove unnecessary variable overrides
    git-svn-id: http://svn.freetz.org/trunk@11495 f5190166-0702-4917-9039-51ec32eddaf5

--- make/irssi/irssi.mk
+++ make/irssi/irssi.mk
@@ -2,6 +2,7 @@ $(call PKG_INIT_BIN, 0.8.15)
 $(PKG)_SOURCE:=irssi-$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5:=870db8e319f640c2bf446c30d0c24ef6
 $(PKG)_SITE:=http://irssi.org/files
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/fe-text/irssi
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/irssi
 
@@ -42,11 +43,7 @@ $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(IRSSI_DIR) \
-		CC="$(TARGET_CC)" \
-		CFLAGS="$(TARGET_CFLAGS)" \
-		LD="$(TARGET_CC)" \
-		LIBS=-ldl
+	$(SUBMAKE) -C $(IRSSI_DIR)
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
