199be05974e53f43ee001c627b0d9bc73723cdcd
Gene Rudoy <gene@freetz.org>
2013-12-29 15:36:29 +0000

    slurm:  * touch configure.in to prevent aclocal.m4 & configure from being regenerated, even being applied our no_host_curses.patch had no effect because of that (configure war overwritten)  * simplify no_host_curses.patch  * remove unnecessary overrides of CC and CFLAGS
    git-svn-id: http://svn.freetz.org/trunk@11486 f5190166-0702-4917-9039-51ec32eddaf5

--- make/slurm/slurm.mk
+++ make/slurm/slurm.mk
@@ -2,19 +2,21 @@ $(call PKG_INIT_BIN, 0.3.3)
 $(PKG)_SOURCE:=slurm-$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5:=e68d09202b835c644f7f6b7f070f29a2
 $(PKG)_SITE:=http://www.wormulon.net/slurm
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/slurm
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/slurm
 
 $(PKG)_DEPENDS_ON := ncurses
+
+# touch configure.in to prevent aclocal.m4 & configure from being regenerated
+$(PKG)_CONFIGURE_PRE_CMDS += touch -t 200001010000.00 ./configure.in;
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(SLURM_DIR) \
-		CC="$(TARGET_CC)" \
-		CFLAGS="$(TARGET_CFLAGS)"
+	$(SUBMAKE) -C $(SLURM_DIR)
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
