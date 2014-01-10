6953e4b1dd3022fa011ec98756fce9a03645dacc
Gene Rudoy <gene@freetz.org>
2014-01-10 16:05:57 +0000

    ltrace:  * beautify makefile a bit
    git-svn-id: http://svn.freetz.org/trunk@11537 f5190166-0702-4917-9039-51ec32eddaf5

--- make/ltrace/ltrace.mk
+++ make/ltrace/ltrace.mk
@@ -2,15 +2,18 @@ $(call PKG_INIT_BIN, 0.5.3)
 $(PKG)_SOURCE:=ltrace_$($(PKG)_VERSION).orig.tar.gz
 $(PKG)_SOURCE_MD5:=3fa7fe715ab879db08bd06d1d59fd90f
 $(PKG)_SITE:=http://ftp.debian.org/debian/pool/main/l/ltrace
-$(PKG)_BINARY:=$($(PKG)_DIR)/ltrace
-$(PKG)_CONF:=$($(PKG)_DIR)/etc/ltrace.conf
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/ltrace
-$(PKG)_TARGET_CONF:=$($(PKG)_DEST_DIR)/etc/ltrace.conf
+
 $(PKG)_CATEGORY:=Debug helpers
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/ltrace
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/ltrace
+
+$(PKG)_CONF:=$($(PKG)_DIR)/etc/ltrace.conf
+$(PKG)_TARGET_CONF:=$($(PKG)_DEST_DIR)/etc/ltrace.conf
 
 $(PKG)_DEPENDS_ON := libelf
 
-$(PKG)_CONFIGURE_PRE_CMDS += ln -sf ./mipsel sysdeps/linux-gnu/mips ;
+$(PKG)_CONFIGURE_PRE_CMDS += ln -sf ./mipsel sysdeps/linux-gnu/mips;
 $(PKG)_CONFIGURE_PRE_CMDS += ( cd sysdeps/linux-gnu/mips; chmod +x ../mksyscallent_mips; \
 	../mksyscallent_mips $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/asm/unistd.h > syscallent.h; \
 	../mksignalent $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/asm/signal.h > signalent.h; );
