cf267a1e0b83ae43ebd68622ccdf89d18332fcd0
Gene Rudoy <gene@freetz.org>
2014-01-10 16:04:00 +0000

    ltrace:  * fix download URL
    git-svn-id: http://svn.freetz.org/trunk@11536 f5190166-0702-4917-9039-51ec32eddaf5

--- make/ltrace/ltrace.mk
+++ make/ltrace/ltrace.mk
@@ -1,7 +1,7 @@
 $(call PKG_INIT_BIN, 0.5.3)
 $(PKG)_SOURCE:=ltrace_$($(PKG)_VERSION).orig.tar.gz
 $(PKG)_SOURCE_MD5:=3fa7fe715ab879db08bd06d1d59fd90f
-$(PKG)_SITE:=http://ftp.debian.org/pool/main/l/ltrace
+$(PKG)_SITE:=http://ftp.debian.org/debian/pool/main/l/ltrace
 $(PKG)_BINARY:=$($(PKG)_DIR)/ltrace
 $(PKG)_CONF:=$($(PKG)_DIR)/etc/ltrace.conf
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/ltrace
