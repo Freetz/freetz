0e915c357c09df06c4a86322a357667a7821f615
cuma <cuma@freetz.org>
2014-01-15 19:47:10 +0000

    bump: HAProxy 1.5-dev21 (refs #2088)
    git-svn-id: http://svn.freetz.org/trunk@11562 f5190166-0702-4917-9039-51ec32eddaf5

--- make/haproxy/haproxy.mk
+++ make/haproxy/haproxy.mk
@@ -1,12 +1,15 @@
-$(call PKG_INIT_BIN, 1.4.24)
+$(call PKG_INIT_BIN, 1.5-dev21)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=86422620faa9759907563d5e0524b98c
-$(PKG)_SITE:=http://haproxy.1wt.eu/download/1.4/src
+$(PKG)_SOURCE_MD5:=c784f55537c8718ed024b2b725cd3770
+$(PKG)_SITE:=http://haproxy.1wt.eu/download/1.5/src/devel
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/haproxy
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/haproxy
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_KERNEL_VERSION_2_6_28_MIN
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_HAPROXY_WITH_OPENSSL
+
+$(PKG)_DEPENDS_ON += $(if $(FREETZ_PACKAGE_HAPROXY_WITH_OPENSSL),openssl)
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -15,6 +18,7 @@ $(PKG_CONFIGURED_NOP)
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(HAPROXY_DIR) \
 		TARGET=$(if $(FREETZ_KERNEL_VERSION_2_6_28_MIN),linux2628,linux26) \
+		USE_OPENSSL=$(if $(FREETZ_PACKAGE_HAPROXY_WITH_OPENSSL),1) \
 		CC="$(TARGET_CC)" \
 		CFLAGS="$(TARGET_CFLAGS) -ffunction-sections -fdata-sections" \
 		LDFLAGS="-Wl,--gc-sections"
