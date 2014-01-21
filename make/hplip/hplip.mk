1e8292225b48de6606cc8e13b827266343b01635
Whoopie <whoopie79@gmx.net>
2014-01-21 20:26:34 +0000

    hplip: bump version to 3.14.1
    git-svn-id: http://svn.freetz.org/trunk@11580 f5190166-0702-4917-9039-51ec32eddaf5

--- make/hplip/hplip.mk
+++ make/hplip/hplip.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 3.13.9)
+$(call PKG_INIT_BIN, 3.14.1)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=8fcbbd0d3a0b76550f325bd6cf46d9d9
+$(PKG)_SOURCE_MD5:=11eb45f3d3edf1f03887fd13afc61b51
 $(PKG)_SITE:=@SF/hplip
 $(PKG)_LIB_IP_VERSION=0.0.1
 $(PKG)_LIB_IP_BINARY:=$($(PKG)_DIR)/.libs/libhpip.so.$($(PKG)_LIB_IP_VERSION)
