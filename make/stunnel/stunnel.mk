6b120b1a57fad5dd68ccf37a3c4e3c8627c8e9fb
Gene Rudoy <gene@freetz.org>
2014-01-29 23:51:21 +0000

    stunnel:  * use magic (C|LD)FLAGS to reduce binary size
    git-svn-id: http://svn.freetz.org/trunk@11599 f5190166-0702-4917-9039-51ec32eddaf5

--- make/stunnel/stunnel.mk
+++ make/stunnel/stunnel.mk
@@ -9,10 +9,20 @@ $(PKG)_BINARY:=$($(PKG)_DIR)/src/$(pkg)
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/$(pkg)
 
 $(PKG)_DEPENDS_ON := openssl zlib
+
 $(PKG)_REBUILD_SUBOPTS += FREETZ_OPENSSL_SHLIB_VERSION
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_STUNNEL_STATIC
+$(PKG)_REBUILD_SUBOPTS += FREETZ_TARGET_IPV6_SUPPORT
+
+# add EXTRA_CFLAGS, EXTRA_LDFLAGS variables to all Makefile.in's
+$(PKG)_CONFIGURE_PRE_CMDS += $(SED) -i -r -e 's,^((C|LD)FLAGS)[ \t]*=[ \t]*@\1@,& $$$$(EXTRA_\1),' `find . -name Makefile.in`;
+
+# reduce binary size by setting appropriate CFLAGS/LDFLAGS
+$(PKG)_EXTRA_CFLAGS  += -ffunction-sections -fdata-sections
+$(PKG)_EXTRA_LDFLAGS += -Wl,--gc-sections
 
 ifeq ($(strip $(FREETZ_PACKAGE_STUNNEL_STATIC)),y)
-$(PKG)_LDFLAGS := -all-static
+$(PKG)_EXTRA_LDFLAGS += -all-static
 endif
 
 $(PKG)_CONFIGURE_ENV += ac_cv_file__dev_ptmx=no
@@ -24,16 +34,14 @@ $(PKG)_CONFIGURE_OPTIONS += --with-threads=pthread
 $(PKG)_CONFIGURE_OPTIONS += --with-ssl="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_TARGET_IPV6_SUPPORT),--enable-ipv6,--disable-ipv6)
 
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_STUNNEL_STATIC
-$(PKG)_REBUILD_SUBOPTS += FREETZ_TARGET_IPV6_SUPPORT
-
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(STUNNEL_DIR) \
-		LDFLAGS="$(STUNNEL_LDFLAGS)"
+		EXTRA_CFLAGS="$(STUNNEL_EXTRA_CFLAGS)" \
+		EXTRA_LDFLAGS="$(STUNNEL_EXTRA_LDFLAGS)"
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
