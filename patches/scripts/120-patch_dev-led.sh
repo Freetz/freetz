a14d273b35fb0b4b1b6a8a72563f8418d3229bb5
Gene Rudoy <gene@freetz.org>
2014-03-02 19:07:09 +0000

    * remove /dev/led patch, we don't know anymore if, why and for what boxes it was needed  * fixes #1548, feel free to reopen the ticket if after removing the patch you get error message you haven't had before
    git-svn-id: http://svn.freetz.org/trunk@11840 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/120-patch_dev-led.sh
+++
@@ -1,5 +0,0 @@
-echo1 "applying /dev/led patch"
-
-for f in /bin/update_led_on /bin/update_led_off; do
-	[ -f "${FILESYSTEM_MOD_DIR}${f}" ] && modsed 's,/var/led,/dev/led,g' "${FILESYSTEM_MOD_DIR}${f}"
-done
