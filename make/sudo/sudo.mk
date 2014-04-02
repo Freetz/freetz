e5bd23a624a3ccc403edf1ff479049889cbd1ea8
Gene Rudoy <gene@freetz.org>
2014-04-02 19:52:49 +0000

    sudo:  * fix typos/copy&paste errors  * refs #1492
    git-svn-id: http://svn.freetz.org/trunk@11927 f5190166-0702-4917-9039-51ec32eddaf5

--- make/sudo/sudo.mk
+++ make/sudo/sudo.mk
@@ -2,7 +2,7 @@ $(call PKG_INIT_BIN, 1.7.10p8)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5:=4620e31c28b387dd9582dfd42e930cfb
 $(PKG)_SITE:=http://www.sudo.ws/sudo/dist
-$(PKG)_BINARIY:=$(pkg)
+$(PKG)_BINARY:=$(pkg)
 $(PKG)_BINARY_BUILD_DIR:=$($(PKG)_DIR)/$(pkg)
 $(PKG)_BINARY_TARGET_DIR:=$($(PKG)_DEST_DIR)/usr/bin/$(pkg)
 
@@ -46,6 +46,6 @@ $(pkg)-clean:
 	$(RM) $(SUDO_DIR)/.configured
 
 $(pkg)-uninstall:
-	$(RM) $(SUDO_BINARIES_TARGET_DIR) $(TRICKLE_LIBS_TARGET_DIR)
+	$(RM) $(SUDO_BINARY_TARGET_DIR) $(SUDO_LIBS_TARGET_DIR)
 
 $(PKG_FINISH)
