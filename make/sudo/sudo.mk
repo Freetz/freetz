5e5aeb57991f5e24644fe3838da9eedd3ed91b40
Jörg Eilers <eilersj@gmx.de>
2014-04-02 16:30:18 +0000

    Add package sudo 	* Version 1.7.10p8 	* with sudoers-file 	* Inspired by ticket #1492 	* Binary based on the patches by sf3978 and dns 	* refs #1492
    git-svn-id: http://svn.freetz.org/trunk@11924 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/sudo/sudo.mk
@@ -0,0 +1,52 @@
+$(call PKG_INIT_BIN, 1.7.10p8)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+#$(PKG)_SOURCE_MD5:=d9002b306986fa7735ee6aa003624885
+$(PKG)_SITE:=http://www.sudo.ws/sudo/dist
+$(PKG)_BINARIY:=$(pkg)
+$(PKG)_BINARY_BUILD_DIR:=$($(PKG)_DIR)/$(pkg)
+$(PKG)_BINARY_TARGET_DIR:=$($(PKG)_DEST_DIR)/usr/bin/$(pkg)
+
+$(PKG)_LIBS:=lib$(pkg)_noexec.so
+$(PKG)_LIBS_BUILD_DIR:=$($(PKG)_LIBS:%=$($(PKG)_DIR)/.libs/%)
+$(PKG)_LIBS_TARGET_DIR:=$($(PKG)_LIBS:%=$($(PKG)_DEST_LIBDIR)/%)
+
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
+
+$(PKG)_CONFIGURE_OPTIONS += --with-editor=/bin/nano \
+			    --without-lecture \
+			    --without-pam \
+			    --without-sendmail \
+			    --disable-pam-session \
+			    --disable-root-mailer \
+			    --disable-root-sudo \
+			    --sysconfdir=/mod/etc \
+			    --disable-zlib
+
+$(PKG)_CONFIGURE_ENV += sudo_cv_uid_t_len=10
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY_BUILD_DIR) $($(PKG)_LIBS_BUILD_DIR): $($(PKG)_DIR)/.configured
+	$(SUBMAKE1) -C $(SUDO_DIR)
+
+$($(PKG)_BINARY_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/%
+	$(INSTALL_BINARY_STRIP)
+	chmod +s $(SUDO_BINARY_TARGET_DIR)
+
+$($(PKG)_LIBS_TARGET_DIR): $($(PKG)_DEST_LIBDIR)/%: $($(PKG)_DIR)/.libs/%
+	$(INSTALL_LIBRARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_BINARY_TARGET_DIR) $($(PKG)_LIBS_TARGET_DIR)
+
+$(pkg)-clean:
+	-$(SUBMAKE1) -C $(SUDO_DIR) clean
+	$(RM) $(SUDO_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(SUDO_BINARIES_TARGET_DIR) $(TRICKLE_LIBS_TARGET_DIR)
+
+$(PKG_FINISH)
