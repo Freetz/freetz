de64f7a271277ca5ea13cca3be077bf34dfef77c
Gene Rudoy <gene@freetz.org>
2014-04-24 21:25:52 +0000

    7240/7270v2/7270v3 Labor:  * latest labor firmwares for these boxes (XX.06.04-27861) do not support /var/flash/debug.cfg file anymore (s. [http://www.ip-phone-forum.de/showthread.php?t=269276 this thread] for details), add patch restoring /var/flash/debug.cfg support  * refs r11948
    git-svn-id: http://svn.freetz.org/trunk@11950 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ patches/scripts/630-restore_debug_cfg_support.sh
@@ -0,0 +1,14 @@
+grep -q "mknod /var/flash/debug.cfg" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.tail.sh" 2>/dev/null && return 0
+
+echo1 "restoring support for /var/flash/debug.cfg"
+modsed '/echo 1 > \/proc\/sys\/kernel\/panic_on_oops/ a\
+########################################################################################## \
+## user rc file \
+########################################################################################## \
+if [ -z "$CPU_NR" ] || [ "$CPU_NR" = "1" ] ; then \
+mknod /var/flash/debug.cfg c $tffs_major $((0x62)) \
+if ! /usr/bin/checkempty /var/flash/debug.cfg 2>/dev/null; then \
+. /var/flash/debug.cfg \
+fi \
+fi' \
+"${FILESYSTEM_MOD_DIR}/etc/init.d/rc.tail.sh"
