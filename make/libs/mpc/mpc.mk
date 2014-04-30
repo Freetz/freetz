c94d3bb0f4a3f33c2c7165b78e8078075862ef2d
Gene Rudoy <gene@freetz.org>
2014-04-30 20:59:36 +0000

    mpc:  * bump version to [http://www.multiprecision.org/index.php?prog=mpc&page=home 1.0.2]  * refs #1939
    git-svn-id: http://svn.freetz.org/trunk@11971 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/mpc/mpc.mk
+++ make/libs/mpc/mpc.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_LIB, 1.0.1)
+$(call PKG_INIT_LIB, 1.0.2)
 $(PKG)_LIB_VERSION:=3.0.0
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=b32a2e1a3daa392372fbd586d1ed3679
+$(PKG)_SOURCE_SHA1:=5072d82ab50ec36cc8c0e320b5c377adb48abe70
 $(PKG)_SITE:=http://www.multiprecision.org/mpc/download
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/.libs/libmpc.so.$($(PKG)_LIB_VERSION)
