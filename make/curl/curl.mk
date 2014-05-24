19592b4c38c9eecf98454015f4f3b688fcfffb59
Gene Rudoy <gene@freetz.org>
2014-05-24 23:17:12 +0000

    curl:  * bump version to [http://curl.haxx.se/changes.html#7_37_0 7.37.0]  * (temporarily) drop support for curl linked against PolarSSL, the new version requires PolarSSL-1.3.x
    git-svn-id: http://svn.freetz.org/trunk@12059 f5190166-0702-4917-9039-51ec32eddaf5

--- make/curl/curl.mk
+++ make/curl/curl.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_BIN, 7.36.0)
+$(call PKG_INIT_BIN, 7.37.0)
 $(PKG)_LIB_VERSION:=4.3.0
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=e6d1f9d1b59da5062109ffe14e0569a4
+$(PKG)_SOURCE_MD5:=7dda0cc2e4136f78d5801ac347be696b
 $(PKG)_SITE:=http://curl.haxx.se/download
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src$(if $(FREETZ_PACKAGE_CURL_STATIC),,/.libs)/curl
