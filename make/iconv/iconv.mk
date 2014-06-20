fe66f7d178070817740d89bf38fd7e68514cd199
Gene Rudoy <gene@freetz.org>
2014-06-20 16:52:53 +0000

    libiconv:  * convert from "library only"-package to "library+binary"-package  * provide iconv-binary  * compile-tested only
    git-svn-id: http://svn.freetz.org/trunk@12151 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/iconv/iconv.mk
@@ -0,0 +1,64 @@
+$(call PKG_INIT_BIN, 1.13.1)
+$(PKG)_LIB_VERSION:=2.5.0
+$(PKG)_SOURCE:=lib$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=7ab33ebd26687c744a37264a330bbe9a
+$(PKG)_SITE:=@GNU/$(pkg)
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/lib$(pkg)-$($(PKG)_VERSION)
+
+ifeq ($(strip $(FREETZ_TARGET_UCLIBC_0_9_28)),y)
+LIB$(PKG)_PREFIX:=/usr
+else
+LIB$(PKG)_PREFIX:=/usr/lib/lib$(pkg)
+endif
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/.libs/iconv_no_i18n
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/iconv
+
+$(PKG)_LIB_BINARY:=$($(PKG)_DIR)/lib/.libs/lib$(pkg).so.$($(PKG)_LIB_VERSION)
+$(PKG)_LIB_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)$(LIB$(PKG)_PREFIX)/lib/lib$(pkg).so.$($(PKG)_LIB_VERSION)
+$(PKG)_LIB_TARGET_BINARY:=$($(PKG)_TARGET_LIBDIR)/lib$(pkg).so.$($(PKG)_LIB_VERSION)
+
+$(PKG)_CONFIGURE_OPTIONS += --enable-shared
+$(PKG)_CONFIGURE_OPTIONS += --enable-static
+$(PKG)_CONFIGURE_OPTIONS += --disable-rpath
+$(PKG)_CONFIGURE_OPTIONS += --enable-relocatable
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY) $($(PKG)_LIB_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(ICONV_DIR)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$($(PKG)_LIB_STAGING_BINARY): $($(PKG)_LIB_BINARY)
+	mkdir -p $(TARGET_TOOLCHAIN_STAGING_DIR)$(LIBICONV_PREFIX)/{include,lib}
+	cp -a $(ICONV_DIR)/include/iconv.h.inst $(TARGET_TOOLCHAIN_STAGING_DIR)$(LIBICONV_PREFIX)/include/iconv.h
+	chmod 644 $(TARGET_TOOLCHAIN_STAGING_DIR)$(LIBICONV_PREFIX)/include/iconv.h
+	cat $(ICONV_DIR)/lib/libiconv.la \
+		| sed -r -e 's,^(installed=)no,\1yes,g' -e "s,^(libdir=)'.*',\1'$(LIBICONV_PREFIX)/lib',g" \
+		> $(TARGET_TOOLCHAIN_STAGING_DIR)$(LIBICONV_PREFIX)/lib/libiconv.la
+	chmod 755 $(TARGET_TOOLCHAIN_STAGING_DIR)$(LIBICONV_PREFIX)/lib/libiconv.la
+	$(PKG_FIX_LIBTOOL_LA) $(TARGET_TOOLCHAIN_STAGING_DIR)$(LIBICONV_PREFIX)/lib/libiconv.la
+	cp -a $(ICONV_DIR)/lib/.libs/libiconv.{a,so*} $(TARGET_TOOLCHAIN_STAGING_DIR)$(LIBICONV_PREFIX)/lib/
+
+$($(PKG)_LIB_TARGET_BINARY): $($(PKG)_LIB_STAGING_BINARY)
+	$(INSTALL_LIBRARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_LIB_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(ICONV_DIR) clean
+	$(RM) \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)$(LIBICONV_PREFIX)/lib/libiconv* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)$(LIBICONV_PREFIX)/include/iconv.h
+
+$(pkg)-uninstall:
+	$(RM) $(ICONV_TARGET_BINARY) $(ICONV_TARGET_LIBDIR)/libiconv*.so*
+
+$(call PKG_ADD_LIB,libiconv)
+$(PKG_FINISH)
