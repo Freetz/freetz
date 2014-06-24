ea9accf9f0ec4f0b0bca923f20b3a493f0ed0131
Gene Rudoy <gene@freetz.org>
2014-06-24 18:11:27 +0000

    rsync:  * fix download URL  * fixes #2485
    git-svn-id: http://svn.freetz.org/trunk@12179 f5190166-0702-4917-9039-51ec32eddaf5

--- make/rsync/rsync.mk
+++ make/rsync/rsync.mk
@@ -1,7 +1,7 @@
 $(call PKG_INIT_BIN, 3.1.0)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5:=3be148772a33224771a8d4d2a028b132
-$(PKG)_SITE:=http://samba.anu.edu.au/ftp/rsync
+$(PKG)_SITE:=http://samba.org/ftp/rsync/src
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/rsync
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/rsync
