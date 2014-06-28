56d5fdf826eb85ddcf71eb6575227388dd3d75ca
Gene Rudoy <gene@freetz.org>
2014-06-28 11:28:46 +0000

    lzo:  * bump version to [http://www.oberhumer.com/opensource/lzo/#news 2.07] (by SilenBob)  * fixes #2489
    git-svn-id: http://svn.freetz.org/trunk@12199 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/lzo/lzo.mk
+++ make/libs/lzo/lzo.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_LIB, 2.06)
+$(call PKG_INIT_LIB, 2.07)
 $(PKG)_LIB_VERSION:=2.0.0
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=95380bd4081f85ef08c5209f4107e9f8
+$(PKG)_SOURCE_MD5:=4011935e95171e78ad4894f7335c982a
 $(PKG)_SITE:=http://www.oberhumer.com/opensource/lzo/download
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/.libs/liblzo2.so.$($(PKG)_LIB_VERSION)
