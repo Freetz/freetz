517d41e3db5c2ae6492b7fbd585cb05a79ca868e
Gene Rudoy <gene@freetz.org>
2014-06-29 21:30:22 +0000

    * introduce new freetz-function {{{create_LD_PRELOAD_wrapper}}}  * use it to create ctlmgr-wrapper
    git-svn-id: http://svn.freetz.org/trunk@12208 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/105-add_ctlmgr-wrapper.sh
+++ patches/scripts/105-add_ctlmgr-wrapper.sh
@@ -1,12 +1,3 @@
 [ "$FREETZ_LIB_libctlmgr" == "y" ] || return 0
 
-mkdir -p "$FILESYSTEM_MOD_DIR/usr/bin/avm/"
-mv "$FILESYSTEM_MOD_DIR/usr/bin/ctlmgr" "$FILESYSTEM_MOD_DIR/usr/bin/avm/ctlmgr"
-
-cat << 'EOF' >> "$FILESYSTEM_MOD_DIR/usr/bin/ctlmgr"
-#!/bin/sh
-export LD_PRELOAD=libctlmgr.so
-exec /usr/bin/avm/ctlmgr "$@"
-EOF
-
-chmod 755 "$FILESYSTEM_MOD_DIR/usr/bin/ctlmgr"
+create_LD_PRELOAD_wrapper /usr/bin/ctlmgr libctlmgr.so
