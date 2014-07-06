84400cffe8a0f157343d7dff4652a17d0b8d6ac5
Gene Rudoy <gene@freetz.org>
2014-07-06 12:17:47 +0000

    python:  * bump version to [http://hg.python.org/cpython/file/ee879c0ffa11/Misc/NEWS 2.7.8]
    git-svn-id: http://svn.freetz.org/trunk@12237 f5190166-0702-4917-9039-51ec32eddaf5

--- make/python/python.mk
+++ make/python/python.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_BIN, 2.7.6)
+$(call PKG_INIT_BIN, 2.7.8)
 $(PKG)_MAJOR_VERSION:=$(call GET_MAJOR_VERSION,$($(PKG)_VERSION))
 $(PKG)_SOURCE:=Python-$($(PKG)_VERSION).tar.xz
-$(PKG)_SOURCE_MD5:=bcf93efa8eaf383c98ed3ce40b763497
+$(PKG)_SOURCE_MD5:=d235bdfa75b8396942e360a70487ee00
 $(PKG)_SITE:=http://www.python.org/ftp/python/$($(PKG)_VERSION)
 
 $(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/Python-$($(PKG)_VERSION)
