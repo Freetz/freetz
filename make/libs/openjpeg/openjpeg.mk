bf86f9c7718c6584d1310aeb18e2083dbd8361fd
Gene Rudoy <gene@freetz.org>
2014-07-07 22:13:59 +0000

    openjpeg:  * bump version to [http://openjpeg.googlecode.com/svn/tags/version.1.5.2/NEWS 1.5.2]
    git-svn-id: http://svn.freetz.org/trunk@12242 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/openjpeg/openjpeg.mk
+++ make/libs/openjpeg/openjpeg.mk
@@ -1,19 +1,17 @@
-$(call PKG_INIT_LIB, 1_4)
-$(PKG)_LIB_VERSION:=1.4.0
-$(PKG)_TARBALL_DIR:=$(pkg)_v$($(PKG)_VERSION)_sources_r697
-$(PKG)_SOURCE:=$($(PKG)_TARBALL_DIR).tgz
-$(PKG)_SOURCE_MD5:=7870bb84e810dec63fcf3b712ebb93db
-$(PKG)_SITE:=http://openjpeg.googlecode.com/files
-
-$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/$($(PKG)_TARBALL_DIR)
+$(call PKG_INIT_LIB, 1.5.2)
+$(PKG)_LIB_VERSION:=1.5.2
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=c41772c30fb1c272358b3707233134a1
+$(PKG)_SITE:=http://openjpeg.googlecode.com/files,@SF/openjpeg.mirror
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/libopenjpeg/.libs/libopenjpeg.so.$($(PKG)_LIB_VERSION)
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libopenjpeg.so.$($(PKG)_LIB_VERSION)
 $(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libopenjpeg.so.$($(PKG)_LIB_VERSION)
 
-$(PKG)_DEPENDS_ON += zlib
+$(PKG)_BUILD_PREREQ += autoreconf
 
-$(PKG)_CONFIGURE_PRE_CMDS += touch -t 200001010000.00 configure.ac aclocal.m4;
+$(PKG)_CONFIGURE_PRE_CMDS += autoreconf -f -i;
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
 
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
@@ -31,14 +29,12 @@ $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(OPENJPEG_DIR)/libopenjpeg \
-		FREETZ_CFLAGS="$(TARGET_CFLAGS)" \
 		libopenjpeg.la
 
 $($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
 	$(SUBMAKE) -C $(OPENJPEG_DIR)/libopenjpeg \
-		includedir=/usr/include \
 		DESTDIR=$(TARGET_TOOLCHAIN_STAGING_DIR) \
-		install-includeHEADERS install-libLTLIBRARIES
+		install-includesHEADERS install-libLTLIBRARIES
 	$(PKG_FIX_LIBTOOL_LA) \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libopenjpeg.la
 
@@ -51,8 +47,8 @@ $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(OPENJPEG_DIR) clean
-	rm -f \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/openjpeg.h \
+	$(RM) -r \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/openjpeg* \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libopenjpeg*
 
 $(pkg)-uninstall:
