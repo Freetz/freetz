17e35c6aa1b722b1e0557ba10666c87a6f1339b8
Gene Rudoy <gene@freetz.org>
2014-07-12 14:47:53 +0000

    uClibc++:  * bump version to the [http://git.uclibc.org/uClibc++/commit/?id=7c90261eb9e5cff4ea3a4e5580e4f2bc7543cb21 latest] [http://git.uclibc.org/uClibc++/log/ git-revision]  * revise patches  * refs #1939
    git-svn-id: http://svn.freetz.org/trunk@12255 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/uclibcxx/uclibc++.mk
+++ make/libs/uclibcxx/uclibc++.mk
@@ -1,8 +1,7 @@
-$(call PKG_INIT_LIB, 5976d7536d8c7a8d5a7f60fd2a3c34876a224f30, uclibcxx)
-$(PKG)_LIB_VERSION:=0.2.3
+$(call PKG_INIT_LIB, 7c90261eb9e5cff4ea3a4e5580e4f2bc7543cb21, uclibcxx)
+$(PKG)_LIB_VERSION:=0.2.5
 $(PKG)_SOURCE:=uClibc++-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=838aa335ef7f2fcf7191e82d375d0178
-#release URL: $(PKG)_SITE:=http://cxx.uclibc.org/src
+$(PKG)_SOURCE_MD5:=b7b81b8ffa75e6b530b850fddd00e83b
 $(PKG)_SITE:=http://git.uclibc.org/uClibc++/snapshot
 $(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/uClibc++-$($(PKG)_VERSION)
 
@@ -10,6 +9,13 @@ $(PKG)_BINARY:=$($(PKG)_DIR)/src/libuClibc++-$($(PKG)_LIB_VERSION).so
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libuClibc++-$($(PKG)_LIB_VERSION).so
 $(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libuClibc++-$($(PKG)_LIB_VERSION).so
 
+$(PKG)_STAGING_WRAPPER:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/$(REAL_GNU_TARGET_NAME)-g++-uc
+
+$(PKG)_COMMON_MAKE_OPTS := -C $($(PKG)_DIR)
+$(PKG)_COMMON_MAKE_OPTS += CPU_CFLAGS="$(TARGET_CFLAGS)"
+$(PKG)_COMMON_MAKE_OPTS += CROSS_COMPILE="$(TARGET_CROSS)"
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_TARGET_LFS
 $(PKG)_REBUILD_SUBOPTS += FREETZ_LIB_libuClibc__WITH_WCHAR
 
 $(PKG_SOURCE_DOWNLOAD)
@@ -27,35 +33,36 @@ $($(PKG)_DIR)/.configured: $($(PKG)_DIR)/.unpacked
 	touch $@
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(UCLIBCXX_DIR) \
-		CPU_CFLAGS="$(TARGET_CFLAGS)" \
-		CROSS="$(TARGET_CROSS)" \
-		all
+	$(SUBMAKE) $(UCLIBCXX_COMMON_MAKE_OPTS)
 
 $($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
-	$(SUBMAKE) -C $(UCLIBCXX_DIR) \
-		CPU_CFLAGS="$(TARGET_CFLAGS)" \
-		CROSS="$(TARGET_CROSS)" \
+	$(SUBMAKE) $(UCLIBCXX_COMMON_MAKE_OPTS) \
 		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr" \
-		install
-	$(SED) -i -e 's|-I/include/uClibc++|-I$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/uClibc++|g' $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/g++-uc
-	$(SED) -i -e 's|-L/lib/|-L$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/|g' $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/g++-uc
-	mv	$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/g++-uc \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/$(REAL_GNU_TARGET_NAME)-g++-uc
-	ln -sf $(REAL_GNU_TARGET_NAME)-g++-uc $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/$(GNU_TARGET_NAME)-g++-uc
+		install-include install-lib
+	touch -c $@
+
+$($(PKG)_STAGING_WRAPPER): $($(PKG)_BINARY)
+	$(SUBMAKE) $(UCLIBCXX_COMMON_MAKE_OPTS) \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr" \
+		install-bin
+	$(SED)  -i \
+		-e 's,-I/include/uClibc++,-I$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/uClibc++,g' \
+		-e 's,-L/lib/,-L$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/,g' \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/g++-uc
+	mv $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/g++-uc $@
+	ln -sf $(notdir $@) $(dir $@)$(GNU_TARGET_NAME)-g++-uc
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
 	$(INSTALL_LIBRARY_STRIP_WILDCARD_BEFORE_SO)
 
-uclibcxx: $($(PKG)_STAGING_BINARY)
+uclibcxx: $($(PKG)_STAGING_BINARY) $($(PKG)_STAGING_WRAPPER)
 
 uclibcxx-precompiled: $($(PKG)_TARGET_BINARY)
 
 uclibcxx-clean:
-	-$(SUBMAKE) -C $(UCLIBCXX_DIR) clean
+	-$(SUBMAKE) $(UCLIBCXX_COMMON_MAKE_OPTS) clean
 	$(RM) -r \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/$(GNU_TARGET_NAME)-g++-uc \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/$(REAL_GNU_TARGET_NAME)-g++-uc \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/*-g++-uc \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libuClibc++* \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/uClibc++
 
