ba78068e0a2151ba9b2d0cc9cb6d468bed88bed1
Whoopie <whoopie79@gmx.net>
2014-07-18 16:24:06 +0000

    curl: bump version to 7.37.1
    git-svn-id: http://svn.freetz.org/trunk@12262 f5190166-0702-4917-9039-51ec32eddaf5

--- make/curl/curl.mk
+++ make/curl/curl.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_BIN, 7.37.0)
+$(call PKG_INIT_BIN, 7.37.1)
 $(PKG)_LIB_VERSION:=4.3.0
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=7dda0cc2e4136f78d5801ac347be696b
+$(PKG)_SOURCE_MD5:=95c627abcf6494f5abe55effe7cd6a57
 $(PKG)_SITE:=http://curl.haxx.se/download
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src$(if $(FREETZ_PACKAGE_CURL_STATIC),,/.libs)/curl
