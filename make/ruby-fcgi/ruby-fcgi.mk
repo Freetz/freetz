ad53ce20f35c69932d9ec6acd225ad033d1739ab
Gene Rudoy <gene@freetz.org>
2014-07-25 22:56:36 +0000

    ruby/ruby-fcgi:  * check for ruby version on the build-system, refs #2511  * minor makefile cleanups
    git-svn-id: http://svn.freetz.org/trunk@12272 f5190166-0702-4917-9039-51ec32eddaf5

--- make/ruby-fcgi/ruby-fcgi.mk
+++ make/ruby-fcgi/ruby-fcgi.mk
@@ -2,8 +2,9 @@ $(call PKG_INIT_BIN, 0.8.7)
 $(PKG)_SOURCE:=ruby-fcgi-$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5:=fe4d4a019785e8108668a3e81a5df5e1
 $(PKG)_SITE:=http://www.moonwolf.com/ruby/archive
+
 $(PKG)_BINARY:=$(RUBY_FCGI_DIR)/lib/fcgi.rb
-$(PKG)_TARGET_BINARY:=$(RUBY_FCGI_DEST_DIR)/usr/lib/ruby/1.8/fcgi.rb
+$(PKG)_TARGET_BINARY:=$(RUBY_FCGI_DEST_DIR)/usr/lib/ruby/$(RUBY_MAJOR_VERSION)/fcgi.rb
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -12,8 +13,7 @@ $(PKG_CONFIGURED_NOP)
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
-	mkdir -p $(dir $@)
-	cp $^ $@
+	$(INSTALL_FILE)
 
 $(pkg):
 
--- make/ruby/ruby.mk
+++ make/ruby/ruby.mk
@@ -1,12 +1,14 @@
 $(call PKG_INIT_BIN, 1.8.6-p368)
+$(PKG)_MAJOR_VERSION:=$(call GET_MAJOR_VERSION,$($(PKG)_VERSION))
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5:=508bf1911173ac43e4e6c31d9dc36b8f
-$(PKG)_SITE:=ftp://ftp.ruby-lang.org/pub/ruby/1.8/
+$(PKG)_SITE:=ftp://ftp.ruby-lang.org/pub/ruby/$($(PKG)_MAJOR_VERSION)
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/$(pkg)
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/$(pkg)
 
-$(PKG)_BUILD_PREREQ += ruby
-$(PKG)_BUILD_PREREQ_HINT := Hint: on Debian-like systems this binary is provided by a package with the same name
+$(PKG)_BUILD_PREREQ += ruby:$(RUBY_MAJOR_VERSION)
+$(PKG)_BUILD_PREREQ_HINT := Hint: on Debian-like systems the required files are provided by the package ruby$(RUBY_MAJOR_VERSION)
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_TARGET_IPV6_SUPPORT
 
@@ -26,8 +28,8 @@ $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	mkdir -p $(dir $@)
 	$(SUBMAKE) DESTDIR=$(abspath $(RUBY_DEST_DIR)) -C $(RUBY_DIR) install
-	rm -rf $(RUBY_DEST_DIR)/usr/{share,lib/*.a,lib/ruby/site_ruby,lib/ruby/1.8/$(GNU_TARGET_NAME)/*.{h,rb}}
-	$(TARGET_STRIP) $@ $(RUBY_DEST_DIR)/usr/lib/ruby/1.8/$(GNU_TARGET_NAME)/{,*/}*.so
+	$(RM) -r $(RUBY_DEST_DIR)/usr/{share,lib/*.a,lib/ruby/site_ruby,lib/ruby/$(RUBY_MAJOR_VERSION)/$(GNU_TARGET_NAME)/*.{h,rb}}
+	$(TARGET_STRIP) $@ $(RUBY_DEST_DIR)/usr/lib/ruby/$(RUBY_MAJOR_VERSION)/$(GNU_TARGET_NAME)/{,*/}*.so
 
 $(pkg):
 
