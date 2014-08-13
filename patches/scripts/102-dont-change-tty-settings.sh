5991171a182fd8f2a29c99d15e4680113b1e16c6
Gene Rudoy <gene@freetz.org>
2014-08-13 20:04:35 +0000

    * rename all *_06_XX symbols to *_06_0X (a preparation step for the integration of 06.20 firmware(s))
    git-svn-id: http://svn.freetz.org/trunk@12307 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/102-dont-change-tty-settings.sh
+++ patches/scripts/102-dont-change-tty-settings.sh
@@ -3,6 +3,6 @@
 # don't try to set/change tty settings as it won't work
 #
 
-if [ "$FREETZ_AVM_VERSION_06_XX_MIN" == "y" ]; then
+if [ "$FREETZ_AVM_VERSION_06_0X_MIN" == "y" ]; then
 	modsed -r 's,^([ \t]*stty .*),## disabled by freetz: \1,' "${FILESYSTEM_MOD_DIR}/etc/init.d/S01-head"
 fi
