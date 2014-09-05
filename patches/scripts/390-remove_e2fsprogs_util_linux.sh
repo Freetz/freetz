51a5e1d7af67a0d68ecac36e53746b0c20c59166
Gene Rudoy <gene@freetz.org>
2014-09-05 23:53:42 +0000

    * combine remove_e2fsprogs- & remove_lsblk-patches - all AVM binaries/libraries removed by these patches come actually from the [https://www.kernel.org/pub/linux/utils/util-linux/ util-linux] package  * add some other (unused) binaries/libraries from this package: findmnt, mountpoint, libmount - saves about 250KB (uncompressed)  * refs r12365, refs #2540
    git-svn-id: http://svn.freetz.org/trunk@12439 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ patches/scripts/390-remove_e2fsprogs_util_linux.sh
@@ -0,0 +1,37 @@
+[ "$FREETZ_REMOVE_AVM_E2FSPROGS" == "y" ] || return 0
+
+# Note: all AVM binaries are actually from util-linux package
+# mkswap, swapon, swapoff are replaced with their busybox counterparts
+
+echo1 "remove AVM's e2fsprogs/util-linux binaries"
+for files in \
+  blkid \
+  e2fsck \
+  findmnt \
+  fsck* \
+  lsblk \
+  mke2fs \
+  mkfs* \
+  mkswap \
+  mountpoint \
+  swapon \
+  swapoff \
+  ; do
+	for filenpath in $(find ${FILESYSTEM_MOD_DIR}/{bin,sbin,usr/bin,usr/sbin}/ -name $files); do
+		rm_files "$filenpath"
+	done
+done
+
+echo1 "remove AVM's e2fsprogs/util-linux libraries"
+for files in \
+  libblkid* \
+  libcom_err* \
+  libe2p* \
+  libext2fs* \
+  libmount* \
+  libuuid* \
+  ; do
+	for filenpath in $(find ${FILESYSTEM_MOD_DIR}/{lib,usr/lib}/ -name $files); do
+		rm_files "$filenpath"
+	done
+done
--- patches/scripts/563-remove_lsblk.sh
+++
@@ -1,4 +0,0 @@
-[ -e "${FILESYSTEM_MOD_DIR}/bin/lsblk" ] || return 0
-
-echo1 "removing lsblk"
-rm_files "${FILESYSTEM_MOD_DIR}/bin/lsblk"
