e77d14542ad8f583ec42b719260a82f3b0dba6f1
Gene Rudoy <gene@freetz.org>
2014-09-20 09:12:22 +0000

    * fix german umlaut
    git-svn-id: http://svn.freetz.org/trunk@12467 f5190166-0702-4917-9039-51ec32eddaf5

--- make/mod/files/root/usr/lib/cgi-bin/mod/conf/30-mount.sh
+++ make/mod/files/root/usr/lib/cgi-bin/mod/conf/30-mount.sh
@@ -5,7 +5,7 @@ cgi_print_checkbox_p "stor_uselabel" "$MOD_STOR_USELABEL" \
 	"$(lang de:"Partitionsname (falls vorhanden) als Mountpoint nutzen." en:"Use partition label (if defined) as mount point.")"
 
 cgi_print_checkbox_p "stor_autorunend" "$MOD_STOR_AUTORUNEND" \
-	"$(lang de:"Automatisch autorun.sh und autoend.sh ausführen." en:"Run autorun.sh and autoend.sh automatically.")"
+	"$(lang de:"Automatisch autorun.sh und autoend.sh ausf&uuml;hren." en:"Run autorun.sh and autoend.sh automatically.")"
 
 cgi_print_checkbox_p "stor_killblocker" "$MOD_STOR_KILLBLOCKER" \
 	"$(lang de:"Alle Programme beenden die das Aush&auml;ngen verhindern." en:"Kill all programs blocking unmount.")"
