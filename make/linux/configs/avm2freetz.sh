678afa969a50e44e746c5257bad51d63a4407990
Gene Rudoy <gene@freetz.org>
2014-10-12 13:34:35 +0000

    * add a script (actually just a template) intended to be used for (semi-)automatic conversion of AVM kernel .config to the Freetz one  * not (yet) suitable for production use, WORK IN PROGRESS  * refs #2418
    git-svn-id: http://svn.freetz.org/trunk@12559 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/linux/configs/avm2freetz.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#
+# this script is intended to be used for (semi-)automatic conversion of AVM kernel .config to the Freetz one
+#
+# not (yet) suitable for production use, WORK IN PROGRESS
+#
+
+usage() {
+cat << EOF
+
+Usage: ${SELF} TODO
+EOF
+}
+
+join() {
+	(IFS="$1"; shift; echo -n "$*")
+}
+
+_if_disabled_impl() {
+	local value="$1"; shift
+	if [ "$1" == "--append" -o "$1" == "-a" ]; then
+		local append="$2"; shift 2
+	fi
+	sed -r -e 's,^# ('$(join $'|' "$@")') is not set$,\1='"${value}${append:+\n}${append}"','
+}
+
+m_if_disabled() {
+	_if_disabled_impl m "$@"
+}
+
+y_if_disabled() {
+	_if_disabled_impl y "$@"
+}
+
+fs_related() {
+	m_if_disabled -a "CONFIG_EXT2_FS_XATTR=y" CONFIG_EXT2_FS | m_if_disabled CONFIG_FS_MBCACHE \
+	| \
+	m_if_disabled -a "CONFIG_EXT3_FS_XATTR=y" CONFIG_EXT3_FS | m_if_disabled CONFIG_FS_MBCACHE CONFIG_JBD \
+	| \
+	m_if_disabled -a "CONFIG_EXT4_FS_XATTR=y" CONFIG_EXT4_FS | m_if_disabled CONFIG_FS_MBCACHE CONFIG_JBD2 CONFIG_CRC16 \
+	| \
+	m_if_disabled CONFIG_AUTOFS4_FS CONFIG_CODA_FS CONFIG_FAT_FS CONFIG_MSDOS_FS CONFIG_VFAT_FS \
+	| \
+	m_if_disabled CONFIG_CIFS \
+	| \
+	m_if_disabled CONFIG_HFS_FS CONFIG_HFSPLUS_FS
+}
+
+SELF=$(basename "$0")
+
+fs_related
