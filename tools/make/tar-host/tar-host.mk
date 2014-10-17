a345d8fdbee6aa59f8a8a54cb3a7e69cd1d08fe2
Gene Rudoy <gene@freetz.org>
2014-10-17 21:29:22 +0000

    tar:  * rename to tar-host  * bump version to 1.28
    git-svn-id: http://svn.freetz.org/trunk@12586 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ tools/make/tar-host/tar-host.mk
@@ -0,0 +1,44 @@
+TAR_HOST_VERSION:=1.28
+TAR_HOST_SOURCE:=tar-$(TAR_HOST_VERSION).tar.bz2
+TAR_HOST_SOURCE_MD5:=8f32b2bc1ed7ddf4cf4e4a39711341b0
+TAR_HOST_SITE:=@GNU/tar
+TAR_HOST_DIR:=$(TOOLS_SOURCE_DIR)/tar-$(TAR_HOST_VERSION)
+
+$(DL_DIR)/$(TAR_HOST_SOURCE): | $(DL_DIR)
+	$(DL_TOOL) $(DL_DIR) $(TAR_HOST_SOURCE) $(TAR_HOST_SITE) $(TAR_HOST_SOURCE_MD5)
+
+$(TAR_HOST_DIR)/.unpacked: $(DL_DIR)/$(TAR_HOST_SOURCE) | $(TOOLS_SOURCE_DIR)
+	tar -C $(TOOLS_SOURCE_DIR) $(VERBOSE) -xf $(DL_DIR)/$(TAR_HOST_SOURCE)
+	touch $@
+
+$(TAR_HOST_DIR)/.configured: $(TAR_HOST_DIR)/.unpacked
+	(cd $(TAR_HOST_DIR); rm -rf config.cache; \
+		CFLAGS="-Wall -O2" \
+		CC="$(TOOLS_CC)" \
+		./configure \
+		--prefix=/usr \
+		--without-selinux \
+		$(DISABLE_NLS) \
+	);
+	touch $@
+
+$(TAR_HOST_DIR)/src/tar: $(TAR_HOST_DIR)/.configured
+	$(MAKE) -C $(TAR_HOST_DIR) all
+	touch -c $@
+
+$(TOOLS_DIR)/tar-gnu: $(TAR_HOST_DIR)/src/tar
+	$(INSTALL_FILE)
+	strip $@
+
+tar-host: $(TOOLS_DIR)/tar-gnu
+
+tar-host-source: $(TAR_HOST_DIR)/.unpacked
+
+tar-host-clean:
+	-$(MAKE) -C $(TAR_HOST_DIR) clean
+
+tar-host-dirclean:
+	$(RM) -r $(TAR_HOST_DIR)
+
+tar-host-distclean: tar-host-dirclean
+	$(RM) $(TOOLS_DIR)/tar-gnu
