0e403bbecfe48ae8fd6c8fac67794b28466194af
Gene Rudoy <gene@freetz.org>
2014-10-18 15:40:43 +0000

    tichksum makefile:  * {{{sed -e 's,TI_CHKSUM,TICHKSUM,g'}}}
    git-svn-id: http://svn.freetz.org/trunk@12605 f5190166-0702-4917-9039-51ec32eddaf5

--- tools/make/tichksum/tichksum.mk
+++ tools/make/tichksum/tichksum.mk
@@ -1,26 +1,26 @@
-TI_CHKSUM_SRC:=$(TOOLS_DIR)/make/tichksum/src
-TI_CHKSUM_DIR:=$(TOOLS_SOURCE_DIR)/tichksum
+TICHKSUM_SRC:=$(TOOLS_DIR)/make/tichksum/src
+TICHKSUM_DIR:=$(TOOLS_SOURCE_DIR)/tichksum
 
-tichksum-unpacked: $(TI_CHKSUM_DIR)/.unpacked
-$(TI_CHKSUM_DIR)/.unpacked: $(wildcard $(TI_CHKSUM_SRC)/*) | $(TOOLS_SOURCE_DIR) tar-host
-	$(RM) -r $(TI_CHKSUM_DIR)
-	mkdir -p $(TI_CHKSUM_DIR)
-	$(TAR) -C $(TI_CHKSUM_SRC) -c . | $(TAR) --exclude=.svn -C $(TI_CHKSUM_DIR) -x $(VERBOSE)
+tichksum-unpacked: $(TICHKSUM_DIR)/.unpacked
+$(TICHKSUM_DIR)/.unpacked: $(wildcard $(TICHKSUM_SRC)/*) | $(TOOLS_SOURCE_DIR) tar-host
+	$(RM) -r $(TICHKSUM_DIR)
+	mkdir -p $(TICHKSUM_DIR)
+	$(TAR) -C $(TICHKSUM_SRC) -c . | $(TAR) --exclude=.svn -C $(TICHKSUM_DIR) -x $(VERBOSE)
 	touch $@
 
-$(TI_CHKSUM_DIR)/tichksum: $(TI_CHKSUM_DIR)/.unpacked
-	$(MAKE) CC="$(TOOLS_CC)" -C $(TI_CHKSUM_DIR)
+$(TICHKSUM_DIR)/tichksum: $(TICHKSUM_DIR)/.unpacked
+	$(MAKE) CC="$(TOOLS_CC)" -C $(TICHKSUM_DIR)
 
-$(TOOLS_DIR)/tichksum: $(TI_CHKSUM_DIR)/tichksum
+$(TOOLS_DIR)/tichksum: $(TICHKSUM_DIR)/tichksum
 	$(INSTALL_FILE)
 
 tichksum: $(TOOLS_DIR)/tichksum
 
 tichksum-clean:
-	-$(MAKE) -C $(TI_CHKSUM_DIR) clean
+	-$(MAKE) -C $(TICHKSUM_DIR) clean
 
 tichksum-dirclean:
-	$(RM) -r $(TI_CHKSUM_DIR)
+	$(RM) -r $(TICHKSUM_DIR)
 
 tichksum-distclean: tichksum-dirclean
 	$(RM) $(TOOLS_DIR)/tichksum
