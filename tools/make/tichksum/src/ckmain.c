c2caaeeb3c320040c6196845767435d63b55563c
Ralf Friedl <Ralf.Friedl@online.de>
2014-10-18 10:03:29 +0000

    tichksum: replace with upward compatible rewrite  * works on little endian and big endian  * works on 32 bit and 64 bit  * can add, verify and remove a checksum  * default action is compatible with original tichksum refs #1682
    git-svn-id: http://svn.freetz.org/trunk@12596 f5190166-0702-4917-9039-51ec32eddaf5

--- tools/make/tichksum/src/ckmain.c
+++ tools/make/tichksum/src/ckmain.c
@@ -1,87 +1,104 @@
-/*
- * Copyright (c) 2002 
- *      Texas Instruments.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Texas Instruments. 
- * 4. Neither the name of the Company nor of the product may be used
- *    to endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-
-/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- * File: cfgmgr_ckmain.c
- *
- * Created: Wed Aug 14 18:24:53 2002
- *
- * $Id: ckmain.c,v 1.1.1.1 2003/11/07 18:51:55 weix Exp $
- */
 
 #include "cksum.h"
+#include <stdio.h>
+#include <fcntl.h>
+#ifndef O_BINARY
+#define O_BINARY 0
+#endif
 
-int main(int argc, char **argv)
+int
+main (int argc, char **argv)
 {
-  FILE *fp;
-  unsigned int sum = 0;
-  unsigned int res = 0;
+  int fd;
+  uint32_t sum;
+  uint32_t res;
+  int fn_ind = 1;
+  int status;
+  enum action {
+    ACT_DEFAULT,
+    ACT_ADD,
+    ACT_VERIFY,
+    ACT_REMOVE,
+    ACT_ERR,
+  } act = ACT_DEFAULT;
+  char const *fn;
 
-  if(argc != 2)
-  {
-    printf("Usage: cfgmgr_cksum filename\n\n");
+  if (argc > 1 && argv[1][0] == '-') {
+    switch (argv[1][1]) {
+    case 'a':
+      act = ACT_ADD;
+      break;
+    case 'v':
+      act = ACT_VERIFY;
+      break;
+    case 'r':
+      act = ACT_REMOVE;
+      break;
+    case '-':
+      break;
+    default:
+      act = ACT_ERR;
+      break;
+    }
+    fn_ind++;
+  }
+  if (argc <= fn_ind || act == ACT_ERR) {
+    printf ("Usage: %s [-a|-v|-r|--] filename\n", argv[0]);
+    return 1;
+  }
+  fn = argv[fn_ind];
+
+  fd = open (fn, act == ACT_VERIFY ? O_RDONLY|O_BINARY : O_RDWR|O_BINARY);
+  if (fd < 0) {
+    perror (fn);
     return 1;
   }
 
-  fp = fopen(argv[1], "rw+");
-
-  if(!cs_is_tagged(fp))
-  {
-    printf("File doesn't contain the checksum, adding\n");
-    if(cs_calc_sum(fp, &sum, 0))
-    {
-      printf("Calculated checksum is %X\n", sum);
-      if(cs_set_sum(fp, sum, 0))
-        printf("Added successfully\n");
-      else
-        printf("Adding failed\n");
+  if (act == ACT_DEFAULT) {
+    if (!cs_is_tagged (fd, NULL, NULL)) {
+      printf ("File doesn't contain the checksum, adding\n");
+      act = ACT_ADD;
     }
-  }
-  else
-  {
-    printf("File already contains the checksum, verifying\n");
-    if(cs_calc_sum(fp, &sum, 1))
-    {
-      printf("Calculated checksum is %X\n", sum);
-      cs_get_sum(fp, &res);
-      printf("Saved checksum is %X\n", res);
-      if(sum != res)
-        printf("Checksum validation failed!\n");
-      else
-        printf("Checksum validation successful!\n");
+    else {
+      printf ("File already contains the checksum, verifying\n");
+      act = ACT_VERIFY;
     }
   }
 
-  fclose(fp);
+  switch (act) {
+  case ACT_ADD:
+    status = cs_add_sum (fd, &sum);
+    if (status < 0)
+      printf ("Adding failed\n");
+    else {
+      printf ("Calculated checksum is %08X\n", sum);
+      printf ("Added successfully\n");
+    }
+    break;
+  case ACT_VERIFY:
+    status = cs_verify_sum (fd, &sum, &res);
+    if (status >= 0) {
+      printf ("Calculated checksum is %08X\n", sum);
+      printf ("Saved checksum is %X\n", res);
+    }
+    if (status > 0)
+      printf ("Checksum validation successful\n");
+    else
+      printf ("Checksum validation failed\n");
+    break;
+  case ACT_REMOVE:
+    status = cs_remove_sum (fd, NULL);
+    if (status < 0)
+      printf ("Checksum remove failed\n");
+    else
+      printf ("Checksum remove successful\n");
+    break;
+    
+  default:
+    status = 3;
+    break;
+  }
+  close (fd);
 
-  return 0;
+  return status;
 }
--- tools/make/tichksum/src/cksum.h
+++ tools/make/tichksum/src/cksum.h
@@ -1,56 +1,14 @@
-/*
- * Copyright (c) 2002 
- *      Texas Instruments.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Texas Instruments. 
- * 4. Neither the name of the Company nor of the product may be used
- *    to endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
+#ifndef CKSUM_H
+#define CKSUM_H
 
-
-/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- * File: cfgmgr_cksum.h
- *
- * Created: Wed Aug 14 18:25:30 2002
- *
- * $Id: cksum.h,v 1.1.1.1 2003/11/07 18:51:55 weix Exp $
- */
-
-#ifndef CFGMGR_CKSUM_H
-#define CFGMGR_CKSUM_H
-
-#include <inttypes.h>
-#include <stdio.h>
+#include <stdint.h>
 #include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
 
-int cs_is_tagged(FILE*);
-unsigned int cs_read_sum(FILE*);
-int cs_calc_sum(FILE*, unsigned int*, int);
-int cs_set_sum(FILE*, unsigned int, int);
-void cs_get_sum(FILE*, unsigned int*);
-unsigned int cs_calc_buf_sum(char*, unsigned long);
+int cs_is_tagged (int fd, uint32_t *sum, off_t *length);
+int cs_add_sum (int fd, uint32_t *sum);
+int cs_verify_sum (int fd, uint32_t *sum, uint32_t *res);
+int cs_remove_sum (int fd, uint32_t *res);
 
 #endif
