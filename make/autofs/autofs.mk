e30a94c348192fdf9a07abd8fb60da3a88eb7f96
Gene Rudoy <gene@freetz.org>
2014-10-21 21:06:38 +0000

    autofs makefile:  * various cleanups & fixes
    git-svn-id: http://svn.freetz.org/trunk@12628 f5190166-0702-4917-9039-51ec32eddaf5

--- make/autofs/autofs.mk
+++ make/autofs/autofs.mk
@@ -3,10 +3,14 @@ $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5:=0fe158444818f64da731520e31dc09bd
 $(PKG)_SITE:=@KERNEL/linux/daemons/$(pkg)/v5
 
+$(PKG)_BUILD_PREREQ += bison flex
+$(PKG)_STARTLEVEL=50
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/daemon/automount
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/automount
 
-$(PKG)_LIBRARIES_ALL := lookup_file.so \
+$(PKG)_MODULES := \
+	lookup_file.so \
 	lookup_hosts.so \
 	lookup_multi.so \
 	lookup_program.so \
@@ -18,41 +22,35 @@ $(PKG)_LIBRARIES_ALL := lookup_file.so \
 	mount_generic.so \
 	mount_nfs.so \
 	parse_sun.so
-
-$(PKG)_LIBRARIES := $($(PKG)_LIBRARIES_ALL)
-$(PKG)_LIBS_BUILD_DIR := $($(PKG)_LIBRARIES:%=$($(PKG)_DIR)/modules/%)
-$(PKG)_LIBS_TARGET_DIR := $($(PKG)_LIBRARIES:%=$($(PKG)_DEST_LIBDIR)/%)
-
-
-$(PKG)_BUILD_PREREQ += bison flex
-$(PKG)_STARTLEVEL=50
+$(PKG)_MODULES_BUILD_DIR := $($(PKG)_MODULES:%=$($(PKG)_DIR)/modules/%)
+$(PKG)_MODULES_TARGET_DIR := $($(PKG)_MODULES:%=$($(PKG)_DEST_LIBDIR)/%)
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_NOP)
 
-$($(PKG)_LIBS_BUILD_DIR) $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE1) daemon -C $(AUTOFS_DIR) \
+$($(PKG)_BINARY) $($(PKG)_MODULES_BUILD_DIR): $($(PKG)_DIR)/.configured
+	$(SUBMAKE1) -C $(AUTOFS_DIR) \
 		FREETZ=1 \
 		CC="$(TARGET_CC)" \
-		AUTOFS_CFLAGS="$(TARGET_CFLAGS)"
+		AUTOFS_CFLAGS="$(TARGET_CFLAGS)" \
+		daemon
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
 
-$($(PKG)_LIBS_TARGET_DIR): $($(PKG)_DEST_LIBDIR)/%: $($(PKG)_DIR)/modules/%
+$($(PKG)_MODULES_TARGET_DIR): $($(PKG)_DEST_LIBDIR)/%: $($(PKG)_DIR)/modules/%
 	$(INSTALL_BINARY_STRIP)
 
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_LIBS_TARGET_DIR) $($(PKG)_TARGET_BINARY)
-
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_MODULES_TARGET_DIR)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(AUTOFS_DIR) clean
 	$(RM) $(AUTOFS_DIR)/.configured
 
 $(pkg)-uninstall:
-	$(RM) $(AUTOFS_TARGET_BINARY)
+	$(RM) $(AUTOFS_TARGET_BINARY) $(AUTOFS_MODULES_TARGET_DIR)
 
 $(PKG_FINISH)
