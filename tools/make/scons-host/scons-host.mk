001ee36264deeeaf1f63ff669806da1fd07d5535
Gene Rudoy <gene@freetz.org>
2014-11-09 14:38:06 +0000

    * add new host package: [http://www.scons.org/ SCons]  * refs #2301
    git-svn-id: http://svn.freetz.org/trunk@12711 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ tools/make/scons-host/scons-host.mk
@@ -0,0 +1,38 @@
+SCONS_HOST_VERSION:=2.3.4
+SCONS_HOST_SOURCE:=scons-$(SCONS_HOST_VERSION).tar.gz
+SCONS_HOST_SOURCE_MD5:=91fbbb67c2c65b03c746601baac4a6a5
+SCONS_HOST_SITE:=@SF/scons
+
+SCONS_HOST_MAKE_DIR:=$(TOOLS_DIR)/make/scons
+SCONS_HOST_DIR:=$(TOOLS_SOURCE_DIR)/scons-$(SCONS_HOST_VERSION)
+
+SCONS_HOST:=$(HOST_TOOLS_DIR)/usr/bin/scons
+
+scons-host-source: $(DL_DIR)/$(SCONS_HOST_SOURCE)
+$(DL_DIR)/$(SCONS_HOST_SOURCE): | $(DL_DIR)
+	$(DL_TOOL) $(DL_DIR) $(SCONS_HOST_SOURCE) $(SCONS_HOST_SITE) $(SCONS_HOST_SOURCE_MD5)
+
+scons-host-unpacked: $(SCONS_HOST_DIR)/.unpacked
+$(SCONS_HOST_DIR)/.unpacked: $(DL_DIR)/$(SCONS_HOST_SOURCE) | $(TOOLS_SOURCE_DIR) $(UNPACK_TARBALL_PREREQUISITES)
+	$(call UNPACK_TARBALL,$(DL_DIR)/$(SCONS_HOST_SOURCE),$(TOOLS_SOURCE_DIR))
+	$(call APPLY_PATCHES,$(SCONS_HOST_MAKE_DIR)/patches,$(SCONS_HOST_DIR))
+	touch $@
+
+$(SCONS_HOST): $(SCONS_HOST_DIR)/.unpacked | python-host
+	$(abspath $(HOST_TOOLS_DIR)/usr/bin/python) \
+		$(SCONS_HOST_DIR)/setup.py install \
+		--prefix=$(abspath $(HOST_TOOLS_DIR)/usr) \
+		--symlink-scons \
+		--no-install-man
+	find $(dir $@) -maxdepth 1 -type f -name "scons*" -exec $(SED) -i -r -e 's,^#![ ]*/usr/bin/env[ ]*python,#!$(abspath $(HOST_TOOLS_DIR)/usr/bin/python),g' \{\} \+
+
+scons-host: $(SCONS_HOST)
+
+scons-host-clean:
+	$(RM) -r $(SCONS_HOST_DIR)/build
+
+scons-host-dirclean:
+	$(RM) -r $(SCONS_HOST_DIR)
+
+scons-host-distclean: scons-host-dirclean
+	$(RM) -r $(HOST_TOOLS_DIR)/usr/{bin,lib}/scons*
