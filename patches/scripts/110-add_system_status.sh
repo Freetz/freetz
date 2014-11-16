7a8c40b2ee91798a10fba23e38fff579fbd3c441
Gene Rudoy <gene@freetz.org>
2014-11-16 23:50:03 +0000

    patches:  * remove some unnecessary numeric prefixes from patches filenames
    git-svn-id: http://svn.freetz.org/trunk@12745 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/110-add_system_status.sh
+++ patches/scripts/110-add_system_status.sh
@@ -1,4 +1,4 @@
 [ "$FREETZ_MODIFY_AVM_VERSION" == "y" ] || return 0
 
 echo1 "adding Freetz version to AVM version data"
-modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_COND_DIR}/110-system_status.patch"
+modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_COND_DIR}/system_status.patch"
--- patches/scripts/480-add_3rd_alarmclock.sh
+++ patches/scripts/480-add_3rd_alarmclock.sh
@@ -1,4 +1,4 @@
 [ "$FREETZ_PATCH_ALARMCLOCK" == "y" ] || return 0
 echo1 "adding alarm-clock"
 
-modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_COND_DIR}/${FREETZ_TYPE_LANGUAGE}/480-alarmclock_${FREETZ_TYPE_PREFIX}.patch"
+modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_COND_DIR}/${FREETZ_TYPE_LANGUAGE}/alarmclock_${FREETZ_TYPE_PREFIX}.patch"
