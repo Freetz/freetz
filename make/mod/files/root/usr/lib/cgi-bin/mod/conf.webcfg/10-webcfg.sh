28ebc061cb914189f692536aae4cfe376eea262e
Whoopie <whoopie79@gmx.net>
2014-12-24 14:26:02 +0000

    webgui: fix popup message (refs #2559)
    git-svn-id: http://svn.freetz.org/trunk@12827 f5190166-0702-4917-9039-51ec32eddaf5

--- make/mod/files/root/usr/lib/cgi-bin/mod/conf.webcfg/10-webcfg.sh
+++ make/mod/files/root/usr/lib/cgi-bin/mod/conf.webcfg/10-webcfg.sh
@@ -21,7 +21,7 @@ cgi_print_textline "httpd_sessiontimeout" "$MOD_HTTPD_SESSIONTIMEOUT" 5 "$(lang 
 
 cat << EOF
 <script >
-var conftext='$(lang de:"Noch in der Erprobung!\nZur Sicherheit sollte ein anderer Zugang zur Box vorhanden sein (telnet/ssh)\nStandard PW ist "freetz"!\n" en:"Still under development!\nPlease make sure, you have an alternate way to access your box (telnet/ssh).\nDefault password is "freetz"!\n")'
+var conftext='$(lang de:"Noch in der Erprobung!\\nZur Sicherheit sollte ein anderer Zugang zur Box vorhanden sein (telnet/ssh).\\nStandard PW ist \"freetz\"!\\n" en:"Still under development!\\nPlease make sure, you have an alternate way to access your box (telnet/ssh).\\nDefault password is \"freetz\"!\\n")'
 var el=document.getElementById('httpd_newlogin_yes');
 var eltime=document.getElementById('httpd_sessiontimeout');
 $([ "$MOD_HTTPD_NEWLOGIN" = yes ] || echo "eltime.disabled = true;")
