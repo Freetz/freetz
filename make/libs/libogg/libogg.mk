f2cd3eaee448db1f40c507c953db3667b148c8d3
Gene Rudoy <gene@freetz.org>
2015-01-20 18:25:02 +0000

    libogg:  * bump version to 1.3.2
    git-svn-id: http://svn.freetz.org/trunk@12879 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/libogg/libogg.mk
+++ make/libs/libogg/libogg.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_LIB, 1.3.1)
-$(PKG)_LIB_VERSION:=0.8.1
+$(call PKG_INIT_LIB, 1.3.2)
+$(PKG)_LIB_VERSION:=0.8.2
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
-$(PKG)_SOURCE_MD5:=ca25d8da0ddfc8c6cbbf78d847a209fe
+$(PKG)_SOURCE_MD5:=5c3a34309d8b98640827e5d0991a4015
 $(PKG)_SITE:=http://downloads.xiph.org/releases/ogg
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/.libs/$(pkg).so.$($(PKG)_LIB_VERSION)
