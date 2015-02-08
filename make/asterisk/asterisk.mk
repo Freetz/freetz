105080c58f9b807a5bf311b113ab277858994d50
Gene Rudoy <gene@freetz.org>
2015-02-08 09:32:12 +0000

    asterisk:  * bump version to [http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-11.16.0-summary.html 11.16.0]  * refs #706
    git-svn-id: http://svn.freetz.org/trunk@12916 f5190166-0702-4917-9039-51ec32eddaf5

--- make/asterisk/asterisk.mk
+++ make/asterisk/asterisk.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 11.15.0)
+$(call PKG_INIT_BIN, 11.16.0)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_SHA1:=95d4bf6ceda14dd9a3e9fffe02be866a07ff5a1c
+$(PKG)_SOURCE_SHA1:=9130e49a071ce496a0dc73afb6f880a567c37d58
 $(PKG)_SITE:=http://downloads.asterisk.org/pub/telephony/asterisk/releases
 
 $(PKG)_CATEGORY:=Unstable
