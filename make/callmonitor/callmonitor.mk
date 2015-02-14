cf7e29d77def22a0588827f5981a4ef229e3cff0
Gene Rudoy <gene@freetz.org>
2015-02-14 13:01:03 +0000

    callmonitor:  * switch to using the latest git revision - this simplifies a lot the integration of the fixes
    git-svn-id: http://svn.freetz.org/trunk@12929 f5190166-0702-4917-9039-51ec32eddaf5

--- make/callmonitor/callmonitor.mk
+++ make/callmonitor/callmonitor.mk
@@ -1,10 +1,13 @@
-$(call PKG_INIT_BIN, 1.20.9)
-$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION)-freetz.tar.bz2
-$(PKG)_SOURCE_MD5:=787b61b2b1e8795686149f2431800884
-$(PKG)_SITE:=@SF/callmonitor
+$(call PKG_INIT_BIN, ae5b0b1420)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
+$(PKG)_SITE:=git@https://github.com/er13/callmonitor.git
+
 $(PKG)_STARTLEVEL=71
-$(PKG)_BINARY:=$($(PKG)_DIR)/src/recode
-$(PKG)_TARGET_BINARY:=$($(PKG)_DIR)/root/usr/lib/callmonitor/bin/recode
+
+$(PKG)_BUILD_DIR := $($(PKG)_DIR)/build/freetz/$(pkg)-$($(PKG)_VERSION)
+
+$(PKG)_BINARY:=$($(PKG)_BUILD_DIR)/src/recode
+$(PKG)_TARGET_BINARY:=$($(PKG)_BUILD_DIR)/root/usr/lib/callmonitor/bin/recode
 
 $(PKG)_SUBOPTS := webif actions monitor phonebook
 $(PKG)_REBUILD_SUBOPTS += $(foreach subopt,$($(PKG)_SUBOPTS),FREETZ_PACKAGE_CALLMONITOR_$(subopt))
@@ -14,12 +17,16 @@ $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 
 $($(PKG)_DIR)/.configured: $($(PKG)_DIR)/.unpacked
-	@echo $(CALLMONITOR_FEATURES) > $(CALLMONITOR_DIR)/.features
-	$(SUBMAKE) -C $(CALLMONITOR_DIR) configure
+	$(SUBMAKE) -C $(CALLMONITOR_DIR) \
+		BUSYBOX=$(abspath $(TOOLS_DIR)/busybox) \
+		VERSION=$(CALLMONITOR_VERSION) \
+		collect
+	@echo $(CALLMONITOR_FEATURES) > $(CALLMONITOR_BUILD_DIR)/.features
+	$(SUBMAKE) -C $(CALLMONITOR_BUILD_DIR) configure
 	@touch $@
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) $(TARGET_CONFIGURE_ENV) -C $(CALLMONITOR_DIR) build
+	$(SUBMAKE) $(TARGET_CONFIGURE_ENV) -C $(CALLMONITOR_BUILD_DIR) build
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
@@ -33,8 +40,8 @@ $(pkg):
 $(pkg)-precompiled: $($(PKG)_TARGET_DIR)/.packaged
 
 $($(PKG)_TARGET_DIR)/.packaged: $($(PKG)_DIR)/.configured $($(PKG)_TARGET_BINARY)
-	$(call COPY_USING_TAR,$(CALLMONITOR_DIR)/root,$(CALLMONITOR_DEST_DIR),--files-from=$(CALLMONITOR_DIR)/.files)
-	cp $(CALLMONITOR_DIR)/.language $(CALLMONITOR_TARGET_DIR)/
+	$(call COPY_USING_TAR,$(CALLMONITOR_BUILD_DIR)/root,$(CALLMONITOR_DEST_DIR),--files-from=$(CALLMONITOR_BUILD_DIR)/.files)
+	cp $(CALLMONITOR_BUILD_DIR)/.language $(CALLMONITOR_TARGET_DIR)/
 	@touch $@
 
 $(PKG_FINISH)
