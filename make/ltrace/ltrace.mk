a210edf5f3b6ded686fe8f0fb91286d2c9ce8c2e
Gene Rudoy <gene@freetz.org>
2015-02-20 23:40:05 +0000

    ltrace:  * bump version to [http://anonscm.debian.org/cgit/collab-maint/ltrace.git/commit/?id=c18a46eff706e956c4dc755144d752dc163a1145 master@c18a46eff7] with the following two additionally applied patches from [http://anonscm.debian.org/cgit/collab-maint/ltrace.git/log/?h=pmachata/mips pmachata/mips branch]: [http://anonscm.debian.org/cgit/collab-maint/ltrace.git/commit/?h=pmachata/mips&id=cf55336f3d220bf9bb8b879eb7fba9d774c19957 cf55336f3d], [http://anonscm.debian.org/cgit/collab-maint/ltrace.git/commit/?h=pmachata/mips&id=a1a5182053d61de1223719961f067898586f7ad1 a1a5182053]  * the new version still has some known bugs (e.g. {{{ltrace /usr/bin/wget-gnu --no-check-certificate https://www.kernel.org/pub/software/scm/git/git-2.3.0.tar.xz}}} fails with {{{snprintf(ltrace: value.c: 343:value_set_word: Assertion `sz <= sizeof(value->u.value)' failed}}}) but it's definitely better than the version we had so far (doesn't work for PLT-less binaries at all)  * refs #2326
    git-svn-id: http://svn.freetz.org/trunk@12957 f5190166-0702-4917-9039-51ec32eddaf5

--- make/ltrace/ltrace.mk
+++ make/ltrace/ltrace.mk
@@ -1,50 +1,52 @@
-$(call PKG_INIT_BIN, 0.5.3)
-$(PKG)_SOURCE:=ltrace_$($(PKG)_VERSION).orig.tar.gz
-$(PKG)_SOURCE_MD5:=3fa7fe715ab879db08bd06d1d59fd90f
-$(PKG)_SITE:=http://ftp.debian.org/debian/pool/main/l/ltrace
+$(call PKG_INIT_BIN, c18a46eff7)
+$(PKG)_SOURCE:=ltrace-$($(PKG)_VERSION).tar.xz
+$(PKG)_SITE:=git://git.debian.org/git/collab-maint/ltrace.git
 
 $(PKG)_CATEGORY:=Debug helpers
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/ltrace
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/ltrace
 
-$(PKG)_CONF:=$($(PKG)_DIR)/etc/ltrace.conf
-$(PKG)_TARGET_CONF:=$($(PKG)_DEST_DIR)/etc/ltrace.conf
+$(PKG)_CONFIGS            := libacl.so.conf libc.so.conf libm.so.conf syscalls.conf
+$(PKG)_CONFIGS_BUILD_DIR  := $($(PKG)_CONFIGS:%=$($(PKG)_DIR)/etc/%)
+$(PKG)_CONFIGS_TARGET_DIR := $($(PKG)_CONFIGS:%=$($(PKG)_DEST_DIR)/usr/share/ltrace/%)
 
 $(PKG)_DEPENDS_ON += libelf
 
-$(PKG)_CONFIGURE_PRE_CMDS += ln -sf ./mipsel sysdeps/linux-gnu/mips;
-$(PKG)_CONFIGURE_PRE_CMDS += ( cd sysdeps/linux-gnu/mips; chmod +x ../mksyscallent_mips; \
-	../mksyscallent_mips $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/asm/unistd.h > syscallent.h; \
-	../mksignalent $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/asm/signal.h > signalent.h; );
+$(PKG)_CONFIGURE_PRE_CMDS += ./autogen.sh;
 
-$(PKG)_CONFIGURE_ENV += LD="$(TARGET_LD)"
+# disable demangling support
+$(PKG)_CONFIGURE_ENV += ac_cv_lib_iberty_cplus_demangle=no
+$(PKG)_CONFIGURE_ENV += ac_cv_lib_stdcpp___cxa_demangle=no
+$(PKG)_CONFIGURE_ENV += ac_cv_lib_supcpp___cxa_demangle=no
+
+$(PKG)_CONFIGURE_OPTIONS += --with-libelf="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
+$(PKG)_CONFIGURE_OPTIONS += --with-libunwind=no
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
-$($(PKG)_CONF): $($(PKG)_DIR)/.unpacked
-
-$($(PKG)_TARGET_CONF): $($(PKG)_CONF)
-	$(INSTALL_FILE)
-
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(LTRACE_DIR) \
-		ARCH=mips \
-		CFLAGS="$(TARGET_CFLAGS)"
+	$(SUBMAKE) -C $(LTRACE_DIR)
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
 
+$($(PKG)_CONFIGS_BUILD_DIR): $($(PKG)_DIR)/etc/%: $($(PKG)_DIR)/.unpacked
+	@touch $@
+
+$($(PKG)_CONFIGS_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/share/ltrace/%: $($(PKG)_DIR)/etc/%
+	$(INSTALL_FILE)
+
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_TARGET_CONF)
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_CONFIGS_TARGET_DIR)
 
 $(pkg)-clean:
-	-$(SUBMAKE) -C $(LTRACE_DIR) clean ARCH=mips
+	-$(SUBMAKE) -C $(LTRACE_DIR) clean
 
 $(pkg)-uninstall:
-	$(RM) $(LTRACE_TARGET_BINARY) $(LTRACE_TARGET_CONF)
+	$(RM) $(LTRACE_TARGET_BINARY) $(LTRACE_CONFIGS_TARGET_DIR)
 
 $(PKG_FINISH)
