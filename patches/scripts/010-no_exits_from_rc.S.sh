0b651011fa68dbfd5970073df8a25fb021c63463
Gene Rudoy <gene@freetz.org>
2015-03-04 21:31:20 +0000

    * fix minor AVM code quirk - there is a (probably dead) code branch that might lead to an exit from {{{rc.S}}}
    git-svn-id: http://svn.freetz.org/trunk@12987 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/010-no_exits_from_rc.S.sh
+++ patches/scripts/010-no_exits_from_rc.S.sh
@@ -13,3 +13,8 @@ modsed -r '
 	$ !b collect
 	s/([ \t]*(exit[ \t]+0)?[ \t\n]*)*$//
 ' "$rcfile"
+
+# remove 'exit' from files sourced from rc.S
+if [ "$FREETZ_AVM_VERSION_06_2X_MIN" == "y" ]; then
+	[ -e "${FILESYSTEM_MOD_DIR}/etc/init.d/S78-aha" ] && modpatch "${FILESYSTEM_MOD_DIR}" "${PATCHES_COND_DIR}/exit_in_S78-aha_sourced_from_rc.S-6.2x.patch"
+fi
