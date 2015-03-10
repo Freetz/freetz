c667a4479a3f9b39188d0bf2ca2dbfa6cfa2fe2f
Whoopie <whoopie79@gmx.net>
2015-03-10 13:18:36 +0000

    sslh: bump version to 1.17
    git-svn-id: http://svn.freetz.org/trunk@12999 f5190166-0702-4917-9039-51ec32eddaf5

--- make/sslh/sslh.mk
+++ make/sslh/sslh.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN,1.16)
+$(call PKG_INIT_BIN,1.17)
 $(PKG)_SOURCE:=$(pkg)-v$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=c6e7d1cb0adb15f6efe480e36d98c560
+$(PKG)_SOURCE_MD5:=1fc3ada4bafaca5a9786cc1431f48ed4
 $(PKG)_SITE:=http://rutschle.net/tech
 $(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/$(pkg)-v$($(PKG)_VERSION)
 $(PKG)_BINARY:=$($(PKG)_DIR)/sslh-fork
