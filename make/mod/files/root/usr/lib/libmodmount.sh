da3e297fed7ac2b1e6e8590e0a7540b5b2cefb9f
Gene Rudoy <gene@freetz.org>
2015-03-14 15:16:36 +0000

    FreetzMount:  * remove workaround added in r12487, it's not necessary anymore - please update your configurations accordingly - the 1st character of the mount point won't be upper-cased anymore  * refs #2499, refs #2572
    git-svn-id: http://svn.freetz.org/trunk@13012 f5190166-0702-4917-9039-51ec32eddaf5

--- make/mod/files/root/usr/lib/libmodmount.sh
+++ make/mod/files/root/usr/lib/libmodmount.sh
@@ -45,13 +45,6 @@ find_mnt_name() {
 		mnt_name="${storage_prefix:0:30}${dev_idx}${part_idx}"
 	fi
 
-if [ $(get_avm_firmware_version) -ge 610 ]; then
-	# ensure the 1st character of the mount point is an upper-case one
-	# needed to workaround deficiencies of 6.20 firmware series (s. http://freetz.org/ticket/2499 for details)
-	mnt_name="$(echo ${mnt_name:0:1} | tr '[:lower:]' '[:upper:]')${mnt_name:1}"       # (try to) capitalize
-	[ -z "$(echo ${mnt_name:0:1} | tr -d -c '[:upper:]')" ] && mnt_name="U${mnt_name}" # 1st character is still not in upper-case => prefix mnt_name with "U"
-fi
-
 	echo $mnt_name
 }
 
