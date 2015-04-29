a70f4bca648270e2fd9ade4d501e4e809532b51a
Gene Rudoy <gene@freetz.org>
2015-04-29 22:38:33 +0000

    curl:  * bump version to [http://curl.haxx.se/changes.html#7_42_1 7.42.1]
    git-svn-id: http://svn.freetz.org/trunk@13088 f5190166-0702-4917-9039-51ec32eddaf5

--- make/curl/curl.mk
+++ make/curl/curl.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_BIN, 7.42.0)
+$(call PKG_INIT_BIN, 7.42.1)
 $(PKG)_LIB_VERSION:=4.3.0
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=0a10174a0ea5105c46e92b51e1b311f8
+$(PKG)_SOURCE_MD5:=296945012ce647b94083ed427c1877a8
 $(PKG)_SITE:=http://curl.haxx.se/download
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src$(if $(FREETZ_PACKAGE_CURL_STATIC),,/.libs)/curl
