2ff434170e16e7a35912b71bd761b0fc8390d334
Gene Rudoy <gene@freetz.org>
2015-04-30 20:42:11 +0000

    stunnel:  * bump version to [https://www.stunnel.org/sdf_ChangeLog.html 5.17]
    git-svn-id: http://svn.freetz.org/trunk@13090 f5190166-0702-4917-9039-51ec32eddaf5

--- make/stunnel/stunnel.mk
+++ make/stunnel/stunnel.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 5.16)
+$(call PKG_INIT_BIN, 5.17)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=ffe666e8bf1f0f9dad67b8d7e9ead48a
+$(PKG)_SOURCE_MD5:=e70f001ee190105c5a10a74f4bd54901
 $(PKG)_SITE:=http://www.stunnel.org/downloads/archive/5.x
 
 $(PKG)_STARTLEVEL=81
