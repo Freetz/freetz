74fb14451eaead621889a20032d4fa82c278543a
Gene Rudoy <gene@freetz.org>
2015-05-25 18:39:06 +0000

    "remove e2fsprogs/util-linux"-patch:  * remove more binaries from util-linux package (all unused by AVM): [http://man7.org/linux/man-pages/man8/chcpu.8.html chcpu], [http://man7.org/linux/man-pages/man8/losetup.8.html losetup], [http://man7.org/linux/man-pages/man8/wdctl.8.html wdctl]
    git-svn-id: http://svn.freetz.org/trunk@13131 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/390-remove_e2fsprogs_util_linux.sh
+++ patches/scripts/390-remove_e2fsprogs_util_linux.sh
@@ -1,21 +1,25 @@
 [ "$FREETZ_REMOVE_AVM_E2FSPROGS" == "y" ] || return 0
 
 # Note: all AVM binaries are actually from util-linux package
-# mkswap, swapon, swapoff are replaced with their busybox counterparts
+# mkswap, swapon, swapoff, renice are replaced with their busybox counterparts
 
 echo1 "remove AVM's e2fsprogs/util-linux binaries"
 for files in \
   blkid \
+  chcpu \
   e2fsck \
   findmnt \
   fsck* \
+  losetup \
   lsblk \
   mke2fs \
   mkfs* \
   mkswap \
   mountpoint \
+  renice \
   swapon \
   swapoff \
+  wdctl \
   ; do
 	for filenpath in $(find ${FILESYSTEM_MOD_DIR}/{bin,sbin,usr/bin,usr/sbin}/ -name $files); do
 		rm_files "$filenpath"
