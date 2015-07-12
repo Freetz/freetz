48fa8d0a7794f73b27eb682f4f0b4292db496af5
Gene Rudoy <gene@freetz.org>
2015-07-12 23:24:35 +0000

    squashfs4-host:  * add support for creating SquashFS4 images in AVM-BE format (HIGHLY-EXPERIMENTAL, not yet tested on the box)  * based on a patch provided by PeterPawn via eMail  * refs #2691
    Note: please call {{{make squashfs4-host-distclean}}} after updating to this revision
    
    
    git-svn-id: http://svn.freetz.org/trunk@13246 f5190166-0702-4917-9039-51ec32eddaf5

--- tools/make/squashfs4-host/squashfs4-host.mk
+++ tools/make/squashfs4-host/squashfs4-host.mk
@@ -5,11 +5,14 @@ SQUASHFS4_HOST_SITE:=@SF/squashfs
 
 SQUASHFS4_HOST_MAKE_DIR:=$(TOOLS_DIR)/make/squashfs4-host
 SQUASHFS4_HOST_DIR:=$(TOOLS_SOURCE_DIR)/squashfs$(SQUASHFS4_HOST_VERSION)
-SQUASHFS4_HOST_BUILD_DIR:=$(SQUASHFS4_HOST_DIR)/squashfs-tools
 
-SQUASHFS4_HOST_TOOLS:=unsquashfs
-SQUASHFS4_HOST_TOOLS_BUILD_DIR:=$(addprefix $(SQUASHFS4_HOST_BUILD_DIR)/,$(SQUASHFS4_HOST_TOOLS))
-SQUASHFS4_HOST_TOOLS_TARGET_DIR:=$(SQUASHFS4_HOST_TOOLS:%=$(TOOLS_DIR)/%4-lzma)
+SQUASHFS4_HOST_unsquashfs_BUILD_DIR:=$(SQUASHFS4_HOST_DIR)/squashfs-tools
+SQUASHFS4_HOST_unsquashfs:=$(SQUASHFS4_HOST_unsquashfs_BUILD_DIR)/unsquashfs
+SQUASHFS4_HOST_unsquashfs_TARGET:=$(TOOLS_DIR)/unsquashfs4-lzma
+
+SQUASHFS4_HOST_mksquashfs_BUILD_DIR:=$(SQUASHFS4_HOST_DIR)/squashfs-tools-mksquashfs-avm-be
+SQUASHFS4_HOST_mksquashfs:=$(SQUASHFS4_HOST_mksquashfs_BUILD_DIR)/mksquashfs
+SQUASHFS4_HOST_mksquashfs_TARGET:=$(TOOLS_DIR)/mksquashfs4-lzma-avm-be
 
 squashfs4-host-source: $(DL_DIR)/$(SQUASHFS4_HOST_SOURCE)
 $(DL_DIR)/$(SQUASHFS4_HOST_SOURCE): | $(DL_DIR)
@@ -18,30 +21,47 @@ $(DL_DIR)/$(SQUASHFS4_HOST_SOURCE): | $(DL_DIR)
 squashfs4-host-unpacked: $(SQUASHFS4_HOST_DIR)/.unpacked
 $(SQUASHFS4_HOST_DIR)/.unpacked: $(DL_DIR)/$(SQUASHFS4_HOST_SOURCE) | $(TOOLS_SOURCE_DIR) $(UNPACK_TARBALL_PREREQUISITES)
 	$(call UNPACK_TARBALL,$(DL_DIR)/$(SQUASHFS4_HOST_SOURCE),$(TOOLS_SOURCE_DIR))
+	# unsquashfs only build dir
 	$(call APPLY_PATCHES,$(SQUASHFS4_HOST_MAKE_DIR)/patches,$(SQUASHFS4_HOST_DIR))
+	# mksquashfs only build dir
+	(cd $(SQUASHFS4_HOST_DIR); cp -a squashfs-tools $(notdir $(SQUASHFS4_HOST_mksquashfs_BUILD_DIR)))
+	$(call APPLY_PATCHES,$(SQUASHFS4_HOST_MAKE_DIR)/patches/mksquashfs-avm-be,$(SQUASHFS4_HOST_DIR))
 	touch $@
 
-$(SQUASHFS4_HOST_TOOLS_BUILD_DIR): $(SQUASHFS4_HOST_DIR)/.unpacked $(LZMA2_DIR)/liblzma.a
-	$(MAKE) -C $(SQUASHFS4_HOST_BUILD_DIR) \
-		CC="$(TOOLS_CC)" \
-		XZ_SUPPORT=1 \
-		XZ_DIR="$(abspath $(LZMA2_DIR))" \
-		GZIP_SUPPORT=1 \
-		COMP_DEFAULT=xz \
-		$(SQUASHFS4_HOST_TOOLS) mksquashfs
+SQUASHFS4_HOST_COMMON_MAKE_OPTS += CC="$(TOOLS_CC)"
+SQUASHFS4_HOST_COMMON_MAKE_OPTS += XZ_SUPPORT=1
+SQUASHFS4_HOST_COMMON_MAKE_OPTS +=  XZ_DIR="$(abspath $(LZMA2_DIR))"
+SQUASHFS4_HOST_COMMON_MAKE_OPTS += GZIP_SUPPORT=1
+SQUASHFS4_HOST_COMMON_MAKE_OPTS += COMP_DEFAULT=xz
+
+$(SQUASHFS4_HOST_unsquashfs): $(SQUASHFS4_HOST_DIR)/.unpacked $(LZMA2_DIR)/liblzma.a
+	$(MAKE) -C $(SQUASHFS4_HOST_unsquashfs_BUILD_DIR) \
+		$(SQUASHFS4_HOST_COMMON_MAKE_OPTS) \
+		unsquashfs
 	touch -c $@
 
-$(SQUASHFS4_HOST_TOOLS_TARGET_DIR): $(TOOLS_DIR)/%4-lzma: $(SQUASHFS4_HOST_BUILD_DIR)/%
+$(SQUASHFS4_HOST_unsquashfs_TARGET): $(SQUASHFS4_HOST_unsquashfs)
 	$(INSTALL_FILE)
 	strip $@
 
-squashfs4-host: $(SQUASHFS4_HOST_TOOLS_TARGET_DIR)
+$(SQUASHFS4_HOST_mksquashfs): $(SQUASHFS4_HOST_DIR)/.unpacked $(LZMA2_DIR)/liblzma.a
+	$(MAKE) -C $(SQUASHFS4_HOST_mksquashfs_BUILD_DIR) \
+		$(SQUASHFS4_HOST_COMMON_MAKE_OPTS) \
+		mksquashfs
+	touch -c $@
+
+$(SQUASHFS4_HOST_mksquashfs_TARGET): $(SQUASHFS4_HOST_mksquashfs)
+	$(INSTALL_FILE)
+	strip $@
+
+squashfs4-host: $(SQUASHFS4_HOST_unsquashfs_TARGET) $(SQUASHFS4_HOST_mksquashfs_TARGET)
 
 squashfs4-host-clean:
-	-$(MAKE) -C $(SQUASHFS4_HOST_BUILD_DIR) clean
+	-$(MAKE) -C $(SQUASHFS4_HOST_unsquashfs_BUILD_DIR) clean
+	-$(MAKE) -C $(SQUASHFS4_HOST_mksquashfs_BUILD_DIR) clean
 
 squashfs4-host-dirclean:
 	$(RM) -r $(SQUASHFS4_HOST_DIR)
 
 squashfs4-host-distclean: squashfs4-host-dirclean
-	$(RM) $(SQUASHFS4_HOST_TOOLS_TARGET_DIR)
+	$(RM) $(SQUASHFS4_HOST_unsquashfs_TARGET) $(SQUASHFS4_HOST_mksquashfs_TARGET)
