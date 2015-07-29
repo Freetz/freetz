10dd577da568944656be29c616c9a40bd7217c80
Gene Rudoy <gene@freetz.org>
2015-07-29 22:20:14 +0000

    squashfs3:  * add support for LZMA1 compression (the binaries provided by this package support both ZLIB and LZMA1 based compression)  * refs #2730, refs #2731
    git-svn-id: http://svn.freetz.org/trunk@13286 f5190166-0702-4917-9039-51ec32eddaf5

--- make/squashfs3/squashfs3.mk
+++ make/squashfs3/squashfs3.mk
@@ -5,16 +5,13 @@ $(PKG)_SOURCE:=$($(PKG)_PRJNAME)$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5:=2a4d2995ad5aa6840c95a95ffa6b1da6
 $(PKG)_SITE:=@SF/$($(PKG)_PRJNAME)
 
-$(PKG)_DEPENDS_ON += zlib
-
-# TODO:
-# - (Re-)add support for LZMA compression, it's not used at the primary target model 7490.
+$(PKG)_DEPENDS_ON += zlib liblzma1
 
 $(PKG)_BUILD_DIR := $($(PKG)_DIR)/squashfs-tools
 
-$(PKG)_BINARIES            := mksquashfs unsquashfs
+$(PKG)_BINARIES            := mksquashfs-multi unsquashfs-multi
 $(PKG)_BINARIES_BUILD_DIR  := $($(PKG)_BINARIES:%=$($(PKG)_BUILD_DIR)/%)
-$(PKG)_BINARIES_TARGET_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DEST_DIR)/usr/bin/%3)
+$(PKG)_BINARIES_TARGET_DIR := $($(PKG)_BINARIES:%-multi=$($(PKG)_DEST_DIR)/usr/bin/%3-multi)
 
 ifneq ($(strip $(DL_DIR)/$(SQUASHFS3_SOURCE)),$(strip $(DL_DIR)/$(SQUASHFS3_HOST_SOURCE)))
 $(PKG_SOURCE_DOWNLOAD)
@@ -25,9 +22,11 @@ $(PKG_CONFIGURED_NOP)
 $($(PKG)_BINARIES_BUILD_DIR): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(SQUASHFS3_BUILD_DIR) \
 		CC="$(TARGET_CC)" \
-		CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE"
+		CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE" \
+		LZMA1_SUPPORT=1 \
+		LZMA_LIBNAME=lzma1
 
-$($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%3: $($(PKG)_BUILD_DIR)/%
+$($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%3-multi: $($(PKG)_BUILD_DIR)/%-multi
 	$(INSTALL_BINARY_STRIP)
 
 $(pkg):
