6231fed359c4c441f17b79e6fdb7468c3fded5d3
Gene Rudoy <gene@freetz.org>
2015-08-08 10:56:27 +0000

    xpdf:  * bump version to [http://www.foolabs.com/xpdf/CHANGES 3.04] (silences a lot of warnings with recent compiler versions)
    git-svn-id: http://svn.freetz.org/trunk@13339 f5190166-0702-4917-9039-51ec32eddaf5

--- make/xpdf/xpdf.mk
+++ make/xpdf/xpdf.mk
@@ -1,18 +1,20 @@
-$(call PKG_INIT_BIN, 3.02)
+$(call PKG_INIT_BIN, 3.04)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=599dc4cc65a07ee868cf92a667a913d2
+$(PKG)_SOURCE_MD5:=3bc86c69c8ff444db52461270bef3f44
 $(PKG)_SITE:=ftp://ftp.foolabs.com/pub/$(pkg)
 
 $(PKG)_BINARIES_ALL := pdftops pdftotext pdfinfo pdffonts pdfimages
 $(PKG)_BINARIES := $(call PKG_SELECTED_SUBOPTIONS,$($(PKG)_BINARIES_ALL))
 $(PKG)_BINARIES_BUILD_DIR := $(addprefix $($(PKG)_DIR)/xpdf/,$($(PKG)_BINARIES))
 $(PKG)_BINARIES_TARGET_DIR := $(addprefix $($(PKG)_DEST_DIR)/usr/bin/,$($(PKG)_BINARIES))
+
 $(PKG)_LIBS_BUILD_DIR := $($(PKG)_DIR)/xpdf/libxpdf.so.1
 $(PKG)_LIBS_TARGET_DIR := $($(PKG)_DEST_LIBDIR)/libxpdf.so.1
 $(PKG)_EXCLUDED += $(patsubst %,$($(PKG)_DEST_DIR)/usr/bin/%,$(filter-out $($(PKG)_BINARIES),$($(PKG)_BINARIES_ALL)))
 
 $(PKG)_DEPENDS_ON += $(STDCXXLIB)
 $(PKG)_REBUILD_SUBOPTS += FREETZ_STDCXXLIB
+$(PKG)_REBUILD_SUBOPTS += $($(PKG)_BINARIES_ALL:%=FREETZ_PACKAGE_XPDF_%)
 
 $(PKG)_CONFIGURE_OPTIONS += \
 	--enable-a4-paper \
@@ -26,8 +28,7 @@ $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARIES_BUILD_DIR) $($(PKG)_LIBS_BUILD_DIR): $($(PKG)_DIR)/.configured
-	$(SUBMAKE1) -C $(XPDF_DIR) $(XPDF_PROGRAMS)
-# -j 1 because my ad-hoc 'shared' patch does not specify all depencies properly
+	$(SUBMAKE1) -C $(XPDF_DIR) $(XPDF_BINARIES) LIBXPDF_BINS="$(XPDF_BINARIES)"
 
 $($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/xpdf/%
 	$(INSTALL_BINARY_STRIP)
