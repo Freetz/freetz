b3f82a45463da6bca6369a8c2b05fbb1d1e03b32
Gene Rudoy <gene@freetz.org>
2015-08-14 21:09:10 +0000

    Fritz!OS 6.35:  * adjust webmenu_signed-patch  * refs #2774
    git-svn-id: http://svn.freetz.org/trunk@13355 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/195-patch_webmenu_signed.sh
+++ patches/scripts/195-patch_webmenu_signed.sh
@@ -1,6 +1,11 @@
 [ "$FREETZ_PATCH_SIGNED" == "y" ] || return 0
 echo1 "applying webmenu signed patch"
 
+for file in /usr/www/all/home/home.js /usr/www/avme/home/home.js; do
+	[ ! -e ${FILESYSTEM_MOD_DIR}${file} ] && continue
+	modsed 's/data[.]fritzos[.]FirmwareSigned/true/g' ${FILESYSTEM_MOD_DIR}${file}
+done
+
 for file in /usr/www/all/home/home.lua /usr/www/avme/home/home.lua; do
 	[ ! -e ${FILESYSTEM_MOD_DIR}${file} ] && continue
 	modsed 's/^g_coninf_data.FirmwareSigned = .*/g_coninf_data.FirmwareSigned = "1"/g' ${FILESYSTEM_MOD_DIR}${file}
@@ -11,7 +16,7 @@ for file in /usr/www/all/html/de/home/home.js /usr/www/avme/html/de/home/home.js
 	modsed 's/^.*var signed.*/\tvar signed = 1;/g' ${FILESYSTEM_MOD_DIR}${file}
 done
 
-# patcht System > Diagnose (ab 05.59)
-sedfile="${HTML_LANG_MOD_DIR}/system/diagnosis.lua"
-echo1 "patching ${sedfile##*/}"
-modsed 's/signed_firmware") == "1"/& or true /' $sedfile
+for file in "${HTML_LANG_MOD_DIR##${FILESYSTEM_MOD_DIR}}/system/diagnosis.lua" /usr/lua/retrieve_data.lua; do
+	[ ! -e ${FILESYSTEM_MOD_DIR}${file} ] && continue
+	modsed 's,box[.]query("[^"]*/signed_firmware"),"1",g' ${FILESYSTEM_MOD_DIR}${file}
+done
