8a323b1e49c42e07d24d146bd08a4cc6fcb7aeb5
Gene Rudoy <gene@freetz.org>
2015-08-15 10:42:51 +0000

    Fritz!OS 6.35:  * adjust led_deactivation-patch  * refs #2774, refs #2595
    git-svn-id: http://svn.freetz.org/trunk@13357 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/535-enable_led_deactivation.sh
+++ patches/scripts/535-enable_led_deactivation.sh
@@ -1,4 +1,4 @@
 [ "$FREETZ_ENABLE_LED_DEACTIVATION" == "y" -a -f "${FILESYSTEM_MOD_DIR}/usr/www/all/system/led_display.lua" ] || return 0
 
 echo1 "re-adding LED (de-)activation page"
-modpatch "${FILESYSTEM_MOD_DIR}" "${PATCHES_COND_DIR}/enable_led_deactivation.patch"
+modpatch "${FILESYSTEM_MOD_DIR}" "${PATCHES_COND_DIR}/enable_led_deactivation-${FREETZ_TYPE_PREFIX_SERIES_SUBDIR}.patch"
