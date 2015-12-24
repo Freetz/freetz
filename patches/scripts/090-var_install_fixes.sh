627e763073bc9dc7f22b64320e11bcf32b3c8086
Gene Rudoy <gene@freetz.org>
2015-12-24 13:54:59 +0000

    kernel-3.10.x based 6.5x-firmwares:  * remove unnecessary {{{conv=sync}}} option from dd call in {{{/var/install}}} (by [http://www.ip-phone-forum.de/showthread.php?t=282943&page=3&p=2135210&viewfull=1#post2135210 PeterPawn])  * refs #2774
    git-svn-id: http://svn.freetz.org/trunk@13523 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/090-var_install_fixes.sh
+++ patches/scripts/090-var_install_fixes.sh
@@ -1,6 +1,11 @@
 var_install_file="${FIRMWARE_MOD_DIR}/var/install"
 
 if grep -q "find [.] -name dd" "${var_install_file}" 2>/dev/null; then
-	echo1 "fixing test for dd-tool in /var/install"
+	echo1 "fixing test for dd in /var/install"
 	modsed -r 's,(find )[.]( -name dd)( -xdev),\1/bin/\3 -maxdepth 1\2,g' "${var_install_file}"
+fi
+
+if grep -q "bs=256 skip=1 conv=sync" "${var_install_file}" 2>/dev/null; then
+	echo1 "removing unnecessary conv=sync option from dd call in /var/install"
+	modsed -r 's,(bs=256 skip=1) conv=sync,\1,' "${var_install_file}"
 fi
