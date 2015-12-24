8f4737882545b76f67114515d2b84b48ec7dac88
Gene Rudoy <gene@freetz.org>
2015-12-24 15:01:16 +0000

    kernel-3.10.x based 6.5x-firmwares:  * implement an alternative offset-based ext2-mount-method (reduces memory footprint while flashing the firmware)  * based on the idea from [http://www.ip-phone-forum.de/showthread.php?t=282943&page=2&p=2134811&viewfull=1#post2134811 this PeterPawn's post]  * refs #2774
    git-svn-id: http://svn.freetz.org/trunk@13524 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/090-var_install_fixes.sh
+++ patches/scripts/090-var_install_fixes.sh
@@ -9,3 +9,16 @@ if grep -q "bs=256 skip=1 conv=sync" "${var_install_file}" 2>/dev/null; then
 	echo1 "removing unnecessary conv=sync option from dd call in /var/install"
 	modsed -r 's,(bs=256 skip=1) conv=sync,\1,' "${var_install_file}"
 fi
+
+if grep -q "cannot mount squashfs, trying ext2" "${var_install_file}" 2>/dev/null; then
+	echo1 "adding alternative offset-based ext2-mount-method (reduces memory footprint while flashing the firmware)"
+
+modsed '/cannot mount squashfs, trying ext2/ a\
+# Try mounting using the offset option (available only in Freetz) to reduce memory footprint, if it fails use AVMs dd-based method\
+echo '"'"'    mount -t ext2 -o loop,offset=256 /var/tmp/filesystem.image /var/tmp/fs'"'"' >> /var/post_install\
+echo '"'"'    if ! mount | grep -q "/var/tmp/fs type ext2" 2>/dev/null; then'"'"' >> /var/post_install' "${var_install_file}"
+
+modsed '/mount -t ext2 \/var\/tmp\/fsimage.ext2/ a\
+echo '"'"'    fi'"'"' >> /var/post_install' "${var_install_file}"
+
+fi
