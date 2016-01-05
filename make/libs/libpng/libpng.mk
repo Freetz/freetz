d152730bc407abbb26bbde2d31d99769cbec8df0
Gene Rudoy <gene@freetz.org>
2016-01-05 22:43:51 +0000

    libpng:  * bump version to [ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng12/libpng-1.2.56-README.txt 1.2.56]
    git-svn-id: http://svn.freetz.org/trunk@13532 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/libpng/libpng.mk
+++ make/libs/libpng/libpng.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_LIB, 1.2.55)
-$(PKG)_LIB_VERSION:=0.55.0
+$(call PKG_INIT_LIB, 1.2.56)
+$(PKG)_LIB_VERSION:=0.56.0
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
-$(PKG)_SOURCE_MD5:=68ac486976e73fd164d54740bece57a2
+$(PKG)_SOURCE_MD5:=868562bd1c58b76ed8703f135a2e439a
 $(PKG)_SITE:=@SF/libpng
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/.libs/libpng12.so.$($(PKG)_LIB_VERSION)
