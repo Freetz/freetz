1c23da2c40865648ed74d622a10f5297b8c0af8f
Gene Rudoy <gene@freetz.org>
2016-01-16 23:15:15 +0000

    * rename {{{FREETZ_AVMPLUGINS_ENABLED}}} to {{{FREETZ_AVMPLUGINS_INTEGRATE}}}  * menuconfig wording tweaks
    git-svn-id: http://svn.freetz.org/trunk@13581 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/500-remove_mediasrv.sh
+++ patches/scripts/500-remove_mediasrv.sh
@@ -1,5 +1,5 @@
 [ "$FREETZ_REMOVE_MEDIASRV" == "y" ] || \
-[ "$FREETZ_AVMPLUGINS_ENABLED" -a "$FREETZ_AVMPLUGINS_MEDIASRV" != "y" ] || \
+[ "$FREETZ_AVMPLUGINS_INTEGRATE" -a "$FREETZ_AVMPLUGINS_MEDIASRV" != "y" ] || \
 return 0
 
 # if nas, mediaserv und samba are removed -> remove_nas deletes menu item Heimnetz > Speicher (NAS)
