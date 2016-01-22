2805108630d59fe47bf78a2022bf272ecd821bae
Gene Rudoy <gene@freetz.org>
2016-01-22 21:31:05 +0000

    7390:  * force loop module loading in start_plugin.sh (I don't understand why it works with the original firmware but let's try to workaround the problem)  * refs #2774
    git-svn-id: http://svn.freetz.org/trunk@13589 149334a1-2f27-0410-a3b9-fc62619ac1e6

--- patches/scripts/080-start_plugin.sh
+++ patches/scripts/080-start_plugin.sh
@@ -1,6 +1,12 @@
 start_plugin="${FILESYSTEM_MOD_DIR}/sbin/start_plugin.sh"
 if [ -e "${start_plugin}" ]; then
+
 	# 'init-done' is rc.tail.sh spelling, 'init_done' is start_plugin.sh spelling
-	echo1 "modifying start_plugin.sh to support both 'init-done' & 'init_done' spellings"
+	echo1 "start_plugin.sh: support both 'init-done' & 'init_done' spellings"
 	modsed -r 's,(init_done\)),init-done|\1,' "${start_plugin}"
+
+	echo1 "start_plugin.sh: forcing loop module loading"
+	modsed -r '/^[a-z]+_mount\)/ a\
+modprobe loop' "${start_plugin}"
+
 fi
