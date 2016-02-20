85f790443ed382317de87bdcca2959d991f2d2ee
Gene Rudoy <gene@freetz.org>
2016-02-20 11:10:11 +0000

    decrypt-fritzos-cfg:  * bump version to [https://github.com/PeterPawn/decode_passwords/releases/tag/v0.2 v0.2]  * switch to the official [https://github.com/PeterPawn/decode_passwords author's repository] (we hovwever still provide the script under a name deviating from the original one)
    git-svn-id: http://svn.freetz.org/trunk@13626 f5190166-0702-4917-9039-51ec32eddaf5

--- make/decrypt-fritzos-cfg/decrypt-fritzos-cfg.mk
+++ make/decrypt-fritzos-cfg/decrypt-fritzos-cfg.mk
@@ -1,11 +1,32 @@
-$(call PKG_INIT_BIN,0.0)
+$(call PKG_INIT_BIN,v0.2)
+$(PKG)_ORIG_NAME:=decode_passwords
+$(PKG)_SOURCE:=$(subst -,_,$(pkg))-$($(PKG)_VERSION).tar.xz
+$(PKG)_SITE:=git@https://github.com/PeterPawn/$($(PKG)_ORIG_NAME).git
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/$(subst -,_,$(pkg))-$($(PKG)_VERSION)
 
+$(PKG)_PATCH_POST_CMDS += $(SED) -i -e 's,$($(PKG)_ORIG_NAME),$(pkg),g' $($(PKG)_ORIG_NAME);
+
+$(PKG)_BINARY := $($(PKG)_DIR)/$($(PKG)_ORIG_NAME)
+$(PKG)_TARGET_BINARY := $($(PKG)_DEST_DIR)/usr/bin/$(pkg)
+
+$(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	@chmod 755 $@
+	@touch -c $@
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_FILE)
 
 $(pkg):
 
-$(pkg)-precompiled:
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
 $(pkg)-clean:
+
+$(pkg)-uninstall:
+	$(RM) $(DECRYPT_FRITZOS_CFG_TARGET_BINARY)
 
 $(PKG_FINISH)
