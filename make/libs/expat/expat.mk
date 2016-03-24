b54db9c6ecef71985130ac2ffa13fa37c5f4f2b9
Gene Rudoy <gene@freetz.org>
2016-03-24 18:32:54 +0000

    expat:  * bump version to [https://sourceforge.net/p/expat/code_git/ci/R_2_1_1/tree/expat/Changes 2.1.1]
    git-svn-id: http://svn.freetz.org/trunk@13663 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/expat/expat.mk
+++ make/libs/expat/expat.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_LIB, 2.1.0)
+$(call PKG_INIT_LIB, 2.1.1)
 $(PKG)_LIB_VERSION:=1.6.0
-$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=dd7dab7a5fea97d2a6a43f511449b7cd
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_SOURCE_MD5:=7380a64a8e3a9d66a9887b01d0d7ea81
 $(PKG)_SITE:=@SF/expat
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/.libs/libexpat.so.$($(PKG)_LIB_VERSION)
