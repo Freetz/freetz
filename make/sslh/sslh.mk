fa8613774b6da0ea0f92516b5256325263db0415
Gene Rudoy <gene@freetz.org>
2016-04-04 20:58:17 +0000

    sslh:  * bump version to [http://www.rutschle.net/tech/sslh.shtml 1.18]  * note: config file format has changed (s. [http://www.rutschle.net/tech/sslh.shtml changelog] for details), you might need to update your config
    git-svn-id: http://svn.freetz.org/trunk@13682 f5190166-0702-4917-9039-51ec32eddaf5

--- make/sslh/sslh.mk
+++ make/sslh/sslh.mk
@@ -1,8 +1,8 @@
-$(call PKG_INIT_BIN,1.17)
-$(PKG)_SOURCE:=$(pkg)-v$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=1fc3ada4bafaca5a9786cc1431f48ed4
-$(PKG)_SITE:=http://rutschle.net/tech
-$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/$(pkg)-v$($(PKG)_VERSION)
+$(call PKG_INIT_BIN,v1.18)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=0e3568d5d234516c634d4df156473298
+$(PKG)_SITE:=http://rutschle.net/tech/sslh
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/sslh-fork
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/sslh
 
@@ -15,9 +15,8 @@ $(PKG_CONFIGURED_NOP)
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(SSLH_DIR) \
 		CC="$(TARGET_CC)" \
-		CFLAGS="$(TARGET_CFLAGS) -DLIBCONFIG" \
-		USELIBWRAP= \
-		sslh
+		CFLAGS="$(TARGET_CFLAGS)" \
+		sslh-fork
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
