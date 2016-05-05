1fbbe7f7c8a4a687225c32c46e439c54fbab115a
Gene Rudoy <gene@freetz.org>
2016-05-05 14:06:36 +0000

    sfk:  * add sfk package for target  * refs [https://github.com/PeterPawn/freetz/commit/3a34b806da9ffdf3fd1c754d4c3824bc00acc379 3a34b806da]
    git-svn-id: http://svn.freetz.org/trunk@13710 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/sfk/sfk.mk
@@ -0,0 +1,42 @@
+$(call PKG_INIT_BIN, 1.7.6)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=6e612077b4bde1f2752c8df4954bbb02
+$(PKG)_SITE:=@SF/swissfileknife
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/sfk
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/sfk
+
+$(PKG)_DEPENDS_ON += $(STDCXXLIB)
+$(PKG)_REBUILD_SUBOPTS += FREETZ_STDCXXLIB
+
+$(PKG)_EXTRA_CXXFLAGS  += -ffunction-sections -fdata-sections
+$(PKG)_EXTRA_LDFLAGS   += -Wl,--gc-sections
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_SFK_STATIC
+$(PKG)_EXTRA_LDFLAGS   += $(if $(FREETZ_PACKAGE_SFK_STATIC),-static)
+
+ifneq ($(strip $(DL_DIR)/$($(PKG)_SOURCE)), $(strip $(DL_DIR)/$($(PKG)_HOST_SOURCE)))
+$(PKG_SOURCE_DOWNLOAD)
+endif
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(SFK_DIR) \
+		EXTRA_CXXFLAGS="$(SFK_EXTRA_CXXFLAGS)" \
+		EXTRA_LDFLAGS="$(SFK_EXTRA_LDFLAGS)"
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(SFK_DIR) clean
+
+$(pkg)-uninstall:
+	$(RM) $(SFK_TARGET_BINARY)
+
+$(PKG_FINISH)
