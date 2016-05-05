1b6a7a5de2ee07721e6d37e82991ef455a872f59
Gene Rudoy <gene@freetz.org>
2016-05-05 13:13:51 +0000

    sfk:  * rename host sfk package to sfk-host  * refs [https://github.com/PeterPawn/freetz/commit/3a34b806da9ffdf3fd1c754d4c3824bc00acc379 3a34b806da]
    git-svn-id: http://svn.freetz.org/trunk@13709 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ tools/make/sfk-host/sfk-host.mk
@@ -0,0 +1,44 @@
+SFK_HOST_VERSION:=1.7.6
+SFK_HOST_SOURCE:=sfk-$(SFK_HOST_VERSION).tar.gz
+SFK_HOST_SOURCE_MD5:=6e612077b4bde1f2752c8df4954bbb02
+SFK_HOST_SITE:=@SF/swissfileknife
+
+SFK_HOST_MAKE_DIR:=$(TOOLS_DIR)/make/sfk-host
+SFK_HOST_DIR:=$(TOOLS_SOURCE_DIR)/sfk-$(SFK_HOST_VERSION)
+SFK_HOST_DESTDIR:=$(FREETZ_BASE_DIR)/$(TOOLS_DIR)
+
+sfk-host-source: $(DL_DIR)/$(SFK_HOST_SOURCE)
+$(DL_DIR)/$(SFK_HOST_SOURCE): | $(DL_DIR)
+	$(DL_TOOL) $(DL_DIR) $(SFK_HOST_SOURCE) $(SFK_HOST_SITE) $(SFK_HOST_SOURCE_MD5)
+
+sfk-host-unpacked: $(SFK_HOST_DIR)/.unpacked
+$(SFK_HOST_DIR)/.unpacked: $(DL_DIR)/$(SFK_HOST_SOURCE) | $(TOOLS_SOURCE_DIR) $(UNPACK_TARBALL_PREREQUISITES)
+	$(call UNPACK_TARBALL,$(DL_DIR)/$(SFK_HOST_SOURCE),$(TOOLS_SOURCE_DIR))
+	$(call APPLY_PATCHES,$(SFK_HOST_MAKE_DIR)/patches,$(SFK_HOST_DIR))
+	touch $@
+
+$(SFK_HOST_DIR)/.configured: $(SFK_HOST_DIR)/.unpacked
+	(cd $(SFK_HOST_DIR); $(RM) config.cache; \
+		./configure \
+		--prefix=$(SFK_HOST_DESTDIR) \
+		$(DISABLE_NLS) \
+	);
+	touch $@
+
+$(SFK_HOST_DIR)/sfk: $(SFK_HOST_DIR)/.configured
+	$(MAKE) -C $(SFK_HOST_DIR) all
+
+$(TOOLS_DIR)/sfk: $(SFK_HOST_DIR)/sfk
+	$(INSTALL_FILE)
+	strip $@
+
+sfk-host: $(TOOLS_DIR)/sfk
+
+sfk-host-clean:
+	-$(MAKE) -C $(SFK_HOST_DIR) clean
+
+sfk-host-dirclean:
+	$(RM) -r $(SFK_HOST_DIR)
+
+sfk-host-distclean: sfk-host-dirclean
+	$(RM) $(TOOLS_DIR)/sfk
