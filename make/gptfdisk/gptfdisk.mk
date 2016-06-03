02c5e3d9ced6679bb4e82d2b4f4a7624a4556cb3
Gene Rudoy <gene@freetz.org>
2016-06-03 22:20:34 +0000

    * add new package - [http://www.rodsbooks.com/gdisk/ gptfdisk] (by olvidalo + fixes by me)  * refs #2837
    git-svn-id: http://svn.freetz.org/trunk@13746 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/gptfdisk/gptfdisk.mk
@@ -0,0 +1,38 @@
+$(call PKG_INIT_BIN, 1.0.1)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=d7f3d306b083123bcc6f5941efade586
+$(PKG)_SITE:=http://pkgs.fedoraproject.org/lookaside/pkgs/gdisk/$(pkg)-$($(PKG)_VERSION).tar.gz/md5/$($(PKG)_SOURCE_MD5)
+
+# log2/log are provided by libm in uClibc
+$(PKG)_PATCH_POST_CMDS += $(SED) -r -i -e 's,(-luuid),\1 -lm,g' Makefile;
+
+$(PKG)_CATEGORY:=Unstable
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/gdisk
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/gdisk
+
+$(PKG)_DEPENDS_ON += e2fsprogs
+$(PKG)_DEPENDS_ON += $(STDCXXLIB)
+$(PKG)_REBUILD_SUBOPTS += FREETZ_STDCXXLIB
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(GPTFDISK_DIR) gdisk \
+		CXX="$(TARGET_CXX)" \
+		CXXFLAGS="$(TARGET_CFLAGS) -D_BSD_SOURCE"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(GPTFDISK_DIR) clean
+
+$(pkg)-uninstall:
+	$(RM) $(GPTFDISK_TARGET_BINARY)
+
+$(PKG_FINISH)
