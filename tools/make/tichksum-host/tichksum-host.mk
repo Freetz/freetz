2c8fde6c9b5301ec6cc483ab0fb6b1fe5d5c51b0
Gene Rudoy <gene@freetz.org>
2016-08-09 21:20:22 +0000

    * rename {{{tichksum}}} host package to {{{tichksum-host}}}
    git-svn-id: http://svn.freetz.org/trunk@13878 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ tools/make/tichksum-host/tichksum-host.mk
@@ -0,0 +1,26 @@
+TICHKSUM_HOST_SRC:=$(TOOLS_DIR)/make/tichksum-host/src
+TICHKSUM_HOST_DIR:=$(TOOLS_SOURCE_DIR)/tichksum
+
+tichksum-host-unpacked: $(TICHKSUM_HOST_DIR)/.unpacked
+$(TICHKSUM_HOST_DIR)/.unpacked: $(wildcard $(TICHKSUM_HOST_SRC)/*) | $(TOOLS_SOURCE_DIR) tar-host
+	$(RM) -r $(TICHKSUM_HOST_DIR)
+	mkdir -p $(TICHKSUM_HOST_DIR)
+	$(call COPY_USING_TAR,$(TICHKSUM_HOST_SRC),$(TICHKSUM_HOST_DIR))
+	touch $@
+
+$(TICHKSUM_HOST_DIR)/tichksum: $(TICHKSUM_HOST_DIR)/.unpacked
+	$(MAKE) CC="$(TOOLS_CC)" -C $(TICHKSUM_HOST_DIR)
+
+$(TOOLS_DIR)/tichksum: $(TICHKSUM_HOST_DIR)/tichksum
+	$(INSTALL_FILE)
+
+tichksum-host: $(TOOLS_DIR)/tichksum
+
+tichksum-host-clean:
+	-$(MAKE) -C $(TICHKSUM_HOST_DIR) clean
+
+tichksum-host-dirclean:
+	$(RM) -r $(TICHKSUM_HOST_DIR)
+
+tichksum-host-distclean: tichksum-host-dirclean
+	$(RM) $(TOOLS_DIR)/tichksum
