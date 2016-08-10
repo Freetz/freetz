Freetz/freetz

commit 686d29e42ee1a6c77803f824261984eee5d329fe

tichksum:  * cs_is_tagged: do always return payload_length
git-svn-id: http://svn.freetz.org/trunk@13885 f5190166-0702-4917-9039-51ec32eddaf5

tools/make/tichksum-host/src/cksum.c
@@ -52,7 +52,7 @@ set_le32 (void *p, uint32_t v)
 
 // return 1: tagged, 0: not tagged, -1: error
 int
-cs_is_tagged (int fd, uint32_t *sum, off_t *length)
+cs_is_tagged (int fd, uint32_t *sum, off_t *payload_length)
 {
   cksum_t cksum;
   off_t len;
@@ -69,15 +69,17 @@ cs_is_tagged (int fd, uint32_t *sum, off_t *length)
 #endif
   if (len < 0)
     return -1;
+
   int is_tagged =
     read (fd, &cksum, sizeof (cksum_t)) == sizeof (cksum_t)
     && get_le32 (cksum.ck_magic) == MAGIC_NUMBER;
-  if (is_tagged) {
-    if (sum)
-      *sum = get_le32 (cksum.ck_crc);
-    if (length)
-      *length = len;
-  }
+
+  if (is_tagged && sum)
+    *sum = get_le32 (cksum.ck_crc);
+
+  if (payload_length)
+    *payload_length = len + (is_tagged ? 0 : sizeof (cksum_t));
+
   return is_tagged;
 }
 

tools/make/tichksum-host/src/cksum.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <sys/types.h>
 
-int cs_is_tagged (int fd, uint32_t *sum, off_t *length);
+int cs_is_tagged (int fd, uint32_t *sum, off_t *payload_length);
 int cs_add_sum (int fd, uint32_t *sum);
 int cs_verify_sum (int fd, uint32_t *sum, uint32_t *res);
 int cs_remove_sum (int fd, uint32_t *res);

commit 9037092ee631e01c3b92b2b2970a58fccaa2ca65

tichksum:  * cs_is_tagged: do return correct payload_length also for files smaller than 8 bytes, bug/thinko introduced in r13885
git-svn-id: http://svn.freetz.org/trunk@13891 f5190166-0702-4917-9039-51ec32eddaf5

tools/make/tichksum-host/src/cksum.c
@@ -54,16 +54,17 @@ cs_is_tagged (int fd, uint32_t *saved_sum, off_t *payload_length)
   cksum_t cksum;
   off_t len;
 
-#if 0
-  // for whatever reason SEEK_END with negative offset doesn't work on the target (most likely a uClibc issue)
-  len = lseek (fd, -sizeof (cksum_t), SEEK_END);
-#else
-  // workaround it by using two lseek calls
-  len = lseek (fd,                0, SEEK_END);
+  len = lseek (fd, 0, SEEK_END);
   if (len < 0)
     return -1;
+
+  if (payload_length)
+    *payload_length = len;
+
+  if (len < (off_t) sizeof (cksum_t))
+    return 0;
+
   len = lseek (fd, len - sizeof (cksum_t), SEEK_SET);
-#endif
   if (len < 0)
     return -1;
 
@@ -71,11 +72,12 @@ cs_is_tagged (int fd, uint32_t *saved_sum, off_t *payload_length)
     read (fd, &cksum, sizeof (cksum_t)) == sizeof (cksum_t)
     && get_le32 (cksum.ck_magic) == MAGIC_NUMBER;
 
-  if (is_tagged && saved_sum)
-    *saved_sum = get_le32 (cksum.ck_crc);
-
-  if (payload_length)
-    *payload_length = len + (is_tagged ? 0 : sizeof (cksum_t));
+  if (is_tagged) {
+    if (saved_sum)
+      *saved_sum = get_le32 (cksum.ck_crc);
+    if (payload_length)
+      *payload_length -= sizeof (cksum_t);
+  }
 
   return is_tagged;
 }