9380f21962496a04a8079f7fcd408437fbf020ca
Gene Rudoy <gene@freetz.org>
2016-10-15 18:36:45 +0000

    fakeroot:  * bump version to 1.21  * add some patches from debian/ubuntu  * closes #2864
    git-svn-id: http://svn.freetz.org/trunk@13956 f5190166-0702-4917-9039-51ec32eddaf5

--- tools/make/fakeroot/fakeroot.mk
+++ tools/make/fakeroot/fakeroot.mk
@@ -1,6 +1,6 @@
-FAKEROOT_VERSION:=1.20.2
-FAKEROOT_SOURCE:=fakeroot_$(FAKEROOT_VERSION).orig.tar.bz2
-FAKEROOT_SOURCE_MD5:=a4b4564a75024aa96c86e4d1017ac786
+FAKEROOT_VERSION:=1.21
+FAKEROOT_SOURCE:=fakeroot_$(FAKEROOT_VERSION).orig.tar.gz
+FAKEROOT_SOURCE_MD5:=be5c9a0e516869fca4a6758105968e5a
 FAKEROOT_SITE:=http://ftp.debian.org/debian/pool/main/f/fakeroot
 
 FAKEROOT_MAKE_DIR:=$(TOOLS_DIR)/make/fakeroot
@@ -30,12 +30,15 @@ $(DL_DIR)/$(FAKEROOT_SOURCE): | $(DL_DIR)
 fakeroot-unpacked: $(FAKEROOT_DIR)/.unpacked
 $(FAKEROOT_DIR)/.unpacked: $(DL_DIR)/$(FAKEROOT_SOURCE) | $(TOOLS_SOURCE_DIR) $(UNPACK_TARBALL_PREREQUISITES)
 	$(call UNPACK_TARBALL,$(DL_DIR)/$(FAKEROOT_SOURCE),$(TOOLS_SOURCE_DIR))
-	$(SED) -i "s,getopt --version,getopt --version 2>/dev/null," \
-		$(FAKEROOT_DIR)/scripts/fakeroot.in
 	$(call APPLY_PATCHES,$(FAKEROOT_MAKE_DIR)/patches,$(FAKEROOT_DIR))
 	touch $@
 
-$(FAKEROOT_MAINARCH_DIR)/.configured: $(FAKEROOT_DIR)/.unpacked
+fakeroot-bootstrap: $(FAKEROOT_DIR)/.bootstrapped
+$(FAKEROOT_DIR)/.bootstrapped: $(FAKEROOT_DIR)/.unpacked
+	(cd $(FAKEROOT_DIR); ./bootstrap)
+	touch $@
+
+$(FAKEROOT_MAINARCH_DIR)/.configured: $(FAKEROOT_DIR)/.bootstrapped
 	(mkdir -p $(FAKEROOT_MAINARCH_DIR); cd $(FAKEROOT_MAINARCH_DIR); $(RM) config.cache; \
 		CFLAGS="-O3 -Wall" \
 		CC="$(TOOLS_CC)" \
@@ -50,7 +53,7 @@ $(FAKEROOT_TARGET_SCRIPT): $(FAKEROOT_MAINARCH_DIR)/.configured
 	$(MAKE) -C $(FAKEROOT_MAINARCH_DIR) install
 	$(SED) -i -e 's,^PATHS=.*,PATHS=$(FAKEROOT_MAINARCH_LD_PRELOAD_PATH)$(if $(BIARCH_BUILD_SYSTEM),:$(FAKEROOT_BIARCH_LD_PRELOAD_PATH)),g' $(FAKEROOT_TARGET_SCRIPT)
 
-$(FAKEROOT_BIARCH_DIR)/.configured: $(FAKEROOT_DIR)/.unpacked
+$(FAKEROOT_BIARCH_DIR)/.configured: $(FAKEROOT_DIR)/.bootstrapped
 	(mkdir -p $(FAKEROOT_BIARCH_DIR); cd $(FAKEROOT_BIARCH_DIR); $(RM) config.cache; \
 		CFLAGS="-m32 -O3 -Wall" \
 		CC="$(TOOLS_CC)" \
