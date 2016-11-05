eef0755a0218d8d83a577de19955ea1fc7aca36d
Whoopie <whoopie79@gmx.net>
2016-11-05 15:50:37 +0000

    udpxy: bump version to 1.0.23-9
    git-svn-id: http://svn.freetz.org/trunk@13971 f5190166-0702-4917-9039-51ec32eddaf5

--- make/udpxy/udpxy.mk
+++ make/udpxy/udpxy.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_BIN, 1.0-Chipmunk-16)
-$(PKG)_SOURCE:=$(pkg).$($(PKG)_VERSION).tgz
-$(PKG)_SOURCE_MD5:=0bba6fd3e0b0065424b198abdf0acb7d
-$(PKG)_SITE:=@SF/udpxy
+$(call PKG_INIT_BIN, 1.0.23-9)
+$(PKG)_SOURCE:=$(pkg).$($(PKG)_VERSION)-prod.tar.gz
+$(PKG)_SOURCE_MD5:=0c953f7dd80329c1a062997afb9b6744
+$(PKG)_SITE:=http://www.udpxy.com/download/1_23
 $(PKG)_BINARY:=$($(PKG)_DIR)/udpxy
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/udpxy
 
