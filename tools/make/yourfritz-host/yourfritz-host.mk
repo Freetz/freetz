88ea1a7920882ecdd67f8eeec9df5a4b2eb5ab8b
Gene Rudoy <gene@freetz.org>
2017-01-13 20:43:50 +0000

    yourfritz-host:  * change the way YourFritz tools are made available within the Freetz build system - from now on all tools could be accessed using the {{{tools/yf}}} symlink, e.g. {{{tools/yf/signimage/sign_image}}}
    git-svn-id: http://svn.freetz.org/trunk@14037 f5190166-0702-4917-9039-51ec32eddaf5

--- tools/make/yourfritz-host/yourfritz-host.mk
+++ tools/make/yourfritz-host/yourfritz-host.mk
@@ -5,11 +5,6 @@ YOURFRITZ_HOST_SITE:=git@https://github.com/PeterPawn/YourFritz.git
 YOURFRITZ_HOST_MAKE_DIR:=$(TOOLS_DIR)/make/yourfritz-host
 YOURFRITZ_HOST_DIR:=$(TOOLS_SOURCE_DIR)/yourfritz-$(YOURFRITZ_HOST_VERSION)
 
-YOURFRITZ_HOST_TARGET_DIR:=$(TOOLS_DIR)
-
-YOURFRITZ_HOST_COMPONENTS:=signimage
-#YOURFRITZ_HOST_COMPONENTS+=eva_tools
-
 yourfritz-host-source: $(DL_DIR)/$(YOURFRITZ_HOST_SOURCE)
 $(DL_DIR)/$(YOURFRITZ_HOST_SOURCE): | $(DL_DIR)
 	$(DL_TOOL) $(DL_DIR) $(YOURFRITZ_HOST_SOURCE) $(YOURFRITZ_HOST_SITE) $(YOURFRITZ_HOST_SOURCE_MD5)
@@ -20,13 +15,11 @@ $(YOURFRITZ_HOST_DIR)/.unpacked: $(DL_DIR)/$(YOURFRITZ_HOST_SOURCE) | $(TOOLS_SO
 	$(call APPLY_PATCHES,$(YOURFRITZ_HOST_MAKE_DIR)/patches,$(YOURFRITZ_HOST_DIR))
 	touch $@
 
-$(YOURFRITZ_HOST_COMPONENTS:%=$(YOURFRITZ_HOST_DIR)/%): | $(YOURFRITZ_HOST_DIR)/.unpacked
-
-$(YOURFRITZ_HOST_COMPONENTS:%=$(YOURFRITZ_HOST_DIR)/%/.symlinked): $(YOURFRITZ_HOST_DIR)/%/.symlinked: $(YOURFRITZ_HOST_DIR)/%
-	@target_abs="$<"; target_rel=$${target_abs#$(FREETZ_BASE_DIR)/}; ln -Tsf ../$${target_rel} $(YOURFRITZ_HOST_TARGET_DIR)/$(notdir $<)
+$(YOURFRITZ_HOST_DIR)/.symlinked: | $(YOURFRITZ_HOST_DIR)/.unpacked
+	@ln -Tsf ../$(YOURFRITZ_HOST_DIR:$(FREETZ_BASE_DIR)/%=%) $(TOOLS_DIR)/yf
 	touch $@
 
-yourfritz-host: $(YOURFRITZ_HOST_COMPONENTS:%=$(YOURFRITZ_HOST_DIR)/%/.symlinked)
+yourfritz-host: $(YOURFRITZ_HOST_DIR)/.symlinked
 
 yourfritz-host-clean:
 
@@ -34,4 +27,4 @@ yourfritz-host-dirclean:
 	$(RM) -r $(YOURFRITZ_HOST_DIR)
 
 yourfritz-host-distclean: yourfritz-host-dirclean
-	$(RM) $(YOURFRITZ_HOST_COMPONENTS:%=$(YOURFRITZ_HOST_TARGET_DIR)/%)
+	$(RM) $(TOOLS_DIR)/yf
