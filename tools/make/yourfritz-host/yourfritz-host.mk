beca7dc8b883da1d05d067e286c9746b2fb90865
Gene Rudoy <gene@freetz.org>
2017-01-30 23:20:33 +0000

    yourfritz-host:  * build tools from avm_kernel_config  * refs #2774, refs [http://www.ip-phone-forum.de/showthread.php?t=287995 this IPPF thread]
    git-svn-id: http://svn.freetz.org/trunk@14063 f5190166-0702-4917-9039-51ec32eddaf5

--- tools/make/yourfritz-host/yourfritz-host.mk
+++ tools/make/yourfritz-host/yourfritz-host.mk
@@ -5,6 +5,12 @@ YOURFRITZ_HOST_SITE:=git@https://github.com/PeterPawn/YourFritz.git
 YOURFRITZ_HOST_MAKE_DIR:=$(TOOLS_DIR)/make/yourfritz-host
 YOURFRITZ_HOST_DIR:=$(TOOLS_SOURCE_DIR)/yourfritz-$(YOURFRITZ_HOST_VERSION)
 
+# AKC stands for AVM_KERNEL_CONFIG
+YOURFRITZ_HOST_AKC_BUILD_DIR:=$(YOURFRITZ_HOST_DIR)/avm_kernel_config
+YOURFRITZ_HOST_AKC_TOOLS:=extract_avm_kernel_config gen_avm_kernel_config
+YOURFRITZ_HOST_AKC_TOOLS_BUILD_DIR:=$(YOURFRITZ_HOST_AKC_TOOLS:%=$(YOURFRITZ_HOST_AKC_BUILD_DIR)/%)
+YOURFRITZ_HOST_AKC_TOOLS_TARGET_DIR:=$(YOURFRITZ_HOST_AKC_TOOLS:%=$(TOOLS_DIR)/%)
+
 yourfritz-host-source: $(DL_DIR)/$(YOURFRITZ_HOST_SOURCE)
 $(DL_DIR)/$(YOURFRITZ_HOST_SOURCE): | $(DL_DIR)
 	$(DL_TOOL) $(DL_DIR) $(YOURFRITZ_HOST_SOURCE) $(YOURFRITZ_HOST_SITE) $(YOURFRITZ_HOST_SOURCE_MD5)
@@ -19,12 +25,23 @@ $(YOURFRITZ_HOST_DIR)/.symlinked: | $(YOURFRITZ_HOST_DIR)/.unpacked
 	@ln -Tsf ../$(YOURFRITZ_HOST_DIR:$(FREETZ_BASE_DIR)/%=%) $(TOOLS_DIR)/yf
 	touch $@
 
-yourfritz-host: $(YOURFRITZ_HOST_DIR)/.symlinked
+$(YOURFRITZ_HOST_AKC_TOOLS_BUILD_DIR): $(YOURFRITZ_HOST_DIR)/.unpacked $(DTC_LIBFDT_HOST_DIR)/libfdt.a
+	$(MAKE) -f Makefile.freetz -C $(YOURFRITZ_HOST_AKC_BUILD_DIR) \
+		CC="$(TOOLS_CC)" \
+		LIBFDT_DIR=$(DTC_LIBFDT_HOST_DIR) \
+		$(YOURFRITZ_HOST_AKC_TOOLS)
+	touch -c $@
+
+$(YOURFRITZ_HOST_AKC_TOOLS_TARGET_DIR): $(TOOLS_DIR)/%: $(YOURFRITZ_HOST_AKC_BUILD_DIR)/%
+	$(INSTALL_FILE)
+
+yourfritz-host: $(YOURFRITZ_HOST_DIR)/.symlinked $(YOURFRITZ_HOST_AKC_TOOLS_TARGET_DIR)
 
 yourfritz-host-clean:
+	-$(MAKE) -f Makefile.freetz -C $(YOURFRITZ_HOST_AKC_BUILD_DIR) clean
 
 yourfritz-host-dirclean:
 	$(RM) -r $(YOURFRITZ_HOST_DIR)
 
 yourfritz-host-distclean: yourfritz-host-dirclean
-	$(RM) $(TOOLS_DIR)/yf
+	$(RM) $(TOOLS_DIR)/yf $(YOURFRITZ_HOST_AKC_TOOLS_TARGET_DIR)
