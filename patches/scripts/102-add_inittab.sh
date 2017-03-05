f0bd87670366648a442bd5a0b123584dd3ce1e28
Gene Rudoy <gene@freetz.org>
2017-03-05 11:38:45 +0000

    * introduce a menuconfig variable for serial console device
    git-svn-id: http://svn.freetz.org/trunk@14110 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/102-add_inittab.sh
+++ patches/scripts/102-add_inittab.sh
@@ -1,9 +1,3 @@
-if isFreetzType 7312 7320 7330; then
-	console="/dev/ttyS1" # 7312, 7320 and 7330 all use ttyS1 as serial console
-else
-	console="/dev/ttyS0"
-fi
-
 # Do not start the shell if it should be disabled
 [ "$FREETZ_DISABLE_SERIAL_CONSOLE" == "y" ] && shell="" || shell="-/bin/sh"
 
@@ -18,7 +12,7 @@ cat << EOF > "${FILESYSTEM_MOD_DIR}/etc/inittab"
 ::sysinit:/etc/init.d/rc.S
 
 # Start an "askfirst" shell on the console (whatever that may be)
-$console::askfirst:$shell
+${FREETZ_AVM_SERIAL_CONSOLE_DEVICE}::askfirst:$shell
 $ip_watchdog
 # Stuff to do before rebooting
 ::shutdown:/bin/sh -c /etc/inittab.shutdown
