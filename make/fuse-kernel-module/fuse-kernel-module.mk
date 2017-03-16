04c60bb08662c608d6ff62c1c5a0aff7ffc0b3f4
Gene Rudoy <gene@freetz.org>
2017-03-16 21:09:36 +0000

    fuse:  * split package in fuse and fuse-kernel-module, the latter one is used only for kernel-2.13.x
    git-svn-id: http://svn.freetz.org/trunk@14158 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/fuse-kernel-module/fuse-kernel-module.mk
@@ -0,0 +1,45 @@
+$(call PKG_INIT_BIN, 2.7.6)
+$(PKG)_SOURCE:=fuse-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=319f12dffd6f7073628fefc980254f4a
+$(PKG)_SITE:=https://github.com/libfuse/libfuse/releases/download/fuse_2_9_4
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/fuse.ko
+$(PKG)_TARGET_DIR:=$(KERNEL_MODULES_DIR)/fs/fuse
+$(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/fuse.ko
+
+$(PKG)_DEPENDS_ON += kernel
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_KERNEL_LAYOUT
+
+$(PKG)_CONFIGURE_OPTIONS += --enable-kernel-module
+$(PKG)_CONFIGURE_OPTIONS += --with-kernel="$(FREETZ_BASE_DIR)/$(KERNEL_SOURCE_DIR)"
+
+define $(PKG)_CUSTOM_UNPACK
+mkdir -p $($(PKG)_DIR); $(TOOLS_DIR)/gunzip -c $(1) | $(TAR) -C $($(PKG)_DIR) $(VERBOSE) -x --strip-components=2 fuse-$($(PKG)_VERSION)/kernel;
+endef
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(FUSE_KERNEL_MODULE_DIR) \
+		ARCH="$(KERNEL_ARCH)" \
+		CROSS_COMPILE="$(KERNEL_CROSS)" \
+		V=1 \
+		all
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_FILE)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(FUSE_KERNEL_MODULE_DIR) clean
+
+$(pkg)-uninstall:
+	$(RM) $(FUSE_KERNEL_MODULE_TARGET_BINARY)
+
+$(PKG_FINISH)
--- make/fuse/fuse.mk
+++ make/fuse/fuse.mk
@@ -1,15 +1,9 @@
-$(call PKG_INIT_BIN, $(if $(FREETZ_PACKAGE_FUSE_VERSION_2_7),2.7.6,2.9.7))
+$(call PKG_INIT_BIN, 2.9.7)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5_2.7.6:=319f12dffd6f7073628fefc980254f4a
-$(PKG)_SOURCE_MD5_2.9.7:=9bd4ce8184745fd3d000ca2692adacdb
-$(PKG)_SOURCE_MD5:=$($(PKG)_SOURCE_MD5_$($(PKG)_VERSION))
-# don't get confused by 2_9_4 - it's a subdir containing 2.9.4 and all older fuse versions (s. https://github.com/libfuse/libfuse/releases)
-$(PKG)_SITE:=https://github.com/libfuse/libfuse/releases/download/fuse-$($(PKG)_VERSION),https://github.com/libfuse/libfuse/releases/download/fuse_2_9_4
+$(PKG)_SOURCE_MD5:=9bd4ce8184745fd3d000ca2692adacdb
+$(PKG)_SITE:=https://github.com/libfuse/libfuse/releases/download/fuse-$($(PKG)_VERSION)
 
-$(PKG)_CONDITIONAL_PATCHES+=$(call GET_MAJOR_VERSION,$($(PKG)_VERSION))
 $(PKG)_PATCH_POST_CMDS += $(SED) -i -r -e 's| -D_FILE_OFFSET_BITS=64||g;' fuse.pc.in {example,lib,util}/Makefile.{am,in};
-
-$(PKG)_BUILD_KERNEL_MODULE:=$(strip $(FREETZ_KERNEL_VERSION_2_6_13))
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/util/fusermount
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/fusermount
@@ -18,47 +12,27 @@ $(PKG)_LIB_BINARY:=$($(PKG)_DIR)/lib/.libs/libfuse.so.$($(PKG)_VERSION)
 $(PKG)_LIB_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libfuse.so.$($(PKG)_VERSION)
 $(PKG)_LIB_TARGET_BINARY:=$($(PKG)_TARGET_LIBDIR)/libfuse.so.$($(PKG)_VERSION)
 
-$(PKG)_DEPENDS_ON += kernel
-
-$(PKG)_REBUILD_SUBOPTS += FREETZ_KERNEL_LAYOUT
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_FUSE_VERSION_2_7
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_FUSE_VERSION_2_9
-
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
-$(PKG)_CONFIGURE_OPTIONS += --disable-rpath
 $(PKG)_CONFIGURE_OPTIONS += --enable-lib
 $(PKG)_CONFIGURE_OPTIONS += --enable-util
-$(PKG)_CONFIGURE_OPTIONS += --disable-example
 $(PKG)_CONFIGURE_OPTIONS += --disable-mtab
+$(PKG)_CONFIGURE_OPTIONS += --disable-example
 $(PKG)_CONFIGURE_OPTIONS += --with-gnu-ld
-$(PKG)_CONFIGURE_OPTIONS += --with-kernel="$(FREETZ_BASE_DIR)/$(KERNEL_SOURCE_DIR)"
-
-ifeq ($($(PKG)_BUILD_KERNEL_MODULE),y)
-$(PKG)_MOD_BINARY:=$($(PKG)_DIR)/kernel/fuse.ko
-$(PKG)_MOD_TARGET_DIR:=$(KERNEL_MODULES_DIR)/fs/fuse
-$(PKG)_MOD_TARGET_BINARY:=$($(PKG)_MOD_TARGET_DIR)/fuse.ko
-$(PKG)_CONFIGURE_OPTIONS += --enable-kernel-module
-else
-$(PKG)_CONFIGURE_OPTIONS += --disable-kernel-module
-endif
+$(PKG)_CONFIGURE_OPTIONS += --disable-rpath
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
-$($(PKG)_BINARY) $($(PKG)_LIB_BINARY) $(if $($(PKG)_BUILD_KERNEL_MODULE),$($(PKG)_MOD_BINARY)): $($(PKG)_DIR)/.configured
+$($(PKG)_BINARY) $($(PKG)_LIB_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(FUSE_DIR) \
-		ARCH="$(KERNEL_ARCH)" \
-		CROSS_COMPILE="$(KERNEL_CROSS)" \
 		V=1 \
 		all
 
 $($(PKG)_LIB_STAGING_BINARY): $($(PKG)_LIB_BINARY)
 	cp $(FUSE_DIR)/fuse.pc $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/fuse.pc
 	$(SUBMAKE) -C $(FUSE_DIR)/lib \
-		ARCH="$(KERNEL_ARCH)" \
-		CROSS_COMPILE="$(KERNEL_CROSS)" \
 		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
 		install
 	$(PKG_FIX_LIBTOOL_LA) \
@@ -66,8 +40,6 @@ $($(PKG)_LIB_STAGING_BINARY): $($(PKG)_LIB_BINARY)
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libulockmgr.la \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/fuse.pc
 	$(SUBMAKE) -C $(FUSE_DIR)/include \
-		ARCH="$(KERNEL_ARCH)" \
-		CROSS_COMPILE="$(KERNEL_CROSS)" \
 		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
 		install
 
@@ -77,15 +49,9 @@ $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 $($(PKG)_LIB_TARGET_BINARY): $($(PKG)_LIB_STAGING_BINARY)
 	$(INSTALL_LIBRARY_STRIP)
 
-ifeq ($($(PKG)_BUILD_KERNEL_MODULE),y)
-$($(PKG)_MOD_TARGET_BINARY): $($(PKG)_MOD_BINARY)
-	$(INSTALL_FILE)
-endif
-
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_LIB_TARGET_BINARY) \
-		$(if $($(PKG)_BUILD_KERNEL_MODULE),$($(PKG)_MOD_TARGET_BINARY))
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_LIB_TARGET_BINARY)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(FUSE_DIR) clean
@@ -99,8 +65,7 @@ $(pkg)-clean:
 $(pkg)-uninstall:
 	$(RM) \
 		$(FUSE_TARGET_BINARY) \
-		$(FUSE_TARGET_LIBDIR)/libfuse*.so* \
-		$(if $(FUSE_BUILD_KERNEL_MODULE),$(FUSE_MOD_TARGET_BINARY))
+		$(FUSE_TARGET_LIBDIR)/libfuse*.so*
 
 $(call PKG_ADD_LIB,libfuse)
 $(PKG_FINISH)
