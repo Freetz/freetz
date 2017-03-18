30eb9acc7daacbd4fe0155e9f073060db0efe6a2
Gene Rudoy <gene@freetz.org>
2017-03-18 20:41:34 +0000

    sundtek:  * provide menuconfig option for adding sundtek streaming media server to the image
    git-svn-id: http://svn.freetz.org/trunk@14173 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/sundtek-rtspd/sundtek-rtspd.mk
@@ -0,0 +1,30 @@
+$(call PKG_INIT_BIN, 0.0)
+$(PKG)_SOURCE_DOWNLOAD_NAME:=rtspd
+$(PKG)_SOURCE:=sundtek-$($(PKG)_SOURCE_DOWNLOAD_NAME)-$(SUNDTEK_ARCH)
+$(PKG)_SITE:=http://sundtek.de/media/streamer/$(SUNDTEK_ARCH)
+
+$(PKG)_TARGET_PATH:=/usr/bin/sundtek-$($(PKG)_SOURCE_DOWNLOAD_NAME)
+
+$(if $(FREETZ_PACKAGE_SUNDTEK_RTSPD_FORCE_DOWNLOAD),.PHONY: $(DL_DIR)/$($(PKG)_SOURCE))
+
+define $(PKG)_CUSTOM_UNPACK
+	mkdir -p $($(PKG)_DIR)
+endef
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+
+$($(PKG)_DEST_DIR)$($(PKG)_TARGET_PATH): $(DL_DIR)/$($(PKG)_SOURCE)
+	$(INSTALL_BINARY_STRIP)
+	@chmod 755 $@
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_DEST_DIR)$($(PKG)_TARGET_PATH)
+
+$(pkg)-clean:
+
+$(pkg)-uninstall:
+	$(RM) $(SUNDTEK_RTSPD_DEST_DIR)$(SUNDTEK_RTSPD_TARGET_PATH)
+
+$(PKG_FINISH)
