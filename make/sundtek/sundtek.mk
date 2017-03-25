7ef4e4a4ba61570334177257bc07ce54db4da7c2
Gene Rudoy <gene@freetz.org>
2017-03-25 12:33:54 +0000

    sundtek:  * add support for automatic fetching of the latest version
    git-svn-id: http://svn.freetz.org/trunk@14203 f5190166-0702-4917-9039-51ec32eddaf5

--- make/sundtek/sundtek.mk
+++ make/sundtek/sundtek.mk
@@ -1,9 +1,15 @@
-$(call PKG_INIT_BIN,$(if $(FREETZ_PACKAGE_SUNDTEK_VERSION_2013),130210.134617,170310.204343))
+SUNDTEK_SITE:=http://www.sundtek.de/media
+SUNDTEK_INSTALLER_PATTERN:=$(SUNDTEK_SITE)/sundtek_installer_([0-9]{6}[.][0-9]{6})[.]sh
+
+define sundtek-get-latest-version
+$(shell ver=$$(wget -q $(SUNDTEK_SITE) -O - 2>/dev/null | grep -Eo '$(SUNDTEK_INSTALLER_PATTERN)' | sed -rne 's,$(SUNDTEK_INSTALLER_PATTERN),\1,p' | sort -u | tail -n 1); echo "$${ver:-FAILED_TO_DETERMINE_LATEST_VERSION}")
+endef
+
+$(call PKG_INIT_BIN,$(if $(FREETZ_PACKAGE_SUNDTEK_VERSION_LATEST),$(call sundtek-get-latest-version),$(if $(FREETZ_PACKAGE_SUNDTEK_VERSION_2013),130210.134617,170310.204343)))
 $(PKG)_SOURCE:=$(pkg)_installer_$($(PKG)_VERSION).sh
 $(PKG)_SOURCE_MD5_130210.134617:=81451e57207ad2d3e8c6e148be110040
 $(PKG)_SOURCE_MD5_170310.204343:=f3001a7a45e558aab2e98e7b3a84ff6d
 $(PKG)_SOURCE_MD5:=$($(PKG)_SOURCE_MD5_$($(PKG)_VERSION))
-$(PKG)_SITE:=http://www.sundtek.de/media
 
 $(PKG)_ARCH:=$(or $(call qstrip,$(FREETZ_PACKAGE_SUNDTEK_ARCH)),undefined)
 
@@ -19,6 +25,7 @@ $(PKG)_LIBS_TARGET_DIR     := $($(PKG)_LIBS:lib%=$($(PKG)_DEST_DIR)/usr/lib/libs
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_SUNDTEK_VERSION_2013
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_SUNDTEK_VERSION_2017
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_SUNDTEK_VERSION_LATEST
 
 $(PKG)_BUILD_PREREQ += dd
 
