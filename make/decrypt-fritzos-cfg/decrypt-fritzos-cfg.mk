048b4df51f8316f338f4fd7922541c6a1808bf5f
Gene Rudoy <gene@freetz.org>
2017-06-04 20:05:11 +0000

    decrypt-fritzos-cfg (Freetz name of [https://github.com/PeterPawn/decode_passwords decode_passwords]):  * integrate [https://github.com/PeterPawn/decode_passwords/commits/master?after=4fff7910e9585d62bd5394261af102fd7bb22a89+0 the latest changes from upstream], switch to using the binary version of the [https://github.com/PeterPawn/decode_passwords/blob/master/src/decoder.c decoder]  * closes #2717
    git-svn-id: http://svn.freetz.org/trunk@14294 f5190166-0702-4917-9039-51ec32eddaf5

--- make/decrypt-fritzos-cfg/decrypt-fritzos-cfg.mk
+++ make/decrypt-fritzos-cfg/decrypt-fritzos-cfg.mk
@@ -1,32 +1,54 @@
-$(call PKG_INIT_BIN,v0.2)
+$(call PKG_INIT_BIN, 4fff7910e9)
 $(PKG)_ORIG_NAME:=decode_passwords
 $(PKG)_SOURCE:=$(subst -,_,$(pkg))-$($(PKG)_VERSION).tar.xz
 $(PKG)_SITE:=git@https://github.com/PeterPawn/$($(PKG)_ORIG_NAME).git
 $(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/$(subst -,_,$(pkg))-$($(PKG)_VERSION)
 
-$(PKG)_PATCH_POST_CMDS += $(SED) -i -e 's,$($(PKG)_ORIG_NAME),$(pkg),g' $($(PKG)_ORIG_NAME);
+$(PKG)_DEPENDS_ON += openssl
 
-$(PKG)_BINARY := $($(PKG)_DIR)/$($(PKG)_ORIG_NAME)
-$(PKG)_TARGET_BINARY := $($(PKG)_DEST_DIR)/usr/bin/$(pkg)
+$(PKG)_BINARY := $($(PKG)_DIR)/src/decoder
+$(PKG)_TARGET_BINARY := $($(PKG)_DEST_DIR)/usr/bin/decoder
+
+$(PKG)_SYMLINKS_ALL := decrypt-fritzos-cfg
+$(PKG)_SYMLINKS_ALL += decode_secrets decode_export decode_secret user_password device_password password_from_device
+$(PKG)_SYMLINKS_ALL += hexdec hexenc b64dec b64enc b32dec b32enc
+
+$(PKG)_SYMLINKS := $(call PKG_SELECTED_SUBOPTIONS,$($(PKG)_SYMLINKS_ALL))
+$(PKG)_SYMLINKS_TARGET_DIR := $($(PKG)_SYMLINKS:%=$($(PKG)_DEST_DIR)/usr/bin/%)
+
+$(PKG)_EXCLUDED += $(patsubst %,$($(PKG)_DEST_DIR)/usr/bin/%,$(filter-out $($(PKG)_SYMLINKS),$($(PKG)_SYMLINKS_ALL)))
+
+$(PKG)_LINK_MODE := $(call PKG_SELECTED_SUBOPTIONS,ALL_DYN CRYPTO_STAT ALL_STAT)
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_DECRYPT_FRITZOS_CFG_ALL_DYN
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_DECRYPT_FRITZOS_CFG_CRYPTO_STAT
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_DECRYPT_FRITZOS_CFG_ALL_STAT
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_NOP)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	@chmod 755 $@
-	@touch -c $@
+	$(SUBMAKE) -C $(DECRYPT_FRITZOS_CFG_DIR)/src -f Makefile.freetz \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)" \
+		OPT="" \
+		LINK_MODE=$(DECRYPT_FRITZOS_CFG_LINK_MODE)
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
-	$(INSTALL_FILE)
+	$(INSTALL_BINARY_STRIP)
+
+$($(PKG)_SYMLINKS_TARGET_DIR): $($(PKG)_TARGET_BINARY)
+	@ln -sf $(notdir $<) $@
 
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_SYMLINKS_TARGET_DIR)
 
 $(pkg)-clean:
+	$(SUBMAKE) -C $(DECRYPT_FRITZOS_CFG_DIR)/src -f Makefile.freetz clean
 
 $(pkg)-uninstall:
-	$(RM) $(DECRYPT_FRITZOS_CFG_TARGET_BINARY)
+	$(RM) $(DECRYPT_FRITZOS_CFG_TARGET_BINARY) $(DECRYPT_FRITZOS_CFG_SYMLINKS_ALL:%=$(DECRYPT_FRITZOS_CFG_DEST_DIR)/usr/bin/%)
 
 $(PKG_FINISH)
