245aac90d991841f7e26d7b9c6995c35155d3a6a
Gene Rudoy <gene@freetz.org>
2017-06-21 20:33:38 +0000

    polarssl13:  * bump version to [https://tls.mbed.org/tech-updates/releases/mbedtls-2.5.1-2.1.8-and-1.3.20-released 1.3.20]
    git-svn-id: http://svn.freetz.org/trunk@14316 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/polarssl13/polarssl13.mk
+++ make/libs/polarssl13/polarssl13.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_LIB, 1.3.19)
+$(call PKG_INIT_LIB, 1.3.20)
 $(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/mbedtls-$($(PKG)_VERSION)
 $(PKG)_SOURCE:=mbedtls-$($(PKG)_VERSION)-gpl.tgz
-$(PKG)_SOURCE_SHA1:=229014b979980cf5bdea8aa00578ae2ddeb4a499
+$(PKG)_SOURCE_SHA1:=973bc304e6e85c9beaa354d3881a84f7cf0fc9e1
 $(PKG)_SITE:=http://polarssl.org/code/releases
 
 $(PKG)_LIBNAME:=lib$(pkg).so.$($(PKG)_VERSION)
