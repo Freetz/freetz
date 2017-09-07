410f6a0b1bbb407f7036eb604fc908654fed93ec
Gene Rudoy <gene@freetz.org>
2017-09-07 22:25:32 +0000

    decrypt-fritzos-cfg (Freetz name of [https://github.com/PeterPawn/decoder decoder]):  * bump version to the latest git revision
    git-svn-id: http://svn.freetz.org/trunk@14401 f5190166-0702-4917-9039-51ec32eddaf5

--- make/decrypt-fritzos-cfg/decrypt-fritzos-cfg.mk
+++ make/decrypt-fritzos-cfg/decrypt-fritzos-cfg.mk
@@ -1,4 +1,4 @@
-$(call PKG_INIT_BIN, 17fef6c015)
+$(call PKG_INIT_BIN, 428de17b93)
 $(PKG)_SOURCE:=$(subst -,_,$(pkg))-$($(PKG)_VERSION).tar.xz
 $(PKG)_SITE:=git@https://github.com/PeterPawn/decoder.git
 $(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/$(subst -,_,$(pkg))-$($(PKG)_VERSION)
