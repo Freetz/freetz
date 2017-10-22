6273bf474cda38eba96a67e100dc8af9b294df96
f666 <>
2017-10-22 15:37:13 +0200

    add kernel and uclibc config files for 6490, extend diff-configs.sh tool

--- make/linux/configs/diff-configs.sh
+++ make/linux/configs/diff-configs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in freetz/config-{ar9,ar934x,ar10,iks,qca956x,ur8,vr9}-*; do
+for i in freetz/config-{ar9,ar934x,ar10,iks,puma6,qca956x,ur8,vr9}-*; do
 	i=$(basename $i)
 	sed -r -e 's,=n$,=m,' "avm/$i" | diff -du --label "avm/$i" --label "freetz/$i" - freetz/$i > $i.diff
 done
