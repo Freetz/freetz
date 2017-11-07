fbf8841c9dc1a029e4309629e8b4a66848a52a3e
Gene Rudoy <gene@freetz.org>
2017-11-07 20:24:12 +0000

    expat:  * bump version to [https://raw.githubusercontent.com/libexpat/libexpat/R_2_2_5/expat/Changes 2.2.5]
    git-svn-id: http://svn.freetz.org/trunk@14516 f5190166-0702-4917-9039-51ec32eddaf5

--- make/libs/expat/expat.mk
+++ make/libs/expat/expat.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_LIB, 2.2.4)
-$(PKG)_LIB_VERSION:=1.6.6
+$(call PKG_INIT_LIB, 2.2.5)
+$(PKG)_LIB_VERSION:=1.6.7
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_SOURCE_MD5:=6e3980aba29a224a9f478d88ac7ec207
+$(PKG)_SOURCE_MD5:=789e297f547980fc9ecc036f9a070d49
 $(PKG)_SITE:=@SF/expat
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/lib/.libs/libexpat.so.$($(PKG)_LIB_VERSION)
