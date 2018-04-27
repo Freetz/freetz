a8f6e3fbc7f6d01bb822ea75c48edba5e61824d0
Gene Rudoy <gene@freetz.org>
2018-04-27 19:29:50 +0000

    getdns: add new package - getdns/stubby https://dnsprivacy.org/wiki/display/DP/DNS+Privacy+Daemon+-+Stubby
    by zyrill (https://github.com/zyrill)
    
    closes https://github.com/Freetz/freetz/pull/24
    
    
    git-svn-id: http://svn.freetz.org/trunk@14635 f5190166-0702-4917-9039-51ec32eddaf5

---
+++ make/getdns/getdns.mk
@@ -0,0 +1,43 @@
+$(call PKG_INIT_BIN, 1.4.0)
+$(PKG)_SOURCE:=getdns-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_SHA256:=de360cd554fdec4bae3f5afbb36145872b8ff7306ded5deb0905442c4909f7b3
+$(PKG)_SITE:=https://getdnsapi.net/releases/getdns-1-4-0
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/stubby
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/stubby
+
+$(PKG)_DEPENDS_ON += openssl
+$(PKG)_DEPENDS_ON += yaml
+
+$(PKG)_CONFIGURE_OPTIONS += --without-libidn
+$(PKG)_CONFIGURE_OPTIONS += --without-libidn2
+$(PKG)_CONFIGURE_OPTIONS += --enable-stub-only
+$(PKG)_CONFIGURE_OPTIONS += --with-stubby
+$(PKG)_CONFIGURE_OPTIONS += --without-getdns_query
+$(PKG)_CONFIGURE_OPTIONS += --without-getdns_server_mon
+$(PKG)_CONFIGURE_OPTIONS += --enable-static
+$(PKG)_CONFIGURE_OPTIONS += --disable-shared
+$(PKG)_CONFIGURE_OPTIONS += --disable-rpath
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(GETDNS_DIR)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(GETDNS_DIR) clean
+	$(RM) $(GETDNS_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(GETDNS_TARGET_BINARY)
+
+$(PKG_FINISH)
