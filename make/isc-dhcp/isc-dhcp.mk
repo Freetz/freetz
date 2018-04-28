71318fcb597400df7392a4d51f36c3db13ea0a4c
Gene Rudoy <gene@freetz.org>
2018-04-28 15:04:18 +0000

    isc-dhcp: bump version to 4.3.6-P1
    https://kb.isc.org/article/AA-01570/82/DHCP-4.3.6-P1-Release-Notes.html
    
    refs https://github.com/Freetz/freetz/pull/28
    
    
    git-svn-id: http://svn.freetz.org/trunk@14639 f5190166-0702-4917-9039-51ec32eddaf5

--- make/isc-dhcp/isc-dhcp.mk
+++ make/isc-dhcp/isc-dhcp.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 4.3.3-P1)
+$(call PKG_INIT_BIN, 4.3.6-P1)
 $(PKG)_SOURCE:=dhcp-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_SHA256:=c11e896dffa1bfbc49462965d3f6dec45534e34068603546d9a236f2aa669921
+$(PKG)_SOURCE_SHA256:=
 $(PKG)_SITE:=http://ftp.isc.org/isc/dhcp/$($(PKG)_VERSION)
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/server/dhcpd
@@ -8,7 +8,6 @@ $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/isc-dhcpd
 
 $(PKG)_DEPENDS_ON += bind
 
-$(PKG)_CONFIGURE_PRE_CMDS += $(AUTORECONF)
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_ADD_EXTRA_FLAGS,(C|LD)FLAGS)
 
@@ -18,6 +17,7 @@ $(PKG)_MAKE_OPTIONS += EXTRA_CFLAGS="-ffunction-sections -fdata-sections"
 $(PKG)_MAKE_OPTIONS += EXTRA_LDFLAGS="-Wl,--gc-sections"
 
 $(PKG)_CONFIGURE_ENV += ac_cv_file__dev_random=yes
+$(PKG)_CONFIGURE_OPTIONS += --with-randomdev="/dev/random"
 $(PKG)_CONFIGURE_OPTIONS += --with-libbind="$(BIND_EXPORT_LIB_DIR)/usr"
 
 $(PKG_SOURCE_DOWNLOAD)
