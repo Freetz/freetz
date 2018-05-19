7a6ab59264e0a98c5b570ffb53a9af84f1b741e7
Gene Rudoy <gene@freetz.org>
2018-05-19 11:59:49 +0000

    Fritz!OS 7.0X: fix problem reported in https://www.ip-phone-forum.de/threads/build-fehler-fb-7490-v7.299606/
    git-svn-id: http://svn.freetz.org/trunk@14688 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/110-add_system_status.sh
+++ patches/scripts/110-add_system_status.sh
@@ -1,4 +1,8 @@
 [ "$FREETZ_MODIFY_AVM_VERSION" == "y" ] || return 0
 
 echo1 "adding Freetz version to AVM version data"
-modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_COND_DIR}/system_status.patch"
+if [ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ]; then
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_COND_DIR}/system_status-07_0X_MIN.patch"
+else
+	modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_COND_DIR}/system_status-04_XX_MIN.patch"
+fi
