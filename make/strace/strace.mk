3fa95faf7776317bd1dce96f9f95e41aff393a1e
Gene Rudoy <gene@freetz.org>
2018-06-07 16:26:06 +0000

    strace: bump version to 4.9 [1] (older kernel versions only)
    This is the latest version of strace supporting kernel 2.6.13. Strarting with 4.10 at least 2.6.15 is required (s. [2]).
    
    refs #2958
    
    [1] https://raw.githubusercontent.com/strace/strace/v4.9/NEWS
    [2] https://raw.githubusercontent.com/strace/strace/v4.10/NEWS
    
    
    
    git-svn-id: http://svn.freetz.org/trunk@14717 f5190166-0702-4917-9039-51ec32eddaf5

--- make/strace/strace.mk
+++ make/strace/strace.mk
@@ -2,9 +2,9 @@
 # recent versions of strace do not support older kernel versions anymore,
 # that's the reason we have to stick to an older strace version for kernel versions 2.6.13 & 2.6.19
 #
-$(call PKG_INIT_BIN, $(if $(FREETZ_KERNEL_VERSION_2_6_19_MAX),4.8,4.22))
+$(call PKG_INIT_BIN, $(if $(FREETZ_KERNEL_VERSION_2_6_19_MAX),4.9,4.22))
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
-$(PKG)_SOURCE_MD5_4.8 :=c575ef43829586801f514fd91bfe7575
+$(PKG)_SOURCE_MD5_4.9 :=885eafadb10f6c60464a266d3929a2a4
 $(PKG)_SOURCE_MD5_4.22:=7a2a7d7715da6e6834bc65bd09bace1c
 $(PKG)_SOURCE_MD5:=$($(PKG)_SOURCE_MD5_$($(PKG)_VERSION))
 $(PKG)_SITE:=@SF/$(pkg),https://strace.io/files/$($(PKG)_VERSION)
@@ -22,7 +22,7 @@ $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(STRACE_DIR)
+	$(SUBMAKE1) -C $(STRACE_DIR)
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
