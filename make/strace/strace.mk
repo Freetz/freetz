2ec2c034814382f5ec0343ef52d394affa9e0aa3
Gene Rudoy <gene@freetz.org>
2018-06-07 17:14:25 +0000

    strace: use strace version 4.10 for kernel-2.6.19
    refs #2958
    
    
    git-svn-id: http://svn.freetz.org/trunk@14718 f5190166-0702-4917-9039-51ec32eddaf5

--- make/strace/strace.mk
+++ make/strace/strace.mk
@@ -2,9 +2,13 @@
 # recent versions of strace do not support older kernel versions anymore,
 # that's the reason we have to stick to an older strace version for kernel versions 2.6.13 & 2.6.19
 #
-$(call PKG_INIT_BIN, $(if $(FREETZ_KERNEL_VERSION_2_6_19_MAX),4.9,4.22))
+STRACE_VERSION__KERNEL_2.6.13:=4.9
+STRACE_VERSION__KERNEL_2.6.19:=4.10
+
+$(call PKG_INIT_BIN, $(or $(strip $(STRACE_VERSION__KERNEL_$(call qstrip,$(FREETZ_KERNEL_VERSION_MAJOR)))),4.22))
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
 $(PKG)_SOURCE_MD5_4.9 :=885eafadb10f6c60464a266d3929a2a4
+$(PKG)_SOURCE_MD5_4.10:=107a5be455493861189e9b57a3a51912
 $(PKG)_SOURCE_MD5_4.22:=7a2a7d7715da6e6834bc65bd09bace1c
 $(PKG)_SOURCE_MD5:=$($(PKG)_SOURCE_MD5_$($(PKG)_VERSION))
 $(PKG)_SITE:=@SF/$(pkg),https://strace.io/files/$($(PKG)_VERSION)
