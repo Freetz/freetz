95dae3f4ba19285c41fb979f0d909b384f4def89
Gene Rudoy <gene@freetz.org>
2018-06-07 14:34:24 +0000

    strace: bump version to 4.22 (by f_666)
    https://raw.githubusercontent.com/strace/strace/v4.22/NEWS
    
    closes #2958
    
    
    git-svn-id: http://svn.freetz.org/trunk@14716 f5190166-0702-4917-9039-51ec32eddaf5

--- make/strace/strace.mk
+++ make/strace/strace.mk
@@ -1,7 +1,15 @@
-$(call PKG_INIT_BIN, 4.8)
+#
+# recent versions of strace do not support older kernel versions anymore,
+# that's the reason we have to stick to an older strace version for kernel versions 2.6.13 & 2.6.19
+#
+$(call PKG_INIT_BIN, $(if $(FREETZ_KERNEL_VERSION_2_6_19_MAX),4.8,4.22))
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
-$(PKG)_SOURCE_MD5:=c575ef43829586801f514fd91bfe7575
-$(PKG)_SITE:=@SF/$(pkg)
+$(PKG)_SOURCE_MD5_4.8 :=c575ef43829586801f514fd91bfe7575
+$(PKG)_SOURCE_MD5_4.22:=7a2a7d7715da6e6834bc65bd09bace1c
+$(PKG)_SOURCE_MD5:=$($(PKG)_SOURCE_MD5_$($(PKG)_VERSION))
+$(PKG)_SITE:=@SF/$(pkg),https://strace.io/files/$($(PKG)_VERSION)
+
+$(PKG)_CONDITIONAL_PATCHES+=$($(PKG)_VERSION)
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/strace
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/strace
