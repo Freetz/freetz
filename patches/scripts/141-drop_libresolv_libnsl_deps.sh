19ebb8789f34de008d089aaeff239ae7c4b941be
Whoopie <whoopie79@gmx.net>
2018-08-13 18:50:33 +0000

    drop libresolv and libnsl for all boxes with FritzOS 7.0 and USB host.
    git-svn-id: http://svn.freetz.org/trunk@14813 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/141-drop_libresolv_libnsl_deps.sh
+++ patches/scripts/141-drop_libresolv_libnsl_deps.sh
@@ -1,6 +1,5 @@
-isFreetzType 7590 || return 0
-
-if [ "$FREETZ_AVM_UCLIBC_1_0_14" = "y" ]; then
+if [ "$FREETZ_AVM_VERSION_07_0X_MIN" = "y" -a "$FREETZ_AVM_HAS_USB_HOST" = "y" ]; then
+	echo1 "dropping libresolv and libnsl dependency from binaries/libraries"
 	${TOOLS_DIR}/patchelf --remove-needed libresolv.so.1 ${FILESYSTEM_MOD_DIR}/lib/libsamba.so
 	${TOOLS_DIR}/patchelf --remove-needed libresolv.so.1 ${FILESYSTEM_MOD_DIR}/sbin/ftpd
 	${TOOLS_DIR}/patchelf --remove-needed libnsl.so.1 ${FILESYSTEM_MOD_DIR}/sbin/ftpd
