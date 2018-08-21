b7b57f3c7e4a7e50d40155570dc59034b22b3c85
Gene Rudoy <gene@freetz.org>
2018-08-21 22:44:56 +0000

    support hiding "vom Hersteller nicht unterstützte Änderungen" also in "no freetz"-mode
    git-svn-id: http://svn.freetz.org/trunk@14829 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/195-patch_webmenu_signed.sh
+++ patches/scripts/195-patch_webmenu_signed.sh
@@ -1,22 +1,26 @@
 [ "$FREETZ_PATCH_SIGNED" == "y" ] || return 0
 echo1 "applying webmenu signed patch"
 
-for file in /usr/www/all/home/home.js /usr/www/avme/home/home.js; do
-	[ ! -e ${FILESYSTEM_MOD_DIR}${file} ] && continue
-	modsed 's/data[.]fritzos[.]FirmwareSigned/true/g' ${FILESYSTEM_MOD_DIR}${file}
-done
+for oem in $(supported_brandings) all; do
+	[ -d "${FILESYSTEM_MOD_DIR}/usr/www/${oem}" -a ! -L "${FILESYSTEM_MOD_DIR}/usr/www/${oem}" ] || continue
 
-for file in /usr/www/all/home/home.lua /usr/www/avme/home/home.lua; do
-	[ ! -e ${FILESYSTEM_MOD_DIR}${file} ] && continue
-	modsed 's/^g_coninf_data.FirmwareSigned = .*/g_coninf_data.FirmwareSigned = "1"/g' ${FILESYSTEM_MOD_DIR}${file}
-done
+	for file in /usr/www/${oem}/home/home.js; do
+		[ ! -e ${FILESYSTEM_MOD_DIR}${file} ] && continue
+		modsed 's/data[.]fritzos[.]FirmwareSigned/true/g' ${FILESYSTEM_MOD_DIR}${file}
+	done
 
-for file in /usr/www/all/html/de/home/home.js /usr/www/avme/html/de/home/home.js /usr/www/avme/en/html/en/home/home.js; do
-	[ ! -e ${FILESYSTEM_MOD_DIR}${file} ] && continue
-	modsed 's/^.*var signed.*/\tvar signed = 1;/g' ${FILESYSTEM_MOD_DIR}${file}
-done
+	for file in /usr/www/${oem}/home/home.lua; do
+		[ ! -e ${FILESYSTEM_MOD_DIR}${file} ] && continue
+		modsed 's/^g_coninf_data[.]FirmwareSigned = .*/g_coninf_data.FirmwareSigned = "1"/g' ${FILESYSTEM_MOD_DIR}${file}
+	done
 
-for file in "${HTML_LANG_MOD_DIR##${FILESYSTEM_MOD_DIR}}/system/diagnosis.lua" /usr/lua/retrieve_data.lua; do
-	[ ! -e ${FILESYSTEM_MOD_DIR}${file} ] && continue
-	modsed 's,box[.]query("[^"]*/signed_firmware"),"1",g' ${FILESYSTEM_MOD_DIR}${file}
+	for file in /usr/www/${oem}/html/de/home/home.js /usr/www/${oem}/en/html/en/home/home.js; do
+		[ ! -e ${FILESYSTEM_MOD_DIR}${file} ] && continue
+		modsed 's/^.*var signed.*/\tvar signed = 1;/g' ${FILESYSTEM_MOD_DIR}${file}
+	done
+
+	for file in /usr/www/${oem}/system/diagnosis.lua /usr/lua/retrieve_data.lua; do
+		[ ! -e ${FILESYSTEM_MOD_DIR}${file} ] && continue
+		modsed 's,box[.]query("[^"]*/signed_firmware"),"1",g' ${FILESYSTEM_MOD_DIR}${file}
+	done
 done
