9b69cc8b1c53cb4d1f16c2d46b37a34ea4c6d672
Gene Rudoy <gene@freetz.org>
2018-08-22 06:07:35 +0000

    enforce urlader settings patch: properly escape single quotes
    git-svn-id: http://svn.freetz.org/trunk@14830 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/101-enforce_urlader-settings.sh
+++ patches/scripts/101-enforce_urlader-settings.sh
@@ -9,18 +9,18 @@ mount -t proc proc /proc
 EOF
 
 	if [ -n "$FREETZ_ENFORCE_URLADER_SETTING_FIRMWARE_VERSION" ]; then
-		echo 'firmware_version="$(echo $(grep firmware_version /proc/sys/urlader/environment) | cut -d' ' -f2)"'
+		echo 'firmware_version="$(echo $(grep firmware_version /proc/sys/urlader/environment) | cut -d'"' '"' -f2)"'
 		echo 'if [ "$firmware_version" != "'$FREETZ_ENFORCE_URLADER_SETTING_FIRMWARE_VERSION'" ]; then'
 		echo 'echo firmware_version '$FREETZ_ENFORCE_URLADER_SETTING_FIRMWARE_VERSION' > /proc/sys/urlader/environment'
 #		echo 'echo '$FREETZ_ENFORCE_URLADER_SETTING_FIRMWARE_VERSION' > /proc/sys/urlader/firmware_version'
 		echo 'fi'
 	fi >> "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
 	if [ -n "$FREETZ_ENFORCE_URLADER_SETTING_MY_IPADDRESS" ]; then
-		echo 'my_ipaddress="$(echo $(grep my_ipaddress /proc/sys/urlader/environment) | cut -d' ' -f2)"'
+		echo 'my_ipaddress="$(echo $(grep my_ipaddress /proc/sys/urlader/environment) | cut -d'"' '"' -f2)"'
 		echo '[ "$my_ipaddress" = '$FREETZ_ENFORCE_URLADER_SETTING_MY_IPADDRESS' ] || echo my_ipaddress '$FREETZ_ENFORCE_URLADER_SETTING_MY_IPADDRESS' > /proc/sys/urlader/environment'
 	fi >> "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
 	if [ -n "$FREETZ_ENFORCE_URLADER_SETTING_PRODUCTID" ]; then
-		echo 'ProductID="$(echo $(grep ProductID /proc/sys/urlader/environment) | cut -d' ' -f2)"'
+		echo 'ProductID="$(echo $(grep ProductID /proc/sys/urlader/environment) | cut -d'"' '"' -f2)"'
 		echo '[ "$ProductID" = '$FREETZ_ENFORCE_URLADER_SETTING_PRODUCTID' ] || echo ProductID '$FREETZ_ENFORCE_URLADER_SETTING_PRODUCTID' > /proc/sys/urlader/environment'
 	fi >> "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
 
