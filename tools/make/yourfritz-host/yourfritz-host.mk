f154c8f6ae08ef23842b07921391c3352c39db81
Gene Rudoy <gene@freetz.org>
2018-11-21 21:02:53 +0000

    yourfritz-host: bump version to the latest git revision
    git-svn-id: http://svn.freetz.org/trunk@14999 f5190166-0702-4917-9039-51ec32eddaf5

--- tools/make/yourfritz-host/yourfritz-host.mk
+++ tools/make/yourfritz-host/yourfritz-host.mk
@@ -1,4 +1,4 @@
-YOURFRITZ_HOST_VERSION:=6aba4a3b5f
+YOURFRITZ_HOST_VERSION:=eaa8e3b2ba
 YOURFRITZ_HOST_SOURCE:=yourfritz-$(YOURFRITZ_HOST_VERSION).tar.xz
 YOURFRITZ_HOST_SITE:=git_no_submodules@https://github.com/PeterPawn/YourFritz.git
 
