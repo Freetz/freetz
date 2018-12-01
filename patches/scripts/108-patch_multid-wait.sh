3c9794f83ad677206c230961b0c3152b3584377e
Gene Rudoy <gene@freetz.org>
2018-12-01 16:11:24 +0000

    6820/1750: add menuconfig support for Fritz!OS-7.01
    compile-tested only
    
    Note: kernel-4.4.x sources published by AVM so far are for ARM boxes
          and do not contain any MIPS related patches =>
          kernel .config used by Freetz is a just some dummy one, almost
          all hardware related symbols in it are wrong
    
    
    git-svn-id: http://svn.freetz.org/trunk@15005 f5190166-0702-4917-9039-51ec32eddaf5

--- patches/scripts/108-patch_multid-wait.sh
+++ patches/scripts/108-patch_multid-wait.sh
@@ -1,5 +1,9 @@
-echo1 "applying multid-wait patch"
-modsed \
-  's@\(eval.*multid $.*PARAM\)@\1\nlocal countdown=10; while [ $((countdown--)) -gt 0 ] \&\& ! ifconfig eth0 >/dev/null 2>\&1; do sleep 1; done@g' \
-  "$FILESYSTEM_MOD_DIR/etc/init.d/rc.net" \
-  'local countdown=10'
+if [ -e "$FILESYSTEM_MOD_DIR/sbin/multid" ]; then
+	echo1 "applying multid-wait patch"
+	modsed \
+	  's@\(eval.*multid $.*PARAM\)@\1\nlocal countdown=10; while [ $((countdown--)) -gt 0 ] \&\& ! ifconfig eth0 >/dev/null 2>\&1; do sleep 1; done@g' \
+	  "$FILESYSTEM_MOD_DIR/etc/init.d/rc.net" \
+	  'local countdown=10'
+else
+	echo1 "no multid found, skipping multid-wait patch"
+fi
