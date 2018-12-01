2f4ce60638cc2edc76feb78b4f87017a5bea10bd
Gene Rudoy <gene@freetz.org>
2018-12-01 20:06:12 +0000

    diff-configs: add missing ipq40xx
    git-svn-id: http://svn.freetz.org/trunk@15007 f5190166-0702-4917-9039-51ec32eddaf5

--- make/linux/configs/diff-configs.sh
+++ make/linux/configs/diff-configs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in freetz/config-{ar9,ar934x,ar10,iks,puma6.x86,qca955x,qca956x,ur8,vr9}-*; do
+for i in freetz/config-{ar9,ar934x,ar10,iks,ipq40xx,puma6.x86,qca955x,qca956x,ur8,vr9}-*; do
 	i=$(basename $i)
 	sed -r -e 's,=n$,=m,' "avm/$i" | diff -du --label "avm/$i" --label "freetz/$i" - freetz/$i > $i.diff
 done
