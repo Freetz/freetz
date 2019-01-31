9480941b48fa0f16fe1702e2106e7b3b4df344da
fda77 <fda77@users.noreply.github.com>
2019-01-31 23:57:41 +0100

    webif: remove dns option, this could be change since a long time by avm webif

--- make/mod/files/root/usr/lib/cgi-bin/conf.avm/20-multid.sh
+++
@@ -1,5 +0,0 @@
-sec_begin 'multid'
-
-cgi_print_textline_p "multid_dns" "$AVM_MULTID_DNS" 55/255 "$(lang de:"Statt den DNS-Servern des Providers diese nutzen (durch Leerzeichen getrennt)" en:"Use these dns-servers instead of the automatically assigned (separated by space)"):<br>"
-
-sec_end
