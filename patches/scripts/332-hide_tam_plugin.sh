38d5786fcdf5e9c9b4817907b426f3840375ed4f
fda77 <fda77@users.noreply.github.com>
2019-02-04 16:49:56 +0100

    new patch: hide tam plugin

---
+++ patches/scripts/332-hide_tam_plugin.sh
@@ -0,0 +1,16 @@
+[ "$FREETZ_AVMPLUGINS_INTEGRATE" == "y" ] || return 0
+[ "$FREETZ_AVM_HAS_PLUGIN_TAM" != "y" ] && return 0
+[ "$FREETZ_AVMPLUGINS_TAM" == "y" ] && return 0
+echo1 "hiding TAM plugin"
+
+
+# patcht Telefonie > Anrufbeantworter
+modsed \
+  's/pageData\["tam"\] /&and nil /g' \
+  "${HTML_LANG_MOD_DIR}/menus/menu_data.lua"
+	
+# patcht Übersicht > Anrufbeantworter
+modsed \
+  '/, create_Tam(/d' \
+  "${HTML_LANG_MOD_DIR}/home/home.js"
+
