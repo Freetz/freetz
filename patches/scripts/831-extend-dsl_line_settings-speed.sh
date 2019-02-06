262d8a9ee9dc8c05dda60974b93ce531c91194f2
fda77 <fda77@users.noreply.github.com>
2019-02-06 20:20:45 +0100

    make dsl+counter patches optional and only for dsl devices

--- patches/scripts/831-extend-dsl_line_settings-speed.sh
+++ patches/scripts/831-extend-dsl_line_settings-speed.sh
@@ -1,6 +1,17 @@
-[ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ] || return 0;
+[ "$FREETZ_MODIFY_DSL_SETTINGS" == "y" ] || return 0;
 
-echo1 "extending dsl settings"
-modsed 's/{/ {\n/g;s/}/\n}\n/g;s/;/;\n/g' "${HTML_LANG_MOD_DIR}/css/default/dsl_line_settings.css"
-modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_COND_DIR}/831-extend-dsl_line_settings-speed.patch"
+echo1 "patching dsl settings"
+
+if [ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ] ; then
+	PVER="07_0X"
+elif [ "$FREETZ_AVM_VERSION_06_8X_MIN" == "y" ] ; then
+	PVER="06_8X"
+elif [ "$FREETZ_AVM_VERSION_06_5X_MIN" == "y" ] ; then
+	PVER="06_5X"
+fi
+
+# allow applying patch
+[ "$PVER" == "07_0X" ] && modsed 's/{/ {\n/g;s/}/\n}\n/g;s/;/;\n/g' "${HTML_LANG_MOD_DIR}/css/default/dsl_line_settings.css"
+
+modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_COND_DIR}/831-extend-dsl_line_settings-speed_${PVER}.patch"
 
---
+++ patches/scripts/832-hide_dsl_overview-hint.sh
@@ -0,0 +1,14 @@
+[ "$FREETZ_MODIFY_DSL_WARNING" == "y" ] || return 0;
+
+echo1 "patching dsl overview"
+
+if [ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ] ; then
+	PVER="07_0X"
+elif [ "$FREETZ_AVM_VERSION_06_8X_MIN" == "y" ] ; then
+	PVER="06_8X"
+elif [ "$FREETZ_AVM_VERSION_06_5X_MIN" == "y" ] ; then
+	PVER="06_5X"
+fi
+
+modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_COND_DIR}/832-hide_dsl_overview-hint_${PVER}.patch"
+
