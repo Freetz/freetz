0b1278bb7522b22a42c65e7ac5ca50b3b26739e5
fda77 <fda77@users.noreply.github.com>
2019-02-09 19:59:32 +0100

    remove fhem: add cp210x

--- patches/scripts/225-remove_fhem.sh
+++ patches/scripts/225-remove_fhem.sh
@@ -4,4 +4,5 @@ echo1 "removing fhem"
 rm_files \
   ${FILESYSTEM_MOD_DIR}/etc/hotplug/udev-fhem-usb \
   ${MODULES_DIR}/kernel/drivers/usb/class/cdc-acm.ko \
+  ${MODULES_DIR}/kernel/drivers/usb/serial/cp210x.ko \
   ${MODULES_DIR}/kernel/drivers/usb/serial/ftdi_sio.ko
