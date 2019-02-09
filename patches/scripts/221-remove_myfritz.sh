41f2a288ce650889ffa1fd94a96ca92528c1c0dc
fda77 <fda77@users.noreply.github.com>
2019-02-09 22:16:50 +0100

    fix remove myfritz

--- patches/scripts/221-remove_myfritz.sh
+++ patches/scripts/221-remove_myfritz.sh
@@ -5,7 +5,13 @@ rm -rf "${FILESYSTEM_MOD_DIR}/usr/www.myfritz"
 ln -s www "${FILESYSTEM_MOD_DIR}/usr/www.myfritz"
 
 rm_files "${HTML_LANG_MOD_DIR}/internet/myfritz*.lua"
+[ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ] && rm_files "${FILESYSTEM_MOD_DIR}/bin/letsencrypt"
 
+#mui
+# entfernt Internet > Freigaben > Tab: Speicher
+modsed \
+  's/pageData\["shareUsb"\] =/& false and/' \
+  "${HTML_LANG_MOD_DIR}/menus/menu_data.lua"
 #lua
 homelua_disable_wrapper myfritz
 quickstart_remove myfritz
