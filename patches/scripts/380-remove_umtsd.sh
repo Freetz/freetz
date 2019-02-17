25d106d6f070e392c11c382b6b98b09cabe27d28
fda77 <fda77@users.noreply.github.com>
2019-02-17 22:09:17 +0100

    fix remove umtsd

--- patches/scripts/380-remove_umtsd.sh
+++ patches/scripts/380-remove_umtsd.sh
@@ -18,13 +18,16 @@ rm_files \
   "${FILESYSTEM_MOD_DIR}/usr/bin/csvd" \
   "${FILESYSTEM_MOD_DIR}/usr/www/all/assis/internet_umts.lua"
 
-[ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ] && rm_files \
-  "${FILESYSTEM_MOD_DIR}/bin/mobiled" \
-  "${FILESYSTEM_MOD_DIR}/etc/hotplug/udev-mobiled" \
-  "${FILESYSTEM_MOD_DIR}/usr/share/ctlmgr/libmobiled.so" \
-  "${FILESYSTEM_MOD_DIR}/usr/share/configd/C20_mobiled.so"
+if [ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ]; then
+	rm_files \
+	  "${FILESYSTEM_MOD_DIR}/bin/mobiled" \
+	  "${FILESYSTEM_MOD_DIR}/etc/hotplug/udev-mobiled" \
+	  "${FILESYSTEM_MOD_DIR}/usr/share/ctlmgr/libmobiled.so" \
+	  "${FILESYSTEM_MOD_DIR}/usr/share/configd/C20_mobiled.so" \
+	  "${FILESYSTEM_MOD_DIR}/usr/lua/mobiled.lua"
 
-[ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ] && modern_remove mobile
+	modern_remove mobile
+fi
 
 echo1 "patching rc.conf"
 modsed "s/CONFIG_USB_GSM=.*$/CONFIG_USB_GSM=\"n\"/g" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
