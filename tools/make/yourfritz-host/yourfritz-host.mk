f7216540e162ce50fbf36741c6419e8fbcb3010d
YourFritz <git@yourfritz.de>
2019-03-07 23:12:45 +0100

    yourfritz-host: new upstream version with 'signimage' changes

--- tools/make/yourfritz-host/yourfritz-host.mk
+++ tools/make/yourfritz-host/yourfritz-host.mk
@@ -1,4 +1,4 @@
-YOURFRITZ_HOST_VERSION:=eaa8e3b2ba
+YOURFRITZ_HOST_VERSION:=2aa2e986ef
 YOURFRITZ_HOST_SOURCE:=yourfritz-$(YOURFRITZ_HOST_VERSION).tar.xz
 YOURFRITZ_HOST_SITE:=git_no_submodules@https://github.com/PeterPawn/YourFritz.git
 
