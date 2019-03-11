02b638edf2c7475aa6117afed11149da990eb6cd
fda77 <fda77@users.noreply.github.com>
2019-03-11 12:45:24 +0100

    signature: new option to remove public firmware key

---
+++ patches/scripts/265-remove_pubkey.sh
@@ -0,0 +1,8 @@
+[ "$FREETZ_REMOVE_PUBKEY" == "y" ] || return 0
+echo1 "removing avm_firmware_public_key1"
+rm_files ${FILESYSTEM_MOD_DIR}/etc/avm_firmware_public_key1
+
+[ -e "${FILESYSTEM_MOD_DIR}/etc/avm_firmware_public_key4" ] || return 0
+echo1 "removing avm_firmware_public_key4"
+rm_files ${FILESYSTEM_MOD_DIR}/etc/avm_firmware_public_key4
+
