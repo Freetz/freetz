f6e55cb7f3aa7ae4e8925123a541e668af2d9e80
fda77 <fda77@users.noreply.github.com>
2019-04-11 08:37:56 +0200

    fix enable gsmvoice

--- patches/scripts/382-enable-gsmvoice.sh
+++ patches/scripts/382-enable-gsmvoice.sh
@@ -6,9 +6,15 @@ if [ "$FREETZ_AVM_HAS_ONLY_LUA" != "y" ]; then
 	  "s/^\(return !isNaN(voiceStatus)\).*/\1;/" \
 	  "${HTML_SPEC_MOD_DIR}/gsm/gsm.js" \
 	  'return !isNaN(voiceStatus)'
-else
+elif [ "$FREETZ_AVM_VERSION_06_9X_MAX" == "y" ] ; then
 	modsed \
 	  's/^\(local voice_status=\).*/\11/' \
 	  "${LUA_MOD_DIR}/umts.lua" \
 	  'voice_status=1'
+else
+	modsed \
+	  's/\(^function umts.is_voice_modem\)\(()\)/\1()\nreturn umts.stick_present()\nend\n\1_()/' \
+	  "${LUA_MOD_DIR}/umts.lua" \
+	  ' umts.is_voice_modem_()'
 fi
+
