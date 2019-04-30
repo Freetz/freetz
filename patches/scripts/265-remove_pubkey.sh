1d6dff46c7e1496127b621c007ce2053e02575b0
fda77 <fda77@users.noreply.github.com>
2019-04-30 22:15:53 +0200

    remove pubkey: dont consider *key4

--- patches/scripts/265-remove_pubkey.sh
+++ patches/scripts/265-remove_pubkey.sh
@@ -2,7 +2,7 @@
 echo1 "removing avm_firmware_public_key1"
 rm_files ${FILESYSTEM_MOD_DIR}/etc/avm_firmware_public_key1
 
-[ -e "${FILESYSTEM_MOD_DIR}/etc/avm_firmware_public_key4" ] || return 0
-echo1 "removing avm_firmware_public_key4"
-rm_files ${FILESYSTEM_MOD_DIR}/etc/avm_firmware_public_key4
+#[ -e "${FILESYSTEM_MOD_DIR}/etc/avm_firmware_public_key4" ] || return 0
+#echo1 "removing avm_firmware_public_key4"
+#rm_files ${FILESYSTEM_MOD_DIR}/etc/avm_firmware_public_key4
 
