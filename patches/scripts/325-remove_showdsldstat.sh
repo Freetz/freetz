dabae943e1ffd849862cc4f0b10160eb19242f6d
fda77 <fda77@users.noreply.github.com>
2019-05-01 08:55:57 +0200

    fix remove showdsldstat

--- patches/scripts/325-remove_showdsldstat.sh
+++ patches/scripts/325-remove_showdsldstat.sh
@@ -1,7 +1,7 @@
 [ "$FREETZ_REMOVE_SHOWDSLDSTAT" == "y" ] || return 0
 
 echo1 "removing showdsldstat file"
-rm_files $(find ${FILESYSTEM_MOD_DIR}/sbin -name showdsldstat)
+rm_files $(find ${FILESYSTEM_MOD_DIR}/bin ${FILESYSTEM_MOD_DIR}/sbin -name showdsldstat)
 
 if [ "$FREETZ_AVM_HAS_WEBDAV" == "y" -a "$FREETZ_REMOVE_WEBDAV" != "y" ]; then
 	modsed 's/ -o ! -x $EXE_SHOWDSLDSTAT//' "$FILESYSTEM_MOD_DIR/etc/webdav_control"
