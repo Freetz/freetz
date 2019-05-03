999d60d595247e1ec4dd35a6fadb3c219170991e
Eugene Rudoy <gene.devel@gmail.com>
2019-05-03 16:01:36 +0200

    add new make helper-function - GET_STRING_COMPONENT
    add support for spaces to GET_STRING_COMPONENTS
    
    (cherry picked from commit 4e8f120352a21eb0b683ffc1352e41781cd2cde1)

--- include/make/010-special-characters-symbolic-names.mk
+++ include/make/010-special-characters-symbolic-names.mk
@@ -9,3 +9,7 @@ define _newline
 
 
 endef
+
+# a magic string that is very unlikely to appear in real strings, generated using:
+#  echo -n _space_escape | sha1sum | sha1sum | cut -d " " -f 1
+_space_escape:=23e5fcb635d4b5f2e6809257b10bb7aba88848bd
