dcbd4885dea3ee8ef7a15a54ecd852ce913a0492
fda77 <fda77@users.noreply.github.com>
2019-05-09 22:20:13 +0200

    (!) noexec: split patch into external & internal storages

--- patches/scripts/222-drop_noexec.sh
+++ patches/scripts/222-drop_noexec.sh
@@ -1,7 +1,9 @@
-[ "$FREETZ_DROP_NOEXEC" == "y" ] || return 0
+[ "$FREETZ_DROP_NOEXEC_EXTERNAL" == "y" -o "$FREETZ_DROP_NOEXEC_INTERNAL" == "y" ] || return 0
 
 echo1 "dropping noexec"
-for file in "${FILESYSTEM_MOD_DIR}/etc/init.d/S15-filesys" "${FILESYSTEM_MOD_DIR}/etc/hotplug/udev-mount-sd"; do
+[ "$FREETZ_DROP_NOEXEC_EXTERNAL" == "y" ] && file_ext="${FILESYSTEM_MOD_DIR}/etc/hotplug/udev-mount-sd"
+[ "$FREETZ_DROP_NOEXEC_INTERNAL" == "y" ] && file_int="${FILESYSTEM_MOD_DIR}/etc/init.d/S15-filesys"
+for file in "$file_ext" "$file_int"; do
 	[ -e "$file" ] && modsed 's/, *noexec//g;s/-o  *noexec *,/-o /g;s/-o  *noexec *//g' "$file"
 done
 
