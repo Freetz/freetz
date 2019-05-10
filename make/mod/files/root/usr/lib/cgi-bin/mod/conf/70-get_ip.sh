24c708fe1b2f1d9eb654afd86e6dd270f643e7b1
WileC <djwilec@d-jay.de>
2019-05-10 08:56:05 +0200

    webgui: display correct ipv4/ipv6 help of get_ip in freetz preferences

--- make/mod/files/root/usr/lib/cgi-bin/mod/conf/70-get_ip.sh
+++ make/mod/files/root/usr/lib/cgi-bin/mod/conf/70-get_ip.sh
@@ -1,6 +1,10 @@
 
-current_ip="$(get_ip)"
-[ -z "$current_ip" ] && current_ip="-$(lang de:"keine" en:"none")-"
+current_ip_v4="$(get_ip)"
+current_ip_v6="$(get_ip --ctlm6)"
+current_pre_v6="$(get_ip --ctlmpre6)"
+[ -z "$current_ip_v4" ] && current_ip_v4="-$(lang de:"keine" en:"none")-"
+[ -z "$current_ip_v6" ] && current_ip_v6="-$(lang de:"keine" en:"none")-"
+[ -z "$current_pre_v6" ] && current_pre_v6="-$(lang de:"keine" en:"none")-"
 
 sec_begin 'get_ip'
 cat << EOF
@@ -17,13 +21,15 @@ $(lang de:"Server f&uuml;r STUN/VoIP Methode" en:"Server for STUN/VoIP method"):
 <div style='margin-top:6px;'>$(lang de:"M&ouml;gliche Methoden" en:"Available methods"):</div>
 <pre>
 EOF
-get_ip --help | sed -nr 's/    -.,()/\1/p' | html
+get_ip --help | sed -n '5,20p' | html
 cat << EOF
 </pre>
 </p>
 
 <p>
-$(lang de:"Ermittelte IP" en:"Determined IP"): $current_ip
+$(lang de:"Ermittelte IPv4" en:"Determined IPv4"): $current_ip_v4
+$(lang de:"Ermittelte IPv6" en:"Determined IPv6"): $current_ip_v6
+$(lang de:"IPv6 Präfix" en:"IPv6-prefix"): $current_pre_v6
 </p>
 
 EOF
