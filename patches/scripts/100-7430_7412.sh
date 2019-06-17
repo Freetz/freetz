19b64a889fe179e8a0d88fbbfe4100e042ee2c15
fda77 <fda77@users.noreply.github.com>
2019-06-17 13:39:58 +0200

    add 7412-hardware 7430-software alien

---
+++ patches/scripts/100-7430_7412.sh
@@ -0,0 +1,22 @@
+isFreetzType 7430_7412 || return 0
+echo1 "adapt firmware for 7412"
+
+
+echo2 "moving default config dir"
+mv ${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_HW218 \
+   ${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_HW209
+
+echo2 "patching rc.S and rc.conf"
+modsed -r 's/(CONFIG_ETH_COUNT)=.*$/\1="1"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+#modsed -r 's/(CONFIG_DECT_HOME)=.*$/\1="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"  #AHA?
+#modsed -r 's/(CONFIG_HOME_AUTO)=.*$/\1="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"  #AHA?
+#modsed -r 's/(CONFIG_HOME_AUTO_NET)=.*$/\1="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"  #AHA?
+modsed -r 's/(CONFIG_INSTALL_TYPE)=.*$/\1="mips34_128MB_vdsl_1eth_1ab_wlan11n_56213"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed -r 's/(CONFIG_PRODUKT)=.*$/\1="Fritz_Box_HW209"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed -r 's/(CONFIG_PRODUKT_NAME)=.*$/\1="FRITZ!WLAN Repeater DVB-C"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed -r 's/(CONFIG_VERSION_MAJOR)=.*$/\1="137"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+
+# patch install script to accept firmware from 7430
+echo2 "applying install patch"
+modsed "s/mips34_128MB_vdsl_dect441_4eth_1usb_host_1ab_wlan11n_04371/mips34_128MB_vdsl_1eth_1ab_wlan11n_56213/g" "${FIRMWARE_MOD_DIR}/var/install"
+
