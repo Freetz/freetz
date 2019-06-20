c0662bcde683929d6993b99bbe9e26371b4c97bb
fda77 <fda77@users.noreply.github.com>
2019-06-20 20:03:00 +0200

    7412: fix typo + c&p errors

--- patches/scripts/100-7430_7412.sh
+++ patches/scripts/100-7430_7412.sh
@@ -1,4 +1,4 @@
-isFreetzType 7430_7412 || return 0
+isFreetzType 7412_7430 || return 0
 echo1 "adapt firmware for 7412"
 
 
@@ -13,7 +13,7 @@ modsed -r 's/(CONFIG_ETH_COUNT)=.*$/\1="1"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/
 #modsed -r 's/(CONFIG_HOME_AUTO_NET)=.*$/\1="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"  #AHA?
 modsed -r 's/(CONFIG_INSTALL_TYPE)=.*$/\1="mips34_128MB_vdsl_1eth_1ab_wlan11n_56213"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
 modsed -r 's/(CONFIG_PRODUKT)=.*$/\1="Fritz_Box_HW209"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
-modsed -r 's/(CONFIG_PRODUKT_NAME)=.*$/\1="FRITZ!WLAN Repeater DVB-C"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed -r 's/(CONFIG_PRODUKT_NAME)=.*$/\1="FRITZ!Box 7412"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
 modsed -r 's/(CONFIG_VERSION_MAJOR)=.*$/\1="137"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
 
 # patch install script to accept firmware from 7430
