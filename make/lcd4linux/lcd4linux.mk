16ad2713490812e067a784270ff9c0c0fc68b232
fda77 <fda77@users.noreply.github.com>
2019-06-24 14:18:43 +0200

    lcd4linux: switch to newer git fork

--- make/lcd4linux/lcd4linux.mk
+++ make/lcd4linux/lcd4linux.mk
@@ -1,18 +1,20 @@
-$(call PKG_INIT_BIN, 1204)
-$(PKG)_SOURCE:=$(pkg)-r$($(PKG)_VERSION).tar.xz
+$(call PKG_INIT_BIN, 60925a0)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
 $(PKG)_SOURCE_CHECKSUM:=X
-$(PKG)_SITE:=svn@https://ssl.bulix.org/svn/lcd4linux/trunk
+$(PKG)_SITE:=git@https://github.com/TangoCash/lcd4linux.git
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/lcd4linux
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/lcd4linux
 
 $(PKG)_DEPENDS_ON += ncurses libgd jpeg libusb libusb1 libftdi
+
+$(PKG)_CONFIGURE_PRE_CMDS += autoreconf -fi;
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
 $(PKG)_CONFIGURE_OPTIONS += --disable-rpath
 $(PKG)_CONFIGURE_OPTIONS += --with-x=no
 $(PKG)_CONFIGURE_OPTIONS += --with-drivers=all,!DPF,!LCDLinux,!LUIse,!RouterBoard,!serdisplib,!st2205,!VNC,!X11,!HD44780,!LPH7508,!M50530,!T6963,!Noritake,!T6963,!Sample
 $(PKG)_CONFIGURE_OPTIONS += --with-plugins=all,!dbus,!gps,!mpd,!mpris_dbus,!mysql,!netinfo,!qnaplog,!wireless
-$(PKG)_PATCH_POST_CMDS += echo "\#define SVN_VERSION \"$($(PKG)_VERSION)M\"" > svn_version.h;
+$(PKG)_PATCH_POST_CMDS += echo "\#define VCS_VERSION \"$($(PKG)_VERSION)\"" > vcs_version.h;
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
