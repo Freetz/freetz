44f46958ede7b31e3b838447a5ba4291bc6813fc
fda77 <fda77@users.noreply.github.com>
2019-06-26 14:05:22 +0200

    450+540+546: still no FOS 7.xx sources  - so use latest FOS with old kernel

--- patches/scripts/821-show_network_repeater.sh
+++ patches/scripts/821-show_network_repeater.sh
@@ -1,5 +1,6 @@
 [ "$FREETZ_TYPE_EXTENDER" == "y" ] || return 0
-[ "$FREETZ_AVM_VERSION_06_2X_MAX" != "y" -a "$FREETZ_TYPE_0546" != "y" ] && return 0
+[ "$FREETZ_AVM_VERSION_06_5X_MAX" == "y" ] || return 0
+[ "$FREETZ_AVM_VERSION_06_5X_MIN" == "y" -a "$FREETZ_TYPE_0546" == "y" ] && return 0
 echo1 "enabling network config"
 
 
