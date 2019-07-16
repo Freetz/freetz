a855272c65e79a98e525f5c28fb5b479f8458f08
fda77 <fda77@users.noreply.github.com>
2019-07-16 15:24:35 +0200

    7412alien: add dsl&dect icons  - thx aussua  - see https://www.digital-eliteboard.com/threads/fw-der-7430-auf-der-7412-nutzen.477126/

--- patches/scripts/100-7430_7412.sh
+++ patches/scripts/100-7430_7412.sh
@@ -7,13 +7,8 @@ fi
 
 echo1 "adapt firmware for 7412"
 
-cp -a "${FILESYSTEM_TK_DIR}/etc/default.Fritz_Box_HW209/avm/wlan_product.cfg" \
-     "${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_HW218/avm/wlan_product.cfg"
-
-files="lib/modules/dectfw_secondlevel_441.hex"
-for i in $files; do
-	cp -a "${FILESYSTEM_TK_DIR}/$i" "${FILESYSTEM_MOD_DIR}/$i"
-done
+echo2 "copying 7412 wlan files"
+cp -a ${FILESYSTEM_TK_DIR}/etc/default.Fritz_Box_HW209/avm/wlan* ${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_HW218/avm/
 
 modules=" net/ath_hal.ko"
 #modules=" net/ath_hal.ko net/aae.ko net/asf.ko"
@@ -21,9 +16,20 @@ for i in $modules; do
 	cp -a "${FILESYSTEM_TK_DIR}/lib/modules/3.10.73/$i" "${FILESYSTEM_MOD_DIR}/lib/modules/${FREETZ_KERNEL_VERSION_MODULES_SUBDIR}/$i"
 done
 
+echo2 "copying 7412 dect files"
+file="lib/modules/dectfw_secondlevel_441.hex"
+cp -a "${FILESYSTEM_TK_DIR}/$file" "${FILESYSTEM_MOD_DIR}/$file"
+
+echo2 "copying 7412 webif files"
+#dsl
+[ "$FREETZ_AVM_VERSION_07_1X_MIN" == "y" ] && file="usr/www/avm/css/rd/illustrations/box.gif" || file="usr/www/avm/css/default/images/box.gif"
+cp -a "${FILESYSTEM_TK_DIR}/usr/www/avm/css/default/images/box.gif" "${FILESYSTEM_MOD_DIR}/$file"
+#dect
+[ "$FREETZ_AVM_VERSION_07_1X_MIN" == "y" ] && file="usr/www/avm/css/rd/illustrations/illu_dectFbox.png" || file="usr/www/avm/css/default/images/dect_fbox_icon.png"
+cp -a "${FILESYSTEM_TK_DIR}/usr/www/avm/css/default/images/dect_fbox_icon.png" "${FILESYSTEM_MOD_DIR}/$file"
+
 echo2 "moving default config dir"
-mv ${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_HW218 \
-   ${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_HW209
+mv ${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_HW218 ${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_HW209
 
 echo2 "patching rc.S and rc.conf"
 modsed -r 's/(CONFIG_ETH_COUNT)=.*$/\1="1"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
