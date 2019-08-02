98f176f6b27cc0f8aeafaf883a722d3341691924
Eugene Rudoy <gene.devel@gmail.com>
2019-08-03 00:28:31 +0200

    unpack-kernel rule: adjust "Creating FOO/Makefile"-related patterns
    Fixes
    
     Linking  "source/kernel/ref-qca955x-7530_07.02/linux-4.4/include/uapi/linux/new_capi_debug.h" to "../../../drivers/isdn/capi_oslib/linux_new_capi_debug.h"sh: 1: Syntax error: ")" unexpected
     sh: 1: Syntax error: ")" unexpected
    
    observed with kernel-4.4

--- make/linux/kernel.mk
+++ make/linux/kernel.mk
@@ -70,14 +70,14 @@ $(KERNEL_DIR)/.unpacked: $(DL_FW_DIR)/$(DL_KERNEL_SOURCE) | $(UNPACK_TARBALL_PRE
 	done;
 	@for i in $$( \
 		find $(KERNEL_SOURCE_DIR) -name Makefile -exec \
-		awk '/(obj|subdir)-.*=/ && !/(obj|subdir)-ccflags.*=/ { \
+		awk '/^[ \t]*(obj|subdir)-.*=/ && !/(obj|subdir)-ccflags.*=/ { \
 			while (match ($$0,/\\/)) {sub(/\\/," "); getline l;$$0=$$0""l} \
 			sub(/\r/,""); \
 			gsub(/(#.*|.*=)/,""); \
 			if (! match ($$0,/,/)) { \
 				dirname=substr(FILENAME,1,length(FILENAME)-8); \
 				for (i=1;i<=NF;i++) { \
-					if (match ($$i,/\.o$$|\.lds$$|\$$/)) { \
+					if (match ($$i,/\.(o|lds)[)]?$$|\$$/)) { \
 						$$i=""; \
 					} else if (substr($$i,length($$i))!="/") { \
 						$$i=$$i"/"; \
