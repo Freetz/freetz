17d93f4587bfbf25b8694a2d43c10e1efee0fa70
fda77 <fda77@users.noreply.github.com>
2019-08-05 13:23:55 +0200

    add remove-plcd

---
+++ patches/scripts/510-remove_plcd.sh
@@ -0,0 +1,8 @@
+[ "$FREETZ_REMOVE_PLCD" == "y" ] || return 0
+echo1 "removing plcd files"
+
+rm_files \
+  "${FILESYSTEM_MOD_DIR}/etc/init.d/E50-plcd" \
+  "${FILESYSTEM_MOD_DIR}/usr/sbin/plcd" \
+# "${FILESYSTEM_MOD_DIR}/lib/libmesh_plcservice.so"
+
