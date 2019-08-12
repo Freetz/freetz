e631427f865eac887133b344ad59668f99b50f89
Florian Schilling <flosch.devel@gmail.com>
2019-08-12 02:09:56 +0200

    added package cacerts. It will download ca-bundel from curl.haxx.se and place in /etc/ssl/certs/cacert.pem. further it's recommended to add location in .profile: export SSL_CERT_FILE=/etc/ssl/certs/cacert.pem

---
+++ make/cacerts/cacerts.mk
@@ -0,0 +1,23 @@
+$(call PKG_INIT_BIN, 2019-05-15)
+$(PKG)_SOURCE:=cacert-$($(PKG)_VERSION).pem
+$(PKG)_SOURCE_SHA256:=cb2eca3fbfa232c9e3874e3852d43b33589f27face98eef10242a853d83a437a
+$(PKG)_SITE:=https://curl.haxx.se/ca
+
+$(PKG)_BINARY:=$(DL_DIR)/$($(PKG)_SOURCE)
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/etc/ssl/certs/cacert.pem
+
+$(PKG_SOURCE_DOWNLOAD)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_FILE)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+
+$(pkg)-uninstall:
+	$(RM) $($(PKG)_DEST_DIR)$($(PKG)_TARGET_PATH)
+
+$(PKG_FINISH)
