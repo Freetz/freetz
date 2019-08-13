ce34cc955c6f16367f1203ac11cafec423dc3472
fda77 <fda77@users.noreply.github.com>
2019-08-13 15:15:29 +0200

    make inhaus patch readable

--- patches/scripts/114-run_telefon_in_inhouse_mode.sh
+++ patches/scripts/114-run_telefon_in_inhouse_mode.sh
@@ -1,8 +1,12 @@
 [ "${FREETZ_RUN_TELEFON_IN_INHOUSE_MODE}" == "y" ] || return 0
 
-# CONFIG_RELEASE=0: in-house development version
-# CONFIG_RELEASE=1: official release
-# CONFIG_RELEASE=2: public beta/labor version
+# CONFIG_RELEASE=0: inhaus
+# CONFIG_RELEASE=1: release
+# CONFIG_RELEASE=2: labor
 
-echo1 "forcing telefon daemon to run in in-house mode"
-modsed -r 's,^([ \t]*)(telefon[ \t]),\1CONFIG_RELEASE=0 \2,' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.voip" '^[ \t]*CONFIG_RELEASE=0 telefon[ \t]'
+echo1 'forcing telefon daemon to run in "inhaus mode"'
+modsed -r \
+  's,^([ \t]*)(telefon[ \t]),\1CONFIG_RELEASE=0 \2,' \
+  "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.voip" \
+  '^[ \t]*CONFIG_RELEASE=0 telefon[ \t]'
+
