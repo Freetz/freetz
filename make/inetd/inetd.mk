fdc5750186860dbad21d0748a4b38e7461e73bad
fda77 <fda77@users.noreply.github.com>
2019-08-13 14:42:56 +0200

    telnet: dont show on webif if not available

--- make/inetd/inetd.mk
+++ make/inetd/inetd.mk
@@ -2,6 +2,7 @@ $(call PKG_INIT_BIN, 0.2)
 $(PKG)_STARTLEVEL=14
 
 $(PKG)_EXCLUDED += $(if $(FREETZ_REMOVE_WEBSRV),,etc/default.websrv)
+$(PKG)_EXCLUDED += $(if $(FREETZ_BUSYBOX_TELNETD),,etc/default.telnetd)
 $(PKG)_EXCLUDED += $(if $(or $(call not-y,$(FREETZ_AVM_HAS_USB_HOST)),$(FREETZ_REMOVE_FTPD)),etc/default.ftpd bin/inetdftp)
 $(PKG)_EXCLUDED += $(if $(or $(call not-y,$(FREETZ_AVM_HAS_USB_HOST)),$(FREETZ_REMOVE_SAMBA),$(FREETZ_PACKAGE_SAMBA_SMBD)),etc/default.smbd bin/inetdsamba)
 $(PKG)_EXCLUDED += $(if $(or $(call not-y,$(FREETZ_AVM_HAS_USB_HOST)),$(and $(or $(FREETZ_REMOVE_SAMBA),$(FREETZ_PACKAGE_SAMBA_SMBD)),$(FREETZ_REMOVE_FTPD))),bin/inetdctl)
