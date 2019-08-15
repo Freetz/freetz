9ad843ceaa92a677880efaac94ba95bd7b817635
Florian Schilling <flosch.devel@gmail.com>
2019-08-15 10:37:41 +0200

    wireguard bump -> 20190702

--- make/wireguard/wireguard.mk
+++ make/wireguard/wireguard.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 0.0.20190601)
+$(call PKG_INIT_BIN, 0.0.20190702)
 $(PKG)_SOURCE:=WireGuard-$($(PKG)_VERSION).tar.xz
-$(PKG)_SOURCE_MD5:=eeeabbbd5592987b5308d1cc495f756f
+$(PKG)_SOURCE_MD5:=7410a52bacc5eb85e1634a20c07d3bed
 $(PKG)_SITE:=https://git.zx2c4.com/WireGuard/snapshot
 
 $(PKG)_BINARIES            := wg
