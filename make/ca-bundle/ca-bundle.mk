71f9d579044f7955ae1e33683714810220522781
flosch-dev <47741987+flosch-dev@users.noreply.github.com>
2019-08-18 14:55:23 +0200

    Add new package cacerts (#218)
    It will download ca-bundle from curl.haxx.se and make it available on the box under /etc/ssl/certs/cacert.pem

--- make/ca-bundle/ca-bundle.mk
+++ make/ca-bundle/ca-bundle.mk
@@ -3,21 +3,13 @@ $(PKG)_SOURCE:=cacert-$($(PKG)_VERSION).pem
 $(PKG)_SOURCE_SHA256:=cb2eca3fbfa232c9e3874e3852d43b33589f27face98eef10242a853d83a437a
 $(PKG)_SITE:=https://curl.haxx.se/ca
 
-$(PKG)_BINARY:=$($(PKG)_DIR)/cacert.pem
+$(PKG)_BINARY:=$(DL_DIR)/$($(PKG)_SOURCE)
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/etc/ssl/certs/ca-bundle.crt
 
 $(PKG)_STARTLEVEL=30
 
-define $(PKG)_CUSTOM_UNPACK
-	mkdir -p $($(PKG)_DIR); \
-	cp -p $(strip $(1)) $($(PKG)_BINARY)
-endef
-
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
-$(PKG_CONFIGURED_NOP)
-
-$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_FILE)
