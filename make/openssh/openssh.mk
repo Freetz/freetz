7bb50cd4e40707d1983b92b641fcc03e8f08d7ba
fda77 <fda77@users.noreply.github.com>
2019-09-12 20:45:25 +0200

    bump openssh

--- make/openssh/openssh.mk
+++ make/openssh/openssh.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 7.9p1)
+$(call PKG_INIT_BIN, 8.0p1)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_SHA1:=993aceedea8ecabb1d0dd7293508a361891c4eaa
+$(PKG)_SOURCE_SHA1:=756dbb99193f9541c9206a667eaa27b0fa184a4f
 $(PKG)_SITE:=http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable,ftp://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 
 $(PKG)_BIN_BINARIES             := ssh scp ssh-add ssh-agent ssh-keygen ssh-keysign ssh-keyscan sftp
