d8b2c0128253801fd3051c2d0807dd856231db2a
fda77 <fda77@users.noreply.github.com>
2019-09-14 18:12:15 +0200

    remove plcd: add alternate path

--- patches/scripts/510-remove_plcd.sh
+++ patches/scripts/510-remove_plcd.sh
@@ -3,6 +3,7 @@ echo1 "removing plcd files"
 
 rm_files \
   "${FILESYSTEM_MOD_DIR}/etc/init.d/E50-plcd" \
+  "${FILESYSTEM_MOD_DIR}/sbin/plcd" \
   "${FILESYSTEM_MOD_DIR}/usr/sbin/plcd" \
 # "${FILESYSTEM_MOD_DIR}/lib/libmesh_plcservice.so"
 
