c936e66798606d4acfcd8ef2b3620b2f93c136e2
Eugene Rudoy <gene.devel@gmail.com>
2019-09-27 00:11:23 +0200

    curl: bump version to 7.66.0
    https://curl.haxx.se/changes.html#7_66_0

--- make/curl/curl.mk
+++ make/curl/curl.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_BIN, 7.65.3)
-$(PKG)_LIB_VERSION:=4.5.0
+$(call PKG_INIT_BIN, 7.66.0)
+$(PKG)_LIB_VERSION:=4.6.0
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
-$(PKG)_SOURCE_SHA256:=f2d98854813948d157f6a91236ae34ca4a1b4cb302617cebad263d79b0235fea
+$(PKG)_SOURCE_SHA256:=dbb48088193016d079b97c5c3efde8efa56ada2ebf336e8a97d04eb8e2ed98c1
 $(PKG)_SITE:=http://curl.haxx.se/download
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src$(if $(FREETZ_PACKAGE_CURL_STATIC),,/.libs)/curl
