8c52ab5580f7cb4d99ecc312332cf23c93062012
fda77 <fda77@users.noreply.github.com>
2019-09-27 20:26:57 +0200

    fix hide_dsl_overview-hint

--- patches/scripts/832-hide_dsl_overview-hint.sh
+++ patches/scripts/832-hide_dsl_overview-hint.sh
@@ -2,7 +2,9 @@
 
 echo1 "patching dsl overview"
 
-if [ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ] ; then
+if [ "$FREETZ_AVM_VERSION_07_2X_MIN" == "y" ] ; then
+	PVER="07_2X"
+elif [ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ] ; then
 	PVER="07_0X"
 elif [ "$FREETZ_AVM_VERSION_06_8X_MIN" == "y" ] ; then
 	PVER="06_8X"
