f3b897152a8cdf4412d631e0090e403896e2458d
fda77 <fda77@users.noreply.github.com>
2019-10-15 00:06:47 +0200

    tor: avoid rebuild

--- make/tor/tor.mk
+++ make/tor/tor.mk
@@ -6,18 +6,15 @@ $(PKG)_SITE:=https://www.torproject.org/dist
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/app/tor
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/tor
 
+$(PKG)_ALL_GEOIPDB := geoip geoip6
+$(PKG)_TARGET_GEOIP := $(addprefix $($(PKG)_DEST_DIR)/usr/share/tor/,$($(PKG)_ALL_GEOIPDB))
+
+$(PKG)_EXCLUDED += $(if $(FREETZ_PACKAGE_TOR_GEOIP_V4),,$(if $(FREETZ_PACKAGE_TOR_GEOIP_V6),/usr/share/tor/geoip))
+$(PKG)_EXCLUDED += $(if $(FREETZ_PACKAGE_TOR_GEOIP_V6),,$(if $(FREETZ_PACKAGE_TOR_GEOIP_V4),/usr/share/tor/geoip6))
+$(PKG)_EXCLUDED += $(if $(FREETZ_PACKAGE_TOR_GEOIP_V4),,$(if $(FREETZ_PACKAGE_TOR_GEOIP_V6),,/usr/share/))
+
+
 $(PKG)_DEPENDS_ON += zlib openssl libevent
-
-$(PKG)_GEOIPDB_DIR := /usr/share/tor
-$(PKG)_GEOIPDB_ALL := geoip geoip6
-
-$(PKG)_GEOIPDB := $(if $(FREETZ_PACKAGE_TOR_GEOIP_V4),geoip) $(if $(FREETZ_PACKAGE_TOR_GEOIP_V6),geoip6)
-$(PKG)_GEOIPDB_BUILD_DIR := $($(PKG)_GEOIPDB:%=$($(PKG)_DIR)/src/config/%)
-$(PKG)_GEOIPDB_TARGET_DIR := $($(PKG)_GEOIPDB:%=$($(PKG)_DEST_DIR)$($(PKG)_GEOIPDB_DIR)/%)
-
-$(PKG)_EXCLUDED += $(patsubst %,$($(PKG)_DEST_DIR)$($(PKG)_GEOIPDB_DIR)/%,$(filter-out $($(PKG)_GEOIPDB),$($(PKG)_GEOIPDB_ALL)))
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_TOR_GEOIP_V4
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_TOR_GEOIP_V6
 
 $(PKG)_CONFIGURE_ENV += tor_cv_malloc_zero_works=no
 $(PKG)_CONFIGURE_ENV += tor_cv_null_is_zero=yes
@@ -54,31 +51,33 @@ ifeq ($(strip $(FREETZ_PACKAGE_TOR_STATIC)),y)
 $(PKG)_EXTRA_LDFLAGS += -static
 endif
 
+
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
+
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(TOR_DIR) \
 		EXTRA_CFLAGS="$(TOR_EXTRA_CFLAGS)" \
 		EXTRA_LDFLAGS="$(TOR_EXTRA_LDFLAGS)"
 
-$($(PKG)_GEOIPDB_BUILD_DIR): $($(PKG)_DIR)/.unpacked
-
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
 
-$($(PKG)_GEOIPDB_TARGET_DIR): $($(PKG)_DEST_DIR)$($(PKG)_GEOIPDB_DIR)/%: $($(PKG)_DIR)/src/config/%
+$($(PKG)_TARGET_GEOIP): $($(PKG)_DEST_DIR)/usr/share/tor/%: $($(PKG)_DIR)/src/config/%
 	$(INSTALL_FILE)
+
 
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_GEOIPDB_TARGET_DIR)
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_TARGET_GEOIP)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(TOR_DIR) clean
 
 $(pkg)-uninstall:
-	$(RM) -r $(TOR_TARGET_BINARY) $(TOR_DEST_DIR)$(MNISATIP_GEOIPDB_DIR)
+	$(RM) $(TOR_TARGET_BINARY)
+	$(RM) $(TOR_TARGET_GEOIP)
 
 $(PKG_FINISH)
