4d5e77954c471663635339062bd378f25e2f4813
fda77 <fda77@users.noreply.github.com>
2019-10-16 17:10:46 +0200

    add acme(.sh) - no webif yet

---
+++ make/acme/acme.mk
@@ -0,0 +1,40 @@
+$(call PKG_INIT_BIN, b8cc10ab5dd7925b022ecf32ab0650b5c784cc86)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
+$(PKG)_SOURCE_CHECKSUM:=X
+$(PKG)_SITE:=git@https://github.com/Neilpang/acme.sh.git
+
+$(PKG)_ALL_ADDS:=deploy dnsapi notify acme.sh
+$(PKG)_ADDS:=$(addprefix $($(PKG)_DIR)/,$($(PKG)_ALL_ADDS))
+$(PKG)_TARGET_ADDS:=$(addprefix $($(PKG)_DEST_DIR)/usr/bin/acme/,$($(PKG)_ALL_ADDS))
+$(PKG)_TARGET_LINK:=$($(PKG)_DEST_DIR)/usr/bin/acme.sh
+
+$(PKG)_EXCLUDED += $(if $(FREETZ_PACKAGE_ACME_deploy),,usr/bin/acme/deploy)
+$(PKG)_EXCLUDED += $(if $(FREETZ_PACKAGE_ACME_dnsapi),,usr/bin/acme/dnsapi)
+$(PKG)_EXCLUDED += $(if $(FREETZ_PACKAGE_ACME_notify),,usr/bin/acme/notify)
+
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+
+$($(PKG)_TARGET_LINK):
+	ln -s acme/acme.sh $@
+
+$($(PKG)_ADDS): $($(PKG)_DIR)/.unpacked
+
+$($(PKG)_TARGET_ADDS): $($(PKG)_DEST_DIR)/usr/bin/acme/%: $($(PKG)_DIR)/%
+	$(INSTALL_DIR)
+
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_ADDS) $($(PKG)_TARGET_LINK)
+
+$(pkg)-clean:
+
+$(pkg)-uninstall:
+	$(RM) $(ACME_TARGET_LINK)
+	$(RM) -r $(ACME_DEST_DIR)/usr/bin/acme/
+
+$(PKG_FINISH)
