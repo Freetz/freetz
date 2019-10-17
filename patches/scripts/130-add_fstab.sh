f2b7921dc8061bab7976cfeaea6f62614da563d9
fda77 <fda77@users.noreply.github.com>
2019-10-17 17:21:02 +0200

    fix fstab

--- patches/scripts/130-add_fstab.sh
+++ patches/scripts/130-add_fstab.sh
@@ -1,11 +1,18 @@
 cat << 'EOF' > "${FILESYSTEM_MOD_DIR}/etc/fstab"
+## /etc/fstab: static file system information.
+## <file system>  <mount point>  <type>  <options>                 <dump>  <pass>
 
-# /etc/fstab: static file system information.
-#
-# <file system> <mount point>   <type>  <options>               <dump>  <pass>
-devpts          /dev/pts        devpts  mode=0620,gid=5         0       0
-proc            /proc           proc    nosuid,nodev,noexec     0       0
-tmpfs           /var            tmpfs   defaults                0       0
-sysfs           /sys            sysfs   nosuid,nodev,noexec     0       0
+# AVM
+tmpfs             /var           tmpfs   defaults                  0       0
+/dev/root         /              auto    defaults,errors=continue  0       0
+proc              /proc          proc    nosuid,nodev,noexec       0       0
+
+# MOD
+sysfs             /sys           sysfs   nosuid,nodev,noexec       0       0
+devpts            /dev/pts       devpts  mode=0620,gid=5           0       0
 
 EOF
+
+VARFS="$(sed -rn 's,^([^ \t]*)[ \t]*/var[ \t].*,\1,p' ${FILESYSTEM_DIR}/etc/fstab)"
+[ "$VARFS" != "tmpfs" ] && sed -i "s/tmpfs/$VARFS/g" "${FILESYSTEM_MOD_DIR}/etc/fstab"
+
