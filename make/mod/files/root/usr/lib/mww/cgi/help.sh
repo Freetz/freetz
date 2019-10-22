fa90f1a8989086687f775aa98bb636255134559d
fda77 <fda77@users.noreply.github.com>
2019-10-22 10:06:01 +0200

    change help link

--- make/mod/files/root/usr/lib/mww/cgi/help.sh
+++ make/mod/files/root/usr/lib/mww/cgi/help.sh
@@ -1,4 +1,6 @@
 _cgi_help_url() {
 	local path=$1
-	echo "https://freetz-ng.github.io/freetz-ng/wiki${path}"
+#	echo "https://freetz-ng.github.io/freetz-ng/wiki${path}"
+	echo -n "https://github.com/Freetz-NG/freetz-ng/blob/master/make/README.md#"
+	echo $path | sed 's,.*/,,;s,#.*,,'
 }
