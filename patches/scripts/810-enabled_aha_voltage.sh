96cbb30c61831c165165c4773b7d817227e79ce8
fda77 <fda77@users.noreply.github.com>
2020-01-22 14:54:01 +0100

    fix aha-voltage patch

--- patches/scripts/810-enabled_aha_voltage.sh
+++ patches/scripts/810-enabled_aha_voltage.sh
@@ -4,9 +4,15 @@ echo1 "enabling aha voltage"
 
 # patcht homeautoswitch.lua > getdevicelistinfos"
 
-[ "$FREETZ_AVM_VERSION_07_0X_MIN" != "y" ] && mod_patch='enabled_aha_voltage.FOS6.patch' || mod_patch='enabled_aha_voltage.FOS7.patch'
+if [ "$FREETZ_AVM_VERSION_07_2X_MIN" == "y" ] ; then
+	PVER="07_2X"
+elif [ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ] ; then
+	PVER="07_0X"
+else
+	PVER="06_XX"
+fi
 
 modpatch \
   "$FILESYSTEM_MOD_DIR" \
-  "${PATCHES_COND_DIR}/${mod_patch}"
+  "${PATCHES_COND_DIR}/enabled_aha_voltage_${PVER}.patch"
 
