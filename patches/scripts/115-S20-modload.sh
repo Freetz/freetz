57ba6632434ccced409398b466eadf118d73b37d
fda77 <fda77@users.noreply.github.com>
2020-01-29 12:31:49 +0100

    fix modload patch

--- patches/scripts/115-S20-modload.sh
+++ patches/scripts/115-S20-modload.sh
@@ -1,8 +1,8 @@
 [ "$FREETZ_AVM_VERSION_05_2X_MIN" == "y" ] || return 0
 
-echo1 "adding /etc/init.d/S20-modload"
+[ -e "${FILESYSTEM_MOD_DIR}/etc/boot.d/core/99-tail" ] && file="etc/boot.d/core/20-modload" || file="etc/init.d/S20-modload"
+echo1 "adding /$file"
 
-cat << 'EOF' > "${FILESYSTEM_MOD_DIR}/etc/init.d/S20-modload"
-/usr/bin/modload 2>&1 | tee /var/log/mod_load.log
+echo '/usr/bin/modload 2>&1 | tee /var/log/mod_load.log' > "${FILESYSTEM_MOD_DIR}/$file"
+chmod +x "${FILESYSTEM_MOD_DIR}/$file"
 
-EOF
