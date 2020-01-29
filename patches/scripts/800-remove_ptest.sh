1876352b285193781bd5ff5910cdd2004bbaf45b
fda77 <fda77@users.noreply.github.com>
2020-01-29 22:25:35 +0100

    fix ptest patch

--- patches/scripts/800-remove_ptest.sh
+++ patches/scripts/800-remove_ptest.sh
@@ -1,7 +1,9 @@
 
 echo1 "removing ptest"
 rm_files \
+  "${FILESYSTEM_MOD_DIR}/lib/systemd/system/ptest_release.service" \
   "${FILESYSTEM_MOD_DIR}/lib/systemd/system/ptest.service" \
+  "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.ptest.release" \
   "${FILESYSTEM_MOD_DIR}/etc/init.d/S42-ptest" \
   "${FILESYSTEM_MOD_DIR}/etc/init.d/S46-ptest" \
   "${FILESYSTEM_MOD_DIR}/etc/boot.d/ptest"
