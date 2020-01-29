3f3eb5f008ee65b9d1e924f83e23cc46d7faf5b3
fda77 <fda77@users.noreply.github.com>
2020-01-29 12:26:06 +0100

    fix ds_off patch

--- patches/scripts/115-patch-ds_off.sh
+++ patches/scripts/115-patch-ds_off.sh
@@ -1,16 +1,18 @@
-if [ -e "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.tail.sh" ]; then
+
+if [ -e "${FILESYSTEM_MOD_DIR}/etc/boot.d/core/99-tail" ]; then
+	dsfile="${FILESYSTEM_MOD_DIR}/etc/boot.d/core/99-zzz-rcmod"
+	rcfile=""
+elif [ -e "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.tail.sh" ]; then
+	dsfile="${FILESYSTEM_MOD_DIR}/etc/init.d/E99-zzz-rcmod"
 	rcfile="${FILESYSTEM_MOD_DIR}/etc/init.d/rc.tail.sh"
-
-cat << 'EOF' > "${FILESYSTEM_MOD_DIR}/etc/init.d/E99-zzz-rcmod"
-# Emergency stop switch for execution of Freetz as a whole
-[ "$ds_off" == "y" ] || . /etc/init.d/rc.mod 2>&1 | tee /var/log/mod.log
-EOF
-
 else
+	dsfile="${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
 	rcfile="${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
-	# Emergency stop switch for execution of Freetz as a whole
-	echo '[ "$ds_off" == "y" ] || . /etc/init.d/rc.mod 2>&1 | tee /var/log/mod.log' >> "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.S"
 fi
 
+# Emergency stop switch for execution of Freetz as a whole
+echo '[ "$ds_off" == "y" ] || . /etc/init.d/rc.mod 2>&1 | tee /var/log/mod.log' >> "$dsfile"
+chmod +x "$dsfile"
+
 # Emergency stop switch for execution of debug.cfg
-modsed -r 's,(if[ \t]+)([!][ \t]*(/usr/bin/)?checkempty[ \t]+/var/flash/debug[.]cfg),\1[ "$dbg_off" != "y" ] \&\& \2,' $rcfile
+[ -n "$rcfile" ] && modsed -r 's,(if[ \t]+)([!][ \t]*(/usr/bin/)?checkempty[ \t]+/var/flash/debug[.]cfg),\1[ "$dbg_off" != "y" ] \&\& \2,' $rcfile
