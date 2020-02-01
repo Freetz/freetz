d014b0255437e41f2057128ecaf0a93f8823146a
fda77 <fda77@users.noreply.github.com>
2020-02-02 00:58:00 +0100

    pimp dsl-settings patch

--- patches/scripts/831-extend-dsl_line_settings-speed.sh
+++ patches/scripts/831-extend-dsl_line_settings-speed.sh
@@ -3,7 +3,7 @@
 echo1 "patching dsl settings"
 
 # allow applying patch
-[ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ] && modsed 's/{/ {\n/g;s/}/\n}\n/g;s/;/;\n/g' "${HTML_LANG_MOD_DIR}/css/default/dsl_line_settings.css"
+decrip_file "${HTML_LANG_MOD_DIR}/css/default/dsl_line_settings.css"
 
 modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_COND_DIR}/831-extend-dsl_line_settings-speed_${FREETZ_TYPE_PREFIX_SERIES_SUBDIR}.patch"
 
