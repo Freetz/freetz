6203066b45906043e549ea64fa69ff7b282e4379
fda77 <fda77@users.noreply.github.com>
2020-02-17 13:18:13 +0100

    fix drop-noexec patch  - thx peterpawn  - refs https://www.ip-phone-forum.de/threads/301118/post-2358215  - both files dont exist in all labors

--- patches/scripts/222-drop_noexec.sh
+++ patches/scripts/222-drop_noexec.sh
@@ -1,7 +1,7 @@
 [ "$FREETZ_AVM_HAS_NOEXEC" == "y" ] || return 0
 
 echo1 "dropping noexec"
-files="${FILESYSTEM_MOD_DIR}/etc/init.d/S15-filesys"
+files="${FILESYSTEM_MOD_DIR}/etc/init.d/S15-filesys ${FILESYSTEM_MOD_DIR}/etc/boot.d/ubi_functions ${FILESYSTEM_MOD_DIR}/etc/boot.d/yaffs_functions"
 [ "$FREETZ_DROP_NOEXEC_EXTERNAL" == "y" ] && files="$files ${FILESYSTEM_MOD_DIR}/etc/hotplug/udev-mount-sd"
 for file in $files; do
 	[ -e "$file" ] && modsed 's/, *noexec//g;s/-o  *noexec *,/-o /g;s/-o  *noexec *//g' "$file"
