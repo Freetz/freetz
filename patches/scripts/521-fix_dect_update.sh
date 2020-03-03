76c2bf9af581918e105a01e99e590f7e2906043a
fda77 <fda77@users.noreply.github.com>
2020-03-03 08:29:40 +0100

    fix dect-update patch

--- patches/scripts/521-fix_dect_update.sh
+++ patches/scripts/521-fix_dect_update.sh
@@ -3,7 +3,7 @@
 
 echo1 "fixing start_dect_update.sh (atom)"
 modsed -r \
-  's/^(local mps=`)(mount)(.*)/\1rpc \2 | grep -v \/var\/nfsv4\3/g' \
+  's/^([ \t]*local mps=`)(mount)(.*)/\1rpc \2 | grep -v \/var\/nfsv4\3/g' \
   "${FILESYSTEM_MOD_DIR}/sbin/start_dect_update.sh" \
   "rpc mount"
 
