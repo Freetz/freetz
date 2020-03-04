0ae3cc2d11a9516f42fc30b6ab117a351fd5f10a
fda77 <fda77@users.noreply.github.com>
2020-03-04 16:52:09 +0100

    fix tty-settings patch

--- patches/scripts/102-dont-change-tty-settings.sh
+++ patches/scripts/102-dont-change-tty-settings.sh
@@ -3,6 +3,11 @@
 # freetz launches sysinit without controlling tty (s. patches/scripts/102-add_inittab.sh)
 # don't try to set/change tty settings as it won't work
 
-[ -n "$SYSTEMD_CORE_MOD_DIR" ] && file="etc/boot.d/core/00-head" || file="etc/init.d/S01-head"
+for file in \
+  etc/boot.d/core/00-head \
+  etc/boot.d/core/head \
+  etc/init.d/S01-head \
+  ; do
 [ -e "${FILESYSTEM_MOD_DIR}/$file" ] && modsed -r 's,^([ \t]*stty .*),## disabled by freetz: \1,' "${FILESYSTEM_MOD_DIR}/$file" 'disabled by freetz'
+done
 
