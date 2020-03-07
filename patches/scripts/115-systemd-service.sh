8e905da3363f833029710b012a69a8579844ddb4
fda77 <fda77@users.noreply.github.com>
2020-03-07 16:29:51 +0100

    add systemd services

---
+++ patches/scripts/115-systemd-service.sh
@@ -0,0 +1,24 @@
+[ -n "$SYSTEMD_CORE_MOD_DIR" ] || return 0
+
+echo1 "adding modload.service"
+cat << 'EOF' > "${FILESYSTEM_MOD_DIR}/lib/systemd/system/modload.service"
+[Unit]
+ExecStart=/etc/boot.d/core/20-modload
+Type=oneshot
+DefaultDependencies=no
+After=environment.service
+[Install]
+WantedBy=environment.target
+EOF
+
+echo1 "adding rcmod.service"
+cat << 'EOF' > "${FILESYSTEM_MOD_DIR}/lib/systemd/system/rcmod.service"
+[Unit]
+ExecStart=/etc/boot.d/core/99-zzz-rcmod
+Type=oneshot
+DefaultDependencies=no
+After=net.service modload.service
+[Install]
+WantedBy=multi-user.target
+EOF
+
