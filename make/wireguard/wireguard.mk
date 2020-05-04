59ad6ffb6387adba1e30c76ef949789edd1ef7f6
Florian Schilling <flosch.devel@gmail.com>
2020-05-04 21:26:15 +0200

    correction of sha256 checksum

--- make/wireguard/wireguard.mk
+++ make/wireguard/wireguard.mk
@@ -1,6 +1,6 @@
 $(call PKG_INIT_BIN, 1.0.20200319)
 $(PKG)_SOURCE:=wireguard-tools-$($(PKG)_VERSION).tar.xz
-$(PKG)_SOURCE_SHA256:=d1ab35a93507321c5db885e02d41ce1414f0507c
+$(PKG)_SOURCE_SHA256:=757ed31d4d48d5fd7853bfd9bfa6a3a1b53c24a94fe617439948784a2c0ed987
 $(PKG)_SITE:=https://git.zx2c4.com/wireguard-tools/snapshot
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/wg
@@ -9,7 +9,7 @@ $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/wg
 $(PKG)_EXTRA_CFLAGS += --function-section -fdata-sections -fstack-protector-strong
 $(PKG)_EXTRA_LDFLAGS += -Wl,--gc-sections
 
-$(PKG)_DEPENDS_ON += kernel 
+$(PKG)_DEPENDS_ON += kernel
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_KERNEL_VERSION
 
@@ -33,9 +33,9 @@ $(pkg):
 $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
 $(pkg)-clean:
-	-$(SUBMAKE) -C $(WIREGUARD_DIR)/src clean 
+	-$(SUBMAKE) -C $(WIREGUARD_DIR)/src clean
 
 $(pkg)-uninstall:
-	$(RM)  $(WIREGUARD_TARGET_BINARY) 
+	$(RM)  $(WIREGUARD_TARGET_BINARY)
 
 $(PKG_FINISH)
