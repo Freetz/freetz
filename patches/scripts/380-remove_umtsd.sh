810ae1b8e4315b88bc6b575eb94e2c52d2468aa5
fda77 <fda77@users.noreply.github.com>
2020-07-05 17:40:20 +0200

    fix remove umtsd

--- patches/scripts/380-remove_umtsd.sh
+++ patches/scripts/380-remove_umtsd.sh
@@ -21,11 +21,15 @@ rm_files \
 if [ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ]; then
 	rm_files \
 	  "${FILESYSTEM_MOD_DIR}/bin/mobiled" \
-	  "${FILESYSTEM_MOD_DIR}/etc/udev/rules.d/38-mobiled.rules" \
+	  "${FILESYSTEM_MOD_DIR}/etc/udev/rules.d/??-mobiled.rules" \
 	  "${FILESYSTEM_MOD_DIR}/etc/hotplug/udev-mobiled" \
 	  "${FILESYSTEM_MOD_DIR}/usr/share/ctlmgr/libmobiled.so" \
 	  "${FILESYSTEM_MOD_DIR}/usr/share/configd/C20_mobiled.so" \
 	  "${FILESYSTEM_MOD_DIR}/usr/lua/mobile_*.lua"
+
+	modsed \
+	  '/\/etc\/hotplug\/udev-mobiled/d' \
+	  "${FILESYSTEM_MOD_DIR}/etc/udev/rules.d/??-usb*.rules"
 
 	modern_remove mobile
 fi
