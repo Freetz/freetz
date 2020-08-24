0b19b9c42333fdafbec51a50c31b7e0e1bc654e7
Florian Schilling <flosch.devel@gmail.com>
2020-08-24 19:33:28 +0200

    curl: bump version to 7.72.0 Changelog: https://curl.haxx.se/changes.html#7_72_0

--- make/curl/curl.mk
+++ make/curl/curl.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_BIN, 7.71.1)
+$(call PKG_INIT_BIN, 7.72.0)
 $(PKG)_LIB_VERSION:=4.6.0
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
-$(PKG)_SOURCE_SHA256:=40f83eda27cdbeb25cd4da48cefb639af1b9395d6026d2da1825bf059239658c
+$(PKG)_SOURCE_SHA256:=0ded0808c4d85f2ee0db86980ae610cc9d165e9ca9da466196cc73c346513713
 $(PKG)_SITE:=http://curl.haxx.se/download
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src$(if $(FREETZ_PACKAGE_CURL_STATIC),,/.libs)/curl
