bba161ddf0272062e99c0e2f830509ae6cd6ff7c
fda77 <fda77@users.noreply.github.com>
2020-09-18 14:45:49 +0200

    kos: add more

--- config/generate_kos.sh
+++ config/generate_kos.sh
@@ -6,13 +6,21 @@ PARENT="$(dirname ${SCRIPT%/*})"
 PDIR="config/.kos"
 PROP="$PARENT/$PDIR"
 CINTMP="$PROP/Config.in"
-SYMS="TUN
- AUTOFS4_FS CIFS  EXT2_FS EXT3_FS EXT4_FS FUSE_FS JFFS2_FS
- EXPORTFS GRACE_PERIOD LOCKD NFS_FS NFS_V2 NFS_V3 NFSD SUNRPC
+SYMS="
+ MTD_BLOCK2MTD
+ CRYPTO_AEAD CRYPTO_AES CRYPTO_ARC4 CRYPTO_BLKCIPHER CRYPTO_CBC CRYPTO_ALGAPI CRYPTO_HASH
+ CRYPTO_WORKQUEUE CRYPTO_MANAGER CRYPTO_PCOMP CRYPTO_RNG CRYPTO_SHA1 CRYPTO_SHA256
+ 
+ EXT2_FS EXT3_FS JBD EXT4_FS CRC16 JBD2 FS_MBCACHE
+ FAT_FS VFAT_FS HFS_FS HFSPLUS_FS ISO9660_FS JFFS2_FS MINI_FO MINIX_FS MSDOS_FS REISERFS_FS UDF_FS UNION_FS
+ AUTOFS4_FS CIFS CRYPTO_CMAC CRYPTO_DES CRYPTO_ECB CRYPTO_HMAC CRYPTO_MD4 CRYPTO_MD5 FUSE_FS
+ LOCKD GRACE_PERIOD SUNRPC NFS_FS NFS_V2 NFS_V3 NFSD EXPORTFS
  NLS_CODEPAGE_437 NLS_ISO8859_1 NLS_ISO8859_15 NLS_UTF8
- CRYPTO_ALGAPI CRYPTO_AEAD CRYPTO_AES CRYPTO_ARC4 CRYPTO_BLKCIPHER CRYPTO_CMAC CRYPTO_CBC CRYPTO_DES CRYPTO_ECB CRYPTO_HASH
- CRYPTO_HMAC CRYPTO_MANAGER CRYPTO_MD4 CRYPTO_MD5 CRYPTO_PCOMP CRYPTO_RNG CRYPTO_SHA1 CRYPTO_SHA256 CRYPTO_WORKQUEUE"
-
+ CRC_CCITT LZO_COMPRESS LZO_DECOMPRESS
+ TUN
+ BT BT_BNEP BT_L2CAP BT_RFCOMM
+ NET_CLS_U32 NET_SCH_CBQ NET_SCH_HTB NET_SCH_LLQ NET_SCH_SFQ NET_SCH_TBF
+"
 rm -rf "$PROP"
 mkdir -p "$PROP"
 touch "$CINTMP"
@@ -20,26 +28,24 @@ touch "$CINTMP"
 for src in $PARENT/make/linux/configs/avm/*; do
 	src="${src%--not-available}"
 	[ ! -e "$src" ] && src="${src/avm/freetz}"
+	echo "source \"$PDIR/${src##*/}.in\"" >> "$CINTMP"
 
 	case ${src##*/} in
-		config-ar7-04.33)        sym="04_33 && !FREETZ_TYPE_7140 && FREETZ_SYSTEM_TYPE_AR7_SANGAM"  ;;
-		config-ohio-04.33)       sym="04_33 && !FREETZ_TYPE_7140 && FREETZ_SYSTEM_TYPE_AR7_OHIO"    ;;
-		config-ohio-7140_04.33)  sym="04_33 && FREETZ_TYPE_7140"                                    ;;
-		config-ohio-5140_04.67)  sym="04_67 && FREETZ_TYPE_5140"                                    ;;
-		config-ohio-04.67)       sym="04_67 && !FREETZ_TYPE_5140"                                   ;;
-		config-ohio-r7203)       sym="r7203"                                                        ;;
-		*)                       sym="$(echo $src | sed 's/.*-//;s/\./_/')"                         ;;
+		config-ar7-04.33)        box="04_33 \&\& !FREETZ_TYPE_7140 \&\& FREETZ_SYSTEM_TYPE_AR7_SANGAM"  ;;
+		config-ohio-04.33)       box="04_33 \&\& !FREETZ_TYPE_7140 \&\& FREETZ_SYSTEM_TYPE_AR7_OHIO"    ;;
+		config-ohio-7140_04.33)  box="04_33 \&\& FREETZ_TYPE_7140"                                      ;;
+		config-ohio-5140_04.67)  box="04_67 \&\& FREETZ_TYPE_5140"                                      ;;
+		config-ohio-04.67)       box="04_67 \&\& !FREETZ_TYPE_5140"                                     ;;
+		config-ohio-r7203)       box="r7203"                                                            ;;
+		*)                       box="$(echo $src | sed 's/.*-//;s/\./_/')"                             ;;
 	esac
-	echo "source \"$PDIR/${src##*/}.in\"" >> "$CINTMP"
 
 	echo -n .
 	out="$PROP/${src##*/}.in"
-	echo -e "if FREETZ_AVM_SOURCE_$sym\n" > "$out"
 	for sym in $SYMS; do
-		grep -q "^CONFIG_$sym=y$" "$src" && echo -e "config FREETZ_AVM_HAS_${sym}_BUILTIN\n\tdef_bool y\n" >> "$out"
-	done
-	echo "endif" >> "$out"
-
+		grep -q "^CONFIG_$sym=y$" "$src" && echo -e "config FREETZ_AVM_HAS_${sym}_BUILTIN"
+	done | sort > "$out"
+	sed -i "s/$/\n\tdef_bool y\n/g;1s/^/if FREETZ_AVM_SOURCE_$box\n\n/;$ s/$/\nendif/g" "$out"
 done
 echo " done."
 
