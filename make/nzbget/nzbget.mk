ab6c5f3f4d7b16cd0f8eac713624b207eb8a58d4
fda77 <fda77@users.noreply.github.com>
2020-10-12 14:14:35 +0200

    bump nzbget

--- make/nzbget/nzbget.mk
+++ make/nzbget/nzbget.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 16.4)
+$(call PKG_INIT_BIN, 21.0)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION)-src.tar.gz
-$(PKG)_SOURCE_SHA1:=0a7cf3d269aa531c9c5d30de80dcfe1f513d92f0
+$(PKG)_SOURCE_SHA1:=df1b43fbca6c0696006b60a45b3e03ebc915f0c4
 $(PKG)_SITE:=https://github.com/nzbget/nzbget/releases/download/v$($(PKG)_VERSION)
 
 $(PKG)_PATCH_POST_CMDS += $(call PKG_ADD_EXTRA_FLAGS,(CXX|LD)FLAGS)
