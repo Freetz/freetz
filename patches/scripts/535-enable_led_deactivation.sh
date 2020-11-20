2f9f309ce40fe65e727e8f04a07bf4fc179824c3
fda77 <fda77@users.noreply.github.com>
2020-11-21 00:17:15 +0100

    fix dsl_line_settings-speed patch

--- patches/scripts/535-enable_led_deactivation.sh
+++ patches/scripts/535-enable_led_deactivation.sh
@@ -1,4 +1,17 @@
 [ "$FREETZ_ENABLE_LED_DEACTIVATION" == "y" -a -f "${FILESYSTEM_MOD_DIR}/usr/www/all/system/led_display.lua" ] || return 0
 
 echo1 "re-adding LED (de-)activation page"
-modpatch "${FILESYSTEM_MOD_DIR}" "${PATCHES_COND_DIR}/enable_led_deactivation-${FREETZ_TYPE_PREFIX_SERIES_SUBDIR}.patch"
+
+if [ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ] ; then
+	PVER="07_0X"
+elif [ "$FREETZ_AVM_VERSION_06_9X_MIN" == "y" ] ; then
+	PVER="06_9X"
+elif [ "$FREETZ_AVM_VERSION_06_8X_MIN" == "y" ] ; then
+	PVER="06_8X"
+elif [ "$FREETZ_AVM_VERSION_06_5X_MIN" == "y" ] ; then
+	PVER="06_5X"
+elif [ "$FREETZ_AVM_VERSION_06_0X_MIN" == "y" ] ; then
+	PVER="06_0X"
+fi
+
+modpatch "${FILESYSTEM_MOD_DIR}" "${PATCHES_COND_DIR}/enable_led_deactivation-${PVER}.patch"
