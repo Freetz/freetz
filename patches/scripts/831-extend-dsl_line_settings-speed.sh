836276a1d516f910b05a58477f760d48cca74c61
fda77 <fda77@users.noreply.github.com>
2020-11-21 00:18:01 +0100

    fix extend-dsl_line_settings-speed patch

--- patches/scripts/831-extend-dsl_line_settings-speed.sh
+++ patches/scripts/831-extend-dsl_line_settings-speed.sh
@@ -5,5 +5,17 @@ echo1 "patching dsl settings"
 # allow applying patch
 decrip_file "${HTML_LANG_MOD_DIR}/css/default/dsl_line_settings.css"
 
-modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_COND_DIR}/831-extend-dsl_line_settings-speed_${FREETZ_TYPE_PREFIX_SERIES_SUBDIR}.patch"
+if [ "$FREETZ_AVM_VERSION_07_2X_MIN" == "y" ] ; then
+	PVER="07_2X"
+elif [ "$FREETZ_AVM_VERSION_07_1X_MIN" == "y" ] ; then
+	PVER="07_1X"
+elif [ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ] ; then
+	PVER="07_0X"
+elif [ "$FREETZ_AVM_VERSION_06_8X_MIN" == "y" ] ; then
+	PVER="06_8X"
+elif [ "$FREETZ_AVM_VERSION_06_5X_MIN" == "y" ] ; then
+	PVER="06_5X"
+fi
+
+modpatch "$FILESYSTEM_MOD_DIR" "${PATCHES_COND_DIR}/831-extend-dsl_line_settings-speed_${PVER}.patch"
 
