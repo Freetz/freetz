e1b5e4c3b6ff1dc0f30078096eaa0ddd8ec1aeb7
fda77 <fda77@users.noreply.github.com>
2021-01-03 01:05:02 +0100

    nfs-utils: use libtirpc  - refs https://github.com/Freetz-NG/freetz-ng/issues/137

--- make/nfs-utils/nfs-utils.mk
+++ make/nfs-utils/nfs-utils.mk
@@ -3,6 +3,8 @@ $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_SOURCE_MD5:=2fabdadb8ff415a1eafcfb12ab1bf781
 $(PKG)_SITE:=@SF/nfs
 
+$(PKG)_DEPENDS_ON += $(if $(FREETZ_TARGET_UCLIBC_SUPPORTS_rpc),,libtirpc)
+
 $(PKG)_BINARIES            := exportfs mountd nfsd showmount
 $(PKG)_BINARIES_BUILD_DIR  := $(addprefix $($(PKG)_DIR)/utils/, $(join $($(PKG)_BINARIES),$(addprefix /,$($(PKG)_BINARIES))))
 $(PKG)_BINARIES_TARGET_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DEST_DIR)/usr/sbin/%)
@@ -15,7 +17,7 @@ $(PKG)_DEPENDS_ON += tcp_wrappers
 #$(PKG)_CONFIGURE_OPTIONS += --enable-ipv6=yes
 #else
 $(PKG)_CONFIGURE_OPTIONS += --enable-ipv6=no
-$(PKG)_CONFIGURE_OPTIONS += --disable-tirpc
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_TARGET_UCLIBC_SUPPORTS_rpc),--disable-tirpc,--enable-tirpc)
 #endif
 
 $(PKG)_CONFIGURE_ENV += ac_cv_type_getgroups=gid_t
@@ -29,12 +31,17 @@ $(PKG)_CONFIGURE_OPTIONS += --disable-mount
 $(PKG)_CONFIGURE_OPTIONS += --disable-gss
 $(PKG)_CONFIGURE_OPTIONS += --disable-uuid
 
+ifneq ($(strip $(FREETZ_TARGET_UCLIBC_SUPPORTS_rpc)),y)
+$(PKG)_CFLAGS += -I$(TARGET_TOOLCHAIN_STAGING_DIR)/include/tirpc
+endif
+
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARIES_BUILD_DIR): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(NFS_UTILS_DIR)
+	$(SUBMAKE) -C $(NFS_UTILS_DIR) \
+		CFLAGS="$(TARGET_CFLAGS) $(NFS_UTILS_CFLAGS)"
 
 $(foreach binary,$($(PKG)_BINARIES_BUILD_DIR),$(eval $(call INSTALL_BINARY_STRIP_RULE,$(binary),/usr/sbin)))
 
