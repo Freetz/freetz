a3e6854e3413c50b5db0c56fc81b19f9711b4719
fda77 <fda77@users.noreply.github.com>
2021-01-05 23:09:22 +0100

    trickle: use libtirpc  - closes https://github.com/Freetz-NG/freetz-ng/issues/142

--- make/trickle/trickle.mk
+++ make/trickle/trickle.mk
@@ -12,6 +12,7 @@ $(PKG)_LIBS_BUILD_DIR:=$($(PKG)_LIBS:%=$($(PKG)_DIR)/%)
 $(PKG)_LIBS_TARGET_DIR:=$($(PKG)_LIBS:%=$($(PKG)_DEST_LIBDIR)/%)
 
 $(PKG)_DEPENDS_ON += libevent
+$(PKG)_DEPENDS_ON += $(if $(FREETZ_TARGET_UCLIBC_SUPPORTS_rpc),,libtirpc)
 
 # touch some patched files to prevent configure from being regenerated
 $(PKG)_CONFIGURE_PRE_CMDS += touch -t 200001010000.00 ./configure.in ./config.h.in ./acconfig.h;
@@ -22,12 +23,17 @@ $(PKG)_CONFIGURE_ENV   += UCLIBC_MAJOR=$(FREETZ_TARGET_UCLIBC_MAJOR_VERSION)
 
 $(PKG)_CONFIGURE_OPTIONS += --with-libevent="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
 
+ifneq ($(strip $(FREETZ_TARGET_UCLIBC_SUPPORTS_rpc)),y)
+$(PKG)_CFLAGS += -ltirpc -I$(TARGET_TOOLCHAIN_STAGING_DIR)/include/tirpc
+endif
+
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARIES_BUILD_DIR) $($(PKG)_LIBS_BUILD_DIR): $($(PKG)_DIR)/.configured
-	$(SUBMAKE1) -C $(TRICKLE_DIR)
+	$(SUBMAKE1) -C $(TRICKLE_DIR) \
+		CFLAGS="$(TARGET_CFLAGS) $(TRICKLE_CFLAGS)"
 
 $($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/%
 	$(INSTALL_BINARY_STRIP)
