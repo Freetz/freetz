8dade124822583e7530b8ac66e14da7aa53d7af2
fda77 <fda77@users.noreply.github.com>
2021-01-10 17:52:50 +0100

    dect fix only for IKS, thx PeterPawn  - refs https://github.com/Freetz-NG/freetz-ng/issues/120

--- patches/scripts/090-var_install_fixes.sh
+++ patches/scripts/090-var_install_fixes.sh
@@ -6,7 +6,7 @@ if grep -q "bs=256 skip=1 conv=sync" "${var_install_file}" 2>/dev/null; then
 fi
 
 # fix 7390 reboot problem on module unload
-modsed '/rmmod .*dect/d' "$VARTAR_MOD_DIR/var/post_install"
+[ "$FREETZ_SYSTEM_TYPE_IKS" == "y" ] && modsed '/rmmod .*dect/d' "$VARTAR_MOD_DIR/var/post_install"
 
 if grep -q "cannot mount squashfs, trying ext2" "${var_install_file}" 2>/dev/null; then
 	echo1 "adding alternative offset-based ext2-mount-method (reduces memory footprint while flashing the firmware)"
