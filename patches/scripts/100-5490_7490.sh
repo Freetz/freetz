684184cc23d98a0ec1fd357ec7e2ec6587419ba3
fda77 <fda77@users.noreply.github.com>
2021-01-12 12:46:02 +0100

    fix 5490 alien patch, thx hippie

--- patches/scripts/100-5490_7490.sh
+++ patches/scripts/100-5490_7490.sh
@@ -4,7 +4,7 @@ echo1 "adapt firmware for 5490"
 
 echo2 "moving default config dir"
 mv ${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_HW185 \
-   ${FILESYSTEM_MOD_DIR}/etc/default.
+   ${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_HW226
 
 echo2 "patching rc.S and rc.conf"
 modsed 's/CONFIG_INSTALL_TYPE=.*$/CONFIG_INSTALL_TYPE="mips34_512MB_xilinx_fiber_dect446_4geth_2ab_isdn_nt_2usb_host_wlan11n_65480"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
