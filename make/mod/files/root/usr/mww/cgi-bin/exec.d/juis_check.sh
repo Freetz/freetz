fe86578cb82d7f5f058398aa469e1945e7e2fb54
fda77 <fda77@users.noreply.github.com>
2021-02-10 17:21:20 +0100

    juis: use local option & add alien support

--- make/mod/files/root/usr/mww/cgi-bin/exec.d/juis_check.sh
+++ make/mod/files/root/usr/mww/cgi-bin/exec.d/juis_check.sh
@@ -3,11 +3,11 @@ echo '<pre>'
 echo 'Please wait ...'
 echo
 
+[ -r /etc/options.cfg ] && . /etc/options.cfg
 do__juis_check() {
-	local MAJOR VER REV BUILDTYPE TMP MAC NAME HWREV OEM LANG COUNTRY ANNEX FLAG NONCE JUIS
+	local MAJOR VER REV BUILDTYPE TMP MAC NONCE JUIS
 
 	MAJOR="$(sed -nr 's/^firmware_info[ \t]*([^\.]*).*/\1/p' /proc/sys/urlader/environment)"
-
 	VER="$(sed -n 's/^firmware_info[ \t]*[^\.]*.//p' /proc/sys/urlader/environment)"
 	REV="$(/etc/version --project)"
 	[ -z "$REV" ] && REV="$(/etc/version -vsub | sed 's/-//')"
@@ -15,9 +15,11 @@ do__juis_check() {
 	echo "Local version: $MAJOR.$VER-$REV"
 
 	BUILDTYPE="$(sed -n 's/"//g;s/.* CONFIG_BUILDTYPE=//p' /etc/init.d/rc.conf 2>/dev/null)"
-	if [ -n "$BUILDTYPE" -a "$BUILDTYPE" != "1" ]; then
+	[ -z "$BUILDTYPE" ] && BUILDTYPE='1'
+	if [ "$BUILDTYPE" != "1" -o "$FREETZ_TYPE_ALIEN_HARDWARE" == "y" ]; then
+		[ "$FREETZ_TYPE_ALIEN_HARDWARE" == "y" ] && TMP='30' || TMP='1'
 		VER="$(echo ${VER} | sed 's/\.//;s/^0*//')"
-		VER="$(echo 0$(( $VER - 1 )) | sed -r 's/.*(..)(..)$/\1.\2/')"
+		VER="$(echo 0$(( $VER - $TMP )) | sed -r 's/.*(..)(..)$/\1.\2/')"
 	fi
 	if [ "$REV" -gt 0 2>/dev/null ]; then
 		while [ "${#TMP}" != "3" ]; do TMP="$(echo $(( RANDOM % 10 ))$(( RANDOM % 10 ))$(( RANDOM % 10 )) | sed 's/[^0-9]//')"; done
@@ -28,26 +30,12 @@ do__juis_check() {
 	while [ "${#MAC}" != "6" ]; do MAC="$(hexdump -n3 -e '/1 "%02X"' /dev/urandom | sed 's/[^0-9A-F]//')"; done
 	MAC="$MAC$(sed -nr 's/^maca[ \t]*(..):(..):(..):.*/\1\2\3/p' /proc/sys/urlader/environment)"
 
-	NAME="$(grep ' _PRODUKT_NAME=' /etc/init.d/rc.init 2>/dev/null | sed -rn 's/^HW=[^a][^ ]* OEM=all _PRODUKT_NAME=//p' | sed 's/\#/ /g')"
-	[ -z "$NAME" ] && NAME="$(sed -rn 's/^export CONFIG_PRODUKT_NAME=\"?([^\"]*)\"?$/\1/p' /etc/init.d/rc.conf | head -n1)"
-	NAME="${NAME// /$(printf '\342\200\212')}"
-
-	HWREV="$(sed -n 's/^HWRevision[ \t]*//p' /proc/sys/urlader/environment)"
-
-	OEM="$(sed -n 's/^firmware_version[ \t]*//p' /proc/sys/urlader/environment)"
-	[ "$OEM" == "avm" ] && LANG='de' && COUNTRY='049'
-	[ "$OEM" != "avm" ] && LANG='en' && COUNTRY='99'
-
-	ANNEX="$(sed -n 's/^annex[ \t]*//p' /proc/sys/urlader/environment)"
-	[ "$ANNEX" == "Kabel" ] && FLAG='cable_retail' || FLAG='empty'
-
 	NONCE="$(dd if=/dev/urandom bs=16 count=1 2>/dev/null | base64)"
 
-	JUIS="Version=$MAJOR.$VER-$REV Serial=$MAC Name=$NAME HW=$HWREV OEM=$OEM Lang=$LANG Country=$COUNTRY Annex=$ANNEX Flag=$FLAG Buildtype=$BUILDTYPE Nonce=$NONCE"
+	JUIS="Version=$MAJOR.$VER-$REV Serial=$MAC Buildtype=$BUILDTYPE Nonce=$NONCE"
 	echo
-#	echo -e "JUIS: $JUIS\n"
-#	juis_check -d -n -i -s /tmp/.juis_check $JUIS 2>&1            | html | sed 's/\[[0-9]*m//g;s/.*juis_check.: //'
-	juis_check    -n -i -s /tmp/.juis_check $JUIS 2>&1 >/dev/null | html | sed 's/\[[0-9]*m//g;s/.*juis_check.: //'
+	juis_check -d -i -l -s /tmp/.juis_check $JUIS 2>&1            | sed -r 's/>(<[^\/])/>\n\t\1/g'           | html
+#	juis_check -n -i -l -s /tmp/.juis_check $JUIS 2>&1 >/dev/null | sed 's/\[[0-9]*m//g;s/.*juis_check.: //' | html
 	touch /tmp/.juis_check
 }
 do__juis_check
