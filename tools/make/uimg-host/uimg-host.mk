965b41254525535e844c83e15af0cad276a67f61
fda77 <fda77@users.noreply.github.com>
2021-02-14 21:50:49 +0100

    host tools: rename uimg

--- tools/make/uimg-host/uimg-host.mk
+++ tools/make/uimg-host/uimg-host.mk
@@ -1,49 +1,40 @@
-UIMG_VERSION:=871930df297e3a03bc315be75ecfc8c5c7a809ab
-UIMG_SOURCE:=uimg-$(UIMG_VERSION).tar.xz
-UIMG_SOURCE_SHA256:=1f5b3b473f50c6ff79a7859be336245c39fc1d2b6c89baf862de6de1f7caf8e0
-UIMG_SITE:=git@https://bitbucket.org/fesc2000/uimg-tool.git
+UIMG_HOST_VERSION:=871930df297e3a03bc315be75ecfc8c5c7a809ab
+UIMG_HOST_SOURCE:=uimg-$(UIMG_HOST_VERSION).tar.xz
+UIMG_HOST_SOURCE_SHA256:=1f5b3b473f50c6ff79a7859be336245c39fc1d2b6c89baf862de6de1f7caf8e0
+UIMG_HOST_SITE:=git@https://bitbucket.org/fesc2000/uimg-tool.git
 
-UIMG_MAKE_DIR:=$(TOOLS_DIR)/make/uimg
-UIMG_DIR:=$(TOOLS_SOURCE_DIR)/uimg-$(UIMG_VERSION)
+UIMG_HOST_MAKE_DIR:=$(TOOLS_DIR)/make/uimg
+UIMG_HOST_DIR:=$(TOOLS_SOURCE_DIR)/uimg-$(UIMG_HOST_VERSION)
 
-uimg-source: $(DL_DIR)/$(UIMG_SOURCE)
-$(DL_DIR)/$(UIMG_SOURCE): | $(DL_DIR)
-	$(DL_TOOL) $(DL_DIR) $(UIMG_SOURCE) $(UIMG_SITE) $(UIMG_SOURCE_SHA256)
 
-uimg-unpacked: $(UIMG_DIR)/.unpacked
-$(UIMG_DIR)/.unpacked: $(DL_DIR)/$(UIMG_SOURCE) | $(TOOLS_SOURCE_DIR) $(UNPACK_TARBALL_PREREQUISITES)
-	$(call UNPACK_TARBALL,$(DL_DIR)/$(UIMG_SOURCE),$(TOOLS_SOURCE_DIR))
-	$(call APPLY_PATCHES,$(UIMG_MAKE_DIR)/patches,$(UIMG_DIR))
+uimg-host-source: $(DL_DIR)/$(UIMG_HOST_SOURCE)
+$(DL_DIR)/$(UIMG_HOST_SOURCE): | $(DL_DIR)
+	$(DL_TOOL) $(DL_DIR) $(UIMG_HOST_SOURCE) $(UIMG_HOST_SITE) $(UIMG_HOST_SOURCE_SHA256)
+
+uimg-host-unpacked: $(UIMG_HOST_DIR)/.unpacked
+$(UIMG_HOST_DIR)/.unpacked: $(DL_DIR)/$(UIMG_HOST_SOURCE) | $(TOOLS_SOURCE_DIR) $(UNPACK_TARBALL_PREREQUISITES)
+	$(call UNPACK_TARBALL,$(DL_DIR)/$(UIMG_HOST_SOURCE),$(TOOLS_SOURCE_DIR))
+	$(call APPLY_PATCHES,$(UIMG_HOST_MAKE_DIR)/patches,$(UIMG_HOST_DIR))
 	touch $@
 
-
-
-
-
-
-
-
-
-
-
-
-$(UIMG_DIR)/uimg: $(UIMG_DIR)/.unpacked
-	$(MAKE) -C $(UIMG_DIR) all
+$(UIMG_HOST_DIR)/uimg: $(UIMG_HOST_DIR)/.unpacked
+	$(MAKE) -C $(UIMG_HOST_DIR) all
 	touch -c $@
 
-$(TOOLS_DIR)/uimg: $(UIMG_DIR)/uimg
+$(TOOLS_DIR)/uimg: $(UIMG_HOST_DIR)/uimg
 	$(INSTALL_FILE)
-	strip $@
 
-uimg: $(TOOLS_DIR)/uimg
+uimg-host: $(TOOLS_DIR)/uimg
 
-uimg-clean:
-	-$(MAKE) -C $(UIMG_DIR) clean
 
-uimg-dirclean:
-	$(RM) -r $(UIMG_DIR)
+uimg-host-clean:
+	-$(MAKE) -C $(UIMG_HOST_DIR) clean
 
-uimg-distclean: uimg-dirclean
+uimg-host-dirclean:
+	$(RM) -r $(UIMG_HOST_DIR)
+
+uimg-host-distclean: uimg-host-dirclean
 	$(RM) $(TOOLS_DIR)/uimg
 
-.PHONY: uimg-source uimg-unpacked uimg uimg-clean uimg-dirclean uimg-distclean
+.PHONY: uimg-host-source uimg-host-unpacked uimg-host uimg-host-clean uimg-host-dirclean uimg-host-distclean
+
