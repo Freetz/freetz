beadebd2a52ccb2801286f2f9ff488e2d642607e
fda77 <fda77@users.noreply.github.com>
2021-02-18 13:31:19 +0100

    juis_check: fix makefile

--- make/juis_check/juis_check.mk
+++ make/juis_check/juis_check.mk
@@ -2,10 +2,13 @@ $(call PKG_INIT_BIN, 049839bdf5811ed0c7d9b3036249c12a294ecffe)
 $(PKG)_BINARY:=$($(PKG)_DIR)/juis/$(pkg)
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/$(pkg)
 
+$(PKG)_HOST_DEPENDS_ON += yourfritz-host
+
 $(pkg)-unpacked: $($(PKG)_DIR)/.unpacked
 $($(PKG)_DIR)/.unpacked:
 	mkdir -p $(JUIS_CHECK_DIR)
 	tar cf - -C $(TOOLS_DIR)/yf/ juis/ | tar xf - -C $(JUIS_CHECK_DIR)/
+	touch $@
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.unpacked
 
