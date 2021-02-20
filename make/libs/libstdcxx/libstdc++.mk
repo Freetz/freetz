8471a25606a16e5890134f7960eaeb5d19eee9e7
fda77 <fda77@users.noreply.github.com>
2021-02-20 16:54:28 +0100

    fix libstdcxx

--- make/libs/libstdcxx/libstdc++.mk
+++ make/libs/libstdcxx/libstdc++.mk
@@ -1,7 +1,8 @@
 $(call PKG_INIT_LIB,$(call qstrip,$(FREETZ_GNULIBSTDCXX_VERSION)),libstdcxx)
 
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libstdc++.so.$($(PKG)_VERSION)
-$(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libstdc++.so.$($(PKG)_VERSION)
+$(PKG)_TARGET_BINARY:=$(if $(FREETZ_SEPARATE_AVM_UCLIBC),$($(PKG)_TARGET_DIR),$(TARGET_SPECIFIC_ROOT_DIR)/lib)/libstdc++.so.$($(PKG)_VERSION)
+
 
 $($(PKG)_STAGING_BINARY): gcc
 
@@ -12,9 +13,12 @@ $(pkg): $($(PKG)_STAGING_BINARY)
 
 $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
+
 $(pkg)-clean:
 
 $(pkg)-uninstall:
+	$(RM) $(TARGET_SPECIFIC_ROOT_DIR)/lib/libstdc++.so*
 	$(RM) $(LIBSTDCXX_TARGET_DIR)/libstdc++.so*
 
 $(PKG_FINISH)
+
