84b3acac9e4d539f5635263bc0038b9ca702bd2e
fda77 <fda77@users.noreply.github.com>
2021-02-23 15:05:22 +0100

    add remove-oemcheck

---
+++ patches/scripts/800-remove_oemcheck.sh
@@ -0,0 +1,7 @@
+[ "$FREETZ_AVM_VERSION_07_2X_MIN" == "y" ] || return 0
+
+echo1 "removing oemcheck"
+rm_files \
+  "${FILESYSTEM_MOD_DIR}/etc/boot.d/core/oemcheck"
+supervisor_delete_service "oemcheck"
+
