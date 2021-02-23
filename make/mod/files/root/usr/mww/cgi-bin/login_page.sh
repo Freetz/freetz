91413e64129588be4feb8b9169eef2aefbfce150
fda77 <fda77@users.noreply.github.com>
2021-02-23 15:21:57 +0100

    webif: more space for session-login page

--- make/mod/files/root/usr/mww/cgi-bin/login_page.sh
+++ make/mod/files/root/usr/mww/cgi-bin/login_page.sh
@@ -7,18 +7,21 @@
 echo "Set-Cookie: SID=$SENDSID;Path=/"
 
 cgi_begin 'login'
-# Waren wir schonmal hier? Dann war was falsch!
-[ "$WRONGPW" = 1 ] && echo "<b>$(lang de:"Passwort falsch!" en:"Wrong password!")</b><p>"
 
 . /usr/mww/cgi-bin/md5hash.sh
 
-cat << 'EOF'
+cat << EOF
+<br><br>
 $(lang de:"Passwort" en:"Password"): <input  type="password" id="inp_pw" maxlength="45"  onkeydown="if (event.keyCode == 13) document.getElementById('id_go').click()">
-                                   
+&nbsp;
 <input type="button" name="go" id="id_go" value="$(lang de:"Anmelden" en:"Login")"
 EOF
 echo "onclick='location.href=\"/cgi-bin/login.cgi?hash=\"+makemd5(document.getElementById(\"inp_pw\").value, \"$SENDSID\"); '>"
 echo "<script> document.getElementById(\"inp_pw\").focus(); </script>"
+echo '<br><br>'
+
+# Waren wir schonmal hier? Dann war was falsch!
+[ "$WRONGPW" = 1 ] && echo "<b>$(lang de:"Passwort falsch!" en:"Wrong password!")</b><p>"
 
 cgi_end
 
