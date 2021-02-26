82f9f680e38b568608d4167d4512346c769e2fed
fda77 <fda77@users.noreply.github.com>
2021-02-26 21:21:12 +0100

    fix libstdcxx

--- make/libs/libstdcxx/libstdc++.mk
+++ make/libs/libstdcxx/libstdc++.mk
@@ -1,7 +1,7 @@
 $(call PKG_INIT_LIB,$(call qstrip,$(FREETZ_GNULIBSTDCXX_VERSION)),libstdcxx)
 
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libstdc++.so.$($(PKG)_VERSION)
-$(PKG)_TARGET_BINARY:=$(if $(FREETZ_SEPARATE_AVM_UCLIBC),$($(PKG)_TARGET_DIR),$(TARGET_SPECIFIC_ROOT_DIR)/lib)/libstdc++.so.$($(PKG)_VERSION)
+$(PKG)_TARGET_BINARY:=$(if $(FREETZ_SEPARATE_AVM_UCLIBC),$($(PKG)_TARGET_DIR),$(TARGET_SPECIFIC_ROOT_DIR)/usr/lib)/libstdc++.so.$($(PKG)_VERSION)
 
 
 $($(PKG)_STAGING_BINARY): gcc
@@ -17,7 +17,7 @@ $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 $(pkg)-clean:
 
 $(pkg)-uninstall:
-	$(RM) $(TARGET_SPECIFIC_ROOT_DIR)/lib/libstdc++.so*
+	$(RM) $(TARGET_SPECIFIC_ROOT_DIR)/usr/lib/libstdc++.so*
 	$(RM) $(LIBSTDCXX_TARGET_DIR)/libstdc++.so*
 
 $(PKG_FINISH)
