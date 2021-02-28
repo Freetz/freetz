5f10951b01846ef2ca72bd9d47e5808232f9e4cf
fda77 <fda77@users.noreply.github.com>
2021-02-28 17:20:49 +0100

    patches: run alien scripts only if needed

---
+++ patches/scripts/100-alien_files.sh
@@ -0,0 +1,9 @@
+[ "$FREETZ_TYPE_ALIEN_HARDWARE" == "y" ] || return 0
+echo1 "applying alien files"
+
+for i in "${PATCHES_COND_DIR}/100-alien_files/"*.sh; do
+	[ -r "$i" ] || continue
+	echo2 -l "applying patch file $i" 
+	source $i
+done
+
