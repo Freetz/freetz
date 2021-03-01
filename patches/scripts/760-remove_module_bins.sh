dd5d73046aa4395bdb3d2ab84ba52620af652221
fda77 <fda77@users.noreply.github.com>
2021-03-01 06:12:25 +0100

    patches: add remove-modulebins

---
+++ patches/scripts/760-remove_module_bins.sh
@@ -0,0 +1,9 @@
+[ "$FREETZ_REMOVE_MODULE_BINS" == "y" ] || return 0
+echo1 "removing module bins"
+
+MODULES_DIR="$(find ${FILESYSTEM_MOD_DIR}/lib/modules/[2345].[0123456789]*.* -maxdepth 0 -type d)"
+rm_files \
+  $MODULES_DIR/modules.dep.bin  \
+  $MODULES_DIR/modules.alias.bin \
+  $MODULES_DIR/modules.symbols.bin
+
