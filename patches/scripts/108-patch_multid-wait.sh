40d1074cf161f749aacc0dcf8dcab635d7ab28f4
fda77 <fda77@users.noreply.github.com>
2021-03-01 09:12:13 +0100

    patches: disable multid-wait for new fos  - refs https://trac.boxmatrix.info/freetz-ng/browser/freetz-ng/trunk/patches/110-multid-wait.patch?rev=1415  - refs ip-phone-forum.de/threads/no-internet-connection-with-7140-in-ata-mode.148885/

--- patches/scripts/108-patch_multid-wait.sh
+++ patches/scripts/108-patch_multid-wait.sh
@@ -1,9 +1,13 @@
+[ "$FREETZ_AVM_VERSION_06_5X_MAX" == "y" ] || return 0
+echo1 "applying multid-wait for netif"
+
 if [ -e "$FILESYSTEM_MOD_DIR/sbin/multid" ]; then
-	echo1 "applying multid-wait patch"
+	echo2 "applying multid-wait patch"
 	modsed \
 	  's@\(eval.*multid $.*PARAM\)@\1\nlocal countdown=10; while [ $((countdown--)) -gt 0 ] \&\& ! ifconfig eth0 >/dev/null 2>\&1; do sleep 1; done@g' \
 	  "$FILESYSTEM_MOD_DIR/etc/init.d/rc.net" \
 	  'local countdown=10'
 else
-	echo1 "no multid found, skipping multid-wait patch"
+	echo2 "no multid found, skipping multid-wait patch"
 fi
+
