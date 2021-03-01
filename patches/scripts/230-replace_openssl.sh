71d7f841a49a8e04492824d720d80118f532ad14
fda77 <fda77@users.noreply.github.com>
2021-03-01 06:17:30 +0100

    patches: rename _openssl

---
+++ patches/scripts/230-replace_openssl.sh
@@ -0,0 +1,14 @@
+[ "$FREETZ_REPLACE_OPENSSL" == "y" -a "${FREETZ_LIBRARY_DIR}" != "/lib" ] || return 0
+echo1 "replacing openssl"
+
+if [ "$FREETZ_LIB_libcrypto" == "y" ]; then
+	echo1 "removing avm's libcrypto and create symlinks"
+	rm_files "${FILESYSTEM_MOD_DIR}/lib/libcrypto.so.0.9.8"
+	ln -sf "${FREETZ_LIBRARY_DIR}/libcrypto.so.0.9.8" "${FILESYSTEM_MOD_DIR}/lib/libcrypto.so.0.9.8"
+fi
+if [ "$FREETZ_LIB_libssl" == "y" ]; then
+	echo1 "removing avm's libssl and create symlinks"
+	rm_files "${FILESYSTEM_MOD_DIR}/lib/libssl.so.0.9.8"
+	ln -sf "${FREETZ_LIBRARY_DIR}/libssl.so.0.9.8" "${FILESYSTEM_MOD_DIR}/lib/libssl.so.0.9.8"
+fi
+
