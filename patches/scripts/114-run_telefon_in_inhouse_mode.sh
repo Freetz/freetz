38e71b8ebab563def7175feba27005ebfcb72b80
fda77 <fda77@users.noreply.github.com>
2021-03-02 23:27:38 +0100

    [telefon in in-house mode] add support for Fritz!OS 7.2x - Freetz-patch by @fda77 - Analysis and original (modfs) implementation by @PeterPawn

--- patches/scripts/114-run_telefon_in_inhouse_mode.sh
+++ patches/scripts/114-run_telefon_in_inhouse_mode.sh
@@ -1,8 +1,34 @@
 [ "${FREETZ_RUN_TELEFON_IN_INHOUSE_MODE}" == "y" ] || return 0
 
-# CONFIG_RELEASE=0: in-house development version
+# https://www.ip-phone-forum.de/threads/307385/post-2416060
+
+#since FOS 07-2X
+#*CONFIG_BUILDTYPE=998  : {'private','private' }
+#*CONFIG_BUILDTYPE=2    : {'beta'   ,'intern  '}
+# CONFIG_BUILDTYPE=1000 : {'labor'  ,'inhaus'  }
+# CONFIG_BUILDTYPE=1001 : {'labor'  ,'labbeta' }
+# CONFIG_BUILDTYPE=1004 : {'labor'  ,'labphone'}
+# CONFIG_BUILDTYPE=1005 : {'labor'  ,'labor'   }
+# CONFIG_BUILDTYPE=1006 : {'labor'  ,'labtest' }
+# CONFIG_BUILDTYPE=1007 : {'labor'  ,'labplus' }
+# CONFIG_BUILDTYPE=1    : {'release','release' }
+
+#up to FOS 07-1X
+#*CONFIG_RELEASE=0: in-house development version
 # CONFIG_RELEASE=1: official release
 # CONFIG_RELEASE=2: public beta/labor version
 
+# https://github.com/PeterPawn/modfs/blob/64c6c9a3ea09bdf46a119f80e998dd29d74bd9c2/files/telnetd_by_avm#L25
+if grep -a -q CONFIG_BUILDTYPE "${FILESYSTEM_MOD_DIR}/usr/bin/telefon"; then
+	var="CONFIG_BUILDTYPE"
+	val=998
+else
+	var="CONFIG_RELEASE"
+	val=0
+fi
+
 echo1 "forcing telefon daemon to run in in-house mode"
-modsed -r 's,^([ \t]*)(telefon[ \t]),\1CONFIG_RELEASE=0 \2,' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.voip" '^[ \t]*CONFIG_RELEASE=0 telefon[ \t]'
+modsed -r \
+  "s,^([ \t]*)(telefon[ \t]),\1$var=$val \2," \
+  "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.voip" \
+  "^[ \t]*$var=$val telefon[ \t]"
