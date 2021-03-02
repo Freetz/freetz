2e31587f0fd971bc3c2b220d9bafee32c08117e0
fda77 <fda77@users.noreply.github.com>
2021-03-02 09:13:50 +0100

    extend remove-ptest

--- patches/cond/300-remove-leftover/800-remove_ptest.sh
+++ patches/cond/300-remove-leftover/800-remove_ptest.sh
@@ -2,6 +2,7 @@
 echo1 "removing ptest"
 
 rm_files \
+  "${FILESYSTEM_MOD_DIR}/usr/bin/ptest-waitForNetwork.sh" \
   "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.ptest.release" \
   "${FILESYSTEM_MOD_DIR}/etc/init.d/S42-ptest" \
   "${FILESYSTEM_MOD_DIR}/etc/init.d/S46-ptest" \
