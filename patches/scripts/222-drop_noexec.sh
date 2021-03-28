f4da0aefacb3c6a184380fbb38eb65a6452726c7
fda77 <fda77@users.noreply.github.com>
2021-03-28 01:24:36 +0100

    fix drop-noexec (external)

--- patches/scripts/222-drop_noexec.sh
+++ patches/scripts/222-drop_noexec.sh
@@ -2,7 +2,7 @@
 echo1 "dropping noexec"
 
 files="${FILESYSTEM_MOD_DIR}/etc/init.d/S15-filesys ${FILESYSTEM_MOD_DIR}/etc/boot.d/ubi_functions ${FILESYSTEM_MOD_DIR}/etc/boot.d/yaffs_functions"
-[ "$FREETZ_DROP_NOEXEC_EXTERNAL" == "y" ] && files="$files ${FILESYSTEM_MOD_DIR}/etc/hotplug/udev-mount-sd"
+[ "$FREETZ_DROP_NOEXEC_EXTERNAL" == "y" ] && files="$files ${FILESYSTEM_MOD_DIR}/etc/init.d/rc.mount.sh ${FILESYSTEM_MOD_DIR}/etc/hotplug/udev-mount-sd"
 for file in $files; do
 	[ -e "$file" ] && modsed 's/, *noexec//g;s/-o  *noexec *,/-o /g;s/-o  *noexec *//g' "$file"
 done
