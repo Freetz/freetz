46284af3779602068d67c96dd0091f80079b5a63
fda77 <fda77@users.noreply.github.com>
2021-04-01 14:50:36 +0200

    drop noexec: replace noexec by exec, by @PeterPawn  - refs https://github.com/Freetz-NG/freetz-ng/commit/f4da0aefacb3c6a184380fbb38eb65a6452726c7

--- patches/scripts/222-drop_noexec.sh
+++ patches/scripts/222-drop_noexec.sh
@@ -4,6 +4,6 @@ echo1 "dropping noexec"
 files="${FILESYSTEM_MOD_DIR}/etc/init.d/S15-filesys ${FILESYSTEM_MOD_DIR}/etc/boot.d/ubi_functions ${FILESYSTEM_MOD_DIR}/etc/boot.d/yaffs_functions"
 [ "$FREETZ_DROP_NOEXEC_EXTERNAL" == "y" ] && files="$files ${FILESYSTEM_MOD_DIR}/etc/init.d/rc.mount.sh ${FILESYSTEM_MOD_DIR}/etc/hotplug/udev-mount-sd"
 for file in $files; do
-	[ -e "$file" ] && modsed 's/, *noexec//g;s/-o  *noexec *,/-o /g;s/-o  *noexec *//g' "$file"
+	[ -e "$file" ] && modsed -r "s|([ \t,;'\"])no(exec[ \t,;'\"])|\1\2|g" "$file"
 done
 
