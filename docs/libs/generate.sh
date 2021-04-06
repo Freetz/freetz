b12e8115af126a5a7497741cd5d4a27b66f6331e
fda77 <fda77@users.noreply.github.com>
2021-04-06 17:27:20 +0200

    docs: add indendation to scripts

--- docs/libs/generate.sh
+++ docs/libs/generate.sh
@@ -8,23 +8,23 @@ echo "# Libraries" >> "$MDPWD/README.md"
 ( echo "Index:" ; for CTN in $(echo {A..Z}); do echo "[$CTN](#${CTN,,})"; done ) >> "$MDPWD/README.md"
 
 for dir in $(find "$INPWD" -maxdepth 1 -mindepth 1 -type d | sort); do
-lib="${dir##*/}"
-[ "${lib:0:1}" != "$IDX" ] && IDX="${lib:0:1}" && echo -e "\n### ${IDX^^}"
+	lib="${dir##*/}"
+	[ "${lib:0:1}" != "$IDX" ] && IDX="${lib:0:1}" && echo -e "\n### ${IDX^^}"
 
-dsc="$(sed -rn 's/[ \t]*bool "(.*)"[ \t]*/\1/p' "$INPWD/$lib/Config.in" 2>/dev/null | head -n1)"
-[ -z "$dsc" ] && dsc="$lib" && echo "ignored: $lib" 1>&2
-[ "$dsc" != "$(echo "$dsc" | sed "s/^$lib//I")" ] && itm="$dsc" || itm="$lib: $dsc"
-anc="${lib//_/-}"
-echo -e "\n  * **<u>$itm</u><a id='${anc%-cgi}'></a>**<br>"
+	dsc="$(sed -rn 's/[ \t]*bool "(.*)"[ \t]*/\1/p' "$INPWD/$lib/Config.in" 2>/dev/null | head -n1)"
+	[ -z "$dsc" ] && dsc="$lib" && echo "ignored: $lib" 1>&2
+	[ "$dsc" != "$(echo "$dsc" | sed "s/^$lib//I")" ] && itm="$dsc" || itm="$lib: $dsc"
+	anc="${lib//_/-}"
+	echo -e "\n  * **<u>$itm</u><a id='${anc%-cgi}'></a>**<br>"
 
-L="$(grep -P '^[ \t]*help[ \t]*$' -nm1 "$INPWD/$lib/Config.in" 2>/dev/null | sed 's/:.*//')"
-C="$(wc -l "$INPWD/$lib/Config.in" 2>/dev/null  | sed 's/ .*//')"
-[ -z "$L" -o -z "$C" ] && echo "nohelp1: $lib" 1>&2 && continue
-T=$(( $C - $L))
-N="$(tail -n "$T" "$INPWD/$lib/Config.in" | grep -P "^[ \t]*(#|(end)*if|config|bool|string|int|depends on|(end)*menu|comment|menuconfig|(end)*choice|prompt|select|default|source|help)" -n | head -n1 | sed 's/:.*//')"
-help="$(tail -n "$T" "$INPWD/$lib/Config.in" | head -n "$(( ${N:-99} - 1 ))" | grep -vP '^[ \t]*$' | sed 's/[ \t]*$//g;s/^[ \t]*//g;s/$/ /g' | tr -d '\n' | sed 's/ $//')"
-[ -z "$help" ] && echo "nohelp2: $lib" 1>&2 && continue
-echo "    $help"
+	L="$(grep -P '^[ \t]*help[ \t]*$' -nm1 "$INPWD/$lib/Config.in" 2>/dev/null | sed 's/:.*//')"
+	C="$(wc -l "$INPWD/$lib/Config.in" 2>/dev/null  | sed 's/ .*//')"
+	[ -z "$L" -o -z "$C" ] && echo "nohelp1: $lib" 1>&2 && continue
+	T=$(( $C - $L))
+	N="$(tail -n "$T" "$INPWD/$lib/Config.in" | grep -P "^[ \t]*(#|(end)*if|config|bool|string|int|depends on|(end)*menu|comment|menuconfig|(end)*choice|prompt|select|default|source|help)" -n | head -n1 | sed 's/:.*//')"
+	help="$(tail -n "$T" "$INPWD/$lib/Config.in" | head -n "$(( ${N:-99} - 1 ))" | grep -vP '^[ \t]*$' | sed 's/[ \t]*$//g;s/^[ \t]*//g;s/$/ /g' | tr -d '\n' | sed 's/ $//')"
+	[ -z "$help" ] && echo "nohelp2: $lib" 1>&2 && continue
+	echo "    $help"
 
 done >> "$MDPWD/README.md"
 
--- docs/screenshots/generate.sh
+++ docs/screenshots/generate.sh
@@ -3,12 +3,12 @@
 MYPWD="$(dirname $(realpath $0))"
 
 for file in $MYPWD/*.???; do
-src="${file##*/}"
-dst="${src%.*}_md.${src##*.}"
-[ "${dst/_md_md./}" != "$dst" ] && continue
-[ -e "$MYPWD/$dst" ] && continue
-echo "$src -> $dst"
-convert "$MYPWD/$src" -resize x333 "$MYPWD/$dst"
-chmod -x "$MYPWD/$src" "$MYPWD/$dst"
+	src="${file##*/}"
+	dst="${src%.*}_md.${src##*.}"
+	[ "${dst/_md_md./}" != "$dst" ] && continue
+	[ -e "$MYPWD/$dst" ] && continue
+	echo "$src -> $dst"
+	convert "$MYPWD/$src" -resize x333 "$MYPWD/$dst"
+	chmod -x "$MYPWD/$src" "$MYPWD/$dst"
 done
 
