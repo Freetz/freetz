c2edb99fe90f0eae5f2024c96ebe516d40e2611d
Eugene Rudoy <gene.devel@gmail.com>
2021-05-06 23:06:31 +0200

    yourfritz-host: bump version to the latest git revision

--- tools/make/yourfritz-host/yourfritz-host.mk
+++ tools/make/yourfritz-host/yourfritz-host.mk
@@ -1,4 +1,4 @@
-YOURFRITZ_HOST_VERSION:=ed5d2ab931
+YOURFRITZ_HOST_VERSION:=27036fec3d
 YOURFRITZ_HOST_SOURCE:=yourfritz-$(YOURFRITZ_HOST_VERSION).tar.xz
 YOURFRITZ_HOST_SITE:=git_no_submodules@https://github.com/PeterPawn/YourFritz.git
 
