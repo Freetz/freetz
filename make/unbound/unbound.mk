63249bf7ece76eb8cbfd1d13abf6bd7aa07fe580
fda77 <fda77@users.noreply.github.com>
2021-05-22 19:50:12 +0200

    unbound: unbound-control-setup

--- make/unbound/unbound.mk
+++ make/unbound/unbound.mk
@@ -19,6 +19,10 @@ $(PKG)_LIBS:=lib$(pkg).so.8.1.12
 $(PKG)_LIBS_BUILD_DIR:=$($(PKG)_LIBS:%=$($(PKG)_DIR)/.libs/%)
 $(PKG)_LIBS_TARGET_DIR:=$($(PKG)_LIBS:%=$($(PKG)_DEST_LIBDIR)/%)
 
+$(PKG)_SCRIPTS:=$(pkg)-control-setup
+$(PKG)_SCRIPTS_BUILD_DIR:=$($(PKG)_SCRIPTS:%=$($(PKG)_DIR)/%)
+$(PKG)_SCRIPTS_TARGET_DIR:=$($(PKG)_SCRIPTS:%=$($(PKG)_DEST_DIR)/usr/bin/%)
+
 $(PKG)_FILES:=root.hints
 $(PKG)_FILES_BUILD_DIR:=$($(PKG)_FILES:%=$($(PKG)_DIR)/%)
 $(PKG)_FILES_TARGET_DIR:=$($(PKG)_FILES:%=$($(PKG)_DEST_DIR)/etc/$(pkg)/%)
@@ -40,17 +44,18 @@ $(PKG)_CONFIGURE_OPTIONS += --with-pthreads
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_LIB_libcrypto_WITH_EC),--enable-ecdsa,--disable-ecdsa)
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_OPENSSL_VERSION_1_MAX),--enable-gost,--disable-gost)
 
-$(PKG)_EXCLUDED += $(if $(FREETZ_PACKAGE_UNBOUND_ANCHOR)    ,,usr/bin/unbound-anchor)
-$(PKG)_EXCLUDED += $(if $(FREETZ_PACKAGE_UNBOUND_CHECKCONF) ,,usr/bin/unbound-checkconf)
-$(PKG)_EXCLUDED += $(if $(FREETZ_PACKAGE_UNBOUND_CONTROL)   ,,usr/bin/unbound-control)
-$(PKG)_EXCLUDED += $(if $(FREETZ_PACKAGE_UNBOUND_HOST)      ,,usr/bin/unbound-host)
+$(PKG)_EXCLUDED += $(if $(FREETZ_PACKAGE_UNBOUND_ANCHOR)        ,,usr/bin/unbound-anchor)
+$(PKG)_EXCLUDED += $(if $(FREETZ_PACKAGE_UNBOUND_CHECKCONF)     ,,usr/bin/unbound-checkconf)
+$(PKG)_EXCLUDED += $(if $(FREETZ_PACKAGE_UNBOUND_CONTROL)       ,,usr/bin/unbound-control)
+$(PKG)_EXCLUDED += $(if $(FREETZ_PACKAGE_UNBOUND_CONTROL_SETUP) ,,usr/bin/unbound-control-setup)
+$(PKG)_EXCLUDED += $(if $(FREETZ_PACKAGE_UNBOUND_HOST)          ,,usr/bin/unbound-host)
 
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
-$($(PKG)_BINARIES_BUILD_DIR) $($(PKG)_WRAPPED) $($(PKG)_LIBS_BUILD_DIR): $($(PKG)_DIR)/.configured
+$($(PKG)_BINARIES_BUILD_DIR) $($(PKG)_WRAPPED_BUILD_DIR) $($(PKG)_LIBS_BUILD_DIR) $($(PKG)_SCRIPTS_BUILD_DIR): $($(PKG)_DIR)/.configured
 	wget -N -q -c https://www.internic.net/domain/named.cache -O $(UNBOUND_DIR)/root.hints
 	$(SUBMAKE) -C $(UNBOUND_DIR)
 
@@ -63,12 +68,15 @@ $($(PKG)_WRAPPED_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/.libs/
 $($(PKG)_LIBS_TARGET_DIR): $($(PKG)_DEST_LIBDIR)/%: $($(PKG)_DIR)/.libs/%
 	$(INSTALL_LIBRARY_STRIP)
 
+$($(PKG)_SCRIPTS_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/%
+	$(INSTALL_FILE)
+
 $($(PKG)_FILES_TARGET_DIR): $($(PKG)_DEST_DIR)/etc/$(pkg)/%: $($(PKG)_DIR)/%
 	$(INSTALL_FILE)
 
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR) $($(PKG)_WRAPPED_TARGET_DIR) $($(PKG)_LIBS_TARGET_DIR) $($(PKG)_FILES_TARGET_DIR)
+$(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR) $($(PKG)_WRAPPED_TARGET_DIR) $($(PKG)_LIBS_TARGET_DIR) $($(PKG)_SCRIPTS_TARGET_DIR) $($(PKG)_FILES_TARGET_DIR)
 
 
 $(pkg)-clean:
@@ -76,7 +84,7 @@ $(pkg)-clean:
 	$(RM) $(UNBOUND_DIR)/.configured
 
 $(pkg)-uninstall:
-	$(RM) $(UNBOUND_BINARIES_TARGET_DIR) $(UNBOUND_WRAPPED_TARGET_DIR) $(UNBOUND_LIBS_TARGET_DIR) $(UNBOUND_FILES_TARGET_DIR)
+	$(RM) $(UNBOUND_BINARIES_TARGET_DIR) $(UNBOUND_WRAPPED_TARGET_DIR) $(UNBOUND_LIBS_TARGET_DIR) $(UNBOUND_SCRIPTS_TARGET_DIR) $(UNBOUND_FILES_TARGET_DIR)
 
 $(PKG_FINISH)
 
