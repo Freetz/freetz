f3eefc6c84990fdbc8711d59f0fd265d8045552e
Alex <alexander.noack@web.de>
2021-05-28 23:03:09 +0200

    libunbound als separate Install-Option (#311)
    * Separated libunbound as installable option
    
    * fixed library install
    
    * removed $(PKG) reference from $(pkg)-uninstall:
    
    Co-authored-by: Alexander Noack <alex@keeeb.com>

--- make/unbound/unbound.mk
+++ make/unbound/unbound.mk
@@ -1,4 +1,5 @@
 $(call PKG_INIT_BIN, 1.13.1)
+$(PKG)_LIB_VERSION:=8.1.12
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5:=0cd660a40d733acc6e7cce43731cac62
 $(PKG)_SITE:=https://www.$(pkg).net/downloads
@@ -17,9 +18,10 @@ $(PKG)_WRAPPED:=$(pkg)-anchor $(pkg)-host
 $(PKG)_WRAPPED_BUILD_DIR:=$($(PKG)_WRAPPED:%=$($(PKG)_DIR)/.libs/%)
 $(PKG)_WRAPPED_TARGET_DIR:=$($(PKG)_WRAPPED:%=$($(PKG)_DEST_DIR)/usr/bin/%)
 
-$(PKG)_LIBS:=lib$(pkg).so.8.1.12
-$(PKG)_LIBS_BUILD_DIR:=$($(PKG)_LIBS:%=$($(PKG)_DIR)/.libs/%)
-$(PKG)_LIBS_TARGET_DIR:=$($(PKG)_LIBS:%=$($(PKG)_DEST_LIBDIR)/%)
+$(PKG)_LIB_BINARY:=$($(PKG)_DIR)/.libs/libunbound.so.$($(PKG)_LIB_VERSION)
+$(PKG)_LIB_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libunbound.so.$($(PKG)_LIB_VERSION)
+$(PKG)_LIB_TARGET_BINARY:=$($(PKG)_TARGET_LIBDIR)/libunbound.so.$($(PKG)_LIB_VERSION)
+
 
 $(PKG)_SCRIPTS:=$(pkg)-control-setup
 $(PKG)_SCRIPTS_BUILD_DIR:=$($(PKG)_SCRIPTS:%=$($(PKG)_DIR)/%)
@@ -55,7 +57,7 @@ $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
-$($(PKG)_BINARIES_BUILD_DIR) $($(PKG)_WRAPPED_BUILD_DIR) $($(PKG)_LIBS_BUILD_DIR) $($(PKG)_SCRIPTS_BUILD_DIR): $($(PKG)_DIR)/.configured
+$($(PKG)_BINARIES_BUILD_DIR) $($(PKG)_WRAPPED_BUILD_DIR) $($(PKG)_SCRIPTS_BUILD_DIR): $($(PKG)_DIR)/.configured
 	wget -N -q -c https://www.internic.net/domain/named.cache -O $(UNBOUND_DIR)/root.hints
 	$(SUBMAKE) -C $(UNBOUND_DIR)
 
@@ -65,7 +67,15 @@ $($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/%
 $($(PKG)_WRAPPED_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/.libs/%
 	$(INSTALL_BINARY_STRIP)
 
-$($(PKG)_LIBS_TARGET_DIR): $($(PKG)_DEST_LIBDIR)/%: $($(PKG)_DIR)/.libs/%
+$($(PKG)_LIB_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(UNBOUND_DIR)
+
+$($(PKG)_LIB_STAGING_BINARY): $($(PKG)_LIB_BINARY)
+	$(SUBMAKE) -C $(UNBOUND_DIR) \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+		install
+
+$($(PKG)_LIB_TARGET_BINARY): $($(PKG)_LIB_STAGING_BINARY)
 	$(INSTALL_LIBRARY_STRIP)
 
 $($(PKG)_SCRIPTS_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/%
@@ -73,15 +83,14 @@ $($(PKG)_SCRIPTS_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/%
 
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR) $($(PKG)_WRAPPED_TARGET_DIR) $($(PKG)_LIBS_TARGET_DIR) $($(PKG)_SCRIPTS_TARGET_DIR)
-
+$(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR) $($(PKG)_WRAPPED_TARGET_DIR) $($(PKG)_LIB_TARGET_BINARY) $($(PKG)_SCRIPTS_TARGET_DIR)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(UNBOUND_DIR) clean
 	$(RM) $(UNBOUND_DIR)/.configured
 
 $(pkg)-uninstall:
-	$(RM) $(UNBOUND_BINARIES_TARGET_DIR) $(UNBOUND_WRAPPED_TARGET_DIR) $(UNBOUND_LIBS_TARGET_DIR) $(UNBOUND_SCRIPTS_TARGET_DIR)
+	$(RM) $(UNBOUND_BINARIES_TARGET_DIR) $(UNBOUND_WRAPPED_TARGET_DIR) $(UNBOUND_LIB_TARGET_BINARY) $(UNBOUND_SCRIPTS_TARGET_DIR)
 
+$(call PKG_ADD_LIB,libunbound)
 $(PKG_FINISH)
-
