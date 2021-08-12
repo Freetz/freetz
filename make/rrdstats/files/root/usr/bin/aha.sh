5a72340ef70d5f851accea97948eca3557ed545f
fda77 <fda77@users.noreply.github.com>
2021-08-12 08:03:46 +0200

    rrdstats: add bit18 description to aha.sh

--- make/rrdstats/files/root/usr/bin/aha.sh
+++ make/rrdstats/files/root/usr/bin/aha.sh
@@ -73,7 +73,7 @@ dec2bin() {
 		bin="$(( $dec % 2 ))$bin"
 		dec="$(( $dec / 2 ))"
 	done
-	while [ ${#bin} -le 17 ]; do bin="0$bin"; done
+	while [ ${#bin} -le 18 ]; do bin="0$bin"; done
 	echo -n "$bin"
 }
 
@@ -94,7 +94,7 @@ raw_out() {
 		for item in manufacturer productname fwversion functionbitmask identifier id; do
 			min_len "$item = "
 			readitm "$device " $item
-			[ "$item" == "functionbitmask" ] && min_len && dec2bin $(readitm "$device " $item) | sed -re 's/(.*)(....)(....)(.....)(.)$/\1 \2 \3 \4 \5\n/;s/0/-/g'
+			[ "$item" == "functionbitmask" ] && min_len && dec2bin $(readitm "$device " $item) | sed -re 's/(.*)(....)(....)(....)(....)(..)$/\1 \2 \3 \4 \5 \6\n/;s/0/-/g'
 		done
 
 		for child in name  present state mode lock devicelock  power voltage energy  celsius offset  current factor  masterdeviceid members  tist tsoll absenk komfort windowopenactiv  battery batterylow  lastpressedtimestamp; do
@@ -123,29 +123,31 @@ raw_out() {
 		echo -e "\n"
 	done
 
-	min_len && echo "HGFE DCBA 9876 54321 0 "
-	min_len && echo "|||| |||| |||| ||||  | "
-	min_len && echo "|||| |||| |||| ||||  +- Bit  0: HANFUN Gerät"
-	min_len && echo "|||| |||| |||| ||||    "
-	min_len && echo "|||| |||| |||| |||+---- Bit  2: Lampe"
-	min_len && echo "|||| |||| |||| ||+----- Bit  3: ?Action"
-	min_len && echo "|||| |||| |||| |+------ Bit  4: Alarmsensor"
-	min_len && echo "|||| |||| |||| +------- Bit  5: Taster"
-	min_len && echo "|||| |||| ||||         "
-	min_len && echo "|||| |||| |||+--------- Bit  6: Heizkörperregler"
-	min_len && echo "|||| |||| ||+---------- Bit  7: Energiemessgerät"
-	min_len && echo "|||| |||| |+----------- Bit  8: Temperatursensor"
-	min_len && echo "|||| |||| +------------ Bit  9: Schaltsteckdose"
-	min_len && echo "|||| ||||              "
-	min_len && echo "|||| |||+-------------- Bit 10: DECT-Repeater"
-	min_len && echo "|||| ||+--------------- Bit 11: Mikrofon"
-	min_len && echo "|||| |+---------------- Bit 12: ?Bundle"
-	min_len && echo "|||| +----------------- Bit 13: HANFUN Unit"
-	min_len && echo "||||                   "
-	min_len && echo "|||+------------------- Bit 14: ?Template"
-	min_len && echo "||+-------------------- Bit 15: Schaltbar"
-	min_len && echo "|+--------------------- Bit 16: Dimmbar"
-	min_len && echo "+---------------------- Bit 17: Farbtemperatur"
+	min_len && echo "I HGFE DCBA 9876 5432 10 "
+	min_len && echo "| |||| |||| |||| ||||  | "
+	min_len && echo "| |||| |||| |||| ||||  +- Bit  0: HANFUN Gerät"
+	min_len && echo "| |||| |||| |||| ||||    "
+	min_len && echo "| |||| |||| |||| |||+---- Bit  2: Lampe"
+	min_len && echo "| |||| |||| |||| ||+----- Bit  3: ?Action"
+	min_len && echo "| |||| |||| |||| |+------ Bit  4: Alarmsensor"
+	min_len && echo "| |||| |||| |||| +------- Bit  5: Taster"
+	min_len && echo "| |||| |||| ||||         "
+	min_len && echo "| |||| |||| |||+--------- Bit  6: Heizkörperregler"
+	min_len && echo "| |||| |||| ||+---------- Bit  7: Energiemessgerät"
+	min_len && echo "| |||| |||| |+----------- Bit  8: Temperatursensor"
+	min_len && echo "| |||| |||| +------------ Bit  9: Schaltsteckdose"
+	min_len && echo "| |||| ||||              "
+	min_len && echo "| |||| |||+-------------- Bit 10: DECT-Repeater"
+	min_len && echo "| |||| ||+--------------- Bit 11: Mikrofon"
+	min_len && echo "| |||| |+---------------- Bit 12: ?Bundle"
+	min_len && echo "| |||| +----------------- Bit 13: HANFUN Unit"
+	min_len && echo "| ||||                   "
+	min_len && echo "| |||+------------------- Bit 14: ?Template"
+	min_len && echo "| ||+-------------------- Bit 15: Schaltbar"
+	min_len && echo "| |+--------------------- Bit 16: Potentiometer"
+	min_len && echo "| +---------------------- Bit 17: Farbtemperatur"
+	min_len && echo "|                        "
+	min_len && echo "+------------------------ Bit 18: Rollladensteuerung"
 	echo
 }
 
