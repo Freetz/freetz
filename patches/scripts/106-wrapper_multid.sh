ccdf8e494ab8e4caa659a79911bdd3744f92c9d6
fda77 <fda77@users.noreply.github.com>
2021-11-13 09:09:13 +0100

    patches: fix multid wrapper

--- patches/scripts/106-wrapper_multid.sh
+++ patches/scripts/106-wrapper_multid.sh
@@ -1,6 +1,8 @@
 
 echo1 "preparing multid wrapper"
 
+grep -q "^PATH=" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.net" || sed '2 i\PATH=$PATH' -i "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.net"
+
 modsed \
   "s#\(^PATH=\)\(.*\)#\1/usr/bin/wrapper:\2#g" \
   "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.net" \
