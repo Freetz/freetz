05c3320b1bbce2654313e4d4a382b65f8141da01
fda77 <fda77@users.noreply.github.com>
2021-11-14 20:50:31 +0100

    patches: fix show_dsl_spectrum-minmax, thx @freetz-ng-mod
     - closes https://github.com/Freetz-NG/freetz-ng/issues/407

--- patches/scripts/833-show_dsl_spectrum-minmax.sh
+++ patches/scripts/833-show_dsl_spectrum-minmax.sh
@@ -1,7 +1,9 @@
 [ "$FREETZ_MODIFY_DSL_SPECTRUM" == "y" ] || return 0
 echo1 "patching dsl spectrum"
 
-if [ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ] ; then
+if [ "$FREETZ_AVM_VERSION_07_4X_MIN" == "y" ] ; then
+	PVER="07_4X"
+elif [ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ] ; then
 	PVER="07_0X"
 elif [ "$FREETZ_AVM_VERSION_06_8X_MIN" == "y" ] ; then
 	PVER="06_8X"
