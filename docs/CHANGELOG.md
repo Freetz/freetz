f16285bd32c31ff475b40bc78936f642c0a205ba
fda77 <fda77@users.noreply.github.com>
2022-01-11 20:20:18 +0100

    bump kconfig

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -50,7 +50,7 @@ Latest changes
  - Updated tools and hooks:
    * BusyBox 1.34.1
    * fakeroot 1.26-1.1
-   * kconfig 5.15
+   * kconfig 5.16
    * pseudo 1.9.0 oe-core 0cda3ba
 
  - BusyBox package:
--- tools/make/kconfig-host/kconfig-host.mk
+++ tools/make/kconfig-host/kconfig-host.mk
@@ -1,6 +1,6 @@
-KCONFIG_HOST_VERSION:=v5.15
+KCONFIG_HOST_VERSION:=v5.16
 KCONFIG_HOST_SOURCE:=kconfig-$(KCONFIG_HOST_VERSION).tar.xz
-KCONFIG_HOST_SOURCE_SHA256:=e94aa86adf895dd952baa2164ba18a66b3d90d1a74ecf65a7861041ea4211d0c
+KCONFIG_HOST_SOURCE_SHA256:=f202ee6ad12411c0178368212423ae2553e4d29abdb14830c03141f099550583
 KCONFIG_HOST_SITE:=git_archive@git://repo.or.cz/linux.git,scripts/basic,scripts/kconfig,scripts/Kbuild.include,scripts/Makefile.compiler,scripts/Makefile.build,scripts/Makefile.host,scripts/Makefile.lib,Documentation/kbuild/kconfig-language.rst,Documentation/kbuild/kconfig-macro-language.rst,Documentation/kbuild/kconfig.rst
 KCONFIG_HOST_DIR:=$(TOOLS_SOURCE_DIR)/kconfig-$(KCONFIG_HOST_VERSION)
 KCONFIG_HOST_MAKE_DIR:=$(TOOLS_DIR)/make/kconfig-host
