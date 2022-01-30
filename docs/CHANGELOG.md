4819e42f31da54ba73b598e776b02e2ea2677a18
fda77 <fda77@users.noreply.github.com>
2022-01-30 12:18:04 +0100

    bump expat

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -72,7 +72,7 @@ Latest changes
    * cifs-utils 6.14
    * Curl 7.81.0
    * davfs2 1.5.2/1.6.1
-   * expat 2.4.3
+   * expat 2.4.4
    * git 2.34.1
    * HAProxy 2.5.0
    * iksemel 3.1.1
--- make/libs/expat/expat.mk
+++ make/libs/expat/expat.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_LIB, 2.4.3)
-$(PKG)_LIB_VERSION:=1.8.3
+$(call PKG_INIT_LIB, 2.4.4)
+$(PKG)_LIB_VERSION:=1.8.4
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
-$(PKG)_SOURCE_SHA256:=b1f9f1b1a5ebb0acaa88c9ff79bfa4e145823b78aa5185e5c5d85f060824778a
+$(PKG)_SOURCE_SHA256:=b5d25d6e373351c2ed19b562b4732d01d2589ac8c8e9e7962d8df1207cc311b8
 $(PKG)_SITE:=@SF/expat,https://github.com/libexpat/libexpat/releases/download/R_$(subst .,_,$($(PKG)_VERSION))
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/lib/.libs/libexpat.so.$($(PKG)_LIB_VERSION)
