c42bc912792fa44fa955ff20eafa5b79b29fd18e
YourFritz <git@yourfritz.de>
2022-03-11 20:11:22 +0100

    yf_bootmanager: unify handling of fixed branding between Freetz-NG
    and modfs as a preparation for support of other options to modify/select
    a new value at system restart (YF_CHANGE_OEM and yf_custom_environment)

--- patches/scripts/101-enforce_branding_via_rc.conf.sh
+++ patches/scripts/101-enforce_branding_via_rc.conf.sh
@@ -4,7 +4,7 @@ echo1 "force branding '${FREETZ_ENFORCE_FIRMWARE_VERSION_VIA_RCCONF}'"
 for oem in $(supported_brandings); do
 	if [ "${FREETZ_ENFORCE_FIRMWARE_VERSION_VIA_RCCONF}" == "$oem" ]; then
 		echo2 "hardcoding branding in /etc/init.d/rc.conf"
-		modsed -r 's,^([ \t]*OEM_tmp=).*firmware_version.*$,\1"'"${FREETZ_ENFORCE_FIRMWARE_VERSION_VIA_RCCONF}"'",' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf" 'OEM_tmp="'"${FREETZ_ENFORCE_FIRMWARE_VERSION_VIA_RCCONF}"'"$'
+		modsed "s|^\([ \t]*export OEM\)\$|\1=${FREETZ_ENFORCE_FIRMWARE_VERSION_VIA_RCCONF}|" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf" "export OEM=${FREETZ_ENFORCE_FIRMWARE_VERSION_VIA_RCCONF}\$"
 		return 0
 	fi
 done
