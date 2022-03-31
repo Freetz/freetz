19112bd697460ba4279b4d1ad9a082df12c9e70a
fda77 <fda77@users.noreply.github.com>
2022-03-31 22:15:33 +0200

    pcre: prepare for pcre2

--- docs/libs/pcre.md
+++ docs/libs/pcre.md
@@ -1,4 +1,7 @@
 # pcre: libpcre (libpcre.so) 8.45
+ - Homepage: [https://www.pcre.org/](https://www.pcre.org/)
+ - Manpage: [https://www.pcre.org/original/doc/html/](https://www.pcre.org/original/doc/html/)
+ - Changelog: [https://www.pcre.org/original/changelog.txt](https://www.pcre.org/original/changelog.txt)
  - Library: [master/make/libs/pcre/](https://github.com/Freetz-NG/freetz-ng/tree/master/make/libs/pcre/)
 
 A Perl Compatible Regular Expression library
--- make/libs/pcre/pcre.mk
+++ make/libs/pcre/pcre.mk
@@ -2,6 +2,9 @@ $(call PKG_INIT_LIB, 8.45)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_SOURCE_SHA256:=4dae6fdcd2bb0bb6c37b5f97c33c2be954da743985369cddac3546e3218bffb8
 $(PKG)_SITE:=@SF/pcre,ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre
+### WEBSITE:=https://www.pcre.org/
+### MANPAGE:=https://www.pcre.org/original/doc/html/
+### CHANGES:=https://www.pcre.org/original/changelog.txt
 
 $(PKG)_LIB_VERSION:=1.2.13
 $(PKG)_LIBNAME=libpcre.so.$($(PKG)_LIB_VERSION)
@@ -27,6 +30,7 @@ $(PKG)_CONFIGURE_OPTIONS += --disable-pcretest-libedit
 $(PKG)_CONFIGURE_OPTIONS += --disable-pcregrep-libz
 $(PKG)_CONFIGURE_OPTIONS += --disable-pcregrep-libbz2
 
+
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
@@ -40,9 +44,12 @@ $($(PKG)_STAGING_BINARY) $($(PKG)_POSIX_STAGING_BINARY): $($(PKG)_BINARY) $($(PK
 		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
 		install
 	$(PKG_FIX_LIBTOOL_LA) \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcre*.la \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcre.la \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcreposix.la \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/pcre-config \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libpcre*.pc
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libpcre.pc \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libpcreposix.pc \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/share/doc/pcre/
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
 	$(INSTALL_LIBRARY_STRIP)
@@ -54,17 +61,24 @@ $(pkg): $($(PKG)_STAGING_BINARY) $($(PKG)_POSIX_STAGING_BINARY)
 
 $(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_POSIX_TARGET_BINARY)
 
+
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(PCRE_DIR) clean
 	$(RM) -r \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcre*.* \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/pcre*.h \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/pcre* \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libpcre*.pc \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/share/man/man?/pcre* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcre.* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libpcreposix.* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/pcre.h \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/pcreposix.h \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/pcre-config \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/pcregrep \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/pcretest \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libpcre.pc \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libpcreposix.pc \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/share/doc/pcre/
 
 $(pkg)-uninstall:
-	$(RM) $(PCRE_TARGET_DIR)/libpcre*.so*
+	$(RM) \
+		$(PCRE_TARGET_DIR)/libpcre.so* \
+		$(PCRE_TARGET_DIR)/libpcreposix.so*
 
 $(PKG_FINISH)
