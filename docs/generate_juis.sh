a24d2e86472e8f30797b1ece79e3f142830a9f93
fda77 <fda77@users.noreply.github.com>
2022-05-12 12:41:50 +0200

    extend dect of juis action

--- docs/generate_juis.sh
+++ docs/generate_juis.sh
@@ -6,37 +6,49 @@ TOOLS="$PARENT/tools"
 
 
 #rel
-echo -e '\n### Release ####################################################'
-for x in $(seq 150 300); do  env - $TOOLS/juis_check        HW=$x                              -a; done | tee rel
+echo -e '\n### FOS-Release ################################################'
+for x in $(seq 150 300); do  env - $TOOLS/juis_check        HW=$x                                     -a; done | tee fos-rel
 #dwn
-echo -e '\n### Downgrade ##################################################'
-for x in $(seq 150 300); do  env - $TOOLS/juis_check --down HW=$x                              -a; done | tee dwn
+echo -e '\n### FOS-Downgrade ##############################################'
+for x in $(seq 150 300); do  env - $TOOLS/juis_check --down HW=$x                                     -a; done | tee fos-dwn
 #add
-cat dwn rel | sort -u > xxx
-#( cat rel ; cat dwn | while read -s x; do grep -q "^${x%=*}=" rel || echo $x; done ) | sort -u > xxx
-#( cat dwn ; cat rel | while read -s x; do grep -q "^${x%=*}=" dwn || echo $x; done ) | sort -u > xxx
-
+cat fos-dwn fos-rel | sort -u > fos-xxx
+#( cat fos-rel ; cat fos-dwn | while read -s x; do grep -q "^${x%=*}=" fos-rel || echo $x; done ) | sort -u > fos-xxx
+#( cat fos-dwn ; cat fos-rel | while read -s x; do grep -q "^${x%=*}=" fos-dwn || echo $x; done ) | sort -u > fos-xxx
 
 #lab
-echo -e '\n### Labor ######################################################'
+echo -e '\n### FOS-Labor ##################################################'
 for x in $(seq 200 300); do  [ "$x" -lt 248 2>/dev/null ] && m="$(( $x - 72 ))" || m=$x; m="$m.07.39-95000"
-                             env - $TOOLS/juis_check        HW=$x  Buildtype=1001  Version=$m  -a; done | tee lab
+                             env - $TOOLS/juis_check        HW=$x         Buildtype=1001  Version=$m  -a; done | tee fos-lab
 #inh
-echo -e '\n### Inhaus #####################################################'
+echo -e '\n### FOS-Inhaus #################################################'
 for x in $(seq 200 300); do  [ "$x" -lt 248 2>/dev/null ] && m="$(( $x - 72 ))" || m=$x; m="$m.07.39-95000"
-                             env - $TOOLS/juis_check        HW=$x  Buildtype=1000  Version=$m  -a; done | tee inh
+                             env - $TOOLS/juis_check        HW=$x         Buildtype=1000  Version=$m  -a; done | tee fos-inh
 #sub
-cat xxx | while read -s x; do sed "/^${x//\//\\\/}$/d" -i lab inh; done
+cat fos-xxx | while read -s x; do sed "/^${x//\//\\\/}$/d" -i fos-lab fos-inh; done
+#cat fos-xxx | while read -s x; do sed "/\/${x##*/}$/d" -i fos-lab fos-inh; done
 
 
-#dect
-echo -e '\n### Dect #######################################################'
-for x in $(seq  10 109); do [ ${#x} != 3 ] && x="0$x"; 
-                             env - $TOOLS/juis_check --dect HW=252 DHW=${x::-1}.0${x:2}        -a; done | tee dect
+#dect-rel
+echo -e '\n### Dect-Release ###############################################'
+for x in $(seq  10 109); do [ ${#x} != 3 ] && x="0$x"; x="${x::-1}.0${x:2}";
+                             env - $TOOLS/juis_check --dect HW=252 DHW=$x                             -a; done | tee dect-rel
+#dect-lab
+echo -e '\n### Dect-Labor #################################################'
+for x in $(seq  10 109); do [ ${#x} != 3 ] && x="0$x"; x="${x::-1}.0${x:2}";             m="252.07.39-95000"
+                             env - $TOOLS/juis_check --dect HW=252 DHW=$x Buildtype=1000  Version=$m  -a; done | tee dect-lab
+#dect-inh
+echo -e '\n### Dect-Inhaus ################################################'
+for x in $(seq  10 109); do [ ${#x} != 3 ] && x="0$x"; x="${x::-1}.0${x:2}";             m="252.07.39-95000"
+                             env - $TOOLS/juis_check --dect HW=252 DHW=$x Buildtype=1001  Version=$m  -a; done | tee dect-inh
+#dect-sub
+cat dect-rel | while read -s x; do sed "/\/${x##*/}$/d" -i dect-lab dect-inh; done
+cat dect-lab | while read -s x; do sed "/\/${x##*/}$/d" -i          dect-inh; done
+
 
 #bpjm
 echo -e '\n### BPjM #######################################################'
-                             env - $TOOLS/juis_check --bpjm HW=252                             -a       | tee bpjm
+                             env - $TOOLS/juis_check --bpjm HW=252                                    -a       | tee bpjm
 [ ! -s bpjm ] || curl "$(sed -n 's/.*=//p' bpjm)" -o bpjm.out
 read="$(head -c4 bpjm.out | xxd -p)"
 calc="$(crc32 <( tail -c +$((1 + 4)) bpjm.out ))"
@@ -47,19 +59,22 @@ sed -i "s/.*=/$comp=/" bpjm
 #gen
 (
 echo -n "Content: "
-echo -e "Release\nLabor\nInhaus\nDect\nBPjM" | while read cat; do echo -n "[$cat](#$(echo ${cat,,} | sed 's/ /-/g')) - "; done | sed 's/...$//'
+echo -e "FOS-Release\nFOS-Labor\nFOS-Inhaus\nDect-Release\nDect-Labor\nDect-Inhaus\nBPjM" \
+  | while read cat; do echo -n "[$cat](#$(echo ${cat,,} | sed 's/ /-/g')) - "; done | sed 's/...$//'
 echo
-echo -e '# Links von AVM-Juis ab HWR 150'
-echo -e ' - AVM nutzt unsichere http:// Links, daher muss die Signatur der Dateien vor Verwendung geprüft werden'
-echo -e ' - Sollten verschieden Links für ein Gerät angezeigt werden sind die Angaben von Juis inkonsistent'
-echo -e ' - Diese Liste ist weder vollständig, korrekt noch aktuell'
-echo -e '\n### Release' ; cat xxx  | while read -s x; do echo " - HWR ${x%=*}: [${x##*/}](${x#*=})"; done
-echo -e '\n### Labor'   ; cat lab  | while read -s x; do echo " - HWR ${x%=*}: [${x##*/}](${x#*=})"; done
-echo -e '\n### Inhaus'  ; cat inh  | while read -s x; do echo " - HWR ${x%=*}: [${x##*/}](${x#*=})"; done
-echo -e '\n### Dect'    ; cat dect | while read -s x; do echo " - MHW ${x%=*}: [${x##*/}](${x#*=})"; done
-echo -e '\n### BPjM'    ; cat bpjm | while read -s x; do echo " - CRC ${x%=*}: [${x##*/}](${x#*=})"; done
+echo -e '# Links von AVM-Juis für FOS ab HWR 150 sowie Dect und BPjM'
+echo -e ' - AVM nutzt unsichere http:// Links, daher muss die Signatur der Dateien vor Verwendung geprüft werden.'
+echo -e ' - Sollten verschieden Links für ein Gerät angezeigt werden sind die Angaben von Juis inkonsistent.'
+echo -e ' - Diese Liste ist weder vollständig, korrekt noch aktuell.'
+echo -e '\n### FOS-Release'  ; cat fos-xxx  | while read -s x; do echo " - HWR ${x%=*}: [${x##*/}](${x#*=})"; done
+echo -e '\n### FOS-Labor'    ; cat fos-lab  | while read -s x; do echo " - HWR ${x%=*}: [${x##*/}](${x#*=})"; done
+echo -e '\n### FOS-Inhaus'   ; cat fos-inh  | while read -s x; do echo " - HWR ${x%=*}: [${x##*/}](${x#*=})"; done
+echo -e '\n### Dect-Release' ; cat dect-rel | while read -s x; do echo " - MHW ${x%=*}: [${x##*/}](${x#*=})"; done
+echo -e '\n### Dect-Labor'   ; cat dect-lab | while read -s x; do echo " - MHW ${x%=*}: [${x##*/}](${x#*=})"; done
+echo -e '\n### Dect-Inhaus'  ; cat dect-inh | while read -s x; do echo " - MHW ${x%=*}: [${x##*/}](${x#*=})"; done
+echo -e '\n### BPjM'         ; cat bpjm     | while read -s x; do echo " - CRC ${x%=*}: [${x##*/}](${x#*=})"; done
 ) | sed 's/_-/-/' > $PARENT/docs/JUIS.md
 
 #tmp
-rm -f xxx rel dwn lab inh dect bpjm bpjm.out
+rm -f fos-xxx fos-rel fos-dwn fos-lab fos-inh  dect-rel dect-lab dect-inh  bpjm bpjm.out
 
