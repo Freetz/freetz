ecbe505f0e862c371fc2a83e0d692e4fc6fa8074
fda77 <fda77@users.noreply.github.com>
2022-05-29 00:23:10 +0200

    patch webmenu_secure: fwmod_custom support by Whoopie

--- patches/scripts/194-patch_webmenu_secure.sh
+++ patches/scripts/194-patch_webmenu_secure.sh
@@ -1,6 +1,13 @@
 [ "$FREETZ_PATCH_SECURE" == "y" ] || return 0
 echo1 "applying webmenu secure patch"
 
-[ "$FREETZ_PATCH_SECURE_2fa" == "y" ] && modsed 's/data[.]fritzos[.]twofactor_disabled/false/g'   ${FILESYSTEM_MOD_DIR}/usr/www/all/home/home.js
-[ "$FREETZ_PATCH_SECURE_sip" == "y" ] && modsed 's/data[.]fritzos[.]ipphone_from_outside/false/g' ${FILESYSTEM_MOD_DIR}/usr/www/all/home/home.js
+for oem in $(supported_brandings) all; do
+   [ -d "${FILESYSTEM_MOD_DIR}/usr/www/${oem}" -a ! -L "${FILESYSTEM_MOD_DIR}/usr/www/${oem}" ] || continue
+
+   for file in /usr/www/${oem}/home/home.js; do
+       [ ! -e ${FILESYSTEM_MOD_DIR}${file} ] && continue
+           [ "$FREETZ_PATCH_SECURE_2fa" == "y" ] && modsed 's/data[.]fritzos[.]twofactor_disabled/false/g'   ${FILESYSTEM_MOD_DIR}${file}
+           [ "$FREETZ_PATCH_SECURE_sip" == "y" ] && modsed 's/data[.]fritzos[.]ipphone_from_outside/false/g' ${FILESYSTEM_MOD_DIR}${file}
+   done
+done
 
