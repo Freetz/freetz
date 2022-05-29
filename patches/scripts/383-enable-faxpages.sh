752b3672133d107a207fc397120fc140c634925c
fda77 <fda77@users.noreply.github.com>
2022-05-29 19:32:07 +0200

    patch enable-faxpages: fwmod_custom support by Whoopie

--- patches/scripts/383-enable-faxpages.sh
+++ patches/scripts/383-enable-faxpages.sh
@@ -1,8 +1,15 @@
 [ "$FREETZ_PATCH_FAXPAGES" == "y" ] || return 0
 echo1 "enabling multiple-fax-pages"
 
-modsed \
-  's/accept="image\/\*">/accept="image\/\*" multiple="multiple">/' \
-  "${HTML_LANG_MOD_DIR}/fon_devices/fax_send.lua" \
-  'multiple="multiple">'
+for oem in $(supported_brandings) all; do
+   [ -d "${FILESYSTEM_MOD_DIR}/usr/www/${oem}" -a ! -L "${FILESYSTEM_MOD_DIR}/usr/www/${oem}" ] || continue
+
+   for file in /usr/www/${oem}/fon_devices/fax_send.lua; do
+       [ ! -e ${FILESYSTEM_MOD_DIR}${file} ] && continue
+           modsed \
+             's/accept="image\/\*">/accept="image\/\*" multiple="multiple">/' \
+             ${FILESYSTEM_MOD_DIR}${file} \
+             'multiple="multiple">'
+   done
+done
 
