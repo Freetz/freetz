cc41768431cb6e29658b9ba96c9c38f5b1ff79ab
fda77 <fda77@users.noreply.github.com>
2022-05-31 21:05:47 +0200

    mute bootmanager

--- patches/scripts/800-yf_bootmanager.sh
+++ patches/scripts/800-yf_bootmanager.sh
@@ -1,12 +1,14 @@
 [ "$FREETZ_PATCH_MODFS_BOOT_MANAGER" == "y" ] || return 0
 echo1 "adding yf-bootmanager"
 
+[ "$FREETZ_VERBOSITY_LEVEL" -lt "2" 2>/dev/null ] && exec >/dev/null
 TEMPDIR=$(mktemp -d)
 pushd "${TOOLS_DIR}/yf/bootmanager" >/dev/null
 TMP="$TEMPDIR" \
   TARGET_SYSTEM_VERSION="${AVM_FW_MAJOR}.${AVM_FW_VERSION}" \
   TARGET_DIR="${ABS_BASE_DIR}/${FILESYSTEM_MOD_DIR}" \
-  sh "./add_to_system_reboot.sh" 2>&1 | sed 's/^/    /g'  # >/dev/null
+  sh "./add_to_system_reboot.sh" 2>&1 | sed 's/^/    /g'
 popd >/dev/null
 rmdir "$TEMPDIR"
+[ "$FREETZ_VERBOSITY_LEVEL" -lt "2" 2>/dev/null ] && exec >$(tty)
 
