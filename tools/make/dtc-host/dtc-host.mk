046530b2a32a7aebeebb71322e3b3bac8bff927a
fda77 <fda77@users.noreply.github.com>
2022-06-08 13:55:56 +0200

    dtc-host: install to build  - refs https://github.com/PeterPawn/YourFreetz/commit/b8ec32d85b49463f6d49cd929180885b6f788c84

--- tools/make/dtc-host/dtc-host.mk
+++ tools/make/dtc-host/dtc-host.mk
@@ -3,28 +3,32 @@ $(PKG)_SOURCE:=dtc-$($(PKG)_VERSION).tar.xz
 $(PKG)_HASH:=65cec529893659a49a89740bb362f507a3b94fc8cd791e76a8d6a2b6f3203473
 $(PKG)_SITE:=@KERNEL/software/utils/dtc
 
-$(PKG)_LIBFDT_DIR:=$($(PKG)_DIR)/libfdt
-
+$(PKG)_LIBFDT_BINARY:=$($(PKG)_DIR)/libfdt/libfdt.a
+$(PKG)_BINARY:=$(HOST_TOOLS_DIR)/lib/libfdt.a
 
 $(TOOLS_SOURCE_DOWNLOAD)
 $(TOOLS_UNPACKED)
 $(TOOLS_CONFIGURED_NOP)
 
-$($(PKG)_LIBFDT_DIR)/libfdt.a: $($(PKG)_DIR)/.unpacked
-	$(TOOLS_SUBMAKE) -f Makefile.freetz -C $(DTC_HOST_LIBFDT_DIR) all \
+$($(PKG)_LIBFDT_BINARY): $($(PKG)_DIR)/.unpacked
+	$(TOOLS_SUBMAKE) -f Makefile.freetz -C $(DTC_HOST_DIR)/libfdt all \
 		CC="$(TOOLS_CC)" \
 		BITNESS="$(HOST_CFLAGS_FORCE_32BIT_CODE)"
-	touch -c $@
 
-$(pkg)-precompiled: $($(PKG)_LIBFDT_DIR)/libfdt.a
+$($(PKG)_BINARY): $($(PKG)_LIBFDT_BINARY)
+	$(TOOLS_SUBMAKE) -f Makefile.freetz -C $(DTC_HOST_DIR)/libfdt install \
+		PREFIX="$(HOST_TOOLS_DIR)"
+
+$(pkg)-precompiled: $($(PKG)_BINARY)
 
 
 $(pkg)-clean:
-	-$(MAKE) -f Makefile.freetz -C $(DTC_HOST_LIBFDT_DIR) clean
+	-$(MAKE) -f Makefile.freetz -C $(DTC_HOST_DIR)/libfdt clean
 
 $(pkg)-dirclean:
 	$(RM) -r $(DTC_HOST_DIR)
 
 $(pkg)-distclean: $(pkg)-dirclean
+	$(RM) $(HOST_TOOLS_DIR)/lib/libfdt* $(HOST_TOOLS_DIR)/include/*fdt*.h
 
 $(TOOLS_FINISH)
