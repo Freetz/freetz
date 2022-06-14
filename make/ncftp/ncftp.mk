810fa71b411891dec94025583c88c7d690c6213c
fda77 <fda77@users.noreply.github.com>
2022-06-14 13:28:05 +0200

    add ncftp links

--- make/ncftp/ncftp.mk
+++ make/ncftp/ncftp.mk
@@ -1,7 +1,10 @@
 $(call PKG_INIT_BIN, 3.2.6)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION)-src.tar.xz
 $(PKG)_HASH:=5f200687c05d0807690d9fb770327b226f02dd86155b49e750853fce4e31098d
-$(PKG)_SITE:=ftp://ftp.ncftp.com/ncftp
+$(PKG)_SITE:=https://www.ncftp.com/downloads/ncftp,ftp://ftp.ncftp.com/ncftp
+### WEBSITE:=https://www.ncftp.com/ncftp/
+### MANPAGE:=https://www.ncftp.com/ncftp/doc/faq.html
+### CHANGES:=https://www.ncftp.com/ncftp/doc/changelog.html
 
 $(PKG)_BINARIES_ALL := ncftp ncftpget ncftpput ncftpbatch ncftpls
 # ncftp is always included
@@ -21,6 +24,7 @@ $(PKG)_CONFIGURE_ENV += TARGET_ARCH=$(TARGET_ARCH_ENDIANNESS_DEPENDENT)
 $(PKG)_CONFIGURE_OPTIONS += --without-curses
 $(PKG)_CONFIGURE_OPTIONS += --without-ncurses
 
+
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
@@ -34,6 +38,7 @@ $($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/bin/%
 $(pkg):
 
 $(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR)
+
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(NCFTP_DIR) clean
