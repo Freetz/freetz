bd7c679fb3e93ccce225f296791d6a5ed549ea01
fda77 <fda77@users.noreply.github.com>
2022-06-20 09:28:19 +0200

    fit.sh: fix TOOLS_DIR

--- tools/fit.sh
+++ tools/fit.sh
@@ -2,22 +2,21 @@
 # fitimg unpacker & scanner
 # uses: fdtdump dtc fdtget dumpimage and diff
 MYPWD="$(dirname $(realpath $0))"
-TOOLS_DIR="$MYPWD/tools"
 
 # $1: .itb-file to scan
 # $2: create kernel/filesystem/kernel-args links in this dir
 analyze() {
 	local PART COUNT=0 FILE="$1" LINK="$2"
 	KPART= FPART= KTYPE= FTYPE= KNAME= FNAME= XARGS=
-	for PART in $($TOOLS_DIR/fdtget $FILE /images -l); do
+	for PART in $($MYPWD/fdtget $FILE /images -l); do
 		let COUNT++
 		[ "${PART/TZ_HW/}" != "$PART" ] && continue # TrustZone
 		[ "${PART/_HW0/}"  != "$PART" ] && continue # other CPU
 		[ "${PART/HW273/}" != "$PART" ] && continue # also 5590
-		TYPE="$($TOOLS_DIR/fdtget $FILE /images/$PART 'type')"
+		TYPE="$($MYPWD/fdtget $FILE /images/$PART 'type')"
 		[ "$TYPE" == "flat_dt"  ] && continue
 		if   [ "$TYPE" == "filesystem" -o "$TYPE" == "ramdisk" ]; then
-			FPART="$COUNT"; FTYPE="$TYPE"; FNAME="$PART"; XARGS="$($TOOLS_DIR/fdtget $FILE /images/$PART 'avm,kernel-args' 2>/dev/null)"
+			FPART="$COUNT"; FTYPE="$TYPE"; FNAME="$PART"; XARGS="$($MYPWD/fdtget $FILE /images/$PART 'avm,kernel-args' 2>/dev/null)"
 		elif [ "$TYPE" == "kernel"     -o "$TYPE" == "avm,fit" ]; then
 			KPART="$COUNT"; KTYPE="$TYPE"; KNAME="$PART"
 		else
@@ -27,7 +26,7 @@ analyze() {
 
 	if [ -n "$LINK" ]; then
 		echo -n "$XARGS" > "$LINK/args.txt"
-		$TOOLS_DIR/fdtget "$FILE" / 'timestamp' > "$LINK/date.txt"
+		$MYPWD/fdtget "$FILE" / 'timestamp' > "$LINK/date.txt"
 		# root
 		[ $FPART -le 9 ] && image="image.00$FPART" || image="image.0$FPART"
 		echo -n "$image"  >  "$LINK/filesystem.txt"
@@ -49,12 +48,12 @@ unpack() {
 	mkdir -p "$OUTP"
 	$MYPWD/yf/fit_tools/fit-remove-avm-header.sh "$FILE" > "$OUTP/image.itb" 2>/dev/null || cat "$FILE" > "$OUTP/image.itb"
 	FILE="$OUTP/image.itb"
-	$TOOLS_DIR/dtc -I dtb -O dts "$FILE" | tee "$OUTP/image.dts" | sed "s/^[ \t]*data = .*/XDATAXSEQUENCEX/g" > "$OUTP/image.its"
+	$MYPWD/dtc -I dtb -O dts "$FILE" | tee "$OUTP/image.dts" | sed "s/^[ \t]*data = .*/XDATAXSEQUENCEX/g" > "$OUTP/image.its"
 	[ ! -s  "$OUTP/image.its" ] && rm -f "$OUTP"/image.* && exit 1
 	while grep -q "XDATAXSEQUENCEX" "$OUTP/image.its"; do
 		[ $c -lt 9 ] && image="image.00$(($c+1))" || image="image.0$(($c+1))"
 		sed "0,/XDATAXSEQUENCEX/s//\t\t\tdata = \/incbin\/(\"$image\");/" -i "$OUTP/image.its"
-		$TOOLS_DIR/dumpimage "$FILE" -T flat_dt -p $c -o "$OUTP/$image" >/dev/null
+		$MYPWD/dumpimage "$FILE" -T flat_dt -p $c -o "$OUTP/$image" >/dev/null
 		let c++
 	done
 	analyze "$FILE" "$OUTP"
@@ -78,11 +77,11 @@ scandir() {
 case "$1" in
 	u|unpack)	unpack  "$2" "$3" ;;
 	s|scandir)	scandir "$2"      ;;
-	o|original)	$TOOLS_DIR/fdtdump $MYPWD/../build/original/fit-image/image.itb | grep -vE 'data = ' ;;
-	m|modified)	$TOOLS_DIR/fdtdump $MYPWD/../build/modified/fit-image/image.itb | grep -vE 'data = ' ;;
+	o|original)	$MYPWD/fdtdump $MYPWD/../build/original/fit-image/image.itb | grep -vE 'data = ' ;;
+	m|modified)	$MYPWD/fdtdump $MYPWD/../build/modified/fit-image/image.itb | grep -vE 'data = ' ;;
 	d|diff)		diff -Naur \
-			  <($TOOLS_DIR/dtc -I dtb -O dts $MYPWD/../build/o*/fit-image/image.itb | grep -vE 'data = |^$') \
-			  <($TOOLS_DIR/dtc -I dtb -O dts $MYPWD/../build/m*/fit-image/image.itb | grep -vE 'data = |^$')
+			  <($MYPWD/dtc -I dtb -O dts $MYPWD/../build/o*/fit-image/image.itb | grep -vE 'data = |^$') \
+			  <($MYPWD/dtc -I dtb -O dts $MYPWD/../build/m*/fit-image/image.itb | grep -vE 'data = |^$')
 			;;
 	*)		echo "Usage: $0 <unpack|scandir|original|modified|diff>" ;;
 esac
