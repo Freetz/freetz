af692a5ea31d840a98c249c944ee584467f8d714
fda77 <fda77@users.noreply.github.com>
2022-06-27 20:43:27 +0200

    yf-bootmanager: add version selection

--- tools/make/yf-bootmanager-host/yf-bootmanager-host.mk
+++ tools/make/yf-bootmanager-host/yf-bootmanager-host.mk
@@ -1,14 +1,18 @@
 YF_BOOTMANAGER_HOST_REPOSITORY:=https://github.com/PeterPawn/YourFritz.git
-$(call TOOLS_INIT, $(shell git ls-remote --tags $(YF_BOOTMANAGER_HOST_REPOSITORY) freetz-ng-version | sed -n -e "s|^\([0-9a-f]*\).*|\1|p"))
+$(call TOOLS_INIT, $(if $(FREETZ_PATCH_MODFS_BOOT_MANAGER_labor),$(call git-get-tag-revision,$(YF_BOOTMANAGER_HOST_REPOSITORY),freetz-ng-test),$(if $(FREETZ_PATCH_MODFS_BOOT_MANAGER_latest),$(call git-get-tag-revision,$(YF_BOOTMANAGER_HOST_REPOSITORY),freetz-ng-version),87ee8c3b73edf4ba70025a017aa1d49cd2c66036)))
 # Versions after this commit have no vanilla GPL3 - but it's not a problem to use the unchanged(!) version in an own image.
 $(PKG)_SOURCE:=yf-bootmanager-$($(PKG)_VERSION).tar.xz
-$(PKG)_HASH:=X
+$(PKG)_HASH:=f47e9395048d2d095f8d6a5286941adf9da837bb303b2366466365b2f984c1bb
 $(PKG)_SITE:=git_sparse@$($(PKG)_REPOSITORY),bootmanager
 ### VERSION:=0.8.6
 ### WEBSITE:=https://github.com/PeterPawn/YourFritz/tree/main/bootmanager
 ### MANPAGE:=https://github.com/PeterPawn/YourFritz/tree/main/bootmanager#readme
 ### CHANGES:=https://github.com/PeterPawn/YourFritz/commits/main/bootmanager
 ### CVSREPO:=https://github.com/PeterPawn/YourFritz/tree/main/bootmanager
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PATCH_MODFS_BOOT_MANAGER_tested
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PATCH_MODFS_BOOT_MANAGER_latest
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PATCH_MODFS_BOOT_MANAGER_labor
 
 
 $(TOOLS_SOURCE_DOWNLOAD)
