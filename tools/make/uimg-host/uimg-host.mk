e2dd8a54ec92b0fbf743a79fbad273341653878a
fda77 <fda77@users.noreply.github.com>
2022-07-08 15:19:52 +0200

    uimg-host: add links

--- tools/make/uimg-host/uimg-host.mk
+++ tools/make/uimg-host/uimg-host.mk
@@ -2,6 +2,10 @@ $(call TOOLS_INIT, 871930df297e3a03bc315be75ecfc8c5c7a809ab)
 $(PKG)_SOURCE:=uimg-$($(PKG)_VERSION).tar.xz
 $(PKG)_HASH:=1f5b3b473f50c6ff79a7859be336245c39fc1d2b6c89baf862de6de1f7caf8e0
 $(PKG)_SITE:=git@https://bitbucket.org/fesc2000/uimg-tool.git
+### WEBSITE:=https://bitbucket.org/fesc2000/uimg-tool/
+### MANPAGE:=https://bitbucket.org/fesc2000/uimg-tool/src/master/README.md
+### CHANGES:=https://bitbucket.org/fesc2000/uimg-tool/commits/
+### CVSREPO:=https://bitbucket.org/fesc2000/uimg-tool/src/master/
 
 
 $(TOOLS_SOURCE_DOWNLOAD)
