12708862e3d98595afd51ca25fd5ecdabb892efc
fda77 <fda77@users.noreply.github.com>
2022-09-27 23:46:15 +0200

    dehydrated: add links

--- make/dehydrated/dehydrated.mk
+++ make/dehydrated/dehydrated.mk
@@ -2,6 +2,10 @@ $(call PKG_INIT_BIN, 0.7.0)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_HASH:=1c5f12c2e57e64b1762803f82f0f7e767a72e65a6ce68e4d1ec197e61b9dc4f9
 $(PKG)_SITE:=https://github.com/dehydrated-io/dehydrated/releases/download/v$($(PKG)_VERSION)
+### WEBSITE:=https://dehydrated.io/
+### MANPAGE:=https://github.com/dehydrated-io/dehydrated/wiki
+### CHANGES:=https://github.com/dehydrated-io/dehydrated/releases
+### CVSREPO:=https://github.com/dehydrated-io/dehydrated/commits/master
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/dehydrated
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/dehydrated
