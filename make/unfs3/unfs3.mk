0958c41bbf5373039eb59ff950385072e7dd1f7b
fda77 <fda77@users.noreply.github.com>
2022-10-10 16:47:42 +0200

    bump unfs3gph

--- make/unfs3/unfs3.mk
+++ make/unfs3/unfs3.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_BIN, 0.9.22)
+$(call PKG_INIT_BIN, 0.9.23)
 $(PKG)_SOURCE:=unfs3-$($(PKG)_VERSION).tar.gz
-$(PKG)_HASH:=482222cae541172c155cd5dc9c2199763a6454b0c5c0619102d8143bb19fdf1c
-$(PKG)_SITE:=@SF/project/unfs3/unfs3/$($(PKG)_VERSION),https://github.com/unfs3/unfs3/releases/download/$($(PKG)_VERSION)
+$(PKG)_HASH:=5fc913aa0a1f3580fa691a32be835b6b5aed9d5abe0af3fd623d13e55a69f6c2
+$(PKG)_SITE:=@SF/project/unfs3/unfs3/$($(PKG)_VERSION),https://github.com/unfs3/unfs3/releases/download/unfs3-$($(PKG)_VERSION)
 $(PKG)_BINARY:=$($(PKG)_DIR)/unfsd
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/unfsd
 ### WEBSITE:=https://unfs3.github.io/
@@ -15,6 +15,7 @@ $(PKG)_DEPENDS_ON += $(if $(FREETZ_TARGET_UCLIBC_SUPPORTS_rpc),,libtirpc)
 ifneq ($(strip $(FREETZ_TARGET_UCLIBC_SUPPORTS_rpc)),y)
 $(PKG)_CFLAGS += -ltirpc -I$(TARGET_TOOLCHAIN_STAGING_DIR)/include/tirpc
 endif
+
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -30,6 +31,7 @@ $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 $(pkg):
 
 $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(UNFS3_DIR) clean
