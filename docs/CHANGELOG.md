078c9c7b98e2e058f5059fa61d416193296e2481
fda77 <fda77@users.noreply.github.com>
2022-11-08 17:38:17 +0100

    bump rpcbind

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -57,6 +57,7 @@ Latest changes
    * OpenSSH 9.1p1
    * OpenSSL 0.9.8zh/1.0.2u/1.1.1s/3.0.7
    * OpenVPN 2.4.12/2.5.8
+   * RPCBind 1.2.6
    * strace 4.9/5.0/6.0
 
  - Libraries:
--- make/pkgs/rpcbind/rpcbind.mk
+++ make/pkgs/rpcbind/rpcbind.mk
@@ -1,7 +1,11 @@
-$(call PKG_INIT_BIN,1.2.5)
+$(call PKG_INIT_BIN,1.2.6)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_HASH:=2ce360683963b35c19c43f0ee2c7f18aa5b81ef41c3fdbd15ffcb00b8bffda7a
+$(PKG)_HASH:=5613746489cae5ae23a443bb85c05a11741a5f12c8f55d2bb5e83b9defeee8de
 $(PKG)_SITE:=@SF/rpcbind
+### WEBSITE:=https://sourceforge.net/projects/rpcbind/
+### MANPAGE:=https://linux.die.net/man/8/rpcbind
+### CHANGES:=http://git.linux-nfs.org/?p=steved/rpcbind.git;a=shortlog;h=refs/heads/master
+### CVSREPO:=http://git.linux-nfs.org/?p=steved/rpcbind.git
 
 $(PKG)_BINARY:=rpcbind rpcinfo
 $(PKG)_BINARY_BUILD_DIR:=$($(PKG)_BINARY:%=$($(PKG)_DIR)/%)
@@ -20,6 +24,7 @@ $(PKG)_CONFIGURE_OPTIONS += --with-rpcuser=nobody
 
 $(PKG)_CFLAGS += -I$(TARGET_TOOLCHAIN_STAGING_DIR)/include/tirpc
 
+
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
@@ -34,6 +39,7 @@ $($(PKG)_BINARY_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/sbin/%: $($(PKG)_DIR)/$($(PK
 $(pkg):
 
 $(pkg)-precompiled: $($(PKG)_BINARY_TARGET_DIR)
+
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(RPCBIND_DIR) clean
