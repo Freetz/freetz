b2a35657e8308594961c777a7edd9d8db3bfa41e
fda77 <fda77@users.noreply.github.com>
2022-11-15 13:32:34 +0100

    libdvbcsa: add links

--- make/libs/libdvbcsa/libdvbcsa.mk
+++ make/libs/libdvbcsa/libdvbcsa.mk
@@ -3,10 +3,15 @@ $(PKG)_LIB_VERSION:=1.0.1
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_HASH:=4db78af5cdb2641dfb1136fe3531960a477c9e3e3b6ba19a2754d046af3f456d
 $(PKG)_SITE:=https://download.videolan.org/pub/videolan/libdvbcsa/$($(PKG)_VERSION)
+### WEBSITE:=https://www.videolan.org/developers/libdvbcsa.html
+### MANPAGE:=https://code.videolan.org/videolan/libdvbcsa/blob/master/README
+### CHANGES:=https://code.videolan.org/videolan/libdvbcsa/blob/master/ChangeLog
+### CVSREPO:=https://code.videolan.org/videolan/libdvbcsa/
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/.libs/libdvbcsa.so.$($(PKG)_LIB_VERSION)
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libdvbcsa.so.$($(PKG)_LIB_VERSION)
 $(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libdvbcsa.so.$($(PKG)_LIB_VERSION)
+
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -28,6 +33,7 @@ $($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
 $(pkg): $($(PKG)_STAGING_BINARY)
 
 $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(LIBDVBCSA_DIR) clean
