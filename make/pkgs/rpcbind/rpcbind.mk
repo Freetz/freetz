5e0d4b81bec47e2539166dc773f397a00072e651
fda77 <fda77@users.noreply.github.com>
2022-11-15 12:07:55 +0100

    rpcbind: change rpcinfo dir

--- make/pkgs/rpcbind/rpcbind.mk
+++ make/pkgs/rpcbind/rpcbind.mk
@@ -7,11 +7,13 @@ $(PKG)_SITE:=@SF/rpcbind
 ### CHANGES:=http://git.linux-nfs.org/?p=steved/rpcbind.git;a=shortlog;h=refs/heads/master
 ### CVSREPO:=http://git.linux-nfs.org/?p=steved/rpcbind.git
 
-$(PKG)_BINARY:=rpcbind rpcinfo
-$(PKG)_BINARY_BUILD_DIR:=$($(PKG)_BINARY:%=$($(PKG)_DIR)/%)
-$(PKG)_BINARY_TARGET_DIR:=$($(PKG)_BINARY:%=$($(PKG)_DEST_DIR)/usr/sbin/%)
+$(PKG)_BINARY_USRSBIN:=rpcbind
+$(PKG)_BINARY_BIN:=rpcinfo
+$(PKG)_BINARY_BUILD_DIR:=$($(PKG)_BINARY_USRSBIN:%=$($(PKG)_DIR)/%) $($(PKG)_BINARY_BIN:%=$($(PKG)_DIR)/%)
+$(PKG)_BINARY_TARGET_DIR_USRSBIN:=$($(PKG)_BINARY_USRSBIN:%=$($(PKG)_DEST_DIR)/usr/sbin/%)
+$(PKG)_BINARY_TARGET_DIR_BIN:=$($(PKG)_BINARY_BIN:%=$($(PKG)_DEST_DIR)/bin/%)
 
-$(PKG)_EXCLUDED += $(if $(FREETZ_PACKAGE_RPCBIND_RPCINFO),,/usr/sbin/rpcinfo)
+$(PKG)_EXCLUDED += $(if $(FREETZ_PACKAGE_RPCBIND_RPCINFO),,/bin/rpcinfo)
 
 $(PKG)_DEPENDS_ON += tcp_wrappers
 $(PKG)_DEPENDS_ON += libtirpc
@@ -33,18 +35,21 @@ $($(PKG)_BINARY_BUILD_DIR): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(RPCBIND_DIR) \
 		CFLAGS="$(TARGET_CFLAGS) $(RPCBIND_CFLAGS)"
 
-$($(PKG)_BINARY_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/sbin/%: $($(PKG)_DIR)/$($(PKG)_BUILD_SUBDIR)%
+$($(PKG)_BINARY_TARGET_DIR_USRSBIN): $($(PKG)_DEST_DIR)/usr/sbin/%: $($(PKG)_DIR)/$($(PKG)_BUILD_SUBDIR)%
+	$(INSTALL_BINARY_STRIP)
+
+$($(PKG)_BINARY_TARGET_DIR_BIN): $($(PKG)_DEST_DIR)/bin/%: $($(PKG)_DIR)/$($(PKG)_BUILD_SUBDIR)%
 	$(INSTALL_BINARY_STRIP)
 
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_BINARY_TARGET_DIR)
+$(pkg)-precompiled: $($(PKG)_BINARY_TARGET_DIR_USRSBIN) $($(PKG)_BINARY_TARGET_DIR_BIN)
 
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(RPCBIND_DIR) clean
 
 $(pkg)-uninstall:
-	$(RM) $(RPCBIND_BINARY_TARGET_DIR)
+	$(RM) $(RPCBIND_BINARY_TARGET_DIR_USRSBIN) $(RPCBIND_BINARY_TARGET_DIR_BIN)
 
 $(PKG_FINISH)
