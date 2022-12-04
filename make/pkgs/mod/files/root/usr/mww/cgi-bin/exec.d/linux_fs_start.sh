24eee6a67618790a7d096ac39a43c219cbd0fe6a
fda77 <fda77@users.noreply.github.com>
2022-12-04 22:30:40 +0100

    linux_fs_start: fix indendation

--- make/pkgs/mod/files/root/usr/mww/cgi-bin/exec.d/linux_fs_start.sh
+++ make/pkgs/mod/files/root/usr/mww/cgi-bin/exec.d/linux_fs_start.sh
@@ -8,35 +8,35 @@ FWLAYOUT=''
 
 if [ -x "$(which bootslotctl)" ]; then
 case "$FWLAYOUT" in
-	5) # UIMG
-	LFS_LIVE="$(sed -n 's/^linux_fs_start[ \t]*//p' /proc/sys/urlader/environment)"
-	[ -z "$LFS_LIVE" ] && LFS_LIVE=0
-	LFS_DEAD="$(( ($LFS_LIVE+1) %2 ))"
-	echo "changing $LFS_LIVE -> $LFS_DEAD ... and rebooting"
-	/bin/aicmd pumaglued uimg switchandreboot
-	LFS_TEST="$LFS_DEAD"
-	;;
-	6) # FIT
-	. /var/env.mod.daemon  # CONFIG_ENVIRONMENT_PATH
-	LFS_LIVE="$(bootslotctl get_active)"
-	LFS_DEAD="$(bootslotctl get_other)"
-	if [ "$LFS_LIVE" == "$LFS_DEAD" ]; then
-		echo "unavailable"
-		LFS_TEST="9"
-	else
-		echo "changing $LFS_LIVE -> $LFS_DEAD"
-		bootslotctl activate_other
-		LFS_TEST="$(bootslotctl get_active)"
-	fi
-	;;
+	5)	# UIMG
+		LFS_LIVE="$(sed -n 's/^linux_fs_start[ \t]*//p' /proc/sys/urlader/environment)"
+		[ -z "$LFS_LIVE" ] && LFS_LIVE=0
+		LFS_DEAD="$(( ($LFS_LIVE+1) %2 ))"
+		echo "changing $LFS_LIVE -> $LFS_DEAD ... and rebooting"
+		/bin/aicmd pumaglued uimg switchandreboot
+		LFS_TEST="$LFS_DEAD"
+		;;
+	6)	# FIT
+		. /var/env.mod.daemon  # CONFIG_ENVIRONMENT_PATH
+		LFS_LIVE="$(bootslotctl get_active)"
+		LFS_DEAD="$(bootslotctl get_other)"
+		if [ "$LFS_LIVE" == "$LFS_DEAD" ]; then
+			echo "unavailable"
+			LFS_TEST="9"
+		else
+			echo "changing $LFS_LIVE -> $LFS_DEAD"
+			bootslotctl activate_other
+			LFS_TEST="$(bootslotctl get_active)"
+		fi
+		;;
 	*)
-	LFS_LIVE="$(sed -n 's/^linux_fs_start[ \t]*//p' /proc/sys/urlader/environment)"
-	[ -z "$LFS_LIVE" ] && LFS_LIVE=0
-	LFS_DEAD="$(( ($LFS_LIVE+1) %2 ))"
-	echo "changing $LFS_LIVE -> $LFS_DEAD"
-	echo "linux_fs_start $LFS_DEAD" > /proc/sys/urlader/environment
-	LFS_TEST="$(sed -n 's/^linux_fs_start[ \t]*//p' /proc/sys/urlader/environment)"
-	;;
+		LFS_LIVE="$(sed -n 's/^linux_fs_start[ \t]*//p' /proc/sys/urlader/environment)"
+		[ -z "$LFS_LIVE" ] && LFS_LIVE=0
+		LFS_DEAD="$(( ($LFS_LIVE+1) %2 ))"
+		echo "changing $LFS_LIVE -> $LFS_DEAD"
+		echo "linux_fs_start $LFS_DEAD" > /proc/sys/urlader/environment
+		LFS_TEST="$(sed -n 's/^linux_fs_start[ \t]*//p' /proc/sys/urlader/environment)"
+		;;
 esac
 [ "$LFS_TEST" != "$LFS_DEAD" ] && echo "failed." || echo "done."
 
