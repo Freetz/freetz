f49de87daa1532c7618abfb48b6417492396a932
fda77 <fda77@users.noreply.github.com>
2022-12-11 09:06:22 +0100

    update wrapper-multid

--- patches/scripts/106-wrapper_multid.sh
+++ patches/scripts/106-wrapper_multid.sh
@@ -1,6 +1,16 @@
 
 echo1 "preparing multid wrapper"
 
+wrapath="/usr/bin/wrapper/"
+for daemon in dsld multid rextd; do
+	file="${FILESYSTEM_MOD_DIR}/lib/systemd/system/$daemon.service"
+	[ -e "$file" ] || continue
+	modsed -r \
+	  "s,^(ExecStart *=).*/*($daemon *.*)$,\1$wrapath\2," \
+	  "$file" \
+	  "$wrapath$daemon"
+done
+
 grep -q "^PATH=" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.net" || sed '2 i\PATH=$PATH' -i "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.net"
 
 modsed \
