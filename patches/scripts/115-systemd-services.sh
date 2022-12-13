5ba10b3973a1eb8b0b23cea6928894e1a4e0f861
fda77 <fda77@users.noreply.github.com>
2022-12-13 14:09:09 +0100

    modload: net is gone, thx @PeterPawn

--- patches/scripts/115-systemd-services.sh
+++ patches/scripts/115-systemd-services.sh
@@ -12,13 +12,19 @@ After=tffs.service environment.service
 WantedBy=environment.target
 EOF
 
+after_service=''
+for service in multid rextd net_basic net; do
+[ -e "${FILESYSTEM_MOD_DIR}/lib/systemd/system/${service}.service" ] && after_service="${service}.service ${after_service}"  # && break
+done
+[ -z "${after_service}" ] && warn "Could not find any usable service file!"
+
 echo2 "adding zzz-rcmod.service"
-cat << 'EOF' > "${FILESYSTEM_MOD_DIR}/lib/systemd/system/zzz-rcmod.service"
+cat << EOF > "${FILESYSTEM_MOD_DIR}/lib/systemd/system/zzz-rcmod.service"
 [Unit]
 ExecStart=/etc/boot.d/core/99-zzz-rcmod
 Type=oneshot
 DefaultDependencies=no
-After=net.service modload.service
+After=${after_service}modload.service
 [Install]
 WantedBy=multi-user.target
 EOF
