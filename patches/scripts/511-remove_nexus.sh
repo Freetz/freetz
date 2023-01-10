ed531658513fa316451f573c6f6855349bbd284d
fda77 <fda77@users.noreply.github.com>
2023-01-10 16:38:45 +0100

    cleanup remove-nexus

--- patches/scripts/511-remove_nexus.sh
+++ patches/scripts/511-remove_nexus.sh
@@ -5,7 +5,6 @@ for files in \
   etc/init.d/e45-avmnexusd \
   etc/init.d/E45-avmnexusd \
   bin/avmnexusd \
-  usr/www/all/css/rd/images/ic_nexus.svg \
   ; do
 	rm_files "${FILESYSTEM_MOD_DIR}/$files"
 done
@@ -17,23 +16,7 @@ supervisor_delete_service "avmnexusd"
 #  lib/avmnexus_nocsock.so* \
 #  lib/avmnexus_tab.so* \
 #  etc/default.Fritz_Box_HW185/all/avmnexus*.xml \
-#  ; do
-#	rm_files "${FILESYSTEM_MOD_DIR}/$files"
-#done
-
-if [ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ]; then
-	for files in \
-	  etc/init.d/E48-mesh \
-	  sbin/meshd \
-	  lib/libmesh_shared.so \
-	  ; do
-		rm_files "${FILESYSTEM_MOD_DIR}/$files"
-	done
-	supervisor_delete_service "meshd_config"
-	supervisor_delete_service "meshd"
-fi
-
-#[ "$FREETZ_AVM_VERSION_07_0X_MIN" == "y" ] && for files in \
+#  usr/www/all/css/rd/images/ic_nexus.svg \
 #  lib/libavmnexuscpp.so.0.0.0 \
 #  lib/libavmnexusmail.so.0.0.0 \
 #  ; do
