f42f5d47a254b258f50be55eed5f3039d1ed75ff
fda77 <fda77@users.noreply.github.com>
2023-01-13 21:43:50 +0100

    kconfig: better fix for missing libs  - refs https://github.com/Freetz-NG/freetz-ng/issues/627

--- make/host-tools/kconfig-host/kconfig-host.mk
+++ make/host-tools/kconfig-host/kconfig-host.mk
@@ -28,7 +28,9 @@ $(TOOLS_UNPACKED)
 $(TOOLS_CONFIGURED_NOP)
 
 $($(PKG)_TARGET_PRG:%=$($(PKG)_DIR)/scripts/kconfig/%): $($(PKG)_DIR)/.unpacked
-	$(TOOLS_SUBMAKE) -C $(KCONFIG_HOST_DIR) $(subst --$(notdir $@),,$(filter %--$(notdir $@),$(KCONFIG_HOST_TARGET_ALL)))
+	$(TOOLS_SUBMAKE) -C $(KCONFIG_HOST_DIR) \
+		HOSTPKG_CONFIG="pkgconf" \
+		$(subst --$(notdir $@),,$(filter %--$(notdir $@),$(KCONFIG_HOST_TARGET_ALL)))
 
 $(patsubst %,$($(PKG)_TARGET_DIR)/%,$($(PKG)_TARGET_PRG)): $($(PKG)_TARGET_DIR)/% : $($(PKG)_DIR)/scripts/kconfig/%
 	$(INSTALL_FILE)
