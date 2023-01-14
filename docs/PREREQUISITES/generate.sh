86476ef7ed3b537973caf8fc889aee51454f7462
fda77 <fda77@users.noreply.github.com>
2023-01-14 10:29:42 +0100

    sort prerequisites output

--- docs/PREREQUISITES/generate.sh
+++ docs/PREREQUISITES/generate.sh
@@ -4,7 +4,7 @@ MDPWD="$(dirname $(realpath $0))"
 
 cat "$MDPWD/template.md" > "$MDPWD/README.md"
 for x in "$MDPWD/packages/"*; do
-	vals="$(sed 's/[\t ]*#.*//g' $x | tr '\n' ' ' | sed 's/^ *//;s/ *$//g')"
+	vals="$(sed 's/[\t ]*#.*//g' $x | sort | tr '\n' ' ' | sed 's/^ *//;s/  */ /g;s/ *$//g')"
 	sed -i "s!%%${x##*/}%%!${vals}!" "$MDPWD/README.md"
 done
 
