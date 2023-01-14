daa5adae6ccbba260fa9a6d09b5fe5589267cdef
fda77 <fda77@users.noreply.github.com>
2023-01-14 10:32:37 +0100

    linebreaks for prerequisites

--- docs/PREREQUISITES/generate.sh
+++ docs/PREREQUISITES/generate.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 # generates docs/PREREQUISITES/README.md
 MDPWD="$(dirname $(realpath $0))"
+LIMIT=99
 
 cat "$MDPWD/template.md" > "$MDPWD/README.md"
 for x in "$MDPWD/packages/"*; do
-	vals="$(sed 's/[\t ]*#.*//g' $x | sort | tr '\n' ' ' | sed 's/^ *//;s/  */ /g;s/ *$//g')"
-	sed -i "s!%%${x##*/}%%!${vals}!" "$MDPWD/README.md"
+	vals="\\\\\n "
+	c=0
+	for v in $(sed 's/[\t ]*#.*//g' $x | sort | tr '\n' ' '); do
+		c=$(( ${c} + ${#v} ))
+		[ $c -gt $LIMIT ] && c=0 && vals="$vals \\\\\n "
+		vals="$vals $v"
+	done
+	sed -i "s!%%${x##*/}%%!${vals% }!" "$MDPWD/README.md"
 done
 
