9ec4e4737f5b984107a750f8097958ca7a69366a
Chihab Djaidja <dj.chihab@gmail.com>
2023-01-15 14:08:56 +0100

    Fix pyLoad version string

--- make/pkgs/pyload/pyload.mk
+++ make/pkgs/pyload/pyload.mk
@@ -1,5 +1,5 @@
 PYLOAD_GIT_REPOSITORY:=https://github.com/pyload/pyload.git
-$(call PKG_INIT_BIN, $(if $(FREETZ_PACKAGE_PYLOAD_VERSION_LATEST_GIT),$(call git-get-latest-revision,$(PYLOAD_GIT_REPOSITORY),stable),0.4.20))
+$(call PKG_INIT_BIN, $(if $(FREETZ_PACKAGE_PYLOAD_VERSION_LATEST_GIT),$(call git-get-latest-revision,$(PYLOAD_GIT_REPOSITORY),stable),v0.4.20))
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
 $(PKG)_HASH:=X
 $(PKG)_SITE:=git@$($(PKG)_GIT_REPOSITORY)
