29921eba16a5e807a5c2225431b7f352f89c8df0
fda77 <fda77@users.noreply.github.com>
2023-01-17 13:48:19 +0100

    simpler enforce-branding selection

--- patches/scripts/101-enforce_branding_via_rc.conf.sh
+++ patches/scripts/101-enforce_branding_via_rc.conf.sh
@@ -1,13 +1,20 @@
-[ -n "$FREETZ_ENFORCE_FIRMWARE_VERSION_VIA_RCCONF" ] || return 0
-echo1 "force branding '${FREETZ_ENFORCE_FIRMWARE_VERSION_VIA_RCCONF}'"
+[ "$FREETZ_ENFORCE_BRANDING_none" != "y" ] || return 0
+supported_brandings="$(supported_brandings)"
 
-for oem in $(supported_brandings); do
-	if [ "${FREETZ_ENFORCE_FIRMWARE_VERSION_VIA_RCCONF}" == "$oem" ]; then
-		echo2 "hardcoding branding in /etc/init.d/rc.conf"
-		modsed "s|^\([ \t]*export OEM\)\$|\1=${FREETZ_ENFORCE_FIRMWARE_VERSION_VIA_RCCONF}|" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf" "export OEM=${FREETZ_ENFORCE_FIRMWARE_VERSION_VIA_RCCONF}\$"
-		return 0
-	fi
+firmware_version="error"
+for x in ${supported_brandings}; do
+	[ "$(eval echo "\$FREETZ_ENFORCE_BRANDING_$x")" == "y" ] && firmware_version="$x"
 done
 
-error 1 "selected branding '${FREETZ_ENFORCE_FIRMWARE_VERSION_VIA_RCCONF}' is not supported by this firmware"
+echo1 "enforce branding '${firmware_version}'"
+[ "${firmware_version}" == "error" ] && error 1 "Selected branding is not supported by this firmware"
+
+for oem in ${supported_brandings}; do
+	[ "${firmware_version}" != "$oem" ] && continue
+	echo2 "hardcoding branding in /etc/init.d/rc.conf"
+	modsed "s|^\([ \t]*export OEM\)\$|\1=${firmware_version}|" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf" "export OEM=${firmware_version}\$"
+	return 0
+done
+
+error 1 "Selected branding '${firmware_version}' is not supported by this firmware"
 
