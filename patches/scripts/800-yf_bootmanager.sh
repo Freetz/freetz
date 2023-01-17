6e876ade97d0b636e6a451742e1b9c14877eb065
leo22 <87432648+leo-022@users.noreply.github.com>
2023-01-17 14:05:36 +0100

    Patch 2 (#703)
    * Update 800-yf_bootmanager.sh
    
    YourFritz-Bootmanager funktionierte in FW 7.50 nicht mehr
    Dank an PeterPawn https://www.ip-phone-forum.de/threads/yf-bootmanger-mit-freetz-ng-und-fw-7-50-nicht-nutzbar.314478/post-2502442

--- patches/scripts/800-yf_bootmanager.sh
+++ patches/scripts/800-yf_bootmanager.sh
@@ -7,8 +7,8 @@ pushd "${TOOLS_DIR}/yf/bootmanager" >/dev/null
 TMP="$TEMPDIR" \
   TARGET_SYSTEM_VERSION="${AVM_FW_MAJOR}.${AVM_FW_VERSION}" \
   TARGET_DIR="${ABS_BASE_DIR}/${FILESYSTEM_MOD_DIR}" \
+  TARGET_BRANDING="all" \
   sh "./add_to_system_reboot.sh" 2>&1 | sed 's/^/    /g'
 popd >/dev/null
 rmdir "$TEMPDIR"
 [ "$FREETZ_VERBOSITY_LEVEL" -lt "2" 2>/dev/null ] && exec >$(tty)
-
